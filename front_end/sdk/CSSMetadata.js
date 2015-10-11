ead6dacb6a1c44dbeab2be55a2621182e29381ee
kojii <kojii@chromium.org>
2015-10-11 13:20:23 -0700

    Devtools: Update autocomplete for unprefixed CSS Writing Modes
    Unprefixed CSS Writing Modes properties are added with the up-to-date
    syntax[1] in the following patches:
    
    writing-mode
    https://codereview.chromium.org/1376203003/
    
    text-combine-upright
    text-orientation
    https://codereview.chromium.org/1355513002/
    
    This patch updates autocomplete for these properties.
    
    [1] https://drafts.csswg.org/css-writing-modes/
    
    BUG=533448
    
    Review URL: https://codereview.chromium.org/1381563008
    
    Cr-Original-Commit-Position: refs/heads/master@{#353477}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 5cf706c93a7d93df435f9f50ab6bec072cc4465e

--- front_end/sdk/CSSMetadata.js
+++ front_end/sdk/CSSMetadata.js
@@ -215,6 +215,9 @@ WebInspector.CSSMetadata._propertyDataMap = {
     "box-shadow": { values: [
         "inset", "none"
     ] },
+    "writing-mode": { values: [
+        "horizontal-tb", "vertical-rl", "vertical-lr"
+    ] },
     "-webkit-writing-mode": { values: [
         "lr", "rl", "tb", "lr-tb", "rl-tb", "tb-rl", "horizontal-tb", "vertical-rl", "vertical-lr", "horizontal-bt"
     ] },
@@ -336,8 +339,14 @@ WebInspector.CSSMetadata._propertyDataMap = {
         "lower-armenian", "upper-armenian", "georgian", "cjk-ideographic", "hiragana", "katakana", "hiragana-iroha",
         "katakana-iroha"
     ] },
+    "text-combine-upright": { values: [
+        "none", "all"
+    ] },
     "-webkit-text-combine": { values: [
         "none", "horizontal"
+    ] },
+    "text-orientation": { values: [
+        "mixed", "upright", "sideways"
     ] },
     "outline": { values: [
         "none", "hidden", "inset", "groove", "ridge", "outset", "dotted", "dashed", "solid", "double"

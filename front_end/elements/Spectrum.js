6a1c3e4620ffd6445d90781dcd80db4821547ff9
pfeldman <pfeldman@chromium.org>
2015-10-23 13:34:36 -0700

    Revert of Devtools Color: Ignore focus and use document keydown events instead (patchset #1 id:1 of https://codereview.chromium.org/1419053002/ )
    Reason for revert:
    Breaks editing in devtools after opening color picker. Lets remove the listeners from the elements we add them to. Listening to document's key events is probably not the best idea.
    
    Original issue's description:
    > Devtools Color: Ignore focus and use document keydown events instead
    >
    > In r350929, focus was restored to ensure Esc key continues to work.
    > This change reverts this since it is causing unexpected issues. Document
    > keypress events are used instead when a popover is open.
    >
    > BUG=535838,542077,542078
    >
    > Committed: https://crrev.com/4c152e0a918ad7c81e4266ca289cf95eaa854159
    > Cr-Commit-Position: refs/heads/master@{#355636}
    
    TBR=dgozman@chromium.org,samli@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=535838,542077,542078
    
    Review URL: https://codereview.chromium.org/1406153007
    
    Cr-Original-Commit-Position: refs/heads/master@{#355873}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 87117367e7e9d568bbf97c1756248fa6725513c7

--- front_end/elements/Spectrum.js
+++ front_end/elements/Spectrum.js
@@ -223,6 +223,8 @@ WebInspector.Spectrum.prototype = {
             return;
         if (show)
             this._updatePalettePanel();
+        else
+            WebInspector.restoreFocusFromElement(this.element);
         this._palettePanelShowing = show;
         this.contentElement.classList.toggle("palette-panel-showing", show);
     },

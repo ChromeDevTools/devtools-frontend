08868bd3bf40f6627f723d7536adbb26ec8b0f64
kozyatinskiy <kozyatinskiy@chromium.org>
2015-11-17 12:47:50 -0800

    [DevTools] Show target function in popover
    Popover contains native function for bound function. With this CL it will contain target function description.
    
    R=pfeldman@chromium.org
    
    Review URL: https://codereview.chromium.org/1444213002
    
    Cr-Original-Commit-Position: refs/heads/master@{#360156}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 9f9a2c5916c15d252f5f1f6429d377835720b025

--- front_end/components/ObjectPopoverHelper.js
+++ front_end/components/ObjectPopoverHelper.js
@@ -58,12 +58,13 @@ WebInspector.ObjectPopoverHelper.prototype = {
         /**
          * @param {!WebInspector.RemoteObject} funcObject
          * @param {!Element} popoverContentElement
+         * @param {!Element} popoverValueElement
          * @param {!Element} anchorElement
          * @param {?Array.<!WebInspector.RemoteObjectProperty>} properties
          * @param {?Array.<!WebInspector.RemoteObjectProperty>} internalProperties
          * @this {WebInspector.ObjectPopoverHelper}
          */
-        function didGetFunctionProperties(funcObject, popoverContentElement, anchorElement, properties, internalProperties)
+        function didGetFunctionProperties(funcObject, popoverContentElement, popoverValueElement, anchorElement, properties, internalProperties)
         {
             if (internalProperties) {
                 for (var i = 0; i < internalProperties.length; i++) {
@@ -73,6 +74,7 @@ WebInspector.ObjectPopoverHelper.prototype = {
                     }
                 }
             }
+            WebInspector.ObjectPropertiesSection.formatObjectAsFunction(funcObject, popoverValueElement, true);
             funcObject.functionDetails(didGetFunctionDetails.bind(this, popoverContentElement, anchorElement));
         }
 
@@ -146,13 +148,11 @@ WebInspector.ObjectPopoverHelper.prototype = {
 
                 if (result.type === "string")
                     valueElement.createTextChildren("\"", description, "\"");
-                else if (result.type === "function")
-                    WebInspector.ObjectPropertiesSection.formatObjectAsFunction(result, valueElement, true);
-                else
+                else if (result.type !== "function")
                     valueElement.textContent = description;
 
                 if (result.type === "function") {
-                    result.getOwnProperties(didGetFunctionProperties.bind(this, result, popoverContentElement, anchorElement));
+                    result.getOwnProperties(didGetFunctionProperties.bind(this, result, popoverContentElement, valueElement, anchorElement));
                     return;
                 }
                 popover.showForAnchor(popoverContentElement, anchorElement);

922241f4997cfecb1fbae1a1c75ede0511a92498
lushnikov <lushnikov@chromium.org>
2016-01-12 12:31:11 -0800

    DevTools: use timeout to release ignoreExecutionLineEvents flag
    R=pfeldman, sergeyv
    
    Committed: https://crrev.com/e36a51a56562e3a63925156a2333ecd1f22bc4d2
    Cr-Commit-Position: refs/heads/master@{#366021}
    
    Committed: https://crrev.com/db1bf2f99ca78b6fbfff2ea336d4b0c884148f9b
    Cr-Commit-Position: refs/heads/master@{#366512}
    
    Review URL: https://codereview.chromium.org/1531483002
    
    Cr-Original-Commit-Position: refs/heads/master@{#368975}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 691bc2d3ef7b411cc81f84112384f7ba2c9f76cf

--- front_end/sources/JavaScriptSourceFrame.js
+++ front_end/sources/JavaScriptSourceFrame.js
@@ -266,9 +266,8 @@ WebInspector.JavaScriptSourceFrame.prototype = {
 
     onTextChanged: function(oldRange, newRange)
     {
-        this._scriptsPanel.setIgnoreExecutionLineEvents(true);
+        this._scriptsPanel.updateLastModificationTime();
         WebInspector.UISourceCodeFrame.prototype.onTextChanged.call(this, oldRange, newRange);
-        this._scriptsPanel.setIgnoreExecutionLineEvents(false);
         if (this._compiler)
             this._compiler.scheduleCompile();
     },
@@ -371,20 +370,16 @@ WebInspector.JavaScriptSourceFrame.prototype = {
 
     _workingCopyCommitted: function(event)
     {
+        this._scriptsPanel.updateLastModificationTime();
         if (this._supportsEnabledBreakpointsWhileEditing())
             return;
 
-        if (!this._scriptFileForTarget.size) {
+        if (!this._scriptFileForTarget.size)
             this._restoreBreakpointsAfterEditing();
-            return;
-        }
-
-        this._scriptsPanel.setIgnoreExecutionLineEvents(true);
     },
 
     _didMergeToVM: function()
     {
-        this._scriptsPanel.setIgnoreExecutionLineEvents(false);
         if (this._supportsEnabledBreakpointsWhileEditing())
             return;
         this._updateDivergedInfobar();
@@ -393,7 +388,6 @@ WebInspector.JavaScriptSourceFrame.prototype = {
 
     _didDivergeFromVM: function()
     {
-        this._scriptsPanel.setIgnoreExecutionLineEvents(false);
         if (this._supportsEnabledBreakpointsWhileEditing())
             return;
         this._updateDivergedInfobar();

bf9ddf2b0ab048cd5bf5c72a2ac17d5e4f693e4e
keishi <keishi@chromium.org>
2016-04-14 07:21:40 -0700

    Rename Heap to ThreadHeap
    BUG=591606
    CQ_EXTRA_TRYBOTS=tryserver.chromium.perf:winx64_10_perf_cq;tryserver.chromium.perf:mac_retina_perf_cq;tryserver.chromium.perf:linux_perf_cq
    
    Committed: https://crrev.com/191c640cc25f76c0c0f8a0b448de33cd059f4a8e
    Cr-Commit-Position: refs/heads/master@{#387233}
    
    Review URL: https://codereview.chromium.org/1845543002
    
    Cr-Original-Commit-Position: refs/heads/master@{#387307}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 011b8046a07ac2cedde530a05f81e8fb41f80c62

--- front_end/timeline/TimelineModel.js
+++ front_end/timeline/TimelineModel.js
@@ -156,7 +156,7 @@ WebInspector.TimelineModel.RecordType = {
     ImplSideFling: "InputHandlerProxy::HandleGestureFling::started",
     GCIdleLazySweep: "ThreadState::performIdleLazySweep",
     GCCompleteSweep: "ThreadState::completeSweep",
-    GCCollectGarbage: "Heap::collectGarbage",
+    GCCollectGarbage: "BlinkGCMarking",
 
     // CpuProfile is a virtual event created on frontend to support
     // serialization of CPU Profiles within tracing timeline data.

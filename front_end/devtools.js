212853d09e3bacbc607e2a0281149cf0bd683786
dgozman <dgozman@chromium.org>
2016-04-15 19:22:54 -0700

    [DevTools] Support broken UMA metric from M49 frontend.
    This patch mitigates bad message crash due to wrong UMA metric
    introduced in https://codereview.chromium.org/1520403004.
    It was reverted later, but was not merged to M49.
    
    BUG=603789
    
    Review URL: https://codereview.chromium.org/1892743005
    
    Cr-Original-Commit-Position: refs/heads/master@{#387793}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 92efcfdf8fdbdb17af835a889ed6ee5bed729bca

--- front_end/devtools.js
+++ front_end/devtools.js
@@ -525,6 +525,9 @@ InspectorFrontendHostImpl.prototype = {
      */
     recordEnumeratedHistogram: function(actionName, actionCode, bucketSize)
     {
+        // Support for M49 frontend.
+        if (actionName === "DevTools.DrawerShown")
+            return;
         DevToolsAPI.sendMessageToEmbedder("recordEnumeratedHistogram", [actionName, actionCode, bucketSize], null);
     },
 

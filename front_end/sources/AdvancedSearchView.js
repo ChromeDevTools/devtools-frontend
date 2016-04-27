3dd8c258cd1a3d52d6f9b21401620d5cfe7daf2a
luoe <luoe@chromium.org>
2016-04-27 13:52:28 -0700

    DevTools: Update styles for full text search drawer panel
    BUG=604833
    
    Review-Url: https://codereview.chromium.org/1902283002
    Cr-Original-Commit-Position: refs/heads/master@{#390177}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: de496bd9eb61224a98706299f73b1898a3a210c0

--- front_end/sources/AdvancedSearchView.js
+++ front_end/sources/AdvancedSearchView.js
@@ -18,17 +18,23 @@ WebInspector.AdvancedSearchView = function()
 
     this._searchPanelElement = this.contentElement.createChild("div", "search-drawer-header");
     this._searchPanelElement.addEventListener("keydown", this._onKeyDown.bind(this), false);
+    this._searchPanelElement.addEventListener("keyup", this._onKeyUp.bind(this), false);
 
     this._searchResultsElement = this.contentElement.createChild("div");
     this._searchResultsElement.className = "search-results";
 
     this._search = WebInspector.HistoryInput.create();
     this._searchPanelElement.appendChild(this._search);
-    this._search.placeholder = WebInspector.UIString("Enter query, use `file:` to filter by path");
+    this._search.placeholder = WebInspector.UIString("Search all sources (use \"file:\" to filter by path)");
     this._search.setAttribute("type", "text");
     this._search.classList.add("search-config-search");
     this._search.setAttribute("results", "0");
     this._search.setAttribute("size", 42);
+
+    this._searchPanelElement.createChild("div", "search-icon");
+    this._searchInputClearElement = this._searchPanelElement.createChild("div", "search-cancel-button");
+    this._searchInputClearElement.hidden = true;
+    this._searchInputClearElement.addEventListener("click", this._onSearchInputClear.bind(this), false);
 
     this._ignoreCaseLabel = createCheckboxLabel(WebInspector.UIString("Ignore case"));
     this._ignoreCaseLabel.classList.add("search-config-label");
@@ -99,6 +105,13 @@ WebInspector.AdvancedSearchView.prototype = {
         this._searchMessageElement.textContent = WebInspector.UIString("Indexing\u2026");
         this._progressIndicator.show(this._searchProgressPlaceholderElement);
         this._searchScope.performIndexing(new WebInspector.ProgressProxy(this._progressIndicator, this._onIndexingFinished.bind(this)));
+    },
+
+    _onSearchInputClear: function()
+    {
+        this._search.value = "";
+        this.focus();
+        this._searchInputClearElement.hidden = true;
     },
 
     /**
@@ -283,6 +296,14 @@ WebInspector.AdvancedSearchView.prototype = {
         }
     },
 
+    _onKeyUp: function()
+    {
+        if (this._search.value && this._search.value.length)
+            this._searchInputClearElement.hidden = false;
+        else
+            this._searchInputClearElement.hidden = true;
+    },
+
     _save: function()
     {
         this._advancedSearchConfig.set(this._buildSearchConfig().toPlainObject());
@@ -294,6 +315,8 @@ WebInspector.AdvancedSearchView.prototype = {
         this._search.value = searchConfig.query();
         this._ignoreCaseCheckbox.checked = searchConfig.ignoreCase();
         this._regexCheckbox.checked = searchConfig.isRegex();
+        if (this._search.value && this._search.value.length)
+            this._searchInputClearElement.hidden = false;
     },
 
     _onAction: function()

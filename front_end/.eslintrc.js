e917ee3a57259f4571cebfeabbed9945a2b87fe3
chenwilliam <chenwilliam@chromium.org>
2016-09-19 18:53:37 -0700

    DevTools: Add eslint rules
    I added rules that our codebase already has been following, but codifying
    it in eslint helps us catch them before code review and really helps external
    contributors who are less familiar with our code style.
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2339003002
    Cr-Original-Commit-Position: refs/heads/master@{#419642}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 42c1091e98b6546621a43f5ae099e1e499c0489a

--- front_end/.eslintrc.js
+++ front_end/.eslintrc.js
@@ -38,6 +38,9 @@ module.exports = {
             "getWithoutSet": false,
             "setWithoutGet": false
         }],
+        "new-parens": 2,
+        "func-call-spacing": 2,
+        "semi-spacing": 2,
 
         // anti-patterns
         "no-with": 2,
@@ -49,6 +52,15 @@ module.exports = {
         "no-octal-escape": 2,
         "no-self-compare": 2,
         "no-shadow-restricted-names": 2,
+        "no-cond-assign": 2,
+        "no-debugger": 2,
+        "no-dupe-keys": 2,
+        "no-duplicate-case": 2,
+        "no-empty-character-class": 2,
+        "no-unreachable": 2,
+        "no-unsafe-negation": 2,
+        "radix": 2,
+        "valid-typeof": 2,
 
         // es2015 features
         "no-useless-constructor": 2,

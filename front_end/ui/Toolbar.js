f21e360402310c4656b424f15389d7df505dd040
luoe <luoe@chromium.org>
2016-11-02 18:15:51 -0700

    DevTools: throttle calls to ToolbarMenuButton.trigger() to avoid bad context menu state on Mac
    WI.ToolbarMenuButton shows a real (not-soft) contextMenu on click. It is
    possible to reach a bad state on Mac OS by focusing on a different window, then
    making two fast clicks on a ToolbarMenuButton (e.g. 3 dot drawer menu) in the
    originally unfocused DevTools. On the first mouse press, nothing happens, but on
    the second, two mousedown events are sent within 300ms of each other. The
    context menu starts to open, but is cleared almost immediately afterwards. After
    that, the button no longer shows context menus.
    
    Showing a real context menu on click only happens on ToolbarMenuButton, and
    throttling its trigger function will fix the symptom. The core issue is tracked
    at crbug.com/661766.
    
    BUG=655556
    
    Review-Url: https://codereview.chromium.org/2469813005
    Cr-Original-Commit-Position: refs/heads/master@{#429498}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: b231468fd5825415fff761c25f561c8c782ab25c

--- front_end/ui/Toolbar.js
+++ front_end/ui/Toolbar.js
@@ -657,11 +657,17 @@ WebInspector.ToolbarMenuButton = class extends WebInspector.ToolbarButton {
    */
   _trigger(event) {
     delete this._triggerTimeout;
+
+    // Throttling avoids entering a bad state on Macs when rapidly triggering context menus just
+    // after the window gains focus. See crbug.com/655556
+    if (this._lastTriggerTime && Date.now() - this._lastTriggerTime < 300)
+      return;
     var contextMenu = new WebInspector.ContextMenu(
         event, this._useSoftMenu, this.element.totalOffsetLeft(),
         this.element.totalOffsetTop() + this.element.offsetHeight);
     this._contextMenuHandler(contextMenu);
     contextMenu.show();
+    this._lastTriggerTime = Date.now();
   }
 
   /**

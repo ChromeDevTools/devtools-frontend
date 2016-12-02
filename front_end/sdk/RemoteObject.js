a471b665f259e0959c73061ec4b8458fea0f7b3b
luoe <luoe@chromium.org>
2016-12-02 13:34:36 -0800

    DevTools: rebaseline tests after V8 roll
    Test expectations were modified for tests that included the old format for
    descriptions 'Array[2]'. Since the V8 change landed that updated descriptions
    to 'Array(2)', this CL manually rebaselines the tests that failed.
    
    CLs that marked expectations:
    https://codereview.chromium.org/2524913002/
    https://codereview.chromium.org/2550513002/
    
    BUG=405845
    
    Review-Url: https://codereview.chromium.org/2543303002
    Cr-Original-Commit-Position: refs/heads/master@{#436036}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: a464b6447319d296f677b4250c1b0d858eb3ed5e

--- front_end/sdk/RemoteObject.js
+++ front_end/sdk/RemoteObject.js
@@ -70,6 +70,8 @@ SDK.RemoteObject = class {
   static arrayLength(object) {
     if (object.subtype !== 'array' && object.subtype !== 'typedarray')
       return 0;
+    // Array lengths in V8-generated descriptions switched from square brackets to parentheses.
+    // Both formats are checked in case the front end is dealing with an old version of V8.
     var matches = object.description.match(/\[([0-9]+)\]/) || object.description.match(/\(([0-9]+)\)/);
     if (!matches)
       return 0;

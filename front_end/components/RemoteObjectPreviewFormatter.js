4a78428a8dbb800411ae59fc16031ed9eb7ba4e2
luoe <luoe@chromium.org>
2016-12-15 20:40:04 -0800

    DevTools: add tooltip for truncated classnames and full name for title
    This CL adds a tooltip for name abbreviations and no longer truncates the object
    name when it is the title. Long names in previews are still truncated.
    
    BUG=667614
    
    Review-Url: https://codereview.chromium.org/2553293002
    Cr-Original-Commit-Position: refs/heads/master@{#439024}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: aa907d22729b4080eb1f3b15f0296b27799cfba0

--- front_end/components/RemoteObjectPreviewFormatter.js
+++ front_end/components/RemoteObjectPreviewFormatter.js
@@ -28,10 +28,8 @@ Components.RemoteObjectPreviewFormatter = class {
       parentElement.appendChild(this.renderPropertyPreview(preview.type, preview.subtype, description));
       return;
     }
-    if (description && preview.subtype !== 'array') {
-      var text = preview.subtype ? description : this._abbreviateFullQualifiedClassName(description);
-      parentElement.createTextChildren(text, ' ');
-    }
+    if (description && preview.subtype !== 'array')
+      parentElement.createTextChildren(description, ' ');
 
     var isArray = preview.subtype === 'array' || preview.subtype === 'typedarray';
     parentElement.createTextChild(isArray ? '[' : '{');
@@ -218,6 +216,7 @@ Components.RemoteObjectPreviewFormatter = class {
 
     if (type === 'object' && !subtype) {
       span.textContent = this._abbreviateFullQualifiedClassName(description);
+      span.title = description;
       return span;
     }
 

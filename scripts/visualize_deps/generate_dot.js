530179e715b78c8e66eb09a393cce29dfdf5d4d7
pfeldman <pfeldman@chromium.org>
2017-01-11 18:28:10 -0800

    DevTools: generate dot file with module sizes in visualizer.
    Review-Url: https://codereview.chromium.org/2620363003
    Cr-Original-Commit-Position: refs/heads/master@{#443118}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: e62b61b8ae8d5679c3433e616ba0c88f01d0592c

--- scripts/visualize_deps/generate_dot.js
+++ scripts/visualize_deps/generate_dot.js
@@ -15,13 +15,31 @@ const OUT = path.resolve(__dirname, 'out', 'dependencies.dot');
 function main() {
   const modules = new Set();
   const moduleToDependencyList = ['digraph dependencies {'];
+  moduleToDependencyList.push('fixedsize = true;')
   fs.readdirSync(FRONTEND_PATH).forEach(function(file) {
     const moduleJSONPath = path.join(FRONTEND_PATH, file, 'module.json');
     if (fs.statSync(path.join(FRONTEND_PATH, file)).isDirectory() &&
       utils.isFile(moduleJSONPath)) {
       const module = file;
+      if (module === 'audits2_worker')
+        return;
       modules.add(module);
       const moduleJSON = require(moduleJSONPath);
+      let moduleSize = 0;
+
+      let resources = (moduleJSON.scripts || []).concat((moduleJSON.resources || []));
+      for (let script of resources) {
+        if (fs.existsSync(path.join(FRONTEND_PATH, module, script))) {
+          console.log(path.join(FRONTEND_PATH, module, script));
+          moduleSize += fs.statSync(path.join(FRONTEND_PATH, module, script)).size;
+        }
+      }
+      moduleSize /= 200000;
+      moduleSize = Math.max(0.5, moduleSize);
+      let fontSize = Math.max(moduleSize*14, 14);
+
+      moduleToDependencyList.push(`${module} [width=${moduleSize}, height=${moduleSize} fontsize=${fontSize}];`);
+
       if (moduleJSON.dependencies) {
         for (let d of moduleJSON.dependencies) {
           moduleToDependencyList.push(`  ${module} -> ${d}`);

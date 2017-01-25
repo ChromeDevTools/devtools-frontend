461e349055581393a4d391840d1ffe4acb51627b
lushnikov <lushnikov@chromium.org>
2017-01-25 01:11:59 -0800

    Revert of DevTools: Correct Source Panel Show/Hide Toolbar buttons (patchset #1 id:1 of https://codereview.chromium.org/2600493002/ )
    Reason for revert:
    It turned out the icons are actually wrong. Compare:
    - Without this patch (correct): http://imgur.com/a/eFCnz
    - With this patch (incorrect): http://imgur.com/a/gJ24q
    
    Original issue's description:
    > DevTools: Correct Source Panel Show/Hide Toolbar buttons
    >
    > Regression : Misalignment of 'Hide Navigator' and 'Hide Debugger' buttons is seen in devtools.
    >
    > Flipped mask positions and removed—now unnecessary—rotation transforms around show/hide panel icon containers.
    >
    > BUG=671930
    > R=lushnikov@chromium.org
    > TEST=Open Dev Tools, open Sources Pane, observe show/hide sidebar buttons
    > are correctly aligned. crbug.com/671930
    >
    > Review-Url: https://codereview.chromium.org/2600493002
    > Cr-Commit-Position: refs/heads/master@{#445339}
    > Committed: https://chromium.googlesource.com/chromium/src/+/4d2364eb32a5b242790324536efccd843cb81aa8
    
    TBR=dgozman@chromium.org,pfeldman@chromium.org,me@jakehendy.com
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=671930
    
    Review-Url: https://codereview.chromium.org/2655793003
    Cr-Original-Commit-Position: refs/heads/master@{#445983}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: d52b38109b89d346334cc0bc582328d6131fe720

--- front_end/ui/Icon.js
+++ front_end/ui/Icon.js
@@ -170,8 +170,10 @@ UI.Icon.Descriptors = {
 
   'largeicon-show-left-sidebar': {x: -160, y: -72, width: 28, height: 24, spritesheet: 'largeicons', isMask: true},
   'largeicon-hide-left-sidebar': {x: -192, y: -72, width: 28, height: 24, spritesheet: 'largeicons', isMask: true},
-  'largeicon-show-right-sidebar': {x: -192, y: -72, width: 28, height: 24, spritesheet: 'largeicons', isMask: true},
-  'largeicon-hide-right-sidebar': {x: -160, y: -72, width: 28, height: 24, spritesheet: 'largeicons', isMask: true},
+  'largeicon-show-right-sidebar':
+      {x: -160, y: -72, width: 28, height: 24, spritesheet: 'largeicons', isMask: true, transform: 'rotate(180deg)'},
+  'largeicon-hide-right-sidebar':
+      {x: -192, y: -72, width: 28, height: 24, spritesheet: 'largeicons', isMask: true, transform: 'rotate(180deg)'},
   'largeicon-show-top-sidebar': {
     x: -160,
     y: -72,

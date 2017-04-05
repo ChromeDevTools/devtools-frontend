45903b7bf88f1587169007465b8e027cda84a107
eostroukhov <eostroukhov@chromium.org>
2017-04-05 09:14:26 -0700

    [DevTools] Pass sidebar and not the panel.
    BUG=707950
    
    Review-Url: https://codereview.chromium.org/2793203002
    Cr-Original-Commit-Position: refs/heads/master@{#462096}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: de3db7c8aa94857f8360b94b66dfaa3d3caf51ce

--- front_end/resources/ApplicationPanelSidebar.js
+++ front_end/resources/ApplicationPanelSidebar.js
@@ -736,7 +736,7 @@ Resources.DatabaseTreeElement = class extends Resources.BaseStorageTreeElement {
     function tableNamesCallback(tableNames) {
       var tableNamesLength = tableNames.length;
       for (var i = 0; i < tableNamesLength; ++i)
-        this.appendChild(new Resources.DatabaseTableTreeElement(this._storagePanel, this._database, tableNames[i]));
+        this.appendChild(new Resources.DatabaseTableTreeElement(this._sidebar, this._database, tableNames[i]));
     }
     this._database.getTableNames(tableNamesCallback.bind(this));
   }
@@ -746,6 +746,11 @@ Resources.DatabaseTreeElement = class extends Resources.BaseStorageTreeElement {
  * @unrestricted
  */
 Resources.DatabaseTableTreeElement = class extends Resources.BaseStorageTreeElement {
+  /**
+   * @param {!Resources.ApplicationPanelSidebar} sidebar
+   * @param {!Resources.Database} database
+   * @param {string} tableName
+   */
   constructor(sidebar, database, tableName) {
     super(sidebar._panel, tableName, false);
     this._sidebar = sidebar;

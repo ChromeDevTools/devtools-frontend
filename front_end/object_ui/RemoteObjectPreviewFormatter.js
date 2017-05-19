933ca5a7c5cce36a5cd13860c53fd1816c02fd95
luoe <luoe@chromium.org>
2017-05-18 20:42:39 -0700

    DevTools: iterate on object preview style
    This CL changes the following:
    - Regexes, errors, entries do not need a value preview
    - Adds a comma before the last ellipsis at the end of an overflowing preview
    
    Screenshot:
    http://imgur.com/a/emsR8
    
    BUG=405845
    
    Review-Url: https://codereview.chromium.org/2717853002
    Cr-Original-Commit-Position: refs/heads/master@{#473063}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 636de3e97a000f91960ee9382c010d9a7e7e17d4

--- front_end/object_ui/RemoteObjectPreviewFormatter.js
+++ front_end/object_ui/RemoteObjectPreviewFormatter.js
@@ -26,7 +26,9 @@ ObjectUI.RemoteObjectPreviewFormatter = class {
   appendObjectPreview(parentElement, preview, isEntry) {
     const previewExperimentEnabled = Runtime.experiments.isEnabled('objectPreviews');
     var description = preview.description;
-    if (preview.type !== 'object' || preview.subtype === 'null' || (previewExperimentEnabled && isEntry)) {
+    var subTypesWithoutValuePreview = new Set(['null', 'regexp', 'error', 'internal#entry']);
+    if (preview.type !== 'object' || subTypesWithoutValuePreview.has(preview.subtype) ||
+        (previewExperimentEnabled && isEntry)) {
       parentElement.appendChild(this.renderPropertyPreview(preview.type, preview.subtype, description));
       return;
     }
@@ -54,8 +56,10 @@ ObjectUI.RemoteObjectPreviewFormatter = class {
       this._appendArrayPropertiesPreview(propertiesElement, preview);
     else
       this._appendObjectPropertiesPreview(propertiesElement, preview);
-    if (preview.overflow)
-      propertiesElement.createChild('span').textContent = '\u2026';
+    if (preview.overflow) {
+      var ellipsisText = propertiesElement.textContent.length > 1 ? ',\u00a0\u2026' : '\u2026';
+      propertiesElement.createChild('span').textContent = ellipsisText;
+    }
     propertiesElement.createTextChild(isArrayOrTypedArray ? ']' : '}');
   }
 

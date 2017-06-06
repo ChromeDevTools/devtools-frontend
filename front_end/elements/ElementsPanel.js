7bfc472b92e68fae61ef120207f7bce959682a1b
luoe <luoe@chromium.org>
2017-06-05 20:59:50 -0700

    DevTools: jsdoc for showToolbarPane in Elements
    This is part of an effort to migrate SSP to shadow DOM as its own component.
    
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2915233002
    Cr-Original-Commit-Position: refs/heads/master@{#477187}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: a91ddbd742e1c7aaf65dff31a1aaf4b1a95f68d4

--- front_end/elements/ElementsPanel.js
+++ front_end/elements/ElementsPanel.js
@@ -68,7 +68,14 @@ Elements.ElementsPanel = class extends UI.Panel {
     this._breadcrumbs.show(crumbsContainer);
     this._breadcrumbs.addEventListener(Elements.ElementsBreadcrumbs.Events.NodeSelected, this._crumbNodeSelected, this);
 
+    /** @type {?UI.Widget} */
     this._currentToolbarPane = null;
+    /** @type {?UI.Widget} */
+    this._animatedToolbarPane = null;
+    /** @type {?UI.Widget} */
+    this._pendingWidget = null;
+    /** @type {?UI.ToolbarToggle} */
+    this._pendingWidgetToggle = null;
 
     this._stylesWidget = new Elements.StylesSidebarPane();
     this._computedStyleWidget = new Elements.ComputedStyleWidget();
@@ -141,14 +148,14 @@ Elements.ElementsPanel = class extends UI.Panel {
 
   /**
    * @param {?UI.Widget} widget
-   * @param {!UI.ToolbarToggle=} toggle
+   * @param {?UI.ToolbarToggle} toggle
    */
   showToolbarPane(widget, toggle) {
     if (this._pendingWidgetToggle)
       this._pendingWidgetToggle.setToggled(false);
     this._pendingWidgetToggle = toggle;
 
-    if (this._animatedToolbarPane !== undefined)
+    if (this._animatedToolbarPane)
       this._pendingWidget = widget;
     else
       this._startToolbarPaneAnimation(widget);
@@ -198,11 +205,11 @@ Elements.ElementsPanel = class extends UI.Panel {
       this._currentToolbarPane = this._animatedToolbarPane;
       if (this._currentToolbarPane)
         this._currentToolbarPane.focus();
-      delete this._animatedToolbarPane;
+      this._animatedToolbarPane = null;
 
-      if (this._pendingWidget !== undefined) {
+      if (this._pendingWidget) {
         this._startToolbarPaneAnimation(this._pendingWidget);
-        delete this._pendingWidget;
+        this._pendingWidget = null;
       }
     }
   }
@@ -831,7 +838,7 @@ Elements.ElementsPanel = class extends UI.Panel {
     }
 
     this._splitWidget.setVertical(this._splitMode === Elements.ElementsPanel._splitMode.Vertical);
-    this.showToolbarPane(null);
+    this.showToolbarPane(null /* widget */, null /* toggle */);
 
     var matchedStylesContainer = new UI.VBox();
     matchedStylesContainer.element.appendChild(this._stylesSidebarToolbar);

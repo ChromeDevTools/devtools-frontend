8a6afdcb4c12d648b869196888fbb9e8aea6bbe1
Eugene Ostroukhov <eostroukhov@chromium.org>
2017-06-05 18:01:35 -0700

    [DevTools] Display usage & quota information
    Adds a simple display for temporary and persistent storage information.
    
    Bug: 619521
    Change-Id: Idad89ab215ba0ab6dcea2a37ef8e68d4b38d3244
    Reviewed-on: https://chromium-review.googlesource.com/517920
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Eugene Ostroukhov <eostroukhov@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#477413}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 910a7bdf5cf34e70b2fb46bb70ad4746a0a892a3

--- front_end/resources/ClearStorageView.js
+++ front_end/resources/ClearStorageView.js
@@ -3,7 +3,6 @@
 // found in the LICENSE file.
 /**
  * @implements {SDK.TargetManager.Observer}
- * @unrestricted
  */
 Resources.ClearStorageView = class extends UI.VBox {
   constructor() {
@@ -13,6 +12,11 @@ Resources.ClearStorageView = class extends UI.VBox {
     this._reportView.registerRequiredCSS('resources/clearStorageView.css');
     this._reportView.element.classList.add('clear-storage-header');
     this._reportView.show(this.contentElement);
+    /** @type {?SDK.Target} */
+    this._target = null;
+    /** @type {?string} */
+    this._securityOrigin = null;
+    this._throttler = new Common.Throttler(1000);
 
     this._settings = new Map();
     for (var type
@@ -22,6 +26,8 @@ Resources.ClearStorageView = class extends UI.VBox {
                  Protocol.Storage.StorageType.Websql])
       this._settings.set(type, Common.settings.createSetting('clear-storage-' + type, true));
 
+    var quota = this._reportView.appendSection(Common.UIString('Usage'));
+    this._quotaRow = quota.appendRow();
 
     var application = this._reportView.appendSection(Common.UIString('Application'));
     this._appendItem(application, Common.UIString('Unregister service workers'), 'service_workers');
@@ -41,6 +47,9 @@ Resources.ClearStorageView = class extends UI.VBox {
     this._clearButton = UI.createTextButton(
         Common.UIString('Clear site data'), this._clear.bind(this), Common.UIString('Clear site data'));
     footer.appendChild(this._clearButton);
+
+
+    this._refreshQuota();
   }
 
   /**
@@ -96,6 +105,8 @@ Resources.ClearStorageView = class extends UI.VBox {
   }
 
   _clear() {
+    if (!this._securityOrigin)
+      return;
     var storageTypes = [];
     for (var type of this._settings.keys()) {
       if (this._settings.get(type).get())
@@ -154,5 +165,39 @@ Resources.ClearStorageView = class extends UI.VBox {
       this._clearButton.disabled = false;
       this._clearButton.textContent = label;
     }, 500);
+  }
+
+  /**
+   * @param {?Protocol.Storage.QuotaAndUsage} quotaAndUsage
+   */
+  _updateQuotaDisplay(quotaAndUsage) {
+    this._quotaRow.textContent = '';
+
+    if (!quotaAndUsage)
+      return;
+
+    this._quotaRow.textContent = Common.UIString(
+        '%s storage quota used out of %s', Number.bytesToString(quotaAndUsage.usage),
+        Number.bytesToString(quotaAndUsage.quota));
+  }
+
+  /**
+   * @return {!Promise<?>}
+   */
+  _refreshQuota() {
+    if (this.isShowing())
+      this._throttler.schedule(this._refreshQuota.bind(this));
+
+    if (!this._securityOrigin)
+      return Promise.resolve(true);
+
+    return this._target.storageAgent().getUsageAndQuota(this._securityOrigin).then(this._updateQuotaDisplay.bind(this));
+  }
+
+  /**
+   * @override
+   */
+  wasShown() {
+    this._refreshQuota();
   }
 };

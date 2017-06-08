93ae4607d64583f50d5e0848deca8095497b4bd9
dgozman <dgozman@chromium.org>
2017-06-08 10:48:46 -0700

    Revert of [DevTools] Show icon in top toolbar when Node target is available (patchset #3 id:40001 of https://codereview.chromium.org/2890973002/ )
    Reason for revert:
    See issue 727517
    
    Original issue's description:
    > [DevTools] Show icon in top toolbar when Node target is available
    >
    > Clicking on it opens dedicated Node frontend.
    >
    > BUG=706916
    >
    > Review-Url: https://codereview.chromium.org/2890973002
    > Cr-Commit-Position: refs/heads/master@{#472739}
    > Committed: https://chromium.googlesource.com/chromium/src/+/ad3ffab411cc396ffb20c1d35dcff6de038f14b4
    
    TBR=pfeldman@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=706916
    
    Review-Url: https://codereview.chromium.org/2932703002
    Cr-Original-Commit-Position: refs/heads/master@{#478017}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 24309d877571d159bc14f871671ff0630d6f8d32

--- front_end/sources/ThreadsSidebarPane.js
+++ front_end/sources/ThreadsSidebarPane.js
@@ -16,7 +16,14 @@ Sources.ThreadsSidebarPane = class extends UI.VBox {
     this._list = new UI.ListControl(this._items, this, UI.ListMode.NonViewport);
     this.contentElement.appendChild(this._list.element);
 
+    this._availableNodeTargetsElement = this.contentElement.createChild('div', 'hidden available-node-targets');
+
     UI.context.addFlavorChangeListener(SDK.Target, this._targetFlavorChanged, this);
+
+    SDK.targetManager.addEventListener(
+        SDK.TargetManager.Events.AvailableNodeTargetsChanged, this._availableNodeTargetsChanged, this);
+    this._availableNodeTargetsChanged();
+
     SDK.targetManager.observeModels(SDK.DebuggerModel, this);
   }
 
@@ -25,7 +32,27 @@ Sources.ThreadsSidebarPane = class extends UI.VBox {
    */
   static shouldBeShown() {
     var minJSTargets = Runtime.queryParam('nodeFrontend') ? 1 : 2;
-    return SDK.targetManager.models(SDK.DebuggerModel).length >= minJSTargets;
+    if (SDK.targetManager.models(SDK.DebuggerModel).length >= minJSTargets)
+      return true;
+    return !!SDK.targetManager.availableNodeTargetsCount();
+  }
+
+  _availableNodeTargetsChanged() {
+    var count = SDK.targetManager.availableNodeTargetsCount();
+    if (!count) {
+      this._availableNodeTargetsElement.classList.add('hidden');
+      return;
+    }
+    this._availableNodeTargetsElement.removeChildren();
+    this._availableNodeTargetsElement.createTextChild(
+        count === 1 ? Common.UIString('Node instance available.') :
+                      Common.UIString('%d Node instances available.', count));
+    var link = this._availableNodeTargetsElement.createChild('span', 'link');
+    link.textContent = Common.UIString('Connect');
+    link.addEventListener('click', () => {
+      InspectorFrontendHost.openNodeFrontend();
+    }, false);
+    this._availableNodeTargetsElement.classList.remove('hidden');
   }
 
   /**

69b6c492b29d561022ed368a53fe9e2700fb6b0e
Alexei Filippov <alph@chromium.org>
2017-06-16 19:35:30 -0700

    DevTools: Fix CPU profile flamechart empty.
    Bug: 734239
    Change-Id: I509acca90a007c7705750d97c969ec028e0632e9
    Reviewed-on: https://chromium-review.googlesource.com/539132
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Commit-Queue: Alexei Filippov <alph@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#480588}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: c81a6b895381f8d921cbee83bc814710856cdec2

--- front_end/perf_ui/FlameChart.js
+++ front_end/perf_ui/FlameChart.js
@@ -1152,6 +1152,7 @@ PerfUI.FlameChart = class extends UI.VBox {
     var groupStack = [{nestingLevel: -1, visible: true}];
     var lastGroupLevel = Math.max(levelCount, groups.length ? groups.peekLast().startLevel + 1 : 0);
     for (var level = 0; level < lastGroupLevel; ++level) {
+      var parentGroupIsVisible = true;
       while (groupIndex < groups.length - 1 && level === groups[groupIndex + 1].startLevel) {
         ++groupIndex;
         var style = groups[groupIndex].style;
@@ -1162,7 +1163,7 @@ PerfUI.FlameChart = class extends UI.VBox {
         }
         var thisGroupIsVisible =
             groupIndex >= 0 && this._isGroupCollapsible(groupIndex) ? groups[groupIndex].expanded : true;
-        var parentGroupIsVisible = groupStack.peekLast().visible;
+        parentGroupIsVisible = groupStack.peekLast().visible;
         visible = thisGroupIsVisible && parentGroupIsVisible;
         groupStack.push({nestingLevel: style.nestingLevel, visible: visible});
         if (parentGroupIsVisible)

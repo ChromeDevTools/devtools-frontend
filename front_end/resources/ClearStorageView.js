673986c1387520e26e96fe97cee810e4d4bac6fa
kristipark <kristipark@chromium.org>
2017-06-22 10:20:43 -0700

    [DevTools] [Quota] Display usage per storage type
    Bug: 729772
    Change-Id: I34b16707b790f65faba01bdcc44bbd98c911b93b
    Reviewed-on: https://chromium-review.googlesource.com/541981
    Commit-Queue: Kristi Park <kristipark@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#481713}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 19f69e905f6158f2d22afd25ec20b52b36916639

--- front_end/resources/ClearStorageView.js
+++ front_end/resources/ClearStorageView.js
@@ -7,6 +7,15 @@
 Resources.ClearStorageView = class extends UI.ThrottledWidget {
   constructor() {
     super(true, 1000);
+    this._pieColors = [
+      'rgba(110, 161, 226, 1)',  // blue
+      'rgba(229, 113, 113, 1)',  // red
+      'rgba(239, 196, 87, 1)',   // yellow
+      'rgba(155, 127, 230, 1)',  // purple
+      'rgba(116, 178, 102, 1)',  // green
+      'rgba(255, 167, 36, 1)',   // orange
+      'rgba(203, 220, 56, 1)',   // lime
+    ];
 
     this._reportView = new UI.ReportView(Common.UIString('Clear storage'));
     this._reportView.registerRequiredCSS('resources/clearStorageView.css');
@@ -27,13 +36,18 @@ Resources.ClearStorageView = class extends UI.ThrottledWidget {
 
     var quota = this._reportView.appendSection(Common.UIString('Usage'));
     this._quotaRow = quota.appendRow();
+    this._pieChart = new PerfUI.PieChart(110, Number.bytesToString, true);
+    this._pieChartLegend = createElementWithClass('div', 'legend');
+    var usageBreakdownRow = quota.appendRow();
+    usageBreakdownRow.appendChild(this._pieChart.element);
+    usageBreakdownRow.appendChild(this._pieChartLegend);
 
     var application = this._reportView.appendSection(Common.UIString('Application'));
     this._appendItem(application, Common.UIString('Unregister service workers'), 'service_workers');
 
     var storage = this._reportView.appendSection(Common.UIString('Storage'));
     this._appendItem(storage, Common.UIString('Local and session storage'), 'local_storage');
-    this._appendItem(storage, Common.UIString('Indexed DB'), 'indexeddb');
+    this._appendItem(storage, Common.UIString('IndexedDB'), 'indexeddb');
     this._appendItem(storage, Common.UIString('Web SQL'), 'websql');
     this._appendItem(storage, Common.UIString('Cookies'), 'cookies');
 
@@ -176,19 +190,82 @@ Resources.ClearStorageView = class extends UI.ThrottledWidget {
     var response = await this._target.storageAgent().invoke_getUsageAndQuota({origin: securityOrigin});
     if (response[Protocol.Error]) {
       this._quotaRow.textContent = '';
+      this._resetPieChart(0);
       return;
     }
     this._quotaRow.textContent = Common.UIString(
-        '%s storage quota used out of %s', Number.bytesToString(response.usage), Number.bytesToString(response.quota));
+        '%s used out of %s storage quota', Number.bytesToString(response.usage), Number.bytesToString(response.quota));
 
-    this._usageUpdatedForTest(response.usage, response.quota);
+    this._resetPieChart(response.usage);
+    var colorIndex = 0;
+    for (var usageForType of response.usageBreakdown) {
+      if (!usageForType.usage)
+        continue;
+      if (colorIndex === this._pieColors.length)
+        colorIndex = 0;
+      this._appendLegendRow(
+          this._getStorageTypeName(usageForType.storageType), usageForType.usage, this._pieColors[colorIndex++]);
+    }
+
+    this._usageUpdatedForTest(response.usage, response.quota, response.usageBreakdown);
     this.update();
+  }
+
+  _formatPieChartBytes(value) {
+    return Number.bytesToString(value);
+  }
+
+  /**
+   * @param {number} total
+   */
+  _resetPieChart(total) {
+    this._pieChart.setTotal(total);
+    this._pieChartLegend.removeChildren();
+  }
+
+  /**
+   * @param {string} title
+   * @param {number} value
+   * @param {string} color
+   */
+  _appendLegendRow(title, value, color) {
+    if (!value)
+      return;
+    this._pieChart.addSlice(value, color);
+    var rowElement = this._pieChartLegend.createChild('div');
+    rowElement.createChild('span', 'usage-breakdown-legend-value').textContent = Number.bytesToString(value);
+    rowElement.createChild('span', 'usage-breakdown-legend-swatch').style.backgroundColor = color;
+    rowElement.createChild('span', 'usage-breakdown-legend-title').textContent = title;
+  }
+
+  /**
+   * @param {string} type
+   * @return {string}
+   */
+  _getStorageTypeName(type) {
+    switch (type) {
+      case Protocol.Storage.StorageType.File_systems:
+        return Common.UIString('File System');
+      case Protocol.Storage.StorageType.Websql:
+        return Common.UIString('Web SQL');
+      case Protocol.Storage.StorageType.Appcache:
+        return Common.UIString('Application Cache');
+      case Protocol.Storage.StorageType.Indexeddb:
+        return Common.UIString('IndexedDB');
+      case Protocol.Storage.StorageType.Cache_storage:
+        return Common.UIString('Cache Storage');
+      case Protocol.Storage.StorageType.Service_workers:
+        return Common.UIString('Service Workers');
+      default:
+        return Common.UIString('Other');
+    }
   }
 
   /**
    * @param {number} usage
    * @param {number} quota
+   * @param {!Array<!Protocol.Storage.UsageForType>} usageBreakdown
    */
-  _usageUpdatedForTest(usage, quota) {
+  _usageUpdatedForTest(usage, quota, usageBreakdown) {
   }
 };

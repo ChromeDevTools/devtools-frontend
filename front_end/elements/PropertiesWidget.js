d83af15e643a19a04c08ebad9a4636f7bcbe6e7c
luoe <luoe@chromium.org>
2017-07-20 16:37:43 -0700

    DevTools: migrate PropertiesWidget to shadow
    BUG=726972
    
    Review-Url: https://codereview.chromium.org/2978713002
    Cr-Original-Commit-Position: refs/heads/master@{#488482}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 23eca3d61bd69eeaec6ca6554f7fb99a7c566d14

--- front_end/elements/PropertiesWidget.js
+++ front_end/elements/PropertiesWidget.js
@@ -32,7 +32,8 @@
  */
 Elements.PropertiesWidget = class extends UI.ThrottledWidget {
   constructor() {
-    super();
+    super(true /* isWebComponent */);
+    this.registerRequiredCSS('elements/propertiesWidget.css');
 
     SDK.targetManager.addModelListener(SDK.DOMModel, SDK.DOMModel.Events.AttrModified, this._onNodeChange, this);
     SDK.targetManager.addModelListener(SDK.DOMModel, SDK.DOMModel.Events.AttrRemoved, this._onNodeChange, this);
@@ -65,7 +66,7 @@ Elements.PropertiesWidget = class extends UI.ThrottledWidget {
     }
 
     if (!this._node) {
-      this.element.removeChildren();
+      this.contentElement.removeChildren();
       this.sections = [];
       return Promise.resolve();
     }
@@ -127,7 +128,7 @@ Elements.PropertiesWidget = class extends UI.ThrottledWidget {
       for (var i = 0; i < sections.length; ++i)
         expanded.push(sections[i].expanded);
 
-      this.element.removeChildren();
+      this.contentElement.removeChildren();
       this.sections = [];
 
       // Get array of property user-friendly names.
@@ -140,7 +141,7 @@ Elements.PropertiesWidget = class extends UI.ThrottledWidget {
         var section = new ObjectUI.ObjectPropertiesSection(property, title);
         section.element.classList.add('properties-widget-section');
         this.sections.push(section);
-        this.element.appendChild(section.element);
+        this.contentElement.appendChild(section.element);
         if (expanded[this.sections.length - 1])
           section.expand();
         section.addEventListener(UI.TreeOutline.Events.ElementExpanded, this._propertyExpanded, this);

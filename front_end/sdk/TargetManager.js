b438c487ce3488ff38769ba8d54201ae6cc2da8a
Tsuyoshi Horo <horo@chromium.org>
2017-08-02 23:58:10 +0000

    [DevTools] Enable network capability in worker unconditionally
    We are planning to start the field trial experiment of
    off-main-thread-fetch optimization.
    
    But in the current implementation, web developers need to enable
    'Network in workers' experiment of DevTools to show the network requests
    from workers in DevTools if the optimization is enabled in their Chrome.
    It is very troublesome for them while debugging PWA.
    
    To avoid this problem, this CL enables the network capability in worker
    unconditionally.
    
    Bug: 443374
    Change-Id: I5ed24996cd0ff30a5bd22ce1067f653a5c68ae9b
    Reviewed-on: https://chromium-review.googlesource.com/595294
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Tsuyoshi Horo <horo@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#491559}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: ae8e4ee9cc4171beadcb571238e5bcb341824245

--- front_end/sdk/TargetManager.js
+++ front_end/sdk/TargetManager.js
@@ -466,12 +466,8 @@ SDK.ChildTargetManager = class {
    * @return {number}
    */
   _capabilitiesForType(type) {
-    if (type === 'worker') {
-      if (Runtime.experiments.isEnabled('networkInWorkers'))
-        return SDK.Target.Capability.JS | SDK.Target.Capability.Log | SDK.Target.Capability.Network;
-      else
-        return SDK.Target.Capability.JS | SDK.Target.Capability.Log;
-    }
+    if (type === 'worker')
+      return SDK.Target.Capability.JS | SDK.Target.Capability.Log | SDK.Target.Capability.Network;
     if (type === 'service_worker')
       return SDK.Target.Capability.Log | SDK.Target.Capability.Network | SDK.Target.Capability.Target;
     if (type === 'iframe') {

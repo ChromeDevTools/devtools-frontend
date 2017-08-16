2ad81e00a62b76fa4e61b306d02380adb4588e96
Dmitry Gozman <dgozman@chromium.org>
2017-08-16 16:13:06 +0000

    [DevTools] Fix shadow editor layout
    Bug: 739255
    Change-Id: I94b7012df8f89165c45e247498885190e0e4ac4e
    Reviewed-on: https://chromium-review.googlesource.com/616220
    Commit-Queue: Andrey Lushnikov <lushnikov@chromium.org>
    Reviewed-by: Andrey Lushnikov <lushnikov@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#494806}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 22579d77792af61fefe6d185f81f036ed3574737

--- front_end/inline_editor/CSSShadowEditor.js
+++ front_end/inline_editor/CSSShadowEditor.js
@@ -11,7 +11,7 @@ InlineEditor.CSSShadowEditor = class extends UI.VBox {
     this.contentElement.tabIndex = 0;
     this.setDefaultFocusedElement(this.contentElement);
 
-    this._typeField = this.contentElement.createChild('div', 'shadow-editor-field');
+    this._typeField = this.contentElement.createChild('div', 'shadow-editor-field shadow-editor-flex-field');
     this._typeField.createChild('label', 'shadow-editor-label').textContent = Common.UIString('Type');
     this._outsetButton = this._typeField.createChild('button', 'shadow-editor-button-left');
     this._outsetButton.textContent = Common.UIString('Outset');
@@ -34,11 +34,12 @@ InlineEditor.CSSShadowEditor = class extends UI.VBox {
     this._xySlider.addEventListener('keydown', this._onCanvasArrowKey.bind(this), false);
     this._xySlider.addEventListener('blur', this._onCanvasBlur.bind(this), false);
 
-    var blurField = this.contentElement.createChild('div', 'shadow-editor-blur-field');
+    var blurField =
+        this.contentElement.createChild('div', 'shadow-editor-field shadow-editor-flex-field shadow-editor-blur-field');
     this._blurInput = this._createTextInput(blurField, Common.UIString('Blur'));
     this._blurSlider = this._createSlider(blurField);
 
-    this._spreadField = this.contentElement.createChild('div', 'shadow-editor-field');
+    this._spreadField = this.contentElement.createChild('div', 'shadow-editor-field shadow-editor-flex-field');
     this._spreadInput = this._createTextInput(this._spreadField, Common.UIString('Spread'));
     this._spreadSlider = this._createSlider(this._spreadField);
   }

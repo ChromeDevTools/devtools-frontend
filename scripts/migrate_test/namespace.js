829931e620c42e456e33900a07d4b8d4e22e8060
Will Chen <chenwilliam@chromium.org>
2017-08-17 01:48:02 +0000

    Devtools: namespace DevTools tests - inspector/console & inspector-enabled (part 1)
    This aliases InspectorTest into the new test runner modules' namespaces
    and updates references of InspectorTest -> namespace version.
    
    This is getting landed in a series of CLs to avoid a CQ jam.
    
    Caveat:
    * .bind(InspectorTest, ...) calls need to be namespaced in another pass.
    (This will require parsing the AST).
    
    Bug: 667560
    Change-Id: Ib97fc444510faa439f7881d6fee7523555806c5c
    Reviewed-on: https://chromium-review.googlesource.com/617924
    Commit-Queue: Will Chen <chenwilliam@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#495042}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: d70e4b50e0733ec801469cd4709a15d85c8fb10b

---
+++ scripts/migrate_test/namespace.js
@@ -0,0 +1,112 @@
+/**
+ * Create the fake namespaces
+ */
+
+const path = require('path');
+const fs = require('fs');
+
+const utils = require('../utils');
+
+const FRONT_END_PATH = path.resolve(__dirname, '..', '..', 'front_end');
+
+function main() {
+  const identifierMap = generateTestHelperMap();
+  renameIdentifiers(identifierMap);
+}
+
+String.prototype.replaceAll = function(search, replacement) {
+  let target = this;
+  return target.replace(new RegExp('\\b' + search + '\\b', 'g'), replacement);
+};
+
+main();
+
+function generateTestHelperMap() {
+  const map = new Map();
+  for (const folder of fs.readdirSync(FRONT_END_PATH)) {
+    if (folder.indexOf('test_runner') === -1) {
+      continue;
+    }
+    const testRunnerModulePath = path.resolve(FRONT_END_PATH, folder);
+    if (!utils.isDir(testRunnerModulePath)) {
+      continue;
+    }
+    for (const filename of fs.readdirSync(testRunnerModulePath)) {
+      if (filename === 'module.json') {
+        continue;
+      }
+      scrapeTestHelperIdentifiers(path.resolve(testRunnerModulePath, filename));
+    }
+  }
+
+  // Manual overrides
+  map.set('InspectorTest.consoleModel', 'TestRunner.consoleModel');
+  map.set('InspectorTest.networkLog', 'TestRunner.networkLog');
+
+  return map;
+
+  function scrapeTestHelperIdentifiers(filePath) {
+    var content = fs.readFileSync(filePath).toString();
+    var lines = content.split('\n');
+    for (var line of lines) {
+      var line = line.trim();
+      if (line.indexOf('TestRunner.') === -1)
+        continue;
+      var match = line.match(/^\s*(\b\w*TestRunner.[a-z_A-Z0-9]+)\s*(\=[^,}]|[;])/) ||
+          line.match(/^(TestRunner.[a-z_A-Z0-9]+)\s*\=$/);
+      if (!match)
+        continue;
+      var name = match[1];
+      var components = name.split('.');
+      if (components.length !== 2) {
+        console.log('BAD', name);
+        continue;
+      }
+      map.set(['InspectorTest', components[1]].join('.'), name);
+    }
+  }
+}
+
+function renameIdentifiers(identifierMap) {
+  // walkSync('../../../../LayoutTests/http/tests/inspector', write);
+  walkSync('../../../../LayoutTests/http/tests/inspector-enabled', write);
+  walkSync('../../../../LayoutTests/http/tests/inspector-unit', write);
+  // walkSync('../../../../LayoutTests/inspector', write);
+  walkSync('../../../../LayoutTests/inspector/console', write);
+  walkSync('../../../../LayoutTests/inspector-enabled', write);
+
+  function walkSync(currentDirPath, process) {
+    fs.readdirSync(currentDirPath).forEach(function(name) {
+      let filePath = path.join(currentDirPath, name);
+      let stat = fs.statSync(filePath);
+      if (stat.isFile() &&
+          (filePath.endsWith('.html') || filePath.endsWith('.xhtml') || filePath.endsWith('.xsl') ||
+           filePath.endsWith('-expected.txt'))) {
+        // This file causes issues with renaming namespace
+        if (filePath.endsWith('heap-snapshot.html'))
+          return;
+        process(filePath);
+      } else if (stat.isDirectory()) {
+        walkSync(filePath, process);
+      }
+    });
+  }
+
+  function write(filePath) {
+    let content = fs.readFileSync(filePath).toString();
+    let newContent = content;
+    for (let key of identifierMap.keys()) {
+      let originalIdentifier = key;
+      let newIdentifier = identifierMap.get(key);
+      newContent = newContent.replaceAll(originalIdentifier, newIdentifier);
+    }
+
+    if (newContent.indexOf('InspectorTest') !== -1) {
+      console.log('WARNING', filePath, 'has InspectorTest references remaining');
+      console.log(newContent);
+    }
+
+    if (content !== newContent)
+      fs.writeFileSync(filePath, newContent);
+  }
+}

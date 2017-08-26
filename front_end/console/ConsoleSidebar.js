7370898ac70b522b2cc9c6bd3cfd4ee7522cbc78
Erik Luo <luoe@chromium.org>
2017-08-26 02:50:34 +0000

    DevTools: show counters for each sidebar group
    This CL adds upon the Log Management experiment by adding counters to
    each group, showing little icons with the number of info/warn/error
    messages from each group.
    
    Screenshot: http://imgur.com/a/wRmrI
    
    Bug: 685283
    Change-Id: I8180dc849ee6e8f9ba4c84fff63389a5f3848903
    Reviewed-on: https://chromium-review.googlesource.com/631056
    Commit-Queue: Erik Luo <luoe@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#497641}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 8103a3311e4900b7f840cabb5bc33cd7927eeab5

--- front_end/console/ConsoleSidebar.js
+++ front_end/console/ConsoleSidebar.js
@@ -18,11 +18,12 @@ Console.ConsoleSidebar = class extends UI.VBox {
     this._list.element.classList.add('list');
     this.contentElement.appendChild(this._list.element);
 
-    this._items.replaceAll([Console.ConsoleSidebar._createAllGroup()]);
+    this._allGroup = this._createAllGroup();
+    this._items.replaceAll([this._allGroup]);
     this._list.selectItem(this._items.at(0));
 
-    /** @type {!Set<string>} */
-    this._contexts = new Set();
+    /** @type {!Map<string, !Console.ConsoleSidebar.GroupItem>} */
+    this._contextToItem = new Map();
     /** @type {!Set<!Console.ConsoleSidebar.GroupItem>} */
     this._pendingItemsToAdd = new Set();
     this._pendingClear = false;
@@ -31,8 +32,9 @@ Console.ConsoleSidebar = class extends UI.VBox {
   /**
    * @return {!Console.ConsoleSidebar.GroupItem}
    */
-  static _createAllGroup() {
-    return {context: Console.ConsoleSidebar.AllContextsFilter, name: 'All'};
+  _createAllGroup() {
+    this._allGroup = {context: Console.ConsoleSidebar.AllContextsFilter, name: 'All', info: 0, warning: 0, error: 0};
+    return this._allGroup;
   }
 
   /**
@@ -51,21 +53,43 @@ Console.ConsoleSidebar = class extends UI.VBox {
   }
 
   /**
-   * @param {!Console.ConsoleSidebar.GroupItem} item
+   * @param {!ConsoleModel.ConsoleMessage} message
    */
-  addGroup(item) {
+  onMessageAdded(message) {
     if (!Runtime.experiments.isEnabled('logManagement'))
       return;
-    if (this._contexts.has(item.context))
+    incrementCounters(this._allGroup, message.level);
+    var context = message.context;
+    if (!context)
       return;
-    this._contexts.add(item.context);
-    this._pendingItemsToAdd.add(item);
+
+    var item = this._contextToItem.get(context);
+    if (!item) {
+      item = {name: context, context: context, info: 0, warning: 0, error: 0};
+      this._contextToItem.set(context, item);
+      this._pendingItemsToAdd.add(item);
+    }
+    incrementCounters(item, message.level);
+
+    /**
+     * @param {!Console.ConsoleSidebar.GroupItem} item
+     * @param {?ConsoleModel.ConsoleMessage.MessageLevel} level
+     */
+    function incrementCounters(item, level) {
+      if (level === ConsoleModel.ConsoleMessage.MessageLevel.Info)
+        item.info++;
+      else if (level === ConsoleModel.ConsoleMessage.MessageLevel.Warning)
+        item.warning++;
+      else if (level === ConsoleModel.ConsoleMessage.MessageLevel.Error)
+        item.error++;
+      item[Console.ConsoleSidebar._itemIsDirtySymbol] = true;
+    }
   }
 
   clear() {
     if (!Runtime.experiments.isEnabled('logManagement'))
       return;
-    this._contexts.clear();
+    this._contextToItem.clear();
     this._pendingItemsToAdd.clear();
     this._pendingClear = true;
   }
@@ -74,10 +98,19 @@ Console.ConsoleSidebar = class extends UI.VBox {
     if (!Runtime.experiments.isEnabled('logManagement'))
       return;
     if (this._pendingClear) {
-      this._items.replaceAll([Console.ConsoleSidebar._createAllGroup()]);
+      this._items.replaceAll([this._createAllGroup()]);
       this._list.selectItem(this._items.at(0));
       this._pendingClear = false;
     }
+    // Refresh counters for stale groups.
+    for (var item of this._items) {
+      if (item[Console.ConsoleSidebar._itemIsDirtySymbol]) {
+        this._list.refreshItem(item);
+        delete item[Console.ConsoleSidebar._itemIsDirtySymbol];
+      }
+    }
+
+    // Add new groups.
     if (this._pendingItemsToAdd.size > 0) {
       this._items.replaceRange(this._items.length, this._items.length, Array.from(this._pendingItemsToAdd));
       this._pendingItemsToAdd.clear();
@@ -93,6 +126,13 @@ Console.ConsoleSidebar = class extends UI.VBox {
     var element = createElementWithClass('div', 'context-item');
     element.createChild('div', 'name').textContent = item.name;
     element.title = item.name;
+    var counters = element.createChild('div', 'counters');
+    if (item.error)
+      counters.createChild('span', 'error-count').textContent = item.error > 99 ? '99+' : item.error;
+    if (item.warning)
+      counters.createChild('span', 'warning-count').textContent = item.warning > 99 ? '99+' : item.warning;
+    if (item.info)
+      counters.createChild('span', 'info-count').textContent = item.info > 99 ? '99+' : item.info;
     return element;
   }
 
@@ -139,5 +179,15 @@ Console.ConsoleSidebar.Events = {
   ContextSelected: Symbol('ContextSelected')
 };
 
-/** @typedef {{context: (string|symbol), name: string}} */
+/** @typedef {{
+        context: (string|symbol),
+        name: string,
+        info: number,
+        warning: number,
+        error: number
+    }}
+ */
 Console.ConsoleSidebar.GroupItem;
+
+/** @type {symbol} */
+Console.ConsoleSidebar._itemIsDirtySymbol = Symbol('itemIsDirty');
--- front_end/console/ConsoleView.js
+++ front_end/console/ConsoleView.js
@@ -447,6 +447,7 @@ Console.ConsoleView = class extends UI.VBox {
       this._needsFullUpdate = true;
     }
 
+    this._sidebar.onMessageAdded(message);
     this._scheduleViewportRefresh();
     this._consoleMessageAddedForTest(viewMessage);
 
@@ -482,10 +483,6 @@ Console.ConsoleView = class extends UI.VBox {
    * @param {!Console.ConsoleViewMessage} viewMessage
    */
   _appendMessageToEnd(viewMessage) {
-    var context = viewMessage.consoleMessage().context;
-    if (context)
-      this._sidebar.addGroup({name: context, context: context});
-
     if (!this._filter.shouldBeVisible(viewMessage)) {
       this._hiddenByFilterCount++;
       return;

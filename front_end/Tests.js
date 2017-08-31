e590d46db490ab3dce0cec24993a31c88ae0142e
Andrey Kosyakov <caseq@chromium.org>
2017-08-31 17:00:52 +0000

    Get raw response headers using URLRequest::SetResponseHeadersCallback()
    DevTools used to get actual response headers with NetLogObserver. Once we
    removed NetLogObserver, we started using URLRequest::response_headers().
    However, it returns the original cached headers in case of revalidation
    requests, which is not what DevTools expect.
    This adds SetResponseHeadersCallback() similar to SetRequestHeadersCallback()
    to resolve the issue.
    
    Bug: 758090, 721408
    Change-Id: I8d8a7a757601fe282a82b8a092148726cdab85e2
    Reviewed-on: https://chromium-review.googlesource.com/631119
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#498894}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 2e893e6c6fd7eeec25d2cd15ae1e2f435e6b5fc4

--- front_end/Tests.js
+++ front_end/Tests.js
@@ -930,6 +930,50 @@
     }
   };
 
+  TestSuite.prototype.testRawHeadersWithHSTS = function(url) {
+    var test = this;
+    test.takeControl();
+    SDK.targetManager.addModelListener(
+        SDK.NetworkManager, SDK.NetworkManager.Events.ResponseReceived, onResponseReceived);
+
+    this.evaluateInConsole_(`
+      var img = document.createElement('img');
+      img.src = "${url}";
+      document.body.appendChild(img);
+    `, () => {});
+
+    var count = 0;
+    function onResponseReceived(event) {
+      var networkRequest = event.data;
+      if (!networkRequest.url().startsWith('http'))
+        return;
+      switch (++count) {
+        case 1:  // Original redirect
+          test.assertEquals(301, networkRequest.statusCode);
+          test.assertEquals('Moved Permanently', networkRequest.statusText);
+          test.assertTrue(url.endsWith(networkRequest.responseHeaderValue('Location')));
+          break;
+
+        case 2:  // HSTS internal redirect
+          test.assertTrue(networkRequest.url().startsWith('http://'));
+          test.assertEquals(undefined, networkRequest.requestHeadersText());
+          test.assertEquals(307, networkRequest.statusCode);
+          test.assertEquals('Internal Redirect', networkRequest.statusText);
+          test.assertEquals('HSTS', networkRequest.responseHeaderValue('Non-Authoritative-Reason'));
+          test.assertTrue(networkRequest.responseHeaderValue('Location').startsWith('https://'));
+          break;
+
+        case 3:  // Final response
+          test.assertTrue(networkRequest.url().startsWith('https://'));
+          test.assertTrue(networkRequest.requestHeaderValue('Referer').startsWith('http://127.0.0.1'));
+          test.assertEquals(200, networkRequest.statusCode);
+          test.assertEquals('OK', networkRequest.statusText);
+          test.assertEquals('132', networkRequest.responseHeaderValue('Content-Length'));
+          test.releaseControl();
+      }
+    }
+  };
+
   TestSuite.prototype.waitForTestResultsInConsole = function() {
     var messages = ConsoleModel.consoleModel.messages();
     for (var i = 0; i < messages.length; ++i) {

cff1cd75449f6c4de3011af6ff8bfc85ae29f6d5
Will Chen <chenwilliam@chromium.org>
2017-09-27 23:22:23 +0000

    DevTools: manually fix tests that use output(...)
    I've inlined the handful of tests that used the output API call, which
    is removed from the new test framework (it was confusing and not available
    in the new world)
    
    Bug: 667560
    Change-Id: Ie188797bffe7253ed90dc5ee2ca90c41b495d0e2
    Reviewed-on: https://chromium-review.googlesource.com/667805
    Commit-Queue: Will Chen <chenwilliam@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#504798}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 9aa97209c99ecaaa9879a5ef16a2bfd332a2176c

--- scripts/migrate_test/migrate_test.js
+++ scripts/migrate_test/migrate_test.js
@@ -4,6 +4,7 @@
 
 'use strict';
 
+var childProcess = require('child_process');
 const fs = require('fs');
 const path = require('path');
 
@@ -20,6 +21,9 @@ const RUN_TEST_REGEX = /\s*runTest\(\);?\n*/
 const DRY_RUN = process.env.DRY_RUN || false;
 const FRONT_END_PATH = path.resolve(__dirname, '..', '..', 'front_end');
 const LINE_BREAK = '$$SECRET_IDENTIFIER_FOR_LINE_BREAK$$();';
+const LAYOUT_TESTS_PATH = path.resolve(__dirname, '..', '..', '..', '..', 'LayoutTests');
+const FLAG_EXPECTATIONS_PATH = path.resolve(LAYOUT_TESTS_PATH, 'FlagExpectations');
+const TEMP_JS_FILE = path.resolve(__dirname, 'temp.js');
 
 function main() {
   const files = process.argv.slice(2);
@@ -36,12 +40,23 @@ main();
 
 function getPrologue(inputExpectationsPath, bodyText) {
   const expectations = fs.readFileSync(inputExpectationsPath, 'utf-8').split('\n');
-  const prologueBeginning = bodyText.split('\n')[0];
+  let prologueBeginning = bodyText.split('\n')[0];
   for (const line of expectations) {
     if (line.startsWith(prologueBeginning)) {
       return line;
     }
   }
+
+  prologueBeginning = bodyText.split(' ')[0];
+  for (const line of expectations) {
+    if (line.startsWith(prologueBeginning)) {
+      return line;
+    }
+  }
+}
+
+function escapeRegExp(str) {
+  return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, '\\$&');
 }
 
 function migrateTest(inputPath, identifierMap) {
@@ -90,8 +105,12 @@ function migrateTest(inputPath, identifierMap) {
                          .replace(prologue + '\n', '')
                          // Tries to remove it if it's inline
                          .replace(prologue, '')
+                         // Optimistically remove the first sentence of the prologue and everything in that block
+                         .replace(new RegExp(`<p>\\s*${escapeRegExp(prologue).split('.')[0]}[\\s\\S]*?<\\/p>`), '')
+                         .replace(new RegExp(`<div>\\s*${escapeRegExp(prologue).split('.')[0]}[\\s\\S]*?<\\/div>`), '')
                          .replace(/<p>\s*<\/p>/, '')
                          .replace(/<div>\s*<\/div>/, '')
+                         .replace(/<script.*?>([\S\s]*?)<\/script>/g, '')
                          .trim();
     const docType = htmlTestFile.match(/<!DOCTYPE.*>/) ? htmlTestFile.match(/<!DOCTYPE.*>/)[0] : '';
     if (docType)
@@ -104,37 +123,18 @@ function migrateTest(inputPath, identifierMap) {
     console.log('ERROR: ', err);
     process.exit(1);
   }
-
+  fs.writeFileSync(TEMP_JS_FILE, outputCode);
+  outputCode = shellOutput(`clang-format ${TEMP_JS_FILE} --style=FILE`);
+  fs.unlinkSync(TEMP_JS_FILE);
+  outputCode = unicodeEscape(outputCode);
   console.log(outputCode);
   if (!DRY_RUN) {
-    const testsPath = path.resolve(__dirname, 'tests.txt');
-    const newToOldTests =
-        new Map(fs.readFileSync(testsPath, 'utf-8').split('\n').map(line => line.split(' ').reverse()));
-    const originalTestPath = path.resolve(
-        __dirname, '..', '..', '..', '..', 'LayoutTests',
-        newToOldTests.get(inputPath.slice(inputPath.indexOf('http/'))));
-
-    const srcResourcePaths = resourceScripts.map(s => path.resolve(path.dirname(originalTestPath), s));
-
-    fs.writeFileSync(inputPath, outputCode);
-    copyResourceScripts(srcResourcePaths, destResourcePaths, inputPath);
+    const jsInputPath = inputPath.replace('.html', '.js');
+    fs.writeFileSync(jsInputPath, outputCode);
     console.log('Migrated: ', inputPath);
+    fs.unlinkSync(inputPath);
+    updateTestExpectations(path.relative(LAYOUT_TESTS_PATH, inputPath), path.relative(LAYOUT_TESTS_PATH, jsInputPath));
   }
-}
-
-function copyResourceScripts(srcResourcePaths, destResourcePaths, inputPath) {
-  destResourcePaths.forEach((p, i) => {
-    mkdirp.sync(path.dirname(p));
-    if (!utils.isFile(p)) {
-      fs.writeFileSync(p, fs.readFileSync(srcResourcePaths[i]));
-    } else {
-      const originalResource = fs.readFileSync(srcResourcePaths[i]);
-      const newResource = fs.readFileSync(p);
-      if (originalResource !== newResource) {
-        console.log('Discrepancy with resource script', p, 'for file: ', inputPath);
-      }
-    }
-  });
 }
 
 function transformTestScript(
@@ -195,7 +195,6 @@ function transformTestScript(
     visitCallExpression: function(path) {
       const node = path.value;
       if (node.callee.property && node.callee.property.name === 'bind') {
-        const code = recast.prettyPrint(node);
         if (node.arguments[0].name === 'InspectorTest') {
           node.arguments[0].name = node.callee.object.object.name;
         }
@@ -233,7 +232,7 @@ function transformTestScript(
   if (domFixture) {
     headerLines.push(createAwaitExpressionNode(`await TestRunner.loadHTML(\`
 ${domFixture.split('\n').map(line => '    ' + line).join('\n')}
-\`);`));
+  \`);`));
   }
 
   stylesheetPaths.forEach(p => {
@@ -248,12 +247,12 @@ ${domFixture.split('\n').map(line => '    ' + line).join('\n')}
     headerLines.push(fixture);
   }
 
-  const nonTestCode = formatNonTestCode(nonTestAst, onloadFunctionName).trim();
+  const formattedNonTestCode = formatNonTestCode(nonTestAst, onloadFunctionName);
 
-  if (nonTestCode) {
+  if (formattedNonTestCode.trim()) {
     headerLines.push((createAwaitExpressionNode(`await TestRunner.evaluateInPagePromise(\`
-  ${nonTestCode}
-\`);`)));
+${formattedNonTestCode}
+  \`);`)));
   }
 
   headerLines.push(createNewLineNode());
@@ -283,9 +282,8 @@ function processScriptCode(code, javascriptFixtures, onloadFunctionName) {
     return code;
   }
   const formattedCode = formatNonTestCode(ast, onloadFunctionName);
-
   javascriptFixtures.push(createAwaitExpressionNode(`await TestRunner.evaluateInPagePromise(\`${formattedCode}
-\`);`));
+  \`);`));
   return;
 }
 
@@ -332,7 +330,6 @@ function replaceBodyWithFunctionDeclaration(ast, functionName) {
 }
 
 function inlineFunctionDeclaration(ast, functionName) {
-  debugger;
   const index = ast.program.body.findIndex(n => n.type === 'FunctionDeclaration' && n.id.name === functionName);
   if (index > -1) {
     const functionNode = ast.program.body[index];
@@ -347,14 +344,10 @@ function print(ast) {
 
 `;
 
-  /**
-   * Not using clang-format because certain tests look bad when formatted by it.
-   * Recast pretty print is smarter about preserving existing spacing.
-   */
-  let code = recast.print(ast).code;
+  let code = recast.prettyPrint(ast, {tabWidth: 2, wrapColumn: 120}).code;
   code = code.replace(/(\/\/\#\s*sourceURL=[\w-]+)\.html/, '$1.js');
   code = code.replace(/\s*\$\$SECRET_IDENTIFIER_FOR_LINE_BREAK\$\$\(\);/g, '\n');
-  const copyrightedCode = copyrightNotice + code + '\n';
+  const copyrightedCode = copyrightNotice + code.trim() + '\n';
   return copyrightedCode;
 }
 
@@ -455,22 +448,54 @@ function createExpressionNode(code) {
  */
 function createAwaitExpressionNode(code) {
   code = code.split('\n').map(line => line.trimRight()).join('\n');
-  var prettyPrintedCode =
-      convertToTwoSpaceIndent(recast.prettyPrint(recast.parse(`(async function(){${code}});`)).code);
-  return recast.parse(prettyPrintedCode).program.body[0].expression.body.body[0];
-}
-
-function convertToTwoSpaceIndent(code) {
-  var lines = code.split('\n').map(line => dedent(line));
-  return lines.join('\n');
-
-  function dedent(line) {
-    if (line.startsWith('    '))
-      return '  ' + dedent(line.slice(4));
-    return line;
-  }
+  return recast.parse(`(async function(){${code}});`).program.body[0].expression.body.body[0];
 }
 
 function createNewLineNode() {
   return createExpressionNode(LINE_BREAK);
+}
+
+function updateTestExpectations(oldRelativeTestPath, newRelativeTestPath) {
+  // Update additional test expectations
+  for (const filename
+           of ['TestExpectations', 'ASANExpectations', 'LeakExpectations', 'MSANExpectations', 'NeverFixTests',
+               'SlowTests', 'SmokeTests', 'StaleTestExpectations']) {
+    const filePath = path.resolve(LAYOUT_TESTS_PATH, filename);
+    updateExpectationsFile(filePath);
+  }
+
+  // Update FlagExpectations
+  for (const filename of fs.readdirSync(FLAG_EXPECTATIONS_PATH)) {
+    const filePath = path.resolve(FLAG_EXPECTATIONS_PATH, filename);
+    updateExpectationsFile(filePath);
+  }
+
+  function updateExpectationsFile(filePath) {
+    const expectations = fs.readFileSync(filePath, 'utf-8');
+    const updatedExpectations = expectations.split('\n').map(line => {
+      return line.replace(oldRelativeTestPath, newRelativeTestPath);
+    });
+    fs.writeFileSync(filePath, updatedExpectations.join('\n'));
+  }
+}
+
+function shellOutput(command) {
+  return childProcess.execSync(command).toString();
+}
+
+function unicodeEscape(string) {
+  function padWithLeadingZeros(string) {
+    return new Array(5 - string.length).join('0') + string;
+  }
+
+  function unicodeCharEscape(charCode) {
+    return '\\u' + padWithLeadingZeros(charCode.toString(16));
+  }
+
+  return string.split('')
+      .map(function(char) {
+        var charCode = char.charCodeAt(0);
+        return charCode > 127 ? unicodeCharEscape(charCode) : char;
+      })
+      .join('');
 }

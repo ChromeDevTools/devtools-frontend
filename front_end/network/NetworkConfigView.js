f797487d0390cf351f4fa360abf8a5f5d8d1d48f
Michael Giuffrida <michaelpg@chromium.org>
2017-11-09 20:29:46 +0000

    Add Chrome OS to user agent overrides
    Adds an option for Chrome on Chrome OS to the DevTools User Agent
    string customization.
    
    Like the other values, this uses a static platform version and a
    placeholder for the Chrome version.
    
    R=dgozman@chromium.org
    
    Change-Id: I367484918e3921ff19c56fb97ece8a763d760496
    Reviewed-on: https://chromium-review.googlesource.com/738931
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Michael Giuffrida <michaelpg@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#515266}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 967b31cdd72cdc719013eac00f64db1779daabe3

--- front_end/network/NetworkConfigView.js
+++ front_end/network/NetworkConfigView.js
@@ -203,6 +203,10 @@ Network.NetworkConfigView._userAgentGroups = [
             'Mozilla/5.0 (iPad; CPU OS 9_1 like Mac OS X) AppleWebKit/601.1 (KHTML, like Gecko) CriOS/%s Mobile/13B143 Safari/601.1.46'
       },
       {
+        title: 'Chrome \u2014 Chrome OS',
+        value: 'Mozilla/5.0 (X11; CrOS x86_64 10066.0.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/%s Safari/537.36'
+      },
+      {
         title: 'Chrome \u2014 Mac',
         value:
             'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/%s Safari/537.36'

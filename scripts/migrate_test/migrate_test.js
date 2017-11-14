f88c610b74e3e25e2aff3f4abed2326170b0bf59
Will Chen <chenwilliam@chromium.org>
2017-11-14 20:35:06 +0000

    Reland of DevTools: automatically migrate http/tests/devtools/tracing/*.html
    This was originally reverted because it uncovered a flakiness with the new
    test framework that has since been fixed:
    * https://chromium-review.googlesource.com/c/chromium/src/+/761957
    
    Original CL:
    https://chromium-review.googlesource.com/c/chromium/src/+/731794
    
    This converts a batch of tests from the old test framework (html file)
    into the new test framework (js file).
    
    Most of it was done through an automated transformation script, with
    a small amount of manual clean-up.
    
    TBR=caseq@chromium.org
    
    Bug: 667560
    Change-Id: I786a2ad181281cf84f8c30b4abe1904318e479c5
    Reviewed-on: https://chromium-review.googlesource.com/769331
    Reviewed-by: Will Chen <chenwilliam@chromium.org>
    Commit-Queue: Will Chen <chenwilliam@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#516410}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: f6558aae6aba96fe723678d6890ac0c533706516

--- scripts/migrate_test/migrate_test.js
+++ scripts/migrate_test/migrate_test.js
@@ -120,6 +120,10 @@ function migrateTest(inputPath, identifierMap) {
       domFixture = inlineStylesheets + (domFixture.length ? '\n' : '') + domFixture;
     if (docType)
       domFixture = docType + (domFixture.length ? '\n' : '') + domFixture;
+    const inlineStylesheets =
+        $('style').toArray().map(n => n.children[0].data).map(text => `<style>${text}</style>`).join('\n');
+    if (inlineStylesheets)
+      domFixture = inlineStylesheets + (domFixture.length ? '\n' : '') + domFixture;
     outputCode = transformTestScript(
         inputCode, prologue, identifierMap, testHelpers, javascriptFixtures, getPanels(inputPath, inputCode),
         domFixture, onloadFunctionName, relativeResourcePaths, stylesheetPaths, inputFilename);
@@ -410,9 +414,12 @@ function mapTestHelpers(testHelpers, includeConsole) {
     if (!mappedHelper) {
       throw Error('Could not map helper ' + helper);
     }
-    if (mappedHelper !== 'inspector_test_runner') {
-      mappedHelpers.add(mappedHelper);
-    }
+    if (mappedHelper === 'inspector_test_runner')
+      continue;
+    // Some tests reference tracing-test.js which doesn't exist
+    if (mappedHelper === 'tracing_test_runner')
+      continue;
+    mappedHelpers.add(mappedHelper);
   }
   if (includeConsole)
     mappedHelpers.add('console_test_runner');

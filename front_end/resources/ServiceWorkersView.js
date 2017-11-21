3f1549ea93386519475c7ab15438c10673a9582f
Eugene Ostroukhov <eostroukhov@chromium.org>
2017-11-21 17:46:54 +0000

    [DevTools] Hide service workers filter when no SW
    Bug: 783113
    Change-Id: I8fa7f12d8908615ea9b5bdc0bc3baba646ccec04
    Reviewed-on: https://chromium-review.googlesource.com/778047
    Commit-Queue: Eugene Ostroukhov <eostroukhov@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#518296}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 57737c35bed16999c701ca8d035462322424f93c

--- front_end/resources/ServiceWorkersView.js
+++ front_end/resources/ServiceWorkersView.js
@@ -63,6 +63,7 @@ Resources.ServiceWorkersView = class extends UI.VBox {
     /** @type {!Map<!SDK.ServiceWorkerManager, !Array<!Common.EventTarget.EventDescriptor>>}*/
     this._eventListeners = new Map();
     SDK.targetManager.observeModels(SDK.ServiceWorkerManager, this);
+    this._updateListVisibility();
   }
 
   /**
@@ -136,6 +137,7 @@ Resources.ServiceWorkersView = class extends UI.VBox {
     }
     this.contentElement.classList.toggle('service-worker-has-current', hasThis);
     this._otherWorkers.classList.toggle('hidden', !hasOthers);
+    this._updateListVisibility();
   }
 
   /**
@@ -256,6 +258,10 @@ Resources.ServiceWorkersView = class extends UI.VBox {
     if (path.endsWith('/'))
       path = path.substring(0, path.length - 1);
     return parsedURL.host + path;
+  }
+
+  _updateListVisibility() {
+    this.contentElement.classList.toggle('service-worker-list-empty', this._sections.size === 0);
   }
 };
 

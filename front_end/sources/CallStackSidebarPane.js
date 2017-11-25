df1536e08e0a82b633156ac4f1a874910a8feb46
Alexey Kozyatinskiy <kozyatinskiy@chromium.org>
2017-11-25 22:10:20 +0000

    [DevTools] added "Show more" button for very long async stacks
    Screenshot: https://imgur.com/a/KcS2E
    
    Bug: chromium:778796
    Change-Id: I8000e08c063f86c10278714e2ab8056d35a4e3e6
    Reviewed-on: https://chromium-review.googlesource.com/777998
    Commit-Queue: Aleksey Kozyatinskiy <kozyatinskiy@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#519188}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 5eae5f19e0a1c5e400fbf9519816eb3e913de933

--- front_end/sources/CallStackSidebarPane.js
+++ front_end/sources/CallStackSidebarPane.js
@@ -47,11 +47,16 @@ Sources.CallStackSidebarPane = class extends UI.SimpleView {
     this._list.element.addEventListener('contextmenu', this._onContextMenu.bind(this), false);
     this._list.element.addEventListener('click', this._onClick.bind(this), false);
 
+    this._showMoreMessageElement = this._createShowMoreMessageElement();
+    this._showMoreMessageElement.classList.add('hidden');
+    this.contentElement.appendChild(this._showMoreMessageElement);
+
     this._showBlackboxed = false;
     Bindings.blackboxManager.addChangeListener(this._update.bind(this));
     this._locationPool = new Bindings.LiveLocationPool();
 
     this._updateThrottler = new Common.Throttler(100);
+    this._maxAsyncStackChainDepth = Sources.CallStackSidebarPane._defaultMaxAsyncStackChainDepth;
     this._update();
   }
 
@@ -78,7 +83,9 @@ Sources.CallStackSidebarPane = class extends UI.SimpleView {
     if (!details) {
       this._notPausedMessageElement.classList.remove('hidden');
       this._blackboxedMessageElement.classList.add('hidden');
+      this._showMoreMessageElement.classList.add('hidden');
       this._items.replaceAll([]);
+      this._maxAsyncStackChainDepth = Sources.CallStackSidebarPane._defaultMaxAsyncStackChainDepth;
       UI.context.setFlavor(SDK.DebuggerModel.CallFrame, null);
       return;
     }
@@ -105,7 +112,7 @@ Sources.CallStackSidebarPane = class extends UI.SimpleView {
       asyncStackTrace = debuggerModel ? await debuggerModel.fetchAsyncStackTrace(details.asyncStackTraceId) : null;
     }
     var peviousStackTrace = details.callFrames;
-    var maxAsyncStackChainDepth = 32;
+    var maxAsyncStackChainDepth = this._maxAsyncStackChainDepth;
     while (asyncStackTrace && maxAsyncStackChainDepth > 0) {
       var title = '';
       var isAwait = asyncStackTrace.description === 'async function';
@@ -143,6 +150,10 @@ Sources.CallStackSidebarPane = class extends UI.SimpleView {
         asyncStackTrace = null;
       }
     }
+    if (asyncStackTrace)
+      this._showMoreMessageElement.classList.remove('hidden');
+    else
+      this._showMoreMessageElement.classList.add('hidden');
 
     if (!hiddenCallFramesCount) {
       this._blackboxedMessageElement.classList.add('hidden');
@@ -158,7 +169,8 @@ Sources.CallStackSidebarPane = class extends UI.SimpleView {
     }
 
     this._items.replaceAll(items);
-    this._list.selectNextItem(true /* canWrap */, false /* center */);
+    if (this._maxAsyncStackChainDepth === Sources.CallStackSidebarPane._defaultMaxAsyncStackChainDepth)
+      this._list.selectNextItem(true /* canWrap */, false /* center */);
     this._updatedForTest();
   }
 
@@ -285,6 +297,21 @@ Sources.CallStackSidebarPane = class extends UI.SimpleView {
   }
 
   /**
+   * @return {!Element}
+   */
+  _createShowMoreMessageElement() {
+    var element = createElementWithClass('div', 'show-more-message');
+    element.createChild('span');
+    var showAllLink = element.createChild('span', 'link');
+    showAllLink.textContent = Common.UIString('Show more');
+    showAllLink.addEventListener('click', () => {
+      this._maxAsyncStackChainDepth += Sources.CallStackSidebarPane._defaultMaxAsyncStackChainDepth;
+      this._update();
+    }, false);
+    return element;
+  }
+
+  /**
    * @param {!Event} event
    */
   _onContextMenu(event) {
@@ -400,6 +427,8 @@ Sources.CallStackSidebarPane = class extends UI.SimpleView {
         UI.ShortcutsScreen.SourcesPanelShortcuts.PrevCallFrame, this._selectPreviousCallFrameOnStack.bind(this));
   }
 };
+
+Sources.CallStackSidebarPane._defaultMaxAsyncStackChainDepth = 32;
 
 /**
  * @typedef {{

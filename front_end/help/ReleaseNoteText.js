2b26de24dd71a31110c908edbef446d19067ccda
Kayce Basques <kayce@chromium.org>
2017-11-30 00:48:24 +0000

    update whatsnew for m64
    TBR=pfeldman@chromium.org
    
    Bug: 789652
    Change-Id: Ida3fbf635dc43990a1b0015461e318dd47976e7f
    Reviewed-on: https://chromium-review.googlesource.com/797491
    Commit-Queue: Kayce Basques <kayce@chromium.org>
    Reviewed-by: Will Chen <chenwilliam@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#520349}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 8bbde6061d124dc5f98a0c0ffc2e7df995ad54f7

--- front_end/help/ReleaseNoteText.js
+++ front_end/help/ReleaseNoteText.js
@@ -12,6 +12,33 @@ var commandMenuShortcut = Host.isMac() ? 'Command + Shift + P' : 'Control + Shif
 /** @type {!Array<!Help.ReleaseNote>} */
 Help.releaseNoteText = [
   {
+    version: 7,
+    header: 'Highlights from the Chrome 64 update',
+    highlights: [
+      {
+        title: 'Local overrides',
+        subtitle: 'Persist your HTML, CSS, and JS changes across page loads.',
+        link: 'https://developers.google.com/web/updates/2017/11/devtools-release-notes#overrides',
+      },
+      {
+        title: 'Performance monitor',
+        subtitle: 'Get a real-time view of various performance metrics.',
+        link: 'https://developers.google.com/web/updates/2017/11/devtools-release-notes#perf-monitor',
+      },
+      {
+        title: 'Console sidebar',
+        subtitle: 'Reduce console noise and focus on the messages that are important to you.',
+        link: 'https://developers.google.com/web/updates/2017/11/devtools-release-notes#console-sidebar',
+      },
+      {
+        title: 'Group similar console messages',
+        subtitle: 'The Console now groups similar messages by default.',
+        link: 'https://developers.google.com/web/updates/2017/11/devtools-release-notes#group-similar',
+      },
+    ],
+    link: 'https://developers.google.com/web/updates/2017/11/devtools-release-notes',
+  },
+  {
     version: 6,
     header: 'Highlights from the Chrome 63 update',
     highlights: [

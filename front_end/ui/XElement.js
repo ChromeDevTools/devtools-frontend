cd1b1bf758df728724d206634e67e67f7c3cc61e
Dmitry Gozman <dgozman@chromium.org>
2017-12-16 04:21:57 +0000

    [DevTools] Add a bunch of default custom elements
    Bug: none
    Change-Id: Ic7cc7e665a8dd5f8999b2243b3fae342f5c8d960
    Reviewed-on: https://chromium-review.googlesource.com/830681
    Reviewed-by: Andrey Lushnikov <lushnikov@chromium.org>
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#524587}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 9968c0e5ab28fac7ef33f7b7d9c8d373f8c03da3

--- front_end/ui/XElement.js
+++ front_end/ui/XElement.js
@@ -45,3 +45,109 @@ UI.XElement = class extends HTMLElement {
     }
   }
 };
+
+/**
+ * @extends {UI.XElement}
+ */
+UI._XBox = class extends UI.XElement {
+  /**
+   * @param {string} direction
+   */
+  constructor(direction) {
+    super();
+    this.style.setProperty('display', 'flex');
+    this.style.setProperty('flex-direction', direction);
+    this.style.setProperty('justify-content', 'flex-start');
+  }
+
+  static get observedAttributes() {
+    // TODO(dgozman): should be super.observedAttributes, but does not compile.
+    return UI.XElement.observedAttributes.concat(['x-start', 'x-center', 'x-stretch', 'x-baseline']);
+  }
+
+  /**
+   * @param {string} attr
+   * @param {?string} oldValue
+   * @param {?string} newValue
+   * @override
+   */
+  attributeChangedCallback(attr, oldValue, newValue) {
+    if (attr === 'x-start' || attr === 'x-center' || attr === 'x-stretch' || attr === 'x-baseline') {
+      if (newValue === null)
+        this.style.removeProperty('align-items');
+      else
+        this.style.setProperty('align-items', attr === 'x-start' ? 'flex-start' : attr.substr(2));
+      return;
+    }
+    super.attributeChangedCallback(attr, oldValue, newValue);
+  }
+};
+
+/**
+ * @extends {UI._XBox}
+ */
+UI.XVBox = class extends UI._XBox {
+  constructor() {
+    super('column');
+  }
+};
+
+/**
+ * @extends {UI._XBox}
+ */
+UI.XHBox = class extends UI._XBox {
+  constructor() {
+    super('row');
+  }
+};
+
+/**
+ * @extends {UI.XElement}
+ */
+UI.XCBox = class extends UI.XElement {
+  constructor() {
+    super();
+    this.style.setProperty('display', 'flex');
+    this.style.setProperty('flex-direction', 'column');
+    this.style.setProperty('justify-content', 'center');
+    this.style.setProperty('align-items', 'center');
+  }
+};
+
+/**
+ * @extends {UI.XElement}
+ */
+UI.XDiv = class extends UI.XElement {
+  constructor() {
+    super();
+    this.style.setProperty('display', 'block');
+  }
+};
+
+/**
+ * @extends {UI.XElement}
+ */
+UI.XSpan = class extends UI.XElement {
+  constructor() {
+    super();
+    this.style.setProperty('display', 'inline');
+  }
+};
+
+/**
+ * @extends {UI.XElement}
+ */
+UI.XText = class extends UI.XElement {
+  constructor() {
+    super();
+    this.style.setProperty('display', 'inline');
+    this.style.setProperty('white-space', 'pre');
+  }
+};
+
+self.customElements.define('x-vbox', UI.XVBox);
+self.customElements.define('x-hbox', UI.XHBox);
+self.customElements.define('x-cbox', UI.XCBox);
+self.customElements.define('x-div', UI.XDiv);
+self.customElements.define('x-span', UI.XSpan);
+self.customElements.define('x-text', UI.XText);

b0b1ecd0d482c53d6d80002e4740f03cf88d2cda
Will Chen <chenwilliam@chromium.org>
2017-12-22 11:54:46 -0800

    DevTools: do not format eslintignore'd files in PRESUBMIT
    DevTools has a convention where third-party modules are not linted by
    eslint, and also shouldn't be formatted by clang-format. This re-uses
    the .eslintignore list to create a blacklist for clang-format.
    
    This removes the npm run format command because it wouldn't have the
    blacklist logic.
    
    Bug: 753989
    Change-Id: I54564938aba2b098690ecf028765021b9f2ace63
    Reviewed-on: https://chromium-review.googlesource.com/843235
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Will Chen <chenwilliam@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#526231}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 15d9337b02dd1348584085ad3244fd99823f726e

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -59,12 +59,20 @@ def _CheckFormat(input_api, output_api):
     finally:
         sys.path = original_sys_path
 
-    check_formatting_process = popen(['git', 'cl', 'format', '--js', '--dry-run', input_api.PresubmitLocalPath()])
+    ignore_files = []
+    eslint_ignore_path = input_api.os_path.join(input_api.PresubmitLocalPath(), '.eslintignore')
+    with open(eslint_ignore_path, 'r') as ignore_manifest:
+        for line in ignore_manifest:
+            ignore_files.append(line.strip())
+    formattable_files = [affected_file for affected_file in affected_files
+                         if all(ignore_file not in affected_file for ignore_file in ignore_files)]
+
+    check_formatting_process = popen(['git', 'cl', 'format', '--js', '--dry-run'] + formattable_files)
     check_formatting_process.communicate()
     if check_formatting_process.returncode == 0:
         return []
 
-    format_args = ['git', 'cl', 'format', '--js', input_api.PresubmitLocalPath()]
+    format_args = ['git', 'cl', 'format', '--js'] + formattable_files
     format_process = popen(format_args)
     format_out, _ = format_process.communicate()
     if format_process.returncode != 0:

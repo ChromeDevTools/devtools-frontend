fa6225bcc234347494df66409874033f4cba5330
Kayce Basques <kayce@chromium.org>
2018-01-23 21:49:36 +0000

    Link to Sources Panel Overview
    Bug: 801271
    Change-Id: I4dd17eb097e9d5992717a9127df8d5f7995dace4
    Reviewed-on: https://chromium-review.googlesource.com/862364
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Commit-Queue: Kayce Basques <kayce@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#531360}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 2f4532097210f8c4db2e800deb6602e6c6fcd3d2

--- front_end/sources/SourcesView.js
+++ front_end/sources/SourcesView.js
@@ -111,6 +111,10 @@ Sources.SourcesView = class extends UI.VBox {
     }
     element.createChild('div').textContent = Common.UIString('Drop in a folder to add to workspace');
 
+    element.appendChild(UI.XLink.create(
+        'https://developers.google.com/web/tools/chrome-devtools/sources?utm_source=devtools&utm_campaign=2018Q1',
+        'Learn more'));
+
     return element;
   }
 

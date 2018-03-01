97f3a4314159e8bb8a15354d4ac5a100deff738e
Erik Luo <luoe@chromium.org>
2018-03-01 03:43:53 +0000

    DevTools: extend console value truncation beyond strings
    Console currently truncates super long strings and renders a
    'show more' button to see the full value.
    
    This CL extends the same logic to other value-formatted types,
    including 'regexp' and 'symbol'. Doing so also makes 'bigint'
    easy to include in the future.
    
    Bug: none
    Change-Id: I6e4f607c13e143a8fcecc241aac10866456c0ec3
    Reviewed-on: https://chromium-review.googlesource.com/940808
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Erik Luo <luoe@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#540024}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: d2957b80bfef295144c1147e7828f10183b18821

--- front_end/console/ConsoleViewMessage.js
+++ front_end/console/ConsoleViewMessage.js
@@ -564,7 +564,11 @@ Console.ConsoleViewMessage = class {
    */
   _formatParameterAsValue(obj) {
     const result = createElement('span');
-    result.createTextChild(obj.description || '');
+    const description = obj.description || '';
+    if (description.length > Console.ConsoleViewMessage._MaxTokenizableStringLength)
+      result.appendChild(Console.ConsoleViewMessage._createExpandableFragment(description));
+    else
+      result.createTextChild(description);
     if (obj.objectId)
       result.addEventListener('contextmenu', this._contextMenuEventFired.bind(this, obj), false);
     return result;
@@ -1350,7 +1354,7 @@ Console.ConsoleViewMessage = class {
    */
   static linkifyWithCustomLinkifier(string, linkifier) {
     if (string.length > Console.ConsoleViewMessage._MaxTokenizableStringLength)
-      return createExpandableFragment(string);
+      return Console.ConsoleViewMessage._createExpandableFragment(string);
     const container = createDocumentFragment();
     const tokens = this._tokenizeMessageText(string);
     for (const token of tokens) {
@@ -1372,31 +1376,31 @@ Console.ConsoleViewMessage = class {
       }
     }
     return container;
+  }
 
-    /**
-     * @param {string} text
-     * @return {!DocumentFragment}
-     */
-    function createExpandableFragment(text) {
-      const fragment = createDocumentFragment();
-      fragment.textContent = text.slice(0, Console.ConsoleViewMessage._LongStringVisibleLength);
-      const hiddenText = text.slice(Console.ConsoleViewMessage._LongStringVisibleLength);
+  /**
+   * @param {string} text
+   * @return {!DocumentFragment}
+   */
+  static _createExpandableFragment(text) {
+    const fragment = createDocumentFragment();
+    fragment.textContent = text.slice(0, Console.ConsoleViewMessage._LongStringVisibleLength);
+    const hiddenText = text.slice(Console.ConsoleViewMessage._LongStringVisibleLength);
 
-      const expandButton = fragment.createChild('span', 'console-inline-button');
-      expandButton.setAttribute('data-text', ls`Show ${Number.withThousandsSeparator(hiddenText.length)} more`);
-      expandButton.addEventListener('click', () => {
-        if (expandButton.parentElement)
-          expandButton.parentElement.insertBefore(createTextNode(hiddenText), expandButton);
-        expandButton.remove();
-      });
+    const expandButton = fragment.createChild('span', 'console-inline-button');
+    expandButton.setAttribute('data-text', ls`Show ${Number.withThousandsSeparator(hiddenText.length)} more`);
+    expandButton.addEventListener('click', () => {
+      if (expandButton.parentElement)
+        expandButton.parentElement.insertBefore(createTextNode(hiddenText), expandButton);
+      expandButton.remove();
+    });
 
-      const copyButton = fragment.createChild('span', 'console-inline-button');
-      copyButton.setAttribute('data-text', ls`Copy`);
-      copyButton.addEventListener('click', () => {
-        InspectorFrontendHost.copyText(text);
-      });
-      return fragment;
-    }
+    const copyButton = fragment.createChild('span', 'console-inline-button');
+    copyButton.setAttribute('data-text', ls`Copy`);
+    copyButton.addEventListener('click', () => {
+      InspectorFrontendHost.copyText(text);
+    });
+    return fragment;
   }
 
   /**

7228ed7828567652108d0e78b58e03208f3a701d
Joel Einbinder <einbinder@chromium.org>
2018-05-03 00:30:10 +0000

    DevTools: Clear autocomplete cache when the execution context changes
    This was attempted before, but the ExecutionContextChanged event is
    actually for when the name of an execution context changes, not for
    when a new one is selected by the user (or the page is reloaded).
    
    Also added a safety clear on DebuggerResumed
    
    Change-Id: I4c9360139fe90beee2fa3d8a82317fc3aed1e6ae
    Reviewed-on: https://chromium-review.googlesource.com/1040906
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Joel Einbinder <einbinder@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#555627}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: ae75af0b961edbac04cd8db77e9efaf77e962436

--- front_end/object_ui/JavaScriptAutocomplete.js
+++ front_end/object_ui/JavaScriptAutocomplete.js
@@ -7,8 +7,9 @@ ObjectUI.JavaScriptAutocomplete = class {
     /** @type {!Map<string, {date: number, value: !Promise<?Object>}>} */
     this._expressionCache = new Map();
     SDK.consoleModel.addEventListener(SDK.ConsoleModel.Events.CommandEvaluated, this._clearCache, this);
+    UI.context.addFlavorChangeListener(SDK.ExecutionContext, this._clearCache, this);
     SDK.targetManager.addModelListener(
-        SDK.RuntimeModel, SDK.RuntimeModel.Events.ExecutionContextChanged, this._clearCache, this);
+        SDK.DebuggerModel, SDK.DebuggerModel.Events.DebuggerResumed, this._clearCache, this);
     SDK.targetManager.addModelListener(
         SDK.DebuggerModel, SDK.DebuggerModel.Events.DebuggerPaused, this._clearCache, this);
   }

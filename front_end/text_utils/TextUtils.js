2b3f614b0adcebeab8d7e6bb26dee57c0d97ec1a
Joel Einbinder <einbinder@chromium.org>
2018-05-07 19:21:07 +0000

    DevTools: Improve minified text heuristic
    The previous code checked if the first few lines of code were minified.
    But some scripts start with a non-minified copyright, so this patch
    checks the last few lines as well.
    
    Change-Id: Idc517fc4253d107bfa219fc753cbe772ed8e3ac7
    Reviewed-on: https://chromium-review.googlesource.com/1044865
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Joel Einbinder <einbinder@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#556523}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: da508ffe6469d0fcabef3fedfcd364128e8c8f31

--- front_end/text_utils/TextUtils.js
+++ front_end/text_utils/TextUtils.js
@@ -359,5 +359,17 @@ TextUtils.isMinified = function(text) {
       return true;
     lastPosition = eolIndex + 1;
   } while (--linesToCheck >= 0 && lastPosition < text.length);
+
+  // Check the end of the text as well
+  linesToCheck = 10;
+  lastPosition = text.length;
+  do {
+    let eolIndex = text.lastIndexOf('\n', lastPosition);
+    if (eolIndex < 0)
+      eolIndex = 0;
+    if (lastPosition - eolIndex > kMaxNonMinifiedLength && text.substr(lastPosition, 3) !== '//#')
+      return true;
+    lastPosition = eolIndex - 1;
+  } while (--linesToCheck >= 0 && lastPosition > 0);
   return false;
 };

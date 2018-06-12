56a99365197b85c24b732ac92b0ac70feed80179
Ingvar Stepanyan <ingvar@cloudflare.com>
2018-06-12 14:06:28 +0000

    Fix toggles in hosted mode
     Fix `FilterBar::_update` to pass `false` for disabled items and not
     `undefined` as `classList.toggle` treats them differently.
    
    Bug: 850641
    Change-Id: I964c528ad6da495e2fd4078cde415e7cc250a13f
    Reviewed-on: https://chromium-review.googlesource.com/1091312
    Commit-Queue: Ingvar Stepanyan <ingvar@cloudflare.com>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#566409}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 447883683661cf3fc7c1296dfa9f379510948ada

--- front_end/ui/FilterBar.js
+++ front_end/ui/FilterBar.js
@@ -328,7 +328,7 @@ UI.NamedBitSetFilterUI = class extends Common.Object {
       this._allowedTypes[UI.NamedBitSetFilterUI.ALL_TYPES] = true;
     }
     for (const typeName in this._typeFilterElements)
-      this._typeFilterElements[typeName].classList.toggle('selected', this._allowedTypes[typeName]);
+      this._typeFilterElements[typeName].classList.toggle('selected', !!this._allowedTypes[typeName]);
     this.dispatchEventToListeners(UI.FilterUI.Events.FilterChanged, null);
   }
 

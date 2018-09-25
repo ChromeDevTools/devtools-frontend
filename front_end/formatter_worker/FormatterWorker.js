7f9405148abbfc2e8b7b46af956f941b19c97575
Alexey Kozyatinskiy <kozyatinskiy@chromium.org>
2018-09-25 22:23:23 +0000

    DevTools: roll acorn to 6.0.1
    New acorn support couple nice ES features, e.g.,
    - for await (...) loop,
    - spread operator.
    
    R=lushnikov@chromium.org
    
    Bug: chromium:863656
    Change-Id: I057ebf01fcc220ee1e98707343df457358f248f6
    Reviewed-on: https://chromium-review.googlesource.com/1244600
    Commit-Queue: Aleksey Kozyatinskiy <kozyatinskiy@chromium.org>
    Reviewed-by: Andrey Lushnikov <lushnikov@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#594126}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 57bdd881a25314439a39030c651d90fb65c877cc

--- front_end/formatter_worker/FormatterWorker.js
+++ front_end/formatter_worker/FormatterWorker.js
@@ -151,11 +151,11 @@ FormatterWorker.evaluatableJavaScriptSubstring = function(content) {
  * @param {string} content
  */
 FormatterWorker.preprocessTopLevelAwaitExpressions = function(content) {
-  let wrapped = '(async () => {' + content + '})()';
+  let wrapped = '(async () => {' + content + '\n})()';
   let root;
   let body;
   try {
-    root = acorn.parse(wrapped, {ecmaVersion: 9});
+    root = acorn.parse(wrapped, {ecmaVersion: 10});
     body = root.body[0].expression.callee.body;
   } catch (e) {
     postMessage('');
@@ -184,6 +184,10 @@ FormatterWorker.preprocessTopLevelAwaitExpressions = function(content) {
     }
     AwaitExpression(node) {
       containsAwait = true;
+    }
+    ForOfStatement(node) {
+      if (node.await)
+        containsAwait = true;
     }
     ReturnStatement(node) {
       containsReturn = true;

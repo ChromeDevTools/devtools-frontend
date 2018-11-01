a8a622fa4c0699dbf94779594b7c937c3815b397
Harley Li <hhli@chromium.org>
2018-11-01 23:58:09 +0000

    [DevTools] Handle long string in service worker list's script name and clients
    If the name string of the service worker script or a service worker
    client is too long, this patch restricts its width and denote the
    overflow with ellipsis. Users can still get the string by hovering
    above it, and the string is still clickable.
    
    Bug: 847801
    Change-Id: I9d3a07c410af6bb98ae42046894ea3d7223afcbc
    Reviewed-on: https://chromium-review.googlesource.com/c/1312973
    Commit-Queue: Haihong Li (Harley) <hhli@chromium.org>
    Reviewed-by: Erik Luo <luoe@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#604760}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 244ff23665600a61c9c2ef5ce16951ea7e7100e2

--- front_end/resources/ServiceWorkersView.js
+++ front_end/resources/ServiceWorkersView.js
@@ -534,12 +534,14 @@ Resources.ServiceWorkersView.Section = class {
    */
   _updateClientInfo(element, targetInfo) {
     if (targetInfo.type !== 'page' && targetInfo.type === 'iframe') {
-      element.createTextChild(Common.UIString('Worker: %s', targetInfo.url));
+      const clientString = element.createChild('span', 'service-worker-client-string');
+      clientString.createTextChild(ls`Worker: ` + targetInfo.url);
       return;
     }
     element.removeChildren();
-    element.createTextChild(targetInfo.url);
-    const focusLabel = element.createChild('label', 'link');
+    const clientString = element.createChild('span', 'service-worker-client-string');
+    clientString.createTextChild(targetInfo.url);
+    const focusLabel = element.createChild('label', 'link service-worker-client-focus-link');
     focusLabel.createTextChild('focus');
     focusLabel.addEventListener('click', this._activateTarget.bind(this, targetInfo.targetId), true);
   }

336b9a068e48365492a5441053ef57e91078dd86
Erik Luo <luoe@chromium.org>
2018-12-06 18:37:44 +0000

    DevTools: enable Console keyboard navigation by default
    Bug: 865674
    Change-Id: I6f1bf84a2113f64cc0c00839067f4e41ca72f483
    Reviewed-on: https://chromium-review.googlesource.com/c/1330295
    Commit-Queue: Erik Luo <luoe@chromium.org>
    Reviewed-by: Joel Einbinder <einbinder@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#614424}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 48789ec6e4da515fdeda4ec1c3d569a06466790c

--- front_end/main/Main.js
+++ front_end/main/Main.js
@@ -152,7 +152,7 @@ Main.Main = class {
 
     Runtime.experiments.setDefaultExperiments([
       'colorContrastRatio', 'stepIntoAsync', 'oopifInlineDOM', 'consoleBelowPrompt', 'timelineTracingJSProfile',
-      'pinnedExpressions'
+      'pinnedExpressions', 'consoleKeyboardNavigation'
     ]);
   }
 

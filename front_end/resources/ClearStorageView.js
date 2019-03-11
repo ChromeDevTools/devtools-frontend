d3a65abf6c53d1e9394fd3f57182fcf0e1673d26
Harley Li <hhli@chromium.org>
2019-03-11 22:36:09 +0000

    [DevTools] Add notification of storage being limited in Incognito mode
    Add a message telling users that storage quota is limited in Incognito
    mode (why: crbug 800329 comment \#1). In non-Incognito mode, the quota
    is in gigabytes. But in Incognito, the quota cap is no more than 120MB.
    
    Bug: 794269, 800329
    Change-Id: Ic0d9e9d40119424c0b44bdd314d1f836af4ed357
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1506462
    Commit-Queue: Haihong Li (Harley) <hhli@chromium.org>
    Reviewed-by: Erik Luo <luoe@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#639677}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 35d83281343da301a0f3515ff585cd4890f14ce6

--- front_end/resources/ClearStorageView.js
+++ front_end/resources/ClearStorageView.js
@@ -210,7 +210,12 @@ Resources.ClearStorageView = class extends UI.ThrottledWidget {
       return;
     }
     this._quotaRow.textContent = Common.UIString(
-        '%s used out of %s storage quota', Number.bytesToString(response.usage), Number.bytesToString(response.quota));
+        '%s used out of %s storage quota.\u00a0', Number.bytesToString(response.usage),
+        Number.bytesToString(response.quota));
+    if (response.quota < 125829120) {  // 120 MB
+      this._quotaRow.title = ls`Storage quota is limited in Incognito mode`;
+      this._quotaRow.appendChild(UI.Icon.create('smallicon-info'));
+    }
 
     if (!this._quotaUsage || this._quotaUsage !== response.usage) {
       this._quotaUsage = response.usage;

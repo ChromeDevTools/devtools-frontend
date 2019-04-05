f57dec2a9b49ce860d3c7d95cb69960aa0a7eb16
Alexei Filippov <alph@chromium.org>
2019-04-05 22:11:20 +0000

    DevTools: Add setting to toggle live heap profile on/off
    BUG=937880
    
    Change-Id: I165c6de88f4d63080287704a066728757c2a5c26
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1531561
    Commit-Queue: Alexei Filippov <alph@chromium.org>
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#648369}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: cbf5a0d137c9b6933d99ef1ae40feef186162d9c

--- front_end/perf_ui/LiveHeapProfile.js
+++ front_end/perf_ui/LiveHeapProfile.js
@@ -8,8 +8,13 @@
  */
 PerfUI.LiveHeapProfile = class {
   constructor() {
-    SDK.targetManager.observeModels(SDK.HeapProfilerModel, this);
-    requestIdleCallback(() => this.onUpdateProfiles(), {timeout: 100});
+    this._running = false;
+    this._sessionId = 0;
+    this._loadEventCallback = () => {};
+    this._setting = Common.settings.moduleSetting('memoryLiveHeapProfile');
+    this._setting.addChangeListener(event => event.data ? this._startProfiling() : this._stopProfiling());
+    if (this._setting.get())
+      this._startProfiling();
     PerfUI.LiveHeapProfile.hasStartedForTest(true);
   }
 
@@ -44,19 +49,50 @@ PerfUI.LiveHeapProfile = class {
    * @param {!SDK.HeapProfilerModel} model
    */
   modelRemoved(model) {
-    model.stopSampling();
+    // Cannot do much when the model has already been removed.
   }
 
-  async onUpdateProfiles() {
-    const models = SDK.targetManager.models(SDK.HeapProfilerModel);
-    const profiles = await Promise.all(models.map(model => model.getSamplingProfile()));
-    const lineLevelProfile = self.runtime.sharedInstance(PerfUI.LineLevelProfile.Memory);
-    lineLevelProfile.reset();
-    for (let i = 0; i < profiles.length; ++i) {
-      if (profiles[i])
-        lineLevelProfile.appendHeapProfile(profiles[i], models[i].target());
-    }
-    const updateInterval = Host.isUnderTest() ? 10 : 5000;
-    setTimeout(() => requestIdleCallback(() => this.onUpdateProfiles(), {timeout: 100}), updateInterval);
+  async _startProfiling() {
+    if (this._running)
+      return;
+    this._running = true;
+    const sessionId = this._sessionId;
+    SDK.targetManager.observeModels(SDK.HeapProfilerModel, this);
+    SDK.targetManager.addModelListener(
+        SDK.ResourceTreeModel, SDK.ResourceTreeModel.Events.Load, this._loadEventFired, this);
+
+    do {
+      const models = SDK.targetManager.models(SDK.HeapProfilerModel);
+      const profiles = await Promise.all(models.map(model => model.getSamplingProfile()));
+      if (sessionId !== this._sessionId)
+        break;
+      const lineLevelProfile = self.runtime.sharedInstance(PerfUI.LineLevelProfile.Memory);
+      lineLevelProfile.reset();
+      for (let i = 0; i < profiles.length; ++i) {
+        if (profiles[i])
+          lineLevelProfile.appendHeapProfile(profiles[i], models[i].target());
+      }
+      await Promise.race([
+        new Promise(r => setTimeout(r, Host.isUnderTest() ? 10 : 5000)), new Promise(r => this._loadEventCallback = r)
+      ]);
+    } while (sessionId === this._sessionId);
+
+    SDK.targetManager.unobserveModels(SDK.HeapProfilerModel, this);
+    SDK.targetManager.removeModelListener(
+        SDK.ResourceTreeModel, SDK.ResourceTreeModel.Events.Load, this._loadEventFired, this);
+    for (const model of SDK.targetManager.models(SDK.HeapProfilerModel))
+      model.stopSampling();
+    PerfUI.LineLevelProfile.Memory.instance().reset();
+  }
+
+  _stopProfiling() {
+    if (!this._running)
+      return;
+    this._running = 0;
+    this._sessionId++;
+  }
+
+  _loadEventFired() {
+    this._loadEventCallback();
   }
 };

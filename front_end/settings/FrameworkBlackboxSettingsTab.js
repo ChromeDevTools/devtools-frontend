54daf0889b1eef0e54bd11a46830d8914f7e3886
Collin Baker <collinbaker@chromium.org>
2019-05-17 21:26:07 +0000

    Revert "DevTools: Make Settings Blackboxing Pane Accessible"
    This reverts commit b8296ec2e06868d1365a61bbf0f283e297682252.
    
    Reason for revert: reverting to allow revert @ crrev.com/c/1618170
    
    Original change's description:
    > DevTools: Make Settings Blackboxing Pane Accessible
    > 
    > This change provides fixes for these issues:
    > Problem: The content title was heading role
    > Fix: Added heading role of level 1
    > Problem: Aria-label was missing in drop-downs
    > Fix: Added aria-label in drop-downs
    > 
    > Tested keyboard navigation
    > - navigate up/down drop-down items using arrow keys
    > - Checkbox is checked/unchecked using space bar
    > - Tab into/out (no tab traps)
    > 
    > Tested screen reader
    > - reads checkbox label
    > - reads items selected from the drop down list
    > 
    > Bug: 963183
    > 
    > Change-Id: Iaab2dd37323427215f765b3d7bbef885861c210f
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1605506
    > Commit-Queue: Chandani Shrestha <chshrest@microsoft.com>
    > Reviewed-by: Joel Einbinder <einbinder@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#660991}
    
    TBR=einbinder@chromium.org,luoe@chromium.org,jeffish@microsoft.com,jalissia@microsoft.com,ambake@microsoft.com,juxiao@microsoft.com,chshrest@microsoft.com
    
    Change-Id: I043b25bb968124defd01edd2058bb2603bc1e386
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 963183
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1618026
    Reviewed-by: Collin Baker <collinbaker@chromium.org>
    Commit-Queue: Collin Baker <collinbaker@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#661029}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: a25e20c256ea242b295b9f808e5a57bfd5629db1

--- front_end/settings/FrameworkBlackboxSettingsTab.js
+++ front_end/settings/FrameworkBlackboxSettingsTab.js
@@ -12,9 +12,7 @@ Settings.FrameworkBlackboxSettingsTab = class extends UI.VBox {
     super(true);
     this.registerRequiredCSS('settings/frameworkBlackboxSettingsTab.css');
 
-    const header = this.contentElement.createChild('div', 'header');
-    header.textContent = ls`Framework Blackbox Patterns`;
-    UI.ARIAUtils.markAsHeading(header, 1);
+    this.contentElement.createChild('div', 'header').textContent = Common.UIString('Framework Blackbox Patterns');
     this.contentElement.createChild('div', 'blackbox-content-scripts')
         .appendChild(UI.SettingsUI.createSettingCheckbox(
             Common.UIString('Blackbox content scripts'), Common.moduleSetting('skipContentScripts'), true));
@@ -38,6 +36,7 @@ Settings.FrameworkBlackboxSettingsTab = class extends UI.VBox {
     this._setting.addChangeListener(this._settingUpdated, this);
 
     this.setDefaultFocusedElement(addPatternButton);
+    this.contentElement.tabIndex = 0;
   }
 
   /**
@@ -134,13 +133,11 @@ Settings.FrameworkBlackboxSettingsTab = class extends UI.VBox {
     titles.createChild('div', 'blackbox-behavior').textContent = Common.UIString('Behavior');
 
     const fields = content.createChild('div', 'blackbox-edit-row');
-    const pattern = editor.createInput('pattern', 'text', ls`/framework\\.js$`, patternValidator.bind(this));
-    UI.ARIAUtils.setAccessibleName(pattern, ls`Pattern`);
-    fields.createChild('div', 'blackbox-pattern').appendChild(pattern);
+    fields.createChild('div', 'blackbox-pattern')
+        .appendChild(editor.createInput('pattern', 'text', '/framework\\.js$', patternValidator.bind(this)));
     fields.createChild('div', 'blackbox-separator blackbox-separator-invisible');
-    const behavior = editor.createSelect('behavior', [this._blackboxLabel, this._disabledLabel], behaviorValidator);
-    UI.ARIAUtils.setAccessibleName(behavior, ls`Behavior`);
-    fields.createChild('div', 'blackbox-behavior').appendChild(behavior);
+    fields.createChild('div', 'blackbox-behavior')
+        .appendChild(editor.createSelect('behavior', [this._blackboxLabel, this._disabledLabel], behaviorValidator));
 
     return editor;
 

387dc332f5793d4b10db1099c2ee0799e29c437f
Kayce Basques <kayce@google.com>
2019-05-29 21:15:55 +0000

    [devtools] update whatsnew for m76
    Bug: 967940
    Change-Id: I921dfeadb8780132cef228371395a491c9db264b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1633511
    Auto-Submit: Kayce Basques <kayce@google.com>
    Commit-Queue: Joel Einbinder <einbinder@chromium.org>
    Reviewed-by: Joel Einbinder <einbinder@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#664464}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 1a3f78a6cd736c438bcd54b03c467b18211ccf2a

--- front_end/help/ReleaseNoteText.js
+++ front_end/help/ReleaseNoteText.js
@@ -13,6 +13,29 @@ const commandMenuShortcut = Host.isMac() ? 'Command + Shift + P' : 'Control + Sh
 /** @type {!Array<!Help.ReleaseNote>} */
 Help.releaseNoteText = [
   {
+    version: 18,
+    header: 'Highlights from the Chrome 76 update',
+    highlights: [
+      {
+        title: 'Autocomplete with CSS keyword values',
+        subtitle: 'Typing a keyword value like "bold" in the Styles pane now autocompletes to "font-weight: bold".',
+        link: 'https://developers.google.com/web/updates/2019/05/devtools#values',
+      },
+      {
+        title: 'A new UI for network settings',
+        subtitle:
+            'The "Use large request rows", "Group by frame", "Show overview", and "Capture screenshots" options have moved to the new Network Settings pane.',
+        link: 'https://developers.google.com/web/updates/2019/05/devtools#settings',
+      },
+      {
+        title: 'WebSocket messages in HAR exports',
+        subtitle: 'Network logs downloaded from the Network panel now include WebSocket messages.',
+        link: 'https://developers.google.com/web/updates/2019/05/devtools#websocket',
+      },
+    ],
+    link: 'https://developers.google.com/web/updates/2019/05/devtools',
+  },
+  {
     version: 17,
     header: 'Highlights from the Chrome 75 update',
     highlights: [

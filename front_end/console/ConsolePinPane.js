53d64092ac102ef6d4149c988243cab8772c3c8f
John Emau <johnemau@microsoft.com>
2019-05-31 22:41:56 +0000

    DevTools: Make Console Accessible
    Marked 'Remove expression` console pin as a button with a tabindex,
    accessible name, Enter and Space keydown event activation, and focus
    indicator styling.
    
    Accessibility tree screenshot: https://i.imgur.com/6KCMN5b.png
    NVDA Speech Viewer screenshot: https://i.imgur.com/Dd5AyTW.png
    
    See: https://www.w3.org/WAI/WCAG21/quickref/#name-role-value
    Bug: 963183
    Change-Id: I5dbd6b1d25bf52a3beacaf088bf7395e0f4ef083
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1629787
    Reviewed-by: Erik Luo <luoe@chromium.org>
    Commit-Queue: John Emau <johnemau@microsoft.com>
    Cr-Original-Commit-Position: refs/heads/master@{#665314}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: ad4bda615f79c3ce2222a9f27ed1f6ce27f62f12

--- front_end/console/ConsolePinPane.js
+++ front_end/console/ConsolePinPane.js
@@ -102,6 +102,13 @@ Console.ConsolePin = class extends Common.Object {
     super();
     const deletePinIcon = UI.Icon.create('smallicon-cross', 'console-delete-pin');
     deletePinIcon.addEventListener('click', () => pinPane._removePin(this));
+    deletePinIcon.addEventListener('keydown', event => {
+      if (isEnterKey(event) || event.key === ' ')
+        pinPane._removePin(this);
+    });
+    deletePinIcon.tabIndex = 0;
+    UI.ARIAUtils.setAccessibleName(deletePinIcon, ls`Remove expression`);
+    UI.ARIAUtils.markAsButton(deletePinIcon);
 
     const fragment = UI.Fragment.build`
     <div class='console-pin'>

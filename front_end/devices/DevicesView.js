de221efcc3e5906d9945e8c56a19edc6d23f8920
Mandy Chen <mandy.chen@microsoft.com>
2019-06-14 00:51:26 +0000

    DevTools: Convert concatenated string and link to embedded link in string in remote devices tab
    Use UI.formatLocalize to embed a link inside a localizable string.
    
    UI.formatLocalized takes a string and a list of strings or DOM elements.
    A span element is created, and a string is appended as a text node through
    appendChild(createTextNode(str)), while an element is just appended through
    appendChild(element). Code:
    https://cs.chromium.org/chromium/src/third_party/blink/renderer/devtools/front_end/ui/UIUtils.js?type=cs&q=uiutils.js&sq=package:chromium&g=0&l=622
    
    Example before vs after: https://imgur.com/XBouvPo
    
    There are other places where the same change can be applied. To keep this
    patch small I only modified the remote devices tab. Sebsequent patches will
    be made.
    
    Bug: 941561
    Change-Id: Iaf1ed0451c2f748943c4b9dfc918a969e1c24d03
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1658750
    Commit-Queue: Mandy Chen <mandy.chen@microsoft.com>
    Reviewed-by: Alexei Filippov <alph@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#669072}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: c9ac18186c6b754e296b1f8edaa1f2e56092e82e

--- front_end/devices/DevicesView.js
+++ front_end/devices/DevicesView.js
@@ -35,11 +35,11 @@ Devices.DevicesView = class extends UI.VBox {
 
     const discoveryFooter = this.contentElement.createChild('div', 'devices-footer');
     this._deviceCountSpan = discoveryFooter.createChild('span');
-    discoveryFooter.createChild('span').textContent = Common.UIString(' Read ');
-    discoveryFooter.appendChild(UI.XLink.create(
+    const documentationLink = UI.XLink.create(
         'https://developers.google.com/chrome-developer-tools/docs/remote-debugging',
-        Common.UIString('remote debugging documentation')));
-    discoveryFooter.createChild('span').textContent = Common.UIString(' for more information.');
+        ls`remote debugging documentation`);
+    discoveryFooter.appendChild(
+        UI.formatLocalized('%s Read %s for more information.', [this._deviceCountSpan, documentationLink]));
     this._updateFooter();
     this._selectSidebarListItem(this._discoveryListItem, this._discoveryView);
 
@@ -217,10 +217,10 @@ Devices.DevicesView.DiscoveryView = class extends UI.VBox {
     }, false);
 
     const help = this.element.createChild('div', 'discovery-help');
-    help.createChild('span').textContent = Common.UIString('Need help? Read Chrome ');
-    help.appendChild(UI.XLink.create(
+    const documentationLink = UI.XLink.create(
         'https://developers.google.com/chrome-developer-tools/docs/remote-debugging',
-        Common.UIString('remote debugging documentation.')));
+        ls`remote debugging documentation`);
+    help.appendChild(UI.formatLocalized('Need help? Read Chrome %s.', [documentationLink]));
 
     /** @type {!Adb.Config} */
     this._config;
@@ -265,10 +265,12 @@ Devices.DevicesView.PortForwardingView = class extends UI.VBox {
     this._portForwardingEnabledCheckbox.addEventListener('click', this._update.bind(this), false);
 
     const portForwardingFooter = this.element.createChild('div', 'port-forwarding-footer');
-    portForwardingFooter.createChild('span').textContent = Common.UIString(
-        'Define the listening port on your device that maps to a port accessible from your development machine. ');
-    portForwardingFooter.appendChild(UI.XLink.create(
-        'https://developer.chrome.com/devtools/docs/remote-debugging#port-forwarding', Common.UIString('Learn more')));
+    const documentationLink = UI.XLink.create(
+        'https://developer.chrome.com/devtools/docs/remote-debugging#port-forwarding',
+        ls`remote debugging documentation`);
+    portForwardingFooter.appendChild(UI.formatLocalized(
+        'Define the listening port on your device that maps to a port accessible from your development machine. Read %s for more.',
+        [documentationLink]));
 
     /** @type {!UI.ListWidget<!Adb.PortForwardingRule>} */
     this._list = new UI.ListWidget(this);

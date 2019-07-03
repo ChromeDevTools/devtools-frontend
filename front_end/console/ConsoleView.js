f497b575af055e500d7c55ef702a57db5541e3cd
John Emau <johnemau@microsoft.com>
2019-07-03 02:05:41 +0000

    DevTools: Include console log level visual label in accessible name
    The console log level dropdown button is statically set to 'log level'
    this provides a poor experience for screen readers who must open the
    menu to determine which content is checked/selected.
    
    The visual label of the button is one of the following:
    1. The checked level label if only one level is checked.
    2. 'All levels' if all levels are checked.
    3. 'Default levels' if Info, Warning, and Errors are checked.
    
    This changes makes the accessible name (via UI.ToolbarButton.setTitle method)
    set to `Log level: ${the visual label goes here}`.
    
    This matches the pattern prescribed by ARIA authoring practices for a
    collapsible listbox and WCAG 2.5.3 Label in Name.
    See: https://w3c.github.io/aria-practices/examples//listbox/listbox-collapsible.html#rps_label
    See: https://www.w3.org/WAI/WCAG21/quickref/#label-in-name
    
    Screenshot: https://i.imgur.com/x0JLIK8.png
    Bug: 963183
    Change-Id: I56232d2ca1bfa60dc9b6c3837b2e03522faa13ff
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1642746
    Reviewed-by: Joel Einbinder <einbinder@chromium.org>
    Commit-Queue: John Emau <johnemau@microsoft.com>
    Cr-Original-Commit-Position: refs/heads/master@{#674329}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: f5fbcdacb06886e38d7312589f3836f9d3670f3d

--- front_end/console/ConsoleView.js
+++ front_end/console/ConsoleView.js
@@ -1330,6 +1330,7 @@ Console.ConsoleViewFilter = class {
       text = text || Common.UIString('Hide all');
     this._levelMenuButton.element.classList.toggle('warning', !isAll && !isDefault);
     this._levelMenuButton.setText(text);
+    this._levelMenuButton.setTitle(ls`Log level: ${text}`);
   }
 
   /**

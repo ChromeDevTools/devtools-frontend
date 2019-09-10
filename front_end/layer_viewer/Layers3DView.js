011dbc7fede73e6f8c126241cd0c1c1b1af032b7
Christy Chen <chrche@microsoft.com>
2019-09-10 18:01:47 +0000

    DevTools: Localize two strings for the checkboxes in Layers panel.
    Two strings for the checkboxes in Layers panel are not localized. This CL
    wrap them with ls, and include the strings in the GRDP.
    https://imgur.com/fbrbvKY
    
    Bug: 941561
    Change-Id: Iab4e3bfaa791dbb9542133109367284465b59d98
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1779170
    Reviewed-by: Erik Luo <luoe@chromium.org>
    Commit-Queue: Christy Chen <chrche@microsoft.com>
    Cr-Original-Commit-Position: refs/heads/master@{#695240}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 01b9659ec1181ea493cb2ce794de21ed391a2b45

--- front_end/layer_viewer/Layers3DView.js
+++ front_end/layer_viewer/Layers3DView.js
@@ -678,10 +678,10 @@ LayerViewer.Layers3DView = class extends UI.VBox {
   _initToolbar() {
     this._panelToolbar = this._transformController.toolbar();
     this.contentElement.appendChild(this._panelToolbar.element);
-    this._showSlowScrollRectsSetting =
-        this._createVisibilitySetting('Slow scroll rects', 'frameViewerShowSlowScrollRects', true, this._panelToolbar);
+    this._showSlowScrollRectsSetting = this._createVisibilitySetting(
+        ls`Slow scroll rects`, 'frameViewerShowSlowScrollRects', true, this._panelToolbar);
     this._showPaintsSetting =
-        this._createVisibilitySetting('Paints', 'frameViewerShowPaints', true, this._panelToolbar);
+        this._createVisibilitySetting(ls`Paints`, 'frameViewerShowPaints', true, this._panelToolbar);
     this._showPaintsSetting.addChangeListener(this._updatePaints, this);
     Common.moduleSetting('frameViewerHideChromeWindow').addChangeListener(this._update, this);
   }

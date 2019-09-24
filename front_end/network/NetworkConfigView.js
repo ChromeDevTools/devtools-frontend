d0ae96c0b9459f8b754ce588a2bc0febbdadde70
Christy Chen <chrche@microsoft.com>
2019-09-24 19:05:17 +0000

    DevTools: Localize user agent dropdown menu in Network conditions tool
    In the Network condition tools, user facing strings in the dropdown menu
    of user agent options are not localized.
    
    https://imgur.com/ZDp4j8D
    
    In this change:
    - adds ls() calls to those strings in NetworkConfigView.js.
    - GRDP changes are generated automatically, with manually written descriptions
    - Placeholders (<ph name="LOCKED_1">) are added in the grdp to prevent
    translations of the branding related strings.
    
    
    Bug: 941561
    Change-Id: Ib274dd425733d0ef4f4199ff6334810f113ade27
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1816184
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Reviewed-by: Mandy Chen <mandy.chen@microsoft.com>
    Commit-Queue: Christy Chen <chrche@microsoft.com>
    Cr-Original-Commit-Position: refs/heads/master@{#699434}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 0f8efb4dd92869f559add716ff959724ed799ae5

--- front_end/network/NetworkConfigView.js
+++ front_end/network/NetworkConfigView.js
@@ -151,236 +151,239 @@ Network.NetworkConfigView = class extends UI.VBox {
 /** @type {!Array.<{title: string, values: !Array.<{title: string, value: string}>}>} */
 Network.NetworkConfigView._userAgentGroups = [
   {
-    title: 'Android',
+    title: ls`Android`,
     values: [
       {
-        title: 'Android (4.0.2) Browser \u2014 Galaxy Nexus',
+        title: ls`Android (4.0.2) Browser \u2014 Galaxy Nexus`,
         value:
             'Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30'
       },
       {
-        title: 'Android (2.3) Browser \u2014 Nexus S',
+        title: ls`Android (2.3) Browser \u2014 Nexus S`,
         value:
             'Mozilla/5.0 (Linux; U; Android 2.3.6; en-us; Nexus S Build/GRK39F) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1'
       }
     ]
   },
   {
-    title: 'BlackBerry',
+    title: ls`BlackBerry`,
     values: [
       {
-        title: 'BlackBerry \u2014 BB10',
+        title: ls`BlackBerry \u2014 BB10`,
         value:
             'Mozilla/5.0 (BB10; Touch) AppleWebKit/537.1+ (KHTML, like Gecko) Version/10.0.0.1337 Mobile Safari/537.1+'
       },
       {
-        title: 'BlackBerry \u2014 PlayBook 2.1',
+        title: ls`BlackBerry \u2014 PlayBook 2.1`,
         value:
             'Mozilla/5.0 (PlayBook; U; RIM Tablet OS 2.1.0; en-US) AppleWebKit/536.2+ (KHTML, like Gecko) Version/7.2.1.0 Safari/536.2+'
       },
       {
-        title: 'BlackBerry \u2014 9900',
+        title: ls`BlackBerry \u2014 9900`,
         value:
             'Mozilla/5.0 (BlackBerry; U; BlackBerry 9900; en-US) AppleWebKit/534.11+ (KHTML, like Gecko) Version/7.0.0.187 Mobile Safari/534.11+'
       }
     ]
   },
   {
-    title: 'Chrome',
+    title: ls`Chrome`,
     values: [
       {
-        title: 'Chrome \u2014 Android Mobile',
+        title: ls`Chrome \u2014 Android Mobile`,
         value:
             'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/%s Mobile Safari/537.36'
       },
       {
-        title: 'Chrome \u2014 Android Tablet',
+        title: ls`Chrome \u2014 Android Tablet`,
         value:
             'Mozilla/5.0 (Linux; Android 4.3; Nexus 7 Build/JSS15Q) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/%s Safari/537.36'
       },
       {
-        title: 'Chrome \u2014 iPhone',
+        title: ls`Chrome \u2014 iPhone`,
         value:
             'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1 (KHTML, like Gecko) CriOS/%s Mobile/13B143 Safari/601.1.46'
       },
       {
-        title: 'Chrome \u2014 iPad',
+        title: ls`Chrome \u2014 iPad`,
         value:
             'Mozilla/5.0 (iPad; CPU OS 9_1 like Mac OS X) AppleWebKit/601.1 (KHTML, like Gecko) CriOS/%s Mobile/13B143 Safari/601.1.46'
       },
       {
-        title: 'Chrome \u2014 Chrome OS',
+        title: ls`Chrome \u2014 Chrome OS`,
         value: 'Mozilla/5.0 (X11; CrOS x86_64 10066.0.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/%s Safari/537.36'
       },
       {
-        title: 'Chrome \u2014 Mac',
+        title: ls`Chrome \u2014 Mac`,
         value:
             'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/%s Safari/537.36'
       },
       {
-        title: 'Chrome \u2014 Windows',
+        title: ls`Chrome \u2014 Windows`,
         value: 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/%s Safari/537.36'
       }
     ]
   },
   {
-    title: 'Firefox',
+    title: ls`Firefox`,
     values: [
       {
-        title: 'Firefox \u2014 Android Mobile',
+        title: ls`Firefox \u2014 Android Mobile`,
         value: 'Mozilla/5.0 (Android 4.4; Mobile; rv:46.0) Gecko/46.0 Firefox/46.0'
       },
       {
-        title: 'Firefox \u2014 Android Tablet',
+        title: ls`Firefox \u2014 Android Tablet`,
         value: 'Mozilla/5.0 (Android 4.4; Tablet; rv:46.0) Gecko/46.0 Firefox/46.0'
       },
       {
-        title: 'Firefox \u2014 iPhone',
+        title: ls`Firefox \u2014 iPhone`,
         value:
             'Mozilla/5.0 (iPhone; CPU iPhone OS 8_3 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) FxiOS/1.0 Mobile/12F69 Safari/600.1.4'
       },
       {
-        title: 'Firefox \u2014 iPad',
+        title: ls`Firefox \u2014 iPad`,
         value:
             'Mozilla/5.0 (iPad; CPU iPhone OS 8_3 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) FxiOS/1.0 Mobile/12F69 Safari/600.1.4'
       },
       {
-        title: 'Firefox \u2014 Mac',
+        title: ls`Firefox \u2014 Mac`,
         value: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:46.0) Gecko/20100101 Firefox/46.0'
       },
       {
-        title: 'Firefox \u2014 Windows',
+        title: ls`Firefox \u2014 Windows`,
         value: 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:46.0) Gecko/20100101 Firefox/46.0'
       }
     ]
   },
   {
-    title: 'Googlebot',
+    title: ls`Googlebot`,
     values: [
-      {title: 'Googlebot', value: 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'}, {
-        title: 'Googlebot Smartphone',
+      {title: ls`Googlebot`, value: 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'}, {
+        title: ls`Googlebot Smartphone`,
         value:
             'Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.96 Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'
       }
     ]
   },
   {
-    title: 'Internet Explorer',
+    title: ls`Internet Explorer`,
     values: [
-      {title: 'Internet Explorer 11', value: 'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko'},
-      {title: 'Internet Explorer 10', value: 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)'},
-      {title: 'Internet Explorer 9', value: 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)'},
-      {title: 'Internet Explorer 8', value: 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0)'},
-      {title: 'Internet Explorer 7', value: 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)'}
+      {title: ls`Internet Explorer 11`, value: 'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko'},
+      {
+        title: ls`Internet Explorer 10`,
+        value: 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)'
+      },
+      {title: ls`Internet Explorer 9`, value: 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)'},
+      {title: ls`Internet Explorer 8`, value: 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0)'},
+      {title: ls`Internet Explorer 7`, value: 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)'}
     ]
   },
   {
-    title: 'Microsoft Edge',
+    title: ls`Microsoft Edge`,
     values: [
       {
-        title: 'Microsoft Edge (Chromium) \u2014 Windows',
+        title: ls`Microsoft Edge (Chromium) \u2014 Windows`,
         value:
             'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/%s Safari/537.36 Edg/%s'
       },
       {
-        title: 'Microsoft Edge (Chromium) \u2014 Mac',
+        title: ls`Microsoft Edge (Chromium) \u2014 Mac`,
         value:
             'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/%s Safari/537.36 Edg/%s'
       },
       {
-        title: 'Microsoft Edge \u2014 iPhone',
+        title: ls`Microsoft Edge \u2014 iPhone`,
         value:
             'Mozilla/5.0 (iPhone; CPU iPhone OS 12_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.1 EdgiOS/44.5.0.10 Mobile/15E148 Safari/604.1'
       },
       {
-        title: 'Microsoft Edge \u2014 iPad',
+        title: ls`Microsoft Edge \u2014 iPad`,
         value:
             'Mozilla/5.0 (iPad; CPU OS 12_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0 EdgiOS/44.5.2 Mobile/15E148 Safari/605.1.15'
       },
       {
-        title: 'Microsoft Edge \u2014 Android Mobile',
+        title: ls`Microsoft Edge \u2014 Android Mobile`,
         value:
             'Mozilla/5.0 (Linux; Android 8.1.0; Pixel Build/OPM4.171019.021.D1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.109 Mobile Safari/537.36 EdgA/42.0.0.2057'
       },
       {
-        title: 'Microsoft Edge \u2014 Android Tablet',
+        title: ls`Microsoft Edge \u2014 Android Tablet`,
         value:
             'Mozilla/5.0 (Linux; Android 6.0.1; Nexus 7 Build/MOB30X) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.109 Safari/537.36 EdgA/42.0.0.2057'
       },
       {
-        title: 'Microsoft Edge (EdgeHTML) \u2014 Windows',
+        title: ls`Microsoft Edge (EdgeHTML) \u2014 Windows`,
         value:
             'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.18362'
       },
       {
-        title: 'Microsoft Edge (EdgeHTML) \u2014 XBox',
+        title: ls`Microsoft Edge (EdgeHTML) \u2014 XBox`,
         value:
             'Mozilla/5.0 (Windows NT 10.0; Win64; x64; Xbox; Xbox One) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.18362'
       }
     ]
   },
   {
-    title: 'Opera',
+    title: ls`Opera`,
     values: [
       {
-        title: 'Opera \u2014 Mac',
+        title: ls`Opera \u2014 Mac`,
         value:
             'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.87 Safari/537.36 OPR/37.0.2178.31'
       },
       {
-        title: 'Opera \u2014 Windows',
+        title: ls`Opera \u2014 Windows`,
         value:
             'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.87 Safari/537.36 OPR/37.0.2178.31'
       },
       {
-        title: 'Opera (Presto) \u2014 Mac',
+        title: ls`Opera (Presto) \u2014 Mac`,
         value: 'Opera/9.80 (Macintosh; Intel Mac OS X 10.9.1) Presto/2.12.388 Version/12.16'
       },
-      {title: 'Opera (Presto) \u2014 Windows', value: 'Opera/9.80 (Windows NT 6.1) Presto/2.12.388 Version/12.16'}, {
-        title: 'Opera Mobile \u2014 Android Mobile',
+      {title: ls`Opera (Presto) \u2014 Windows`, value: 'Opera/9.80 (Windows NT 6.1) Presto/2.12.388 Version/12.16'}, {
+        title: ls`Opera Mobile \u2014 Android Mobile`,
         value: 'Opera/12.02 (Android 4.1; Linux; Opera Mobi/ADR-1111101157; U; en-US) Presto/2.9.201 Version/12.02'
       },
       {
-        title: 'Opera Mini \u2014 iOS',
+        title: ls`Opera Mini \u2014 iOS`,
         value: 'Opera/9.80 (iPhone; Opera Mini/8.0.0/34.2336; U; en) Presto/2.8.119 Version/11.10'
       }
     ]
   },
   {
-    title: 'Safari',
+    title: ls`Safari`,
     values: [
       {
-        title: 'Safari \u2014 iPad iOS 9',
+        title: ls`Safari \u2014 iPad iOS 9`,
         value:
             'Mozilla/5.0 (iPad; CPU OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B137 Safari/601.1'
       },
       {
-        title: 'Safari \u2014 iPhone iOS 9',
+        title: ls`Safari \u2014 iPhone iOS 9`,
         value:
             'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B137 Safari/601.1'
       },
       {
-        title: 'Safari \u2014 Mac',
+        title: ls`Safari \u2014 Mac`,
         value:
             'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A'
       }
     ]
   },
   {
-    title: 'UC Browser',
+    title: ls`UC Browser`,
     values: [
       {
-        title: 'UC Browser \u2014 Android Mobile',
+        title: ls`UC Browser \u2014 Android Mobile`,
         value:
             'Mozilla/5.0 (Linux; U; Android 8.1.0; en-US; Nexus 6P Build/OPM7.181205.001) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/57.0.2987.108 UCBrowser/12.11.1.1197 Mobile Safari/537.36'
       },
       {
-        title: 'UC Browser \u2014 iOS',
+        title: ls`UC Browser \u2014 iOS`,
         value:
             'Mozilla/5.0 (iPhone; CPU iPhone OS 12_1 like Mac OS X; zh-CN) AppleWebKit/537.51.1 (KHTML, like Gecko) Mobile/16B92 UCBrowser/12.1.7.1109 Mobile AliApp(TUnionSDK/0.1.20.3)'
       },
       {
-        title: 'UC Browser \u2014 Windows Phone',
+        title: ls`UC Browser \u2014 Windows Phone`,
         value:
             'Mozilla/5.0 (compatible; MSIE 10.0; Windows Phone 8.0; Trident/6.0; IEMobile/10.0; ARM; Touch; NOKIA; Lumia 920) UCBrowser/10.1.0.563 Mobile'
       }

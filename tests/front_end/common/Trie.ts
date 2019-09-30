0e8ef42b1f2d7e5ec9c31f5268eb91709c7149a1
Paul Lewis <aerotwist@chromium.org>
2019-09-30 10:54:12 +0000

    [DevTools] Adds Trie tests
    Change-Id: Ic49e9ca826a645289f1b7819f939ccffd612e9f4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1829700
    Reviewed-by: Mathias Bynens <mathias@chromium.org>
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Commit-Queue: Paul Lewis <aerotwist@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#701056}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: e0c93be5928ddb8bac973d055f346cdfecc99112

---
+++ tests/front_end/common/Trie.ts
@@ -0,0 +1,58 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+const { assert } = chai;
+
+import { default as Trie } from '../../../front_end/common/Trie.js';
+
+describe('Trie', () => {
+  let trie: Trie;
+  beforeEach(() => {
+    trie = new Trie();
+  });
+
+  it('stores and retrieves words', () => {
+    trie.add('foo');
+    assert.isTrue(trie.has('foo'));
+    assert.isFalse(trie.has('bar'));
+  });
+
+  it('removes words', () => {
+    trie.add('foo');
+    assert.isTrue(trie.has('foo'));
+    trie.remove('foo');
+    assert.isFalse(trie.has('foo'));
+  });
+
+  it('completes words based on prefixes', () => {
+    trie.add('foo');
+    trie.add('food');
+    trie.add('flora');
+    trie.add('boat');
+    trie.add('focus');
+    trie.add('banana');
+    assert.deepEqual(trie.words('fo'), ['foo', 'food', 'focus']);
+    assert.isEmpty(trie.words('cat'));
+  });
+
+  it('clears words', () => {
+    trie.add('foo');
+    assert.isTrue(trie.has('foo'));
+
+    trie.clear();
+    assert.isFalse(trie.has('foo'));
+    assert.isEmpty(trie.words('foo'));
+  });
+
+  it('provides the longest prefix', () => {
+    trie.add('super');
+    trie.add('supercar');
+
+    // Longest non-word prefix match.
+    assert.equal(trie.longestPrefix('supercalifragilisticexpialidocious', false), 'superca');
+
+    // Longest word prefix match.
+    assert.equal(trie.longestPrefix('supercalifragilisticexpialidocious', true), 'super');
+  });
+});

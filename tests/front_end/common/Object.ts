110192d2f9ae3a7f721a1598c5b8ae593a3cb696
Paul Lewis <aerotwist@chromium.org>
2019-10-04 14:32:00 +0000

    [DevTools] Adds Karma tests for common/Object
    Bug: 1009443
    Change-Id: If5cc243605332b6264aead0ba2a869a751e57bb4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/1834143
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Reviewed-by: Mathias Bynens <mathias@chromium.org>
    Commit-Queue: Paul Lewis <aerotwist@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#702860}
    Cr-Mirrored-From: https://chromium.googlesource.com/chromium/src
    Cr-Mirrored-Commit: 157d11096bf681561ff6fe88919be960aeef2ad9

---
+++ tests/front_end/common/Object.ts
@@ -0,0 +1,78 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+const { assert } = chai;
+
+import { default as ObjectWrapper } from '../../../front_end/common/Object.js';
+
+describe('ObjectWrapper', () => {
+  let obj: ObjectWrapper;
+  beforeEach(() => {
+    obj = new ObjectWrapper();
+  });
+
+  describe('event listeners', () => {
+    it('adds event listeners', (done) => {
+      obj.addEventListener('foo', () => {
+        done();
+      });
+
+      obj.dispatchEventToListeners('foo');
+    });
+
+    it('reports event listeners correctly', () => {
+      const callback = () => {};
+
+      obj.addEventListener('foo', callback);
+      assert.isTrue(obj.hasEventListeners('foo'));
+
+      obj.removeEventListener('foo', callback);
+      assert.isFalse(obj.hasEventListeners('foo'));
+    });
+
+    it('fires event listeners once', async () => {
+      const fireOnce = obj.once('foo');
+      obj.dispatchEventToListeners('foo');
+      await fireOnce;
+
+      assert.isFalse(obj.hasEventListeners('foo'));
+    });
+
+    it('fires event listeners multiple times', () => {
+      let count = 0;
+      const callback = () => {
+        count++;
+      };
+
+      obj.addEventListener('foo', callback);
+      obj.dispatchEventToListeners('foo');
+      obj.dispatchEventToListeners('foo');
+      assert.equal(count, 2);
+    });
+
+    it('fires event listeners with data', (done) => {
+      let count = 0;
+      const callback = (evt: { data: { bar: string } } ) => {
+        assert.equal(evt.data.bar, 'baz');
+        done();
+      };
+
+      obj.addEventListener('foo', callback);
+      obj.dispatchEventToListeners('foo', { bar: 'baz' });
+      assert.equal(count, 2);
+    });
+
+    it('handles removal of non-existent listener', () => {
+      assert.doesNotThrow(() => {
+        obj.removeEventListener('foo', () => {});
+      });
+    });
+
+    it('handles dispatch of events with zero listeners', () => {
+      assert.doesNotThrow(() => {
+        obj.dispatchEventToListeners('foo');
+      });
+    });
+  });
+});

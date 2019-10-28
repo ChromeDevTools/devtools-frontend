d931f8c9f58ff6dee6019d6a694b64927a623d9a
Michael Liao <michael.liao@microsoft.com>
2019-10-28 14:02:39 -0700

    DevTools [Common - Tabbed Pane]: Fixing context menu item type for more tabs dropdown
    Re-upload of https://chromium-review.googlesource.com/c/chromium/src/+/1863637 to DevTools repository
    
    Issue:
    - All menu items in the "More Tabs" dropdown are read as "Layers not checked 2 of 8" even though they do not appear as functional checkboxes
    
    Changes:
    - Menu items are set as normal menu item types unless the check visual is needed in which case it is set as a checkbox menu item
    
    The check visual is needed when the selected tab is in the "More Tabs" dropdown.
    This is only the case when there are no tabs showing on the toolbar since the selected tab has the highest priority of being shown
    Example:
    https://imgur.com/vkvDEqs
    
    Original SR Functionality:
    https://imgur.com/mEpgmuY
    
    Fixed SR Functionality:
    https://imgur.com/uho36lJ
    
    Bug: 963183
    Change-Id: I119659cfb629e04252b57a25ece85d8863df9119
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/1873131
    Commit-Queue: Michael Liao <michael.liao@microsoft.com>
    Reviewed-by: Lorne Mitchell <lomitch@microsoft.com>

--- front_end/ui/TabbedPane.js
+++ front_end/ui/TabbedPane.js
@@ -620,13 +620,16 @@ export default class TabbedPane extends UI.VBox {
     }
     const rect = this._dropDownButton.getBoundingClientRect();
     const menu = new UI.ContextMenu(event, false, rect.left, rect.bottom);
-    for (let i = 0; i < this._tabs.length; ++i) {
-      const tab = this._tabs[i];
+    for (const tab of this._tabs) {
       if (tab._shown) {
         continue;
       }
-      menu.defaultSection().appendCheckboxItem(
-          tab.title, this._dropDownMenuItemSelected.bind(this, tab), this._tabsHistory[0] === tab);
+      if (this._numberOfTabsShown() === 0 && this._tabsHistory[0] === tab) {
+        menu.defaultSection().appendCheckboxItem(
+            tab.title, this._dropDownMenuItemSelected.bind(this, tab), /* checked */ true);
+      } else {
+        menu.defaultSection().appendItem(tab.title, this._dropDownMenuItemSelected.bind(this, tab));
+      }
     }
     menu.show();
   }

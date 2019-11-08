1fd4edf9b27b2bfc7bf8dd7ddaeb4bd0d6eca2ae
Yang Guo <yangguo@chromium.org>
2019-11-08 13:32:55 +0100

    Roll js_protocol.pdl and css_properties.json5 from upstream
    CSS:
     - Added "anywhere" keyword to "overflow-wrap".
    
    TBR=aerotwist@chromium.org
    
    Protocol:
     - Added type Profiler.CounterInfo.
     - Added command Profiler.enableRuntimeCallStats.
     - Added command Profiler.disableRuntimeCallStats.
     - Added command Profiler.getRuntimeCallStats.
    Change-Id: Ic5a9174427dd0bb8f7ea61094570e77eb3adfac9
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/1905827
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Commit-Queue: Yang Guo <yangguo@chromium.org>

---
+++ test/unittests/front_end/ui/Fragment.ts
@@ -0,0 +1,53 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+const {assert} = chai;
+
+import {default as Fragment} from '../../../../front_end/ui/Fragment.js';
+
+describe('Fragment', () => {
+  it('can be instantiated correctly', () => {
+    const testEl = document.createElement('p');
+    const fragment = new Fragment(testEl);
+    assert.equal(fragment.element().outerHTML, '<p></p>', 'element was not set correctly');
+  });
+
+  it('is able to return an element from an ID', () => {
+    const build = Fragment.build(['<p>', '</p>'], 'Test Text');
+    assert.equal(build.element().outerHTML, '<p>Test Text</p>', 'the element was not built correctly');
+  });
+
+  it('is able to retrieve cached fragment', () => {
+    const cached = Fragment.cached(['<p>', '</p>'], 'Test Text');
+    assert.equal(cached.element().outerHTML, '<p>Test Text</p>', 'the element was not cached correctly');
+  });
+
+  it('is able to build a template with no inside text', () => {
+    const build = Fragment.build(['<', '>'], 'p');
+    assert.equal(
+        build.element().outerHTML, '<template-attribute0></template-attribute0>',
+        'the element was not built correctly');
+  });
+
+  it('is able to build a template with no tags', () => {
+    const build = Fragment.build(['a', 'b'], 'testText');
+    assert.equal(build.element().textContent, 'atestTextb', 'the element was not built correctly');
+  });
+
+  it('is able to build a template with a normal attribute', () => {
+    const build = Fragment.build(['<p align="center">', '</p>'], 'Test Text');
+    assert.equal(build.element().outerHTML, '<p align="center">Test Text</p>', 'the element was not built correctly');
+  });
+
+  it('is able to build a template with a dollar sign attribute', () => {
+    const build = Fragment.build(['<p $>', '</p>'], 'Test Text');
+    assert.equal(build.element().outerHTML, '<p class="">Test Text</p>', 'the element was not built correctly');
+  });
+
+  it('is able to build a template with template attribute', () => {
+    const build = Fragment.build(['<p template-attribute0>', '</p>'], 'testAttribute', 'Test Text');
+    assert.equal(
+        build.element().outerHTML, '<p class="" testattribute="">Test Text</p>', 'the element was not built correctly');
+  });
+});

c4c0786bd310b8780b53eac712e1db10fd979077
Tim van der Lippe <tvanderlippe@chromium.org>
2019-11-27 16:30:36 +0000

    Fix race-condition with CodeMirror syntax highlighting
    Temporary work-around the race-condition by eagerly importing
    all modes for now.
    
    roll CodeMirror
    
    TBR=aerotwist@chromium.org
    
    Bug: 1029037
    Change-Id: I7ff1703b6003a2383f56ce299a94bfd5169f5ed5
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/1939799
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>
    Commit-Queue: Tim van der Lippe <tvanderlippe@chromium.org>
    Auto-Submit: Tim van der Lippe <tvanderlippe@chromium.org>

--- front_end/cm_modes/cm_modes.js
+++ front_end/cm_modes/cm_modes.js
@@ -2,6 +2,18 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// TODO(crbug.com/1029037): lazily load these files again after the
+// race-condition with CodeMirror is fixed
+import './clike.js';
+import './coffeescript.js';
+import './php.js';
+import './python.js';
+import './shell.js';
+import './livescript.js';
+import './markdown.js';
+import './clojure.js';
+import './jsx.js';
+
 import * as DefaultCodeMirrorMimeMode from './DefaultCodeMirrorMimeMode.js';
 
 export {

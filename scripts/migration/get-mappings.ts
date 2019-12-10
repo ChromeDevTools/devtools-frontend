26afc1e4f9279af89987bd76e1f6686198a7b2cc
Paul Lewis <aerotwist@chromium.org>
2019-12-10 13:42:06 +0000

    Adds migration script for refs
    This script expects that -legacy.js files are in place for a
    given module. So, for example, if migrating front_end/search
    one would run move-side-effects-to-legacy for search, then follow
    that with this script, replace-internal-references, which will
    step through the files in front_end/search and update all globals
    to refer to imports.
    
    This script also supports cross-module rewrites, such that if a
    namespace is passed through, the -legacy.js file for the namespace
    will be loaded instead and used for the replacements.
    
    Bug: 1006759
    Change-Id: I7af5e1343a028aed82d5c3f536be886011cf9a2c
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/1958525
    Commit-Queue: Paul Lewis <aerotwist@chromium.org>
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>

---
+++ scripts/migration/get-mappings.ts
@@ -0,0 +1,59 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import { parse, print, types, visit } from 'recast';
+import fs from 'fs';
+import path from 'path';
+import { promisify } from 'util';
+
+const readDir = promisify(fs.readdir);
+const readFile = promisify(fs.readFile);
+const stat = promisify(fs.stat);
+const b = types.builders;
+
+const FRONT_END_FOLDER = path.join(__dirname, '..', '..', 'front_end')
+
+export async function getMappings(namespace: string, mappings: Map<string, any>, useExternalRefs = false) {
+  const src = namespace.toLocaleLowerCase();
+  const externalModule = path.join(FRONT_END_FOLDER, src, `${src}.js`);
+  const legacy = path.join(FRONT_END_FOLDER, src, `${src}-legacy.js`);
+
+  if (!(await stat(legacy))) {
+    console.error(`Unable to find legacy file: ${legacy}`);
+    process.exit(1);
+    return mappings;
+  }
+
+  const legacyFileContents = await readFile(legacy, { encoding: 'utf-8' });
+  const ast = parse(legacyFileContents);
+
+  for (const statement of ast.program.body) {
+    if (statement.type !== 'ExpressionStatement') {
+      continue;
+    }
+
+    // We need to check that we have an assignment expression, of which the left and right are both member expressions.
+    // This allows us to extract things like Foo.Bar = FooModule.Bar.Bar from the legacy file, while ignoring self.Foo
+    // and Foo = Foo || {} statements.
+    const isMemberExpressionOnLeftAndRight = statement.expression && statement.expression.left && statement.expression.right &&
+        statement.expression.type === 'AssignmentExpression' && statement.expression.left.type === 'MemberExpression' &&
+        statement.expression.right.type === 'MemberExpression'
+
+    if (isMemberExpressionOnLeftAndRight) {
+      // Rename FooModule back to Foo because we know we will want to use the latter when doing replacements.
+      if (statement.expression.right.object && statement.expression.right.object.object.type === 'Identifier') {
+        statement.expression.right.object.object.name = statement.expression.right.object.object.name.replace(/Module$/, '');
+      }
+
+      const leftSide = print(statement.expression.left).code;
+      const rightSide = print(statement.expression.right).code;
+      const rightSideParts = rightSide.split('.');
+      const file = useExternalRefs ? externalModule : path.join(FRONT_END_FOLDER, src, rightSideParts[1] + '.js');
+
+      mappings.set(leftSide, { file, replacement: rightSide, sameFolderReplacement: rightSideParts[rightSideParts.length - 1] });
+    }
+  }
+
+  return mappings;
+}
---
+++ scripts/migration/replace-internal-references.ts
@@ -0,0 +1,156 @@
+// Copyright 2019 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import { parse, print, types, visit } from 'recast';
+import fs from 'fs';
+import path from 'path';
+import { promisify } from 'util';
+import { getMappings } from './get-mappings.js';
+
+const readDir = promisify(fs.readdir);
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
+const stat = promisify(fs.stat);
+const b = types.builders;
+
+const FRONT_END_FOLDER = path.join(__dirname, '..', '..', 'front_end')
+
+async function rewriteSource(pathName: string, srcFile: string, mappings:Map<string, any>, useExternalRefs = false) {
+  const filePath = path.join(pathName, srcFile);
+  const srcFileContents = await readFile(filePath, { encoding: 'utf-8' });
+  const ast = parse(srcFileContents);
+
+  const importsRequired = new Set<{file: string, replacement: string, sameFolderReplacement: string}>();
+
+  visit(ast, {
+    visitComment(path) {
+      const comments = (path.node as any).comments;
+      for (const comment of comments) {
+
+        if (comment.loc) {
+          (comment.loc as any).indent = 0;
+        }
+
+        for (const [str, value] of mappings.entries()) {
+          const containsString = new RegExp(`${str}([^\\.\\w])`, 'g');
+          const stringMatches = containsString.exec(comment.value);
+
+          if (!stringMatches) {
+            continue;
+          }
+
+          const replacement = useExternalRefs ? value.replacement : value.sameFolderReplacement;
+
+          importsRequired.add(value);
+          comment.value = comment.value.replace(stringMatches[0], replacement + stringMatches[0].slice(-1));
+        }
+      }
+
+      this.traverse(path);
+    },
+
+    visitMemberExpression(path) {
+      const node = path.node;
+      const nodeCopy = b.memberExpression.from({...node, comments: []});
+      const nodeAsCode = print(nodeCopy).code;
+
+      for (const [str, value] of mappings.entries()) {
+        if (nodeAsCode !== str) {
+          continue;
+        }
+
+        const name = useExternalRefs ? value.replacement : value.sameFolderReplacement;
+
+        importsRequired.add(value);
+        return b.identifier.from({ name, comments: node.comments || [] });
+      }
+
+      this.traverse(path);
+    }
+  });
+
+  const importMap = new Map<string, any[]>();
+  for (const { file, sameFolderReplacement, replacement } of importsRequired) {
+    if (filePath === file) {
+      continue;
+    }
+
+    const src = path.dirname(filePath);
+    const dst = path.dirname(file);
+
+    let replacementIdentifier = '';
+    let relativePath = path.relative(src, dst);
+    const isSameFolder = relativePath === '';
+    if (isSameFolder) {
+      relativePath = './';
+      replacementIdentifier = sameFolderReplacement;
+    } else {
+      relativePath += '/';
+      replacementIdentifier = replacement;
+    }
+
+    const targetImportFile = relativePath + path.basename(file);
+
+    if (!importMap.has(targetImportFile)) {
+      importMap.set(targetImportFile, []);
+    }
+
+    const imports = importMap.get(targetImportFile)!;
+    if (useExternalRefs) {
+      if (imports.length === 0) {
+        // We are creating statements like import * as Foo from '../foo/foo.js' so
+        // here we take the first part of the identifier, e.g. Foo.Bar.Bar gives us
+        // Foo so we can make import * as Foo from that.
+        const namespaceIdentifier = replacementIdentifier.split('.')[0];
+        imports.push(b.importNamespaceSpecifier(b.identifier(namespaceIdentifier)));
+      }
+
+      // Make sure there is only one import * from Foo import added.
+      continue;
+    }
+
+    imports.push(b.importSpecifier(b.identifier(replacementIdentifier)));
+  }
+
+  // Add missing imports.
+  for (const [targetImportFile, specifiers] of importMap) {
+    const newImport = b.importDeclaration.from({
+      specifiers,
+      comments: ast.program.body[0].comments,
+      source: b.literal(targetImportFile)
+    });
+
+    // Remove any file comments.
+    ast.program.body[0].comments = [];
+
+    // Add the import statements.
+    ast.program.body.unshift(newImport);
+  }
+
+  return print(ast).code;
+}
+
+async function main(folder: string, namespace?: string) {
+  const pathName = path.join(FRONT_END_FOLDER, folder);
+  const srcDir = await readDir(pathName);
+  const targetNamespace = namespace ? namespace : folder;
+  const useExternalRefs = namespace !== undefined && (namespace !== folder);
+  const mappings = await getMappings(targetNamespace, new Map(), useExternalRefs);
+
+  for (const srcFile of srcDir) {
+    if (srcFile === `${folder}.js` || srcFile === `${folder}-legacy.js` || !srcFile.endsWith('.js')) {
+      continue;
+    }
+
+    const distFileContents = await rewriteSource(pathName, srcFile, mappings, useExternalRefs);
+    await writeFile(path.join(pathName, `${srcFile}`), distFileContents);
+  }
+}
+
+if (!process.argv[2]) {
+  console.error(`No arguments specified. Run this script with "<folder-name>". For example: "ui"`);
+  process.exit(1);
+}
+
+main(process.argv[2], process.argv[3]);

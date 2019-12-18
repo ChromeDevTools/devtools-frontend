24f39c4397841366751a20454c58c84c0b98df54
Paul Lewis <aerotwist@chromium.org>
2019-12-18 14:27:15 +0000

    Enables Karma to load images
    Change-Id: I58da3e65eebc943207b02401fe3d7b00eb6bcd14
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/1971937
    Reviewed-by: Almothana Athamneh <almuthanna@chromium.org>
    Commit-Queue: Paul Lewis <aerotwist@chromium.org>

--- karma.conf.js
+++ karma.conf.js
@@ -18,7 +18,8 @@ module.exports = function(config) {
     basePath: '',
 
     files: [
-      {pattern: 'front_end/**/*.js', included: false, served: true}, {pattern: 'test/unittests/**/*.ts', type: 'module'}
+      {pattern: 'front_end/**/*.js', included: false, served: true}, {pattern: 'test/unittests/**/*.ts', type: 'module'},
+      {pattern: 'front_end/**/*.svg', included: false, served: true}, {pattern: 'front_end/**/*.png', included: false, served: true},
     ],
 
     // FIXME(https://crbug.com/1006759): Re-enable these tests when ESM work is completed.
@@ -58,6 +59,7 @@ module.exports = function(config) {
 
     proxies: {
       '/front_end': '/base/front_end',
+      '/Images': '/base/front_end/Images',
     },
 
     plugins: [

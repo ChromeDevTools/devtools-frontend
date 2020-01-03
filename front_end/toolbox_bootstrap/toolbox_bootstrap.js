3f265dee6dbffb60e35c95953c68183a72197490
Tim van der Lippe <tvanderlippe@chromium.org>
2020-01-03 16:06:35 +0100

    Migrate toolbox_bootstrap/ to ESM
    Bug: 1006759
    Change-Id: I75c67c829c527b76eba1a0910d6a72843113aa04
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/1985664
    Commit-Queue: Tim van der Lippe <tvanderlippe@chromium.org>
    Reviewed-by: Paul Lewis <aerotwist@chromium.org>

---
+++ front_end/toolbox_bootstrap/toolbox_bootstrap.js
@@ -0,0 +1,16 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @suppressGlobalPropertiesCheck
+ */
+function toolboxLoaded() {
+  if (!window.opener) {
+    return;
+  }
+  const app = window.opener['Emulation']['AdvancedApp']['_instance']();
+  app['toolboxLoaded'](document);
+}
+
+runOnWindowLoad(toolboxLoaded);

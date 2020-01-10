4784784e268d48f7390484b7a9f43cedfc09944b
Sigurd Schneider <sigurds@chromium.org>
2020-01-10 14:05:50 +0100

    [coverage] Refactor backlogging in the CoverageModel
    Bug: chromium:1022031
    Change-Id: I1c137387d2f44d63d3e8a071c278a8f98eafd74f
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/1991510
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Commit-Queue: Sigurd Schneider <sigurds@chromium.org>

--- front_end/coverage/CoverageModel.js
+++ front_end/coverage/CoverageModel.js
@@ -281,22 +281,21 @@ export class CoverageModel extends SDK.SDKModel {
     }
     const now = Date.now();
     const freshRawCoverageData = await this._cpuProfilerModel.takePreciseCoverage();
-    if (this._suspensionState !== SuspensionState.Active) {
-      if (freshRawCoverageData.length > 0) {
-        this._jsBacklog.push({rawCoverageData: freshRawCoverageData, stamp: now});
-      }
+    return this._backlogOrProcessJSCoverage(freshRawCoverageData, now);
+  }
 
+  async _backlogOrProcessJSCoverage(freshRawCoverageData, freshTimestamp) {
+    if (freshRawCoverageData.length > 0) {
+      this._jsBacklog.push({rawCoverageData: freshRawCoverageData, stamp: freshTimestamp});
+    }
+    if (this._suspensionState !== SuspensionState.Active) {
       return [];
     }
     const results = [];
     for (const {rawCoverageData, stamp} of this._jsBacklog) {
       results.push(this._processJSCoverage(rawCoverageData, stamp));
     }
-
     this._jsBacklog = [];
-    if (freshRawCoverageData.length > 0) {
-      results.push(this._processJSCoverage(freshRawCoverageData, now));
-    }
     return results.flat();
   }
 

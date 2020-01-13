383de766af7a096a009df39b926575b620a63d1f
Tim van der Lippe <tvanderlippe@chromium.org>
2020-01-13 16:53:06 +0000

    Fix check for nested directories in check_gn
    Any module.json that was listed in a nested folder (for example in
    third_party/) was not properly being checked in the check_gn script. We
    should traverse not only the direct directories of `front_end/`, but
    also recurse into any of its subdirectories.
    
    Bug: 1041415
    Change-Id: I6c4adbf4315c30b3d90361329d726323020308a7
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/1997352
    Auto-Submit: Tim van der Lippe <tvanderlippe@chromium.org>
    Commit-Queue: Eric Leese <leese@chromium.org>
    Reviewed-by: Eric Leese <leese@chromium.org>

--- scripts/check_gn.js
+++ scripts/check_gn.js
@@ -96,8 +96,8 @@ function checkCopiedDevToolsModules() {
       (moduleJSON) => {
         return moduleJSON.modules || [];
       },
-      (moduleName) => (filename) => {
-        const relativePath = path.normalize(`$resources_out_dir/${moduleName}/${filename}`);
+      (buildGNPath) => (filename) => {
+        const relativePath = path.normalize(`$resources_out_dir/${buildGNPath}/${filename}`);
         return `"${relativePath}",`;
       });
 }
@@ -116,25 +116,44 @@ function checkGNVariable(gnVariable, obtainFiles, obtainRelativePath) {
   }
   const gnFiles = new Set(lines);
   var moduleFiles = [];
-  fs.readdirSync(FRONTEND_PATH).forEach(function(moduleName) {
-    const moduleJSONPath = path.join(FRONTEND_PATH, moduleName, 'module.json');
-    if (utils.isFile(moduleJSONPath)) {
-      const moduleJSON = require(moduleJSONPath);
-      const files = obtainFiles(moduleJSON)
-                        .map(obtainRelativePath && obtainRelativePath(moduleName) || relativePathFromBuildGN)
-                        .filter(file => excludedFiles.every(excludedFile => !file.includes(excludedFile)));
-      moduleFiles = moduleFiles.concat(files);
 
-      function relativePathFromBuildGN(filename) {
-        const relativePath = path.normalize(`front_end/${moduleName}/${filename}`);
-        return `"${relativePath}",`;
-      }
+  function addModuleFilesForDirectory(moduleJSONPath, buildGNPath) {
+    const moduleJSON = require(moduleJSONPath);
+    const files = obtainFiles(moduleJSON)
+                      .map(obtainRelativePath && obtainRelativePath(buildGNPath) || relativePathFromBuildGN)
+                      .filter(file => excludedFiles.every(excludedFile => !file.includes(excludedFile)));
+    moduleFiles = moduleFiles.concat(files);
+
+    function relativePathFromBuildGN(filename) {
+      const relativePath = path.normalize(`front_end/${buildGNPath}/${filename}`);
+      return `"${relativePath}",`;
     }
+  }
+
+  function traverseDirectoriesForModuleJSONFiles(folderName, buildGNPath) {
+    if (!fs.lstatSync(folderName).isDirectory()) {
+      return;
+    }
+    const moduleJSONPath = path.join(folderName, 'module.json');
+    if (utils.isFile(moduleJSONPath)) {
+      addModuleFilesForDirectory(moduleJSONPath, buildGNPath);
+    }
+
+    fs.readdirSync(folderName).forEach((nestedModuleName) => {
+      traverseDirectoriesForModuleJSONFiles(
+          path.join(folderName, nestedModuleName), `${buildGNPath}/${nestedModuleName}`);
+    });
+  }
+
+  fs.readdirSync(FRONTEND_PATH).forEach((moduleName) => {
+    traverseDirectoriesForModuleJSONFiles(path.join(FRONTEND_PATH, moduleName), moduleName);
   });
+
   for (const file of moduleFiles) {
     if (!gnFiles.has(file))
       errors.push(`Missing file in BUILD.gn for ${gnVariable}: ` + file);
   }
+
   return errors;
 }
 

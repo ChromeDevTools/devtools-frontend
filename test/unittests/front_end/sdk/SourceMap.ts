395b69e99301f102f90dc01938cee016c53a074c
Simon Zünd <szuend@chromium.org>
2020-01-14 12:37:22 +0100

    [fix-it] Add unit test for TextSourceMap.StringCharIterator
    R=aerotwist@chromium.org
    
    Bug: chromium:1032561
    Change-Id: Ia7ca19bf71b628fcf97b3282b522566efc7db2f1
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/1998767
    Reviewed-by: Paul Lewis <aerotwist@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>

--- test/unittests/front_end/sdk/SourceMap.ts
+++ test/unittests/front_end/sdk/SourceMap.ts
@@ -4,7 +4,7 @@
 
 const {assert} = chai;
 
-import {SourceMapEntry} from '/front_end/sdk/SourceMap.js';
+import {SourceMapEntry,TextSourceMap} from '/front_end/sdk/SourceMap.js';
 
 describe('SourceMapEntry', () => {
   it('can be instantiated correctly', () => {
@@ -36,6 +36,34 @@ describe('SourceMapEntry', () => {
       const sourceMapEntry1 = new SourceMapEntry(2, 5, '<foo>', 1, 5, 'foo');
       const sourceMapEntry2 = new SourceMapEntry(2, 5, '<foo>', 1, 5, 'foo');
       assert.equal(SourceMapEntry.compare(sourceMapEntry1, sourceMapEntry2), 0);
+    });
+  });
+});
+
+describe('TextSourceMap', () => {
+  describe('StringCharIterator', () => {
+    it('detects when it has reached the end', () => {
+      const empty_iterator = new TextSourceMap.StringCharIterator('');
+      assert.isFalse(empty_iterator.hasNext());
+
+      const iterator = new TextSourceMap.StringCharIterator('foo');
+      assert.isTrue(iterator.hasNext());
+    });
+
+    it('peeks the next character', () => {
+      const empty_iterator = new TextSourceMap.StringCharIterator('');
+      assert.equal(empty_iterator.peek(), '');
+
+      const iterator = new TextSourceMap.StringCharIterator('foo');
+      assert.equal(iterator.peek(), 'f');
+    });
+
+    it('advances when {next} is called', () => {
+      const iterator = new TextSourceMap.StringCharIterator('bar');
+      assert.equal(iterator.next(), 'b');
+      assert.equal(iterator.next(), 'a');
+      assert.equal(iterator.next(), 'r');
+      assert.isFalse(iterator.hasNext());
     });
   });
 });

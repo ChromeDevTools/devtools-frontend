12644229b2eec5b337e49548cc7b0855199ff954
Paul Lewis <aerotwist@chromium.org>
2020-01-14 09:36:12 +0000

    Removes comment from Karma config file
    TBR=tvanderlippe@chromium.org
    Change-Id: I216ffc31ac7333672cb01cb94dcaf2a1854f943a
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/1998759
    Reviewed-by: Paul Lewis <aerotwist@chromium.org>
    Commit-Queue: Paul Lewis <aerotwist@chromium.org>

--- karma.conf.js
+++ karma.conf.js
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// TODO(1011259): switch to true.
 let external_devtools_frontend = true;
 let node_modules_path = external_devtools_frontend
     ? ''

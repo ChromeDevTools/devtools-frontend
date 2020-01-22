a7de1bbca072ff9d5a46d17ad5c71c01d1ed2288
Paul Lewis <aerotwist@chromium.org>
2020-01-22 07:56:40 +0000

    Revert "Fix typescript compilation with ts_library"
    This reverts commit 85de0bc72ef7929abfa352bd7a0efa016cdf5fbb.
    
    Reason for revert: From Chase: build error after this change rolled into Chromium at https://crrev.com/c/2008257
    
    DISABLE_THIRD_PARTY_CHECK=Bypassing checks for revert
    
    Original change's description:
    > Fix typescript compilation with ts_library
    >
    > This CL prepares ts_library to work correctly by building both in Chromium
    > as well as in DevTools. A follow-up CL will add a simple ts_library
    > to verify that it keeps on working.
    >
    > Bug: 1011811
    > Change-Id: I482761bfbcbf459478dc7f51f2df6576ac872de6
    > DISABLE_THIRD_PARTY_CHECK=TypeScript build configuration changes
    > Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/1960456
    > Reviewed-by: Paul Lewis <aerotwist@chromium.org>
    > Commit-Queue: Tim van der Lippe <tvanderlippe@chromium.org>
    
    TBR=aerotwist@chromium.org,mathias@chromium.org,tvanderlippe@chromium.org
    
    
    Bug: 1011811
    Change-Id: Id354882f8f8706241ac377e62ff68cc9eb6ff994
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2013141
    Reviewed-by: Paul Lewis <aerotwist@chromium.org>
    Commit-Queue: Paul Lewis <aerotwist@chromium.org>

--- third_party/typescript/ts_library.py
+++ third_party/typescript/ts_library.py
@@ -11,21 +11,11 @@ from os import path
 _CURRENT_DIR = path.join(path.dirname(__file__))
 TSC_LOCATION = path.join(_CURRENT_DIR, '..', '..', 'node_modules', 'typescript', 'bin', 'tsc')
 
-try:
-    old_sys_path = sys.path[:]
-    sys.path.append(path.join(_CURRENT_DIR, '..', '..', 'scripts'))
-    import devtools_paths
-finally:
-    sys.path = old_sys_path
-NODE_LOCATION = devtools_paths.node_path()
-
-ROOT_TS_CONFIG_LOCATION = path.join(_CURRENT_DIR, '..', '..', 'tsconfig.json')
+ROOT_TS_CONFIG_LOCATION = path.join(_CURRENT_DIR, 'tsconfig.json')
 
 
 def runTsc(tsconfig_location):
-    process = subprocess.Popen([NODE_LOCATION, TSC_LOCATION, '-b', tsconfig_location],
-                               stdout=subprocess.PIPE,
-                               stderr=subprocess.PIPE)
+    process = subprocess.Popen([TSC_LOCATION, '-b', tsconfig_location], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
     # TypeScript does not correctly write to stderr because of https://github.com/microsoft/TypeScript/issues/33849
     return process.returncode, stdout + stderr
@@ -34,7 +24,7 @@ def runTsc(tsconfig_location):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', '--sources', nargs='*', required=True, help='List of TypeScript source files')
-    parser.add_argument('-b', '--tsconfig_output_location', required=True)
+    parser.add_argument('-b', '--tsconfig_location', required=True)
     opts = parser.parse_args()
     with open(ROOT_TS_CONFIG_LOCATION) as root_tsconfig:
         try:
@@ -43,7 +33,7 @@ def main():
             print('Encountered error while loading root tsconfig:')
             print(e)
             return 1
-    tsconfig_output_location = path.join(os.getcwd(), opts.tsconfig_output_location)
+    tsconfig_output_location = path.join(os.getcwd(), opts.tsconfig_location)
     tsconfig['files'] = [path.join(os.getcwd(), src) for src in opts.sources]
     tsconfig['compilerOptions']['outDir'] = path.dirname(tsconfig_output_location)
     with open(tsconfig_output_location, 'w') as generated_tsconfig:

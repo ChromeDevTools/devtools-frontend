2b54391d72e260a675c2327ec38b48a48d30ecef
Tim van der Lippe <tvanderlippe@chromium.org>
2020-01-24 14:54:42 +0000

    Migrate worker_main/ to import cross-module
    Bug: 1006759
    Change-Id: I3b35181af88d569b2d7f803150b9bf147d037510
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2019554
    Reviewed-by: Paul Lewis <aerotwist@chromium.org>
    Commit-Queue: Tim van der Lippe <tvanderlippe@chromium.org>

--- front_end/worker_main/WorkerMain.js
+++ front_end/worker_main/WorkerMain.js
@@ -2,27 +2,32 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import * as Common from '../common/common.js';
+import * as Components from '../components/components.js';
+import * as MobileThrottling from '../mobile_throttling/mobile_throttling.js';
+import * as SDK from '../sdk/sdk.js';
+
 /**
- * @implements {Common.Runnable}
+ * @implements {Common.Runnable.Runnable}
  */
-export class WorkerMainImpl extends Common.Object {
+export class WorkerMainImpl extends Common.ObjectWrapper.ObjectWrapper {
   /**
    * @override
    */
   run() {
-    SDK.initMainConnection(() => {
-      self.SDK.targetManager.createTarget('main', ls`Main`, SDK.Target.Type.ServiceWorker, null);
-    }, Components.TargetDetachedDialog.webSocketConnectionLost);
-    new MobileThrottling.NetworkPanelIndicator();
+    SDK.Connections.initMainConnection(() => {
+      self.SDK.targetManager.createTarget('main', ls`Main`, SDK.SDKModel.Type.ServiceWorker, null);
+    }, Components.TargetDetachedDialog.TargetDetachedDialog.webSocketConnectionLost);
+    new MobileThrottling.NetworkPanelIndicator.NetworkPanelIndicator();
   }
 }
 
-SDK.ChildTargetManager.install(async ({target, waitingForDebugger}) => {
+SDK.ChildTargetManager.ChildTargetManager.install(async ({target, waitingForDebugger}) => {
   // Only pause the new worker if debugging SW - we are going through the pause on start checkbox.
-  if (target.parentTarget() || target.type() !== SDK.Target.Type.ServiceWorker || !waitingForDebugger) {
+  if (target.parentTarget() || target.type() !== SDK.SDKModel.Type.ServiceWorker || !waitingForDebugger) {
     return;
   }
-  const debuggerModel = target.model(SDK.DebuggerModel);
+  const debuggerModel = target.model(SDK.DebuggerModel.DebuggerModel);
   if (!debuggerModel) {
     return;
   }

8f85fb5d24e23eab5201f98c9b1d0498962a7a26
Tim van der Lippe <tvanderlippe@chromium.org>
2020-01-24 12:13:48 +0000

    Migrate javascript_metadata/ to import cross-module
    Bug: 1006759
    Change-Id: Iac659b770be05ee082162fe671302afab263a7ee
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2019126
    Reviewed-by: Paul Lewis <aerotwist@chromium.org>
    Commit-Queue: Tim van der Lippe <tvanderlippe@chromium.org>

--- front_end/javascript_metadata/JavaScriptMetadata.js
+++ front_end/javascript_metadata/JavaScriptMetadata.js
@@ -2,10 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import * as Common from '../common/common.js';  // eslint-disable-line no-unused-vars
+
 import {NativeFunctions} from './NativeFunctions.js';
 
 /**
- * @implements {Common.JavaScriptMetadata}
+ * @implements {Common.JavaScriptMetaData.JavaScriptMetaData}
  */
 export class JavaScriptMetadataImpl {
   constructor() {

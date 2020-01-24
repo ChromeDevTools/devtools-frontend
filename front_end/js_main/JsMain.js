8e16373548108c7a45c2ee07bff2575fd402549e
Tim van der Lippe <tvanderlippe@chromium.org>
2020-01-24 16:24:10 +0000

    Migrate js_main/ to import cross-module
    Bug: 1006759
    Change-Id: I4e4678e974809ee9849aa24caa889b8872fad163
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2019863
    Reviewed-by: Paul Lewis <aerotwist@chromium.org>
    Commit-Queue: Tim van der Lippe <tvanderlippe@chromium.org>

--- front_end/js_main/JsMain.js
+++ front_end/js_main/JsMain.js
@@ -2,18 +2,23 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import * as Common from '../common/common.js';
+import * as Components from '../components/components.js';
+import * as Host from '../host/host.js';
+import * as SDK from '../sdk/sdk.js';
+
 /**
- * @implements {Common.Runnable}
+ * @implements {Common.Runnable.Runnable}
  */
-export class JsMainImpl extends Common.Object {
+export class JsMainImpl extends Common.ObjectWrapper.ObjectWrapper {
   /**
    * @override
    */
   run() {
     Host.userMetrics.actionTaken(Host.UserMetrics.Action.ConnectToNodeJSDirectly);
-    SDK.initMainConnection(() => {
-      const target = self.SDK.targetManager.createTarget('main', ls`Main`, SDK.Target.Type.Node, null);
+    SDK.Connections.initMainConnection(() => {
+      const target = self.SDK.targetManager.createTarget('main', ls`Main`, SDK.SDKModel.Type.Node, null);
       target.runtimeAgent().runIfWaitingForDebugger();
-    }, Components.TargetDetachedDialog.webSocketConnectionLost);
+    }, Components.TargetDetachedDialog.TargetDetachedDialog.webSocketConnectionLost);
   }
 }

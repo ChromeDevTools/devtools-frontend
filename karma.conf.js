65d9ee28e55dc9a586366aee56f7412991877d11
Almothana Athamneh <almuthanna@chromium.org>
2020-01-29 15:29:05 +0100

    Add JSON Summary coverage report
    This CL adds the JSON Summary coverage
    report when running the Karma unit tests
    so it can then later be used with
    Istanbul-diff.
    
    Change-Id: I4d1ad935f7f1f59cf703a8e51752c9b910877804
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2027441
    Reviewed-by: Liviu Rau <liviurau@chromium.org>
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>
    Commit-Queue: Almothana Athamneh <almuthanna@chromium.org>

--- karma.conf.js
+++ karma.conf.js
@@ -77,7 +77,7 @@ module.exports = function(config) {
 
     coverageIstanbulInstrumenter: {esModules: true},
 
-    coverageIstanbulReporter: {reports: NO_TEXT_COVERAGE ? ['html'] : ['text', 'html'], dir: 'karma-coverage'},
+    coverageIstanbulReporter: {reports: NO_TEXT_COVERAGE ? ['html', 'json-summary'] : ['text', 'html', 'json-summary'], dir: 'karma-coverage'},
 
     singleRun: !IS_DEBUG
   };

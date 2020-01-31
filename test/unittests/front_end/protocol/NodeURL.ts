023284c1ce323b8a2d22ffa153432366b4eae273
Simon Zünd <szuend@chromium.org>
2020-01-31 13:42:15 +0100

    Port NodeURL unit test from web test to karma
    This CL ports the NodeURL web test to Karma. Once this CL lands,
    we can remove the web test upstream.
    
    The old web test can be found here:
    https://source.chromium.org/chromium/chromium/src/+/master:third_party/blink/web_tests/http/tests/devtools/unit/node-url.js;l=1;drc=77578ccb4082ae20a9326d9e673225f1189ebb63?originalUrl=https:%2F%2Fcs.chromium.org%2F
    
    Bug: chromium:1032561
    Change-Id: I2a71a046691609a6a8b3b07ed8371fa05ef76dde
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2030776
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>

---
+++ test/unittests/front_end/protocol/NodeURL.ts
@@ -0,0 +1,73 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+const {assert} = chai;
+
+import {NodeURL} from '/front_end/protocol/NodeURL.js';
+import {Platform} from '/front_end/host/host.js';
+
+describe('NodeURL', () => {
+  describe('platform detection for paths', () => {
+    it('works correctly on windows', () => {
+      const isWindows = true;
+      assert.isTrue(NodeURL._isPlatformPath('c:\\prog\\foobar.js', isWindows));
+      assert.isFalse(NodeURL._isPlatformPath('/usr/local/foobar.js', isWindows));
+    });
+
+    it('works correctly on linux', () => {
+      const isWindows = false;
+      assert.isFalse(NodeURL._isPlatformPath('c:\\prog\\foobar.js', isWindows));
+      assert.isTrue(NodeURL._isPlatformPath('/usr/local/foobar.js', isWindows));
+    });
+  });
+
+  describe('patch', () => {
+    const url = Platform.isWin() ? 'c:\\prog\\foobar.js' : '/usr/local/home/prog/foobar.js';
+    const patchedUrl = Platform.isWin() ? 'file:///c:\\prog\\foobar.js' : 'file:///usr/local/home/prog/foobar.js';
+
+    it('does patch url fields', () => {
+      const object = {url};
+
+      NodeURL.patch(object);
+
+      assert.strictEqual(object.url, patchedUrl);
+    });
+
+    it('does not patch the url of the result', () => {
+      const object = {
+        result: {
+          result: {
+            value: {url},
+          },
+        },
+      };
+
+      NodeURL.patch(object);
+
+      assert.strictEqual(object.result.result.value.url, url);
+    });
+
+    it('does patch all urls in an example protocol message', () => {
+      const object = {
+        exceptionDetails: {
+          url,
+          stackTrace: {
+            callFrames: [{
+              columnNumber: 0,
+              functionName: '',
+              lineNumber: 0,
+              scriptId: '0',
+              url,
+            }],
+          },
+        },
+      };
+
+      NodeURL.patch(object);
+
+      assert.strictEqual(object.exceptionDetails.url, patchedUrl);
+      assert.strictEqual(object.exceptionDetails.stackTrace.callFrames[0].url, patchedUrl);
+    });
+  });
+});

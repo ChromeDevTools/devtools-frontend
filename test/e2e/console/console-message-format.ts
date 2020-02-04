00a610e6768c746ae03bb0867c81104a662cfc11
Tim van der Lippe <tvanderlippe@chromium.org>
2020-02-04 15:39:03 +0000

    Add console uncaught promises e2etest
    This replaces
    https://source.chromium.org/chromium/chromium/src/+/master:third_party/blink/web_tests/http/tests/devtools/startup/console/resources/console-uncaught-promise-no-inspector.html;drc=77578ccb4082ae20a9326d9e673225f1189ebb63?originalUrl=https:%2F%2Fcs.chromium.org%2F
    as an e2etest
    
    Bug: 1047671
    Change-Id: Ie24e4eab5f08fd5bf9bb2c390a3f9ef6ec600e9f
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2035948
    Commit-Queue: Tim van der Lippe <tvanderlippe@chromium.org>
    Reviewed-by: Paul Lewis <aerotwist@chromium.org>

--- test/e2e/console/console-message-format.ts
+++ test/e2e/console/console-message-format.ts
@@ -46,4 +46,41 @@ describe('The Console Tab', async () => {
       'null 1n BigInt\xA0{2n}',
     ])
   });
+
+  it('shows uncaught promises', async () => {
+    const {target, frontend} = getBrowserAndPages();
+
+    // Have the target load the page.
+    await target.goto(`${resourcesPath}/console/uncaught-promise.html`);
+
+    // Locate the button for switching to the console tab.
+    const consoleTabButtonLocation = await getElementPosition('#tab-console');
+    if (!consoleTabButtonLocation) {
+      assert.fail('Unable to locate console tab button.');
+    }
+
+    // Click on the button and wait for the console to load. The reason we use this method
+    // rather than elementHandle.click() is because the frontend attaches the behavior to
+    // a 'mousedown' event (not the 'click' event). To avoid attaching the test behavior
+    // to a specific event we instead locate the button in question and ask Puppeteer to
+    // click on it instead.
+    await frontend.mouse.click(consoleTabButtonLocation.x, consoleTabButtonLocation.y);
+    await frontend.waitForSelector('.console-group-messages');
+
+    // Get the first message from the console.
+    const messages = await frontend.evaluate(() => {
+      return Array.from(document.querySelectorAll('.console-group-messages .source-code .console-message-text'))
+          .map(message => message.textContent);
+    });
+
+    assert.deepEqual(messages, [
+      `Uncaught (in promise) Error: err1
+    at uncaught-promise.html:10`,
+      `Uncaught (in promise) Error: err2
+    at uncaught-promise.html:28`,
+      `Uncaught (in promise) DOMException: Failed to execute 'removeChild' on 'Node': The node to be removed is not a child of this node.
+    at throwDOMException (http://localhost:8090/test/e2e/resources/console/uncaught-promise.html:43:11)
+    at catcher (http://localhost:8090/test/e2e/resources/console/uncaught-promise.html:36:9)`
+    ]);
+  });
 });

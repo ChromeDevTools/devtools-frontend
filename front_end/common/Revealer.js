6dcacd6cf916ac3d8c16c2e70b78a72ba5e2244e
Jack Franklin <jacktfranklin@chromium.org>
2020-02-26 17:03:06 +0100

    Move `Common/Revealer.js` to TypeScript
    Bug: 1011811
    Change-Id: I936d82974ee41fd929c50d80ae75c3973c9c1eac
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2074766
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>
    Commit-Queue: Jack Franklin <jacktfranklin@chromium.org>

--- front_end/common/Revealer.js
+++ front_end/common/Revealer.js
@@ -9,9 +9,10 @@ export class Revealer {
   /**
    * @param {!Object} object
    * @param {boolean=} omitFocus
-   * @return {!Promise}
+   * @return {!Promise<void>}
    */
   reveal(object, omitFocus) {
+    throw new Error('not implemented');
   }
 }
 
@@ -24,11 +25,12 @@ export let reveal = function(revealable, omitFocus) {
   if (!revealable) {
     return Promise.reject(new Error('Can\'t reveal ' + revealable));
   }
+  // @ts-ignore self.runtime needs to be moved to ESModules so we can import this
   return self.runtime.allInstances(Revealer, revealable).then(reveal);
 
   /**
    * @param {!Array.<!Revealer>} revealers
-   * @return {!Promise.<undefined>}
+   * @return {!Promise.<void>}
    */
   function reveal(revealers) {
     const promises = [];
@@ -39,6 +41,10 @@ export let reveal = function(revealable, omitFocus) {
   }
 };
 
+/**
+ *
+ * @param {function(?Object, boolean=):!Promise.<undefined>} newReveal
+ */
 export function setRevealForTest(newReveal) {
   reveal = newReveal;
 }
@@ -48,6 +54,7 @@ export function setRevealForTest(newReveal) {
  * @return {?string}
  */
 export const revealDestination = function(revealable) {
+  // @ts-ignore self.runtime needs to be moved to ESModules so we can import this
   const extension = self.runtime.extension(Revealer, revealable);
   if (!extension) {
     return null;

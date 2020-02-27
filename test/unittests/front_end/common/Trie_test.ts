42c3df1e2d5aafb066fdbc7f398415a5dff1a34c
Jack Franklin <jacktfranklin@chromium.org>
2020-02-27 17:58:45 +0100

    Move Trie test to being built with Ninja
    Bug:  1011811
    Change-Id: If5926c20f1847447261ad524afba37d91106e243
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2078854
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>
    Commit-Queue: Jack Franklin <jacktfranklin@chromium.org>

--- test/unittests/front_end/common/Trie_test.ts
+++ test/unittests/front_end/common/Trie_test.ts
@@ -2,14 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-const { assert } = chai;
+const {assert} = chai;
 
-import * as Common from '/front_end/common/common.js';
+import {Trie} from '../../../../front_end/common/Trie.js';
 
 describe('Trie', () => {
-  let trie: Common.Trie.Trie;
+  let trie: Trie;
   beforeEach(() => {
-    trie = new Common.Trie.Trie();
+    trie = new Trie();
   });
 
   it('stores and retrieves words', () => {

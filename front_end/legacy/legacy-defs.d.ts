683c198380dbee96fad08e689d30cb9911a4712c
Jose Leal Chapa <joselea@microsoft.com>
2020-03-03 10:16:11 -0800

    Typecheck SegmentedRange.js
    Bug: 1011811
    Change-Id: I1fb342a5be9432253eb3a503407f2d9ca6c34806
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2084945
    Commit-Queue: Jose Leal <joselea@microsoft.com>
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>

--- front_end/legacy/legacy-defs.d.ts
+++ front_end/legacy/legacy-defs.d.ts
@@ -3,9 +3,12 @@ interface StringConstructor {
   hashCode(id: string): number;
 }
 
-interface Array<T>{peekLast(): T | undefined}
+interface Array<T> {
+  peekLast(): T | undefined;
+  lowerBound(object: T, comparator: {(a:T, b:T):number}): number;
+}
 
-// Type alias for the Closure-supported ItemplateArray which is equivalent
+// Type alias for the Closure-supported ITemplateArray which is equivalent
 // to TemplateStringsArray in TypeScript land
 type ITemplateArray = TemplateStringsArray
 

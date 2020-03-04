958f0b7690948e916d13e47e7548535a9b41dee0
Simon Zünd <szuend@chromium.org>
2020-03-04 06:36:10 +0100

    Type check common/Linkifier.js using TypeScript
    R=tvanderlippe@chromium.org
    
    Change-Id: I2672adf46f3fc8ccac3b908063547a4e8220c629
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2086093
    Auto-Submit: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Tim van der Lippe <tvanderlippe@chromium.org>
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>

--- front_end/common/Linkifier.js
+++ front_end/common/Linkifier.js
@@ -12,6 +12,7 @@ export class Linkifier {
    * @return {!Node}
    */
   linkify(object, options) {
+    throw new Error('linkify not implemented');
   }
 
   /**
@@ -23,9 +24,11 @@ export class Linkifier {
     if (!object) {
       return Promise.reject(new Error('Can\'t linkify ' + object));
     }
+    // @ts-ignore self.runtime needs to be moved to ESModules so we can import this.
     return self.runtime.extension(Linkifier, object).instance().then(linkifier => linkifier.linkify(object, options));
   }
 }
 
 /** @typedef {{tooltip: (string|undefined), preventKeyboardFocus: (boolean|undefined)}} */
+// @ts-ignore typedef.
 export let Options;

4eb885a01e2cb08f764e68bd71dec0663504370e
Gabriel Luong <gabriel.luong@gmail.com>
2020-03-04 10:59:35 -0500

    Migrate Console and SegmentedRange tests into Ninja build
    Bug: 1011811
    Change-Id: I6ca4c24af50640c65725edc02b83038e68d1795f
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2086704
    Commit-Queue: Tim van der Lippe <tvanderlippe@chromium.org>
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>

--- test/unittests/front_end/common/Console_test.ts
+++ test/unittests/front_end/common/Console_test.ts
@@ -4,30 +4,30 @@
 
 const { assert } = chai;
 
-import * as Common from '/front_end/common/common.js';
+import {Console, Events, MessageLevel} from '../../../../front_end/common/Console.js';
 
 describe('Console', () => {
-  let consoleImpl: Common.Console.Console;
+  let consoleImpl: Console;
   beforeEach(() => {
-    consoleImpl = Common.Console.Console.instance({forceNew: true});
+    consoleImpl = Console.instance({forceNew: true});
   });
 
   it('adds messages', () => {
-    consoleImpl.addMessage('Foo', Common.Console.MessageLevel.Info, true);
+    consoleImpl.addMessage('Foo', MessageLevel.Info, true);
     const messages = consoleImpl.messages();
     assert.equal(messages.length, 1);
     assert.equal(messages[0].text, 'Foo');
   });
 
   it('adds handles messages of all types', () => {
-    const messageTypes = new Map<Common.Console.MessageLevel, string>([
+    const messageTypes = new Map<MessageLevel, string>([
       ['Info', 'log'],
       ['Warning', 'warn'],
       ['Error', 'error'],
     ]);
 
     for (const [type, method] of messageTypes) {
-      consoleImpl = Common.Console.Console.instance({forceNew: true});
+      consoleImpl = Console.instance({forceNew: true});
 
       // Dispatch the message of the appropriate type.
       // @ts-ignore
@@ -38,27 +38,27 @@ describe('Console', () => {
       assert.equal(messages.length, 1);
       assert.equal(messages[0].text, type);
       // @ts-ignore
-      assert.equal(messages[0].level, Common.Console.MessageLevel[type]);
+      assert.equal(messages[0].level, MessageLevel[type]);
     }
   });
 
   it('stores messages', () => {
-    consoleImpl.addMessage('Foo', Common.Console.MessageLevel.Info, true);
-    consoleImpl.addMessage('Baz', Common.Console.MessageLevel.Warning, true);
-    consoleImpl.addMessage('Bar', Common.Console.MessageLevel.Error, true);
-    consoleImpl.addMessage('Donkey', Common.Console.MessageLevel.Info, true);
+    consoleImpl.addMessage('Foo', MessageLevel.Info, true);
+    consoleImpl.addMessage('Baz', MessageLevel.Warning, true);
+    consoleImpl.addMessage('Bar', MessageLevel.Error, true);
+    consoleImpl.addMessage('Donkey', MessageLevel.Info, true);
     const messages = consoleImpl.messages();
     assert.equal(messages.length, 4);
   });
 
   it('dispatches events to listeners', done => {
     const callback = ({data}: {data: {text: string}}) => {
-      consoleImpl.removeEventListener(Common.Console.Events.MessageAdded, callback);
+      consoleImpl.removeEventListener(Events.MessageAdded, callback);
       assert.equal(data.text, 'Foo');
       done();
     };
 
-    consoleImpl.addEventListener(Common.Console.Events.MessageAdded, callback);
-    consoleImpl.addMessage('Foo', Common.Console.MessageLevel.Info, true);
+    consoleImpl.addEventListener(Events.MessageAdded, callback);
+    consoleImpl.addMessage('Foo', MessageLevel.Info, true);
   });
 });
--- test/unittests/front_end/common/SegmentedRange_test.ts
+++ test/unittests/front_end/common/SegmentedRange_test.ts
@@ -4,46 +4,46 @@
 
 const { assert } = chai;
 
-import * as Common from '/front_end/common/common.js';
+import {Segment, SegmentedRange} from '../../../../front_end/common/SegmentedRange.js';
 
 describe('Segment', () => {
   it('calculates intersections', () => {
-    const segmentA = new Common.SegmentedRange.Segment(1, 2, 'A');
-    const segmentB = new Common.SegmentedRange.Segment(1.5, 2.5, 'B');
-    const segmentC = new Common.SegmentedRange.Segment(3, 5, 'C');
+    const segmentA = new Segment(1, 2, 'A');
+    const segmentB = new Segment(1.5, 2.5, 'B');
+    const segmentC = new Segment(3, 5, 'C');
 
     assert.isTrue(segmentA.intersects(segmentB));
     assert.isFalse(segmentA.intersects(segmentC));
   });
 
   it('throws for invalid segments', () => {
-    assert.throws(() => new Common.SegmentedRange.Segment(3, 2, 'V'));
+    assert.throws(() => new Segment(3, 2, 'V'));
   });
 });
 
 describe('SegmentedRange', () => {
-  let segmentedRange: Common.SegmentedRange.SegmentedRange;
+  let segmentedRange: SegmentedRange;
 
-  function mergeSegments(first: Common.SegmentedRange.Segment, second: Common.SegmentedRange.Segment) {
+  function mergeSegments(first: Segment, second: Segment) {
     const inOrder = first.end >= second.begin;
     const matchingData = first.data === second.data;
     return inOrder && matchingData ? first : null;
   }
 
   beforeEach(() => {
-    segmentedRange = new Common.SegmentedRange.SegmentedRange(mergeSegments);
+    segmentedRange = new SegmentedRange(mergeSegments);
   });
 
   it('handles single ranges', () => {
-    const segment = new Common.SegmentedRange.Segment(0, 1, 'A');
+    const segment = new Segment(0, 1, 'A');
     segmentedRange.append(segment);
 
     assert.deepEqual(segmentedRange.segments(), [segment]);
   });
 
   it('handles two adjacent ranges', () => {
-    const segmentA = new Common.SegmentedRange.Segment(1, 2, 'A');
-    const segmentB = new Common.SegmentedRange.Segment(2, 3, 'B');
+    const segmentA = new Segment(1, 2, 'A');
+    const segmentB = new Segment(2, 3, 'B');
 
     segmentedRange.append(segmentA);
     segmentedRange.append(segmentB);
@@ -52,125 +52,125 @@ describe('SegmentedRange', () => {
   });
 
   it('handles two overlapping mergeable ranges', () => {
-    const segmentA = new Common.SegmentedRange.Segment(1, 2, 'A');
-    const segmentB = new Common.SegmentedRange.Segment(1.5, 3, 'A');
+    const segmentA = new Segment(1, 2, 'A');
+    const segmentB = new Segment(1.5, 3, 'A');
 
 
     segmentedRange.append(segmentA);
     segmentedRange.append(segmentB);
 
-    const expectedSegment = new Common.SegmentedRange.Segment(1, 3, 'A');
+    const expectedSegment = new Segment(1, 3, 'A');
     assert.deepEqual(segmentedRange.segments(), [expectedSegment]);
   });
 
   it('handles multiple overlapping mergeable ranges', () => {
-    const segmentA = new Common.SegmentedRange.Segment(1, 2, 'A');
-    const segmentB = new Common.SegmentedRange.Segment(3, 5, 'A');
-    const segmentC = new Common.SegmentedRange.Segment(1.5, 3.5, 'A');
+    const segmentA = new Segment(1, 2, 'A');
+    const segmentB = new Segment(3, 5, 'A');
+    const segmentC = new Segment(1.5, 3.5, 'A');
 
     segmentedRange.append(segmentA);
     segmentedRange.append(segmentB);
     segmentedRange.append(segmentC);
 
-    const expectedSegment = new Common.SegmentedRange.Segment(1, 5, 'A');
+    const expectedSegment = new Segment(1, 5, 'A');
     assert.deepEqual(segmentedRange.segments(), [expectedSegment]);
   });
 
   it('handles multiple overlapping non-mergeable ranges', () => {
-    const segmentA = new Common.SegmentedRange.Segment(1, 2, 'A');
-    const segmentB = new Common.SegmentedRange.Segment(3, 5, 'A');
-    const segmentC = new Common.SegmentedRange.Segment(1.5, 3.5, 'B');
+    const segmentA = new Segment(1, 2, 'A');
+    const segmentB = new Segment(3, 5, 'A');
+    const segmentC = new Segment(1.5, 3.5, 'B');
 
     segmentedRange.append(segmentA);
     segmentedRange.append(segmentB);
     segmentedRange.append(segmentC);
 
     const expectedSegments = [
-      new Common.SegmentedRange.Segment(1, 1.5, 'A'),
-      new Common.SegmentedRange.Segment(1.5, 3.5, 'B'),
-      new Common.SegmentedRange.Segment(3.5, 5, 'A'),
+      new Segment(1, 1.5, 'A'),
+      new Segment(1.5, 3.5, 'B'),
+      new Segment(3.5, 5, 'A'),
     ];
     assert.deepEqual(segmentedRange.segments(), expectedSegments);
   });
 
   it('handles two overlapping non-mergeable ranges', () => {
-    const segmentA = new Common.SegmentedRange.Segment(1, 2, 'A');
-    const segmentB = new Common.SegmentedRange.Segment(1.5, 3, 'B');
+    const segmentA = new Segment(1, 2, 'A');
+    const segmentB = new Segment(1.5, 3, 'B');
 
     segmentedRange.append(segmentA);
     segmentedRange.append(segmentB);
 
     const expectedSegments = [
-      new Common.SegmentedRange.Segment(1, 1.5, 'A'),
-      new Common.SegmentedRange.Segment(1.5, 3, 'B'),
+      new Segment(1, 1.5, 'A'),
+      new Segment(1.5, 3, 'B'),
     ];
     assert.deepEqual(segmentedRange.segments(), expectedSegments);
   });
 
   it('handles nested, mergeable ranges', () => {
-    const segmentA = new Common.SegmentedRange.Segment(0, 4, 'A');
-    const segmentB = new Common.SegmentedRange.Segment(2, 3, 'A');
+    const segmentA = new Segment(0, 4, 'A');
+    const segmentB = new Segment(2, 3, 'A');
 
     segmentedRange.append(segmentA);
     segmentedRange.append(segmentB);
 
-    const expectedSegment = new Common.SegmentedRange.Segment(0, 4, 'A');
+    const expectedSegment = new Segment(0, 4, 'A');
     assert.deepEqual(segmentedRange.segments(), [expectedSegment]);
   });
 
   it('handles nested, non-mergeable ranges', () => {
-    const segmentA = new Common.SegmentedRange.Segment(0, 4, 'A');
-    const segmentB = new Common.SegmentedRange.Segment(2, 3, 'B');
+    const segmentA = new Segment(0, 4, 'A');
+    const segmentB = new Segment(2, 3, 'B');
 
     segmentedRange.append(segmentA);
     segmentedRange.append(segmentB);
 
     const expectedSegments = [
-      new Common.SegmentedRange.Segment(0, 2, 'A'),
-      new Common.SegmentedRange.Segment(2, 3, 'B'),
-      new Common.SegmentedRange.Segment(3, 4, 'A'),
+      new Segment(0, 2, 'A'),
+      new Segment(2, 3, 'B'),
+      new Segment(3, 4, 'A'),
     ];
     assert.deepEqual(segmentedRange.segments(), expectedSegments);
   });
 
   it('handles out-of-order, mergeable ranges', () => {
-    const segmentA = new Common.SegmentedRange.Segment(0, 2, 'A');
-    const segmentB = new Common.SegmentedRange.Segment(3, 5, 'A');
-    const segmentC = new Common.SegmentedRange.Segment(2, 3, 'A');
+    const segmentA = new Segment(0, 2, 'A');
+    const segmentB = new Segment(3, 5, 'A');
+    const segmentC = new Segment(2, 3, 'A');
 
     segmentedRange.append(segmentA);
     segmentedRange.append(segmentB);
     segmentedRange.append(segmentC);
 
-    const expectedSegment = new Common.SegmentedRange.Segment(0, 5, 'A');
+    const expectedSegment = new Segment(0, 5, 'A');
     assert.deepEqual(segmentedRange.segments(), [expectedSegment]);
   });
 
   it('handles out-of-order, non-mergeable ranges', () => {
-    const segmentA = new Common.SegmentedRange.Segment(0, 2, 'A');
-    const segmentB = new Common.SegmentedRange.Segment(3, 5, 'A');
-    const segmentC = new Common.SegmentedRange.Segment(2, 3, 'B');
+    const segmentA = new Segment(0, 2, 'A');
+    const segmentB = new Segment(3, 5, 'A');
+    const segmentC = new Segment(2, 3, 'B');
 
     segmentedRange.append(segmentA);
     segmentedRange.append(segmentB);
     segmentedRange.append(segmentC);
 
     const expectedSegments = [
-      new Common.SegmentedRange.Segment(0, 2, 'A'),
-      new Common.SegmentedRange.Segment(2, 3, 'B'),
-      new Common.SegmentedRange.Segment(3, 5, 'A'),
+      new Segment(0, 2, 'A'),
+      new Segment(2, 3, 'B'),
+      new Segment(3, 5, 'A'),
     ];
     assert.deepEqual(segmentedRange.segments(), expectedSegments);
   });
 
   it('handles one segment consuming many mergeable ranges', () => {
-    const segmentA = new Common.SegmentedRange.Segment(0, 1, 'A');
-    const segmentB = new Common.SegmentedRange.Segment(2, 3, 'A');
-    const segmentC = new Common.SegmentedRange.Segment(4, 5, 'A');
-    const segmentD = new Common.SegmentedRange.Segment(6, 7, 'A');
+    const segmentA = new Segment(0, 1, 'A');
+    const segmentB = new Segment(2, 3, 'A');
+    const segmentC = new Segment(4, 5, 'A');
+    const segmentD = new Segment(6, 7, 'A');
 
     // E merges A through D.
-    const segmentE = new Common.SegmentedRange.Segment(2, 6, 'A');
+    const segmentE = new Segment(2, 6, 'A');
 
     segmentedRange.append(segmentA);
     segmentedRange.append(segmentB);
@@ -179,20 +179,20 @@ describe('SegmentedRange', () => {
     segmentedRange.append(segmentE);
 
     const expectedSegments = [
-      new Common.SegmentedRange.Segment(0, 1, 'A'),
-      new Common.SegmentedRange.Segment(2, 7, 'A'),
+      new Segment(0, 1, 'A'),
+      new Segment(2, 7, 'A'),
     ];
     assert.deepEqual(segmentedRange.segments(), expectedSegments);
   });
 
   it('handles one segment consuming many non-mergeable ranges', () => {
-    const segmentA = new Common.SegmentedRange.Segment(0, 1, 'A');
-    const segmentB = new Common.SegmentedRange.Segment(2, 3, 'A');
-    const segmentC = new Common.SegmentedRange.Segment(4, 5, 'A');
-    const segmentD = new Common.SegmentedRange.Segment(6, 7, 'A');
+    const segmentA = new Segment(0, 1, 'A');
+    const segmentB = new Segment(2, 3, 'A');
+    const segmentC = new Segment(4, 5, 'A');
+    const segmentD = new Segment(6, 7, 'A');
 
     // E merges A through D.
-    const segmentE = new Common.SegmentedRange.Segment(2, 6, 'B');
+    const segmentE = new Segment(2, 6, 'B');
 
     segmentedRange.append(segmentA);
     segmentedRange.append(segmentB);
@@ -201,9 +201,9 @@ describe('SegmentedRange', () => {
     segmentedRange.append(segmentE);
 
     const expectedSegments = [
-      new Common.SegmentedRange.Segment(0, 1, 'A'),
-      new Common.SegmentedRange.Segment(2, 6, 'B'),
-      new Common.SegmentedRange.Segment(6, 7, 'A'),
+      new Segment(0, 1, 'A'),
+      new Segment(2, 6, 'B'),
+      new Segment(6, 7, 'A'),
     ];
     assert.deepEqual(segmentedRange.segments(), expectedSegments);
   });

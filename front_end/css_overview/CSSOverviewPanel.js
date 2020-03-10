3ca9e417e03ec7f2426dd0cf0d90cacc8ec24590
Simon Zünd <szuend@chromium.org>
2020-03-10 10:50:18 +0100

    Add missing SDK import to CSSOverviewPanel.js
    TBR=aerotwist@chromium.org
    
    Change-Id: I14ad08304275737523697db5291bad134db70d99
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2096437
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>

--- front_end/css_overview/CSSOverviewPanel.js
+++ front_end/css_overview/CSSOverviewPanel.js
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import * as SDK from '../sdk/sdk.js';
 import * as UI from '../ui/ui.js';
 
 import {CSSOverviewCompletedView} from './CSSOverviewCompletedView.js';

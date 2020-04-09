9d00edb2912ffeb37628acc6cf590e4ee8568912
Alex Rudenko <alexrudenko@chromium.org>
2020-04-09 11:50:09 +0200

    Determine whether request params should be decoded by default
    Not all requests have parameters URL-encoded. This change checks
    if a request is application/x-www-form-urlencoded and decodes the
    parameters by default only in this case (as suggested by phistuck in
    the related bug). Additionally, improved the update code to avoid
    loosing the scroll position when the view is toggled.
    
    Fixed: 1022088
    Bug: 1066415
    Change-Id: Ic238541de9986ffd5b2a431991c9ca494a6afe30
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2143823
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>

--- front_end/network/RequestHeadersView.js
+++ front_end/network/RequestHeadersView.js
@@ -47,6 +47,11 @@ export class RequestHeadersView extends UI.Widget.VBox {
     this._showRequestHeadersText = false;
     this._showResponseHeadersText = false;
 
+    const contentType = request.requestContentType();
+    if (contentType) {
+      this._decodeRequestParameters = !!contentType.match(/^application\/x-www-form-urlencoded\s*(;.*)?$/i);
+    }
+
     /** @type {?UI.TreeOutline.TreeElement} */
     this._highlightedElement = null;
 
@@ -222,20 +227,21 @@ export class RequestHeadersView extends UI.Widget.VBox {
   }
 
   async _refreshFormData() {
-    this._formDataCategory.hidden = true;
-    this._requestPayloadCategory.hidden = true;
-
     const formData = await this._request.requestFormData();
     if (!formData) {
+      this._formDataCategory.hidden = true;
+      this._requestPayloadCategory.hidden = true;
       return;
     }
 
     const formParameters = await this._request.formParameters();
     if (formParameters) {
       this._formDataCategory.hidden = false;
+      this._requestPayloadCategory.hidden = true;
       this._refreshParams(Common.UIString.UIString('Form Data'), formParameters, formData, this._formDataCategory);
     } else {
       this._requestPayloadCategory.hidden = false;
+      this._formDataCategory.hidden = true;
       try {
         const json = JSON.parse(formData);
         this._refreshRequestJSONPayload(json, formData);

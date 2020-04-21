5f6928b9570ed35c4a0b4c1de9b0d1f62370c636
Tim van der Lippe <tvanderlippe@chromium.org>
2020-04-21 12:34:53 +0100

    Remove Properties subpane from Elements panel
    The subpane is replaced by a deprecation message guiding users to
    the `console.dir($0)` feature that already exists in DevTools.
    
    At a later point in time we should remove the deprecation
    message as a whole.
    
    Design doc: https://docs.google.com/document/d/16-PSJSb6BE9Wx2Nn9rYtQOCiDa9-cLwr3R7gk0MirvM/edit?usp=sharing
    Screenshot: https://imgur.com/a/v4wYE3R
    
    R=aerotwist@chromium.org
    
    Change-Id: I137d3f338e4b99ef4de15ced618007991da172ef
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2157037
    Commit-Queue: Tim van der Lippe <tvanderlippe@chromium.org>
    Auto-Submit: Tim van der Lippe <tvanderlippe@chromium.org>
    Reviewed-by: Paul Lewis <aerotwist@chromium.org>

--- front_end/elements/PropertiesWidget.js
+++ front_end/elements/PropertiesWidget.js
@@ -27,10 +27,6 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-import * as Common from '../common/common.js';  // eslint-disable-line no-unused-vars
-import * as Host from '../host/host.js';
-import * as ObjectUI from '../object_ui/object_ui.js';
-import * as SDK from '../sdk/sdk.js';
 import * as UI from '../ui/ui.js';
 
 /**
@@ -39,143 +35,27 @@ import * as UI from '../ui/ui.js';
 export class PropertiesWidget extends UI.ThrottledWidget.ThrottledWidget {
   constructor() {
     super(true /* isWebComponent */);
-    this.registerRequiredCSS('elements/propertiesWidget.css');
 
-    SDK.SDKModel.TargetManager.instance().addModelListener(
-        SDK.DOMModel.DOMModel, SDK.DOMModel.Events.AttrModified, this._onNodeChange, this);
-    SDK.SDKModel.TargetManager.instance().addModelListener(
-        SDK.DOMModel.DOMModel, SDK.DOMModel.Events.AttrRemoved, this._onNodeChange, this);
-    SDK.SDKModel.TargetManager.instance().addModelListener(
-        SDK.DOMModel.DOMModel, SDK.DOMModel.Events.CharacterDataModified, this._onNodeChange, this);
-    SDK.SDKModel.TargetManager.instance().addModelListener(
-        SDK.DOMModel.DOMModel, SDK.DOMModel.Events.ChildNodeCountUpdated, this._onNodeChange, this);
-    self.UI.context.addFlavorChangeListener(SDK.DOMModel.DOMNode, this._setNode, this);
-    this._node = self.UI.context.flavor(SDK.DOMModel.DOMNode);
-
-    this._treeOutline = new ObjectUI.ObjectPropertiesSection.ObjectPropertiesSectionsTreeOutline({readOnly: true});
-    this._treeOutline.setShowSelectionOnKeyboardFocus(/* show */ true, /* preventTabOrder */ false);
-    this._expandController =
-        new ObjectUI.ObjectPropertiesSection.ObjectPropertiesSectionsTreeExpandController(this._treeOutline);
-    this.contentElement.appendChild(this._treeOutline.element);
-
-    this._treeOutline.addEventListener(UI.TreeOutline.Events.ElementExpanded, () => {
-      Host.userMetrics.actionTaken(Host.UserMetrics.Action.DOMPropertiesExpanded);
-    });
-
-    this.update();
-  }
-
-  /**
-   * @param {!Common.EventTarget.EventTargetEvent} event
-   */
-  _setNode(event) {
-    this._node = /** @type {?SDK.DOMModel.DOMNode} */ (event.data);
     this.update();
   }
 
   /**
    * @override
    * @protected
-   * @return {!Promise<undefined>}
+   * @return {!Promise<void>}
    */
   async doUpdate() {
-    if (this._lastRequestedNode) {
-      this._lastRequestedNode.domModel().runtimeModel().releaseObjectGroup(_objectGroupName);
-      delete this._lastRequestedNode;
-    }
+    const deprecationMessage = this.contentElement.createChild('span');
+    deprecationMessage.style.padding = '5px';
+    deprecationMessage.append(UI.UIUtils.formatLocalized(
+        'This panel has been deprecated in favor of executing the following in the console:', []));
 
-    if (!this._node) {
-      this._treeOutline.removeChildren();
-      return;
-    }
+    const codeSnippet = document.createElement('pre');
+    codeSnippet.style.userSelect = 'all';
+    codeSnippet.innerText = 'console.dir($0)';
+    deprecationMessage.append(codeSnippet);
 
-    this._lastRequestedNode = this._node;
-    const object = await this._node.resolveToObject(_objectGroupName);
-    if (!object) {
-      return;
-    }
-
-    const result = await object.callFunction(protoList);
-    object.release();
-
-    if (!result.object || result.wasThrown) {
-      return;
-    }
-
-    const propertiesResult = await result.object.getOwnProperties(false /* generatePreview */);
-    result.object.release();
-
-    if (!propertiesResult || !propertiesResult.properties) {
-      return;
-    }
-
-    const properties = propertiesResult.properties;
-    this._treeOutline.removeChildren();
-
-    let selected = false;
-    // Get array of property user-friendly names.
-    for (let i = 0; i < properties.length; ++i) {
-      if (!parseInt(properties[i].name, 10)) {
-        continue;
-      }
-      const property = properties[i].value;
-      let title = property.description;
-      title = title.replace(/Prototype$/, '');
-
-      const section = this._createSectionTreeElement(property, title);
-      this._treeOutline.appendChild(section);
-      if (!selected) {
-        section.select(/* omitFocus= */ true, /* selectedByUser= */ false);
-        selected = true;
-      }
-    }
-
-    /**
-     * @suppressReceiverCheck
-     * @this {*}
-     */
-    function protoList() {
-      let proto = this;
-      const result = {__proto__: null};
-      let counter = 1;
-      while (proto) {
-        result[counter++] = proto;
-        proto = proto.__proto__;
-      }
-      return result;
-    }
-  }
-
-  /**
-   * @param {!SDK.RemoteObject.RemoteObject} property
-   * @param {string} title
-   * @returns {!ObjectUI.ObjectPropertiesSection.RootElement}
-   */
-  _createSectionTreeElement(property, title) {
-    const titleElement = createElementWithClass('span', 'tree-element-title');
-    titleElement.textContent = title;
-
-    const section = new ObjectUI.ObjectPropertiesSection.RootElement(property);
-    section.title = titleElement;
-    this._expandController.watchSection(title, section);
-
-    return section;
-  }
-
-  /**
-   * @param {!Common.EventTarget.EventTargetEvent} event
-   */
-  _onNodeChange(event) {
-    if (!this._node) {
-      return;
-    }
-    const data = event.data;
-    const node = /** @type {!SDK.DOMModel.DOMNode} */ (data instanceof SDK.DOMModel.DOMNode ? data : data.node);
-    if (this._node !== node) {
-      return;
-    }
-    this.update();
+    deprecationMessage.append(
+        UI.UIUtils.formatLocalized('($0 is a reference to the currently selected DOM node in the elements tree.)', []));
   }
 }
-
-export const _objectGroupName = 'properties-sidebar-pane';

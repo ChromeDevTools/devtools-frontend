973ca26a083a6ea7b6397c387fdac7dc2fe16ed5
Brian Cui <brcui@microsoft.com>
2020-04-24 15:49:19 -0700

    Screencast URL bar now shows decoded characters
    Currently, the Screencast URL bar will show URLs in encoded form
    when the URL contains characters that require escape sequences.
    For example, this link to Korean Wikipedia:
    
    https://ko.wikipedia.org/wiki/위키백과:대문
    
    will appear as the encoded string in Screencast view:
    
    https://ko.wikipedia.org/wiki/%EC%9C%84%ED%82%A4%EB%B0%B1%EA%B3%BC:%EC%A7%88%EB%AC%B8%EB%B0%A9
    
    The Chrome omnibox always shows the URLs in decoded form
    (user friendly), Korean characters included. Thus there is a mismatch
    between what Chrome displays in its addressbar (decoded) and what
    the Screencast view displays (encoded).
    
    This CL updates URL bar in the Screencast view to display URLs in
    decoded (user friendly) form, so it displays the same characters as
    the Chrome address bar.
    
    Note that the Chrome addressbar supports *both* encoded and
    decoded forms of URLs as input, but will convert to decoded once
    the user presses enter. This CL matches the behavior by allowing
    the screencast URL bar to accept both forms while converting to
    decoded once the user presses enter.
    
    Before: https://i.imgur.com/crJ4S7P.gif
    After: https://i.imgur.com/N7TsVXa.gif
    
    Bug: 1070462
    Change-Id: Ie7e11a7cda62cbc15bf2e8be26fa835076586893
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2166449
    Reviewed-by: Brandon Goddard <brgoddar@microsoft.com>
    Reviewed-by: Vidal Diazleal <vidorteg@microsoft.com>
    Reviewed-by: Songtao Xia <soxia@microsoft.com>
    Reviewed-by: Jack Lynch <jalyn@microsoft.com>
    Commit-Queue: Brian Cui <brcui@microsoft.com>

--- front_end/screencast/ScreencastView.js
+++ front_end/screencast/ScreencastView.js
@@ -709,7 +709,12 @@ export class ScreencastView extends UI.Widget.VBox {
     if (!url.match(_SchemeRegex)) {
       url = 'http://' + url;
     }
-    this._resourceTreeModel.navigate(url);
+
+    // Perform decodeURI in case the user enters an encoded string
+    // decodeURI has no effect on strings that are already decoded
+    // encodeURI ensures an encoded URL is always passed to the backend
+    // This allows the input field to support both encoded and decoded URLs
+    this._resourceTreeModel.navigate(encodeURI(decodeURI(url)));
     this._canvasElement.focus();
   }
 
@@ -738,7 +743,7 @@ export class ScreencastView extends UI.Widget.VBox {
       url = match[1];
     }
     Host.InspectorFrontendHost.InspectorFrontendHostInstance.inspectedURLChanged(url);
-    this._navigationUrl.value = url;
+    this._navigationUrl.value = decodeURI(url);
   }
 
   _focusNavigationBar() {

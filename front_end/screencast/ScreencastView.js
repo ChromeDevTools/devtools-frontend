a115bcf652cb317e0de7efe7276fad90718640e1
Mathias Bynens <mathias@chromium.org>
2020-04-30 07:41:31 +0000

    Revert "Screencast URL bar now shows decoded characters"
    This reverts commit 973ca26a083a6ea7b6397c387fdac7dc2fe16ed5.
    
    Reason for revert: appears to break the tree: https://logs.chromium.org/logs/devtools-frontend/buildbucket/cr-buildbucket.appspot.com/8881655897260964688/+/steps/E2E_tests/0/stdout?format=raw
    
    Original change's description:
    > Screencast URL bar now shows decoded characters
    > 
    > Currently, the Screencast URL bar will show URLs in encoded form
    > when the URL contains characters that require escape sequences.
    > For example, this link to Korean Wikipedia:
    > 
    > https://ko.wikipedia.org/wiki/위키백과:대문
    > 
    > will appear as the encoded string in Screencast view:
    > 
    > https://ko.wikipedia.org/wiki/%EC%9C%84%ED%82%A4%EB%B0%B1%EA%B3%BC:%EC%A7%88%EB%AC%B8%EB%B0%A9
    > 
    > The Chrome omnibox always shows the URLs in decoded form
    > (user friendly), Korean characters included. Thus there is a mismatch
    > between what Chrome displays in its addressbar (decoded) and what
    > the Screencast view displays (encoded).
    > 
    > This CL updates URL bar in the Screencast view to display URLs in
    > decoded (user friendly) form, so it displays the same characters as
    > the Chrome address bar.
    > 
    > Note that the Chrome addressbar supports *both* encoded and
    > decoded forms of URLs as input, but will convert to decoded once
    > the user presses enter. This CL matches the behavior by allowing
    > the screencast URL bar to accept both forms while converting to
    > decoded once the user presses enter.
    > 
    > Before: https://i.imgur.com/crJ4S7P.gif
    > After: https://i.imgur.com/N7TsVXa.gif
    > 
    > Bug: 1070462
    > Change-Id: Ie7e11a7cda62cbc15bf2e8be26fa835076586893
    > Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2166449
    > Reviewed-by: Brandon Goddard <brgoddar@microsoft.com>
    > Reviewed-by: Vidal Diazleal <vidorteg@microsoft.com>
    > Reviewed-by: Songtao Xia <soxia@microsoft.com>
    > Reviewed-by: Jack Lynch <jalyn@microsoft.com>
    > Commit-Queue: Brian Cui <brcui@microsoft.com>
    
    TBR=vidorteg@microsoft.com,jalyn@microsoft.com,brgoddar@microsoft.com,brcui@microsoft.com,soxia@microsoft.com
    
    Change-Id: I720d9aef4631c2bcbce74903de9fd725064583b7
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1070462
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2172911
    Reviewed-by: Mathias Bynens <mathias@chromium.org>
    Commit-Queue: Mathias Bynens <mathias@chromium.org>

--- front_end/screencast/ScreencastView.js
+++ front_end/screencast/ScreencastView.js
@@ -709,12 +709,7 @@ export class ScreencastView extends UI.Widget.VBox {
     if (!url.match(_SchemeRegex)) {
       url = 'http://' + url;
     }
-
-    // Perform decodeURI in case the user enters an encoded string
-    // decodeURI has no effect on strings that are already decoded
-    // encodeURI ensures an encoded URL is always passed to the backend
-    // This allows the input field to support both encoded and decoded URLs
-    this._resourceTreeModel.navigate(encodeURI(decodeURI(url)));
+    this._resourceTreeModel.navigate(url);
     this._canvasElement.focus();
   }
 
@@ -743,7 +738,7 @@ export class ScreencastView extends UI.Widget.VBox {
       url = match[1];
     }
     Host.InspectorFrontendHost.InspectorFrontendHostInstance.inspectedURLChanged(url);
-    this._navigationUrl.value = decodeURI(url);
+    this._navigationUrl.value = url;
   }
 
   _focusNavigationBar() {

75ad85f74b8f8fe0ae2c9f0ddb57c4c82f61f5bb
Jose Leal Chapa <joselea@microsoft.com>
2020-05-05 19:41:30 -0700

    Small improvement to e2e tests
    Respect --test-file flag for e2e tests
    
    Change-Id: Ie9db5c4f41f43fd3741c1c7182319919b3c0a982
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2183228
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>
    Commit-Queue: Jose Leal <joselea@microsoft.com>

--- test/e2e/.mocharc.js
+++ test/e2e/.mocharc.js
@@ -9,7 +9,11 @@ const glob = require('glob');
 // To make sure that any leftover JavaScript files (e.g. that were outputs from now-removed tests)
 // aren't incorrectly included, we glob for the TypeScript files instead and use that
 // to instruct Mocha to run the output JavaScript file.
-const testFiles = glob.sync(path.join(__dirname, '**/*_test.ts')).map(fileName => fileName.replace(/\.ts$/, '.js'));
+let testFiles = glob.sync(path.join(__dirname, '**/*_test.ts')).map(fileName => fileName.replace(/\.ts$/, '.js'));
+
+// Respect the test file if defined.
+// This way you can test one single file instead of running all e2e tests every time.
+testFiles = process.env['TEST_FILE'] || testFiles;
 
 // When we are debugging, we don't want to timeout any test. This allows to inspect the state
 // of the application at the moment of the timeout. Here, 0 denotes "indefinite timeout".

947cb8011d13bcf321fbea87e54f02f5eb11f5d5
Peter Marshall <petermarshall@chromium.org>
2020-05-15 16:39:25 +0200

    Fix console autocomplete tests
    Change-Id: Ica329c53a26776f8f1318124e470e193379a221f
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2204114
    Reviewed-by: Benedikt Meurer <bmeurer@chromium.org>

--- test/e2e/console/console-autocomplete_test.ts
+++ test/e2e/console/console-autocomplete_test.ts
@@ -3,22 +3,10 @@
 // found in the LICENSE file.
 
 import {beforeEach, describe, it} from 'mocha';
-import {$, click, getBrowserAndPages, typeText, waitForFunction} from '../../shared/helper.js';
+import {click, getBrowserAndPages, typeText, waitFor, waitForNone} from '../../shared/helper.js';
 import {CONSOLE_TAB_SELECTOR, focusConsolePrompt} from '../helpers/console-helpers.js';
 
 describe('The Console Tab', async () => {
-  function typeTextAndWaitForSuggestBox(text: string) {
-    return Promise.all([
-      waitForFunction(
-          async () => {
-            const element = await $('.suggest-box');
-            return element.asElement();
-          },
-          `No suggest box after typing '${text}'`, 1000),
-      typeText(text),
-    ]);
-  }
-
   beforeEach(async () => {
     const {frontend} = getBrowserAndPages();
 
@@ -27,17 +15,39 @@ describe('The Console Tab', async () => {
 
     await typeText('let object = {a:1, b:2};');
     await frontend.keyboard.press('Enter');
+
+    // Wait for the console to be usable again.
+    await frontend.waitForFunction(() => {
+      return document.querySelectorAll('.console-user-command-result').length === 1;
+    });
   });
 
+  // See the comments in console-repl-mode_test to see why this is necessary.
+  async function objectAutocompleteTest(textAfterObject: string) {
+    const {frontend} = getBrowserAndPages();
+
+    const appearPromise = waitFor('.suggest-box');
+    await typeText('object');
+    await appearPromise;
+
+    const disappearPromise = waitForNone('.suggest-box');
+    await frontend.keyboard.press('Escape');
+    await disappearPromise;
+
+    const appearPromise2 = waitFor('.suggest-box', undefined, 1000);
+    await typeText(textAfterObject);
+    await appearPromise2;
+  }
+
   it('triggers autocompletion for `object.`', async () => {
-    await typeTextAndWaitForSuggestBox('object.');
+    await objectAutocompleteTest('.');
   });
 
   it('triggers autocompletion for `object?.`', async () => {
-    await typeTextAndWaitForSuggestBox('object?.');
+    await objectAutocompleteTest('?.');
   });
 
   it('triggers autocompletion for `object[`', async () => {
-    await typeTextAndWaitForSuggestBox('object[');
+    await objectAutocompleteTest('[');
   });
 });

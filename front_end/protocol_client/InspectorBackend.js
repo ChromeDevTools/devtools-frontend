99a56e31aca7768ea59343ae3d24a38e163c6f70
Sigurd Schneider <sigurds@chromium.org>
2020-05-20 12:08:27 +0200

    Alphabetically sort some methods in InspectorBackend
    Alphabetically sort agent accessors and dispatcher registration
    functions in `InspectorBackend.js`. This reduces merge conflicts
    in the future.
    
    Bug: chromium:1011811, chromium:1079233
    Change-Id: I1759b62c5361593dd693c8ed3d5cf01c08bb677d
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2209466
    Commit-Queue: Sigurd Schneider <sigurds@chromium.org>
    Auto-Submit: Sigurd Schneider <sigurds@chromium.org>
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>

--- front_end/protocol_client/InspectorBackend.js
+++ front_end/protocol_client/InspectorBackend.js
@@ -647,17 +647,13 @@ export class TargetBase {
     return this._router;
   }
 
-  /**
-   * @return {!ProtocolProxyApi.NetworkApi}
-   */
-  networkAgent() {
-    throw new Error('Implemented in InspectorBackend.js');
-  }
+
+  // Agent accessors, keep alphabetically sorted.
 
   /**
-   * @return {!ProtocolProxyApi.LayerTreeApi}
+   * @return {!ProtocolProxyApi.CacheStorageApi}
    */
-  layerTreeAgent() {
+  cacheStorageAgent() {
     throw new Error('Implemented in InspectorBackend.js');
   }
 
@@ -669,13 +665,6 @@ export class TargetBase {
   }
 
   /**
-   * @return {!ProtocolProxyApi.PerformanceApi}
-   */
-  performanceAgent() {
-    throw new Error('Implemented in InspectorBackend.js');
-  }
-
-  /**
    * @return {!ProtocolProxyApi.DOMDebuggerApi}
    */
   domdebuggerAgent() {
@@ -683,16 +672,30 @@ export class TargetBase {
   }
 
   /**
-   * @param {!ProtocolProxyApi.NetworkDispatcher} dispatcher
+   * @return {!ProtocolProxyApi.LayerTreeApi}
    */
-  registerNetworkDispatcher(dispatcher) {
+  layerTreeAgent() {
     throw new Error('Implemented in InspectorBackend.js');
   }
 
   /**
-   * @return {!ProtocolProxyApi.CacheStorageApi}
+   * @return {!ProtocolProxyApi.NetworkApi}
    */
-  cacheStorageAgent() {
+  networkAgent() {
+    throw new Error('Implemented in InspectorBackend.js');
+  }
+
+  /**
+   * @return {!ProtocolProxyApi.PerformanceApi}
+   */
+  performanceAgent() {
+    throw new Error('Implemented in InspectorBackend.js');
+  }
+
+  /**
+   * @return {!ProtocolProxyApi.ServiceWorkerApi}
+   */
+  serviceWorkerAgent() {
     throw new Error('Implemented in InspectorBackend.js');
   }
 
@@ -700,6 +703,16 @@ export class TargetBase {
    * @return {!ProtocolProxyApi.StorageApi}
    */
   storageAgent() {
+    throw new Error('Implemented in InspectorBackend.js');
+  }
+
+
+  // Dispatcher registration, keep alphabetically sorted.
+
+  /**
+   * @param {!ProtocolProxyApi.NetworkDispatcher} dispatcher
+   */
+  registerNetworkDispatcher(dispatcher) {
     throw new Error('Implemented in InspectorBackend.js');
   }
 

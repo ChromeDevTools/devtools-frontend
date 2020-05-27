9f2374511ab39e6383d08ef307ea1ed7e1f2bc57
Christy Chen <chrche@microsoft.com>
2020-05-27 16:20:13 -0700

    Add customization to i18n.js
    i18n requires some customization to be used in DevTools
    1. Remove un-used imports and strings
    2. Change the ultimate fallback from 'en' to 'en-US'
    3. In getMessageInstanceIdFn(), us the filename that gets passed in as the first part of the id
    4. Add lookupClosestLocale() to the file and remove the require statement
    5. Add getFormatter(icuMessageIdOrRawString, locale) and rework the _resolveIcuMessageInstanceId call chain
    
    More details documented here:
    https://docs.google.com/document/d/14bH5pJdAQUmeu3-Ta_snvrHFqQl3Nb1iIrEgHm_Oi8k/edit#heading=h.ifyl2ib4hxje
    
    Change-Id: I356335f67f5305d165fe4c67177fb7aeaa4b51d3
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2199904
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Reviewed-by: Connor Clark <cjamcl@chromium.org>
    Commit-Queue: Christy Chen <chrche@microsoft.com>

--- front_end/third_party/i18n/i18n.js
+++ front_end/third_party/i18n/i18n.js
@@ -5,17 +5,13 @@
  */
 'use strict';
 
-const path = require('path');
 const isDeepEqual = require('lodash.isequal');
-const log = require('lighthouse-logger');
 const MessageFormat = require('intl-messageformat').default;
-const lookupClosestLocale = require('lookup-closest-locale');
 const LOCALES = require('./locales.js');
 
 /** @typedef {import('intl-messageformat-parser').Element} MessageElement */
 /** @typedef {import('intl-messageformat-parser').ArgumentElement} ArgumentElement */
 
-const LH_ROOT = path.join(__dirname, '../../../');
 const MESSAGE_INSTANCE_ID_REGEX = /(.* \| .*) # (\d+)$/;
 // Above regex is very slow against large strings. Use QUICK_REGEX as a much quicker discriminator.
 const MESSAGE_INSTANCE_ID_QUICK_REGEX = / # \d+$/;
@@ -24,9 +20,6 @@ const MESSAGE_INSTANCE_ID_QUICK_REGEX = / # \d+$/;
   // Node without full-icu doesn't come with the locales we want built-in. Load the polyfill if needed.
   // See https://nodejs.org/api/intl.html#intl_options_for_building_node_js
 
-  // Conditionally polyfills itself. Bundler removes this dep, so this will be a no-op in browsers.
-  // @ts-ignore
-  require('intl-pluralrules');
 
   // @ts-ignore
   const IntlPolyfill = require('intl');
@@ -44,77 +37,6 @@ const MESSAGE_INSTANCE_ID_QUICK_REGEX = / # \d+$/;
   }
 })();
 
-
-const UIStrings = {
-  /** Used to show the duration in milliseconds that something lasted. The `{timeInMs}` placeholder will be replaced with the time duration, shown in milliseconds (e.g. 63 ms) */
-  ms: '{timeInMs, number, milliseconds}\xa0ms',
-  /** Used to show the duration in seconds that something lasted. The {timeInMs} placeholder will be replaced with the time duration, shown in seconds (e.g. 5.2 s) */
-  seconds: '{timeInMs, number, seconds}\xa0s',
-  /** Label shown per-audit to show how many bytes smaller the page could be if the user implemented the suggestions. The `{wastedBytes}` placeholder will be replaced with the number of bytes, shown in kilobytes (e.g. 148 KB) */
-  displayValueByteSavings: 'Potential savings of {wastedBytes, number, bytes}\xa0KB',
-  /** Label shown per-audit to show how many milliseconds faster the page load could be if the user implemented the suggestions. The `{wastedMs}` placeholder will be replaced with the time duration, shown in milliseconds (e.g. 140 ms) */
-  displayValueMsSavings: 'Potential savings of {wastedMs, number, milliseconds}\xa0ms',
-  /** Label for a column in a data table; entries will be the URL of a web resource */
-  columnURL: 'URL',
-  /** Label for a column in a data table; entries will be the size of a web resource in kilobytes. */
-  columnSize: 'Size',
-  /** Label for a column in a data table; entries will be the time to live value of the cache header on a web resource. */
-  columnCacheTTL: 'Cache TTL',
-  /** Label for a column in a data table; entries will be the number of kilobytes the user could reduce their page by if they implemented the suggestions. */
-  columnWastedBytes: 'Potential Savings',
-  /** Label for a column in a data table; entries will be the number of milliseconds the user could reduce page load by if they implemented the suggestions. */
-  columnWastedMs: 'Potential Savings',
-  /** Label for a column in a data table; entries will be the number of milliseconds spent during a particular activity. */
-  columnTimeSpent: 'Time Spent',
-  /** Label for a column in a data table; entries will be the location of a specific line of code in a file, in the format "line: 102". */
-  columnLocation: 'Location',
-  /** Label for a column in a data table; entries will be types of resources loaded over the network, e.g. "Scripts", "Third-Party", "Stylesheet". */
-  columnResourceType: 'Resource Type',
-  /** Label for a column in a data table; entries will be the number of network requests done by a webpage. */
-  columnRequests: 'Requests',
-  /** Label for a column in a data table; entries will be the number of kilobytes transferred to load a set of files. */
-  columnTransferSize: 'Transfer Size',
-  /** Label for a column in a data table; entries will be the names of arbitrary objects, e.g. the name of a Javascript library, or the name of a user defined timing event. */
-  columnName: 'Name',
-  /** Label for a column in a data table; entries will be the names of JavaScript code, e.g. the name of a Javascript package or module. */
-  columnSource: 'Source',
-  /** Label for a column in a data table; entries will be how much a predetermined budget has been exeeded by. Depending on the context, this number could represent an excess in quantity or size of network requests, or, an excess in the duration of time that it takes for the page to load.*/
-  columnOverBudget: 'Over Budget',
-  /** Label for a row in a data table; entries will be the total number and byte size of all resources loaded by a web page. */
-  totalResourceType: 'Total',
-  /** Label for a row in a data table; entries will be the total number and byte size of all 'Document' resources loaded by a web page. */
-  documentResourceType: 'Document',
-  /** Label for a row in a data table; entries will be the total number and byte size of all 'Script' resources loaded by a web page. 'Script' refers to JavaScript or other files that are executable by a browser. */
-  scriptResourceType: 'Script',
-  /** Label for a row in a data table; entries will be the total number and byte size of all 'Stylesheet' resources loaded by a web page. 'Stylesheet' refers to CSS stylesheets. */
-  stylesheetResourceType: 'Stylesheet',
-  /** Label for a row in a data table; entries will be the total number and byte size of all 'Image' resources loaded by a web page. */
-  imageResourceType: 'Image',
-  /** Label for a row in a data table; entries will be the total number and byte size of all 'Media' resources loaded by a web page. 'Media' refers to audio and video files. */
-  mediaResourceType: 'Media',
-  /** Label for a row in a data table; entries will be the total number and byte size of all 'Font' resources loaded by a web page. */
-  fontResourceType: 'Font',
-  /** Label for a row in a data table; entries will be the total number and byte size of all resources loaded by a web page that don't fit into the categories of Document, Script, Stylesheet, Image, Media, & Font.*/
-  otherResourceType: 'Other',
-  /** Label for a row in a data table; entries will be the total number and byte size of all third-party resources loaded by a web page. 'Third-party resources are items loaded from URLs that aren't controlled by the owner of the web page. */
-  thirdPartyResourceType: 'Third-party',
-  /** The name of the metric that marks the time at which the first text or image is painted by the browser. Shown to users as the label for the numeric metric value. Ideally fits within a ~40 character limit. */
-  firstContentfulPaintMetric: 'First Contentful Paint',
-  /** The name of the metric that marks when the page has displayed content and the CPU is not busy executing the page's scripts. Shown to users as the label for the numeric metric value. Ideally fits within a ~40 character limit. */
-  firstCPUIdleMetric: 'First CPU Idle',
-  /** The name of the metric that marks the time at which the page is fully loaded and is able to quickly respond to user input (clicks, taps, and keypresses feel responsive). Shown to users as the label for the numeric metric value. Ideally fits within a ~40 character limit. */
-  interactiveMetric: 'Time to Interactive',
-  /** The name of the metric that marks the time at which a majority of the content has been painted by the browser. Shown to users as the label for the numeric metric value. Ideally fits within a ~40 character limit. */
-  firstMeaningfulPaintMetric: 'First Meaningful Paint',
-  /** The name of the metric that marks the estimated time between the page receiving input (a user clicking, tapping, or typing) and the page responding. Shown to users as the label for the numeric metric value. Ideally fits within a ~40 character limit. */
-  estimatedInputLatencyMetric: 'Estimated Input Latency',
-  /** The name of a metric that calculates the total duration of blocking time for a web page. Blocking times are time periods when the page would be blocked (prevented) from responding to user input (clicks, taps, and keypresses will feel slow to respond). Shown to users as the label for the numeric metric value. Ideally fits within a ~40 character limit. */
-  totalBlockingTimeMetric: 'Total Blocking Time',
-  /** The name of the metric "Maximum Potential First Input Delay" that marks the maximum estimated time between the page receiving input (a user clicking, tapping, or typing) and the page responding. Shown to users as the label for the numeric metric value. Ideally fits within a ~40 character limit. */
-  maxPotentialFIDMetric: 'Max Potential First Input Delay',
-  /** The name of the metric that summarizes how quickly the page looked visually complete. The name of this metric is largely abstract and can be loosely translated. Shown to users as the label for the numeric metric value. Ideally fits within a ~40 character limit. */
-  speedIndexMetric: 'Speed Index',
-};
 
 const formats = {
   number: {
@@ -141,7 +63,7 @@ const formats = {
  * Look up the best available locale for the requested language through these fall backs:
  * - exact match
  * - progressively shorter prefixes (`de-CH-1996` -> `de-CH` -> `de`)
- * - the default locale ('en') if no match is found
+ * - the default locale ('en-US') if no match is found
  *
  * If `locale` isn't provided, the default is used.
  * @param {string=} locale
@@ -152,7 +74,7 @@ function lookupLocale(locale) {
   const canonicalLocale = Intl.getCanonicalLocales(locale)[0];
 
   const closestLocale = lookupClosestLocale(canonicalLocale, LOCALES);
-  return closestLocale || 'en';
+  return closestLocale || 'en-US';
 }
 
 /**
@@ -277,10 +199,9 @@ const _ICUMsgNotFoundMsg = 'ICU message not found in destination locale';
  * @param {LH.Locale} locale
  * @param {string} icuMessageId
  * @param {string=} uiStringMessage The original string given in 'UIStrings', used as a backup if no locale message can be found
- * @param {Record<string, string | number>} [values]
- * @return {{formattedString: string, icuMessage: string}}
+ * @return {{localeMessage: string, formatter: MessageFormat}}
  */
-function _formatIcuMessage(locale, icuMessageId, uiStringMessage, values = {}) {
+function _getLocaleMessageAndCreateFormatter(locale, icuMessageId, uiStringMessage) {
   const localeMessages = LOCALES[locale];
   if (!localeMessages) throw new Error(`Unsupported locale '${locale}'`);
   let localeMessage = localeMessages[icuMessageId] && localeMessages[icuMessageId].message;
@@ -291,9 +212,9 @@ function _formatIcuMessage(locale, icuMessageId, uiStringMessage, values = {}) {
     // Try to use the original uiStringMessage
     localeMessage = uiStringMessage;
 
-    // Warn the user that the UIString message != the `en` message ∴ they should update the strings
-    if (!LOCALES.en[icuMessageId] || localeMessage !== LOCALES.en[icuMessageId].message) {
-      log.verbose('i18n', `Message "${icuMessageId}" does not match its 'en' counterpart. ` +
+    // Warn the user that the UIString message != the `en-US` message ∴ they should update the strings
+    if (!LOCALES['en-US'][icuMessageId] || localeMessage !== LOCALES['en-US'][icuMessageId].message) {
+      console.log('i18n', `Message "${icuMessageId}" does not match its 'en-US' counterpart. ` +
         `Run 'i18n' to update.`);
     }
   }
@@ -307,6 +228,16 @@ function _formatIcuMessage(locale, icuMessageId, uiStringMessage, values = {}) {
 
   const formatter = new MessageFormat(localeMessage, localeForMessageFormat, formats);
 
+  return {localeMessage, formatter};
+}
+/**
+ *
+ * @param {string} localeMessage
+ * @param {MessageFormat} formatter
+ * @param {Record<string, string | number>} [values]
+ * @return {{formattedString: string, icuMessage: string}}
+ */
+function _formatMessage(localeMessage, formatter, values = {}) {
   // preformat values for the message format like KB and milliseconds
   const valuesForMessageFormat = _preformatValues(localeMessage, formatter, values);
 
@@ -359,9 +290,6 @@ function getRendererFormattedStrings(locale) {
  * @param {Record<string, string>} fileStrings
  */
 function createMessageInstanceIdFn(filename, fileStrings) {
-  /** @type {Record<string, string>} */
-  const mergedStrings = {...UIStrings, ...fileStrings};
-
   /**
    * Convert a message string & replacement values into an
    * indexed id value in the form '{messageid} | # {index}'.
@@ -370,11 +298,10 @@ function createMessageInstanceIdFn(filename, fileStrings) {
    * @param {Record<string, string | number>} [values]
    * */
   const getMessageInstanceIdFn = (icuMessage, values) => {
-    const keyname = Object.keys(mergedStrings).find(key => mergedStrings[key] === icuMessage);
+    const keyname = Object.keys(fileStrings).find(key => fileStrings[key] === icuMessage);
     if (!keyname) throw new Error(`Could not locate: ${icuMessage}`);
 
-    const filenameToLookup = keyname in fileStrings ? filename : __filename;
-    const unixStyleFilename = path.relative(LH_ROOT, filenameToLookup).replace(/\\/g, '/');
+    const unixStyleFilename = filename.replace(/\\/g, '/');
     const icuMessageId = `${unixStyleFilename} | ${keyname}`;
     const icuMessageInstances = _icuMessageInstanceMap.get(icuMessageId) || [];
 
@@ -409,7 +336,26 @@ function isIcuMessage(icuMessageIdOrRawString) {
  */
 function getFormatted(icuMessageIdOrRawString, locale) {
   if (isIcuMessage(icuMessageIdOrRawString)) {
-    return _resolveIcuMessageInstanceId(icuMessageIdOrRawString, locale).formattedString;
+    const {icuMessageId, icuMessageInstance} = _resolveIcuMessageInstanceId(icuMessageIdOrRawString);
+    const {localeMessage, formatter} = _getLocaleMessageAndCreateFormatter(locale, icuMessageId, icuMessageInstance.icuMessage);
+    const {formattedString} = _formatMessage(localeMessage, formatter, icuMessageInstance.values);
+    return formattedString;
+  }
+
+  return icuMessageIdOrRawString;
+}
+
+/**
+ * @param {string} icuMessageIdOrRawString
+ * @param {LH.Locale} locale
+ * @return {MessageFormat | string}
+ */
+function getFormatter(icuMessageIdOrRawString, locale) {
+  if (isIcuMessage(icuMessageIdOrRawString)) {
+    const {icuMessageId, icuMessageInstance} = _resolveIcuMessageInstanceId(icuMessageIdOrRawString);
+    const {formatter} = _getLocaleMessageAndCreateFormatter(locale, icuMessageId, icuMessageInstance.icuMessage);
+
+    return formatter;
   }
 
   return icuMessageIdOrRawString;
@@ -425,16 +371,17 @@ function getFormattedFromIdAndValues(locale, icuMessageId, values) {
   const icuMessageIdRegex = /(.* \| .*)$/;
   if (!icuMessageIdRegex.test(icuMessageId)) throw new Error('This is not an ICU message ID');
 
-  const {formattedString} = _formatIcuMessage(locale, icuMessageId, undefined, values);
+  const {localeMessage, formatter} = _getLocaleMessageAndCreateFormatter(locale, icuMessageId, undefined);
+  const {formattedString} = _formatMessage(localeMessage, formatter, values);
+
   return formattedString;
 }
 
 /**
  * @param {string} icuMessageInstanceId
- * @param {LH.Locale} locale
- * @return {{icuMessageInstance: IcuMessageInstance, formattedString: string}}
+ * @return {{icuMessageId: string, icuMessageInstance: IcuMessageInstance}}
  */
-function _resolveIcuMessageInstanceId(icuMessageInstanceId, locale) {
+function _resolveIcuMessageInstanceId(icuMessageInstanceId) {
   const matches = icuMessageInstanceId.match(MESSAGE_INSTANCE_ID_REGEX);
   if (!matches) throw new Error(`${icuMessageInstanceId} is not a valid message instance ID`);
 
@@ -442,10 +389,7 @@ function _resolveIcuMessageInstanceId(icuMessageInstanceId, locale) {
   const icuMessageInstances = _icuMessageInstanceMap.get(icuMessageId) || [];
   const icuMessageInstance = icuMessageInstances[Number(icuMessageInstanceIndex)];
 
-  const {formattedString} = _formatIcuMessage(locale, icuMessageId,
-    icuMessageInstance.icuMessage, icuMessageInstance.values);
-
-  return {icuMessageInstance, formattedString};
+  return {icuMessageId, icuMessageInstance};
 }
 
 /**
@@ -470,7 +414,10 @@ function replaceIcuMessageInstanceIds(inputObject, locale) {
 
       // Check to see if the value in the LHR looks like a string reference. If it is, replace it.
       if (typeof value === 'string' && isIcuMessage(value)) {
-        const {icuMessageInstance, formattedString} = _resolveIcuMessageInstanceId(value, locale);
+        const {icuMessageId, icuMessageInstance} = _resolveIcuMessageInstanceId(value);
+        const {localeMessage, formatter} = _getLocaleMessageAndCreateFormatter(locale, icuMessageId, icuMessageInstance.icuMessage);
+        const {formattedString} = _formatMessage(localeMessage, formatter, icuMessageInstance.values);
+
         const messageInstancesInLHR = icuMessagePaths[icuMessageInstance.icuMessageId] || [];
         const currentPathAsString = _formatPathAsString(currentPathInLHR);
 
@@ -507,14 +454,31 @@ function registerLocaleData(locale, lhlMessages) {
   LOCALES[locale] = lhlMessages;
 }
 
+/**
+ * Get the closest locale from the ones available For example,
+ * if es is supported and es-419 is not, then we return es when es-419 is requested
+ * @param {LH.Locale} locale
+ * @param {any} available
+ */
+function lookupClosestLocale(locale, available) {
+  const localeParts = locale.split('-');
+  while (localeParts.length) {
+    let candidate = localeParts.join('-');
+    if (available[candidate]) {
+      return candidate;
+    }
+    localeParts.pop();
+  }
+};
+
 module.exports = {
   _formatPathAsString,
   _ICUMsgNotFoundMsg,
-  UIStrings,
   lookupLocale,
   getRendererFormattedStrings,
   createMessageInstanceIdFn,
   getFormatted,
+  getFormatter,
   getFormattedFromIdAndValues,
   replaceIcuMessageInstanceIds,
   isIcuMessage,

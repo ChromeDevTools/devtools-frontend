322b5eefc97a2c719b41d919a401e09c5d208337
Tim van der Lippe <tvanderlippe@chromium.org>
2020-06-01 16:56:03 +0100

    Cleanup hosted_mode proxy logic
    This logic is unused now, so we can remove it. This will allow us
    to remove the require on `utils`, in preparation for the e2e-test
    changes.
    
    R=aerotwist@chromium.org,jacktfranklin@chromium.org
    
    Bug: 1088463
    Change-Id: Ie2271f4e0dc17f08b8b11860f0fddb1b8b697066
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2224806
    Reviewed-by: Jack Franklin <jacktfranklin@chromium.org>
    Reviewed-by: Paul Lewis <aerotwist@chromium.org>
    Commit-Queue: Tim van der Lippe <tvanderlippe@chromium.org>

--- scripts/hosted_mode/server.js
+++ scripts/hosted_mode/server.js
@@ -6,11 +6,9 @@ const http = require('http');
 const path = require('path');
 const parseURL = require('url').parse;
 
-const utils = require('../utils');
-
 const remoteDebuggingPort = parseInt(process.env.REMOTE_DEBUGGING_PORT, 10) || 9222;
 const serverPort = parseInt(process.env.PORT, 10) || 8090;
-const devtoolsFolder = path.resolve(path.join(__dirname, '../..'));
+const devtoolsFolder = path.resolve(path.join(__dirname, '..', '..'));
 
 http.createServer(requestHandler).listen(serverPort);
 console.log(`Started hosted mode server at http://localhost:${serverPort}\n`);
@@ -24,18 +22,6 @@ function requestHandler(request, response) {
     const landingURL = `http://localhost:${remoteDebuggingPort}#custom=true`;
     sendResponse(200, `<html>Please go to <a href="${landingURL}">${landingURL}</a></html>`);
     return;
-  }
-
-  const proxiedFile = proxy(filePath, sendResponse);
-  if (proxiedFile) {
-    proxiedFile.then(data => sendResponse(200, data)).catch(handleProxyError);
-    return;
-  }
-
-  function handleProxyError(err) {
-    console.log(`Error serving the file ${filePath}:`, err);
-    console.log(`Make sure you opened Chrome with the flag "--remote-debugging-port=${remoteDebuggingPort}"`);
-    sendResponse(500, '500 - Internal Server Error');
   }
 
   const absoluteFilePath = path.join(devtoolsFolder, filePath);
@@ -130,62 +116,5 @@ function requestHandler(request, response) {
     }
 
     response.end();
-  }
-}
-
-const proxyFilePathToURL = {
-  '/favicon.ico': () => 'https://chrome-devtools-frontend.appspot.com/favicon.ico',
-};
-
-const proxyFileCache = new Map();
-
-function proxy(filePath) {
-  if (!(filePath in proxyFilePathToURL)) {
-    return null;
-  }
-  if (process.env.CHROMIUM_COMMIT) {
-    return onProxyFileURL(proxyFilePathToURL[filePath](process.env.CHROMIUM_COMMIT));
-  }
-  return utils.fetch(`http://localhost:${remoteDebuggingPort}/json/version`)
-      .then(onBrowserMetadata)
-      .then(onProxyFileURL);
-
-  function onBrowserMetadata(metadata) {
-    const metadataObject = JSON.parse(metadata);
-    const match = metadataObject['WebKit-Version'].match(/\s\(@(\b[0-9a-f]{5,40}\b)/);
-    const commitHash = match[1];
-    const proxyFileURL = proxyFilePathToURL[filePath](commitHash);
-    return proxyFileURL;
-  }
-
-  function onProxyFileURL(proxyFileURL) {
-    if (proxyFileCache.has(proxyFileURL)) {
-      return Promise.resolve(proxyFileCache.get(proxyFileURL));
-    }
-    return utils.fetch(proxyFileURL).then(cacheProxyFile.bind(null, proxyFileURL)).catch(onMissingFile);
-  }
-
-  function onMissingFile() {
-    const isFullCheckout = utils.shellOutput('git config --get remote.origin.url') ===
-        'https://chromium.googlesource.com/chromium/src.git';
-    let earlierCommitHash;
-    const gitLogCommand = 'git log --max-count=1 --grep="Commit-Position" --before="12 hours ago"';
-    if (isFullCheckout) {
-      earlierCommitHash = utils.shellOutput(`${gitLogCommand} --pretty=format:"%H"`);
-    } else {
-      const commitMessage = utils.shellOutput(`${gitLogCommand}`);
-      earlierCommitHash = commitMessage.match(/Cr-Mirrored-Commit: (.*)/)[1];
-    }
-    const fallbackURL = proxyFilePathToURL[filePath](earlierCommitHash);
-    console.log('WARNING: Unable to fetch generated file based on browser\'s revision');
-    console.log('Fetching earlier revision of same file as fallback');
-    console.log('There may be a mismatch between the front-end and back-end');
-    console.log(fallbackURL, '\n');
-    return utils.fetch(fallbackURL).then(cacheProxyFile.bind(null, fallbackURL));
-  }
-
-  function cacheProxyFile(proxyFileURL, data) {
-    proxyFileCache.set(proxyFileURL, data);
-    return data;
   }
 }

255926129b1086f94f9cb6a38c6a26572e33561c
Patrick Brosset <patrick.brosset@microsoft.com>
2020-06-01 08:29:39 -0700

    Introducing a general UI interaction test for the Network panel
    The Network panel did have 1 e2e test file which covered some very
    specific cases such as encoding, mime type and checkbox interaction.
    But it did not have a simple and general test for the main UI
    interaction flows: displaying the list and selecting requests.
    
    Adding this test means that we now have a place for checks that
    verify that the panel works at all.
    
    Bug: 1079076
    Change-Id: Ice25dee7743a375c033485c9af4853e477a938ca
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2210313
    Commit-Queue: Patrick Brosset <patrick.brosset@microsoft.com>
    Reviewed-by: Robert Paveza <Rob.Paveza@microsoft.com>
    Reviewed-by: Jack Franklin <jacktfranklin@chromium.org>

---
+++ test/e2e/helpers/network-helpers.ts
@@ -0,0 +1,65 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import * as puppeteer from 'puppeteer';
+
+import {$, $$, click, getBrowserAndPages, resourcesPath, waitFor, waitForFunction} from '../../shared/helper.js';
+
+const REQUEST_LIST_SELECTOR = '.network-log-grid .data';
+
+/**
+ * Select the Network tab in DevTools
+ */
+export async function navigateToNetworkTab(target: puppeteer.Page, testName: string) {
+  await target.goto(`${resourcesPath}/network/${testName}`);
+  await click('#tab-network');
+  // Make sure the network tab is shown on the screen
+  await waitFor('.network-log-grid');
+}
+
+/**
+ * Wait until a certain number of requests are shown in the request list.
+ * @param numberOfRequests The expected number of requests to wait for.
+ * @param selector Optional. The selector to use to get the list of requests.
+ */
+export async function waitForSomeRequestsToAppear(numberOfRequests: number, selector = '.name-column') {
+  const {frontend} = getBrowserAndPages();
+
+  await frontend.waitForFunction((numberOfRequests: number, selector: string) => {
+    return document.querySelectorAll(selector).length >= numberOfRequests;
+  }, {}, numberOfRequests, REQUEST_LIST_SELECTOR + ' ' + selector);
+}
+
+export async function getAllRequestNames() {
+  const requests = await $$(REQUEST_LIST_SELECTOR + ' .name-column');
+  return await requests.evaluate((nodes: Element[]) => {
+    return nodes.map(request => request.childNodes[1].textContent);
+  });
+}
+
+export async function getSelectedRequestName() {
+  const request = await $(REQUEST_LIST_SELECTOR + ' tr.selected .name-column');
+  return await request.evaluate((node: Element) => {
+    return node && node.childNodes[1].textContent;
+  });
+}
+
+export async function selectRequestByName(name: string) {
+  const requests = await $$(REQUEST_LIST_SELECTOR + ' .name-column');
+  const request = await requests.evaluateHandle((nodes: Element[], name) => {
+    return nodes.find(node => node.childNodes[1].textContent = name);
+  }, name);
+  await click(request);
+}
+
+export async function waitForSelectedRequestChange(initialRequestName: string|null) {
+  await waitForFunction(async () => {
+    const name = await getSelectedRequestName();
+    return name !== initialRequestName;
+  }, `The selected request did not get updated from ${initialRequestName}`);
+}
+
+export async function togglePersistLog() {
+  await click('[aria-label="Preserve log"]');
+}
---
+++ test/e2e/network/network_test.ts
@@ -0,0 +1,69 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {assert} from 'chai';
+import {describe, it} from 'mocha';
+
+import {getBrowserAndPages} from '../../shared/helper.js';
+import {getAllRequestNames, getSelectedRequestName, navigateToNetworkTab, selectRequestByName, togglePersistLog, waitForSelectedRequestChange, waitForSomeRequestsToAppear} from '../helpers/network-helpers.js';
+
+const SIMPLE_PAGE_REQUEST_NUMBER = 10;
+const SIMPLE_PAGE_URL = `requests.html?num=${SIMPLE_PAGE_REQUEST_NUMBER}`;
+
+describe('The Network Tab', async () => {
+  it('displays requests', async () => {
+    const {target} = getBrowserAndPages();
+    await navigateToNetworkTab(target, SIMPLE_PAGE_URL);
+
+    // Wait for all the requests to be displayed + 1 to account for the page itself.
+    await waitForSomeRequestsToAppear(SIMPLE_PAGE_REQUEST_NUMBER + 1);
+
+    const expectedNames = [SIMPLE_PAGE_URL];
+    for (let i = 0; i < SIMPLE_PAGE_REQUEST_NUMBER; i++) {
+      expectedNames.push(`image.svg?id=${i}`);
+    }
+
+    const names = await getAllRequestNames();
+    assert.deepStrictEqual(names, expectedNames, 'The right request names should appear in the list');
+  });
+
+  it('can select requests', async () => {
+    const {target} = getBrowserAndPages();
+    await navigateToNetworkTab(target, SIMPLE_PAGE_URL);
+
+    let selected = await getSelectedRequestName();
+    assert.isUndefined(selected, 'No request should be selected by default');
+
+    await selectRequestByName(SIMPLE_PAGE_URL);
+    await waitForSelectedRequestChange(selected);
+
+    selected = await getSelectedRequestName();
+    assert.strictEqual(selected, SIMPLE_PAGE_URL, 'Selecting the first request should work');
+
+    const lastRequestName = `image.svg?id=${SIMPLE_PAGE_REQUEST_NUMBER - 1}`;
+    await selectRequestByName(lastRequestName);
+    await waitForSelectedRequestChange(selected);
+
+    selected = await getSelectedRequestName();
+    assert.strictEqual(selected, lastRequestName, 'Selecting the last request should work');
+  });
+
+  it('can persist requests', async () => {
+    const {target} = getBrowserAndPages();
+    await navigateToNetworkTab(target, SIMPLE_PAGE_URL);
+
+    // Wait for all the requests to be displayed + 1 to account for the page itself, and get their names.
+    await waitForSomeRequestsToAppear(SIMPLE_PAGE_REQUEST_NUMBER + 1);
+    const firstPageRequestNames = await getAllRequestNames();
+
+    await togglePersistLog();
+
+    // Navigate to a new page, and wait for the same requests to still be there.
+    await target.goto('about:blank');
+    await waitForSomeRequestsToAppear(SIMPLE_PAGE_REQUEST_NUMBER + 1);
+    const secondPageRequestNames = await getAllRequestNames();
+
+    assert.deepStrictEqual(firstPageRequestNames, secondPageRequestNames, 'The requests were persisted');
+  });
+});

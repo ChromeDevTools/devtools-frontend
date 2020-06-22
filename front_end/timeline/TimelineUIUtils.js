bff241dc95433dccd0621c81d93aae793d46a48e
Mathias Bynens <mathias@chromium.org>
2020-06-22 13:52:57 +0200

    Rework V8 code cache-related UI
    This patch causes code caching information to be displayed regardless
    of whether or not caching was successful. Previously, DevTools wouldn’t
    show anything related to code caching if code caching didn’t happen.
    
    It also improves the UX by presenting the information in a more logical
    order and by formatting the cache sizes nicely.
    
    To distinguish from the many other caches DevTools exposes, the name
    “compilation cache” was chosen. One downside of the term “compilation
    cache” is that it’s used to refer to the “isolate cache” in V8. On the
    other hand, the term “bytecode cache” is not as future-friendly, since
    V8 might one day have cacheable NCI code.
    
    Screenshots:
    
    - Before: https://i.imgur.com/dDHOiMj.png
    - After:
        — Script not eligible: https://i.imgur.com/mrv03ZJ.png
        - Script loaded from cache: https://i.imgur.com/8J4CEqO.png
    
    Test/demo page:
    https://mathiasbynens.github.io/css-dbg-stories/v8-compilation-cache.html
    
    Bug: chromium:912581
    Change-Id: I053fcf2a731757105c00004896b9bd0ef05f3a34
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2256978
    Commit-Queue: Mathias Bynens <mathias@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Leszek Swirski <leszeks@chromium.org>

--- front_end/timeline/TimelineUIUtils.js
+++ front_end/timeline/TimelineUIUtils.js
@@ -795,6 +795,27 @@ export class TimelineUIUtils {
   }
 
   /**
+   * @param {!Object} eventData
+   * @param {!TimelineDetailsContentHelper} contentHelper
+   */
+  static buildCompilationCacheDetails(eventData, contentHelper) {
+    if ('producedCacheSize' in eventData) {
+      contentHelper.appendTextRow(ls`Compilation cache status`, ls`script saved to cache`);
+      contentHelper.appendTextRow(
+          ls`Compilation cache size`, Platform.NumberUtilities.bytesToString(eventData['producedCacheSize']));
+    } else if ('consumedCacheSize' in eventData) {
+      contentHelper.appendTextRow(ls`Compilation cache status`, ls`script loaded from cache`);
+      contentHelper.appendTextRow(
+          ls`Compilation cache size`, Platform.NumberUtilities.bytesToString(eventData['consumedCacheSize']));
+    } else if (eventData && eventData['cacheRejected']) {
+      // Version mismatch or similar.
+      contentHelper.appendTextRow(ls`Compilation cache status`, ls`failed to load script from cache`);
+    } else {
+      contentHelper.appendTextRow(ls`Compilation cache status`, ls`script not eligible`);
+    }
+  }
+
+  /**
    * @param {!SDK.TracingModel.Event} event
    * @param {!TimelineModel.TimelineModel.TimelineModelImpl} model
    * @param {!Components.Linkifier.Linkifier} linkifier
@@ -956,17 +977,10 @@ export class TimelineUIUtils {
         if (url) {
           contentHelper.appendLocationRow(ls`Script`, url, eventData['lineNumber'], eventData['columnNumber']);
         }
-        contentHelper.appendTextRow(ls`Streamed`, eventData['streamed']);
-        const producedCacheSize = eventData && eventData['producedCacheSize'];
-        if (producedCacheSize) {
-          contentHelper.appendTextRow(ls`Produced Cache Size`, producedCacheSize);
-        }
-        const cacheConsumeOptions = eventData && eventData['cacheConsumeOptions'];
-        if (cacheConsumeOptions) {
-          contentHelper.appendTextRow(ls`Cache Consume Options`, cacheConsumeOptions);
-          contentHelper.appendTextRow(ls`Consumed Cache Size`, eventData['consumedCacheSize']);
-          contentHelper.appendTextRow(ls`Cache Successful`, !eventData['cacheRejected']);
-        }
+        const isStreamed = eventData['streamed'];
+        contentHelper.appendTextRow(
+            ls`Streamed`, isStreamed + (isStreamed ? '' : `: ${eventData['notStreamedReason']}`));
+        TimelineUIUtils.buildCompilationCacheDetails(eventData, contentHelper);
         break;
       }
 

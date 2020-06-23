a51e4952cb63c90760540a7202150d1d2ea72acb
Kim-Anh Tran <kimanh@chromium.org>
2020-06-23 14:51:13 +0200

    Enable e2e test for wasm debugging after bugfix
    Bug: chromium:1091116
    Change-Id: I06367b195c79011d4db15a6590d3536fcc6881e0
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2260672
    Auto-Submit: Kim-Anh Tran <kimanh@chromium.org>
    Commit-Queue: Benedikt Meurer <bmeurer@chromium.org>
    Reviewed-by: Benedikt Meurer <bmeurer@chromium.org>

--- test/e2e/sources/debug-raw-wasm_test.ts
+++ test/e2e/sources/debug-raw-wasm_test.ts
@@ -41,73 +41,7 @@ describe('Sources Tab', async () => {
     assert.deepEqual(scriptLocation, 'add.wasm:0x23');
   });
 
-  it('hits breakpoint upon refresh', async () => {
-    const {target, frontend} = getBrowserAndPages();
-
-    await step('navigate to a page and open the Sources tab', async () => {
-      await openSourceCodeEditorForFile('add.wasm', 'wasm/call-to-add-wasm.html');
-    });
-
-    await step('add a breakpoint to line No.5', async () => {
-      await addBreakpointForLine(frontend, 5);
-    });
-
-    await step('reload the page', async () => {
-      await target.reload();
-    });
-
-    await step('wait for all the source code to appear', async () => {
-      await waitFor(await sourceLineNumberSelector(5));
-    });
-
-    await checkBreakpointIsActive(5);
-
-    await step('check that the code has paused on the breakpoint at the correct script location', async () => {
-      const scriptLocation = await retrieveTopCallFrameWithoutResuming();
-      assert.deepEqual(scriptLocation, 'add.wasm:0x27');
-    });
-  });
-
-  // breakpoint activates upon refresh after it is removed
-  it.skip('[crbug.com/1073071]: does not hit the breakpoint after it is removed', async () => {
-    const {target, frontend} = getBrowserAndPages();
-
-    await step('navigate to a page and open the Sources tab', async () => {
-      await openSourceCodeEditorForFile('add.wasm', 'wasm/call-to-add-wasm.html');
-    });
-
-    await step('add a breakpoint to line No.5', async () => {
-      await addBreakpointForLine(frontend, 5);
-    });
-
-    await step('reload the page', async () => {
-      await target.reload();
-    });
-
-    await step('wait for all the source code to appear', async () => {
-      await waitFor(await sourceLineNumberSelector(5));
-    });
-
-    await checkBreakpointIsActive(5);
-
-    await step('remove the breakpoint from the fifth line', async () => {
-      await frontend.click(await sourceLineNumberSelector(5));
-    });
-
-    await step('reload the page', async () => {
-      await target.reload();
-    });
-
-    await step('wait for all the source code to appear', async () => {
-      await waitFor(await sourceLineNumberSelector(5));
-    });
-
-    await checkBreakpointIsNotActive(5);
-    await checkBreakpointDidNotActivate();
-  });
-
-  // breakpoint activates upon refresh after it is removed
-  it.skip('[crbug.com/1073071]: hits two breakpoints that are set and activated separately', async function() {
+  it('hits two breakpoints that are set and activated separately', async function() {
     const {target, frontend} = getBrowserAndPages();
 
     await step('navigate to a page and open the Sources tab', async () => {

19c6bc0fe056a2430b0530d16bbf980a14b7b65b
Mathias Bynens <mathias@chromium.org>
2020-06-26 12:28:25 +0200

    Add e2e test for global evaluation in Console
    This patch ports the following layout test:
    https://source.chromium.org/chromium/chromium/src/+/master:third_party/blink/web_tests/http/tests/devtools/console/console-eval-global.js;drc=78e2bc9c2def358a0745878ffc66ce85ee5221d8
    
    CL removing the upstream test:
    https://chromium-review.googlesource.com/c/chromium/src/+/2270179
    
    Bug: chromium:1099603
    Change-Id: I7560e9e85b83ac864f19c6da92acd46045ed5c0c
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2270163
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Commit-Queue: Mathias Bynens <mathias@chromium.org>

---
+++ test/e2e/console/console-eval-global_test.ts
@@ -0,0 +1,58 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {assert} from 'chai';
+import {describe, it} from 'mocha';
+
+import {click, getBrowserAndPages, pasteText, step} from '../../shared/helper.js';
+import {CONSOLE_TAB_SELECTOR, focusConsolePrompt} from '../helpers/console-helpers.js';
+import {getCurrentConsoleMessages} from '../helpers/console-helpers.js';
+
+describe('The Console Tab', async () => {
+  it('interacts with the global scope correctly', async () => {
+    const {frontend} = getBrowserAndPages();
+
+    await step('navigate to the Console tab', async () => {
+      await click(CONSOLE_TAB_SELECTOR);
+      await focusConsolePrompt();
+    });
+
+    await step('enter code that implicitly creates global properties', async () => {
+      await pasteText(`
+        var foo = 'fooValue';
+        var bar = {
+          a: 'b',
+        };
+      `);
+      await frontend.keyboard.press('Enter');
+
+      // Wait for the console to be usable again.
+      await frontend.waitForFunction(() => {
+        return document.querySelectorAll('.console-user-command-result').length === 1;
+      });
+    });
+
+    await step('enter code that references the created bindings', async () => {
+      await pasteText('foo;');
+      await frontend.keyboard.press('Enter');
+
+      // Wait for the console to be usable again.
+      await frontend.waitForFunction(() => {
+        return document.querySelectorAll('.console-user-command-result').length === 1;
+      });
+
+      await pasteText('bar;');
+      await frontend.keyboard.press('Enter');
+    });
+
+    await step('check that the expected output is logged', async () => {
+      const messages = await getCurrentConsoleMessages();
+      assert.deepEqual(messages, [
+        'undefined',
+        '"fooValue"',
+        '{a: "b"}',
+      ]);
+    });
+  });
+});

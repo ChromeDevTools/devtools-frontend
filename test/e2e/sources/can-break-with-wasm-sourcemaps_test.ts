e4f4d1bbaa9cec393c3b3b8bddb89ed79e21dcfe
Kim-Anh Tran <kimanh@chromium.org>
2020-07-06 13:37:57 +0200

    Enable wasm debugging e2e test
    This enables the e2e test after a blocking bug was fixed,
    and adds a missing step in the test. This also removes
    the tests that are duplicating parts of it.
    
    Bug: chromium:1091116
    Change-Id: I2798fc930bbc0ca2ed12b8352502e8ac073c8319
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2282514
    Reviewed-by: Benedikt Meurer <bmeurer@chromium.org>
    Commit-Queue: Kim-Anh Tran <kimanh@chromium.org>

--- test/e2e/sources/can-break-with-wasm-sourcemaps_test.ts
+++ test/e2e/sources/can-break-with-wasm-sourcemaps_test.ts
@@ -6,7 +6,7 @@ import {assert} from 'chai';
 import {describe, it} from 'mocha';
 
 import {getBrowserAndPages, step, waitFor} from '../../shared/helper.js';
-import {addBreakpointForLine, checkBreakpointDidNotActivate, checkBreakpointIsActive, checkBreakpointIsNotActive, openSourceCodeEditorForFile, retrieveTopCallFrameScriptLocation, retrieveTopCallFrameWithoutResuming, sourceLineNumberSelector} from '../helpers/sources-helpers.js';
+import {addBreakpointForLine, checkBreakpointDidNotActivate, checkBreakpointIsActive, checkBreakpointIsNotActive, openFileInEditor, openSourceCodeEditorForFile, retrieveTopCallFrameScriptLocation, retrieveTopCallFrameWithoutResuming, sourceLineNumberSelector} from '../helpers/sources-helpers.js';
 
 describe('The Sources Tab', async () => {
   it('can add breakpoint for a sourcemapped wasm module', async () => {
@@ -19,7 +19,7 @@ describe('The Sources Tab', async () => {
     assert.deepEqual(scriptLocation, 'with-sourcemap.ll:5');
   });
 
-  it('hits breakpoint upon refresh for a sourcemapped wasm module', async () => {
+  it('hits two breakpoints that are set and activated separately', async function() {
     const {target, frontend} = getBrowserAndPages();
 
     await step('navigate to a page and open the Sources tab', async () => {
@@ -42,76 +42,6 @@ describe('The Sources Tab', async () => {
 
     await step('check that the code has paused on the breakpoint at the correct script location', async () => {
       const scriptLocation = await retrieveTopCallFrameWithoutResuming();
-      // TODO(chromium:1043047): Switch to bytecode offsets here.
-      assert.deepEqual(scriptLocation, 'with-sourcemap.ll:5');
-    });
-  });
-
-  // breakpoint activates upon refresh after it is removed
-  it.skip(
-      '[crbug.com/1073071]: does not hit the breakpoint after it is removed for a sourcemapped wasm module',
-      async () => {
-        const {target, frontend} = getBrowserAndPages();
-
-        await step('navigate to a page and open the Sources tab', async () => {
-          await openSourceCodeEditorForFile('with-sourcemap.ll', 'wasm/wasm-with-sourcemap.html');
-        });
-
-        await step('add a breakpoint to line No.5', async () => {
-          await addBreakpointForLine(frontend, 5);
-        });
-
-        await step('reload the page', async () => {
-          await target.reload();
-        });
-
-        await step('wait for all the source code to appear', async () => {
-          await waitFor(await sourceLineNumberSelector(5));
-        });
-
-        await checkBreakpointIsActive(5);
-
-        await step('remove the breakpoint from the fifth line', async () => {
-          await frontend.click(await sourceLineNumberSelector(5));
-        });
-
-        await step('reload the page', async () => {
-          await target.reload();
-        });
-
-        await step('wait for all the source code to appear', async () => {
-          await waitFor(await sourceLineNumberSelector(5));
-        });
-
-        await checkBreakpointIsNotActive(5);
-        await checkBreakpointDidNotActivate();
-      });
-
-  // breakpoint activates upon refresh after it is removed
-  it.skip('[crbug.com/1073071]: hits two breakpoints that are set and activated separately', async function() {
-    const {target, frontend} = getBrowserAndPages();
-
-    await step('navigate to a page and open the Sources tab', async () => {
-      await openSourceCodeEditorForFile('with-sourcemap.ll', 'wasm/wasm-with-sourcemap.html');
-    });
-
-    await step('add a breakpoint to line No.5', async () => {
-      await addBreakpointForLine(frontend, 5);
-    });
-
-    await step('reload the page', async () => {
-      await target.reload();
-    });
-
-    await step('wait for all the source code to appear', async () => {
-      await waitFor(await sourceLineNumberSelector(5));
-    });
-
-    await checkBreakpointIsActive(5);
-
-    await step('check that the code has paused on the breakpoint at the correct script location', async () => {
-      const scriptLocation = await retrieveTopCallFrameWithoutResuming();
-      // TODO(chromium:1043047): Switch to bytecode offsets here.
       assert.deepEqual(scriptLocation, 'with-sourcemap.ll:5');
     });
 
@@ -121,6 +51,10 @@ describe('The Sources Tab', async () => {
 
     await step('reload the page', async () => {
       await target.reload();
+    });
+
+    await step('open original source file', async () => {
+      await openFileInEditor('with-sourcemap.ll');
     });
 
     await step('wait for all the source code to appear', async () => {
@@ -146,7 +80,6 @@ describe('The Sources Tab', async () => {
 
     await step('check that the code has paused on the breakpoint at the correct script location', async () => {
       const scriptLocation = await retrieveTopCallFrameWithoutResuming();
-      // TODO(chromium:1043047): Switch to bytecode offsets here.
       assert.deepEqual(scriptLocation, 'with-sourcemap.ll:6');
     });
   });

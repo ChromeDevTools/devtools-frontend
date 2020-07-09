6a1e363137c921caf3af563a608eae854ae29ac3
Ted Meyer <tmathmeyer@chromium.org>
2020-07-09 15:06:05 -0700

    Remove need for media flag for media e2e tests
    When I wrote the test, the chrome feature was still behind a flag.
    It isn't behind a flag anymore, so it should run always.
    
    Change-Id: Ide674bb7adf94cd5c0b194ede310c7452d96d0fe
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2290578
    Commit-Queue: Tim van der Lippe <tvanderlippe@chromium.org>
    Auto-Submit: Ted Meyer <tmathmeyer@chromium.org>
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>

--- test/e2e/media/media-tab_test.ts
+++ test/e2e/media/media-tab_test.ts
@@ -5,25 +5,11 @@
 import {assert} from 'chai';
 import {describe, it} from 'mocha';
 
-import {enableExperiment} from '../../shared/helper.js';
 import {getPlayerButtonText, playMediaFile} from '../helpers/media-helpers.js';
 import {openPanelViaMoreTools} from '../helpers/settings-helpers.js';
 
-function shouldRunTest() {
-  const features = process.env['CHROME_FEATURES'];
-  return features !== undefined && features.includes('MediaInspectorLogging');
-}
-
 describe('Media Tab', () => {
-  beforeEach(async () => {
-    await enableExperiment('mediaInspector');
-  });
-
   it('ensures video playback adds entry', async () => {
-    if (!shouldRunTest()) {
-      return;
-    }
-
     await openPanelViaMoreTools('Media');
     await playMediaFile('fisch.webm');
     const entryName = await getPlayerButtonText();

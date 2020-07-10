376000352f18ee3e93b554f495c66744d92e903b
Peter Marshall <petermarshall@chromium.org>
2020-07-10 14:29:37 +0200

    Add steps to console e2e test that is flaking on the mac bot
    I can't repro this locally so adding steps to see where it gets stuck.
    Also changing the typing to pasting to avoid autocomplete issues.
    
    Change-Id: I81f39adb5083f694b30e0139872f43fe814fcc16
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2292270
    Commit-Queue: Peter Marshall <petermarshall@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>

--- test/e2e/console/console-eval-blocked-by-CSP_test.ts
+++ test/e2e/console/console-eval-blocked-by-CSP_test.ts
@@ -4,63 +4,61 @@
 
 import {assert} from 'chai';
 import {describe, it} from 'mocha';
-import * as puppeteer from 'puppeteer';
 
-import {click, getBrowserAndPages, goToResource, typeText, waitFor, waitForNone} from '../../shared/helper.js';
+import {click, getBrowserAndPages, goToResource, pasteText, step} from '../../shared/helper.js';
 import {CONSOLE_TAB_SELECTOR, focusConsolePrompt} from '../helpers/console-helpers.js';
 
 describe('The Console Tab', async function() {
   it('eval in console succeeds for pages with no CSP', async () => {
     const {frontend} = getBrowserAndPages();
-    await click(CONSOLE_TAB_SELECTOR);
-    await focusConsolePrompt();
-
-    await typeEvalAndDismissAutocomplete(frontend);
-
-    await frontend.waitForFunction(() => {
-      return document.querySelectorAll('.console-user-command-result').length === 1;
+    await step('open the console tab and focus the prompt', async () => {
+      await click(CONSOLE_TAB_SELECTOR);
+      await focusConsolePrompt();
     });
 
-    const evaluateResult = await frontend.evaluate(() => {
-      return document.querySelectorAll('.console-user-command-result')[0].textContent;
-    });
-
-    assert.strictEqual(evaluateResult, '2', 'Eval result was not correct');
-  });
-
-  async function typeEvalAndDismissAutocomplete(frontend: puppeteer.Page) {
-    // See the comments in console-repl-mode_test to see why this is necessary.
-    const appearPromise = waitFor('.suggest-box');
-    await typeText('eval(');
-    await appearPromise;
-
-    const disappearPromise = waitForNone('.suggest-box');
-    await frontend.keyboard.press('Escape');
-    await disappearPromise;
-
-    await typeText('"1+1");');
+    await pasteText('eval("1+1")');
     await frontend.keyboard.press('Enter');
-  }
+
+    await step('wait for the result to appear in the console', async () => {
+      await frontend.waitForFunction(() => {
+        return document.querySelectorAll('.console-user-command-result').length === 1;
+      });
+    });
+
+    await step('get the result text from the console', async () => {
+      const evaluateResult = await frontend.evaluate(() => {
+        return document.querySelectorAll('.console-user-command-result')[0].textContent;
+      });
+      assert.strictEqual(evaluateResult, '2', 'Eval result was not correct');
+    });
+
+  });
 
   it('eval in console fails for pages with CSP that blocks eval', async () => {
     await goToResource('console/CSP-blocks-eval.html');
 
     const {frontend} = getBrowserAndPages();
-    await click(CONSOLE_TAB_SELECTOR);
-    await focusConsolePrompt();
-
-    await typeEvalAndDismissAutocomplete(frontend);
-
-    await frontend.waitForFunction(() => {
-      return document.querySelectorAll('.console-user-command-result').length === 1;
+    await step('open the console tab and focus the prompt', async () => {
+      await click(CONSOLE_TAB_SELECTOR);
+      await focusConsolePrompt();
     });
 
-    const evaluateResult = await frontend.evaluate(() => {
-      return document.querySelectorAll('.console-user-command-result')[0].textContent;
+    await pasteText('eval("1+1")');
+    await frontend.keyboard.press('Enter');
+
+    await step('wait for the result to appear in the console', async () => {
+      await frontend.waitForFunction(() => {
+        return document.querySelectorAll('.console-user-command-result').length === 1;
+      });
     });
 
-    assert.include(
-        evaluateResult || '', '\'unsafe-eval\' is not an allowed source of script',
-        'Didn\'t find expected CSP error message');
+    await step('get the result text from the console', async () => {
+      const evaluateResult = await frontend.evaluate(() => {
+        return document.querySelectorAll('.console-user-command-result')[0].textContent;
+      });
+      assert.include(
+          evaluateResult || '', '\'unsafe-eval\' is not an allowed source of script',
+          'Didn\'t find expected CSP error message');
+    });
   });
 });

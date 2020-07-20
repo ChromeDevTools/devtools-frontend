2c1e944aadb253e820ca93047638c276de3fe4e0
Jack Franklin <jacktfranklin@chromium.org>
2020-07-20 11:11:48 +0100

    Migrate `front_end/bindings` to `devtools_entrypoint`
    Bug: 1101738
    Change-Id: I0d43818334425984a160fa9b79076f7413a6a52b
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2306155
    Reviewed-by: Paul Lewis <aerotwist@chromium.org>
    Commit-Queue: Jack Franklin <jacktfranklin@chromium.org>

--- front_end/bindings/ResourceScriptMapping.js
+++ front_end/bindings/ResourceScriptMapping.js
@@ -27,6 +27,8 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+// @ts-nocheck
+// TODO(crbug.com/1011811): Enable TypeScript compiler checks
 
 import * as Common from '../common/common.js';
 import * as SDK from '../sdk/sdk.js';

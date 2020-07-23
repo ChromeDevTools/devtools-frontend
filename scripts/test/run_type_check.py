718ac79a118a5a4b76af9943b28807e05a3b205d
Mathias Bynens <mathias@chromium.org>
2020-07-23 18:24:53 +0200

    Integrate client variations parser
    This patch configures the Closure typings and makes it possible to use
    ClientVariationsParser.js in DevTools UI code.
    
    These changes have already been reviewed [1], but they are now split
    into a separate CL.
    
    [1]: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2316062
    
    TBR=alexrudenko@chromium.org,changhaohan@chromium.org
    
    Bug: chromium:1103854
    Change-Id: Ic94d9c93727f3b6251d684bf3960e3a9de8f1392
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2316071
    Reviewed-by: Mathias Bynens <mathias@chromium.org>
    Commit-Queue: Mathias Bynens <mathias@chromium.org>

--- scripts/test/run_type_check.py
+++ scripts/test/run_type_check.py
@@ -341,9 +341,13 @@ def prepare_closure_frontend_compile(temp_devtools_path, descriptors, namespace_
         if not generated_file in args:
             modular_build.write_file(generated_file, '')
             if os.path.basename(generated_file) in [
-                    'wasm_source_map.js', 'acorn.mjs', 'acorn-loose.mjs',
                     'acorn-logical-assignment.mjs',
-                    'acorn-numeric-separator.mjs', 'marked.esm.js'
+                    'acorn-loose.mjs',
+                    'acorn-numeric-separator.mjs',
+                    'acorn.mjs',
+                    'ClientVariationsParser.js',
+                    'marked.esm.js',
+                    'wasm_source_map.js',
             ]:
                 with open(
                         generated_file.replace('.js', '_types.js').replace(

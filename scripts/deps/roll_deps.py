4ceefeb36e0e63ad75834e9a921249d389aa1658
Mathias Bynens <mathias@chromium.org>
2020-07-24 12:04:49 +0200

    Add support for rolling ClientVariationsParser.js
    Bug: chromium:1103854
    Change-Id: Ie5cebb77290727aadf9e3074f4c95306119df17c
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2316074
    Reviewed-by: Mathias Bynens <mathias@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Changhao Han <changhaohan@chromium.org>
    Commit-Queue: Mathias Bynens <mathias@chromium.org>

--- scripts/deps/roll_deps.py
+++ scripts/deps/roll_deps.py
@@ -13,22 +13,34 @@ import shutil
 import subprocess
 import sys
 
+# Files whose location within devtools-frontend matches the upstream location.
 FILES = [
-    ['v8', 'include', 'js_protocol.pdl'],
-    ['third_party', 'blink', 'renderer', 'core', 'css', 'css_properties.json5'],
-    ['third_party', 'blink', 'renderer', 'core', 'html', 'aria_properties.json5'],
-    ['third_party', 'blink', 'public', 'devtools_protocol', 'browser_protocol.pdl'],
-    ['third_party', 'axe-core', 'axe.d.ts'],
-    ['third_party', 'axe-core', 'axe.js'],
-    ['third_party', 'axe-core', 'axe.min.js'],
-    ['third_party', 'axe-core', 'LICENSE'],
+    'v8/include/js_protocol.pdl',
+    'third_party/blink/renderer/core/css/css_properties.json5',
+    'third_party/blink/renderer/core/html/aria_properties.json5',
+    'third_party/blink/public/devtools_protocol/browser_protocol.pdl',
+    'third_party/axe-core/axe.d.ts',
+    'third_party/axe-core/axe.js',
+    'third_party/axe-core/axe.min.js',
+    'third_party/axe-core/LICENSE',
 ]
+
+# Files whose location within devtools-frontend differs from the upstream location.
+FILE_MAPPINGS = {
+    # chromium_path => devtools_frontend_path
+    'components/variations/proto/devtools/client_variations_parser.js':
+    'front_end/third_party/chromium/client-variations/ClientVariationsParser.js',
+}
+
+for f in FILES:
+    FILE_MAPPINGS[f] = f
 
 
 def parse_options(cli_args):
     parser = argparse.ArgumentParser(description='Roll dependencies from Chromium.')
-    parser.add_argument('chromium_dir', help='Chromium directory')
-    parser.add_argument('devtools_dir', help='DevTools directory')
+    parser.add_argument('chromium_dir', help='path to chromium/src directory')
+    parser.add_argument('devtools_dir',
+                        help='path to devtools/devtools-frontend directory')
     return parser.parse_args(cli_args)
 
 def update(options):
@@ -37,8 +49,12 @@ def update(options):
     subprocess.check_call(['gclient', 'sync'], cwd=options.chromium_dir)
 
 def copy_files(options):
-    for file in FILES:
-        shutil.copy(os.path.join(options.chromium_dir, *file), os.path.join(options.devtools_dir, *file))
+    for from_path, to_path in FILE_MAPPINGS.items():
+        from_path = os.path.normpath(from_path)
+        to_path = os.path.normpath(to_path)
+        print('%s => %s' % (from_path, to_path))
+        shutil.copy(os.path.join(options.chromium_dir, from_path),
+                    os.path.join(options.devtools_dir, to_path))
 
 
 if __name__ == '__main__':

af06661c518421604efa5631c385ace2cae96bd2
Paul Lewis <aerotwist@chromium.org>
2020-07-28 16:32:42 +0100

    Migrates extensions to devtools_entrypoint
    R=jacktfranklin@chromium.org
    
    Bug: 1101738
    Change-Id: If1989c83c856b2c8d60aa4c31a0c1ff5df66bb02
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2323626
    Reviewed-by: Jack Franklin <jacktfranklin@chromium.org>
    Commit-Queue: Paul Lewis <aerotwist@chromium.org>

--- front_end/extensions/ExtensionPanel.js
+++ front_end/extensions/ExtensionPanel.js
@@ -28,6 +28,9 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+// @ts-nocheck
+// TODO(crbug.com/1011811): Enable TypeScript compiler checks
+
 import * as ProtocolClient from '../protocol_client/protocol_client.js';  // eslint-disable-line no-unused-vars
 import * as SDK from '../sdk/sdk.js';
 import * as UI from '../ui/ui.js';
--- front_end/extensions/ExtensionServer.js
+++ front_end/extensions/ExtensionServer.js
@@ -28,6 +28,9 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+// @ts-nocheck
+// TODO(crbug.com/1011811): Enable TypeScript compiler checks
+
 import * as Bindings from '../bindings/bindings.js';
 import * as Common from '../common/common.js';
 import * as Components from '../components/components.js';

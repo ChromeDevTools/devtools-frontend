125702be66ebbda950ac0fca029ca651d9bec2f9
Philip Pfaffe <pfaffe@chromium.org>
2020-07-29 10:51:39 +0200

    Aria-label the scope entries
    This CL assigns an aria-label to list entries in the scope view. The
    label is attached to the actual list item and not the span inside of
    that whose textContent contains the name currently. The textContent
    sometimes is just the name and sometimes both the name and the type.
    
    Change-Id: Ia4f916057ed9c049c7b72537822ac123149f13d3
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2325216
    Commit-Queue: Philip Pfaffe <pfaffe@chromium.org>
    Reviewed-by: Mathias Bynens <mathias@chromium.org>

--- front_end/object_ui/ObjectPropertiesSection.js
+++ front_end/object_ui/ObjectPropertiesSection.js
@@ -615,6 +615,7 @@ export class ObjectPropertyTreeElement extends UI.TreeOutline.TreeElement {
     this._linkifier = linkifier;
     this._maxNumPropertiesToShow = InitialVisibleChildrenLimit;
     this.listItemElement.addEventListener('contextmenu', this._contextMenuFired.bind(this), false);
+    UI.ARIAUtils.setAccessibleName(this.listItemElement, this.property.name);
   }
 
   /**

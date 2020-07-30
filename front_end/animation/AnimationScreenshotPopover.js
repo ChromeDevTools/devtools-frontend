899893a60cc540b0b41953b884e5883f615aecf4
Jack Franklin <jacktfranklin@chromium.org>
2020-07-30 15:01:39 +0100

    Migrate animation to devtools_entrypoint
    Bug: 1101738
    Change-Id: I05f447baa9854b776821d97d6bde76a4f6d223a7
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2329771
    Commit-Queue: Jack Franklin <jacktfranklin@chromium.org>
    Reviewed-by: Jan Scheffler <janscheffler@chromium.org>
    Auto-Submit: Jack Franklin <jacktfranklin@chromium.org>

--- front_end/animation/AnimationScreenshotPopover.js
+++ front_end/animation/AnimationScreenshotPopover.js
@@ -1,6 +1,8 @@
 // Copyright (c) 2015 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+// @ts-nocheck
+// TODO(crbug.com/1011811): Enable TypeScript compiler checks
 
 import * as UI from '../ui/ui.js';
 
--- front_end/animation/AnimationUI.js
+++ front_end/animation/AnimationUI.js
@@ -1,6 +1,8 @@
 // Copyright (c) 2015 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+// @ts-nocheck
+// TODO(crbug.com/1011811): Enable TypeScript compiler checks
 
 import * as Common from '../common/common.js';
 import * as InlineEditor from '../inline_editor/inline_editor.js';

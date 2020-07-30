f168ba416498c1c897fd0a65520eebef6cf00093
Jan Scheffler <janscheffler@chromium.org>
2020-07-30 17:29:47 +0200

    [TypeScript] Migrate `front_end/timeline` to `devtools_entrypoint`
    Bug: 1101738
    Change-Id: Ida2ba236c097adfdbbeadae06cc844f7446073c3
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2307312
    Commit-Queue: Jan Scheffler <janscheffler@chromium.org>
    Reviewed-by: Paul Lewis <aerotwist@chromium.org>

--- front_end/timeline/ExtensionTracingSession.js
+++ front_end/timeline/ExtensionTracingSession.js
@@ -1,6 +1,8 @@
 // Copyright 2016 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+// @ts-nocheck
+// TODO(crbug.com/1011811): Enable TypeScript compiler checks
 
 import * as Extensions from '../extensions/extensions.js';  // eslint-disable-line no-unused-vars
 import * as SDK from '../sdk/sdk.js';                       // eslint-disable-line no-unused-vars
--- front_end/timeline/TimelineFlameChartView.js
+++ front_end/timeline/TimelineFlameChartView.js
@@ -1,6 +1,8 @@
 // Copyright 2016 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+// @ts-nocheck
+// TODO(crbug.com/1011811): Enable TypeScript compiler checks
 
 import * as Bindings from '../bindings/bindings.js';
 import * as Common from '../common/common.js';

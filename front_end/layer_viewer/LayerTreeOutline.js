6a97284aa28f86a6adb6b6675f76c2dff3255fd0
Jan Scheffler <janscheffler@chromium.org>
2020-07-30 12:25:47 +0200

    Migrate `front_end/layer_viewer` to `devtools_entrypoint`
    Bug: 1101738
    Change-Id: I9572585fe67702098a5774870993c8da4cb55c17
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2328851
    Commit-Queue: Jan Scheffler <janscheffler@chromium.org>
    Reviewed-by: Jack Franklin <jacktfranklin@chromium.org>

--- front_end/layer_viewer/LayerTreeOutline.js
+++ front_end/layer_viewer/LayerTreeOutline.js
@@ -28,6 +28,9 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+// @ts-nocheck
+// TODO(crbug.com/1011811): Enable TypeScript compiler checks
+
 import * as Common from '../common/common.js';
 import * as SDK from '../sdk/sdk.js';  // eslint-disable-line no-unused-vars
 import * as UI from '../ui/ui.js';
--- front_end/layer_viewer/Layers3DView.js
+++ front_end/layer_viewer/Layers3DView.js
@@ -27,6 +27,10 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+
+// @ts-nocheck
+// TODO(crbug.com/1011811): Enable TypeScript compiler checks
+
 import * as Common from '../common/common.js';
 import * as Platform from '../platform/platform.js';
 import * as SDK from '../sdk/sdk.js';  // eslint-disable-line no-unused-vars

340bd16cd3f1c6305f3a63c6e6fa426015ab67f8
Sigurd Schneider <sigurds@chromium.org>
2020-08-03 14:46:52 +0000

    Revert "Reland "Updates elements to devtools_module""
    This reverts commit 04858cf7dd5fbf83898bf3069918ff1b2c88e034.
    
    Reason for revert: This seems to trigger the known issue that 
    TSC is rewriting source files because of left-over hard-links.
    See https://ci.chromium.org/p/devtools-frontend/builders/ci/Stand-alone%20Linux/2582
    
    To reproduce locally, check out the commit before this revision,
    do a clean build, then check out this revision, and do a build.
    This will succeed but rewrite source files (which it shouldn't).
    
    
    Original change's description:
    > Reland "Updates elements to devtools_module"
    > 
    > This reverts commit 22d876ca02787c9fbf48a7b3f4e6a9185fb37858.
    > 
    > Reason for revert: Build issues on bots should be fixed
    > 
    > Original change's description:
    > > Revert "Updates elements to devtools_module"
    > > 
    > > This reverts commit 81e818fad86b7fcfe3629070494f7258b6000924.
    > > 
    > > Reason for revert: Anticipating build errors on bots
    > > 
    > > Original change's description:
    > > > Updates elements to devtools_module
    > > > 
    > > > Bug: 1101738
    > > > Change-Id: I294d366c95a3b5dc7ac5b757855846a7b31ac60b
    > > > Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2329779
    > > > Reviewed-by: Jack Franklin <jacktfranklin@chromium.org>
    > > > Commit-Queue: Paul Lewis <aerotwist@chromium.org>
    > > 
    > > TBR=aerotwist@chromium.org,jacktfranklin@chromium.org
    > > 
    > > Change-Id: I3e36a1520e34be3ce1779ecdf2b4cead6a293971
    > > No-Presubmit: true
    > > No-Tree-Checks: true
    > > No-Try: true
    > > Bug: 1101738
    > > Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2332815
    > > Reviewed-by: Paul Lewis <aerotwist@chromium.org>
    > > Commit-Queue: Paul Lewis <aerotwist@chromium.org>
    > 
    > TBR=aerotwist@chromium.org,jacktfranklin@chromium.org
    > 
    > # Not skipping CQ checks because this is a reland.
    > 
    > Bug: 1101738
    > Change-Id: Ia0d06bb75f2610459f4d0b31ff6b770b1031d982
    > Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2335060
    > Reviewed-by: Paul Lewis <aerotwist@chromium.org>
    > Reviewed-by: Jack Franklin <jacktfranklin@chromium.org>
    > Commit-Queue: Paul Lewis <aerotwist@chromium.org>
    
    TBR=aerotwist@chromium.org,jacktfranklin@chromium.org
    
    Change-Id: Iabfbe2aad94033078eac41338d64ca069f49332f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 1101738
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2335273
    Reviewed-by: Sigurd Schneider <sigurds@chromium.org>
    Commit-Queue: Sigurd Schneider <sigurds@chromium.org>

--- front_end/elements/ElementsPanel.js
+++ front_end/elements/ElementsPanel.js
@@ -28,9 +28,6 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-// @ts-nocheck
-// TODO(crbug.com/1011811): Enable TypeScript compiler checks
-
 import * as Common from '../common/common.js';
 import * as Components from '../components/components.js';
 import * as Extensions from '../extensions/extensions.js';

fe1bb43b7a385f2d442ed6c823ebf3cfaa2d2288
Simon Zünd <szuend@chromium.org>
2020-08-11 08:52:20 +0200

    [ts] Type check ui/ActionDelegate.js with TypeScript
    R=jacktfranklin@chromium.org
    
    Bug: chromium:1011811
    Change-Id: I8e10a80de5a17adad2abec65bed42ce1a17d3bad
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2346381
    Reviewed-by: Jack Franklin <jacktfranklin@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>

--- front_end/ui/ActionDelegate.js
+++ front_end/ui/ActionDelegate.js
@@ -2,9 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// @ts-nocheck
-// TODO(crbug.com/1011811): Enable TypeScript compiler checks
-
 import {Context} from './Context.js';  // eslint-disable-line no-unused-vars
 
 /**
@@ -17,5 +14,6 @@ export class ActionDelegate {
    * @return {boolean}
    */
   handleAction(context, actionId) {
+    throw new Error('not implemented');
   }
 }

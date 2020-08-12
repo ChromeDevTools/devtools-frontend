1bc65e62d399d0be4a767a8875552966633eebca
Simon Zünd <szuend@chromium.org>
2020-08-12 09:42:24 +0200

    [ts] Remove @ts-nocheck in 'ui' files where its not needed
    TBR=jacktfranklin@chromium.org
    
    Bug: chromium:1011811
    Change-Id: I6c2480e67a29765a2ef67ae85202532917a4d718
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2351773
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>

--- front_end/ui/EmptyWidget.js
+++ front_end/ui/EmptyWidget.js
@@ -28,9 +28,6 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-// @ts-nocheck
-// TODO(crbug.com/1011811): Enable TypeScript compiler checks
-
 import {VBox} from './Widget.js';
 import {XLink} from './XLink.js';
 

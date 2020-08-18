fb33bb85a9f76785f3edc12bcaae3c57d075da29
Brandon Goddard <brgoddar@microsoft.com>
2020-08-18 08:38:49 -0700

    Performance: Fix perf issue with Event Log
    This CL fixes a performance issue that occurs in the event log.
    Attempting to select the first node on a tree refresh (that occurrs after
    toggling one of the checkboxes) was creating a selection event
    that caused a repeated chain of rebuilding the event tree.
    Suppressing the selection event still produces the desired behavior:
    the first element is selected when the tree is rebuilt after
    changing the event listener filters, but no longer enters a massive
    recursive loop.
    
    Before is in bug
    After: https://imgur.com/NUrTwUj
    
    Bug: 1112663
    Change-Id: Ica51890eeb23234dcb60d59ebfc2815165d16979
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2358069
    Reviewed-by: Jose Leal <joselea@microsoft.com>
    Reviewed-by: Junyi Xiao <juxiao@microsoft.com>
    Commit-Queue: Brandon Goddard <brgoddar@microsoft.com>

--- front_end/timeline/TimelineTreeView.js
+++ front_end/timeline/TimelineTreeView.js
@@ -283,7 +283,7 @@ export class TimelineTreeView extends UI.Widget.VBox {
     }
     const rootNode = this._dataGrid.rootNode();
     if (rootNode.children.length > 0) {
-      rootNode.children[0].select();
+      rootNode.children[0].select(/* supressSelectedEvent */ true);
     }
   }
 
@@ -715,7 +715,7 @@ export class AggregatedTimelineTreeView extends TimelineTreeView {
     super.updateContents(selection);
     const rootNode = this._dataGrid.rootNode();
     if (rootNode.children.length) {
-      rootNode.children[0].select();
+      rootNode.children[0].select(/* suppressSelectedEvent */ true);
     }
   }
 

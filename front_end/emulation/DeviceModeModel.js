96f17dbdf633f466e2a6cfffb4d594181c73116d
Songtao Xia <soxia@microsoft.com>
2020-08-21 14:46:32 -0700

    [DevTools]Fix issue where emulated dual screen's hinge is gone
    When we navigate to a new page, the hinge of a dual screen device may be
    gone. It will reappear.
    
    Fix will be add a listener to show hinge if applicable when FrameNavigated.
    
    Bug: 1120800
    Change-Id: I2d3d41c6e745774c2d805a61b870a39eb23f8ad6
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2368770
    Reviewed-by: Jack Lynch <jalyn@microsoft.com>
    Commit-Queue: Songtao Xia <soxia@microsoft.com>
    (cherry picked from commit 090e3d5a3680376e1f743ea572872b5d506a3989)
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2383852
    Reviewed-by: Songtao Xia <soxia@microsoft.com>
    Reviewed-by: Yang Guo <yangguo@chromium.org>

--- front_end/emulation/DeviceModeModel.js
+++ front_end/emulation/DeviceModeModel.js
@@ -419,7 +419,8 @@ export class DeviceModeModel extends Common.ObjectWrapper.ObjectWrapper {
       }
       const resourceTreeModel = emulationModel.target().model(SDK.ResourceTreeModel.ResourceTreeModel);
       if (resourceTreeModel) {
-        resourceTreeModel.addEventListener(SDK.ResourceTreeModel.Events.FrameResized, this._onFrameResized, this);
+        resourceTreeModel.addEventListener(SDK.ResourceTreeModel.Events.FrameResized, this._onFrameChange, this);
+        resourceTreeModel.addEventListener(SDK.ResourceTreeModel.Events.FrameNavigated, this._onFrameChange, this);
       }
     } else {
       emulationModel.emulateTouch(this._touchEnabled, this._touchMobile);
@@ -443,7 +444,7 @@ export class DeviceModeModel extends Common.ObjectWrapper.ObjectWrapper {
     return this._emulationModel ? this._emulationModel.target().inspectedURL() : null;
   }
 
-  _onFrameResized() {
+  _onFrameChange() {
     const overlayModel = this._emulationModel ? this._emulationModel.overlayModel() : null;
     if (!overlayModel) {
       return;

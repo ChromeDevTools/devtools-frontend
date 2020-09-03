ed9c15fc40f4f94576088ced30a0fdeb8920c5b1
Wolfgang Beyer <wolfi@chromium.org>
2020-09-03 15:03:44 +0200

    Reland of "Fix context menu for issues banner"
    Reland fixes e2e-test for MacOS
    
    Original CL: https://crrev.com/c/2382862
    Revert: https://crrev.com/c/2391248
    
    Bug: chromium:1123455
    Change-Id: If827a8656f2e09b90f7d9f5b84719beac466aa82
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2391228
    Reviewed-by: Sigurd Schneider <sigurds@chromium.org>
    Commit-Queue: Wolfgang Beyer <wolfi@chromium.org>

--- test/e2e/console/issues-toolbar_test.ts
+++ test/e2e/console/issues-toolbar_test.ts
@@ -4,18 +4,34 @@
 
 import {assert} from 'chai';
 
-import {goToResource, waitFor} from '../../shared/helper.js';
+import {click, goToResource, waitFor} from '../../shared/helper.js';
 import {describe, it} from '../../shared/mocha-extensions.js';
 import {navigateToConsoleTab} from '../helpers/console-helpers.js';
 
 describe('The Console Tab', async () => {
   it('shows infobar with button linking to issues tab', async () => {
-    // navigate to page which causes a SameSiteCookieIssue
+    // Navigate to page which causes a SameSiteCookieIssue.
     await goToResource('console/cookie-issue.html');
     await navigateToConsoleTab();
 
     const infobarButton = await waitFor('.infobar .infobar-button');
     const infobarButtonText = await infobarButton.evaluate(node => node.textContent);
     assert.strictEqual(infobarButtonText, 'View issues');
+  });
+
+  it('issues bar has a context menu', async () => {
+    // Navigate to page which causes a SameSiteCookieIssue.
+    await goToResource('console/cookie-issue.html');
+    await navigateToConsoleTab();
+
+    // Find the infobar and right-click it.
+    const selectedNode = await waitFor('.infobar-issue');
+    await click(selectedNode, {clickOptions: {button: 'right'}});
+
+    // Wait for the context menu and compare its content.
+    const contextMenu = await waitFor('.soft-context-menu');
+    const actual = await contextMenu.evaluate(e => e.textContent);
+    assert.include(actual, 'Clear console history');
+    assert.include(actual, 'Save as...');
   });
 });

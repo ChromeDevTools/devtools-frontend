d7d5a2b400a7ff6bcab867cbafd457c22031c490
Wolfgang Beyer <wolfi@chromium.org>
2020-09-03 15:03:44 +0200

    Reland of "Fix context menu for issues banner"
    Reland fixes e2e-test for MacOS
    
    Original CL: https://crrev.com/c/2382862
    Revert: https://crrev.com/c/2391248
    
    Bug: chromium:1123455
    Change-Id: If827a8656f2e09b90f7d9f5b84719beac466aa82
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2391228
    Reviewed-by: Sigurd Schneider <sigurds@chromium.org>
    Commit-Queue: Wolfgang Beyer <wolfi@chromium.org>
    (cherry picked from commit ed9c15fc40f4f94576088ced30a0fdeb8920c5b1)
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2398818
    Reviewed-by: Simon Zünd <szuend@chromium.org>

--- front_end/console/ConsoleView.js
+++ front_end/console/ConsoleView.js
@@ -333,6 +333,7 @@ export class ConsoleView extends UI.Widget.VBox {
         fastHeight() {
           return this._cachedIssueBarHeight || 37;
         },
+        toExportString: () => ls`Some messages have been moved to the Issues panel.`,
         _cachedIssueBarHeight: 0
       };
       this._scheduleViewportRefresh();
@@ -829,7 +830,7 @@ export class ConsoleView extends UI.Widget.VBox {
     }
 
     const sourceElement = eventTarget.enclosingNodeOrSelfWithClass('console-message-wrapper');
-    const consoleMessage = sourceElement ?
+    const consoleMessage = (sourceElement && 'message' in sourceElement) ?
         // @ts-expect-error We can't convert this to a Weakmap, as it comes from `ConsoleViewMessage` instead.
         /** @type {!ConsoleViewMessage} */ (sourceElement.message).consoleMessage() :
         null;

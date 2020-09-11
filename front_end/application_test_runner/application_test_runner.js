374ec554471b5c119c034a65e6074aa8331e29c1
Tim van der Lippe <tvanderlippe@chromium.org>
2020-09-11 14:14:24 +0100

    Load application_test_runner as module
    R=jacktfranklin@chromium.org
    
    Bug: 1105476
    Change-Id: I0541e1ecd27b5518029a1144f4515f05fa2140f8
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2406157
    Auto-Submit: Tim van der Lippe <tvanderlippe@chromium.org>
    Commit-Queue: Jack Franklin <jacktfranklin@chromium.org>
    Reviewed-by: Jack Franklin <jacktfranklin@chromium.org>

---
+++ front_end/application_test_runner/application_test_runner.js
@@ -0,0 +1,10 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import './AppcacheTestRunner.js';
+import './CacheStorageTestRunner.js';
+import './IndexedDBTestRunner.js';
+import './ResourceTreeTestRunner.js';
+import './ResourcesTestRunner.js';
+import './ServiceWorkersTestRunner.js';

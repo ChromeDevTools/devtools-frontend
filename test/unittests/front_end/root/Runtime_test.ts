80c85eb47bfeea1d99061074c81ff88e2bb80ef5
Tim van der Lippe <tvanderlippe@chromium.org>
2020-09-11 13:01:10 +0100

    Add unittests for Root.Runtime.Module.substituteURL
    This replaces third_party/blink/web_tests/http/tests/devtools/runtime.js
    as unittest.
    
    R=aerotwist@chromium.org
    
    Change-Id: I080a58d0407b7011b1721175c897b4b7bfabb0a7
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2406154
    Commit-Queue: Tim van der Lippe <tvanderlippe@chromium.org>
    Reviewed-by: Paul Lewis <aerotwist@chromium.org>

---
+++ test/unittests/front_end/root/Runtime_test.ts
@@ -0,0 +1,62 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import * as Root from '../../../../front_end/root/root.js';
+
+const {assert} = chai;
+
+const BASE_MODULE_NAME = 'testModule';
+const BASE_URL = `${window.location.origin}/${BASE_MODULE_NAME}/`;
+
+describe('Runtime', () => {
+  describe('Module', () => {
+    let module: Root.Runtime.Module;
+    before(() => {
+      module = new Root.Runtime.Module(Root.Runtime.Runtime.instance({forceNew: true, moduleDescriptors: []}), {
+        name: BASE_MODULE_NAME,
+        extensions: [],
+        dependencies: [],
+        scripts: [],
+        modules: [],
+        resources: [],
+        condition: undefined,
+        remote: false,
+        experiment: null,
+      });
+    });
+
+    describe('can substitute URLs', () => {
+      it('as regular string', () => {
+        const regularString = 'no url here';
+        assert.strictEqual(regularString, module.substituteURL(regularString));
+      });
+
+      it('with empty @url', () => {
+        assert.strictEqual(BASE_URL, module.substituteURL('@url()'));
+      });
+
+      it('with single file', () => {
+        assert.strictEqual(`${BASE_URL}file.js`, module.substituteURL('@url(file.js)'));
+      });
+
+      it('with surrounding text', () => {
+        assert.strictEqual(
+            `before ${BASE_URL}long/path/to/the/file.png after`,
+            module.substituteURL('before @url(long/path/to/the/file.png) after'));
+      });
+
+      it('with multiple URLs', () => {
+        assert.strictEqual(
+            `${BASE_URL}first.png ${BASE_URL}second.gif`, module.substituteURL('@url(first.png) @url(second.gif)'));
+      });
+
+      it('with multiple URLs with text around', () => {
+        assert.strictEqual(
+            `a lot of ${BASE_URL}stuff in a ${BASE_URL}singleline and more url() @@url (not/a/resource.gif)`,
+            module.substituteURL(
+                'a lot of @url(stuff) in a @url(single)line and more url() @@url (not/a/resource.gif)'));
+      });
+    });
+  });
+});

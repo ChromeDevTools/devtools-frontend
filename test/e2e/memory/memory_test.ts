8ed34138a32bd6cf6830112e28e0da4c44a53b30
Jack Franklin <jacktfranklin@chromium.org>
2020-09-11 15:36:56 +0100

    Migrate detached DOM tree heap layout test to e2e
    Original layout test here:
    https://source.chromium.org/chromium/chromium/src/+/master:third_party/blink/web_tests/inspector-protocol/heap-profiler/heap-snapshot-with-detached-dom-tree.js
    
    Bug: 1110817
    Change-Id: I1ed1808e89cd6ff63c5aded69c058777689bf549
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2406158
    Commit-Queue: Jack Franklin <jacktfranklin@chromium.org>
    Reviewed-by: Sigurd Schneider <sigurds@chromium.org>

--- test/e2e/memory/memory_test.ts
+++ test/e2e/memory/memory_test.ts
@@ -87,4 +87,13 @@ describe('The Memory Panel', async () => {
       return pendingActiviesChildren.length === 2;
     });
   });
+
+  it('Shows the correct number of divs for a detached DOM tree correctly', async () => {
+    await goToResource('memory/detached-dom-tree.html');
+    await navigateToMemoryTab();
+    await takeHeapSnapshot();
+    await waitForNonEmptyHeapSnapshotData();
+    await setSearchFilter('Detached HTMLDivElement');
+    await waitForSearchResultNumber(3);
+  });
 });

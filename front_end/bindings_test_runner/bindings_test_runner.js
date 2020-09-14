538dd8ec5b105efd4066d7c3733ab26d72ceb353
Tim van der Lippe <tvanderlippe@chromium.org>
2020-09-14 13:16:04 +0100

    Load bindings_test_runner as module
    R=jacktfranklin@chromium.org
    
    Bug: 1105476
    Change-Id: I775ae51418e9c325edc9d1a310cc27fdc97567f0
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2409981
    Auto-Submit: Tim van der Lippe <tvanderlippe@chromium.org>
    Commit-Queue: Jack Franklin <jacktfranklin@chromium.org>
    Reviewed-by: Jack Franklin <jacktfranklin@chromium.org>

---
+++ front_end/bindings_test_runner/bindings_test_runner.js
@@ -0,0 +1,9 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import './BindingsTestRunner.js';
+import './IsolatedFilesystemTestRunner.js';
+import './AutomappingTestRunner.js';
+import './PersistenceTestRunner.js';
+import './OverridesTestRunner.js';

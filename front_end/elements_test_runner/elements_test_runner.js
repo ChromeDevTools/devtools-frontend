8a8e01b0d845315735f4ca28b6077dde00a1257e
Tim van der Lippe <tvanderlippe@chromium.org>
2020-09-14 13:57:33 +0100

    Load elements_test_runner as module
    R=jacktfranklin@chromium.org
    
    Bug: 1105476
    Change-Id: I193aadf389c20e02bfc27995d8ac7f4dfea021db
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2409987
    Commit-Queue: Tim van der Lippe <tvanderlippe@chromium.org>
    Reviewed-by: Jack Franklin <jacktfranklin@chromium.org>

---
+++ front_end/elements_test_runner/elements_test_runner.js
@@ -0,0 +1,9 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import './ElementsTestRunner.js';
+import './EditDOMTestRunner.js';
+import './SetOuterHTMLTestRunner.js';
+import './ElementsPanelShadowSelectionOnRefreshTestRunner.js';
+import './StylesUpdateLinksTestRunner.js';

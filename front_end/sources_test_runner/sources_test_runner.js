85af018334e7a07fbdb8bca516aed78a61e6b39a
Tim van der Lippe <tvanderlippe@chromium.org>
2020-09-14 14:45:07 +0100

    Load sources_test_runner as module
    R=jacktfranklin@chromium.org
    
    Bug: 1105476
    Change-Id: Ic4ebf18e6fd3d8c2a74c4ce0b42e6c74f3bd0c9e
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2410218
    Commit-Queue: Tim van der Lippe <tvanderlippe@chromium.org>
    Reviewed-by: Jack Franklin <jacktfranklin@chromium.org>

---
+++ front_end/sources_test_runner/sources_test_runner.js
@@ -0,0 +1,10 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import './SourcesTestRunner.js';
+import './DebuggerTestRunner.js';
+import './LiveEditTestRunner.js';
+import './SearchTestRunner.js';
+import './EditorTestRunner.js';
+import './AutocompleteTestRunner.js';

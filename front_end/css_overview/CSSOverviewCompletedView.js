3dcefe4832ce8bb466621e18d15736c39772491d
Andres Olivares <andoli@chromium.org>
2020-09-14 11:15:42 -0500

    [ts] Typecheck css_overview/CSSOverviewCompletedView.js with TypeScript
    Bug: chromium:1011811
    Change-Id: I15140b5ada12ca6085fdf0a725c8d131dbce4498
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2406193
    Commit-Queue: Andres Olivares <andoli@chromium.org>
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>

--- front_end/css_overview/CSSOverviewCompletedView.js
+++ front_end/css_overview/CSSOverviewCompletedView.js
@@ -2,9 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// @ts-nocheck
-// TODO(crbug.com/1011811): Enable TypeScript compiler checks
-
 import * as Common from '../common/common.js';
 import * as Components from '../components/components.js';
 import * as DataGrid from '../data_grid/data_grid.js';
@@ -12,13 +9,58 @@ import * as SDK from '../sdk/sdk.js';
 import * as TextUtils from '../text_utils/text_utils.js';
 import * as UI from '../ui/ui.js';
 
-import {Events} from './CSSOverviewController.js';
+import {Events, OverviewController} from './CSSOverviewController.js';  // eslint-disable-line no-unused-vars
 import {CSSOverviewSidebarPanel, SidebarEvents} from './CSSOverviewSidebarPanel.js';
+import {UnusedDeclaration} from './CSSOverviewUnusedDeclarations.js';  // eslint-disable-line no-unused-vars
+
+/**
+ * @typedef {!Map<string,!Set<number>>}
+ */
+// @ts-ignore typedef
+export let NodeStyleStats;
+
+/**
+ * @typedef {{
+ * backgroundColors: NodeStyleStats,
+ * textColors: NodeStyleStats,
+ * fillColors: NodeStyleStats,
+ * borderColors: NodeStyleStats,
+ * globalStyleStats: !{
+ *  styleRules: number,
+ *  inlineStyles: number,
+ *  externalSheets: number,
+ *  stats: !{
+ *    type: number,
+ *    class: number,
+ *    id: number,
+ *    universal: number,
+ *    attribute: number,
+ *    nonSimple: number
+ *  }
+ * },
+ * fontInfo:  FontInfo,
+ * elementCount: number,
+ * mediaQueries: !Map<string, !Array<!Protocol.CSS.CSSMedia>>,
+ * unusedDeclarations: !Map<string, !Array<!UnusedDeclaration>>,
+ * }}
+ */
+// @ts-ignore typedef
+export let OverviewData;
+
+/**
+ * @typedef {!Map<string, !Map<string, !Map<string, !Array<number>>>>}
+ */
+// @ts-ignore typedef
+export let FontInfo;
 
 /**
  * @unrestricted
  */
 export class CSSOverviewCompletedView extends UI.Panel.PanelWithSidebar {
+  /**
+   * @param {!OverviewController} controller
+   * @param {!SDK.SDKModel.Target} target
+   */
   constructor(controller, target) {
     super('css_overview_completed_view');
     this.registerRequiredCSS('css_overview/cssOverviewCompletedView.css');
@@ -83,8 +125,12 @@ export class CSSOverviewCompletedView extends UI.Panel.PanelWithSidebar {
     // TODO(paullewis): update the links in the panels in case source has been .
   }
 
+  /**
+   * @param {!Common.EventTarget.EventTargetEvent} event
+   */
   _sideBarItemSelected(event) {
-    const section = this._fragment.$(event.data);
+    const data = /** @type {string} */ (event.data);
+    const section = /** @type {!UI.Fragment.Fragment}*/ (this._fragment).$(data);
     if (!section) {
       return;
     }
@@ -103,17 +149,26 @@ export class CSSOverviewCompletedView extends UI.Panel.PanelWithSidebar {
     this._viewMap = new Map();
   }
 
+  /**
+   * @param {!Event} evt
+   */
   _onClick(evt) {
-    const type = evt.target.dataset.type;
-    if (!type) {
+    if (!evt.target) {
+      return;
+    }
+    const target = /** @type {!HTMLElement} */ (evt.target);
+    const dataset = target.dataset;
+
+    const type = dataset.type;
+    if (!type || !this._data) {
       return;
     }
 
     let payload;
     switch (type) {
       case 'color': {
-        const color = evt.target.dataset.color;
-        const section = evt.target.dataset.section;
+        const color = dataset.color;
+        const section = dataset.section;
         if (!color) {
           return;
         }
@@ -148,7 +203,10 @@ export class CSSOverviewCompletedView extends UI.Panel.PanelWithSidebar {
       }
 
       case 'unused-declarations': {
-        const declaration = evt.target.dataset.declaration;
+        const declaration = dataset.declaration;
+        if (!declaration) {
+          return;
+        }
         const nodes = this._data.unusedDeclarations.get(declaration);
         if (!nodes) {
           return;
@@ -159,7 +217,10 @@ export class CSSOverviewCompletedView extends UI.Panel.PanelWithSidebar {
       }
 
       case 'media-queries': {
-        const text = evt.target.dataset.text;
+        const text = dataset.text;
+        if (!text) {
+          return;
+        }
         const nodes = this._data.mediaQueries.get(text);
         if (!nodes) {
           return;
@@ -170,9 +231,27 @@ export class CSSOverviewCompletedView extends UI.Panel.PanelWithSidebar {
       }
 
       case 'font-info': {
-        const value = evt.target.dataset.value;
-        const [fontFamily, fontMetric] = evt.target.dataset.path.split('/');
-        const nodesIds = this._data.fontInfo.get(fontFamily).get(fontMetric).get(value);
+        const value = dataset.value;
+        if (!dataset.path) {
+          return;
+        }
+
+        const [fontFamily, fontMetric] = dataset.path.split('/');
+        if (!value) {
+          return;
+        }
+
+        const fontFamilyInfo = this._data.fontInfo.get(fontFamily);
+        if (!fontFamilyInfo) {
+          return;
+        }
+
+        const fontMetricInfo = fontFamilyInfo.get(fontMetric);
+        if (!fontMetricInfo) {
+          return;
+        }
+
+        const nodesIds = /** @type {!Array<number>} */ (fontMetricInfo.get(value));
         if (!nodesIds) {
           return;
         }
@@ -192,10 +271,14 @@ export class CSSOverviewCompletedView extends UI.Panel.PanelWithSidebar {
     this._mainContainer.setSidebarMinimized(false);
   }
 
+  /**
+   * @param {!Event} evt
+   */
   _onMouseOver(evt) {
     // Traverse the event path on the grid to find the nearest element with a backend node ID attached. Use
     // that for the highlighting.
-    const node = evt.path.find(el => el.dataset && el.dataset.backendNodeId);
+    const node =
+        (/** @type {!Array<!HTMLElement>} */ (evt.composedPath())).find(el => el.dataset && el.dataset.backendNodeId);
     if (!node) {
       return;
     }
@@ -204,6 +287,9 @@ export class CSSOverviewCompletedView extends UI.Panel.PanelWithSidebar {
     this._controller.dispatchEventToListeners(Events.RequestNodeHighlight, backendNodeId);
   }
 
+  /**
+   * @param {!OverviewData} data
+   */
   async _render(data) {
     if (!data || !('backgroundColors' in data) || !('textColors' in data)) {
       return;
@@ -331,6 +417,9 @@ export class CSSOverviewCompletedView extends UI.Panel.PanelWithSidebar {
     this._resultsContainer.element.appendChild(this._fragment.element());
   }
 
+  /**
+   * @param {!Common.EventTarget.EventTargetEvent} evt
+   */
   _createElementsView(evt) {
     const {type, nodes} = evt.data;
 
@@ -377,6 +466,9 @@ export class CSSOverviewCompletedView extends UI.Panel.PanelWithSidebar {
     this._elementContainer.appendTab(id, tabTitle, view, true);
   }
 
+  /**
+   * @param {!FontInfo} fontInfo
+   */
   _fontInfoToFragment(fontInfo) {
     const fonts = Array.from(fontInfo.entries());
     return UI.Fragment.Fragment.build`
@@ -387,6 +479,10 @@ export class CSSOverviewCompletedView extends UI.Panel.PanelWithSidebar {
     `;
   }
 
+  /**
+   * @param {string} font
+   * @param {!Map<string, !Map<string, !Array<number>>>} fontMetrics
+   */
   _fontMetricsToFragment(font, fontMetrics) {
     const fontMetricInfo = Array.from(fontMetrics.entries());
 
@@ -403,6 +499,12 @@ export class CSSOverviewCompletedView extends UI.Panel.PanelWithSidebar {
       </div>`;
   }
 
+  /**
+   * @param {!Map<string, !Array<number|!UnusedDeclaration|!Protocol.CSS.CSSMedia>>} items
+   * @param {string} type
+   * @param {string} dataLabel
+   * @param {string} path
+   */
   _groupToFragment(items, type, dataLabel, path = '') {
     // Sort by number of items descending.
     const values = Array.from(items.entries()).sort((d1, d2) => {
@@ -431,16 +533,23 @@ export class CSSOverviewCompletedView extends UI.Panel.PanelWithSidebar {
     </ul>`;
   }
 
+  /**
+   * @param {string} section
+   * @param {string} color
+   */
   _colorsToFragment(section, color) {
     const blockFragment = UI.Fragment.Fragment.build`<li>
       <button data-type="color" data-color="${color}" data-section="${section}" class="block" $="color"></button>
       <div class="block-title">${color}</div>
     </li>`;
 
-    const block = blockFragment.$('color');
+    const block = /** @type {!HTMLElement} */ (blockFragment.$('color'));
     block.style.backgroundColor = color;
 
     const borderColor = Common.Color.Color.parse(color);
+    if (!borderColor) {
+      return;
+    }
     let [h, s, l] = borderColor.hsla();
     h = Math.round(h * 360);
     s = Math.round(s * 100);
@@ -455,14 +564,23 @@ export class CSSOverviewCompletedView extends UI.Panel.PanelWithSidebar {
     return blockFragment;
   }
 
+  /**
+   * @param {!NodeStyleStats} srcColors
+   */
   _sortColorsByLuminance(srcColors) {
     return Array.from(srcColors.keys()).sort((colA, colB) => {
       const colorA = Common.Color.Color.parse(colA);
       const colorB = Common.Color.Color.parse(colB);
+      if (!colorA || !colorB) {
+        return 0;
+      }
       return Common.ColorUtils.luminance(colorB.rgba()) - Common.ColorUtils.luminance(colorA.rgba());
     });
   }
 
+  /**
+   * @param {!OverviewData} data
+   */
   setOverviewData(data) {
     this._render(data);
   }
@@ -501,6 +619,12 @@ export class DetailsView extends UI.Widget.VBox {
 }
 
 export class ElementDetailsView extends UI.Widget.Widget {
+  /**
+   * @param {!OverviewController} controller
+   * @param {!SDK.DOMModel.DOMModel} domModel
+   * @param {!SDK.CSSModel.CSSModel} cssModel
+   * @param {!Components.Linkifier.Linkifier} linkifier
+   */
   constructor(controller, domModel, cssModel, linkifier) {
     super();
 
@@ -510,13 +634,66 @@ export class ElementDetailsView extends UI.Widget.Widget {
     this._linkifier = linkifier;
 
     this._elementGridColumns = [
-      {id: 'nodeId', title: ls`Element`, visible: false, sortable: true, hideable: true, weight: 50},
-      {id: 'declaration', title: ls`Declaration`, visible: false, sortable: true, hideable: true, weight: 50},
-      {id: 'sourceURL', title: ls`Source`, visible: true, sortable: false, hideable: true, weight: 100}
+      {
+        id: 'nodeId',
+        title: ls`Element`,
+        sortable: true,
+        weight: 50,
+        titleDOMFragment: undefined,
+        sort: undefined,
+        align: undefined,
+        fixedWidth: undefined,
+        editable: undefined,
+        nonSelectable: undefined,
+        longText: undefined,
+        disclosure: undefined,
+        allowInSortByEvenWhenHidden: undefined,
+        dataType: undefined,
+        defaultWeight: undefined,
+      },
+      {
+        id: 'declaration',
+        title: ls`Declaration`,
+        sortable: true,
+        weight: 50,
+        titleDOMFragment: undefined,
+        sort: undefined,
+        align: undefined,
+        fixedWidth: undefined,
+        editable: undefined,
+        nonSelectable: undefined,
+        longText: undefined,
+        disclosure: undefined,
+        allowInSortByEvenWhenHidden: undefined,
+        dataType: undefined,
+        defaultWeight: undefined,
+      },
+      {
+        id: 'sourceURL',
+        title: ls`Source`,
+        sortable: false,
+        weight: 100,
+        titleDOMFragment: undefined,
+        sort: undefined,
+        align: undefined,
+        fixedWidth: undefined,
+        editable: undefined,
+        nonSelectable: undefined,
+        longText: undefined,
+        disclosure: undefined,
+        allowInSortByEvenWhenHidden: undefined,
+        dataType: undefined,
+        defaultWeight: undefined,
+      },
     ];
 
-    this._elementGrid = new DataGrid.SortableDataGrid.SortableDataGrid(
-        {displayName: ls`CSS Overview Elements`, columns: this._elementGridColumns});
+    this._elementGrid = new DataGrid.SortableDataGrid.SortableDataGrid({
+      displayName: ls`CSS Overview Elements`,
+      columns: this._elementGridColumns,
+      editCallback: undefined,
+      deleteCallback: undefined,
+      refreshCallback: undefined
+    });
     this._elementGrid.element.classList.add('element-grid');
     this._elementGrid.element.addEventListener('mouseover', this._onMouseOver.bind(this));
     this._elementGrid.setStriped(true);
@@ -536,10 +713,14 @@ export class ElementDetailsView extends UI.Widget.Widget {
     this._elementGrid.sortNodes(comparator, !this._elementGrid.isSortOrderAscending());
   }
 
+  /**
+   * @param {!Event} evt
+   */
   _onMouseOver(evt) {
     // Traverse the event path on the grid to find the nearest element with a backend node ID attached. Use
     // that for the highlighting.
-    const node = evt.path.find(el => el.dataset && el.dataset.backendNodeId);
+    const node =
+        (/** @type {!Array<!HTMLElement>} */ (evt.composedPath())).find(el => el.dataset && el.dataset.backendNodeId);
     if (!node) {
       return;
     }
@@ -548,6 +729,9 @@ export class ElementDetailsView extends UI.Widget.Widget {
     this._controller.dispatchEventToListeners(Events.RequestNodeHighlight, backendNodeId);
   }
 
+  /**
+   * @param {!Array<!Object<string, *>>} data
+   */
   async populateNodes(data) {
     this._elementGrid.rootNode().removeChildren();
 
@@ -566,19 +750,22 @@ export class ElementDetailsView extends UI.Widget.Widget {
     if (visibility.nodeId) {
       // Grab the nodes from the frontend, but only those that have not been
       // retrieved already.
-      const nodeIds = data.reduce((prev, curr) => {
+      const nodeIds = /** @type {!Set<number>} */ (data.reduce((prev, curr) => {
         if (CSSOverviewCompletedView.pushedNodes.has(curr.nodeId)) {
           return prev;
         }
 
         CSSOverviewCompletedView.pushedNodes.add(curr.nodeId);
         return prev.add(curr.nodeId);
-      }, new Set());
+      }, new Set()));
       relatedNodesMap = await this._domModel.pushNodesByBackendIdsToFrontend(nodeIds);
     }
 
     for (const item of data) {
       if (visibility.nodeId) {
+        if (!relatedNodesMap) {
+          continue;
+        }
         const frontendNode = relatedNodesMap.get(item.nodeId);
         if (!frontendNode) {
           continue;
@@ -598,9 +785,12 @@ export class ElementDetailsView extends UI.Widget.Widget {
   }
 }
 
+/**
+ * @extends {DataGrid.SortableDataGrid.SortableDataGridNode<!ElementNode>}
+ */
 export class ElementNode extends DataGrid.SortableDataGrid.SortableDataGridNode {
   /**
-   * @param {!DataGrid.SortableDataGrid.SortableDataGrid} dataGrid
+   * @param {!DataGrid.SortableDataGrid.SortableDataGrid<!ElementNode>} dataGrid
    * @param {!Object<string,*>} data
    * @param {!Components.Linkifier.Linkifier} linkifier
    * @param {!SDK.CSSModel.CSSModel} cssModel
@@ -626,7 +816,7 @@ export class ElementNode extends DataGrid.SortableDataGrid.SortableDataGridNode 
 
       Common.Linkifier.Linkifier.linkify(this.data.node).then(link => {
         cell.textContent = '';
-        link.dataset.backendNodeId = this.data.node.backendNodeId();
+        /** @type {!HTMLElement} */ (link).dataset.backendNodeId = this.data.node.backendNodeId();
         cell.appendChild(link);
       });
       return cell;
@@ -641,10 +831,10 @@ export class ElementNode extends DataGrid.SortableDataGrid.SortableDataGridNode 
             this._cssModel, this._linkifier, this.data.styleSheetId,
             TextUtils.TextRange.TextRange.fromObject(this.data.range));
 
-        if (link.textContent !== '') {
-          cell.appendChild(link);
-        } else {
+        if (!link || link.textContent === '') {
           cell.textContent = '(unable to link)';
+        } else {
+          cell.appendChild(link);
         }
       } else {
         cell.textContent = '(unable to link to inlined styles)';
@@ -655,8 +845,17 @@ export class ElementNode extends DataGrid.SortableDataGrid.SortableDataGridNode 
     return super.createCell(columnId);
   }
 
+  /**
+   * @param {!SDK.CSSModel.CSSModel} cssModel
+   * @param {!Components.Linkifier.Linkifier} linkifier
+   * @param {!Protocol.CSS.StyleSheetId} styleSheetId
+   * @param {!TextUtils.TextRange.TextRange} ruleLocation
+   */
   _linkifyRuleLocation(cssModel, linkifier, styleSheetId, ruleLocation) {
     const styleSheetHeader = cssModel.styleSheetHeaderForId(styleSheetId);
+    if (!styleSheetHeader) {
+      return;
+    }
     const lineNumber = styleSheetHeader.lineNumberInSource(ruleLocation.startLine);
     const columnNumber = styleSheetHeader.columnNumberInSource(ruleLocation.startLine, ruleLocation.startColumn);
     const matchingSelectorLocation = new SDK.CSSModel.CSSLocation(styleSheetHeader, lineNumber, columnNumber);

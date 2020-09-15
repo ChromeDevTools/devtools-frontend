28ecfe40d6aedcf55935154dcbe0f63e7e63147f
Paul Lewis <aerotwist@chromium.org>
2020-09-15 13:50:22 +0100

    [TS] TypeScriptify HeapSnapshotModel
    R=tvanderlippe@chromium.org
    
    Bug: 1011811
    Change-Id: Ieb73863c9c2e3c3c5e514c6547524461f9539c6c
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2410233
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>
    Commit-Queue: Paul Lewis <aerotwist@chromium.org>

--- front_end/heap_snapshot_model/HeapSnapshotModel.js
+++ front_end/heap_snapshot_model/HeapSnapshotModel.js
@@ -28,9 +28,6 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-// @ts-nocheck
-// TODO(crbug.com/1011811): Enable TypeScript compiler checks
-
 export const HeapSnapshotProgressEvent = {
   Update: 'ProgressUpdate',
   BrokenSnapshot: 'BrokenSnapshot'

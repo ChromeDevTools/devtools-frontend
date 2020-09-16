88781c962a03549276d4505cbfd2e5cf3d6d05bd
Jack Franklin <jacktfranklin@chromium.org>
2020-09-16 14:23:01 +0100

    Migrate heap snapshot multiple retainers layout test
    Replaces this layout test: https://source.chromium.org/chromium/chromium/src/+/master:third_party/blink/web_tests/inspector-protocol/heap-profiler/heap-snapshot-with-multiple-retainers.js.
    
    Bug: 1110817
    Change-Id: I2e56235f07ee31b646df6c634885e5c302b33b42
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2409978
    Commit-Queue: Jack Franklin <jacktfranklin@chromium.org>
    Reviewed-by: Sigurd Schneider <sigurds@chromium.org>

--- test/e2e/memory/memory_test.ts
+++ test/e2e/memory/memory_test.ts
@@ -5,7 +5,7 @@
 import {assert} from 'chai';
 import {$$, click, getBrowserAndPages, goToResource, waitForElementsWithTextContent, waitForElementWithTextContent, waitForFunction} from '../../shared/helper.js';
 import {describe, it} from '../../shared/mocha-extensions.js';
-import {changeViewViaDropdown, findSearchResult, navigateToMemoryTab, setSearchFilter, takeHeapSnapshot, waitForNonEmptyHeapSnapshotData, waitForRetainerChain, waitForSearchResultNumber, waitUntilRetainerChainSatisfies} from '../helpers/memory-helpers.js';
+import {changeViewViaDropdown, findSearchResult, getDataGridRows, navigateToMemoryTab, setSearchFilter, takeHeapSnapshot, waitForNonEmptyHeapSnapshotData, waitForRetainerChain, waitForSearchResultNumber, waitUntilRetainerChainSatisfies} from '../helpers/memory-helpers.js';
 
 describe('The Memory Panel', async function() {
   // These tests render large chunks of data into DevTools and filter/search
@@ -119,6 +119,72 @@ describe('The Memory Panel', async function() {
     await waitUntilRetainerChainSatisfies(
         retainerChain => retainerChain.some(
             ({propertyName, retainerClassName}) => propertyName === 'aUniqueName' && retainerClassName === 'Window'));
+  });
+
+  it('Correctly shows multiple retainer paths for an object', async () => {
+    await goToResource('memory/multiple-retainers.html');
+    await navigateToMemoryTab();
+    await takeHeapSnapshot();
+    await waitForNonEmptyHeapSnapshotData();
+    await setSearchFilter('leaking');
+    await waitForSearchResultNumber(4);
+
+    await waitForFunction(async () => {
+      // Wait for all the rows of the data-grid to load.
+      const retainerGridElements = await getDataGridRows('.retaining-paths-view table.data');
+      return retainerGridElements.length === 9;
+    });
+
+    const sharedInLeakingElementRow = await waitForFunction(async () => {
+      const results = await getDataGridRows('.retaining-paths-view table.data');
+      const findPromises = await Promise.all(results.map(async e => {
+        const textContent = await e.evaluate(el => el.textContent);
+        // Can't search for "shared in leaking()" because the different parts are spaced with CSS.
+        return textContent && textContent.startsWith('sharedinleaking()') ? e : null;
+      }));
+      return findPromises.find(result => result !== null);
+    });
+
+    if (!sharedInLeakingElementRow) {
+      assert.fail('Could not find data-grid row with "shared in leaking()" text.');
+    }
+
+    // TODO(jacktfranklin): Consider some helpers to make asserting on contents
+    // of the data-grid in the heap snapshot UI easier.
+    const textOfEl = await sharedInLeakingElementRow.evaluate(e => e.textContent || '');
+    // Double check we got the right element to avoid a confusing text failure
+    // later down the line.
+    assert.isTrue(textOfEl.startsWith('sharedinleaking()'));
+
+    // Have to click it not in the middle as the middle can hold the link to the
+    // file in the sources pane and we want to avoid clicking that.
+    await click(sharedInLeakingElementRow, {maxPixelsFromLeft: 10});
+    const {frontend} = getBrowserAndPages();
+    // Expand the data-grid for the shared list
+    await frontend.keyboard.press('ArrowRight');
+
+    // check that we found two V8EventListener objects
+    await waitForFunction(async () => {
+      const pendingActiviesChildren = await waitForElementsWithTextContent('V8EventListener');
+      return pendingActiviesChildren.length === 2;
+    });
+
+    // Now we want to get the two rows below the "shared in leaking()" row and assert on them.
+    // Unfortunately they are not structured in the data-grid as children, despite being children in the UI
+    // So the best way to get at them is to grab the two subsequent siblings of the "shared in leaking()" row.
+    const nextRow = await sharedInLeakingElementRow.evaluateHandle(e => e.nextSibling);
+    if (!nextRow) {
+      assert.fail('Could not find row below "shared in leaking()" row');
+    }
+    const nextNextRow = await nextRow!.evaluateHandle(e => e.nextSibling);
+    if (!nextNextRow) {
+      assert.fail('Could not find 2nd row below "shared in leaking()" row');
+    }
+
+    const childText = await Promise.all([nextRow, nextNextRow].map(async row => await row.evaluate(r => r.innerText)));
+
+    assert.isTrue(childText[0].includes('inV8EventListener'));
+    assert.isTrue(childText[1].includes('inEventListener'));
   });
 
   it('Shows the correct output for a detached iframe', async () => {

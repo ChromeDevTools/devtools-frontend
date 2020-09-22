37df7ec1a08a06772cd26bf5355afb1ef4df10de
Maksim Sadym <sadym@chromium.org>
2020-09-22 14:13:00 +0000

    Fix DevTools idle state emulation e2e test
    * use the new permission 'idle-detection' instead of 'notifications'
    
    Bug: 1130513
    Change-Id: I8f71917caf53b2837ee9d9fb6f687bd73e74b8e2
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2421701
    Commit-Queue: Maksim Sadym <sadym@chromium.org>
    Reviewed-by: Changhao Han <changhaohan@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Mathias Bynens <mathias@chromium.org>
    Reviewed-by: Sigurd Schneider <sigurds@chromium.org>

--- test/e2e/sensors/idle_test.ts
+++ test/e2e/sensors/idle_test.ts
@@ -16,8 +16,11 @@ describe('Idle Emulation on Sensors panel', () => {
   });
 
   before(async () => {
-    await step('overriding permissions with \'notifications\'', async () => {
-      await overridePermissions(['notifications']);
+    await step('overriding permissions with \'idle-detection\'', async () => {
+      // IdleDetector switched permission from 'notifications' to
+      // 'idle-detection', but 'idle-detection' is not in the @types/puppeteer
+      // package, so `as 'notifications'` needed for TypeScript.
+      await overridePermissions(['idle-detection' as 'notifications']);
     });
   });
 
@@ -59,8 +62,7 @@ describe('Idle Emulation on Sensors panel', () => {
     assert.deepEqual(actual, expected);
   });
 
-  // Skip this test since it's preventing Chrome -> DevTools roll: https://crrev.com/c/2420957
-  it.skip('[crbug.com/1130513] changing idle state emulation causes change of the IdleDetector state', async () => {
+  it('changing idle state emulation causes change of the IdleDetector state', async () => {
     await step('opening idle-detector.html', async () => {
       await goToResource('sensors/idle-detector.html');
     });

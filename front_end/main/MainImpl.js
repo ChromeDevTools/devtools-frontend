87561a3649597398c8188e53937e98472a8fcbc5
Changhao Han <changhaohan@chromium.org>
2020-09-22 21:33:03 +0000

    Disable CSS Grid experiment by default
    A layout test flushed a latent bug in the implementation, this CL
    disables the grid experiment so we can investigate.
    
    Bug: chromium:1131147, chromium:1047356
    Change-Id: I7f1202a8145026f05facb2648c482843618da0fa
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2424311
    Commit-Queue: Yang Guo <yangguo@chromium.org>
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Reviewed-by: Darwin Huang <huangdarwin@chromium.org>
    Auto-Submit: Changhao Han <changhaohan@chromium.org>

--- front_end/main/MainImpl.js
+++ front_end/main/MainImpl.js
@@ -190,7 +190,8 @@ export class MainImpl {
     Root.Runtime.experiments.register('movableTabs', 'Enable support to move tabs between panels');
 
     Root.Runtime.experiments.enableExperimentsByDefault([
-      'cssGridFeatures',
+      // crbug.com/1131147: temporarily disable turning on cssGridFeatures by default
+      // 'cssGridFeatures',
     ]);
     Root.Runtime.experiments.cleanUpStaleExperiments();
     const enabledExperiments = Root.Runtime.queryParam('enabledExperiments');

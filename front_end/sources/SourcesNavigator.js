f6444c825797c601175435cef309a54f42833b7f
Simon Zünd <szuend@chromium.org>
2020-09-22 16:13:20 +0200

    [ts] Remove unnecessary @ts-nocheck from sources/SourcesNavigator.js
    TBR=tvanderlippe@chromium.org
    
    Bug: chromium:1011811
    Change-Id: I2c15e0f244065fd9e1c5854935835239b1266939
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2424205
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>

--- front_end/sources/SourcesNavigator.js
+++ front_end/sources/SourcesNavigator.js
@@ -28,9 +28,6 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-// @ts-nocheck
-// TODO(crbug.com/1011811): Enable TypeScript compiler checks
-
 import * as Common from '../common/common.js';
 import * as Host from '../host/host.js';
 import * as Persistence from '../persistence/persistence.js';

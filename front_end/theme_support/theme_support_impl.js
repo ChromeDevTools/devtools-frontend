16fe4729c7a9af81a4b59e77c927a7be2973c81c
Sigurd Schneider <sigurds@chromium.org>
2020-09-23 10:07:29 +0200

    Reinstate fix after migration accidentally reverted
    The patch https://crrev.com/c/2352711 accidentally reverted the
    fix https://crrev.com/c/2390622. This patch re-applies it to
    the new location.
    
    Bug: chromium:1122511
    Change-Id: I4eec28bd6d6175dd45317089ff37e73a366cac4e
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2424313
    Reviewed-by: Paul Lewis <aerotwist@chromium.org>
    Commit-Queue: Sigurd Schneider <sigurds@chromium.org>

--- front_end/theme_support/theme_support_impl.js
+++ front_end/theme_support/theme_support_impl.js
@@ -292,6 +292,11 @@ export class ThemeSupport {
       return;
     }
 
+    // Don't operate on CSS variables.
+    if (/^var\(.*\)$/.test(value)) {
+      return;
+    }
+
     if (selectorText.indexOf('-theme-') !== -1) {
       return;
     }

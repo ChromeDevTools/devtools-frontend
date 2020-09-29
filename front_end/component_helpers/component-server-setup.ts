27ad3e764a9a99c92fce33aec857f3c5bd5bc9ff
Alex Rudenko <alexrudenko@chromium.org>
2020-09-29 08:29:40 +0000

    Set up ThemeSupport in component-server to allow importing CSS
    Change-Id: Ia407aa70fdbf4ad227f5a758f032744e760507b2
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2436812
    Reviewed-by: Jack Franklin <jacktfranklin@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>

--- front_end/component_helpers/component-server-setup.ts
+++ front_end/component_helpers/component-server-setup.ts
@@ -2,7 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import * as Common from '../common/common.js';
 import * as Root from '../root/root.js';
+import * as ThemeSupport from '../theme_support/theme_support.js';
 
 import {CSS_RESOURCES_TO_LOAD_INTO_RUNTIME} from './get-stylesheet.js';
 
@@ -11,6 +13,13 @@ import {CSS_RESOURCES_TO_LOAD_INTO_RUNTIME} from './get-stylesheet.js';
  * only populating the runtime CSS cache but may be extended in the future.
  */
 export async function setup() {
+  const setting = {
+    get() {
+      return 'default';
+    },
+  } as Common.Settings.Setting<string>;
+  ThemeSupport.ThemeSupport.instance({forceNew: true, setting});
+
   const allPromises = CSS_RESOURCES_TO_LOAD_INTO_RUNTIME.map(resourcePath => {
     return fetch('/' + resourcePath).then(response => response.text()).then(cssText => {
       Root.Runtime.cachedResources.set(resourcePath, cssText);

0428ffbf9f161b3851811b8940f38e57d57b7a73
Simon Zünd <szuend@chromium.org>
2020-09-29 08:50:29 +0200

    [ts] Remove unnecessary @ts-nocheck from timeline/TimelineFilters.js
    TBR=tvanderlippe@chromium.org
    
    Bug: chromium:1011811
    Change-Id: I30e3a4ae009add1f2017468954b5c3c37590eba2
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2436733
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>

--- front_end/timeline/TimelineFilters.js
+++ front_end/timeline/TimelineFilters.js
@@ -1,8 +1,6 @@
 // Copyright 2017 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-// @ts-nocheck
-// TODO(crbug.com/1011811): Enable TypeScript compiler checks
 
 import * as SDK from '../sdk/sdk.js';  // eslint-disable-line no-unused-vars
 import * as TimelineModel from '../timeline_model/timeline_model.js';

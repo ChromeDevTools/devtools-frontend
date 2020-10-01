141a204367c756a3e99b09d5964658ec9ef2aee3
Alex Rudenko <alexrudenko@chromium.org>
2020-10-01 07:31:35 +0000

    Migrate ResourceUtils.js to TypeScript
    Bug: 1011811
    Change-Id: I6e1262798a67c042707214f69c8161d040a3597c
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2440593
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>

--- front_end/bindings/ResourceUtils.js
+++ front_end/bindings/ResourceUtils.js
@@ -27,8 +27,6 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-// @ts-nocheck
-// TODO(crbug.com/1011811): Enable TypeScript compiler checks
 
 import * as Common from '../common/common.js';
 import * as Platform from '../platform/platform.js';
@@ -76,17 +74,17 @@ export function displayNameForURL(url) {
   }
 
   const parsedURL = Common.ParsedURL.ParsedURL.fromString(inspectedURL);
-  const lastPathComponent = parsedURL ? parsedURL.lastPathComponent : parsedURL;
+  if (!parsedURL) {
+    return url;
+  }
+
+  const lastPathComponent = parsedURL.lastPathComponent;
   const index = inspectedURL.indexOf(lastPathComponent);
   if (index !== -1 && index + lastPathComponent.length === inspectedURL.length) {
     const baseURL = inspectedURL.substring(0, index);
     if (url.startsWith(baseURL)) {
       return url.substring(index);
     }
-  }
-
-  if (!parsedURL) {
-    return url;
   }
 
   const displayName = Platform.StringUtilities.trimURL(url, parsedURL.host);

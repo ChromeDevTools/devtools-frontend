182a24a6540bde3aaa252af3d531746b47d8bcd6
Brandon Goddard <brgoddar@microsoft.com>
2020-10-07 15:19:10 -0700

    Force Tabbed Pane to update all Tabbed widths on DPI changes
    This CL fixes an issue where TabbedPane widgets will not recalculate
    their tab header widths during a DPI change (such as dragging devtools
    from low to higher DPI monitor).
    
    Previously, the tabbedpane only cleared all previously measured widths
    when the zoom factor changes. However, zoom factor remains the same
    on a DPI change. This can result in tabbed panes using out of date
    widths when calculating how many tabs to show before the overflow
    menu.
    
    Bug: 1136226
    
    Change-Id: Id2efddc83187c31d7a2219ae5aeca169ddd9ad27
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2458778
    Reviewed-by: Kalon Hinds <kahinds@microsoft.com>
    Reviewed-by: Songtao Xia <soxia@microsoft.com>
    Commit-Queue: Brandon Goddard <brgoddar@microsoft.com>

--- front_end/ui/TabbedPane.js
+++ front_end/ui/TabbedPane.js
@@ -71,6 +71,7 @@ export class TabbedPane extends VBox {
 
     this._triggerDropDownTimeout = null;
     this._dropDownButton = this._createDropDownButton();
+    this._currentDevicePixelRatio = window.devicePixelRatio;
     ZoomManager.instance().addEventListener(ZoomManagerEvents.ZoomChanged, this._zoomChanged, this);
     this.makeTabSlider();
   }
@@ -453,11 +454,15 @@ export class TabbedPane extends VBox {
    * @param {!Common.EventTarget.EventTargetEvent} event
    */
   _zoomChanged(event) {
-    for (let i = 0; i < this._tabs.length; ++i) {
-      delete this._tabs[i]._measuredWidth;
-    }
+    this._clearMeasuredWidths();
     if (this.isShowing()) {
       this._updateTabElements();
+    }
+  }
+
+  _clearMeasuredWidths() {
+    for (let i = 0; i < this._tabs.length; ++i) {
+      delete this._tabs[i]._measuredWidth;
     }
   }
 
@@ -508,6 +513,11 @@ export class TabbedPane extends VBox {
    * @override
    */
   onResize() {
+    if (this._currentDevicePixelRatio !== window.devicePixelRatio) {
+      // Force recalculation of all tab widths on a DPI change
+      this._clearMeasuredWidths();
+      this._currentDevicePixelRatio = window.devicePixelRatio;
+    }
     this._updateTabElements();
   }
 

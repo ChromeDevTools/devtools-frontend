ccf1d008d7f37f07d3c2281b88aaed13063d2bed
Carl Espe <carl@cpespe.com>
2020-10-14 16:40:43 -0700

    Add clip-path property values for autocomplete menu
    Enter values for the clip-path property into the manually maintained
    list that is used to construct Styles editor autocomplete menus.
    While the requestor asked for the list to include all possible options
    as they appear in Firefox, this fix only adds the values that are
    supported by Chromium.
    
    Screenshot before fix: https://bit.ly/3j24XEW
    Screenshot after fix: https://bit.ly/2SWf0B4
    
    Bug: 954126
    Change-Id: I7581ee92452c7796114e50919503110babef366f
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2469450
    Reviewed-by: Changhao Han <changhaohan@chromium.org>
    Commit-Queue: Changhao Han <changhaohan@chromium.org>

--- front_end/sdk/CSSMetadata.js
+++ front_end/sdk/CSSMetadata.js
@@ -544,6 +544,7 @@ const _extraPropertyValues = {
   'overscroll-behavior': {values: ['contain']},
   'text-rendering': {values: ['optimizeSpeed', 'optimizeLegibility', 'geometricPrecision']},
   'text-align': {values: ['-webkit-auto', '-webkit-match-parent']},
+  'clip-path': {values: ['circle', 'ellipse', 'inset', 'polygon', 'url']},
   'color-interpolation': {values: ['sRGB', 'linearRGB']},
   'word-wrap': {values: ['normal', 'break-word']},
   'font-weight': {values: ['100', '200', '300', '400', '500', '600', '700', '800', '900']},

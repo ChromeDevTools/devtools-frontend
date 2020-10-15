c20afe742b9d14102dd6a3287b358ab6eecc6f66
Tim van der Lippe <tvanderlippe@chromium.org>
2020-10-15 17:14:29 +0100

    Move legacy files from ui modules to GN
    R=aerotwist@chromium.org
    
    Bug: 1131500
    Change-Id: I8ece4d0b8373584333b9639e2ba3f4a584088619
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2476335
    Commit-Queue: Tim van der Lippe <tvanderlippe@chromium.org>
    Reviewed-by: Paul Lewis <aerotwist@chromium.org>

--- front_end/source_frame/source_frame-legacy.js
+++ front_end/source_frame/source_frame-legacy.js
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// @ts-nocheck
+
 import * as SourceFrameModule from './source_frame.js';
 
 self.SourceFrame = self.SourceFrame || {};

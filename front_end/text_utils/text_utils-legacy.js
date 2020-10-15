42fbb0aca238d6ad05f740bb7f9fe9061d79ee7e
Tim van der Lippe <tvanderlippe@chromium.org>
2020-10-15 17:03:30 +0100

    Move legacy files from model modules to GN
    R=aerotwist@chromium.org
    
    Bug: 1131500
    Change-Id: I28b598c770801947eace05164e5308c621f5407a
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2476334
    Commit-Queue: Tim van der Lippe <tvanderlippe@chromium.org>
    Reviewed-by: Paul Lewis <aerotwist@chromium.org>

--- front_end/text_utils/text_utils-legacy.js
+++ front_end/text_utils/text_utils-legacy.js
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// @ts-nocheck
+
 import * as TextUtilsModule from './text_utils.js';
 
 self.TextUtils = self.TextUtils || {};
--- front_end/timeline_model/timeline_model-legacy.js
+++ front_end/timeline_model/timeline_model-legacy.js
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// @ts-nocheck
+
 import * as TimelineModelModule from './timeline_model.js';
 
 self.TimelineModel = self.TimelineModel || {};

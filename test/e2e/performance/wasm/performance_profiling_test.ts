ab5d2e3949cfe0f7799fe4d826dee39bf913af44
Almothana Athamneh <almuthanna@google.com>
2020-10-21 15:33:18 +0200

    Fix Flickering for Performance profiling test
    This CL attempts to solve the flickering issue
    for the Performance profiling test by changing
    the assertion for the activities to be checked
    one by one instead of all at once.
    
    Bug: chromium:1140926
    Cq-Include-Trybots: luci.devtools-frontend.try:devtools_frontend_mac_rel
    Change-Id: I3221b2b41a45aae4bdf2c3e8187c542bc9e77ad5
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2489862
    Commit-Queue: Almothana Athamneh <almuthanna@chromium.org>
    Reviewed-by: Sigurd Schneider <sigurds@chromium.org>

--- test/e2e/performance/wasm/performance_profiling_test.ts
+++ test/e2e/performance/wasm/performance_profiling_test.ts
@@ -7,25 +7,27 @@ import * as puppeteer from 'puppeteer';
 
 import {$$, getBrowserAndPages, step, waitFor, waitForFunction} from '../../../shared/helper.js';
 import {describe, it} from '../../../shared/mocha-extensions.js';
-import {ACTIVITY_COLUMN_SELECTOR, navigateToCallTreeTab, retrieveSelectedAndExpandedActivityItems} from '../../helpers/performance-helpers.js';
+import {ACTIVITY_COLUMN_SELECTOR, navigateToCallTreeTab} from '../../helpers/performance-helpers.js';
 import {clickOnFunctionLink, getTotalTimeFromSummary, navigateToBottomUpTab, navigateToPerformanceTab, retrieveActivity, searchForComponent, startRecording, stopRecording} from '../../helpers/performance-helpers.js';
 
-async function expandActivityTree(frontend: puppeteer.Page, numExpectedActivities: number) {
-  const numClicksPerExpand = 2;
-  const numClicks = (numExpectedActivities - 1) * numClicksPerExpand;
-  for (let index = 0; index < numClicks; index++) {
+async function expandAndCheckActivityTree(frontend: puppeteer.Page, expectedActivities: string[]) {
+  let index = 0;
+  do {
+    await waitForFunction(async () => {
+      const expandedActivities = await $$('.data-grid-data-grid-node.selected.revealed');
+      return expandedActivities.length === 1;
+    });
+    const tree_item = await frontend.$('.data-grid-data-grid-node.selected.revealed .activity-name');
+    assert.strictEqual(
+        await frontend.evaluate(el => el.innerText, tree_item), expectedActivities[index],
+        'tree does not display the values correctly');
+    index++;
     await frontend.keyboard.press('ArrowRight');
-  }
-  await waitForFunction(async () => {
-    const expandedActivities =
-        await $$('.data-grid-data-grid-node.parent.revealed.expanded,.data-grid-data-grid-node.selected.revealed');
-    return expandedActivities.length === (numExpectedActivities);
-  });
+    await frontend.keyboard.press('ArrowRight');
+  } while (index < expectedActivities.length);
 }
 
 describe('The Performance panel', async function() {
-  // Custom timeout since this is running into problems on MAC frequently, see https://crbug.com/1140926.
-  this.timeout(20000);
   beforeEach(async () => {
     const {target, frontend} = getBrowserAndPages();
 
@@ -93,18 +95,13 @@ describe('The Performance panel', async function() {
       await navigateToBottomUpTab();
     });
 
-    await step('expand the tree for the "mainWasm" activity', async () => {
-      await waitFor(ACTIVITY_COLUMN_SELECTOR);
-      const mainWasmActivity = await retrieveActivity(frontend, 'mainWasm');
-      await mainWasmActivity!.click();
-      await expandActivityTree(frontend, expectedActivities.length);
-    });
-
-    await step('check that the expanded tree is displaying the correct activity names', async () => {
-      assert.deepEqual(
-          await retrieveSelectedAndExpandedActivityItems(frontend), expectedActivities,
-          'tree does not display the values correctly');
-    });
+    await step(
+        'expand the tree for the "mainWasm" activity and check that it displays the correct values', async () => {
+          await waitFor(ACTIVITY_COLUMN_SELECTOR);
+          const mainWasmActivity = await retrieveActivity(frontend, 'mainWasm');
+          await mainWasmActivity!.click();
+          await expandAndCheckActivityTree(frontend, expectedActivities);
+        });
   });
 
   it('is able to inspect the call stack for a wasm function from the call tree', async () => {
@@ -123,11 +120,7 @@ describe('The Performance panel', async function() {
 
     await step(
         'expand the tree for the "Run Microtasks" activity and check that it displays the correct values', async () => {
-          await expandActivityTree(frontend, expectedActivities.length);
-
-          assert.deepEqual(
-              await retrieveSelectedAndExpandedActivityItems(frontend), expectedActivities,
-              'tree does not display the values correctly');
+          await expandAndCheckActivityTree(frontend, expectedActivities);
         });
   });
 });

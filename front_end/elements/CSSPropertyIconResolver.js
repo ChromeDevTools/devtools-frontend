875a33f832e01db718aa5532e5ea3c75b85c9eea
Alex Rudenko <alexrudenko@chromium.org>
2020-10-26 09:36:33 +0000

    Icons for all flex-direction values
    This CL adds icons for the flex-direction values: column, column-reverse
    and row-reverse. Additionally, it refactors the icon rendering logic
    into two parts: determining actual direction for row and column
    values and handling of the icon rotation according to the direction.
    
    Bug: 1139945
    Change-Id: Ie7a5677963c77c482837735fc4e7c90d3801b451
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2498220
    Reviewed-by: Patrick Brosset <patrick.brosset@microsoft.com>
    Reviewed-by: Mathias Bynens <mathias@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>

--- front_end/elements/CSSPropertyIconResolver.js
+++ front_end/elements/CSSPropertyIconResolver.js
@@ -9,39 +9,101 @@ const writingModesAffectingFlexDirection = new Set([
   'vertical-rl',
 ]);
 
+/** @enum {string} */
+export const PhysicalFlexDirection = {
+  LEFT_TO_RIGHT: 'left-to-right',
+  RIGHT_TO_LEFT: 'right-to-left',
+  BOTTOM_TO_TOP: 'bottom-to-top',
+  TOP_TO_BOTTOM: 'top-to-bottom',
+};
+
 /**
- * @param {!Map<string, string>} computedStyles
- * @return {!IconInfo}
+ * @param {!PhysicalFlexDirection} direction
+ * @return {!PhysicalFlexDirection}
  */
-function flexDirectionRowIcon(computedStyles) {
+export function reverseDirection(direction) {
+  if (direction === PhysicalFlexDirection.LEFT_TO_RIGHT) {
+    return PhysicalFlexDirection.RIGHT_TO_LEFT;
+  }
+  if (direction === PhysicalFlexDirection.RIGHT_TO_LEFT) {
+    return PhysicalFlexDirection.LEFT_TO_RIGHT;
+  }
+  if (direction === PhysicalFlexDirection.TOP_TO_BOTTOM) {
+    return PhysicalFlexDirection.BOTTOM_TO_TOP;
+  }
+  if (direction === PhysicalFlexDirection.BOTTOM_TO_TOP) {
+    return PhysicalFlexDirection.TOP_TO_BOTTOM;
+  }
+  throw new Error('Unknown PhysicalFlexDirection');
+}
+
+/**
+ * @param {!Object.<string, !PhysicalFlexDirection>} directions
+ * @return {!Object.<string, !PhysicalFlexDirection>}
+ */
+function extendWithReverseDirections(directions) {
+  return {
+    ...directions,
+    'row-reverse': reverseDirection(directions.row),
+    'column-reverse': reverseDirection(directions.column),
+  };
+}
+
+/**
+ * Returns absolute directions for row and column values of flex-direction
+ * taking into account the direction and writing-mode attributes.
+ *
+ * @param {!Map<string, string>} computedStyles
+ * @return {!Object.<string, !PhysicalFlexDirection>}
+ */
+export function getPhysicalFlexDirections(computedStyles) {
   const isRtl = computedStyles.get('direction') === 'rtl';
   const writingMode = computedStyles.get('writing-mode');
   const isVertical = writingMode && writingModesAffectingFlexDirection.has(writingMode);
 
-  // Default to LTR and no writing mode.
+  if (isVertical) {
+    return extendWithReverseDirections({
+      row: isRtl ? PhysicalFlexDirection.BOTTOM_TO_TOP : PhysicalFlexDirection.TOP_TO_BOTTOM,
+      column: writingMode === 'vertical-lr' ? PhysicalFlexDirection.LEFT_TO_RIGHT : PhysicalFlexDirection.RIGHT_TO_LEFT
+    });
+  }
+
+  return extendWithReverseDirections({
+    row: isRtl ? PhysicalFlexDirection.RIGHT_TO_LEFT : PhysicalFlexDirection.LEFT_TO_RIGHT,
+    column: PhysicalFlexDirection.TOP_TO_BOTTOM,
+  });
+}
+
+/**
+ * Rotates the flex direction icon in such way that it indicates
+ * the desired `direction` and the arrow in the icon is always at the bottom
+ * or at the right.
+ *
+ * By default, the icon is pointing top-down with the arrow on the right-hand side.
+ *
+ * @param {!PhysicalFlexDirection} direction
+ * @return {!IconInfo}
+ */
+export function rotateFlexDirectionIcon(direction) {
+  // Default to LTR.
   let flipX = true;
   let flipY = false;
   let rotate = -90;
 
-  if (isRtl && !isVertical) {
+  if (direction === PhysicalFlexDirection.RIGHT_TO_LEFT) {
     rotate = 90;
+    flipY = false;
+    flipX = false;
+  } else if (direction === PhysicalFlexDirection.TOP_TO_BOTTOM) {
+    rotate = 0;
     flipX = false;
     flipY = false;
-  }
-
-  if (isRtl && isVertical) {
+  } else if (direction === PhysicalFlexDirection.BOTTOM_TO_TOP) {
     rotate = 0;
     flipX = false;
     flipY = true;
   }
 
-  if (!isRtl && isVertical) {
-    rotate = 0;
-    flipX = false;
-    flipY = false;
-  }
-
-  // The icon is pointing top-down by default with the arrow on the right-hand side.
   return {
     iconName: 'flex-direction-icon',
     rotate: rotate,
@@ -51,11 +113,31 @@ function flexDirectionRowIcon(computedStyles) {
 }
 
 /**
+ *
+ * @param {string} value
+ * @return {function(!Map<string, string>):!IconInfo}
+ */
+function flexDirectionIcon(value) {
+  /**
+   * @param {!Map<string, string>} computedStyles
+   * @return {!IconInfo}
+   */
+  function getIcon(computedStyles) {
+    const directions = getPhysicalFlexDirections(computedStyles);
+    return rotateFlexDirectionIcon(directions[value]);
+  }
+  return getIcon;
+}
+
+/**
  * @type {!Map<string, function(!Map<string, string>):!IconInfo>}
  */
 const textToIconResolver = new Map();
 
-textToIconResolver.set('flex-direction: row', flexDirectionRowIcon);
+textToIconResolver.set('flex-direction: row', flexDirectionIcon('row'));
+textToIconResolver.set('flex-direction: column', flexDirectionIcon('column'));
+textToIconResolver.set('flex-direction: column-reverse', flexDirectionIcon('column-reverse'));
+textToIconResolver.set('flex-direction: row-reverse', flexDirectionIcon('row-reverse'));
 
 /**
  * @param {string} text
--- test/unittests/front_end/elements/CSSPropertyIconResolver_test.ts
+++ test/unittests/front_end/elements/CSSPropertyIconResolver_test.ts
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {findIcon} from '../../../../front_end/elements/CSSPropertyIconResolver.js';
+import {findIcon, getPhysicalFlexDirections, PhysicalFlexDirection, reverseDirection, rotateFlexDirectionIcon} from '../../../../front_end/elements/CSSPropertyIconResolver.js';
 
 const {assert} = chai;
 
@@ -15,151 +15,277 @@ describe('CSSPropertyIconResolver', () => {
     return result;
   }
 
+
+  it('can computed actual directions for row and column', () => {
+    const tests = [
+      {
+        style: {
+          'direction': 'ltr',
+        },
+        expected: {
+          row: PhysicalFlexDirection.LEFT_TO_RIGHT,
+          column: PhysicalFlexDirection.TOP_TO_BOTTOM,
+          'row-reverse': PhysicalFlexDirection.RIGHT_TO_LEFT,
+          'column-reverse': PhysicalFlexDirection.BOTTOM_TO_TOP,
+        },
+      },
+      {
+        style: {
+          'direction': 'ltr',
+          'writing-mode': 'vertical-rl',
+        },
+        expected: {
+          row: PhysicalFlexDirection.TOP_TO_BOTTOM,
+          column: PhysicalFlexDirection.RIGHT_TO_LEFT,
+          'row-reverse': PhysicalFlexDirection.BOTTOM_TO_TOP,
+          'column-reverse': PhysicalFlexDirection.LEFT_TO_RIGHT,
+        },
+      },
+      {
+        style: {
+          'direction': 'ltr',
+          'writing-mode': 'vertical-lr',
+        },
+        expected: {
+          row: PhysicalFlexDirection.TOP_TO_BOTTOM,
+          column: PhysicalFlexDirection.LEFT_TO_RIGHT,
+          'row-reverse': PhysicalFlexDirection.BOTTOM_TO_TOP,
+          'column-reverse': PhysicalFlexDirection.RIGHT_TO_LEFT,
+        },
+      },
+      {
+        style: {
+          'direction': 'ltr',
+          'writing-mode': 'tb',
+        },
+        expected: {
+          row: PhysicalFlexDirection.TOP_TO_BOTTOM,
+          column: PhysicalFlexDirection.RIGHT_TO_LEFT,
+          'row-reverse': PhysicalFlexDirection.BOTTOM_TO_TOP,
+          'column-reverse': PhysicalFlexDirection.LEFT_TO_RIGHT,
+        },
+      },
+      {
+        style: {
+          'direction': 'ltr',
+          'writing-mode': 'tb-rl',
+        },
+        expected: {
+          row: PhysicalFlexDirection.TOP_TO_BOTTOM,
+          column: PhysicalFlexDirection.RIGHT_TO_LEFT,
+          'row-reverse': PhysicalFlexDirection.BOTTOM_TO_TOP,
+          'column-reverse': PhysicalFlexDirection.LEFT_TO_RIGHT,
+        },
+      },
+      {
+        style: {
+          'direction': 'rtl',
+        },
+        expected: {
+          row: PhysicalFlexDirection.RIGHT_TO_LEFT,
+          column: PhysicalFlexDirection.TOP_TO_BOTTOM,
+          'row-reverse': PhysicalFlexDirection.LEFT_TO_RIGHT,
+          'column-reverse': PhysicalFlexDirection.BOTTOM_TO_TOP,
+        },
+      },
+      {
+        style: {
+          'direction': 'rtl',
+          'writing-mode': 'vertical-rl',
+        },
+        expected: {
+          row: PhysicalFlexDirection.BOTTOM_TO_TOP,
+          column: PhysicalFlexDirection.RIGHT_TO_LEFT,
+          'row-reverse': PhysicalFlexDirection.TOP_TO_BOTTOM,
+          'column-reverse': PhysicalFlexDirection.LEFT_TO_RIGHT,
+        },
+      },
+      {
+        style: {
+          'direction': 'rtl',
+          'writing-mode': 'vertical-lr',
+        },
+        expected: {
+          row: PhysicalFlexDirection.BOTTOM_TO_TOP,
+          column: PhysicalFlexDirection.LEFT_TO_RIGHT,
+          'row-reverse': PhysicalFlexDirection.TOP_TO_BOTTOM,
+          'column-reverse': PhysicalFlexDirection.RIGHT_TO_LEFT,
+        },
+      },
+      {
+        style: {
+          'direction': 'rtl',
+          'writing-mode': 'tb',
+        },
+        expected: {
+          row: PhysicalFlexDirection.BOTTOM_TO_TOP,
+          column: PhysicalFlexDirection.RIGHT_TO_LEFT,
+          'row-reverse': PhysicalFlexDirection.TOP_TO_BOTTOM,
+          'column-reverse': PhysicalFlexDirection.LEFT_TO_RIGHT,
+        },
+      },
+      {
+        style: {
+          'direction': 'rtl',
+          'writing-mode': 'tb-rl',
+        },
+        expected: {
+          row: PhysicalFlexDirection.BOTTOM_TO_TOP,
+          column: PhysicalFlexDirection.RIGHT_TO_LEFT,
+          'row-reverse': PhysicalFlexDirection.TOP_TO_BOTTOM,
+          'column-reverse': PhysicalFlexDirection.LEFT_TO_RIGHT,
+        },
+      },
+    ];
+
+    for (const test of tests) {
+      assert.deepEqual(
+          getPhysicalFlexDirections(mapFromStyle(test.style)), test.expected,
+          `Test ${JSON.stringify(test.style)} failed.`);
+    }
+  });
+
+  it('can rotate the icon', () => {
+    assert.deepEqual(rotateFlexDirectionIcon(PhysicalFlexDirection.LEFT_TO_RIGHT), {
+      iconName: 'flex-direction-icon',
+      rotate: -90,
+      scaleX: -1,
+      scaleY: 1,
+    });
+    assert.deepEqual(rotateFlexDirectionIcon(PhysicalFlexDirection.RIGHT_TO_LEFT), {
+      iconName: 'flex-direction-icon',
+      rotate: 90,
+      scaleX: 1,
+      scaleY: 1,
+    });
+    assert.deepEqual(rotateFlexDirectionIcon(PhysicalFlexDirection.TOP_TO_BOTTOM), {
+      iconName: 'flex-direction-icon',
+      rotate: 0,
+      scaleX: 1,
+      scaleY: 1,
+    });
+    assert.deepEqual(rotateFlexDirectionIcon(PhysicalFlexDirection.BOTTOM_TO_TOP), {
+      iconName: 'flex-direction-icon',
+      rotate: 0,
+      scaleX: 1,
+      scaleY: -1,
+    });
+  });
+
   it('can find an icon for flex-direction row', () => {
     const tests = [
       {
-        text: 'flex-direction: row',
         style: {
           'direction': 'ltr',
         },
-        expected: {
-          // left to right
-          iconName: 'flex-direction-icon',
-          rotate: -90,
-          scaleX: -1,
-          scaleY: 1,
-        },
+        expected: PhysicalFlexDirection.LEFT_TO_RIGHT,
       },
       {
-        text: 'flex-direction: row',
         style: {
           'direction': 'ltr',
           'writing-mode': 'tb',
         },
-        expected: {
-          // top to bottom
-          iconName: 'flex-direction-icon',
-          rotate: 0,
-          scaleX: 1,
-          scaleY: 1,
-        },
+        expected: PhysicalFlexDirection.TOP_TO_BOTTOM,
       },
       {
-        text: 'flex-direction: row',
         style: {
           'direction': 'ltr',
           'writing-mode': 'vertical-lr',
         },
-        expected: {
-          // top to bottom
-          iconName: 'flex-direction-icon',
-          rotate: 0,
-          scaleX: 1,
-          scaleY: 1,
-        },
+        expected: PhysicalFlexDirection.TOP_TO_BOTTOM,
       },
       {
-        text: 'flex-direction: row',
         style: {
           'direction': 'ltr',
           'writing-mode': 'vertical-rl',
         },
-        expected: {
-          // top to bottom
-          iconName: 'flex-direction-icon',
-          rotate: 0,
-          scaleX: 1,
-          scaleY: 1,
-        },
+        expected: PhysicalFlexDirection.TOP_TO_BOTTOM,
       },
       {
-        text: 'flex-direction: row',
         style: {
           'direction': 'ltr',
           'writing-mode': 'tb-rl',
         },
-        expected: {
-          // top to bottom
-          iconName: 'flex-direction-icon',
-          rotate: 0,
-          scaleX: 1,
-          scaleY: 1,
-        },
+        expected: PhysicalFlexDirection.TOP_TO_BOTTOM,
       },
       {
-        text: 'flex-direction: row',
         style: {
           'direction': 'rtl',
         },
-        expected: {
-          // right to left
-          iconName: 'flex-direction-icon',
-          rotate: 90,
-          scaleX: 1,
-          scaleY: 1,
-        },
+        expected: PhysicalFlexDirection.RIGHT_TO_LEFT,
       },
       {
-        text: 'flex-direction: row',
         style: {
           'direction': 'rtl',
           'writing-mode': 'tb',
         },
-        expected: {
-          // bottom to top
-          iconName: 'flex-direction-icon',
-          rotate: 0,
-          scaleX: 1,
-          scaleY: -1,
-        },
+        expected: PhysicalFlexDirection.BOTTOM_TO_TOP,
       },
       {
-        text: 'flex-direction: row',
         style: {
           'direction': 'rtl',
           'writing-mode': 'vertical-lr',
         },
-        expected: {
-          // bottom to top
-          iconName: 'flex-direction-icon',
-          rotate: 0,
-          scaleX: 1,
-          scaleY: -1,
-        },
+        expected: PhysicalFlexDirection.BOTTOM_TO_TOP,
       },
       {
-        text: 'flex-direction: row',
         style: {
           'direction': 'rtl',
           'writing-mode': 'vertical-rl',
         },
-        expected: {
-          // bottom to top
-          iconName: 'flex-direction-icon',
-          rotate: 0,
-          scaleX: 1,
-          scaleY: -1,
-        },
+        expected: PhysicalFlexDirection.BOTTOM_TO_TOP,
       },
       {
-        text: 'flex-direction: row',
         style: {
           'direction': 'rtl',
           'writing-mode': 'tb-rl',
         },
-        expected: {
-          // bottom to top
-          iconName: 'flex-direction-icon',
-          rotate: 0,
-          scaleX: 1,
-          scaleY: -1,
-        },
+        expected: PhysicalFlexDirection.BOTTOM_TO_TOP,
       },
     ];
     for (const test of tests) {
       assert.deepEqual(
-          findIcon(test.text, mapFromStyle(test.style)), test.expected,
-          `Test ${test.text} (${JSON.stringify(test.style)}) failed.`);
+          findIcon('flex-direction: row', mapFromStyle(test.style)), rotateFlexDirectionIcon(test.expected),
+          `Test 'flex-direction: row'(${JSON.stringify(test.style)}) failed.`);
+
+      assert.deepEqual(
+          findIcon('flex-direction: row-reverse', mapFromStyle(test.style)),
+          rotateFlexDirectionIcon(reverseDirection(test.expected)),
+          `Test 'flex-direction: row-reverse'(${JSON.stringify(test.style)}) failed.`);
+    }
+  });
+
+  it('can find an icon for flex-direction: column and column-reverse', () => {
+    const tests = [
+      {
+        style: {
+          'direction': 'ltr',
+        },
+        expected: PhysicalFlexDirection.TOP_TO_BOTTOM,
+      },
+      {
+        style: {
+          'writing-mode': 'vertical-rl',
+        },
+        expected: PhysicalFlexDirection.RIGHT_TO_LEFT,
+      },
+      {
+        style: {
+          'writing-mode': 'vertical-lr',
+        },
+        expected: PhysicalFlexDirection.LEFT_TO_RIGHT,
+      },
+    ];
+
+    for (const test of tests) {
+      assert.deepEqual(
+          findIcon('flex-direction: column', mapFromStyle(test.style)), rotateFlexDirectionIcon(test.expected),
+          `Test 'flex-direction: column'(${JSON.stringify(test.style)}) failed.`);
+
+      assert.deepEqual(
+          findIcon('flex-direction: column-reverse', mapFromStyle(test.style)),
+          rotateFlexDirectionIcon(reverseDirection(test.expected)),
+          `Test 'flex-direction: column-reverse'(${JSON.stringify(test.style)}) failed.`);
     }
   });
 });

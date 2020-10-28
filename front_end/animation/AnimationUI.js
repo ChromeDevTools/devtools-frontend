7a518f6f9b527da83fd3a7bed85a1804c1a6c678
Paul Lewis <aerotwist@chromium.org>
2020-10-28 11:29:30 +0000

    TypeScriptify AnimationUI.js
    R=tvanderlippe@chromium.org
    
    Bug: 1011811
    Change-Id: I7af4ae1162076f6920a8bb13b1288f845dbe0859
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2484971
    Commit-Queue: Paul Lewis <aerotwist@chromium.org>
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>

--- front_end/animation/AnimationUI.js
+++ front_end/animation/AnimationUI.js
@@ -1,8 +1,6 @@
 // Copyright (c) 2015 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-// @ts-nocheck
-// TODO(crbug.com/1011811): Enable TypeScript compiler checks
 
 import * as Common from '../common/common.js';
 import * as InlineEditor from '../inline_editor/inline_editor.js';
@@ -26,25 +24,27 @@ export class AnimationUI {
     this._timeline = timeline;
     this._parentElement = parentElement;
 
-    if (this._animation.source().keyframesRule()) {
-      this._keyframes = this._animation.source().keyframesRule().keyframes();
+    const keyframesRule = this._animation.source().keyframesRule();
+    if (keyframesRule) {
+      this._keyframes = keyframesRule.keyframes();
     }
 
-    this._nameElement = parentElement.createChild('div', 'animation-name');
+    /** @type {!HTMLElement} */
+    this._nameElement = /** @type {!HTMLElement} */ (parentElement.createChild('div', 'animation-name'));
     this._nameElement.textContent = this._animation.name();
 
-    this._svg = parentElement.createSVGChild('svg', 'animation-ui');
-    this._svg.setAttribute('height', Options.AnimationSVGHeight);
+    this._svg = /** @type {!HTMLElement} */ (parentElement).createSVGChild('svg', 'animation-ui');
+    this._svg.setAttribute('height', Options.AnimationSVGHeight.toString());
     this._svg.style.marginLeft = '-' + Options.AnimationMargin + 'px';
     this._svg.addEventListener('contextmenu', this._onContextMenu.bind(this));
     this._activeIntervalGroup = this._svg.createSVGChild('g');
     UI.UIUtils.installDragHandle(
         this._activeIntervalGroup, this._mouseDown.bind(this, Events.AnimationDrag, null), this._mouseMove.bind(this),
         this._mouseUp.bind(this), '-webkit-grabbing', '-webkit-grab');
-    Animation.AnimationUI.installDragHandleKeyboard(
-        this._activeIntervalGroup, this._keydownMove.bind(this, Animation.AnimationUI.Events.AnimationDrag, null));
+    AnimationUI.installDragHandleKeyboard(
+        this._activeIntervalGroup, this._keydownMove.bind(this, Events.AnimationDrag, null));
 
-    /** @type {!Array.<{group: ?Element, animationLine: ?Element, keyframePoints: !Object.<number, !Element>, keyframeRender: !Object.<number, !Element>}>} */
+    /** @type {!Array.<{group: ?HTMLElement, animationLine: ?HTMLElement, keyframePoints: !Object.<number, !HTMLElement>, keyframeRender: !Object.<number, !HTMLElement>}>} */
     this._cachedElements = [];
 
     this._movementInMs = 0;
@@ -57,14 +57,20 @@ export class AnimationUI {
    * @return {string}
    */
   static Color(animation) {
-    const names = Object.keys(Colors);
-    const color = Colors[names[String.hashCode(animation.name() || animation.id()) % names.length]];
-    return color.asString(Common.Color.Format.RGB);
+    const names = Array.from(Colors.keys());
+    const hashCode = String.hashCode(animation.name() || animation.id());
+    const cappedHashCode = hashCode % names.length;
+    const colorName = names[cappedHashCode];
+    const color = Colors.get(colorName);
+    if (!color) {
+      throw new Error('Unable to locate color');
+    }
+    return color.asString(Common.Color.Format.RGB) || '';
   }
 
   /**
    * @param {!Element} element
-   * @param {function(...?)} elementDrag
+   * @param {function(!Event):void} elementDrag
    */
   static installDragHandleKeyboard(element, elementDrag) {
     element.addEventListener('keydown', elementDrag, false);
@@ -85,21 +91,21 @@ export class AnimationUI {
   }
 
   /**
-   * @param {!Element} parentElement
+   * @param {!HTMLElement} parentElement
    * @param {string} className
    */
   _createLine(parentElement, className) {
     const line = parentElement.createSVGChild('line', className);
-    line.setAttribute('x1', Options.AnimationMargin);
-    line.setAttribute('y1', Options.AnimationHeight);
-    line.setAttribute('y2', Options.AnimationHeight);
+    line.setAttribute('x1', Options.AnimationMargin.toString());
+    line.setAttribute('y1', Options.AnimationHeight.toString());
+    line.setAttribute('y2', Options.AnimationHeight.toString());
     line.style.stroke = this._color;
     return line;
   }
 
   /**
    * @param {number} iteration
-   * @param {!Element} parentElement
+   * @param {!HTMLElement} parentElement
    */
   _drawAnimationLine(iteration, parentElement) {
     const cache = this._cachedElements[iteration];
@@ -111,17 +117,17 @@ export class AnimationUI {
   }
 
   /**
-   * @param {!Element} parentElement
+   * @param {!HTMLElement} parentElement
    */
   _drawDelayLine(parentElement) {
-    if (!this._delayLine) {
+    if (!this._delayLine || !this._endDelayLine) {
       this._delayLine = this._createLine(parentElement, 'animation-delay-line');
       this._endDelayLine = this._createLine(parentElement, 'animation-delay-line');
     }
     const fill = this._animation.source().fill();
     this._delayLine.classList.toggle('animation-fill', fill === 'backwards' || fill === 'both');
     const margin = Options.AnimationMargin;
-    this._delayLine.setAttribute('x1', margin);
+    this._delayLine.setAttribute('x1', margin.toString());
     this._delayLine.setAttribute('x2', (this._delay() * this._timeline.pixelMsRatio() + margin).toFixed(2));
     const forwardsFill = fill === 'forwards' || fill === 'both';
     this._endDelayLine.classList.toggle('animation-fill', forwardsFill);
@@ -129,7 +135,7 @@ export class AnimationUI {
         this._timeline.width(),
         (this._delay() + this._duration() * this._animation.source().iterations()) * this._timeline.pixelMsRatio());
     this._endDelayLine.style.transform = 'translateX(' + leftMargin.toFixed(2) + 'px)';
-    this._endDelayLine.setAttribute('x1', margin);
+    this._endDelayLine.setAttribute('x1', margin.toString());
     this._endDelayLine.setAttribute(
         'x2', forwardsFill ? (this._timeline.width() - leftMargin + margin).toFixed(2) :
                              (this._animation.source().endDelay() * this._timeline.pixelMsRatio() + margin).toFixed(2));
@@ -149,11 +155,12 @@ export class AnimationUI {
     }
 
     const circle =
-        parentElement.createSVGChild('circle', keyframeIndex <= 0 ? 'animation-endpoint' : 'animation-keyframe-point');
+        /** @type {!HTMLElement} */ (parentElement)
+            .createSVGChild('circle', keyframeIndex <= 0 ? 'animation-endpoint' : 'animation-keyframe-point');
     circle.setAttribute('cx', x.toFixed(2));
-    circle.setAttribute('cy', Options.AnimationHeight);
+    circle.setAttribute('cy', Options.AnimationHeight.toString());
     circle.style.stroke = this._color;
-    circle.setAttribute('r', Options.AnimationMargin / 2);
+    circle.setAttribute('r', (Options.AnimationMargin / 2).toString());
     circle.tabIndex = 0;
     UI.ARIAUtils.setAccessibleName(
         circle, keyframeIndex <= 0 ? ls`Animation Endpoint slider` : ls`Animation Keyframe slider`);
@@ -162,12 +169,13 @@ export class AnimationUI {
       circle.style.fill = this._color;
     }
 
-    this._cachedElements[iteration].keyframePoints[keyframeIndex] = circle;
+    this._cachedElements[iteration].keyframePoints[keyframeIndex] = /** @type {!HTMLElement} */ (circle);
 
     if (!attachEvents) {
       return;
     }
 
+    /** @type {string} */
     let eventType;
     if (keyframeIndex === 0) {
       eventType = Events.StartEndpointMove;
@@ -179,37 +187,38 @@ export class AnimationUI {
     UI.UIUtils.installDragHandle(
         circle, this._mouseDown.bind(this, eventType, keyframeIndex), this._mouseMove.bind(this),
         this._mouseUp.bind(this), 'ew-resize');
-    Animation.AnimationUI.installDragHandleKeyboard(circle, this._keydownMove.bind(this, eventType, keyframeIndex));
+    AnimationUI.installDragHandleKeyboard(circle, this._keydownMove.bind(this, eventType, keyframeIndex));
   }
 
   /**
    * @param {number} iteration
    * @param {number} keyframeIndex
-   * @param {!Element} parentElement
+   * @param {!HTMLElement} parentElement
    * @param {number} leftDistance
    * @param {number} width
    * @param {string} easing
    */
   _renderKeyframe(iteration, keyframeIndex, parentElement, leftDistance, width, easing) {
     /**
-     * @param {!Element} parentElement
+     * @param {!HTMLElement} parentElement
      * @param {number} x
      * @param {string} strokeColor
      */
     function createStepLine(parentElement, x, strokeColor) {
       const line = parentElement.createSVGChild('line');
-      line.setAttribute('x1', x);
-      line.setAttribute('x2', x);
-      line.setAttribute('y1', Options.AnimationMargin);
-      line.setAttribute('y2', Options.AnimationHeight);
+      line.setAttribute('x1', x.toString());
+      line.setAttribute('x2', x.toString());
+      line.setAttribute('y1', Options.AnimationMargin.toString());
+      line.setAttribute('y2', Options.AnimationHeight.toString());
       line.style.stroke = strokeColor;
     }
 
     const bezier = UI.Geometry.CubicBezier.parse(easing);
     const cache = this._cachedElements[iteration].keyframeRender;
     if (!cache[keyframeIndex]) {
-      cache[keyframeIndex] = bezier ? parentElement.createSVGChild('path', 'animation-keyframe') :
-                                      parentElement.createSVGChild('g', 'animation-keyframe-step');
+      cache[keyframeIndex] = /** @type {!HTMLElement} */ (
+          bezier ? parentElement.createSVGChild('path', 'animation-keyframe') :
+                   parentElement.createSVGChild('g', 'animation-keyframe-step'));
     }
     const group = cache[keyframeIndex];
     group.tabIndex = 0;
@@ -227,10 +236,13 @@ export class AnimationUI {
     } else {
       const stepFunction = StepTimingFunction.parse(easing);
       group.removeChildren();
-      /** @const */ const offsetMap = {'start': 0, 'middle': 0.5, 'end': 1};
-      /** @const */ const offsetWeight = offsetMap[stepFunction.stepAtPosition];
-      for (let i = 0; i < stepFunction.steps; i++) {
-        createStepLine(group, (i + offsetWeight) * width / stepFunction.steps, this._color);
+      /**  @type {!Object.<string, number>} */
+      const offsetMap = {'start': 0, 'middle': 0.5, 'end': 1};
+      if (stepFunction) {
+        const offsetWeight = offsetMap[stepFunction.stepAtPosition];
+        for (let i = 0; i < stepFunction.steps; i++) {
+          createStepLine(group, (i + offsetWeight) * width / stepFunction.steps, this._color);
+        }
       }
     }
   }
@@ -245,7 +257,7 @@ export class AnimationUI {
     this._nameElement.style.transform =
         'translateX(' + (this._delay() * this._timeline.pixelMsRatio() + Options.AnimationMargin).toFixed(2) + 'px)';
     this._nameElement.style.width = (this._duration() * this._timeline.pixelMsRatio()).toFixed(2) + 'px';
-    this._drawDelayLine(this._svg);
+    this._drawDelayLine(/** @type {!HTMLElement} */ (this._svg));
 
     if (this._animation.type() === 'CSSTransition') {
       this._renderTransition();
@@ -264,22 +276,25 @@ export class AnimationUI {
       this._renderIteration(this._tailGroup, iteration);
     }
     while (iteration < this._cachedElements.length) {
-      this._cachedElements.pop().group.remove();
+      const poppedElement = this._cachedElements.pop();
+      if (poppedElement && poppedElement.group) {
+        poppedElement.group.remove();
+      }
     }
   }
 
   _renderTransition() {
+    const activeIntervalGroup = /** @type {!HTMLElement} */ (this._activeIntervalGroup);
     if (!this._cachedElements[0]) {
       this._cachedElements[0] = {animationLine: null, keyframePoints: {}, keyframeRender: {}, group: null};
     }
-    this._drawAnimationLine(0, this._activeIntervalGroup);
+    this._drawAnimationLine(0, activeIntervalGroup);
     this._renderKeyframe(
-        0, 0, this._activeIntervalGroup, Options.AnimationMargin, this._duration() * this._timeline.pixelMsRatio(),
+        0, 0, activeIntervalGroup, Options.AnimationMargin, this._duration() * this._timeline.pixelMsRatio(),
         this._animation.source().easing());
-    this._drawPoint(0, this._activeIntervalGroup, Options.AnimationMargin, 0, true);
+    this._drawPoint(0, activeIntervalGroup, Options.AnimationMargin, 0, true);
     this._drawPoint(
-        0, this._activeIntervalGroup, this._duration() * this._timeline.pixelMsRatio() + Options.AnimationMargin, -1,
-        true);
+        0, activeIntervalGroup, this._duration() * this._timeline.pixelMsRatio() + Options.AnimationMargin, -1, true);
   }
 
   /**
@@ -288,20 +303,27 @@ export class AnimationUI {
    */
   _renderIteration(parentElement, iteration) {
     if (!this._cachedElements[iteration]) {
+      const group = /** @type {!HTMLElement} */ (parentElement).createSVGChild('g');
       this._cachedElements[iteration] =
-          {animationLine: null, keyframePoints: {}, keyframeRender: {}, group: parentElement.createSVGChild('g')};
+          {animationLine: null, keyframePoints: {}, keyframeRender: {}, group: /** @type {!HTMLElement} */ (group)};
     }
     const group = this._cachedElements[iteration].group;
+    if (!group) {
+      return;
+    }
+
     group.style.transform =
         'translateX(' + (iteration * this._duration() * this._timeline.pixelMsRatio()).toFixed(2) + 'px)';
     this._drawAnimationLine(iteration, group);
-    console.assert(this._keyframes.length > 1);
-    for (let i = 0; i < this._keyframes.length - 1; i++) {
-      const leftDistance = this._offset(i) * this._duration() * this._timeline.pixelMsRatio() + Options.AnimationMargin;
-      const width = this._duration() * (this._offset(i + 1) - this._offset(i)) * this._timeline.pixelMsRatio();
-      this._renderKeyframe(iteration, i, group, leftDistance, width, this._keyframes[i].easing());
-      if (i || (!i && iteration === 0)) {
-        this._drawPoint(iteration, group, leftDistance, i, iteration === 0);
+    if (this._keyframes && this._keyframes.length > 1) {
+      for (let i = 0; i < this._keyframes.length - 1; i++) {
+        const leftDistance =
+            this._offset(i) * this._duration() * this._timeline.pixelMsRatio() + Options.AnimationMargin;
+        const width = this._duration() * (this._offset(i + 1) - this._offset(i)) * this._timeline.pixelMsRatio();
+        this._renderKeyframe(iteration, i, group, leftDistance, width, this._keyframes[i].easing());
+        if (i || (!i && iteration === 0)) {
+          this._drawPoint(iteration, group, leftDistance, i, iteration === 0);
+        }
       }
     }
     this._drawPoint(
@@ -340,6 +362,10 @@ export class AnimationUI {
    * @return {number} offset
    */
   _offset(i) {
+    if (!this._keyframes) {
+      throw new Error('Unable to calculate offset; keyframes do not exist');
+    }
+
     let offset = this._keyframes[i].offsetAsNumber();
     if (this._mouseEventType === Events.KeyframeMove && i === this._keyframeMoved) {
       console.assert(i > 0 && i < this._keyframes.length - 1, 'First and last keyframe cannot be moved');
@@ -356,7 +382,8 @@ export class AnimationUI {
    * @param {!Event} event
    */
   _mouseDown(mouseEventType, keyframeIndex, event) {
-    if (event.buttons === 2) {
+    const mouseEvent = /** @type {!MouseEvent} */ (event);
+    if (mouseEvent.buttons === 2) {
       return false;
     }
     if (this._svg.enclosingNodeOrSelfWithClass('animation-node-removed')) {
@@ -364,7 +391,7 @@ export class AnimationUI {
     }
     this._mouseEventType = mouseEventType;
     this._keyframeMoved = keyframeIndex;
-    this._downMouseX = event.clientX;
+    this._downMouseX = mouseEvent.clientX;
     event.consume(true);
     if (this._node) {
       Common.Revealer.reveal(this._node);
@@ -376,7 +403,8 @@ export class AnimationUI {
    * @param {!Event} event
    */
   _mouseMove(event) {
-    this._setMovementAndRedraw((event.clientX - this._downMouseX) / this._timeline.pixelMsRatio());
+    const mouseEvent = /** @type {!MouseEvent} */ (event);
+    this._setMovementAndRedraw((mouseEvent.clientX - (this._downMouseX || 0)) / this._timeline.pixelMsRatio());
   }
 
   /**
@@ -394,11 +422,14 @@ export class AnimationUI {
    * @param {!Event} event
    */
   _mouseUp(event) {
-    this._movementInMs = (event.clientX - this._downMouseX) / this._timeline.pixelMsRatio();
+    const mouseEvent = /** @type {!MouseEvent} */ (event);
+    this._movementInMs = (mouseEvent.clientX - (this._downMouseX || 0)) / this._timeline.pixelMsRatio();
 
     // Commit changes
     if (this._mouseEventType === Events.KeyframeMove) {
-      this._keyframes[this._keyframeMoved].setOffset(this._offset(this._keyframeMoved));
+      if (this._keyframes && this._keyframeMoved !== null && typeof this._keyframeMoved !== 'undefined') {
+        this._keyframes[this._keyframeMoved].setOffset(this._offset(this._keyframeMoved));
+      }
     } else {
       this._animation.setTiming(this._duration(), this._delay());
     }
@@ -411,10 +442,16 @@ export class AnimationUI {
     delete this._keyframeMoved;
   }
 
+  /**
+   * @param {!Events} mouseEventType
+   * @param {?number} keyframeIndex
+   * @param {!Event} event
+   */
   _keydownMove(mouseEventType, keyframeIndex, event) {
+    const keyboardEvent = /** @type {!KeyboardEvent} */ (event);
     this._mouseEventType = mouseEventType;
     this._keyframeMoved = keyframeIndex;
-    switch (event.key) {
+    switch (keyboardEvent.key) {
       case 'ArrowLeft':
       case 'ArrowUp':
         this._movementInMs = -this._keyboardMovementRateMs;
@@ -426,8 +463,10 @@ export class AnimationUI {
       default:
         return;
     }
-    if (this._mouseEventType === Animation.AnimationUI.Events.KeyframeMove) {
-      this._keyframes[this._keyframeMoved].setOffset(this._offset(this._keyframeMoved));
+    if (this._mouseEventType === Events.KeyframeMove) {
+      if (this._keyframes && this._keyframeMoved !== null) {
+        this._keyframes[this._keyframeMoved].setOffset(this._offset(this._keyframeMoved));
+      }
     } else {
       this._animation.setTiming(this._duration(), this._delay());
     }
@@ -478,15 +517,11 @@ export const Options = {
   GridCanvasHeight: 40
 };
 
-export const Colors = {
-  'Purple': Common.Color.Color.parse('#9C27B0'),
-  'Light Blue': Common.Color.Color.parse('#03A9F4'),
-  'Deep Orange': Common.Color.Color.parse('#FF5722'),
-  'Blue': Common.Color.Color.parse('#5677FC'),
-  'Lime': Common.Color.Color.parse('#CDDC39'),
-  'Blue Grey': Common.Color.Color.parse('#607D8B'),
-  'Pink': Common.Color.Color.parse('#E91E63'),
-  'Green': Common.Color.Color.parse('#0F9D58'),
-  'Brown': Common.Color.Color.parse('#795548'),
-  'Cyan': Common.Color.Color.parse('#00BCD4')
-};
+/** @type {!Map<string, ?Common.Color.Color>} */
+export const Colors = new Map([
+  ['Purple', Common.Color.Color.parse('#9C27B0')], ['Light Blue', Common.Color.Color.parse('#03A9F4')],
+  ['Deep Orange', Common.Color.Color.parse('#FF5722')], ['Blue', Common.Color.Color.parse('#5677FC')],
+  ['Lime', Common.Color.Color.parse('#CDDC39')], ['Blue Grey', Common.Color.Color.parse('#607D8B')],
+  ['Pink', Common.Color.Color.parse('#E91E63')], ['Green', Common.Color.Color.parse('#0F9D58')],
+  ['Brown', Common.Color.Color.parse('#795548')], ['Cyan', Common.Color.Color.parse('#00BCD4')]
+]);

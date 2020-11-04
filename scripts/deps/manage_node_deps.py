5f6c0652cc712b5959f42b9c1745c8e6635f80c3
Simon Zünd <szuend@chromium.org>
2020-11-04 12:51:42 +0100

    Update TypeScript to 4.1.1-rc
    DISABLE_THIRD_PARTY_CHECK="TypeScript update"
    
    R=tvanderlippe@chromium.org
    
    Bug: chromium:1145510
    Change-Id: I79473360bc94087d19b5b88091f6255d490c1485
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2517570
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>

--- scripts/deps/manage_node_deps.py
+++ scripts/deps/manage_node_deps.py
@@ -67,7 +67,7 @@ DEPS = {
     "source-map-support": "0.5.19",
     "stylelint": "13.5.0",
     "stylelint-config-standard": "20.0.0",
-    "typescript": "4.1.0-beta",
+    "typescript": "4.1.1-rc",
     "yargs": "15.3.1",
 }
 

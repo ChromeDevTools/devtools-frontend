7ab9a99ba524090d086cf3a16d8ec9c4cb6776f2
Paul Lewis <aerotwist@chromium.org>
2020-11-16 17:32:13 +0000

    TypeScriptify register-custom-element.js
    R=jacktfranklin@chromium.org
    
    Bug: 1011811
    Change-Id: I4865b0b7e66817dadb043b9a9c9c254c4024ef7a
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2540559
    Auto-Submit: Paul Lewis <aerotwist@chromium.org>
    Commit-Queue: Jack Franklin <jacktfranklin@chromium.org>
    Reviewed-by: Jack Franklin <jacktfranklin@chromium.org>

--- front_end/ui/utils/register-custom-element.js
+++ front_end/ui/utils/register-custom-element.js
@@ -2,9 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// @ts-nocheck
-// TODO(crbug.com/1011811): Enable TypeScript compiler checks
-
 /**
  * @param {string} localName
  * @param {string} typeExtension
@@ -15,10 +12,13 @@
 export function registerCustomElement(localName, typeExtension, definition) {
   self.customElements.define(typeExtension, class extends definition {
     constructor() {
+      // The JSDoc above does not allow the super call to have no params, but
+      // it seems to be the nearest to something both Closure and TS understand.
+      // @ts-ignore crbug.com/1011811: Fix after Closure has been removed.
       super();
       // TODO(einbinder) convert to classes and custom element tags
       this.setAttribute('is', typeExtension);
     }
   }, {extends: localName});
-  return () => createElement(localName, typeExtension);
+  return () => document.createElement(localName, {is: typeExtension});
 }

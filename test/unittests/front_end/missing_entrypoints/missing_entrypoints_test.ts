57ce4c104484bb2b342d736c10b0426e7d37ae76
Paul Lewis <aerotwist@chromium.org>
2020-11-17 17:05:59 +0000

    [unit test] Adds missing entrypoints
    We don't currently import all entrypoints for testing, which in turn
    means that our coverage report is incomplete.
    
    This CL adds the remaining imports such that we get a complete coverage
    report. In time the plan is to move to importing
    front_end/devtools_app.js as the canonical, single entrypoint, and avoid
    the need to list out all entrypoints here.
    
    R=tvanderlippe@chromium.org
    
    Bug: 1148296
    Change-Id: Iffa183d36b764d4465f20c4e38227f930f188d62
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2544762
    Auto-Submit: Paul Lewis <aerotwist@chromium.org>
    Commit-Queue: Tim van der Lippe <tvanderlippe@chromium.org>
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>

---
+++ test/unittests/front_end/missing_entrypoints/missing_entrypoints_test.ts
@@ -0,0 +1,51 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// This file exists to ensure that entrypoints that we don't currently have unit
+// tests for are at least imported. This means that they will then show up in
+// the coverage reporting.
+
+import {describeWithEnvironment} from '../helpers/EnvironmentHelpers.js';
+
+describeWithEnvironment('Imports entrypoints that are yet to be tested', () => {
+  before(async () => {
+    await import('../../../../front_end/browser_debugger/browser_debugger.js');
+    await import('../../../../front_end/changes/changes.js');
+    await import('../../../../front_end/cm/cm.js');
+    await import('../../../../front_end/cm_headless/cm_headless.js');
+    await import('../../../../front_end/cm_modes/cm_modes.js');
+    await import('../../../../front_end/console/console.js');
+    await import('../../../../front_end/console_counters/console_counters.js');
+    await import('../../../../front_end/css_overview/css_overview.js');
+    await import('../../../../front_end/developer_resources/developer_resources.js');
+    await import('../../../../front_end/heap_snapshot_model/heap_snapshot_model.js');
+    await import('../../../../front_end/heap_snapshot_worker/heap_snapshot_worker.js');
+    await import('../../../../front_end/help/help.js');
+    await import('../../../../front_end/input/input.js');
+    await import('../../../../front_end/inspector_main/inspector_main.js');
+    await import('../../../../front_end/javascript_metadata/javascript_metadata.js');
+    await import('../../../../front_end/js_main/js_main.js');
+    await import('../../../../front_end/layer_viewer/layer_viewer.js');
+    await import('../../../../front_end/layers/layers.js');
+    await import('../../../../front_end/lighthouse/lighthouse.js');
+    await import('../../../../front_end/main/main.js');
+    await import('../../../../front_end/node_main/node_main.js');
+    await import('../../../../front_end/performance_monitor/performance_monitor.js');
+    await import('../../../../front_end/profiler/profiler.js');
+    await import('../../../../front_end/protocol_monitor/protocol_monitor.js');
+    await import('../../../../front_end/resources/resources.js');
+    await import('../../../../front_end/screencast/screencast.js');
+    await import('../../../../front_end/security/security.js');
+    await import('../../../../front_end/services/services.js');
+    await import('../../../../front_end/settings/settings.js');
+    await import('../../../../front_end/startup/startup.js');
+    await import('../../../../front_end/services/services.js');
+    await import('../../../../front_end/toolbox_bootstrap/toolbox_bootstrap.js');
+    await import('../../../../front_end/wasmparser_worker/wasmparser_worker.js');
+    await import('../../../../front_end/web_audio/web_audio.js');
+    await import('../../../../front_end/webauthn/webauthn.js');
+    await import('../../../../front_end/worker_main/worker_main.js');
+    await import('../../../../front_end/worker_service/worker_service.js');
+  });
+});

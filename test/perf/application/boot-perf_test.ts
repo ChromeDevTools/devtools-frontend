d2517fedecf85e747d84b4c9dc34e3dd842f7376
Paul Lewis <aerotwist@chromium.org>
2020-11-18 14:58:52 +0000

    [perf] Restore boot perf test
    We updated the e2e tests to using a different architecture a while ago
    but we didn't update the boot perf tests to match. This CL migrates
    them to using node_ts_library in the same way as e2e, sets the output
    directory to be perf-data in the root of front_end, and ensures that
    the JSON file contains mean and percentile data.
    
    Change-Id: I0738ae664e52ec7a8fa76173a0155e14f583259f
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2546708
    Auto-Submit: Paul Lewis <aerotwist@chromium.org>
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>
    Commit-Queue: Paul Lewis <aerotwist@chromium.org>

--- test/perf/application/boot-perf_test.ts
+++ test/perf/application/boot-perf_test.ts
@@ -8,34 +8,45 @@ import {reloadDevTools} from '../../shared/helper.js';
 import {mean, percentile, storeGeneratedResults} from '../helpers/perf-helper.js';
 
 interface PerfTimings {
-  [name: string]: number[]
+  bootperf: number[], mean: number, percentile_50: number, percentile_90: number, percentile_99: number
 }
 
+const RUNS = 37;
+
 describe('Boot performance', () => {
-  const total = 37;
   const times: PerfTimings = {
     bootperf: [],
+    mean: 0,
+    percentile_50: 0,
+    percentile_90: 0,
+    percentile_99: 0,
   };
 
   after(async () => {
-    await storeGeneratedResults('devtools-perf.json', JSON.stringify(times));
-
     /* eslint-disable no-console */
     const values = times.bootperf;
-    console.log(`Mean boot time: ${mean(values).toFixed(2)}ms`);
-    console.log(`50th percentile boot time: ${percentile(values, 0.5).toFixed(2)}ms`);
-    console.log(`90th percentile boot time: ${percentile(values, 0.9).toFixed(2)}ms`);
-    console.log(`99th percentile boot time: ${percentile(values, 0.99).toFixed(2)}ms`);
+    times.mean = Number(mean(values).toFixed(2));
+    times.percentile_50 = Number(percentile(values, 0.5).toFixed(2));
+    times.percentile_90 = Number(percentile(values, 0.9).toFixed(2));
+    times.percentile_99 = Number(percentile(values, 0.99).toFixed(2));
+
+    await storeGeneratedResults('devtools-perf.json', JSON.stringify(times));
+
+    console.log(`Mean boot time: ${times.mean}ms`);
+    console.log(`50th percentile boot time: ${times.percentile_50}ms`);
+    console.log(`90th percentile boot time: ${times.percentile_90}ms`);
+    console.log(`99th percentile boot time: ${times.percentile_99}ms`);
     /* eslint-enable no-console */
   });
-  for (let run = 1; run <= total; run++) {
-    it(`run ${run}/${total}`, async () => {
+
+  for (let run = 1; run <= RUNS; run++) {
+    it(`run ${run}/${RUNS}`, async () => {
       const start = performance.now();
       await reloadDevTools();
 
       // Ensure only 2 decimal places.
       const timeTaken = (performance.now() - start).toFixed(2);
       times.bootperf.push(Number(timeTaken));
-    }).timeout(5000);  // 5 second timeout because booting can take a second or so.
+    });
   }
 });

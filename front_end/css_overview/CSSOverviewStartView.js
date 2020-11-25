60eb00cd2e37075a4e0683690d4b0fb68e60fe41
Jack Franklin <jacktfranklin@chromium.org>
2020-11-25 11:40:05 +0000

    [DarkMode] turn off legacy patching in CSSOverviewStartView
    It has no colours declared in the CSS so we can disable the patching
    with no further changes required.
    
    Bug: 1152736
    Change-Id: I7d66e17975990276d3342e38edd812a0e7335579
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2560243
    Commit-Queue: Jack Franklin <jacktfranklin@chromium.org>
    Commit-Queue: Paul Lewis <aerotwist@chromium.org>
    Auto-Submit: Jack Franklin <jacktfranklin@chromium.org>
    Reviewed-by: Paul Lewis <aerotwist@chromium.org>

--- front_end/css_overview/CSSOverviewStartView.js
+++ front_end/css_overview/CSSOverviewStartView.js
@@ -15,7 +15,7 @@ export class CSSOverviewStartView extends UI.Widget.Widget {
    */
   constructor(controller) {
     super();
-    this.registerRequiredCSS('css_overview/cssOverviewStartView.css', {enableLegacyPatching: true});
+    this.registerRequiredCSS('css_overview/cssOverviewStartView.css', {enableLegacyPatching: false});
 
     this._controller = controller;
     this._render();

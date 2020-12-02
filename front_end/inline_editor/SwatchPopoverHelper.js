eefa5f3d78a5f7cdfd84a64e0e70780cebf70c1b
Jack Franklin <jacktfranklin@chromium.org>
2020-12-02 11:34:13 +0000

    [Dark Mode] swatchPopover.css
    I've left the --drop-shadow variable alone for now and pinged Peter to
    ask about the plan for those variables moving forward.
    
    Bug: 1152736
    Change-Id: I15c9853be7c6775a70b7ea0eb2713d5d8ac9d9ce
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2567959
    Auto-Submit: Jack Franklin <jacktfranklin@chromium.org>
    Commit-Queue: Wolfgang Beyer <wolfi@chromium.org>
    Reviewed-by: Wolfgang Beyer <wolfi@chromium.org>

--- front_end/inline_editor/SwatchPopoverHelper.js
+++ front_end/inline_editor/SwatchPopoverHelper.js
@@ -14,7 +14,7 @@ export class SwatchPopoverHelper extends Common.ObjectWrapper.ObjectWrapper {
   constructor() {
     super();
     this._popover = new UI.GlassPane.GlassPane();
-    this._popover.registerRequiredCSS('inline_editor/swatchPopover.css', {enableLegacyPatching: true});
+    this._popover.registerRequiredCSS('inline_editor/swatchPopover.css', {enableLegacyPatching: false});
     this._popover.setSizeBehavior(UI.GlassPane.SizeBehavior.MeasureContent);
     this._popover.setMarginBehavior(UI.GlassPane.MarginBehavior.Arrow);
     this._popover.element.addEventListener('mousedown', e => e.consume(), false);

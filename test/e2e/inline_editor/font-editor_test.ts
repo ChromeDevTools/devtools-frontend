a92cf5ad8c2185e9a94c5200c2b6994e870ac019
Michael Liao <michael.liao@microsoft.com>
2020-12-03 17:08:14 -0800

    Reland "[e2e]: Stabilizing Font Editor e2e test"
    Issue:
    - font editor e2e test is flaky
        - https://logs.chromium.org/logs/devtools-frontend/buildbucket/cr-buildbucket.appspot.com/8861953632669108416/+/steps/E2E_tests/0/stdout
        - failures occur on tests that use keyboard presses to select from a dropdown menu
    - failing e2e tests on mac builds
    
    Changes:
    - awaiting keyboard presses to make sure tests that rely on keyboard press and keyboard press order run as expected.
    - changing e2e test interaction to be inclusive for all OS
    
    Bug: 1154489
    Change-Id: I8990a540030c9d48e006e01808a2f96a805aed86
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2571202
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>

--- test/e2e/inline_editor/font-editor_test.ts
+++ test/e2e/inline_editor/font-editor_test.ts
@@ -14,7 +14,6 @@ async function goToTestPageAndSelectTestElement(path: string = 'inline_editor/fo
 
   await goToResource(path);
   await waitForContentOfSelectedElementsNode('<body>\u200B');
-
   await frontend.keyboard.press('ArrowDown');
 }
 
@@ -26,7 +25,7 @@ async function openFontEditor(index: number) {
   await waitFor('.font-selector-section');
 }
 
-describe.skip('[https://crbug.com/1154560] The font editor', async function() {
+describe('The font editor', async function() {
   beforeEach(async function() {
     await enableExperiment('fontEditor');
     await goToTestPageAndSelectTestElement();
@@ -48,11 +47,9 @@ describe.skip('[https://crbug.com/1154560] The font editor', async function() {
     const {frontend} = getBrowserAndPages();
     await openFontEditor(0);
     const fontFamilySelector = await waitFor('[aria-label="Font Family"]');
-    fontFamilySelector.focus();
-    frontend.keyboard.press('Enter');
-    frontend.keyboard.press('ArrowDown');
-    frontend.keyboard.press('Enter');
-    await waitForCSSPropertyValue('element.style', 'font-family', 'Times New Roman');
+    await fontFamilySelector.focus();
+    await frontend.keyboard.press('a');
+    await waitForCSSPropertyValue('element.style', 'font-family', 'Arial');
   });
 
   it('is properly applying slider input changes to the style section', async () => {
@@ -60,27 +57,25 @@ describe.skip('[https://crbug.com/1154560] The font editor', async function() {
     await openFontEditor(0);
     const fontSizeSliderInput = await waitFor('[aria-label="font-size Slider Input"]');
     await fontSizeSliderInput.focus();
-    frontend.keyboard.press('ArrowRight');
+    await frontend.keyboard.press('ArrowRight');
     await waitForCSSPropertyValue('element.style', 'font-size', '11px');
   });
 
   it('is properly applying text input changes to the style section', async () => {
     const {frontend} = getBrowserAndPages();
     await openFontEditor(0);
-    const fontSizeTextInput = await waitFor('[aria-label="line-height Text Input"]');
+    const fontSizeTextInput = await waitFor('[aria-label="font-size Text Input"]');
     await fontSizeTextInput.focus();
-    frontend.keyboard.type('3');
-    await waitForCSSPropertyValue('element.style', 'line-height', '3');
+    await frontend.keyboard.press('ArrowUp');
+    await waitForCSSPropertyValue('element.style', 'font-size', '11px');
   });
 
   it('is properly applying selector key values to the style section', async () => {
     const {frontend} = getBrowserAndPages();
     await openFontEditor(0);
-    const fontSizeKeySelector = await waitFor('[aria-label="font-weight Key Value Selector"]');
-    await fontSizeKeySelector.focus();
-    frontend.keyboard.press('Enter');
-    frontend.keyboard.press('ArrowDown');
-    frontend.keyboard.press('Enter');
+    const fontWeightSelectorInput = await waitFor('[aria-label="font-weight Key Value Selector"]');
+    await fontWeightSelectorInput.focus();
+    await frontend.keyboard.press('i');
     await waitForCSSPropertyValue('element.style', 'font-weight', 'inherit');
   });
 
@@ -89,9 +84,7 @@ describe.skip('[https://crbug.com/1154560] The font editor', async function() {
     await openFontEditor(0);
     const fontSizeUnitInput = await waitFor('[aria-label="font-size Unit Input"]');
     await fontSizeUnitInput.focus();
-    frontend.keyboard.press('Enter');
-    frontend.keyboard.press('ArrowDown');
-    frontend.keyboard.press('Enter');
+    await frontend.keyboard.press('e');
     await waitForCSSPropertyValue('element.style', 'font-size', '0.6em');
   });
 

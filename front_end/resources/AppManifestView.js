707979143b49e80059088901f4119846b4721a8a
Asami Doi <asamidoi@chromium.org>
2020-12-08 05:36:29 +0000

    Show a warning message in Application > Manifest > Installability in DevTools
    This CL updates DevTools to show a warning message when a PWA is not
    installable due to the failure of the offline capability check.
    
    Backend CLs:
    - https://chromium-review.googlesource.com/c/chromium/src/+/2567057
    - https://chromium-review.googlesource.com/c/chromium/src/+/2576980
    
    Bug: 965802
    Change-Id: Ie89d2cf332e3a8c64597afab38b583e072058d5e
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2573849
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Wolfgang Beyer <wolfi@chromium.org>
    Reviewed-by: Sigurd Schneider <sigurds@chromium.org>
    Commit-Queue: Asami Doi <asamidoi@chromium.org>

--- front_end/resources/AppManifestView.js
+++ front_end/resources/AppManifestView.js
@@ -389,6 +389,10 @@ export class AppManifestView extends UI.Widget.VBox {
           errorMessage =
               ls`Manifest contains 'display_override' field, and the first supported display mode must be one of 'standalone', 'fullscreen', or 'minimal-ui'`;
           break;
+        case 'warn-not-offline-capable':
+          errorMessage =
+              ls`Page does not work offline. The page will not be regarded as installable after Chrome 93, stable release August 2021.`;
+          break;
         default:
           console.error(`Installability error id '${installabilityError.errorId}' is not recognized`);
           break;

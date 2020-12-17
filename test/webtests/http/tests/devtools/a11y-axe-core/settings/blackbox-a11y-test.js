60520630673cfb808ef86de57f07ea9c21205c07
Wolfgang Beyer <wolfi@chromium.org>
2020-12-17 15:55:31 +0100

    Remove "blackbox" - refactor blackbox-a11y-test.js
    This patch replaces unwelcoming language with better terms.
    
    Bug: 1152082
    Design doc: http://doc/1UnwKLgjYPDsLsYCBh9LKiaPN6Aq2XLKvt4Ak3FT7J3k
    
    Change-Id: I44c6c276cc8ecc25362e48bb7f5492c2d28bcdf4
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2597309
    Reviewed-by: Paul Lewis <aerotwist@chromium.org>
    Commit-Queue: Wolfgang Beyer <wolfi@chromium.org>

--- test/webtests/http/tests/devtools/a11y-axe-core/settings/blackbox-a11y-test.js
+++ test/webtests/http/tests/devtools/a11y-axe-core/settings/blackbox-a11y-test.js
@@ -3,39 +3,39 @@
 // found in the LICENSE file.
 
 (async function() {
-  TestRunner.addResult('Tests accessibility in Blackboxing view using the axe-core linter.');
+  TestRunner.addResult('Tests accessibility in IgnoreList view using the axe-core linter.');
 
   await TestRunner.loadModule('axe_core_test_runner');
   await UI.viewManager.showView('blackbox');
-  const blackboxWidget = await UI.viewManager.view('blackbox').widget();
+  const ignoreListWidget = await UI.viewManager.view('blackbox').widget();
 
   async function testAddPattern() {
-    const addPatternButton = blackboxWidget._defaultFocusedElement;
+    const addPatternButton = ignoreListWidget._defaultFocusedElement;
     // Make add pattern editor visible
     addPatternButton.click();
 
-    const blackboxInputs = blackboxWidget._list._editor._controls;
-    TestRunner.addResult(`Opened input box: ${Boolean(blackboxInputs)}`);
+    const ignoreListInputs = ignoreListWidget._list._editor._controls;
+    TestRunner.addResult(`Opened input box: ${Boolean(ignoreListInputs)}`);
 
-    await AxeCoreTestRunner.runValidation(blackboxWidget.contentElement);
+    await AxeCoreTestRunner.runValidation(ignoreListWidget.contentElement);
   }
 
   async function testPatternList() {
-    blackboxWidget._list.appendItem('test*', true);
-    TestRunner.addResult(`Added a pattern in the list: ${blackboxWidget._list._items}`);
-    await AxeCoreTestRunner.runValidation(blackboxWidget.contentElement);
+    ignoreListWidget._list.appendItem('test*', true);
+    TestRunner.addResult(`Added a pattern in the list: ${ignoreListWidget._list._items}`);
+    await AxeCoreTestRunner.runValidation(ignoreListWidget.contentElement);
   }
 
   async function testPatternError() {
-    const blackboxEditor = blackboxWidget._list._editor;
-    const patternInput = blackboxEditor._controls[0];
+    const ignoreListEditor = ignoreListWidget._list._editor;
+    const patternInput = ignoreListEditor._controls[0];
     // Blur patternInput to run validator
     patternInput.blur();
 
-    const errorMessage = blackboxEditor._errorMessageContainer.textContent;
+    const errorMessage = ignoreListEditor._errorMessageContainer.textContent;
     TestRunner.addResult(`Error message: ${errorMessage}`);
 
-    await AxeCoreTestRunner.runValidation(blackboxWidget.contentElement);
+    await AxeCoreTestRunner.runValidation(ignoreListWidget.contentElement);
   }
 
   TestRunner.runAsyncTestSuite([testAddPattern, testPatternList, testPatternError]);

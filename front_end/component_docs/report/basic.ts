176336f07e8f3d58f9533d71bc5673313f6d3064
Jack Franklin <jacktfranklin@chromium.org>
2021-01-08 11:34:06 +0000

    Migrate report, theme_colors and webvitals docs to TS
    Bug: chromium:1144106
    Change-Id: I43d4d74d06ec5b448d1c462732546f208948e64c
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2617786
    Commit-Queue: Jack Franklin <jacktfranklin@chromium.org>
    Commit-Queue: Tim van der Lippe <tvanderlippe@chromium.org>
    Auto-Submit: Jack Franklin <jacktfranklin@chromium.org>
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>

---
+++ front_end/component_docs/report/basic.ts
@@ -0,0 +1,58 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import * as ComponentHelpers from '../../component_helpers/component_helpers.js';
+import * as LitHtml from '../../third_party/lit-html/lit-html.js';
+import * as Components from '../../ui/components/components.js';
+
+ComponentHelpers.ComponentServerSetup.setup().then(() => renderComponent());
+
+const exampleRenderHelper = (name: string, value: string): LitHtml.TemplateResult => LitHtml.html`
+          <devtools-report-row>
+            <span slot="name">${name}</span>
+            <span slot="value">${value}</span>
+          </devtools-report-row>
+        `;
+
+const renderComponent = (): void => {
+  const container = document.querySelector('#container');
+  if (!container) {
+    return;
+  }
+
+  LitHtml.render(
+      LitHtml.html`
+          <style>
+            devtools-report {
+              --name-column-width: 300px;
+            }
+
+            .source-code {
+              font-family: monospace;
+            }
+          </style>
+
+          <devtools-report>
+            <devtools-report-section .data=${{
+        sectionTitle: 'Section 1',
+      } as Components.ReportView.ReportSectionData}>
+              <devtools-report-row>
+                <span slot="name">Basic plain text field</span>
+                <span slot="value">And this is the value</span>
+              </devtools-report-row>
+              <devtools-report-row>
+                <span slot="name">A field with a code value</span>
+                <span slot="value" class="source-code">SomeCodeValue</span>
+              </devtools-report-row>
+            </devtools-report-section>
+
+            <devtools-report-section .data=${{
+        sectionTitle: 'Section 2',
+      } as Components.ReportView.ReportSectionData}>
+              ${exampleRenderHelper('Using a small helper', 'to render report rows')}
+            </devtools-report-section>
+          </devtools-report>
+        `,
+      container);
+};

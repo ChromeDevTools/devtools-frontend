40b825b8bba80a89af8b8410021b81c7567288af
Jan Scheffler <janscheffler@chromium.org>
2021-01-12 11:21:48 +0100

    Reland "[JSDOC2TS]: Migrate webauthn"
    This is a reland of 1623b7a7e933555d425584e6a381cd1b3dde4d2f
    
    Original change's description:
    > [JSDOC2TS]: Migrate webauthn
    >
    > Bug: chromium:1158760
    > Change-Id: I1b04015d139133ec608830644d2bd188ba299d53
    > Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2622073
    > Commit-Queue: Jan Scheffler <janscheffler@chromium.org>
    > Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>
    
    Bug: chromium:1158760
    Change-Id: I8487f9f79e454340e4f055effb4a1d1aa84914e7
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2622198
    Commit-Queue: Jan Scheffler <janscheffler@chromium.org>
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>
    Auto-Submit: Jan Scheffler <janscheffler@chromium.org>

--- front_end/webauthn/ModuleUIStrings.ts
+++ front_end/webauthn/ModuleUIStrings.ts
@@ -9,4 +9,4 @@ export const UIStrings = {
   */
   webauthn: 'WebAuthn',
 };
-i18n.i18n.registerUIStrings('webauthn/ModuleUIStrings.js', UIStrings);
+i18n.i18n.registerUIStrings('webauthn/ModuleUIStrings.ts', UIStrings);

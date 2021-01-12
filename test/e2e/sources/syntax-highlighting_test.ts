7297ea89b095d9f777ab2e1e41f1d149f112ebd2
Almothana Athamneh <almuthanna@google.com>
2021-01-12 11:56:12 +0100

    Add E2E test for syntax highlighting test case
    Change-Id: I0e7b0772332cf3f703e906fc2a87e72bcc2f157f
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2618319
    Commit-Queue: Almothana Athamneh <almuthanna@chromium.org>
    Reviewed-by: Kim-Anh Tran <kimanh@chromium.org>

---
+++ test/e2e/sources/syntax-highlighting_test.ts
@@ -0,0 +1,97 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {assert} from 'chai';
+import {$$, step} from '../../shared/helper.js';
+import {describe, it} from '../../shared/mocha-extensions.js';
+import {openSourceCodeEditorForFile, waitForSourceCodeLines} from '../helpers/sources-helpers.js';
+
+describe('Sources Tab', async function() {
+  it('is highlighting the syntax correctly', async () => {
+    const componentsFormats = new Set();
+
+    await step('navigate to a page and open the Sources tab', async () => {
+      await openSourceCodeEditorForFile('syntax-highlighting.wasm', 'wasm/syntax-highlighting.html');
+    });
+
+    const numberOfLines = 7;
+
+    await step('wait for all the source code to appear', async () => {
+      await waitForSourceCodeLines(numberOfLines);
+    });
+
+    await step('check that variables have the correct class and has a different format', async () => {
+      const expectedVariables = ['$add', '$p0', '$p1', '$p0', '$p1'];
+
+      const variableNames = await Promise.all((await $$('.cm-variable-2')).map(elements => {
+        return elements.evaluate(el => (el as HTMLElement).innerText);
+      }));
+      const variableColors = await Promise.all((await $$('.cm-variable-2')).map(elements => {
+        return elements.evaluate(el => getComputedStyle(el).color);
+      }));
+      variableColors.forEach(element => {
+        componentsFormats.add(element);
+      });
+
+      assert.deepEqual(variableNames, expectedVariables, 'highlighed variables are incorrect');
+      assert.strictEqual(componentsFormats.size, 1, 'variables did not yield exactly one format');
+    });
+
+    await step('check that keywords have the correct class and has a different format', async () => {
+      const expectedKeywords =
+          ['module', 'func', 'export', 'param', 'param', 'result', 'local.get', 'local.get', 'i32.add'];
+
+      const keywordNames = await Promise.all((await $$('.cm-keyword')).map(elements => {
+        return elements.evaluate(el => (el as HTMLElement).innerText);
+      }));
+      const keywordColors = await Promise.all((await $$('.cm-keyword')).map(elements => {
+        return elements.evaluate(el => getComputedStyle(el).color);
+      }));
+      keywordColors.forEach(element => {
+        componentsFormats.add(element);
+      });
+
+      assert.deepEqual(keywordNames, expectedKeywords, 'highlighed keywords are incorrect');
+      assert.strictEqual(
+          componentsFormats.size, 2, 'variables and keywords did not yield exactly two different formats');
+    });
+
+    await step('check that comments have the correct class and has a different format', async () => {
+      const expectedComments = ['(;', '0;)', '(;', '0;)', '(;', '1;)'];
+
+      const commentNames = await Promise.all((await $$('.cm-comment')).map(elements => {
+        return elements.evaluate(el => (el as HTMLElement).innerText);
+      }));
+      const commentColors = await Promise.all((await $$('.cm-comment')).map(elements => {
+        return elements.evaluate(el => getComputedStyle(el).color);
+      }));
+      commentColors.forEach(element => {
+        componentsFormats.add(element);
+      });
+
+      assert.deepEqual(commentNames, expectedComments, 'highlighed comments are incorrect');
+      assert.strictEqual(
+          componentsFormats.size, 3, 'variables, keywords and comments did not yield exactly three different formats');
+    });
+
+    await step('check that strings have the correct class and has a different format', async () => {
+      const expectedStrings = ['\"add\"'];
+
+      const stringNames = await Promise.all((await $$('.cm-string')).map(elements => {
+        return elements.evaluate(el => (el as HTMLElement).innerText);
+      }));
+      const stringColors = await Promise.all((await $$('.cm-string')).map(elements => {
+        return elements.evaluate(el => getComputedStyle(el).color);
+      }));
+      stringColors.forEach(element => {
+        componentsFormats.add(element);
+      });
+
+      assert.deepEqual(stringNames, expectedStrings, 'highlighed strings are incorrect');
+      assert.strictEqual(
+          componentsFormats.size, 4,
+          'variables, keywords, comments and strings did not yield exactly four different formats');
+    });
+  });
+});

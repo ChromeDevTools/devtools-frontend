0a3264351baae91b9fb164be1a3a7b8097612bbc
Songtao Xia <soxia@microsoft.com>
2021-01-13 23:22:30 -0800

    [DevTools Frontend] Resync debugger id after navigation
    DebuggerModel maintains a static table between debugger id and
    DebuggerModel. This table is consulted by CallStackSidebarPane to
    display call stack. If the mapping is not updated, call stack information
    may not be displayed correctly, especially for async stack. There is a
    need to resync the mapping, as a debugger id is mathematically based on
    an isolate while a debugger model may work on different isolate.
    
    Previously, we resync the mapping when we cannot find a debugger model
    corresponding to an id. This change resync the id to model mapping when
    responding to FrameNavigated event.
    
    The work is based on conversation related to this CL:
    https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2466642
    
    Change-Id: I605e811b643ae154afb73024ea062f15b7872505
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2610136
    Commit-Queue: Songtao Xia <soxia@microsoft.com>
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Benedikt Meurer <bmeurer@chromium.org>

--- front_end/sdk/DebuggerModel.js
+++ front_end/sdk/DebuggerModel.js
@@ -34,6 +34,7 @@ import * as ProtocolClient from '../protocol_client/protocol_client.js';  // esl
 import * as Root from '../root/root.js';
 
 import {GetPropertiesResult, RemoteObject, ScopeRef} from './RemoteObject.js';  // eslint-disable-line no-unused-vars
+import {Events as ResourceTreeModelEvents, ResourceTreeModel} from './ResourceTreeModel.js';  // eslint-disable-line no-unused-vars
 import {EvaluationOptions, EvaluationResult, ExecutionContext, RuntimeModel} from './RuntimeModel.js';  // eslint-disable-line no-unused-vars
 import {Script} from './Script.js';
 import {Capability, SDKModel, Target, Type} from './SDKModel.js';  // eslint-disable-line no-unused-vars
@@ -145,6 +146,12 @@ export class DebuggerModel extends SDKModel {
     Common.Settings.Settings.instance()
         .moduleSetting('jsSourceMapsEnabled')
         .addChangeListener(event => this._sourceMapManager.setEnabled(/** @type {boolean} */ (event.data)));
+
+    const resourceTreeModel =
+        /** @type {!ResourceTreeModel} */ (target.model(ResourceTreeModel));
+    if (resourceTreeModel) {
+      resourceTreeModel.addEventListener(ResourceTreeModelEvents.FrameNavigated, this._onFrameNavigated, this);
+    }
   }
 
   /**
@@ -223,6 +230,14 @@ export class DebuggerModel extends SDKModel {
     return enablePromise;
   }
 
+  _onFrameNavigated() {
+    if (DebuggerModel._shouldResyncDebuggerId) {
+      return;
+    }
+
+    DebuggerModel._shouldResyncDebuggerId = true;
+  }
+
   /**
    * @param {!Protocol.Debugger.EnableResponse} response
    */
@@ -245,25 +260,23 @@ export class DebuggerModel extends SDKModel {
 
   /**
    * @param {string} debuggerId
-   * @return {?DebuggerModel}
+   * @return {Promise<?DebuggerModel>}
    */
-  static _modelForDebuggerId(debuggerId) {
+  static async modelForDebuggerId(debuggerId) {
+    if (DebuggerModel._shouldResyncDebuggerId) {
+      await DebuggerModel.resyncDebuggerIdForModels();
+      DebuggerModel._shouldResyncDebuggerId = false;
+    }
     return _debuggerIdToModel.get(debuggerId) || null;
   }
 
-  /**
-   * @param {string} debuggerId
-   * @return {!Promise<?DebuggerModel>}
-   */
-  static async modelForDebuggerIdResyncIfNecessary(debuggerId) {
-    const model = DebuggerModel._modelForDebuggerId(debuggerId);
-    if (!model) {
-      const dbgModels = _debuggerIdToModel.values();
-      for (const dbgModel of dbgModels) {
+  static async resyncDebuggerIdForModels() {
+    const dbgModels = _debuggerIdToModel.values();
+    for (const dbgModel of dbgModels) {
+      if (dbgModel.debuggerEnabled()) {
         await dbgModel.syncDebuggerId();
       }
     }
-    return DebuggerModel._modelForDebuggerId(debuggerId);
   }
 
   /**
@@ -1137,6 +1150,8 @@ export class DebuggerModel extends SDKModel {
     await this._enableDebugger();
   }
 }
+
+DebuggerModel._shouldResyncDebuggerId = false;
 
 /** @type {!Map<string, !DebuggerModel>} */
 export const _debuggerIdToModel = new Map();

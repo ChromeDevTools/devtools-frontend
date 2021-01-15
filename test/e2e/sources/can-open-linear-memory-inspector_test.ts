31e1a685231bf5955b8beae369eced722876e571
Simon Zünd <szuend@chromium.org>
2021-01-15 13:08:10 +0100

    Disable flaky linear memory inspector e2e test
    Observed the test over the course of the week and it flaked
    comparatively often (peak rate was 3 times in 11 runs).
    
    R=bmeurer@chromium.org
    
    Bug: chromium:1166106
    Change-Id: I22cf02dc757ca7ef1916fb5694c4091e60637995
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2631111
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Kim-Anh Tran <kimanh@chromium.org>
    Auto-Submit: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Kim-Anh Tran <kimanh@chromium.org>

--- test/e2e/sources/can-open-linear-memory-inspector_test.ts
+++ test/e2e/sources/can-open-linear-memory-inspector_test.ts
@@ -13,7 +13,8 @@ const LINEAR_MEMORY_INSPECTOR_TAB_SELECTOR = '#tab-linear-memory-inspector';
 const LINEAR_MEMORY_INSPECTOR_TABBED_PANE_SELECTOR = DRAWER_PANEL_SELECTOR + ' .tabbed-pane';
 
 describe('Scope View', async () => {
-  it('opens linear memory inspector', async () => {
+  // Flaky on the Mac CQ bot.
+  it.skip('[crbug.com/1166106] opens linear memory inspector', async () => {
     await enableExperiment('wasmDWARFDebugging');
 
     const {frontend, target} = getBrowserAndPages();

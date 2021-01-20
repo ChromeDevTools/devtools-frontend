0217019d5f694118d130fab8acbad91d8a0e42d2
Annabel Zhou <annabelzhou@google.com>
2021-01-20 14:21:14 +1100

    Added click handlers on a11y nodes to collapse/expand children
    This change is hidden behind experiment flag "fullAccessibilityTree".
    
    Design doc: https://goo.gle/devtools-a11y-tree
    Follow-up CLs: adding new children into the model on click if they
    haven't been requested, tying it to the DOM inspector.
    
    Bug: 887173
    Change-Id: Ibaae7ab80e5d8874def6ec805c3f8a8048d1654a
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2631564
    Commit-Queue: Annabel Zhou <annabelzhou@google.com>
    Reviewed-by: Jack Franklin <jacktfranklin@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Chris Hall <chrishall@chromium.org>
    Reviewed-by: Alice Boxhall <aboxhall@chromium.org>

--- front_end/elements/AccessibilityNode.ts
+++ front_end/elements/AccessibilityNode.ts
@@ -14,11 +14,36 @@ export interface AccessibilityNodeData {
 export class AccessibilityNode extends HTMLElement {
   private readonly shadow = this.attachShadow({mode: 'open'});
   private axNode: SDK.AccessibilityModel.AccessibilityNode|null = null;
+  private expanded: boolean = true;
+
+  constructor() {
+    super();
+    this.addEventListener('click', this.onClick.bind(this));
+  }
 
   set data(data: AccessibilityNodeData) {
     this.axNode = data.axNode;
     this.shadow.host.setAttribute('role', 'treeitem');
     this.render();
+  }
+
+  private onClick(e: MouseEvent): void {
+    e.stopPropagation();
+    this.toggleChildren();
+  }
+
+  private toggleChildren(): void {
+    if (!this.axNode) {
+      return;
+    }
+
+    const children = this.axNode.children();
+    if (!children) {
+      return;
+    }
+
+    this.expanded = !this.expanded;
+    this.classList.toggle('expanded', this.expanded);
   }
 
   // TODO(annabelzhou): Track whether the children should be expanded and change arrow accordingly
@@ -135,7 +160,11 @@ export class AccessibilityNode extends HTMLElement {
           }
 
           .children {
-            padding-inline-start: 16px;
+            padding-inline-start: 12px;
+          }
+
+          :host(:not(.expanded)) .children {
+            display: none;
           }
 
           :host(.no-children) {

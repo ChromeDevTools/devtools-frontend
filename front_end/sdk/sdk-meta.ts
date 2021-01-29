feda18a68fc539cebbbb0d478d7817abdec27ca7
Andres Olivares <andoli@chromium.org>
2021-01-29 15:00:42 +0000

    [module.json extensions] sdk part 3
    Bug: 1134103
    Change-Id: If1bdf75f1b4e1116ff91061bb6e1f985616c4479
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2658338
    Commit-Queue: Andres Olivares <andoli@chromium.org>
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>

--- front_end/sdk/sdk-meta.ts
+++ front_end/sdk/sdk-meta.ts
@@ -356,3 +356,280 @@ Common.Settings.registerSettingExtension({
     },
   ],
 });
+
+Common.Settings.registerSettingExtension({
+  category: Common.Settings.SettingCategoryObject.RENDERING,
+  settingName: 'emulatedCSSMedia',
+  settingType: Common.Settings.SettingTypeObject.ENUM,
+  storageType: Common.Settings.SettingStorageType.Session,
+  defaultValue: '',
+  options: [
+    {
+      title: (): Platform.UIString.LocalizedString => ls`Do not emulate CSS media type`,
+      text: (): Platform.UIString.LocalizedString => ls`No emulation`,
+      value: '',
+    },
+    {
+      title: (): Platform.UIString.LocalizedString => ls`Emulate CSS print media type`,
+      text: (): Platform.UIString.LocalizedString => ls`print`,
+      value: 'print',
+    },
+    {
+      title: (): Platform.UIString.LocalizedString => ls`Emulate CSS screen media type`,
+      text: (): Platform.UIString.LocalizedString => ls`screen`,
+      value: 'screen',
+    },
+  ],
+  tags: [
+    (): Platform.UIString.LocalizedString => ls`query`,
+  ],
+  title: (): Platform.UIString.LocalizedString => ls`Emulate CSS media type`,
+});
+
+Common.Settings.registerSettingExtension({
+  category: Common.Settings.SettingCategoryObject.RENDERING,
+  settingName: 'emulatedCSSMediaFeaturePrefersColorScheme',
+  settingType: Common.Settings.SettingTypeObject.ENUM,
+  storageType: Common.Settings.SettingStorageType.Session,
+  defaultValue: '',
+  options: [
+    {
+      title: (): Platform.UIString.LocalizedString => ls`Do not emulate CSS prefers-color-scheme`,
+      text: (): Platform.UIString.LocalizedString => ls`No emulation`,
+      value: '',
+    },
+    {
+      title: (): Platform.UIString.LocalizedString => ls`Emulate CSS prefers-color-scheme: light`,
+      text: (): Platform.UIString.LocalizedString => ls`prefers-color-scheme: light`,
+      value: 'light',
+    },
+    {
+      title: (): Platform.UIString.LocalizedString => ls`Emulate CSS prefers-color-scheme: dark`,
+      text: (): Platform.UIString.LocalizedString => ls`prefers-color-scheme: dark`,
+      value: 'dark',
+    },
+  ],
+  tags: [
+    (): Platform.UIString.LocalizedString => ls`query`,
+  ],
+  title: (): Platform.UIString.LocalizedString => ls`Emulate CSS media feature prefers-color-scheme`,
+});
+
+Common.Settings.registerSettingExtension({
+  category: Common.Settings.SettingCategoryObject.RENDERING,
+  settingName: 'emulatedCSSMediaFeaturePrefersReducedMotion',
+  settingType: Common.Settings.SettingTypeObject.ENUM,
+  storageType: Common.Settings.SettingStorageType.Session,
+  defaultValue: '',
+  options: [
+    {
+      title: (): Platform.UIString.LocalizedString => ls`Do not emulate CSS prefers-reduced-motion`,
+      text: (): Platform.UIString.LocalizedString => ls`No emulation`,
+      value: '',
+    },
+    {
+      title: (): Platform.UIString.LocalizedString => ls`Emulate CSS prefers-reduced-motion: reduce`,
+      text: (): Platform.UIString.LocalizedString => ls`prefers-reduced-motion: reduce`,
+      value: 'reduce',
+    },
+  ],
+  tags: [
+    (): Platform.UIString.LocalizedString => ls`query`,
+  ],
+  title: (): Platform.UIString.LocalizedString => ls`Emulate CSS media feature prefers-reduced-motion`,
+});
+
+Common.Settings.registerSettingExtension({
+  settingName: 'emulatedCSSMediaFeaturePrefersReducedData',
+  settingType: Common.Settings.SettingTypeObject.ENUM,
+  storageType: Common.Settings.SettingStorageType.Session,
+  defaultValue: '',
+  options: [
+    {
+      title: (): Platform.UIString.LocalizedString => ls`Do not emulate CSS prefers-reduced-data`,
+      text: (): Platform.UIString.LocalizedString => ls`No emulation`,
+      value: '',
+    },
+    {
+      title: (): Platform.UIString.LocalizedString => ls`Emulate CSS prefers-reduced-data: reduce`,
+      text: (): Platform.UIString.LocalizedString => ls`prefers-reduced-data: reduce`,
+      value: 'reduce',
+    },
+  ],
+  title: (): Platform.UIString.LocalizedString => ls`Emulate CSS media feature prefers-reduced-data`,
+});
+
+Common.Settings.registerSettingExtension({
+  settingName: 'emulatedCSSMediaFeatureColorGamut',
+  settingType: Common.Settings.SettingTypeObject.ENUM,
+  storageType: Common.Settings.SettingStorageType.Session,
+  defaultValue: '',
+  options: [
+    {
+      title: (): Platform.UIString.LocalizedString => ls`Do not emulate CSS color-gamut`,
+      text: (): Platform.UIString.LocalizedString => ls`No emulation`,
+      value: '',
+    },
+    {
+      title: (): Platform.UIString.LocalizedString => ls`Emulate CSS color-gamut: srgb`,
+      text: (): Platform.UIString.LocalizedString => ls`color-gamut: srgb`,
+      value: 'srgb',
+    },
+    {
+      title: (): Platform.UIString.LocalizedString => ls`Emulate CSS color-gamut: p3`,
+      text: (): Platform.UIString.LocalizedString => ls`color-gamut: p3`,
+      value: 'p3',
+    },
+    {
+      title: (): Platform.UIString.LocalizedString => ls`Emulate CSS color-gamut: rec2020`,
+      text: (): Platform.UIString.LocalizedString => ls`color-gamut: rec2020`,
+      value: 'rec2020',
+    },
+  ],
+  title: (): Platform.UIString.LocalizedString => ls`Emulate CSS media feature color-gamut`,
+});
+
+Common.Settings.registerSettingExtension({
+  category: Common.Settings.SettingCategoryObject.RENDERING,
+  settingName: 'emulatedVisionDeficiency',
+  settingType: Common.Settings.SettingTypeObject.ENUM,
+  storageType: Common.Settings.SettingStorageType.Session,
+  defaultValue: 'none',
+  options: [
+    {
+      title: (): Platform.UIString.LocalizedString => ls`Do not emulate any vision deficiency`,
+      text: (): Platform.UIString.LocalizedString => ls`No emulation`,
+      value: 'none',
+    },
+    {
+      title: (): Platform.UIString.LocalizedString => ls`Emulate blurred vision`,
+      text: (): Platform.UIString.LocalizedString => ls`Blurred vision`,
+      value: 'blurredVision',
+    },
+    {
+      title: (): Platform.UIString.LocalizedString => ls`Emulate protanopia`,
+      text: (): Platform.UIString.LocalizedString => ls`Protanopia`,
+      value: 'protanopia',
+    },
+    {
+      title: (): Platform.UIString.LocalizedString => ls`Emulate deuteranopia`,
+      text: (): Platform.UIString.LocalizedString => ls`Deuteranopia`,
+      value: 'deuteranopia',
+    },
+    {
+      title: (): Platform.UIString.LocalizedString => ls`Emulate tritanopia`,
+      text: (): Platform.UIString.LocalizedString => ls`Tritanopia`,
+      value: 'tritanopia',
+    },
+    {
+      title: (): Platform.UIString.LocalizedString => ls`Emulate achromatopsia`,
+      text: (): Platform.UIString.LocalizedString => ls`Achromatopsia`,
+      value: 'achromatopsia',
+    },
+  ],
+  tags: [
+    (): Platform.UIString.LocalizedString => ls`query`,
+  ],
+  title: (): Platform.UIString.LocalizedString => ls`Emulate vision deficiencies`,
+});
+
+Common.Settings.registerSettingExtension({
+  category: Common.Settings.SettingCategoryObject.RENDERING,
+  settingName: 'localFontsDisabled',
+  settingType: Common.Settings.SettingTypeObject.BOOLEAN,
+  storageType: Common.Settings.SettingStorageType.Session,
+  options: [
+    {
+      value: true,
+      title: (): Platform.UIString.LocalizedString => ls`Disable local fonts`,
+    },
+    {
+      value: false,
+      title: (): Platform.UIString.LocalizedString => ls`Enable local fonts`,
+    },
+  ],
+  defaultValue: false,
+});
+
+Common.Settings.registerSettingExtension({
+  category: Common.Settings.SettingCategoryObject.RENDERING,
+  settingName: 'avifFormatDisabled',
+  settingType: Common.Settings.SettingTypeObject.BOOLEAN,
+  storageType: Common.Settings.SettingStorageType.Session,
+  options: [
+    {
+      value: true,
+      title: (): Platform.UIString.LocalizedString => ls`Disable AVIF format`,
+    },
+    {
+      value: false,
+      title: (): Platform.UIString.LocalizedString => ls`Enable AVIF format`,
+    },
+  ],
+  defaultValue: false,
+});
+
+Common.Settings.registerSettingExtension({
+  category: Common.Settings.SettingCategoryObject.RENDERING,
+  settingName: 'webpFormatDisabled',
+  settingType: Common.Settings.SettingTypeObject.BOOLEAN,
+  storageType: Common.Settings.SettingStorageType.Session,
+  options: [
+    {
+      value: true,
+      title: (): Platform.UIString.LocalizedString => ls`Disable WebP format`,
+    },
+    {
+      value: false,
+      title: (): Platform.UIString.LocalizedString => ls`Enable WebP format`,
+    },
+  ],
+  defaultValue: false,
+});
+
+Common.Settings.registerSettingExtension({
+  category: Common.Settings.SettingCategoryObject.CONSOLE,
+  title: (): Platform.UIString.LocalizedString => ls`Enable custom formatters`,
+  settingName: 'customFormatters',
+  settingType: Common.Settings.SettingTypeObject.BOOLEAN,
+  defaultValue: false,
+});
+
+Common.Settings.registerSettingExtension({
+  category: Common.Settings.SettingCategoryObject.NETWORK,
+  title: (): Platform.UIString.LocalizedString => ls`Enable network request blocking`,
+  settingName: 'requestBlockingEnabled',
+  settingType: Common.Settings.SettingTypeObject.BOOLEAN,
+  storageType: Common.Settings.SettingStorageType.Session,
+  defaultValue: false,
+  options: [
+    {
+      value: true,
+      title: (): Platform.UIString.LocalizedString => ls`Enable network request blocking`,
+    },
+    {
+      value: false,
+      title: (): Platform.UIString.LocalizedString => ls`Disable network request blocking`,
+    },
+  ],
+});
+
+Common.Settings.registerSettingExtension({
+  category: Common.Settings.SettingCategoryObject.NETWORK,
+  title: (): Platform.UIString.LocalizedString => ls`Disable cache (while DevTools is open)`,
+  settingName: 'cacheDisabled',
+  settingType: Common.Settings.SettingTypeObject.BOOLEAN,
+  order: 0,
+  defaultValue: false,
+  userActionCondition: 'hasOtherClients',
+  options: [
+    {
+      value: true,
+      title: (): Platform.UIString.LocalizedString => ls`Disable cache (while DevTools is open)`,
+    },
+    {
+      value: false,
+      title: (): Platform.UIString.LocalizedString => ls`Enable cache`,
+    },
+  ],
+});

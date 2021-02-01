44b95595c9d92e54a3bf58c80a694fc8df02be01
Wolfgang Beyer <wolfi@chromium.org>
2021-02-01 12:01:04 +0100

    Add until directive from lit-html
    lit-html's until directive will be needed in an upcoming CL
    (https://crrev.com/c/2640576), adding it here separately for simplicity.
    
    Bug: 1165710
    Change-Id: I1a939bd45062370efd3b618ac856d0a9d10e437a
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2662824
    Reviewed-by: Jack Franklin <jacktfranklin@chromium.org>
    Commit-Queue: Wolfgang Beyer <wolfi@chromium.org>

--- front_end/third_party/lit-html/directives.ts
+++ front_end/third_party/lit-html/directives.ts
@@ -7,3 +7,4 @@ export * from './package/directives/if-defined.js';
 export * from './package/directives/style-map.js';
 export * from './package/directives/live.js';
 export * from './package/directives/repeat.js';
+export * from './package/directives/until.js';

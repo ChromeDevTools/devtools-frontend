0e84cd982c677db0a9db9fe60314a1972d6e5002
Andres Olivares <andoli@chromium.org>
2021-02-02 13:10:26 +0000

    [module.json extensions] sdk part 2
    Bug: 1134103
    Change-Id: Ibcd9c740e0013ed5b3546ca7baf485081a0a1891
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2658061
    Commit-Queue: Andres Olivares <andoli@chromium.org>
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>

--- front_end/sdk/sdk-meta.ts
+++ front_end/sdk/sdk-meta.ts
@@ -132,3 +132,227 @@ Common.Settings.registerSettingExtension({
   ],
   defaultValue: false,
 });
+
+Common.Settings.registerSettingExtension({
+  category: Common.Settings.SettingCategoryObject.GRID,
+  title: (): Platform.UIString.LocalizedString => ls`Show track sizes`,
+  settingName: 'showGridTrackSizes',
+  settingType: Common.Settings.SettingTypeObject.BOOLEAN,
+  options: [
+    {
+      value: true,
+      title: (): Platform.UIString.LocalizedString => ls`Show grid track sizes`,
+    },
+    {
+      value: false,
+      title: (): Platform.UIString.LocalizedString => ls`Do not show grid track sizes`,
+    },
+  ],
+  defaultValue: false,
+});
+
+Common.Settings.registerSettingExtension({
+  category: Common.Settings.SettingCategoryObject.GRID,
+  title: (): Platform.UIString.LocalizedString => ls`Extend grid lines`,
+  settingName: 'extendGridLines',
+  settingType: Common.Settings.SettingTypeObject.BOOLEAN,
+  options: [
+    {
+      value: true,
+      title: (): Platform.UIString.LocalizedString => ls`Extend grid lines`,
+    },
+    {
+      value: false,
+      title: (): Platform.UIString.LocalizedString => ls`Do not extend grid lines`,
+    },
+  ],
+  defaultValue: false,
+});
+
+Common.Settings.registerSettingExtension({
+  category: Common.Settings.SettingCategoryObject.GRID,
+  title: (): Platform.UIString.LocalizedString => ls`Show line labels`,
+  settingName: 'showGridLineLabels',
+  settingType: Common.Settings.SettingTypeObject.ENUM,
+  options: [
+    {
+      title: (): Platform.UIString.LocalizedString => ls`Hide line labels`,
+      text: (): Platform.UIString.LocalizedString => ls`Hide line labels`,
+      value: 'none',
+    },
+    {
+      title: (): Platform.UIString.LocalizedString => ls`Show line numbers`,
+      text: (): Platform.UIString.LocalizedString => ls`Show line numbers`,
+      value: 'lineNumbers',
+    },
+    {
+      title: (): Platform.UIString.LocalizedString => ls`Show line names`,
+      text: (): Platform.UIString.LocalizedString => ls`Show line names`,
+      value: 'lineNames',
+    },
+  ],
+  defaultValue: 'lineNumbers',
+});
+
+Common.Settings.registerSettingExtension({
+  category: Common.Settings.SettingCategoryObject.RENDERING,
+  settingName: 'showPaintRects',
+  settingType: Common.Settings.SettingTypeObject.BOOLEAN,
+  storageType: Common.Settings.SettingStorageType.Session,
+  options: [
+    {
+      value: true,
+      title: (): Platform.UIString.LocalizedString => ls`Show paint flashing rectangles`,
+    },
+    {
+      value: false,
+      title: (): Platform.UIString.LocalizedString => ls`Hide paint flashing rectangles`,
+    },
+  ],
+  defaultValue: false,
+});
+
+Common.Settings.registerSettingExtension({
+  category: Common.Settings.SettingCategoryObject.RENDERING,
+  settingName: 'showLayoutShiftRegions',
+  settingType: Common.Settings.SettingTypeObject.BOOLEAN,
+  storageType: Common.Settings.SettingStorageType.Session,
+  options: [
+    {
+      value: true,
+      title: (): Platform.UIString.LocalizedString => ls`Show layout shift regions`,
+    },
+    {
+      value: false,
+      title: (): Platform.UIString.LocalizedString => ls`Hide layout shift regions`,
+    },
+  ],
+  defaultValue: false,
+});
+
+Common.Settings.registerSettingExtension({
+  category: Common.Settings.SettingCategoryObject.RENDERING,
+  settingName: 'showAdHighlights',
+  settingType: Common.Settings.SettingTypeObject.BOOLEAN,
+  storageType: Common.Settings.SettingStorageType.Session,
+  options: [
+    {
+      value: true,
+      title: (): Platform.UIString.LocalizedString => ls`Highlight ad frames`,
+    },
+    {
+      value: false,
+      title: (): Platform.UIString.LocalizedString => ls`Do not highlight ad frames`,
+    },
+  ],
+  defaultValue: false,
+});
+
+Common.Settings.registerSettingExtension({
+  category: Common.Settings.SettingCategoryObject.RENDERING,
+  settingName: 'showDebugBorders',
+  settingType: Common.Settings.SettingTypeObject.BOOLEAN,
+  storageType: Common.Settings.SettingStorageType.Session,
+  options: [
+    {
+      value: true,
+      title: (): Platform.UIString.LocalizedString => ls`Show layer borders`,
+    },
+    {
+      value: false,
+      title: (): Platform.UIString.LocalizedString => ls`Hide layer borders`,
+    },
+  ],
+  defaultValue: false,
+});
+
+Common.Settings.registerSettingExtension({
+  category: Common.Settings.SettingCategoryObject.RENDERING,
+  settingName: 'showWebVitals',
+  settingType: Common.Settings.SettingTypeObject.BOOLEAN,
+  storageType: Common.Settings.SettingStorageType.Session,
+  options: [
+    {
+      value: true,
+      title: (): Platform.UIString.LocalizedString => ls`Show Core Web Vitals overlay`,
+    },
+    {
+      value: false,
+      title: (): Platform.UIString.LocalizedString => ls`Hide Core Web Vitals overlay`,
+    },
+  ],
+  defaultValue: false,
+});
+
+Common.Settings.registerSettingExtension({
+  category: Common.Settings.SettingCategoryObject.RENDERING,
+  settingName: 'showFPSCounter',
+  settingType: Common.Settings.SettingTypeObject.BOOLEAN,
+  storageType: Common.Settings.SettingStorageType.Session,
+  options: [
+    {
+      value: true,
+      title: (): Platform.UIString.LocalizedString => ls`Show frames per second (FPS) meter`,
+    },
+    {
+      value: false,
+      title: (): Platform.UIString.LocalizedString => ls`Hide frames per second (FPS) meter`,
+    },
+  ],
+  defaultValue: false,
+});
+
+Common.Settings.registerSettingExtension({
+  category: Common.Settings.SettingCategoryObject.RENDERING,
+  settingName: 'showScrollBottleneckRects',
+  settingType: Common.Settings.SettingTypeObject.BOOLEAN,
+  storageType: Common.Settings.SettingStorageType.Session,
+  options: [
+    {
+      value: true,
+      title: (): Platform.UIString.LocalizedString => ls`Show scroll performance bottlenecks`,
+    },
+    {
+      value: false,
+      title: (): Platform.UIString.LocalizedString => ls`Hide scroll performance bottlenecks`,
+    },
+  ],
+  defaultValue: false,
+});
+
+Common.Settings.registerSettingExtension({
+  category: Common.Settings.SettingCategoryObject.RENDERING,
+  settingName: 'showHitTestBorders',
+  settingType: Common.Settings.SettingTypeObject.BOOLEAN,
+  storageType: Common.Settings.SettingStorageType.Session,
+  options: [
+    {
+      value: true,
+      title: (): Platform.UIString.LocalizedString => ls`Show hit-test borders`,
+    },
+    {
+      value: false,
+      title: (): Platform.UIString.LocalizedString => ls`Hide hit-test borders`,
+    },
+  ],
+  defaultValue: false,
+});
+
+Common.Settings.registerSettingExtension({
+  category: Common.Settings.SettingCategoryObject.RENDERING,
+  title: (): Platform.UIString.LocalizedString => ls`Emulate a focused page`,
+  settingName: 'emulatePageFocus',
+  settingType: Common.Settings.SettingTypeObject.BOOLEAN,
+  storageType: Common.Settings.SettingStorageType.Session,
+  defaultValue: false,
+  options: [
+    {
+      value: true,
+      title: (): Platform.UIString.LocalizedString => ls`Emulate a focused page`,
+    },
+    {
+      value: false,
+      title: (): Platform.UIString.LocalizedString => ls`Do not emulate a focused page`,
+    },
+  ],
+});

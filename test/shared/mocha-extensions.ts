543570c2ba52b1104d6b8106c05cb427e361f698
Philip Pfaffe <pfaffe@chromium.org>
2021-02-08 16:09:19 +0100

    Patch Mocha's describe to include test names
    When tests fail, debugging the failure includes isolating the test
    before opening it in debug mode or adding diagnostics to it. With the
    current test output, opening the file is surprisingly tedious, since
    there is no direct way to map test log output to the tests on the
    filesystem. One has to grep the test directory for test names first.
    This CL wraps Mocha's describe function to automatically include the
    test filename into the test title in a format that can be copy-pasted
    into the test runner's --test-file argument.
    
    Bug: none
    Change-Id: I172bd14f5026ec0041b933c64bcdcccf58536fb9
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2681741
    Commit-Queue: Philip Pfaffe <pfaffe@chromium.org>
    Reviewed-by: Peter Marshall <petermarshall@chromium.org>
    Auto-Submit: Philip Pfaffe <pfaffe@chromium.org>

--- test/shared/mocha-extensions.ts
+++ test/shared/mocha-extensions.ts
@@ -3,12 +3,13 @@
 // found in the LICENSE file.
 
 import * as Mocha from 'mocha';
+import * as Path from 'path';
 
 import {getBrowserAndPages} from '../conductor/puppeteer-state.js';
 
 import {getEnvVar} from './config.js';
 
-export {beforeEach, describe} from 'mocha';
+export {beforeEach} from 'mocha';
 
 async function takeScreenshots() {
   try {
@@ -27,6 +28,47 @@ async function takeScreenshots() {
     console.error('Error taking a screenshot', err);
   }
 }
+
+export function wrapDescribe<ReturnT>(
+    mochaFn: (title: string, fn: (this: Mocha.Suite) => void) => ReturnT, title: string,
+    fn: (this: Mocha.Suite) => void): ReturnT {
+  const originalFn = Error.prepareStackTrace;
+  try {
+    Error.prepareStackTrace = (err, stackTraces) => {
+      if (stackTraces.length < 3) {
+        return '<unknown>';
+      }
+      const filename = stackTraces[2].getFileName();
+      if (!filename) {
+        return '<unknown>';
+      }
+      const parsedPath = Path.parse(filename);
+      const directories = parsedPath.dir.split(Path.sep);
+      const index = directories.lastIndexOf('e2e');
+      if (index < 0) {
+        return parsedPath.name;
+      }
+      return Path.join(...directories.slice(index + 1), parsedPath.name);
+    };
+    const err = new Error();
+
+    return mochaFn(`${err.stack}: ${title}`, fn);
+  } finally {
+    Error.prepareStackTrace = originalFn;
+  }
+}
+
+export function describe(title: string, fn: (this: Mocha.Suite) => void) {
+  return wrapDescribe(Mocha.describe, title, fn);
+}
+
+describe.only = function(title: string, fn: (this: Mocha.Suite) => void) {
+  return wrapDescribe(Mocha.describe.only, title, fn);
+};
+
+describe.skip = function(title: string, fn: (this: Mocha.Suite) => void) {
+  return wrapDescribe(Mocha.describe.skip, title, fn);
+};
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 async function timeoutHook(this: Mocha.Runnable, done: Mocha.Done|undefined, err?: any) {

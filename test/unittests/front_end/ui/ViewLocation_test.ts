e753678c70658d1223dc635644a53fe36d3ff3de
Andres Olivares <andoli@chromium.org>
2021-02-09 19:05:10 +0000

    Add ViewLocation unit test
    This adds a new unit test to replace the layout test implemented in
    http/tests/devtools/unit/view-location.js
    
    Bug: none
    Change-Id: I6fea270d3551dc17b8670682b22180f3546e6713
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2684126
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>
    Commit-Queue: Andres Olivares <andoli@chromium.org>

---
+++ test/unittests/front_end/ui/ViewLocation_test.ts
@@ -0,0 +1,62 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+const {assert} = chai;
+
+import * as UI from '../../../../front_end/ui/ui.js';
+import * as Platform from '../../../../front_end/platform/platform.js';
+import {describeWithEnvironment} from '../helpers/EnvironmentHelpers.js';
+
+describeWithEnvironment('ViewLocation', () => {
+  let tabbedLocation: UI.View.TabbedViewLocation;
+  let viewManager: UI.ViewManager.ViewManager;
+  before(async () => {
+    ['first', 'second', 'third', 'fourth'].forEach(title => {
+      UI.ViewManager.registerViewExtension({
+        // @ts-ignore
+        location: 'mock-location',
+        id: title,
+        title: () => title as Platform.UIString.LocalizedString,
+        commandPrompt: () => title as Platform.UIString.LocalizedString,
+        persistence: UI.ViewManager.ViewPersistence.CLOSEABLE,
+        async loadView() {
+          return new UI.Widget.Widget();
+        },
+      });
+    });
+    viewManager = UI.ViewManager.ViewManager.instance({forceNew: true});
+    tabbedLocation = viewManager.createTabbedLocation(undefined, 'mock-location', true, true);
+  });
+
+  it('Creates an empty tabbed location', () => {
+    assert.deepEqual(tabbedLocation.tabbedPane().tabIds(), []);
+  });
+
+  it('Adds a tab for a selected view', () => {
+    viewManager.showView('first');
+    viewManager.showView('second');
+    viewManager.showView('third');
+
+    assert.deepEqual(tabbedLocation.tabbedPane().tabIds(), ['first', 'second', 'third']);
+  });
+
+  it('Prepends a tab correctly', () => {
+    const thirdTab = tabbedLocation.tabbedPane()._tabsById.get('third');
+    if (!thirdTab) {
+      throw new Error('Could not find a tab');
+    }
+    tabbedLocation.tabbedPane()._insertBefore(thirdTab, 0);
+    assert.deepEqual(tabbedLocation.tabbedPane().tabIds(), ['third', 'first', 'second']);
+  });
+
+  it('Appends a tab correctly', () => {
+    viewManager.showView('fourth');
+    assert.deepEqual(tabbedLocation.tabbedPane().tabIds(), ['third', 'first', 'second', 'fourth']);
+  });
+
+  it('Closes a tab correctly', () => {
+    tabbedLocation.tabbedPane().closeTab('second');
+    assert.deepEqual(tabbedLocation.tabbedPane().tabIds(), ['third', 'first', 'fourth']);
+  });
+});

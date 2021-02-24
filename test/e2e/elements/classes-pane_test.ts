3f2ca7c9940445a50ab97b690d44af1a4a210d29
Philip Pfaffe <pfaffe@chromium.org>
2021-02-24 16:38:08 +0100

    Opt into async tracing in the classes pane tests
    The tests has been timing out on the CQ.
    
    Bug: chromium:1181777
    Change-Id: Iaae1139f913c7f61052dc3bc6249b9596c311368
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2718285
    Commit-Queue: Philip Pfaffe <pfaffe@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>

--- test/e2e/elements/classes-pane_test.ts
+++ test/e2e/elements/classes-pane_test.ts
@@ -2,9 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {beforeEach, describe, it} from 'mocha';
+import {beforeEach} from 'mocha';
 
 import {goToResource, timeout} from '../../shared/helper.js';
+import {describe, it} from '../../shared/mocha-extensions.js';
 import {assertSelectedNodeClasses, toggleClassesPane, toggleClassesPaneCheckbox, typeInClassesPaneInput} from '../helpers/elements-helpers.js';
 
 describe('The Classes pane', async () => {

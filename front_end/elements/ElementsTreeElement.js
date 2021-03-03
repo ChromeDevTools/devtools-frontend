968ecfa1707bd6985d9c17c4307776a459a15021
Eric Leese <leese@chromium.org>
2021-03-03 11:15:15 +0000

    Revert "Collapse whitespaces in DOM elements"
    This reverts commit c84b103b21cbe7910c56eca1463369351160c585.
    
    Reason for revert: This broke blink_web_tests
    
    https://ci.chromium.org/ui/p/devtools-frontend/builders/ci/DevTools%20Linux/4521/overview
    
    virtual/portals/http/tests/devtools/portals/portals-elements-activate.js
    virtual/portals/http/tests/devtools/portals/portals-elements-nesting.js
    
    Diff shows newlines were removed. Please disable tests in Chromium and wait for roll before relanding, then reenable with fixed expectations. 
    
    Original change's description:
    > Collapse whitespaces in DOM elements
    >
    > This CL collapses a sequence of whitespaces into one for *displayed*
    > DOM elements in the Elements tree. The original content won't be
    > affected, and when users want to edit HTML, they can see the original
    > whitespace-ful content.
    >
    > Screenshot: https://imgur.com/a/43HSb47
    >
    > Fixed: chromium:1145610
    > Change-Id: Ic1b6d0006cfdadaa9dbc0972c7141c2ae9fc6467
    > Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2727709
    > Commit-Queue: Changhao Han <changhaohan@chromium.org>
    > Auto-Submit: Changhao Han <changhaohan@chromium.org>
    > Reviewed-by: Jan Scheffler <janscheffler@chromium.org>
    > Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    > Reviewed-by: Mathias Bynens <mathias@chromium.org>
    
    Change-Id: I814118f181d09357cd0733c867a0e103431e1cb1
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2731670
    Auto-Submit: Eric Leese <leese@chromium.org>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>

--- front_end/elements/ElementsTreeElement.js
+++ front_end/elements/ElementsTreeElement.js
@@ -1094,11 +1094,12 @@ export class ElementsTreeElement extends UI.TreeOutline.TreeElement {
     if (maybeInitialValue === null) {
       return;
     }
+    let initialValue = maybeInitialValue;  // To suppress a compiler warning.
     if (this._editing) {
       return;
     }
 
-    const initialValue = this._convertWhitespaceToEntities(maybeInitialValue).text;
+    initialValue = this._convertWhitespaceToEntities(initialValue).text;
 
     this._htmlEditElement = /** @type {!HTMLElement} */ (document.createElement('div'));
     this._htmlEditElement.className = 'source-code elements-tree-editor';
@@ -1906,7 +1907,7 @@ export class ElementsTreeElement extends UI.TreeOutline.TreeElement {
             throw new Error('ElementsTreeElement._nodeTitleInfo expects node.firstChild to be defined.');
           }
           const result = this._convertWhitespaceToEntities(firstChild.nodeValue());
-          textNodeElement.textContent = Platform.StringUtilities.collapseWhitespace(result.text);
+          textNodeElement.textContent = result.text;
           UI.UIUtils.highlightRangesWithStyleClass(textNodeElement, result.entityRanges, 'webkit-html-entity-value');
           UI.UIUtils.createTextChild(titleDOM, '\u200B');
           this._buildTagDOM(titleDOM, tagName, true, false, updateRecord);
@@ -1945,7 +1946,7 @@ export class ElementsTreeElement extends UI.TreeOutline.TreeElement {
           UI.UIUtils.createTextChild(titleDOM, '"');
           const textNodeElement = titleDOM.createChild('span', 'webkit-html-text-node');
           const result = this._convertWhitespaceToEntities(node.nodeValue());
-          textNodeElement.textContent = Platform.StringUtilities.collapseWhitespace(result.text);
+          textNodeElement.textContent = result.text;
           UI.UIUtils.highlightRangesWithStyleClass(textNodeElement, result.entityRanges, 'webkit-html-entity-value');
           UI.UIUtils.createTextChild(titleDOM, '"');
           if (updateRecord && updateRecord.isCharDataModified()) {

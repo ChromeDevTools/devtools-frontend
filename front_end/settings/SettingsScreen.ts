080437fc01191517eb8194a6e8e1f454e42b69d4
Peter Marshall <petermarshall@chromium.org>
2021-03-16 10:59:08 +1100

    Add a text filter to the experiments panel
    It's nice to be able to filter the long list of experiments to quickly
    find the one you are a looking for.
    
    No filter: https://imgur.com/2ZpVFCC
    Found:     https://imgur.com/a/WPEN366
    No result: https://imgur.com/EUPQgMd
    
    Bug: none
    Change-Id: I4b9bb00086a52380b85cdd69bd8313f9baeaaa37
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2763342
    Commit-Queue: Peter Marshall <petermarshall@chromium.org>
    Reviewed-by: Jack Franklin <jacktfranklin@chromium.org>

--- front_end/settings/SettingsScreen.ts
+++ front_end/settings/SettingsScreen.ts
@@ -77,6 +77,15 @@ const UIStrings = {
   *@description Message to display if a setting change requires a reload of DevTools
   */
   oneOrMoreSettingsHaveChanged: 'One or more settings have changed which requires a reload to take effect.',
+  /**
+  * @description Label for a filter text input that controls which experiments are shown.
+  */
+  filterExperimentsLabel: 'Filter',
+  /**
+  * @description Warning text shown when the user has entered text to filter the
+  * list of experiments, but no experiments match the filter.
+  */
+  noResults: 'No experiments match the filter',
 };
 const str_ = i18n.i18n.registerUIStrings('settings/SettingsScreen.ts', UIStrings);
 const i18nString = i18n.i18n.getLocalizedString.bind(undefined, str_);
@@ -227,7 +236,7 @@ class SettingsTab extends UI.Widget.VBox {
                                 .createChild('div', 'settings-tab settings-content settings-container');
   }
 
-  _appendSection(name?: string): Element {
+  _appendSection(name?: string): HTMLElement {
     const block = this.containerElement.createChild('div', 'settings-block');
     if (name) {
       UI.ARIAUtils.markAsGroup(block);
@@ -365,30 +374,57 @@ export class GenericSettingsTab extends SettingsTab {
 let experimentsSettingsTabInstance: ExperimentsSettingsTab;
 
 export class ExperimentsSettingsTab extends SettingsTab {
+  private experimentsSection: HTMLElement|undefined;
+  private unstableExperimentsSection: HTMLElement|undefined;
+
   constructor() {
     super(i18nString(UIStrings.experiments), 'experiments-tab-content');
+    const filterSection = this._appendSection();
+    filterSection.style.paddingTop = '1px';
 
+    const labelElement = filterSection.createChild('label');
+    labelElement.textContent = i18nString(UIStrings.filterExperimentsLabel);
+    const inputElement = UI.UIUtils.createInput('', 'text');
+    UI.ARIAUtils.bindLabelToControl(labelElement, inputElement);
+    filterSection.appendChild(inputElement);
+    inputElement.addEventListener('input', () => this.renderExperiments(inputElement.value.toLowerCase()), false);
+
+    this.renderExperiments('');
+  }
+
+  private renderExperiments(filterText: string): void {
+    if (this.experimentsSection) {
+      this.experimentsSection.remove();
+    }
+    if (this.unstableExperimentsSection) {
+      this.unstableExperimentsSection.remove();
+    }
     const experiments = Root.Runtime.experiments.allConfigurableExperiments().sort();
-    const unstableExperiments = experiments.filter(e => e.unstable);
-    const stableExperiments = experiments.filter(e => !e.unstable);
+    const unstableExperiments = experiments.filter(e => e.unstable && e.title.toLowerCase().includes(filterText));
+    const stableExperiments = experiments.filter(e => !e.unstable && e.title.toLowerCase().includes(filterText));
     if (stableExperiments.length) {
-      const experimentsSection = this._appendSection();
+      this.experimentsSection = this._appendSection();
       const warningMessage = i18nString(UIStrings.theseExperimentsCouldBeUnstable);
-      experimentsSection.appendChild(this._createExperimentsWarningSubsection(warningMessage));
+      this.experimentsSection.appendChild(this._createExperimentsWarningSubsection(warningMessage));
       for (const experiment of stableExperiments) {
-        experimentsSection.appendChild(this._createExperimentCheckbox(experiment));
+        this.experimentsSection.appendChild(this._createExperimentCheckbox(experiment));
       }
     }
     if (unstableExperiments.length) {
-      const experimentsSection = this._appendSection();
+      this.unstableExperimentsSection = this._appendSection();
       const warningMessage = i18nString(UIStrings.theseExperimentsAreParticularly);
-      experimentsSection.appendChild(this._createExperimentsWarningSubsection(warningMessage));
+      this.unstableExperimentsSection.appendChild(this._createExperimentsWarningSubsection(warningMessage));
       for (const experiment of unstableExperiments) {
         // TODO(crbug.com/1161439): remove experiment duplication
         if (experiment.name !== 'blackboxJSFramesOnTimeline') {
-          experimentsSection.appendChild(this._createExperimentCheckbox(experiment));
+          this.unstableExperimentsSection.appendChild(this._createExperimentCheckbox(experiment));
         }
       }
+    }
+    if (!stableExperiments.length && !unstableExperiments.length) {
+      this.experimentsSection = this._appendSection();
+      const warning = this.experimentsSection.createChild('span');
+      warning.textContent = i18nString(UIStrings.noResults);
     }
   }
 

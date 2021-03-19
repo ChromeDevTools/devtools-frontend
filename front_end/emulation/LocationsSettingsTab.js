8a27fdd0598fc519aaf974baa9c03f52a5de5bfc
Andres Olivares <andoli@chromium.org>
2021-03-19 15:16:47 +0000

    [DarkMode] emulation/locationSettingsTab.css
    R=aerotwist@chromium.org
    
    Bug: chromium:1152736
    Change-Id: I24c3607363421f13b88739ed9bf240a001a0c593
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2775811
    Commit-Queue: Andres Olivares <andoli@chromium.org>
    Commit-Queue: Paul Lewis <aerotwist@chromium.org>
    Reviewed-by: Paul Lewis <aerotwist@chromium.org>

--- front_end/emulation/LocationsSettingsTab.js
+++ front_end/emulation/LocationsSettingsTab.js
@@ -103,7 +103,7 @@ export class LocationsSettingsTab extends UI.Widget.VBox {
   /** @private */
   constructor() {
     super(true);
-    this.registerRequiredCSS('emulation/locationsSettingsTab.css', {enableLegacyPatching: true});
+    this.registerRequiredCSS('emulation/locationsSettingsTab.css', {enableLegacyPatching: false});
 
     this.contentElement.createChild('div', 'header').textContent = i18nString(UIStrings.customLocations);
 
@@ -113,7 +113,7 @@ export class LocationsSettingsTab extends UI.Widget.VBox {
 
     this._list = new UI.ListWidget.ListWidget(this);
     this._list.element.classList.add('locations-list');
-    this._list.registerRequiredCSS('emulation/locationsSettingsTab.css', {enableLegacyPatching: true});
+    this._list.registerRequiredCSS('emulation/locationsSettingsTab.css', {enableLegacyPatching: false});
     this._list.show(this.contentElement);
     /** @type {Common.Settings.Setting<Array<LocationDescription>>} */
     this._customSetting = /** @type {Common.Settings.Setting<Array<LocationDescription>>} */ (

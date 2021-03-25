7b65486d589f5e1602e1c37ad7d32b6c5fd09c1e
Jack Franklin <jacktfranklin@chromium.org>
2021-03-25 11:42:10 +0000

    Create a base mocha config file
    Whilst doing the test runner work over the last couple of weeks I
    noticed that we have a variety of `mocharc.js` files that are 99% the
    same, and I've had to make the same changes to most of them. This is
    the first step at creating a base function that can generate the parts
    of the config that are the same for each test suite.
    
    Bug: chromium:1186163
    Change-Id: I818b6831b8953084ee667f44c35a8f279fbdc55f
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2784977
    Commit-Queue: Jack Franklin <jacktfranklin@chromium.org>
    Reviewed-by: Paul Lewis <aerotwist@chromium.org>

---
+++ test/base-mocharc.js
@@ -0,0 +1,80 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+'use strict';
+
+const path = require('path');
+const glob = require('glob');
+const fs = require('fs');
+const {getTestRunnerConfigSetting} = require('../scripts/test/test_config_helpers.js');
+
+// To make sure that any leftover JavaScript files (e.g. that were outputs from now-removed tests)
+// aren't incorrectly included, we glob for the TypeScript files instead and use that
+// to instruct Mocha to run the output JavaScript file.
+const testRunnerCWDConfig = getTestRunnerConfigSetting('cwd');
+const testRunnerTestSourceDirConfig = getTestRunnerConfigSetting('test-suite-source-dir');
+
+function createMochaConfig({suiteName, extraMochaConfig = {}}) {
+  /**
+ * Set the ROOT_DIRECTORY based on the assumed folder structure, but if we have
+ * config from the new test runner, use that instead.
+ *
+ * Once we are fully migrated to the new test runner, this initial
+ * ROOT_DIRECTORY setting can go as we'll always have configuration provided.
+ *
+ * TODO(jacktfranklin): tidy up as part of crbug.com/1186163
+ */
+  let ROOT_DIRECTORY = path.join(__dirname, '..', '..', '..', '..', 'test', suiteName);
+  if (testRunnerCWDConfig && testRunnerTestSourceDirConfig) {
+    ROOT_DIRECTORY = path.join(testRunnerCWDConfig, testRunnerTestSourceDirConfig);
+  }
+  const allTestFiles = glob.sync(path.join(ROOT_DIRECTORY, '**/*_test.ts'));
+  /**
+ * TODO(jacktfranklin): once we are migrated to the new test runner, we can remove the fallback to process.env['TESET_PATTERNS']
+ */
+  const customPattern = getTestRunnerConfigSetting('test-file-pattern', process.env['TEST_PATTERNS']);
+
+  const testFiles = !customPattern ? allTestFiles :
+                                     customPattern.split(',')
+                                         .map(pattern => glob.sync(pattern, {absolute: true, cwd: ROOT_DIRECTORY}))
+                                         .flat()
+                                         .filter(filename => allTestFiles.includes(filename));
+
+
+  if (customPattern && testFiles.length === 0) {
+    throw new Error(
+        `\nNo test found matching custom pattern ${customPattern}.` +
+        ` Use a relative path from test/${suiteName}/.`);
+  }
+
+  const spec = testFiles.map(fileName => {
+    const renamedFile = fileName.replace(/\.ts$/, '.js');
+    const generatedFile = path.join(__dirname, suiteName, path.relative(ROOT_DIRECTORY, renamedFile));
+
+    if (!fs.existsSync(generatedFile)) {
+      throw new Error(`Test file missing in "ts_library": ${generatedFile}`);
+    }
+
+    return generatedFile;
+  });
+
+  // When we are debugging, we don't want to timeout any test. This allows to inspect the state
+  // of the application at the moment of the timeout. Here, 0 denotes "indefinite timeout".
+  const timeout = process.env['DEBUG'] ? 0 : 5 * 1000;
+
+  const jobs = Number(process.env['JOBS']) || 1;
+  const parallel = !process.env['DEBUG'] && jobs > 1;
+
+  return {
+    require: [path.join(__dirname, 'conductor', 'mocha_hooks.js'), 'source-map-support/register'],
+    spec,
+    timeout,
+    parallel,
+    jobs,
+    reporter: path.join(__dirname, 'shared', 'mocha-resultsdb-reporter'),
+    suiteName,
+    ...extraMochaConfig,
+  };
+}
+
+module.exports = {createMochaConfig};
--- test/e2e/.mocharc.js
+++ test/e2e/.mocharc.js
@@ -3,78 +3,19 @@
 // found in the LICENSE file.
 'use strict';
 
-const path = require('path');
-const glob = require('glob');
-const fs = require('fs');
 const {getTestRunnerConfigSetting} = require('../../scripts/test/test_config_helpers.js');
+const {createMochaConfig} = require('../base-mocharc.js');
 
-// To make sure that any leftover JavaScript files (e.g. that were outputs from now-removed tests)
-// aren't incorrectly included, we glob for the TypeScript files instead and use that
-// to instruct Mocha to run the output JavaScript file.
-const testRunnerCWDConfig = getTestRunnerConfigSetting('cwd');
-const testRunnerTestSourceDirConfig = getTestRunnerConfigSetting('test-suite-source-dir');
-
-/**
- * Set the ROOT_DIRECTORY based on the assumed folder structure, but if we have
- * config from the new test runner, use that instead.
- *
- * Once we are fully migrated to the new test runner, this initial
- * ROOT_DIRECTORY setting can go as we'll always have configuration provided.
- *
- * TODO(jacktfranklin): tidy up as part of crbug.com/1186163
- */
-let ROOT_DIRECTORY = path.join(__dirname, '..', '..', '..', '..', '..', 'test', 'e2e');
-if (testRunnerCWDConfig && testRunnerTestSourceDirConfig) {
-  ROOT_DIRECTORY = path.join(testRunnerCWDConfig, testRunnerTestSourceDirConfig);
-}
-const allTestFiles = glob.sync(path.join(ROOT_DIRECTORY, '**/*_test.ts'));
-/**
- * TODO(jacktfranklin): once we are migrated to the new test runner, we can remove the fallback to process.env['TESET_PATTERNS']
- */
-const customPattern = getTestRunnerConfigSetting('test-file-pattern', process.env['TEST_PATTERNS']);
-
-const testFiles = !customPattern ? allTestFiles :
-                                   customPattern.split(';')
-                                       .map(pattern => glob.sync(pattern, {absolute: true, cwd: ROOT_DIRECTORY}))
-                                       .flat()
-                                       .filter(filename => allTestFiles.includes(filename));
-
-
-if (customPattern && testFiles.length === 0) {
-  throw new Error(
-      `\nNo test found matching custom pattern ${customPattern}.` +
-      ' Use a relative path from test/e2e/.');
+// TODO (jacktfranklin): remove this once all scripts locally and all CQ bots use the new test runner and set --test-server-type=. crbug.com/1186163
+const testServerConfig = getTestRunnerConfigSetting('test-server-type');
+if (!testServerConfig) {
+  process.env.TEST_SERVER_TYPE = 'hosted-mode';
 }
 
-const spec = testFiles.map(fileName => {
-  const renamedFile = fileName.replace(/\.ts$/, '.js');
-  const generatedFile = path.join(__dirname, path.relative(ROOT_DIRECTORY, renamedFile));
-
-  if (!fs.existsSync(generatedFile)) {
-    throw new Error(`Test file missing in "ts_library": ${generatedFile}`);
-  }
-
-  return generatedFile;
-});
-
-// When we are debugging, we don't want to timeout any test. This allows to inspect the state
-// of the application at the moment of the timeout. Here, 0 denotes "indefinite timeout".
-const timeout = process.env['DEBUG'] ? 0 : 5 * 1000;
-
-// Our e2e tests are almost always slower than the default 75ms.
-const slow = 1000;
-
-const jobs = Number(process.env['JOBS']) || 1;
-const parallel = !process.env['DEBUG'] && jobs > 1;
-
-process.env.TEST_SERVER_TYPE = 'hosted-mode';
-module.exports = {
-  require: [path.join(__dirname, '..', 'conductor', 'mocha_hooks.js'), 'source-map-support/register'],
-  spec,
-  slow,
-  timeout,
-  parallel,
-  jobs,
-  reporter: path.join(__dirname, '..', 'shared', 'mocha-resultsdb-reporter'),
+module.exports = createMochaConfig({
   suiteName: 'e2e',
-}
+  extraMochaConfig: {
+    // E2E tests are much slower by default than Mocha's 75ms default threshold.
+    slow: 1000,
+  }
+})

7bb4a72c6d77a3cdec6889a9a809420c4851d475
Jan Scheffler <janscheffler@chromium.org>
2021-03-29 12:06:37 +0200

    [JSDOC2TS]: Migrate diff
    Bug: chromium:1158760
    Change-Id: Ie564c35149ccd9ffdb10d9b1e89232b8c6d1f7c8
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2789334
    Commit-Queue: Jan Scheffler <janscheffler@chromium.org>
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>

--- front_end/diff/DiffWrapper.ts
+++ front_end/diff/DiffWrapper.ts
@@ -5,13 +5,7 @@
 import * as Common from '../common/common.js';
 
 export const DiffWrapper = {
-  /**
-   * @param {string} text1
-   * @param {string} text2
-   * @param {boolean=} cleanup
-   * @return {!Array.<!{0: number, 1: string}>}
-   */
-  charDiff: function(text1, text2, cleanup) {
+  charDiff: function(text1: string, text2: string, cleanup?: boolean): {0: number, 1: string}[] {
     const differ = new diff_match_patch();
     const diff = differ.diff_main(text1, text2);
     if (cleanup) {
@@ -20,14 +14,8 @@ export const DiffWrapper = {
     return diff;
   },
 
-  /**
-   * @param {!Array.<string>} lines1
-   * @param {!Array.<string>} lines2
-   * @return {!DiffArray}
-   */
-  lineDiff: function(lines1, lines2) {
-    /** @type {!Common.CharacterIdMap.CharacterIdMap<string>} */
-    const idMap = new Common.CharacterIdMap.CharacterIdMap();
+  lineDiff: function(lines1: string[], lines2: string[]): DiffArray {
+    const idMap = new Common.CharacterIdMap.CharacterIdMap<string>();
     const text1 = lines1.map(line => idMap.toChar(line)).join('');
     const text2 = lines2.map(line => idMap.toChar(line)).join('');
 
@@ -39,16 +27,12 @@ export const DiffWrapper = {
         lines.push(idMap.fromChar(diff[i][1][j]) || '');
       }
 
-      lineDiff.push({0: diff[i][0], 1: lines});
+      lineDiff.push({ 0: diff[i][0], 1: lines });
     }
     return lineDiff;
   },
 
-  /**
-   * @param {!DiffArray} diff
-   * @return {!Array<!Array<number>>}
-   */
-  convertToEditDiff: function(diff) {
+  convertToEditDiff: function(diff: DiffArray): number[][] {
     const normalized = [];
     let added = 0;
     let removed = 0;
@@ -57,16 +41,18 @@ export const DiffWrapper = {
       if (token[0] === Operation.Equal) {
         flush();
         normalized.push([Operation.Equal, token[1].length]);
-      } else if (token[0] === Operation.Delete) {
+      }
+      else if (token[0] === Operation.Delete) {
         removed += token[1].length;
-      } else {
+      }
+      else {
         added += token[1].length;
       }
     }
     flush();
     return normalized;
 
-    function flush() {
+    function flush(): void {
       if (added && removed) {
         const min = Math.min(added, removed);
         normalized.push([Operation.Edit, min]);
@@ -85,11 +71,8 @@ export const DiffWrapper = {
 
   /**
    * Scores character-sequence diffs, giving higher scores for longer sequences.
-   * @param {string} item
-   * @param {string} against
-   * @return {number}
    */
-  characterScore: function(item, against) {
+  characterScore: function(item: string, against: string): number {
     let score = 0;
     const diff = DiffWrapper.charDiff(item, against);
     for (let i = 0; i < diff.length; ++i) {
@@ -99,17 +82,15 @@ export const DiffWrapper = {
     }
     return score;
   },
-
 };
 
-/** @enum {number} */
-export const Operation = {
-  Equal: 0,
-  Insert: 1,
-  Delete: -1,
-  Edit: 2
-};
+// TODO(crbug.com/1167717): Make this a const enum again
+// eslint-disable-next-line rulesdir/const_enum
+export enum Operation {
+  Equal = 0,
+  Insert = 1,
+  Delete = -1,
+  Edit = 2,
+}
 
-/** @typedef {!Array<!{0: !Operation, 1: !Array<string>}>} */
-// @ts-ignore typedef
-export let DiffArray;
+export type DiffArray = {0: Operation, 1: string[]}[];

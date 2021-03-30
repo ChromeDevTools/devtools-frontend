9327dc31d27d2a5061636ce35e726dfd755a6dd3
Tim van der Lippe <tvanderlippe@chromium.org>
2021-03-30 12:02:19 +0100

    Move *_test_runner to legacy_test_runner
    All layout tests are using `loadTestModule` and therefore, we can
    now move all folders containing legacy layout test runners into
    `legacy_test_runner`. Moreover, we can now remove the `module.json`
    files in all of these folders.
    
    We now include integration_test_runner as an entrypoint. As such,
    build_release_applications no longer processes integration_test_runner
    and GN takes over.
    
    R=aerotwist@chromium.org
    
    Bug: 1187573, 1127902
    Change-Id: I6b93bdf8fccc99dbfd4c9ce8cd55e03f55f6b1bd
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2784729
    Commit-Queue: Tim van der Lippe <tvanderlippe@chromium.org>
    Reviewed-by: Paul Lewis <aerotwist@chromium.org>

---
+++ front_end/legacy_test_runner/legacy_test_runner.ts
@@ -0,0 +1,30 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import '../devtools_app.js';
+import '../panels/accessibility/accessibility-legacy.js';
+import '../panels/animation/animation-legacy.js';
+import '../bindings/bindings-legacy.js';
+import '../color_picker/color_picker-legacy.js';
+import '../common/common-legacy.js';
+import '../data_grid/data_grid-legacy.js';
+import '../panels/developer_resources/developer_resources-legacy.js';
+import '../diff/diff-legacy.js';
+import '../panels/event_listeners/event_listeners-legacy.js';
+import '../extensions/extensions-legacy.js';
+import '../formatter/formatter-legacy.js';
+import '../har_importer/har_importer-legacy.js';
+import '../host/host-legacy.js';
+import '../inline_editor/inline_editor-legacy.js';
+import '../root/root-legacy.js';
+import '../sdk/sdk-legacy.js';
+import './test_runner/test_runner.js';
+
+// @ts-ignore
+if (self.testRunner) {
+  // @ts-ignore
+  testRunner.dumpAsText();
+  // @ts-ignore
+  testRunner.waitUntilDone();
+}
---
+++ front_end/legacy_test_runner/sources_test_runner/sources_test_runner.js
@@ -0,0 +1,19 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import '../test_runner/test_runner.js';
+import '../../sources/sources-legacy.js';
+import '../../workspace/workspace-legacy.js';
+import '../../source_frame/source_frame-legacy.js';
+import '../../text_utils/text_utils-legacy.js';
+import '../../object_ui/object_ui-legacy.js';
+import '../../text_editor/text_editor-legacy.js';
+import '../../panels/browser_debugger/browser_debugger-legacy.js';
+
+import './SourcesTestRunner.js';
+import './DebuggerTestRunner.js';
+import './LiveEditTestRunner.js';
+import './SearchTestRunner.js';
+import './EditorTestRunner.js';
+import './AutocompleteTestRunner.js';

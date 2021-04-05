1f0c5567a0aefcc4b25aeaaa5ff002bb3deba539
Jan Scheffler <janscheffler@chromium.org>
2021-04-05 15:09:08 +0200

    [JSDOC2TS]: Migrate HistoryInput in ui
    Bug: chromium:1158760
    Change-Id: I5a73855a4731affdf5cc0cdc9fb15170bc78da22
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2790871
    Commit-Queue: Jan Scheffler <janscheffler@chromium.org>
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>

--- front_end/ui/HistoryInput.ts
+++ front_end/ui/HistoryInput.ts
@@ -2,13 +2,19 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+/* eslint-disable rulesdir/no_underscored_properties */
+
 import {Keys} from './KeyboardShortcut.js';
 import {registerCustomElement} from './utils/register-custom-element.js';
 
-/** @type {?function():!Element} */
-let _constructor = null;
+// TODO(crbug.com/1172300) Ignored during the jsdoc to ts migration
+// eslint-disable-next-line @typescript-eslint/naming-convention
+let _constructor: (() => Element)|null = null;
 
 export class HistoryInput extends HTMLInputElement {
+  _history: string[];
+  _historyPosition: number;
+
   constructor() {
     super();
     this._history = [''];
@@ -16,31 +22,23 @@ export class HistoryInput extends HTMLInputElement {
     this.addEventListener('keydown', this._onKeyDown.bind(this), false);
     this.addEventListener('input', this._onInput.bind(this), false);
   }
-  /**
-   * @return {!HistoryInput}
-   */
-  static create() {
+
+  static create(): HistoryInput {
     if (!_constructor) {
       _constructor = registerCustomElement('input', 'history-input', HistoryInput);
     }
 
-    return /** @type {!HistoryInput} */ (_constructor());
+    return _constructor() as HistoryInput;
   }
 
-  /**
-   * @param {!Event} event
-   */
-  _onInput(event) {
+  _onInput(_event: Event): void {
     if (this._history.length === this._historyPosition + 1) {
       this._history[this._history.length - 1] = this.value;
     }
   }
 
-  /**
-   * @param {!Event} ev
-   */
-  _onKeyDown(ev) {
-    const event = /** @type {!KeyboardEvent} */ (ev);
+  _onKeyDown(ev: Event): void {
+    const event = (ev as KeyboardEvent);
     if (event.keyCode === Keys.Up.code) {
       this._historyPosition = Math.max(this._historyPosition - 1, 0);
       this.value = this._history[this._historyPosition];
@@ -56,7 +54,7 @@ export class HistoryInput extends HTMLInputElement {
     }
   }
 
-  _saveToHistory() {
+  _saveToHistory(): void {
     if (this._history.length > 1 && this._history[this._history.length - 2] === this.value) {
       return;
     }

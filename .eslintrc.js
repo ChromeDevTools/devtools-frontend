331b5024fb5b0ac2cb4a1a6dfa2dba1ef8d50ee8
Tim van der Lippe <tvanderlippe@chromium.org>
2021-04-09 14:45:49 +0100

    Remove ESLint rules concerning spacing
    All of these rules are enforcing concerns that our formatter should
    perform. Therefore, remove the rules and let clang-format handle
    the spacing.
    
    R=jacktfranklin@chromium.org
    
    Bug: none
    Change-Id: I9e84f76847780aa3b8dbe30cb0cac97c43a58d0c
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2817609
    Commit-Queue: Tim van der Lippe <tvanderlippe@chromium.org>
    Commit-Queue: Jack Franklin <jacktfranklin@chromium.org>
    Auto-Submit: Tim van der Lippe <tvanderlippe@chromium.org>
    Reviewed-by: Jack Franklin <jacktfranklin@chromium.org>

--- .eslintrc.js
+++ .eslintrc.js
@@ -85,25 +85,6 @@ module.exports = {
     'require-yield': 2,
     'template-curly-spacing': [2, 'never'],
 
-    // spacing details
-    'space-infix-ops': 2,
-    'space-in-parens': [2, 'never'],
-    'no-whitespace-before-property': 2,
-    'keyword-spacing': [
-      2, {
-        'overrides': {
-          'if': {'after': true},
-          'else': {'after': true},
-          'for': {'after': true},
-          'while': {'after': true},
-          'do': {'after': true},
-          'switch': {'after': true},
-          'return': {'after': true}
-        }
-      }
-    ],
-    'arrow-spacing': [2, {'after': true, 'before': true}],
-
     // file whitespace
     'no-multiple-empty-lines': [2, {'max': 2}],
     'no-mixed-spaces-and-tabs': 2,

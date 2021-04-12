b276117f81a768b00e6627fe47981b88546a4c6c
Tim van der Lippe <tvanderlippe@chromium.org>
2021-04-12 15:09:59 +0100

    Bundle meta files into separate entrypoint bundles
    By bundling all meta files (which happens automatically, as Rollup
    only treats filenames that are equivalent to the direct folder
    they are in, which `-meta.ts` files by definition do not), we
    reduce the amount of network requests performed at startup.
    
    Before this CL:
    
    Mean boot time: 1115.99ms
    50th percentile boot time: 1115.28ms
    90th percentile boot time: 1122.66ms
    99th percentile boot time: 1159.44ms
    
    After this CL:
    
    Mean boot time: 1105.17ms
    50th percentile boot time: 1072.34ms
    90th percentile boot time: 1111.8ms
    99th percentile boot time: 1442.42ms
    
    Note that this is a marginal difference, but run on good hardware.
    The theory is that this change will be more benificial for users
    running slower hardware.
    
    R=jacktfranklin@chromium.org
    
    Bug: none
    Change-Id: I24aa02d9f162d1ac07ceae5df2beaa0a61a7dd43
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2790878
    Commit-Queue: Tim van der Lippe <tvanderlippe@chromium.org>
    Reviewed-by: Jack Franklin <jacktfranklin@chromium.org>

---
+++ front_end/entrypoints/shell/shell-meta-files.ts
@@ -0,0 +1,22 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import '../../sources/sources-meta.js';
+import '../../panels/profiler/profiler-meta.js';
+import '../../panels/console/console-meta.js';
+import '../../panels/coverage/coverage-meta.js';
+import '../../panels/changes/changes-meta.js';
+import '../../panels/input/input-meta.js';
+import '../../linear_memory_inspector/linear_memory_inspector-meta.js';
+import '../../settings/settings-meta.js';
+import '../../panels/protocol_monitor/protocol_monitor-meta.js';
+import '../../models/persistence/persistence-meta.js';
+import '../../browser_sdk/browser_sdk-meta.js';
+import '../../main/main-meta.js';
+import '../../perf_ui/perf_ui-meta.js';
+import '../../quick_open/quick_open-meta.js';
+import '../../core/sdk/sdk-meta.js';
+import '../../source_frame/source_frame-meta.js';
+import '../../panels/console_counters/console_counters-meta.js';
+import '../../object_ui/object_ui-meta.js';
--- front_end/entrypoints/shell/shell.js
+++ front_end/entrypoints/shell/shell.js
@@ -16,22 +16,6 @@ import '../../models/persistence/persistence-legacy.js';
 import '../../browser_sdk/browser_sdk.js';
 import '../../models/extensions/extensions-legacy.js';
 import '../../panels/console_counters/console_counters-legacy.js';
-import '../../sources/sources-meta.js';
-import '../../panels/profiler/profiler-meta.js';
-import '../../panels/console/console-meta.js';
-import '../../panels/coverage/coverage-meta.js';
-import '../../panels/changes/changes-meta.js';
-import '../../panels/input/input-meta.js';
-import '../../linear_memory_inspector/linear_memory_inspector-meta.js';
-import '../../settings/settings-meta.js';
-import '../../panels/protocol_monitor/protocol_monitor-meta.js';
-import '../../models/persistence/persistence-meta.js';
-import '../../browser_sdk/browser_sdk-meta.js';
-import '../../main/main-meta.js';
-import '../../perf_ui/perf_ui-meta.js';
-import '../../quick_open/quick_open-meta.js';
-import '../../core/sdk/sdk-meta.js';
-import '../../source_frame/source_frame-meta.js';
-import '../../panels/console_counters/console_counters-meta.js';
-import '../../object_ui/object_ui-meta.js';
+
+import './shell-meta-files.js';
 // We generate the descriptors in this file, which depend on the runtime.

06d933111d93fb8d48da7001b43dd34500b3d2b0
Sigurd Schneider <sigurds@chromium.org>
2021-04-27 15:57:04 +0200

    Clear error icons when row bucket detaches from frame
    Bug: chromium:1202455
    Change-Id: I7b85a0f94aec5c7f805819ce860a0f3080694790
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2848233
    Reviewed-by: Wolfgang Beyer <wolfi@chromium.org>
    Commit-Queue: Sigurd Schneider <sigurds@chromium.org>
    (cherry picked from commit 6b8d3a71bc4d5f7f43dc6a6a2760d6fd315479ff)
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2853557

--- front_end/sources/UISourceCodeFrame.ts
+++ front_end/sources/UISourceCodeFrame.ts
@@ -766,6 +766,9 @@ export class RowMessageBucket {
       this.textEditor.removeDecoration(this._decoration, editorLineNumber);
       this._decorationStartColumn = null;
     }
+    if (this.bookmark) {
+      this.bookmark.clear();
+    }
   }
 
   uniqueMessagesCount(): number {

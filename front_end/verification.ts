300f2b3d7b972245dd3e92e6ce9f75027863d63b
Tim van der Lippe <tvanderlippe@chromium.org>
2021-04-28 13:40:45 +0100

    Add GN visibility for models/
    Downstream projects can define the devtools_models_visibility in
    their `default_args` in their `.gn` file to add any other
    dependencies.
    
    R=jacktfranklin@chromium.org
    
    Bug: 1202788
    Change-Id: I7e9ab02d3bcb0f76b170f3a5ffe67afb790d6285
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2854687
    Commit-Queue: Tim van der Lippe <tvanderlippe@chromium.org>
    Commit-Queue: Jack Franklin <jacktfranklin@chromium.org>
    Auto-Submit: Tim van der Lippe <tvanderlippe@chromium.org>
    Reviewed-by: Jack Franklin <jacktfranklin@chromium.org>

--- front_end/verification.ts
+++ front_end/verification.ts
@@ -2,11 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import './models/logs/logs.js';
 import './core/common/common.js';
 import './core/host/host.js';
 import './inspector_overlay/inspector_overlay.js';
 import './core/protocol_client/protocol_client.js';
 import './core/root/root.js';
 import './core/sdk/sdk.js';
-import './models/workspace/workspace.js';

381c20278c49c8341374f9458b66b4476036781c
Christy Chen <chrche@microsoft.com>
2021-04-28 00:04:39 -0700

    Remove Chrome Data Saver console message
    The Chrome Data Saver has been deprecated and setting is removed in https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2698373
    This CL removes the leftover console message.
    
    Bug: 1056922
    Change-Id: Ia6ccf256ce0f4cdcc4af25d1432e26d8166c4330
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2856107
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Patrick Brosset <patrick.brosset@microsoft.com>
    Commit-Queue: Christy Chen <chrche@microsoft.com>

--- front_end/core/sdk/PageLoad.ts
+++ front_end/core/sdk/PageLoad.ts
@@ -2,20 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import * as i18n from '../i18n/i18n.js';
-import {Events as NetworkManagerEvents, NetworkManager} from './NetworkManager.js';  // eslint-disable-line no-unused-vars
-import {Events as NetworkRequestEvents, NetworkRequest} from './NetworkRequest.js';
-
-const UIStrings = {
-  /**
-  *@description Text in Network Log
-  *@example {Chrome Data Saver} PH1
-  *@example {https://example.com} PH2
-  */
-  considerDisablingSWhileDebugging: 'Consider disabling {PH1} while debugging. For more info see: {PH2}',
-};
-const str_ = i18n.i18n.registerUIStrings('core/sdk/PageLoad.ts', UIStrings);
-const i18nString = i18n.i18n.getLocalizedString.bind(undefined, str_);
+import {NetworkRequest} from './NetworkRequest.js';
 
 export class PageLoad {
   id: number;
@@ -30,28 +17,6 @@ export class PageLoad {
     this.url = mainRequest.url();
     this.startTime = mainRequest.startTime;
     this.mainRequest = mainRequest;
-
-    this.showDataSaverWarningIfNeeded();
-  }
-
-  private async showDataSaverWarningIfNeeded(): Promise<void> {
-    const manager = NetworkManager.forRequest(this.mainRequest);
-    if (!manager) {
-      return;
-    }
-    if (!this.mainRequest.finished) {
-      await this.mainRequest.once(NetworkRequestEvents.FinishedLoading);
-    }
-    const saveDataHeader = this.mainRequest.requestHeaderValue('Save-Data');
-    if (!PageLoad.dataSaverMessageWasShown && saveDataHeader && saveDataHeader === 'on') {
-      const message = i18nString(
-          UIStrings.considerDisablingSWhileDebugging,
-          {PH1: 'Chrome Data Saver', PH2: 'https://support.google.com/chrome/?p=datasaver'});
-      manager.dispatchEventToListeners(
-          NetworkManagerEvents.MessageGenerated,
-          {message: message, requestId: this.mainRequest.requestId(), warning: true});
-      PageLoad.dataSaverMessageWasShown = true;
-    }
   }
 
   static forRequest(request: NetworkRequest): PageLoad|null {
@@ -63,7 +28,6 @@ export class PageLoad {
   }
 
   private static lastIdentifier = 0;
-  private static dataSaverMessageWasShown = false;
 }
 
 const pageLoadForRequest = new WeakMap<NetworkRequest, PageLoad>();

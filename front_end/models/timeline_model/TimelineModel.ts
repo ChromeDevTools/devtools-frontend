e967dcb9bebdd2b33464207dae7a5627949319cd
Sigurd Schneider <sigurds@chromium.org>
2021-05-05 09:34:39 +0200

    Don't adjust line numbers more than once on trace import
    Bug: chromium:1205430
    Change-Id: Id2d37562988a4b9fc9bf6e442a3eb9c316b70d90
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2871719
    Reviewed-by: Paul Lewis <aerotwist@chromium.org>
    Commit-Queue: Sigurd Schneider <sigurds@chromium.org>

--- front_end/models/timeline_model/TimelineModel.ts
+++ front_end/models/timeline_model/TimelineModel.ts
@@ -904,15 +904,21 @@ export class TimelineModelImpl {
     const eventData = event.args['data'] || event.args['beginData'] || {};
     const timelineData = TimelineData.forEvent(event);
     if (eventData['stackTrace']) {
-      timelineData.stackTrace = eventData['stackTrace'];
-    }
-    if (timelineData.stackTrace && event.name !== RecordType.JSSample) {
-      // TraceEvents come with 1-based line & column numbers. The frontend code
-      // requires 0-based ones. Adjust the values.
-      for (let i = 0; i < timelineData.stackTrace.length; ++i) {
-        --timelineData.stackTrace[i].lineNumber;
-        --timelineData.stackTrace[i].columnNumber;
-      }
+      timelineData.stackTrace = eventData['stackTrace'].map((callFrameOrProfileNode: Protocol.Runtime.CallFrame) => {
+        // `callFrameOrProfileNode` can also be a `SDK.ProfileTreeModel.ProfileNode` for JSSample; that class
+        // has accessors to mimic a `CallFrame`, but apparently we don't adjust stack traces in that case. Whether
+        // we should is unclear.
+        if (event.name !== RecordType.JSSample) {
+          // We need to copy the data so we can safely modify it below.
+          const frame = {...callFrameOrProfileNode};
+          // TraceEvents come with 1-based line & column numbers. The frontend code
+          // requires 0-based ones. Adjust the values.
+          --frame.lineNumber;
+          --frame.columnNumber;
+          return frame;
+        }
+        return callFrameOrProfileNode;
+      });
     }
     let pageFrameId = TimelineModelImpl.eventFrameId(event);
     const last = eventStack[eventStack.length - 1];
@@ -956,7 +962,7 @@ export class TimelineModelImpl {
       case RecordType.StyleRecalcInvalidationTracking:
       case RecordType.StyleInvalidatorInvalidationTracking:
       case RecordType.LayoutInvalidationTracking: {
-        this._invalidationTracker.addInvalidation(new InvalidationTrackingEvent(event));
+        this._invalidationTracker.addInvalidation(new InvalidationTrackingEvent(event, timelineData));
         break;
       }
 
@@ -1947,7 +1953,7 @@ export class InvalidationTrackingEvent {
   cause: InvalidationCause;
   linkedRecalcStyleEvent: boolean;
   linkedLayoutEvent: boolean;
-  constructor(event: SDK.TracingModel.Event) {
+  constructor(event: SDK.TracingModel.Event, timelineData: TimelineData) {
     this.type = event.name;
     this.startTime = event.startTime;
     this._tracingEvent = event;
@@ -1966,7 +1972,7 @@ export class InvalidationTrackingEvent {
     this.selectorPart = eventData['selectorPart'];
     this.extraData = eventData['extraData'];
     this.invalidationList = eventData['invalidationList'];
-    this.cause = {reason: eventData['reason'], stackTrace: eventData['stackTrace']};
+    this.cause = {reason: eventData['reason'], stackTrace: timelineData.stackTrace};
     this.linkedRecalcStyleEvent = false;
     this.linkedLayoutEvent = false;
 
@@ -2118,7 +2124,8 @@ export class InvalidationTracker {
 
   _addSyntheticStyleRecalcInvalidation(
       baseEvent: SDK.TracingModel.Event, styleInvalidatorInvalidation: InvalidationTrackingEvent): void {
-    const invalidation = new InvalidationTrackingEvent(baseEvent);
+    const timelineData = TimelineData.forEvent(baseEvent);
+    const invalidation = new InvalidationTrackingEvent(baseEvent, timelineData);
     invalidation.type = RecordType.StyleRecalcInvalidationTracking;
     if (styleInvalidatorInvalidation.cause.reason) {
       invalidation.cause.reason = styleInvalidatorInvalidation.cause.reason;

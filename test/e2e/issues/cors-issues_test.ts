d460aae69dbd013bb055fd4279b1caea7ce35909
Sigurd Schneider <sigurds@chromium.org>
2021-05-14 14:49:39 +0200

    Simplify CORS e2e test logic
    Bug: chromium:1141824
    Change-Id: I68a1720874cf660adfad11683248486a4b7b3932
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2897281
    Reviewed-by: Jack Franklin <jacktfranklin@chromium.org>
    Commit-Queue: Sigurd Schneider <sigurds@chromium.org>

--- test/e2e/issues/cors-issues_test.ts
+++ test/e2e/issues/cors-issues_test.ts
@@ -4,7 +4,7 @@
 
 import {assert} from 'chai';
 
-import {assertMatchArray, getBrowserAndPages, goToResource} from '../../shared/helper.js';
+import {assertMatchArray, assertNotNull, getBrowserAndPages, goToResource} from '../../shared/helper.js';
 import {describe, it} from '../../shared/mocha-extensions.js';
 import {ensureResourceSectionIsExpanded, expandIssue, extractTableFromResourceSection, getIssueByTitle, getResourcesElement, navigateToIssuesTab} from '../helpers/issues-helpers.js';
 
@@ -19,50 +19,46 @@ describe('CORS issues', async () => {
     await navigateToIssuesTab();
     await expandIssue();
     const issueElement = await getIssueByTitle('Ensure CORS response header values are valid');
-    assert.isNotNull(issueElement);
-    if (issueElement) {
-      const section = await getResourcesElement('requests', issueElement, '.cors-issue-affected-resource-label');
-      const text = await section.label.evaluate(el => el.textContent);
-      assert.strictEqual(text, '3 requests');
-      await ensureResourceSectionIsExpanded(section);
-      const table = await extractTableFromResourceSection(section.content);
-      assert.isNotNull(table);
-      if (table) {
-        assert.strictEqual(table.length, 4);
-        assert.deepEqual(table[0], [
-          'Request',
-          'Status',
-          'Preflight Request (if problematic)',
-          'Header',
-          'Problem',
-          'Invalid Value (if available)',
-        ]);
-        assertMatchArray(table[1], [
-          /^devtools.oopif.test:.*/,
-          'blocked',
-          '',
-          'Access-Control-Allow-Origin',
-          'Missing Header',
-          '',
-        ]);
-        assertMatchArray(table[2], [
-          /^devtools.oopif.test:.*/,
-          'blocked',
-          /^devtools.oopif.test:.*/,
-          'Access-Control-Allow-Origin',
-          'Missing Header',
-          '',
-        ]);
-        assertMatchArray(table[3], [
-          /.*invalid-preflight.*/,
-          'blocked',
-          /.*invalid-preflight.*/,
-          'Access-Control-Allow-Origin',
-          'Missing Header',
-          '',
-        ]);
-      }
-    }
+    assertNotNull(issueElement);
+    const section = await getResourcesElement('requests', issueElement, '.cors-issue-affected-resource-label');
+    const text = await section.label.evaluate(el => el.textContent);
+    assert.strictEqual(text, '3 requests');
+    await ensureResourceSectionIsExpanded(section);
+    const table = await extractTableFromResourceSection(section.content);
+    assertNotNull(table);
+    assert.strictEqual(table.length, 4);
+    assert.deepEqual(table[0], [
+      'Request',
+      'Status',
+      'Preflight Request (if problematic)',
+      'Header',
+      'Problem',
+      'Invalid Value (if available)',
+    ]);
+    assertMatchArray(table[1], [
+      /^devtools.oopif.test:.*/,
+      'blocked',
+      '',
+      'Access-Control-Allow-Origin',
+      'Missing Header',
+      '',
+    ]);
+    assertMatchArray(table[2], [
+      /^devtools.oopif.test:.*/,
+      'blocked',
+      /^devtools.oopif.test:.*/,
+      'Access-Control-Allow-Origin',
+      'Missing Header',
+      '',
+    ]);
+    assertMatchArray(table[3], [
+      /.*invalid-preflight.*/,
+      'blocked',
+      /.*invalid-preflight.*/,
+      'Access-Control-Allow-Origin',
+      'Missing Header',
+      '',
+    ]);
   });
 
   it('should display credentialed+wildcard CORS issues with the correct affected resources', async () => {
@@ -81,28 +77,24 @@ describe('CORS issues', async () => {
     await expandIssue();
     const issueElement =
         await getIssueByTitle('Ensure credentialed requests are not sent to CORS resources with origin wildcards');
-    assert.isNotNull(issueElement);
-    if (issueElement) {
-      const section = await getResourcesElement('request', issueElement, '.cors-issue-affected-resource-label');
-      const text = await section.label.evaluate(el => el.textContent);
-      assert.strictEqual(text, '1 request');
-      await ensureResourceSectionIsExpanded(section);
-      const table = await extractTableFromResourceSection(section.content);
-      assert.isNotNull(table);
-      if (table) {
-        assert.strictEqual(table.length, 2);
-        assert.deepEqual(table[0], [
-          'Request',
-          'Status',
-          'Preflight Request (if problematic)',
-        ]);
-        assertMatchArray(table[1], [
-          'origin-wildcard.rawresponse',
-          'blocked',
-          '',
-        ]);
-      }
-    }
+    assertNotNull(issueElement);
+    const section = await getResourcesElement('request', issueElement, '.cors-issue-affected-resource-label');
+    const text = await section.label.evaluate(el => el.textContent);
+    assert.strictEqual(text, '1 request');
+    await ensureResourceSectionIsExpanded(section);
+    const table = await extractTableFromResourceSection(section.content);
+    assertNotNull(table);
+    assert.strictEqual(table.length, 2);
+    assert.deepEqual(table[0], [
+      'Request',
+      'Status',
+      'Preflight Request (if problematic)',
+    ]);
+    assertMatchArray(table[1], [
+      'origin-wildcard.rawresponse',
+      'blocked',
+      '',
+    ]);
   });
 
   it('should display invalid CORS preflight response codes with the correct affected resources', async () => {
@@ -132,36 +124,32 @@ describe('CORS issues', async () => {
     await navigateToIssuesTab();
     await expandIssue();
     const issueElement = await getIssueByTitle('Ensure preflight responses are valid');
-    assert.isNotNull(issueElement);
-    if (issueElement) {
-      const section = await getResourcesElement('requests', issueElement, '.cors-issue-affected-resource-label');
-      const text = await section.label.evaluate(el => el.textContent);
-      assert.strictEqual(text, '2 requests');
-      await ensureResourceSectionIsExpanded(section);
-      const table = await extractTableFromResourceSection(section.content);
-      assert.isNotNull(table);
-      if (table) {
-        assert.strictEqual(table.length, 3);
-        assert.deepEqual(table[0], [
-          'Request',
-          'Status',
-          'Preflight Request',
-          'Problem',
-        ]);
-        assertMatchArray(table[1], [
-          'invalid-response-code.rawresponse',
-          'blocked',
-          'invalid-response-code.rawresponse',
-          'HTTP status of preflight request didn\'t indicate success',
-        ]);
-        assertMatchArray(table[2], [
-          'redirect.rawresponse',
-          'blocked',
-          'redirect.rawresponse',
-          'Response to preflight was a redirect',
-        ]);
-      }
-    }
+    assertNotNull(issueElement);
+    const section = await getResourcesElement('requests', issueElement, '.cors-issue-affected-resource-label');
+    const text = await section.label.evaluate(el => el.textContent);
+    assert.strictEqual(text, '2 requests');
+    await ensureResourceSectionIsExpanded(section);
+    const table = await extractTableFromResourceSection(section.content);
+    assertNotNull(table);
+    assert.strictEqual(table.length, 3);
+    assert.deepEqual(table[0], [
+      'Request',
+      'Status',
+      'Preflight Request',
+      'Problem',
+    ]);
+    assertMatchArray(table[1], [
+      'invalid-response-code.rawresponse',
+      'blocked',
+      'invalid-response-code.rawresponse',
+      'HTTP status of preflight request didn\'t indicate success',
+    ]);
+    assertMatchArray(table[2], [
+      'redirect.rawresponse',
+      'blocked',
+      'redirect.rawresponse',
+      'Response to preflight was a redirect',
+    ]);
   });
 
   it('should display CORS ACAO mismatches with the correct affected resources', async () => {
@@ -188,39 +176,35 @@ describe('CORS issues', async () => {
     await navigateToIssuesTab();
     await expandIssue();
     const issueElement = await getIssueByTitle('Ensure CORS requesting origin matches resource\'s allowed origin');
-    assert.isNotNull(issueElement);
-    if (issueElement) {
-      const section = await getResourcesElement('requests', issueElement, '.cors-issue-affected-resource-label');
-      const text = await section.label.evaluate(el => el.textContent);
-      assert.strictEqual(text, '2 requests');
-      await ensureResourceSectionIsExpanded(section);
-      const table = await extractTableFromResourceSection(section.content);
-      assert.isNotNull(table);
-      if (table) {
-        assert.strictEqual(table.length, 3);
-        assert.deepEqual(table[0], [
-          'Request',
-          'Status',
-          'Preflight Request (if problematic)',
-          'Initiator Context',
-          'Allowed Origin (from header)',
-        ]);
-        assertMatchArray(table[1], [
-          'acao-mismatch.rawresponse',
-          'blocked',
-          'acao-mismatch.rawresponse',
-          /^https:\/\/localhost.*/,
-          'https://devtools.oopif.test',
-        ]);
-        assertMatchArray(table[2], [
-          'acao-mismatch.rawresponse',
-          'blocked',
-          '',
-          /^https:\/\/localhost.*/,
-          'https://devtools.oopif.test',
-        ]);
-      }
-    }
+    assertNotNull(issueElement);
+    const section = await getResourcesElement('requests', issueElement, '.cors-issue-affected-resource-label');
+    const text = await section.label.evaluate(el => el.textContent);
+    assert.strictEqual(text, '2 requests');
+    await ensureResourceSectionIsExpanded(section);
+    const table = await extractTableFromResourceSection(section.content);
+    assertNotNull(table);
+    assert.strictEqual(table.length, 3);
+    assert.deepEqual(table[0], [
+      'Request',
+      'Status',
+      'Preflight Request (if problematic)',
+      'Initiator Context',
+      'Allowed Origin (from header)',
+    ]);
+    assertMatchArray(table[1], [
+      'acao-mismatch.rawresponse',
+      'blocked',
+      'acao-mismatch.rawresponse',
+      /^https:\/\/localhost.*/,
+      'https://devtools.oopif.test',
+    ]);
+    assertMatchArray(table[2], [
+      'acao-mismatch.rawresponse',
+      'blocked',
+      '',
+      /^https:\/\/localhost.*/,
+      'https://devtools.oopif.test',
+    ]);
   });
 
   it('should display invalid CORS ACAC values with the correct affected resources', async () => {
@@ -247,36 +231,32 @@ describe('CORS issues', async () => {
     await navigateToIssuesTab();
     await expandIssue();
     const issueElement = await getIssueByTitle('Ensure CORS requests include credentials only when allowed');
-    assert.isNotNull(issueElement);
-    if (issueElement) {
-      const section = await getResourcesElement('requests', issueElement, '.cors-issue-affected-resource-label');
-      const text = await section.label.evaluate(el => el.textContent);
-      assert.strictEqual(text, '2 requests');
-      await ensureResourceSectionIsExpanded(section);
-      const table = await extractTableFromResourceSection(section.content);
-      assert.isNotNull(table);
-      if (table) {
-        assert.strictEqual(table.length, 3);
-        assert.deepEqual(table[0], [
-          'Request',
-          'Status',
-          'Preflight Request (if problematic)',
-          'Access-Control-Allow-Credentials Header Value',
-        ]);
-        assertMatchArray(table[1], [
-          'acac-invalid.rawresponse',
-          'blocked',
-          'acac-invalid.rawresponse',
-          'false',
-        ]);
-        assertMatchArray(table[2], [
-          'acac-invalid.rawresponse',
-          'blocked',
-          '',
-          'false',
-        ]);
-      }
-    }
+    assertNotNull(issueElement);
+    const section = await getResourcesElement('requests', issueElement, '.cors-issue-affected-resource-label');
+    const text = await section.label.evaluate(el => el.textContent);
+    assert.strictEqual(text, '2 requests');
+    await ensureResourceSectionIsExpanded(section);
+    const table = await extractTableFromResourceSection(section.content);
+    assertNotNull(table);
+    assert.strictEqual(table.length, 3);
+    assert.deepEqual(table[0], [
+      'Request',
+      'Status',
+      'Preflight Request (if problematic)',
+      'Access-Control-Allow-Credentials Header Value',
+    ]);
+    assertMatchArray(table[1], [
+      'acac-invalid.rawresponse',
+      'blocked',
+      'acac-invalid.rawresponse',
+      'false',
+    ]);
+    assertMatchArray(table[2], [
+      'acac-invalid.rawresponse',
+      'blocked',
+      '',
+      'false',
+    ]);
   });
 
   it('should display CORS requests using disallowed methods with the correct affected resources', async () => {
@@ -298,30 +278,26 @@ describe('CORS issues', async () => {
     await navigateToIssuesTab();
     await expandIssue();
     const issueElement = await getIssueByTitle('Ensure CORS request uses allowed method');
-    assert.isNotNull(issueElement);
-    if (issueElement) {
-      const section = await getResourcesElement('request', issueElement, '.cors-issue-affected-resource-label');
-      const text = await section.label.evaluate(el => el.textContent);
-      assert.strictEqual(text, '1 request');
-      await ensureResourceSectionIsExpanded(section);
-      const table = await extractTableFromResourceSection(section.content);
-      assert.isNotNull(table);
-      if (table) {
-        assert.strictEqual(table.length, 2);
-        assert.deepEqual(table[0], [
-          'Request',
-          'Status',
-          'Preflight Request',
-          'Disallowed Request Method',
-        ]);
-        assertMatchArray(table[1], [
-          'method-disallowed.rawresponse',
-          'blocked',
-          'method-disallowed.rawresponse',
-          'PUT',
-        ]);
-      }
-    }
+    assertNotNull(issueElement);
+    const section = await getResourcesElement('request', issueElement, '.cors-issue-affected-resource-label');
+    const text = await section.label.evaluate(el => el.textContent);
+    assert.strictEqual(text, '1 request');
+    await ensureResourceSectionIsExpanded(section);
+    const table = await extractTableFromResourceSection(section.content);
+    assertNotNull(table);
+    assert.strictEqual(table.length, 2);
+    assert.deepEqual(table[0], [
+      'Request',
+      'Status',
+      'Preflight Request',
+      'Disallowed Request Method',
+    ]);
+    assertMatchArray(table[1], [
+      'method-disallowed.rawresponse',
+      'blocked',
+      'method-disallowed.rawresponse',
+      'PUT',
+    ]);
   });
 
   it('should display CORS requests using disallowed headers with the correct affected resources', async () => {
@@ -342,30 +318,26 @@ describe('CORS issues', async () => {
     await navigateToIssuesTab();
     await expandIssue();
     const issueElement = await getIssueByTitle('Ensure CORS request includes only allowed headers');
-    assert.isNotNull(issueElement);
-    if (issueElement) {
-      const section = await getResourcesElement('request', issueElement, '.cors-issue-affected-resource-label');
-      const text = await section.label.evaluate(el => el.textContent);
-      assert.strictEqual(text, '1 request');
-      await ensureResourceSectionIsExpanded(section);
-      const table = await extractTableFromResourceSection(section.content);
-      assert.isNotNull(table);
-      if (table) {
-        assert.strictEqual(table.length, 2);
-        assert.deepEqual(table[0], [
-          'Request',
-          'Status',
-          'Preflight Request',
-          'Disallowed Request Header',
-        ]);
-        assertMatchArray(table[1], [
-          'method-disallowed.rawresponse',
-          'blocked',
-          'method-disallowed.rawresponse',
-          'x-foo',
-        ]);
-      }
-    }
+    assertNotNull(issueElement);
+    const section = await getResourcesElement('request', issueElement, '.cors-issue-affected-resource-label');
+    const text = await section.label.evaluate(el => el.textContent);
+    assert.strictEqual(text, '1 request');
+    await ensureResourceSectionIsExpanded(section);
+    const table = await extractTableFromResourceSection(section.content);
+    assertNotNull(table);
+    assert.strictEqual(table.length, 2);
+    assert.deepEqual(table[0], [
+      'Request',
+      'Status',
+      'Preflight Request',
+      'Disallowed Request Header',
+    ]);
+    assertMatchArray(table[1], [
+      'method-disallowed.rawresponse',
+      'blocked',
+      'method-disallowed.rawresponse',
+      'x-foo',
+    ]);
   });
 
   it('should display CORS requests redirecting to credentialed URLs', async () => {
@@ -384,25 +356,21 @@ describe('CORS issues', async () => {
     await expandIssue();
     const issueElement =
         await getIssueByTitle('Ensure CORS requests are not redirected to URLs containing credentials');
-    assert.isNotNull(issueElement);
-    if (issueElement) {
-      const section = await getResourcesElement('request', issueElement, '.cors-issue-affected-resource-label');
-      const text = await section.label.evaluate(el => el.textContent);
-      assert.strictEqual(text, '1 request');
-      await ensureResourceSectionIsExpanded(section);
-      const table = await extractTableFromResourceSection(section.content);
-      assert.isNotNull(table);
-      if (table) {
-        assert.strictEqual(table.length, 2);
-        assert.deepEqual(table[0], [
-          'Request',
-          'Status',
-        ]);
-        assertMatchArray(table[1], [
-          'credentialed-redirect.rawresponse',
-          'blocked',
-        ]);
-      }
-    }
+    assertNotNull(issueElement);
+    const section = await getResourcesElement('request', issueElement, '.cors-issue-affected-resource-label');
+    const text = await section.label.evaluate(el => el.textContent);
+    assert.strictEqual(text, '1 request');
+    await ensureResourceSectionIsExpanded(section);
+    const table = await extractTableFromResourceSection(section.content);
+    assertNotNull(table);
+    assert.strictEqual(table.length, 2);
+    assert.deepEqual(table[0], [
+      'Request',
+      'Status',
+    ]);
+    assertMatchArray(table[1], [
+      'credentialed-redirect.rawresponse',
+      'blocked',
+    ]);
   });
 });

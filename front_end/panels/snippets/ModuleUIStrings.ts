38d5ddd7002c71d08ba655d90ec9d9fb692d3a5f
Simon Zünd <szuend@chromium.org>
2021-06-02 13:33:30 +0200

    [l10n] Remove superfluous ModuleUIString files
    Module strings have moved into their respective '-meta.ts' files and
    ModuleUIString files are no longer needed.
    
    R=tvanderlippe@chromium.org
    
    Bug: chromium:1163928
    Change-Id: I0cb4d3a2b195f7dd9e83ea7d507f6f020a3846a4
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2933136
    Auto-Submit: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>
    Commit-Queue: Tim van der Lippe <tvanderlippe@chromium.org>

--- front_end/panels/snippets/ModuleUIStrings.ts
+++
@@ -1,7 +0,0 @@
-// Copyright 2020 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-import * as i18n from '../../core/i18n/i18n.js';
-export const UIStrings = {};
-i18n.i18n.registerUIStrings('panels/snippets/ModuleUIStrings.ts', UIStrings);

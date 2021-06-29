ef6988a72955376451cbb664b9292d1ecb3d59dc
Kriti Sapra <kritisapra@google.com>
2021-06-29 08:29:53 +0000

    [CSS Migration]: Added ESLint rule to ensure wasShown methods contain call to super.wasShown()
    Bug: 1106746
    Change-Id: I7c1289f29e8b8c40e25ba37098d8c7d69e781d50
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2988310
    Commit-Queue: Kriti Sapra <kritisapra@google.com>
    Commit-Queue: Jack Franklin <jacktfranklin@chromium.org>
    Reviewed-by: Jack Franklin <jacktfranklin@chromium.org>

---
+++ scripts/eslint_rules/lib/check_was_shown_methods.js
@@ -0,0 +1,33 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+module.exports = {
+  meta: {
+    type: 'problem',
+
+    docs: {
+      description: 'Checks wasShown() method definitions call super.wasShown();',
+      category: 'Possible Errors',
+    },
+    fixable: 'code',
+    schema: []  // no options
+  },
+  create: function(context) {
+    return {
+      MethodDefinition(node) {
+        const nodeName = node.key.name;
+        if (node.parent.parent.superClass.property?.name === 'Widget' && nodeName === 'wasShown') {
+          const topBodyNode = node.value.body.body[0];
+          if (!(topBodyNode.type === 'ExpressionStatement' && topBodyNode.expression.type === 'CallExpression' &&
+                topBodyNode.expression.callee.object.type === 'Super' &&
+                topBodyNode.expression.callee.property.name === 'wasShown')) {
+            context.report({node, message: 'Please make sure the first call in wasShown is to super.wasShown().'});
+          }
+        }
+      }
+    };
+  }
+};

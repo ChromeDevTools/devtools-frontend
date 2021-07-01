43653d7bd5ac13c2fb0718e2b58539aa376661c1
Kriti Sapra <kritisapra@google.com>
2021-07-01 11:41:21 +0000

    [CSS Migration]: Migrated ReportView.ts
    Bug: 1106746
    Change-Id: I4dfab538ac78ca822ae3fc608e75fc98d29ec1f5
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/2999488
    Commit-Queue: Kriti Sapra <kritisapra@google.com>
    Reviewed-by: Jack Franklin <jacktfranklin@chromium.org>

--- front_end/ui/components/report_view/ReportView.ts
+++ front_end/ui/components/report_view/ReportView.ts
@@ -5,6 +5,12 @@
 import * as ComponentHelpers from '../../components/helpers/helpers.js';
 import * as LitHtml from '../../lit-html/lit-html.js';
 
+import reportStyles from './report.css.js';
+import reportKeyStyles from './reportKey.css.js';
+import reportSectionDividerStyles from './reportSectionDivider.css.js';
+import reportSectionHeaderStyles from './reportSectionHeader.css.js';
+import reportValueStyles from './reportValue.css.js';
+
 /**
  * The `Report` component can be used to display static information. A report
  * usually consists of multiple sections where each section has rows of name/value
@@ -39,39 +45,14 @@ export class Report extends HTMLElement {
   }
 
   connectedCallback(): void {
+    this.shadow.adoptedStyleSheets = [reportStyles];
     this.render();
   }
 
   private render(): void {
     // Disabled until https://crbug.com/1079231 is fixed.
     // clang-format off
-    // eslint-disable-next-line rulesdir/ban_style_tags_in_lit_html
     LitHtml.render(LitHtml.html`
-      <style>
-        :host {
-          display: block;
-        }
-
-        .content {
-          background-color: var(--color-background);
-          display: grid;
-          grid-template-columns: min-content 1fr;
-          user-select: text;
-        }
-
-        .report-title {
-          padding: 12px 24px;
-          font-size: 15px;
-          white-space: nowrap;
-          overflow: hidden;
-          text-overflow: ellipsis;
-          border-bottom: 1px solid var(--color-details-hairline);
-          color: var(--color-text-primary);
-          background-color: var(--color-background);
-          grid-column-start: span 2;
-        }
-      </style>
-
       <div class="content">
         ${this.reportTitle ? LitHtml.html`<div class="report-title">${this.reportTitle}</div>` : LitHtml.nothing}
         <slot></slot>
@@ -90,32 +71,14 @@ export class ReportSectionHeader extends HTMLElement {
 
   private readonly shadow = this.attachShadow({mode: 'open'});
   connectedCallback(): void {
+    this.shadow.adoptedStyleSheets = [reportSectionHeaderStyles];
     this.render();
   }
 
   private render(): void {
     // Disabled until https://crbug.com/1079231 is fixed.
     // clang-format off
-    // eslint-disable-next-line rulesdir/ban_style_tags_in_lit_html
     LitHtml.render(LitHtml.html`
-      <style>
-        :host {
-          grid-column-start: span 2;
-        }
-
-        .section-header {
-          padding: 12px;
-          margin-left: 18px;
-          display: flex;
-          flex-direction: row;
-          align-items: center;
-          flex: auto;
-          text-overflow: ellipsis;
-          overflow: hidden;
-          font-weight: bold;
-          color: var(--color-text-primary);
-        }
-      </style>
       <div class="section-header">
         <slot></slot>
       </div>
@@ -129,23 +92,14 @@ export class ReportSectionDivider extends HTMLElement {
 
   private readonly shadow = this.attachShadow({mode: 'open'});
   connectedCallback(): void {
+    this.shadow.adoptedStyleSheets = [reportSectionDividerStyles];
     this.render();
   }
 
   private render(): void {
     // Disabled until https://crbug.com/1079231 is fixed.
     // clang-format off
-    // eslint-disable-next-line rulesdir/ban_style_tags_in_lit_html
     LitHtml.render(LitHtml.html`
-      <style>
-        :host {
-          grid-column-start: span 2;
-        }
-
-        .section-divider {
-          border-bottom: 1px solid var(--color-details-hairline);
-        }
-      </style>
       <div class="section-divider">
       </div>
     `, this.shadow);
@@ -158,27 +112,14 @@ export class ReportKey extends HTMLElement {
 
   private readonly shadow = this.attachShadow({mode: 'open'});
   connectedCallback(): void {
+    this.shadow.adoptedStyleSheets = [reportKeyStyles];
     this.render();
   }
 
   private render(): void {
     // Disabled until https://crbug.com/1079231 is fixed.
     // clang-format off
-    // eslint-disable-next-line rulesdir/ban_style_tags_in_lit_html
     LitHtml.render(LitHtml.html`
-      <style>
-        :host {
-          line-height: 28px;
-          margin: 0 0 8px 0;
-        }
-
-        .key {
-          color: var(--color-text-secondary);
-          padding: 0 6px;
-          text-align: right;
-          white-space: pre;
-        }
-      </style>
       <div class="key"><slot></slot></div>
     `, this.shadow);
     // clang-format on
@@ -190,27 +131,14 @@ export class ReportValue extends HTMLElement {
 
   private readonly shadow = this.attachShadow({mode: 'open'});
   connectedCallback(): void {
+    this.shadow.adoptedStyleSheets = [reportValueStyles];
     this.render();
   }
 
   private render(): void {
     // Disabled until https://crbug.com/1079231 is fixed.
     // clang-format off
-    // eslint-disable-next-line rulesdir/ban_style_tags_in_lit_html
     LitHtml.render(LitHtml.html`
-      <style>
-        :host {
-          line-height: 28px;
-          margin: 0 0 8px 0;
-          min-width: 150px;
-        }
-
-        .value {
-          color: var(--color-text-primary);
-          margin-inline-start: 0;
-          padding: 0 6px;
-        }
-      </style>
       <div class="value"><slot></slot></div>
     `, this.shadow);
     // clang-format on

121af3bafb4a7c674f0d4395c48bc1239d123bf6
Andres Olivares <andoli@chromium.org>
2021-07-02 18:38:21 +0000

    Remove margin from Settings screen
    The settings screen has a margin that makes its content 3 pixels smaller
    than the DevTools UI. As a result a small gap is seen around its edges
    where the DevTools UI can be seen and interacted with. This means that
    users can accidentaly interact with other parts of DevTools which are
    visible when the Settings screen is open (for instance, closing DevTools
    when trying to close the settings screen because of the closeness of
    both close buttons). This CL removes such margin so that the settings
    screen completely overlays DevTools.
    
    Fixed: 1142747
    Change-Id: I26d6c41bcb4ba0d5207911c86fdf4d827b5c9713
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/3001656
    Auto-Submit: Andres Olivares <andoli@chromium.org>
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>
    Reviewed-by: Mraz Marian <mraz@chromium.org>
    Commit-Queue: Andres Olivares <andoli@chromium.org>

--- front_end/panels/settings/SettingsScreen.ts
+++ front_end/panels/settings/SettingsScreen.ts
@@ -161,7 +161,7 @@ export class SettingsScreen extends UI.Widget.VBox implements UI.View.ViewLocati
     dialog.setOutsideTabIndexBehavior(UI.Dialog.OutsideTabIndexBehavior.PreserveMainViewTabIndex);
     settingsScreen.show(dialog.contentElement);
     dialog.setEscapeKeyCallback(settingsScreen._onEscapeKeyPressed.bind(settingsScreen));
-
+    dialog.setMarginBehavior(UI.GlassPane.MarginBehavior.NoMargin);
     // UI.Dialog extends GlassPane and overrides the `show` method with a wider
     // accepted type. However, TypeScript uses the supertype declaration to
     // determine the full type, which requires a `!Document`.

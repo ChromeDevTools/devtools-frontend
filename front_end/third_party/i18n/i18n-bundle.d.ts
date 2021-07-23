904d28740091bfd7a2e1a8f746e467c1f2237ffc
Simon Zünd <szuend@chromium.org>
2021-07-23 07:24:05 +0200

    [i18n] Remove unneeded bits from the LH bundle
    This CL removes all the pieces of the Lighthouse i18n library that
    are no longer needed after being moved to TypeScript.
    
    R=tvanderlippe@chromium.org
    
    Bug: 1231873
    Change-Id: Ie6c6446832a13c5a4b8fc464c73c200935581cf4
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/3046363
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>

--- front_end/third_party/i18n/i18n-bundle.d.ts
+++ front_end/third_party/i18n/i18n-bundle.d.ts
@@ -13,27 +13,6 @@
  */
 declare function getAllSupportedLocales(): string[];
  /**
-  * Function to retrieve all 'argumentElement's from an ICU message. An argumentElement
-  * is an ICU element with an argument in it, like '{varName}' or '{varName, number, bytes}'. This
-  * differs from 'messageElement's which are just arbitrary text in a message.
-  *
-  * Notes:
-  *  This function will recursively inspect plural elements for nested argumentElements.
-  *
-  *  We need to find all the elements from the plural format sections, but
-  *  they need to be deduplicated. I.e. "=1{hello {icu}} =other{hello {icu}}"
-  *  the variable "icu" would appear twice if it wasn't de duplicated. And they cannot
-  *  be stored in a set because they are not equal since their locations are different,
-  *  thus they are stored via a Map keyed on the "id" which is the ICU varName.
-  *
-  * @param {ArrayLike<Object>} icuElements
-  * @param {Map<string, T>} [seenElementsById]
-  * @return {Map<string, T>}
-  */
- declare function collectAllCustomElementsFromICU<T>(icuElements: ArrayLike<Object>, seenElementsById?: Map<string, T>): T;
- /** @param {ArrayLike<string>} pathInLHR */
- declare function _formatPathAsString(pathInLHR: string[]): string;
-/**
  * Returns a copy of the `values` object, with the values formatted based on how
  * they will be used in their icuMessage, e.g. KB or milliseconds. The original
  * object is unchanged.
@@ -45,43 +24,6 @@ declare function getAllSupportedLocales(): string[];
  */
 declare function _preformatValues(icuMessageId: string, messageFormatter: Object, values: Object, lhlMessage: string): Record<string, string|number|boolean|null|undefined>;
  /**
-  * @param {string} locale
-  */
- declare function getRendererFormattedStrings(locale: string): {};
-/**
- * Returns a function that generates `LH.IcuMessage` objects to localize the
- * messages in `fileStrings` and the shared `i18n.UIStrings`.
- * @param {string} filename
- * @param {Record<string, string>} fileStrings
- */
- declare function createIcuMessageFn(filename: string, fileStrings: Object): typeof getMessageInstanceIdFn;
- /**
-  * Returns true if string is an ICUMessage reference.
-  * @param {string} icuMessageIdOrRawString
-  */
- declare function isIcuMessage(icuMessageIdOrRawString: string): boolean;
- /**
-  * @param {string} icuMessageIdOrRawString
-  * @param {string} locale
-  */
- declare function getFormatted(icuMessageIdOrRawString: string, locale: string): string;
-/**
- * @param {string} icuMessageIdOrRawString
- * @param {string} locale
- */
- declare function getFormatter(icuMessageIdOrRawString: string, locale: string): any;
-/**
- * Recursively walk the input object, looking for property values that are
- * `LH.IcuMessage`s and replace them with their localized values. Primarily
- * used with the full LHR or a Config as input.
- * Returns a map of locations that were replaced to the `IcuMessage` that was at
- * that location.
- * @param {unknown} inputObject
- * @param {LH.Locale} locale
- * @return {LH.IcuMessagePaths}
- */
- declare function replaceIcuMessages(inputObject: any, locale: string): {};
- /**
   * Populate the i18n string lookup dict with locale data
   * Used when the host environment selects the locale and serves lighthouse the intended locale file
   * @see https://docs.google.com/document/d/1jnt3BqKB-4q3AE94UWFA0Gqspx8Sd_jivlB7gQMlmfk/edit
@@ -89,33 +31,13 @@ declare function _preformatValues(icuMessageId: string, messageFormatter: Object
   * @param {*} lhlMessages
   */
  declare function registerLocaleData(locale: string, lhlMessages: any): void;
- /**
-  * @param {string} icuMessage
-  * @param {Object} [values]
-  */
- declare function getMessageInstanceIdFn(icuMessage: string, values: Object | null): string;
-
- /**
- * Throws an error with the given icuMessage id.
- * @param {string} icuMessage
- */
-declare function idNotInMainDictionaryException(icuMessage: string): void;
 
 import type {IntlMessageFormat} from '../intl-messageformat/package/lib/index.js';
 
  declare var i18n: {
-   _formatPathAsString: typeof _formatPathAsString;
    _preformatValues: typeof _preformatValues;
    lookupLocale: typeof lookupLocale;
-   getRendererFormattedStrings: typeof getRendererFormattedStrings;
-   createIcuMessageFn: typeof createIcuMessageFn;
-   getFormatted: typeof getFormatted;
-   getFormatter: typeof getFormatter;
-   replaceIcuMessages: typeof replaceIcuMessages;
-   isIcuMessage: typeof isIcuMessage;
-   collectAllCustomElementsFromICU: typeof collectAllCustomElementsFromICU;
    registerLocaleData: typeof registerLocaleData;
-   idNotInMainDictionaryException: typeof idNotInMainDictionaryException;
    getAllSupportedLocales: typeof getAllSupportedLocales;
    MessageFormat: typeof IntlMessageFormat,
  };

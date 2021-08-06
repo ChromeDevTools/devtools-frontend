a23b6cd9aff046d281e31ef36f6e1ccab84b7ea1
Jan Scheffler <janscheffler@chromium.org>
2021-08-06 16:23:52 +0200

    [Underscore Migration] Migrate models/heap_snapshot_model
    This patch is part of the underscore migration that adds the private
    keyword to all properties that start with an underscore and removes the
    underscore.
    
    Bug:chromium:1207342
    Change-Id: I80712f0508678a6ed3a23920e2e1c28e5c2a083c
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/3077895
    Commit-Queue: Jan Scheffler <janscheffler@chromium.org>
    Commit-Queue: Tim van der Lippe <tvanderlippe@chromium.org>
    Auto-Submit: Jan Scheffler <janscheffler@chromium.org>
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>

--- front_end/models/heap_snapshot_model/HeapSnapshotModel.ts
+++ front_end/models/heap_snapshot_model/HeapSnapshotModel.ts
@@ -28,8 +28,6 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-/* eslint-disable rulesdir/no_underscored_properties */
-
 export const HeapSnapshotProgressEvent = {
   Update: 'ProgressUpdate',
   BrokenSnapshot: 'BrokenSnapshot',

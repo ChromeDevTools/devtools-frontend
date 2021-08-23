fae999f5a75cedd4e4bb8d71b87b44d4f5a78b16
Jack Franklin <jacktfranklin@chromium.org>
2021-08-23 11:58:27 +0100

    Extract context-menu e2e helpers
    I need to test the context menu in future e2e tests, so this CL pulls
    the helpers I created out of the data-grid tests and into a shared
    module.
    
    Bug: none
    Change-Id: Ifcb87863998301527f3e4f71ff38ea381541057d
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/3110372
    Commit-Queue: Jack Franklin <jacktfranklin@chromium.org>
    Commit-Queue: Tim van der Lippe <tvanderlippe@chromium.org>
    Auto-Submit: Jack Franklin <jacktfranklin@chromium.org>
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>

---
+++ test/e2e/helpers/context-menu-helpers.ts
@@ -0,0 +1,64 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+import {assert} from 'chai';
+import type {puppeteer} from '../../shared/helper.js';
+import {$, $$, $textContent, platform, waitForFunction} from '../../shared/helper.js';
+
+export function platformSpecificTextForSubMenuEntryItem(text: string): string {
+  /**
+   * On Mac the context menu adds the ▶ icon to the sub menu entry points in the
+   * context menu, but on Linux/Windows it uses an image. So if we're running on
+   * Mac, we append the search text with the icon, else we do not.
+   */
+  return platform === 'mac' ? `${text}▶` : text;
+}
+
+export async function assertTopLevelContextMenuItemsText(expectedOptions: string[]): Promise<void> {
+  const contextMenu = await $('.soft-context-menu');
+  if (!contextMenu) {
+    assert.fail('Could not find context menu.');
+  }
+
+  const allItems = await $$('.soft-context-menu > .soft-context-menu-item');
+  const allItemsText = await Promise.all(allItems.map(item => item.evaluate(div => div.textContent)));
+
+  assert.deepEqual(allItemsText, expectedOptions);
+}
+export async function findSubMenuEntryItem(text: string): Promise<puppeteer.ElementHandle<Element>> {
+  const textToSearchFor = platformSpecificTextForSubMenuEntryItem(text);
+  const matchingElement = await $textContent(textToSearchFor);
+
+  if (!matchingElement) {
+    const allItems = await $$('.soft-context-menu > .soft-context-menu-item');
+    const allItemsText = await Promise.all(allItems.map(item => item.evaluate(div => div.textContent)));
+    assert.fail(`Could not find "${text}" option on context menu. Found items: ${allItemsText.join(' | ')}`);
+  }
+  return matchingElement;
+}
+
+export async function assertSubMenuItemsText(subMenuText: string, expectedOptions: string[]): Promise<void> {
+  const subMenuEntryItem = await findSubMenuEntryItem(subMenuText);
+  if (!subMenuEntryItem) {
+    const allItems = await $$('.soft-context-menu > .soft-context-menu-item');
+    const allItemsText = await Promise.all(allItems.map(item => item.evaluate(div => div.textContent)));
+    assert.fail(`Could not find "${subMenuText}" option on context menu. Found items: ${allItemsText.join(' | ')}`);
+  }
+
+  await subMenuEntryItem.hover();
+  await waitForFunction(async () => {
+    const menus = await $$('.soft-context-menu');
+    // Wait for the main menu + the sub menu to be in the DOM
+    return menus.length === 2;
+  });
+  const allMenus = await $$('.soft-context-menu');
+  // Each submenu is rendered as a separate context menu and is appended to
+  // the DOM after the main context menu, hence the array index.
+  const subMenuElement = allMenus[1];
+  if (!subMenuElement) {
+    assert.fail(`Could not find sub menu for ${subMenuText}`);
+  }
+  const subMenuItems = await $$('.soft-context-menu-item', subMenuElement);
+  const subMenuItemsText = await Promise.all(subMenuItems.map(item => item.evaluate(div => div.textContent)));
+  assert.deepEqual(subMenuItemsText, expectedOptions);
+}
--- test/interactions/data_grid_controller/data_grid_controller_test.ts
+++ test/interactions/data_grid_controller/data_grid_controller_test.ts
@@ -3,60 +3,12 @@
 // found in the LICENSE file.
 
 import {assert} from 'chai';
-import type * as puppeteer from 'puppeteer';
+
+import {assertSubMenuItemsText, assertTopLevelContextMenuItemsText, findSubMenuEntryItem, platformSpecificTextForSubMenuEntryItem} from '../../e2e/helpers/context-menu-helpers.js';
 import {getDataGrid, getDataGridController, getInnerTextOfDataGridCells} from '../../e2e/helpers/datagrid-helpers.js';
 import {$, $$, $textContent, click, waitFor, waitForFunction} from '../../shared/helper.js';
 import {describe, it} from '../../shared/mocha-extensions.js';
 import {loadComponentDocExample, preloadForCodeCoverage} from '../helpers/shared.js';
-import {platform} from '../../shared/helper.js';
-
-function platformSpecificTextForSubMenuEntryItem(text: string): string {
-  /**
-   * On Mac the context menu adds the ▶ icon to the sub menu entry points in the
-   * context menu, but on Linux/Windows it uses an image. So if we're running on
-   * Mac, we append the search text with the icon, else we do not.
-   */
-  return platform === 'mac' ? `${text}▶` : text;
-}
-
-async function assertTopLevelContextMenuItemsText(expectedOptions: string[]): Promise<void> {
-  const contextMenu = await $('.soft-context-menu');
-  if (!contextMenu) {
-    assert.fail('Could not find context menu.');
-  }
-
-  const allItems = await $$('.soft-context-menu > .soft-context-menu-item');
-  const allItemsText = await Promise.all(allItems.map(item => item.evaluate(div => div.textContent)));
-
-  assert.deepEqual(allItemsText, expectedOptions);
-}
-
-async function assertSubMenuItemsText(subMenuText: string, expectedOptions: string[]): Promise<void> {
-  const subMenuEntryItem = await findSubMenuEntryItem(subMenuText);
-  if (!subMenuEntryItem) {
-    const allItems = await $$('.soft-context-menu > .soft-context-menu-item');
-    const allItemsText = await Promise.all(allItems.map(item => item.evaluate(div => div.textContent)));
-    assert.fail(`Could not find "${subMenuText}" option on context menu. Found items: ${allItemsText.join(' | ')}`);
-    return;
-  }
-
-  await subMenuEntryItem.hover();
-  await waitForFunction(async () => {
-    const menus = await $$('.soft-context-menu');
-    // Wait for the main menu + the sub menu to be in the DOM
-    return menus.length === 2;
-  });
-  const allMenus = await $$('.soft-context-menu');
-  // Each submenu is rendered as a separate context menu and is appended to
-  // the DOM after the main context menu, hence the array index.
-  const subMenuElement = allMenus[1];
-  if (!subMenuElement) {
-    assert.fail(`Could not find sub menu for ${subMenuText}`);
-  }
-  const subMenuItems = await $$('.soft-context-menu-item', subMenuElement);
-  const subMenuItemsText = await Promise.all(subMenuItems.map(item => item.evaluate(div => div.textContent)));
-  assert.deepEqual(subMenuItemsText, expectedOptions);
-}
 
 async function activateContextMenuOnColumnHeader(headerText: string) {
   const dataGridController = await getDataGridController();
@@ -73,17 +25,6 @@ async function activateContextMenuOnColumnHeader(headerText: string) {
   return headerCell;
 }
 
-async function findSubMenuEntryItem(text: string): Promise<puppeteer.ElementHandle<Element>> {
-  const textToSearchFor = platformSpecificTextForSubMenuEntryItem(text);
-  const matchingElement = await $textContent(textToSearchFor);
-
-  if (!matchingElement) {
-    const allItems = await $$('.soft-context-menu > .soft-context-menu-item');
-    const allItemsText = await Promise.all(allItems.map(item => item.evaluate(div => div.textContent)));
-    assert.fail(`Could not find "${text}" option on context menu. Found items: ${allItemsText.join(' | ')}`);
-  }
-  return matchingElement;
-}
 async function activateContextMenuOnBodyCell(cellText: string) {
   const dataGridController = await getDataGridController();
   const dataGrid = await getDataGrid(dataGridController);

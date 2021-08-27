15825adf9491d455e723161dbb65958ae21b5c83
Kateryna Prokopenko <kprokopenko@chromium.org>
2021-08-27 08:36:42 +0000

    Use type-safe events for NetworkPersistenceManager
    Bug: 1228674
    Change-Id: Id4329e1368e207ec62ae3026e058ebb2d1fd3b10
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/3122339
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Kateryna Prokopenko <kprokopenko@google.com>

--- front_end/models/persistence/NetworkPersistenceManager.ts
+++ front_end/models/persistence/NetworkPersistenceManager.ts
@@ -14,7 +14,7 @@ import {PersistenceBinding, PersistenceImpl} from './PersistenceImpl.js';
 
 let networkPersistenceManagerInstance: NetworkPersistenceManager|null;
 
-export class NetworkPersistenceManager extends Common.ObjectWrapper.ObjectWrapper {
+export class NetworkPersistenceManager extends Common.ObjectWrapper.ObjectWrapper<EventTypes> {
   private bindings: WeakMap<Workspace.UISourceCode.UISourceCode, PersistenceBinding>;
   private readonly originalResponseContentPromises: WeakMap<Workspace.UISourceCode.UISourceCode, Promise<string|null>>;
   private savingForOverrides: WeakSet<Workspace.UISourceCode.UISourceCode>;
@@ -499,6 +499,12 @@ const RESERVED_FILENAMES = new Set<string>([
   'com8', 'com9', 'lpt1', 'lpt2', 'lpt3', 'lpt4', 'lpt5', 'lpt6', 'lpt7', 'lpt8', 'lpt9',
 ]);
 
-export const Events = {
-  ProjectChanged: Symbol('ProjectChanged'),
+// TODO(crbug.com/1167717): Make this a const enum again
+// eslint-disable-next-line rulesdir/const_enum
+export enum Events {
+  ProjectChanged = 'ProjectChanged',
+}
+
+export type EventTypes = {
+  [Events.ProjectChanged]: Workspace.Workspace.Project|null,
 };

283076007d77818e0b62ade4745040c341d238e5
Simon Zünd <szuend@chromium.org>
2021-09-14 12:20:17 +0200

    Add mixin for event handling
    This CL adds a mixin based on ObjectWrapper. Its intended use is for
    deep inheritance hierarchies where different subclasses want to
    offer different sets of Events. The only use case in DevTools is
    the legacy Widget system.
    
    As the main use case for event handling remains to inherit from
    ObjectWrapper, we decided to implement the mixin in terms of
    ObjectWrapper and not the other way around. This means the
    mixin is very slightly more expensive to use, due to the indirection
    via a private field.
    
    The private field on the mixin is necessary as TypeScript doesn't
    allow private/protected properties on mixin classes, making this
    a perfect candidate for using private fields, as #events should
    never be accessed directly.
    
    Bug: 1228674
    Change-Id: I0d8295ae8c8a2ac7978b4ef3b3fae31d023bb8c4
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/3159999
    Reviewed-by: Jack Franklin <jacktfranklin@chromium.org>
    Reviewed-by: Philip Pfaffe <pfaffe@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>

--- front_end/core/common/Object.ts
+++ front_end/core/common/Object.ts
@@ -30,7 +30,7 @@
 import type * as Platform from '../platform/platform.js';
 import type {EventDescriptor, EventTarget, EventTargetEvent, EventType, EventPayload, EventPayloadToRestParameters} from './EventTarget.js';
 
-interface ListenerCallbackTuple {
+export interface ListenerCallbackTuple {
   thisObject?: Object;
   listener: (arg0: EventTargetEvent) => void;
   disposed?: boolean;
@@ -105,4 +105,39 @@ export class ObjectWrapper<Events = any> implements EventTarget<Events> {
       }
     }
   }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type Constructor = new (...args: any[]) => {};
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export function eventMixin<Events, Base extends Constructor>(base: Base) {
+  return class EventHandling extends base implements EventTarget<Events> {
+    #events = new ObjectWrapper<Events>();
+
+    addEventListener<T extends EventType<Events>>(
+        eventType: T, listener: (arg0: EventTargetEvent<EventPayload<Events, T>>) => void,
+        thisObject?: Object): EventDescriptor<Events, T> {
+      return this.#events.addEventListener(eventType, listener, thisObject);
+    }
+
+    once<T extends EventType<Events>>(eventType: T): Promise<EventPayload<Events, T>> {
+      return this.#events.once(eventType);
+    }
+
+    removeEventListener<T extends EventType<Events>>(
+        eventType: T, listener: (arg0: EventTargetEvent<EventPayload<Events, T>>) => void, thisObject?: Object): void {
+      this.#events.removeEventListener(eventType, listener, thisObject);
+    }
+
+    hasEventListeners(eventType: EventType<Events>): boolean {
+      return this.#events.hasEventListeners(eventType);
+    }
+
+    dispatchEventToListeners<T extends EventType<Events>>(
+        eventType: Platform.TypeScriptUtilities.NoUnion<T>,
+        ...eventData: EventPayloadToRestParameters<EventPayload<Events, T>>): void {
+      this.#events.dispatchEventToListeners(eventType, ...eventData);
+    }
+  };
 }

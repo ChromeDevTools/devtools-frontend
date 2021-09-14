225aef1530e3d9b249684c794340779504e4cccb
Philip Pfaffe <pfaffe@chromium.org>
2021-09-14 11:22:30 +0200

    Add a test for searching in an ExtensionPanel
    Bug: 1247123
    Change-Id: I5eb29816ef7a529c5e72c7df0da226ae04d3f845
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/3151715
    Commit-Queue: Philip Pfaffe <pfaffe@chromium.org>
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>

---
+++ test/e2e/extensions/can-search-in-extension-panel_test.ts
@@ -0,0 +1,47 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {assert} from 'chai';
+import type {Chrome} from '../../../extension-api/ExtensionAPI.js';
+import {loadExtension} from '../helpers/extension-helpers.js';
+import {describe, it} from '../../shared/mocha-extensions.js';
+import {click, getBrowserAndPages, getResourcesPath, waitFor, waitForFunction} from '../../shared/helper.js';
+
+declare global {
+  interface Window {
+    searchEvents: {action: string, queryString?: string}[];
+  }
+}
+
+const PAGE_TITLE = 'TestPanel';
+describe('Extension panels', async () => {
+  it('can perform search actions', async () => {
+    const extension = await loadExtension('TestExtension');
+
+    const page = new URL(`${getResourcesPath()}/extensions/test_panel.html`).pathname;
+
+    await extension.evaluate(async (title: string, page: string) => {
+      window.searchEvents = [];
+      const panel = await new Promise<Chrome.DevTools.ExtensionPanel>(
+          r => window.chrome.devtools.panels.create('TestPanel', '', page, r));
+      panel.onSearch.addListener((action, queryString) => window.searchEvents.push({action, queryString}));
+    }, PAGE_TITLE, page);
+
+    await click(`[aria-label=${PAGE_TITLE}]`);
+
+    const {frontend} = getBrowserAndPages();
+    await frontend.keyboard.down('Control');
+    await frontend.keyboard.press('F');
+    await frontend.keyboard.up('Control');
+
+    const search = await waitFor('#search-input-field');
+    await search.type('abc');
+
+    const queries = await waitForFunction(() => {
+      return extension.evaluate(() => window.searchEvents.length > 0 ? window.searchEvents : undefined);
+    });
+
+    assert.deepEqual(queries, [{action: 'performSearch', queryString: 'abc'}]);
+  });
+});

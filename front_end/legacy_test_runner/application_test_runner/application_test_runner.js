53e75c363b8685bd4c7028faddcd26f99b9b1dd9
Alex Rudenko <alexrudenko@chromium.org>
2021-09-24 05:14:38 +0000

    Remove AppCache test runner from DevTools
    AppCache is being removed from Chromium in
    crbug.com/582750. We no longer need the test runner
    which also seems to be unused.
    
    Bug: chromium:1084190
    Change-Id: If2db405a9155129d317b5221c221798433a580d2
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/3179141
    Reviewed-by: Mathias Bynens <mathias@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>

--- front_end/legacy_test_runner/application_test_runner/application_test_runner.js
+++ front_end/legacy_test_runner/application_test_runner/application_test_runner.js
@@ -8,7 +8,6 @@ import '../console_test_runner/console_test_runner.js';
 import '../../panels/sources/sources-legacy.js';
 import '../sources_test_runner/sources_test_runner.js';
 
-import './AppcacheTestRunner.js';
 import './CacheStorageTestRunner.js';
 import './IndexedDBTestRunner.js';
 import './ResourceTreeTestRunner.js';

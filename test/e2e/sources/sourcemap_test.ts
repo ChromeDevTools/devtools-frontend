59e9814a6616702d46a41bbdba57f211faba392e
Victor Porof <victorporof@chromium.org>
2021-11-22 15:01:28 +0000

    Disable source map test for being flaky
    Signed-off-by: Victor Porof <victorporof@chromium.org>
    Change-Id: Iaec203260298d06e68ada360019af5adae87a90a
    Bug: 1272490
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/3295535
    Reviewed-by: Tim van der Lippe <tvanderlippe@chromium.org>

--- test/e2e/sources/sourcemap_test.ts
+++ test/e2e/sources/sourcemap_test.ts
@@ -10,7 +10,8 @@ import {clickNthChildOfSelectedElementNode, focusElementsTree, waitForContentOfS
 import {addBreakpointForLine, getBreakpointDecorators, openSourceCodeEditorForFile, removeBreakpointForLine, RESUME_BUTTON, retrieveTopCallFrameScriptLocation, retrieveTopCallFrameWithoutResuming, STEP_OVER_BUTTON} from '../helpers/sources-helpers.js';
 
 describe('The Sources Tab', async () => {
-  it('sets multiple breakpoints in case of code-splitting', async () => {
+  // Flaky test.
+  it.skip('[crbug.com/1272490] sets multiple breakpoints in case of code-splitting', async () => {
     const {target, frontend} = getBrowserAndPages();
     await openSourceCodeEditorForFile('sourcemap-codesplit.ts', 'sourcemap-codesplit.html');
     await addBreakpointForLine(frontend, 3);

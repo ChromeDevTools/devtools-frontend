73b8c4f35c670312d8f0a0d5a35e52e81a9e0760
Tim van der Lippe <tvanderlippe@chromium.org>
2021-12-01 16:06:53 +0000

    Remove entrypoints/devtools_app/devtools_app.json
    R=jacktfranklin@chromium.org
    
    Bug: 1127902
    Change-Id: I0d143909fa7bc40368e223b17b3825b97b6ab772
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/3310865
    Auto-Submit: Tim Van der Lippe <tvanderlippe@chromium.org>
    Commit-Queue: Jack Franklin <jacktfranklin@chromium.org>
    Reviewed-by: Jack Franklin <jacktfranklin@chromium.org>

--- front_end/entrypoints/devtools_app/devtools_app.ts
+++ front_end/entrypoints/devtools_app/devtools_app.ts
@@ -1,7 +1,7 @@
-// Copyright 2021 The Chromium Authors. All rights reserved.
+// Copyright 2018 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
+import '../shell/shell.js';
 import '../../panels/css_overview/css_overview-meta.js';
 import '../../panels/elements/elements-meta.js';
 import '../../panels/browser_debugger/browser_debugger-meta.js';
@@ -25,3 +25,12 @@ import '../../panels/timeline/timeline-meta.js';
 import '../../panels/web_audio/web_audio-meta.js';
 import '../../panels/webauthn/webauthn-meta.js';
 import '../../panels/layer_viewer/layer_viewer-meta.js';
+
+import * as Root from '../../core/root/root.js';
+import * as Main from '../main/main.js';
+
+const runtimeInstance = Root.Runtime.Runtime.instance({forceNew: true, moduleDescriptors: []});
+// @ts-ignore Exposed for legacy layout tests
+self.runtime = runtimeInstance;
+new Main.MainImpl.MainImpl();
+Root.Runtime.appStartedPromiseCallback();

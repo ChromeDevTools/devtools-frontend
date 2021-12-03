2a84998f49d62a6997f62d5c37e454b9b3abc56f
Jack Franklin <jacktfranklin@chromium.org>
2021-12-03 11:08:30 +0000

    Lint for only: true in ESLint tests
    These are just like `it.only()` and we want to ensure they don't make it
    to CQ and cause us to not run all the ESLint tests.
    
    Bug: none
    Change-Id: I96c1ba2838c16c9e15ae204a39bef68095e6848a
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/3314756
    Commit-Queue: Jack Franklin <jacktfranklin@chromium.org>
    Commit-Queue: Tim Van der Lippe <tvanderlippe@chromium.org>
    Auto-Submit: Jack Franklin <jacktfranklin@chromium.org>
    Reviewed-by: Tim Van der Lippe <tvanderlippe@chromium.org>

---
+++ scripts/eslint_rules/lib/no_only_eslint_tests.js
@@ -0,0 +1,53 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+module.exports = {
+  meta: {
+    type: 'problem',
+
+    docs: {
+      description: 'Usage of only: true in ESLint tests',
+      category: 'Possible Errors',
+    },
+    fixable: 'code',
+    messages: {noOnlyInESLintTest: 'You cannot use only: true in an ESLint test.'},
+    schema: []  // no options
+  },
+  create: function(context) {
+    function checkForOnlyInTestCases(testCaseObjects) {
+      for (const testCase of testCaseObjects) {
+        const onlyKeyProp = testCase.properties.find(prop => {
+          return prop.key.name === 'only';
+        });
+        if (onlyKeyProp) {
+          context.report({
+            node: onlyKeyProp,
+            messageId: 'noOnlyInESLintTest'
+
+          });
+        }
+      }
+    }
+
+    return {
+      // match ruleTester.run('foo', rule, {...})
+      'CallExpression[callee.object.name=\'ruleTester\'][callee.property.name=\'run\']'(node) {
+        // first argument = string name
+        // second argument = rule itself
+        // third argument = the object containing the test cases - what we want!
+        const tests = node.arguments[2];
+        if (!tests) {
+          return;
+        }
+
+        for (const testProperty of tests.properties) {
+          // Iterate over the "valid" and "invalid" rules
+          // Here .value = the array of test cases, and .elements gets us each
+          // object (individual test case) within that array.
+          checkForOnlyInTestCases(testProperty.value.elements);
+        }
+      }
+    };
+  }
+};
---
+++ scripts/eslint_rules/tests/no_only_eslint_tests_test.js
@@ -0,0 +1,100 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+const rule = require('../lib/no_only_eslint_tests.js');
+const ruleTester = new (require('eslint').RuleTester)({
+  parserOptions: {ecmaVersion: 9, sourceType: 'module'},
+});
+
+ruleTester.run('no_only_eslint_tests', rule, {
+  valid: [
+    {
+      only: true,
+      code: `ruleTester.run('my_eslint_rule', rule, {
+        valid: [{
+          code: 'foo',
+          filename: 'foo.ts',
+        }]
+      })`,
+      filename: 'scripts/eslint_rules/tests/foo_test.js',
+    },
+    {
+      only: true,
+      code: `ruleTester.run('my_eslint_rule', rule, {
+        invalid: [{
+          code: 'foo',
+          filename: 'foo.ts',
+        }]
+      })`,
+      filename: 'scripts/eslint_rules/tests/foo_test.js',
+    },
+  ],
+
+  invalid: [
+    {
+      only: true,
+      code: `ruleTester.run('my_eslint_rule', rule, {
+        valid: [{
+          only: true,
+          code: 'foo',
+          filename: 'foo.ts',
+        }]
+      })`,
+      filename: 'scripts/eslint_rules/tests/foo_test.js',
+      errors: [{messageId: 'noOnlyInESLintTest'}],
+    },
+    {
+      only: true,
+      code: `ruleTester.run('my_eslint_rule', rule, {
+        invalid: [{
+          only: true,
+          code: 'foo',
+          filename: 'foo.ts',
+          errors: [{ messagId: 'foo' }]
+        }]
+      })`,
+      filename: 'scripts/eslint_rules/tests/foo_test.js',
+      errors: [{messageId: 'noOnlyInESLintTest'}],
+    },
+    {
+      only: true,
+      code: `ruleTester.run('my_eslint_rule', rule, {
+        valid: [{
+          only: true,
+          code: 'foo',
+          filename: 'foo.ts',
+        }],
+        invalid: [{
+          only: true,
+          code: 'foo',
+          filename: 'foo.ts',
+          errors: [{ messagId: 'foo' }]
+        }]
+      })`,
+      filename: 'scripts/eslint_rules/tests/foo_test.js',
+      errors: [{messageId: 'noOnlyInESLintTest'}, {messageId: 'noOnlyInESLintTest'}],
+    },
+    {
+      only: true,
+      code: `ruleTester.run('my_eslint_rule', rule, {
+        valid: [{
+          only: true,
+          code: 'foo',
+          filename: 'foo.ts',
+        }, {
+          code: 'foo',
+          filename: 'foo.ts',
+        }],
+        invalid: [{
+          only: true,
+          code: 'foo',
+          filename: 'foo.ts',
+          errors: [{ messagId: 'foo' }]
+        }]
+      })`,
+      filename: 'scripts/eslint_rules/tests/foo_test.js',
+      errors: [{messageId: 'noOnlyInESLintTest'}, {messageId: 'noOnlyInESLintTest'}],
+    },
+  ]
+});

c1991d2a40722e0f9cba8cb903feac88c40a95fa
Jack Franklin <jacktfranklin@chromium.org>
2021-12-06 14:05:36 +0000

    Extract isLitHtmlTemplateCall to ESLint utils
    Will update usages to use this util in a follow-up CL, and see if there
    are more duplicated utils that deserve to be extracted.
    
    Bug: 1276968
    Change-Id: I2470da9c1f1c6fd0bf1d02a3958eabadec91d3e0
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/3317352
    Reviewed-by: Tim Van der Lippe <tvanderlippe@chromium.org>
    Commit-Queue: Jack Franklin <jacktfranklin@chromium.org>

---
+++ scripts/eslint_rules/lib/utils.js
@@ -0,0 +1,26 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// This file contains utility functions that are commonly needed in ESLint
+// rules. It does not contain any ESLint rules itself.
+
+/**
+ * @param taggedTemplateExpressionNode - a TaggedTemplateExpression node from the AST of the parsed code.
+ * @returns {boolean} - `true` if the code matches LitHtml.html`` or html``, and false otherwise.
+*/
+function isLitHtmlTemplateCall(taggedTemplateExpressionNode) {
+  // Match LitHtml.html``
+  const {tag} = taggedTemplateExpressionNode;
+  if (!tag) {
+    return false;
+  }
+  // Match LitHtml.html``
+  const isLitHtmlDotHtmlCall = tag.object?.name === 'LitHtml' && tag.property?.name === 'html';
+  // Match html`` (and guess that it's Lit)
+  const isDestructuredHtmlCall = tag.type === 'Identifier' && tag.name === 'html';
+
+  return isLitHtmlDotHtmlCall || isDestructuredHtmlCall;
+}
+
+module.exports = {isLitHtmlTemplateCall};
---
+++ scripts/eslint_rules/tests/utils_test.js
@@ -0,0 +1,40 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+const parser = require('@typescript-eslint/parser');
+
+const utils = require('../lib/utils.js');
+const {assert} = require('chai');
+
+describe.only('eslint utils', () => {
+  describe('isLitHtmlTemplateCall', () => {
+    it('returns true if the code is LitHtml.html``', () => {
+      const code = 'LitHtml.html`<span>foo</span>`';
+      const parsed = parser.parse(code);
+      const result = utils.isLitHtmlTemplateCall(parsed.body[0].expression);
+      assert.strictEqual(result, true);
+    });
+
+    it('returns true if the code is html``', () => {
+      const code = 'html`<span>foo</span>`';
+      const parsed = parser.parse(code);
+      const result = utils.isLitHtmlTemplateCall(parsed.body[0].expression);
+      assert.strictEqual(result, true);
+    });
+
+    it('returns false if the code is LitHtml.somethingElse``', () => {
+      const code = 'LitHtml.somethingElse`<span>foo</span>`';
+      const parsed = parser.parse(code);
+      const result = utils.isLitHtmlTemplateCall(parsed.body[0].expression);
+      assert.strictEqual(result, false);
+    });
+
+    it('returns false if the code is another tagged template function``', () => {
+      const code = 'notLitHtml`<span>foo</span>`';
+      const parsed = parser.parse(code);
+      const result = utils.isLitHtmlTemplateCall(parsed.body[0].expression);
+      assert.strictEqual(result, false);
+    });
+  });
+});

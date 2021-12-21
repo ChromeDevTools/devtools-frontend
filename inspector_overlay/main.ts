dc48499246acb27c90ce034619b335726ea2834c
Alex Rudenko <alexrudenko@chromium.org>
2021-12-21 15:49:41 +0000

    Revert "Reland "Batch rendering commands and skip rendering if commands are same""
    This reverts commit 8e6a222bfffc85efe7f7659a52c6adfd88c0e287.
    
    Reason for revert: breaks the overlay (crbug/1281801)
    
    Original change's description:
    > Reland "Batch rendering commands and skip rendering if commands are same"
    >
    > This is a reland of 2288c1657cdb57590a3a6c8c50d7aece4eaaa531 + a fix
    > for the issue when hiding and showing the same overlay lead to the
    > overlay not being painted.
    >
    > Note: on my CPU this optimization reduces cpu usage from ~60% to ~30%
    > when inspecting a single element that does not change.
    >
    > Original change's description:
    > > Batch rendering commands and skip rendering if commands are same
    > >
    > > Bug: 1062574
    > > Change-Id: I2bd2d02055f36a318ecd56194817fb531eb614b9
    > > Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/3301392
    > > Reviewed-by: Simon Zünd <szuend@chromium.org>
    > > Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    >
    > Bug: 1062574
    > Change-Id: I82e6ed03cd7c44f0e8ac7fd78f3623bed8e23187
    > Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/3308808
    > Reviewed-by: Simon Zünd <szuend@chromium.org>
    > Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    
    Bug: 1062574
    Change-Id: I88848f84d5b7d588877e513a02d7cc23a3a5a598
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/3352204
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Auto-Submit: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Commit-Queue: Yang Guo <yangguo@chromium.org>

--- inspector_overlay/main.ts
+++ inspector_overlay/main.ts
@@ -80,17 +80,10 @@ type MessageLookup = {
   drawingFinished: '',
 };
 
-let queue: unknown[][] = [];
-let prevQueue: string = '';
-
 const dispatch = <K extends keyof MessageLookup>(message: [a: K, b: MessageLookup[K]]) => {
   const functionName = message[0];
   if (functionName === 'setOverlay') {
     const overlayName = message[1] as keyof Overlays;
-    // No need to swap overlays if it's the same overlay.
-    if (overlays[overlayName] === currentOverlay) {
-      return;
-    }
     if (currentOverlay) {
       currentOverlay.uninstall();
     }
@@ -104,16 +97,9 @@ const dispatch = <K extends keyof MessageLookup>(message: [a: K, b: MessageLooku
   } else if (functionName === 'setPlatform') {
     platformName = message[1];
   } else if (functionName === 'drawingFinished') {
-    const currentQueue = JSON.stringify(queue);
-    if (currentQueue !== prevQueue) {
-      for (const message of queue) {
-        currentOverlay.dispatch(message);
-      }
-    }
-    prevQueue = currentQueue;
-    queue = [];
+    // TODO The logic needs to be added here once the backend starts sending this event.
   } else {
-    queue.push(message);
+    currentOverlay.dispatch(message);
   }
 };
 

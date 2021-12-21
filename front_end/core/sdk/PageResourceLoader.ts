e124417c7c3855cddf70b9c586a6bf3ec0979a41
Jaroslav Sevcik <jarin@chromium.org>
2021-12-21 11:08:43 +0000

    Revert "Load source maps via chrome-extension:// on the frontend."
    This reverts commit 1e73eb62955de7c4b0920575c7b374d47dab6a65.
    
    Reason for revert: Crashes devtools-on-devtools, see crbug.com/1281823
    
    Original change's description:
    > Load source maps via chrome-extension:// on the frontend.
    >
    > Bug: 212374
    > Change-Id: Ibfebf74d1416b4905f4c2f03124919c5b91ae721
    > Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/3306508
    > Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    > Commit-Queue: Danil Somsikov <dsv@chromium.org>
    
    Bug: 212374
    Bug: 1281823
    Change-Id: Icae256ac8129d258bd1609431d9bc1a4b6866b5a
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/3347794
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Owners-Override: Kim-Anh Tran <kimanh@chromium.org>
    Commit-Queue: Jaroslav Sevcik <jarin@chromium.org>

--- front_end/core/sdk/PageResourceLoader.ts
+++ front_end/core/sdk/PageResourceLoader.ts
@@ -219,8 +219,7 @@ export class PageResourceLoader extends Common.ObjectWrapper.ObjectWrapper<Event
       return this.#loadOverride(url);
     }
     const parsedURL = new Common.ParsedURL.ParsedURL(url);
-    const eligibleForLoadFromTarget =
-        getLoadThroughTargetSetting().get() && parsedURL && parsedURL.scheme !== 'file' && parsedURL.scheme !== 'data';
+    const eligibleForLoadFromTarget = getLoadThroughTargetSetting().get() && parsedURL && parsedURL.isHttpOrHttps();
     Host.userMetrics.developerResourceScheme(this.getDeveloperResourceScheme(parsedURL));
     if (eligibleForLoadFromTarget) {
       try {

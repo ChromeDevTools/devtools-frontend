dcc95c9681e7756e616377d604126364d7472e35
Tim van der Lippe <tvanderlippe@chromium.org>
2022-01-17 12:14:06 +0000

    Fix licenses for about:credits
    This adds an additional_readme_paths.json file that specifies all
    directories of third_party packages that we include in the DevTools
    bundle. The file is used as part of Chromiums about:credits machinery to
    list all licenses of all third_party software (accessible via
    chrome://credits).
    
    It also updates the names of the packages to use the full name, which is
    used as header of the license.
    
    To ensure that this file remains up-to-date, we also include a GN action
    that verifies all third_party directories listed in our GRD are included
    in the .json file.
    
    Lastly, add the missing license for Puppeteer.
    
    R=yangguo@chromium.org
    
    Bug: 1282407
    Change-Id: Ia3142ab5bbc79804c56bc754934b9aaf1d186d7f
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/3386943
    Commit-Queue: Tim Van der Lippe <tvanderlippe@chromium.org>
    Reviewed-by: Yang Guo <yangguo@chromium.org>

---
+++ scripts/build/assert_third_party_readmes.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env vpython
+# -*- coding: UTF-8 -*-
+#
+# Copyright 2022 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""
+Asserts that all third_party packages are included in additional_readme_paths.json.
+"""
+
+import sys
+import json
+
+
+def main(argv):
+    grd_sources = open(argv[1], 'r').read()
+    listed_third_party_directories = open(argv[2], 'r').read()
+    stamp_file = argv[3]
+
+    grd_json = json.loads(grd_sources)
+    listed_third_party_directories_json = json.loads(
+        listed_third_party_directories)
+
+    grd_json.sort()
+    listed_third_party_directories_json.sort()
+
+    return_code = 0
+    found_directories = set()
+    missing_directories = set()
+
+    for grd_file in grd_json:
+        if grd_file.startswith("front_end/third_party"):
+            third_party_directory_name = grd_file.split("/")[2]
+            found_directories.add(third_party_directory_name)
+            if third_party_directory_name not in listed_third_party_directories_json:
+                missing_directories.add(third_party_directory_name)
+
+    for missing_directory in missing_directories:
+        print(
+            "Directory `" + missing_directory + "`" +
+            " is not listed in the `additional_readme_paths.json` file " +
+            " in `front_end/third_party`. Make sure to include all third_party"
+            +
+            " directories in the `.json` file to ensure all licenses are listed"
+            + " in chrome://credits.\n")
+
+        return_code = 1
+
+    for listed_directory in listed_third_party_directories_json:
+        if (listed_directory not in found_directories
+                # TODO(crbug.com/1287519): Remove exception for codemirror 5
+                and not listed_directory == "codemirror"):
+            print(
+                "Directory `" + listed_directory + "`" +
+                " is not included in `config/gni/devtools_grd_files.gni`." +
+                " Make sure to only include third_party directoires that are shipped"
+                + " in Chromium.\n")
+
+            return_code = 1
+
+    with open(stamp_file, 'w', encoding="utf8") as fp:
+        fp.write("")
+
+    return return_code
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))

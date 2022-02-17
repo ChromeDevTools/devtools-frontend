23dc975793cd9335a2e51b4440c76268a4d56159
Tim van der Lippe <tvanderlippe@chromium.org>
2022-02-17 12:51:09 +0000

    Rebuild CodeMirror bundle to pull in HTML parser fix
    This pulls in the fix for https://github.com/codemirror/codemirror.next/issues/732
    which was released as part of `@lezer/html`, which is a transitive
    dependency. Therefore, executing `rebuild.sh` pulls in the fix.
    
    Additionally, pull in the fix for
    https://github.com/codemirror/codemirror.next/issues/734 by updating
    `@codemirror/comment` to the latest version.
    
    R=yangguo@chromium.org
    
    Fixed: 1296983, 1296535
    Change-Id: Id5b66fde1b0b5250d1fc76d918540159ed370532
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/3468516
    Auto-Submit: Tim Van der Lippe <tvanderlippe@chromium.org>
    Reviewed-by: Jack Franklin <jacktfranklin@chromium.org>
    Commit-Queue: Jack Franklin <jacktfranklin@chromium.org>

--- front_end/third_party/codemirror.next/codemirror.next.d.ts
+++ front_end/third_party/codemirror.next/codemirror.next.d.ts
@@ -573,7 +573,9 @@ declare class ChangeSet extends ChangeDesc {
     map(other: ChangeDesc, before?: boolean): ChangeSet;
     /**
     Iterate over the changed ranges in the document, calling `f` for
-    each.
+    each, with the range in the original document (`fromA`-`toA`)
+    and the range that replaces it in the new document
+    (`fromB`-`toB`).
 
     When `individual` is true, adjacent changes are reported
     separately.

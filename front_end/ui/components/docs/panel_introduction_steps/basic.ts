430ef0b9dd1ab8ae648785161a3c48a120a2cd54
Jack Franklin <jacktfranklin@chromium.org>
2022-04-04 14:00:34 +0000

    Create PanelIntroductionSteps component
    To clear up duplication in CSS Overview, Perf Insights & Recorder.
    
    Bug: 1311259
    Change-Id: Ic42c1013afa46aee10a8f20e2dab482db0a51ae0
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/3569022
    Auto-Submit: Jack Franklin <jacktfranklin@chromium.org>
    Reviewed-by: Changhao Han <changhaohan@chromium.org>
    Commit-Queue: Changhao Han <changhaohan@chromium.org>

---
+++ front_end/ui/components/docs/panel_introduction_steps/basic.ts
@@ -0,0 +1,25 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import * as FrontendHelpers from '../../../../../test/unittests/front_end/helpers/EnvironmentHelpers.js';
+import * as PanelIntroductionSteps from '../../../components/panel_introduction_steps/panel_introduction_steps.js';
+import * as LitHtml from '../../../lit-html/lit-html.js';
+import * as ComponentHelpers from '../../helpers/helpers.js';
+
+await ComponentHelpers.ComponentServerSetup.setup();
+await FrontendHelpers.initializeGlobalVars();
+
+const container = document.getElementById('container');
+if (container) {
+  LitHtml.render(
+      LitHtml.html`
+<${PanelIntroductionSteps.PanelIntroductionSteps.PanelIntroductionSteps.litTagName}>
+<span slot="title">Get actionable insights on your website's performance</span>
+<span slot="step-1">Record or import a trace into the Performance Insights panel</span>
+<span slot="step-2">Get an overview of your page’s runtime performance</span>
+<span slot="step-3">Identify improvements to your performance via a list of actionable insights</span>
+</${PanelIntroductionSteps.PanelIntroductionSteps.PanelIntroductionSteps.litTagName}>
+                 `,
+      container, {host: this});
+}
---
+++ front_end/ui/components/panel_introduction_steps/PanelIntroductionSteps.ts
@@ -0,0 +1,44 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import * as ComponentHelpers from '../../components/helpers/helpers.js';
+import * as LitHtml from '../../lit-html/lit-html.js';
+import panelIntroductionStepsStyles from './panelIntroductionSteps.css.js';
+
+export class PanelIntroductionSteps extends HTMLElement {
+  static readonly litTagName = LitHtml.literal`devtools-panel-introduction-steps`;
+  readonly #shadow = this.attachShadow({mode: 'open'});
+  readonly #boundRender = this.#render.bind(this);
+
+  connectedCallback(): void {
+    this.#shadow.adoptedStyleSheets = [panelIntroductionStepsStyles];
+    void ComponentHelpers.ScheduledRender.scheduleRender(this, this.#boundRender);
+  }
+
+  #render(): void {
+    if (!ComponentHelpers.ScheduledRender.isScheduledRender(this)) {
+      throw new Error('FeedbackButton render was not scheduled');
+    }
+
+    // clang-format off
+    LitHtml.render(LitHtml.html`
+      <h1><slot name="title">slot: title</slot></h1>
+
+      <ol class="intro-steps">
+        <li><slot name="step-1">slot: step-1</slot></li>
+        <li><slot name="step-2">slot: step-2</slot></li>
+        <li><slot name="step-3">slot: step-3</slot></li>
+      </ol>
+    `, this.#shadow, {host: this});
+    // clang-format on
+  }
+}
+
+ComponentHelpers.CustomElements.defineComponent('devtools-panel-introduction-steps', PanelIntroductionSteps);
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'devtools-panel-introduction-steps': PanelIntroductionSteps;
+  }
+}

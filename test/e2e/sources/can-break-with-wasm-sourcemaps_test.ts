ad001b2af4d80e469c375d0bd05d79f4110eba45
Andres Olivares <andoli@chromium.org>
2022-05-02 17:55:10 +0000

    Skip sources/can-break-with-wasm-sourcemaps_test.ts on mac
    The test has been flaky recently on the Mac bot:
    
    https://logs.chromium.org/logs/devtools-frontend/buildbucket/cr-buildbucket/8815641271312814129/+/u/E2E_tests/stdout
    
    https://logs.chromium.org/logs/devtools-frontend/buildbucket/cr-buildbucket/8815532227356031937/+/u/E2E_tests/stdout
    
    Disabling it for now
    
    Bug: 1321707
    Change-Id: I16b9cda9b8ad7d36a22a388494b70d6bd58198df
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/3621874
    Reviewed-by: Benedikt Meurer <bmeurer@chromium.org>
    Commit-Queue: Benedikt Meurer <bmeurer@chromium.org>
    Auto-Submit: Andres Olivares <andoli@chromium.org>

--- test/e2e/sources/can-break-with-wasm-sourcemaps_test.ts
+++ test/e2e/sources/can-break-with-wasm-sourcemaps_test.ts
@@ -18,62 +18,64 @@ describe('The Sources Tab', async () => {
     assert.deepEqual(scriptLocation, 'with-sourcemap.ll:5');
   });
 
-  it('hits two breakpoints that are set and activated separately', async function() {
-    const {target, frontend} = getBrowserAndPages();
-    const fileName = 'with-sourcemap.ll';
+  // Flaky on mac
+  it.skipOnPlatforms(
+      ['mac'], '[crbug.com/1321707]: hits two breakpoints that are set and activated separately', async function() {
+        const {target, frontend} = getBrowserAndPages();
+        const fileName = 'with-sourcemap.ll';
 
-    await step('navigate to a page and open the Sources tab', async () => {
-      await openSourceCodeEditorForFile(fileName, 'wasm/wasm-with-sourcemap.html');
-    });
+        await step('navigate to a page and open the Sources tab', async () => {
+          await openSourceCodeEditorForFile(fileName, 'wasm/wasm-with-sourcemap.html');
+        });
 
-    await step('add a breakpoint to line No.5', async () => {
-      await addBreakpointForLine(frontend, 5);
-    });
+        await step('add a breakpoint to line No.5', async () => {
+          await addBreakpointForLine(frontend, 5);
+        });
 
-    await step('reload the page', async () => {
-      await reloadPageAndWaitForSourceFile(frontend, target, fileName);
-    });
+        await step('reload the page', async () => {
+          await reloadPageAndWaitForSourceFile(frontend, target, fileName);
+        });
 
-    await waitForFunction(async () => await isBreakpointSet(5));
+        await waitForFunction(async () => await isBreakpointSet(5));
 
-    await step('check that the code has paused on the breakpoint at the correct script location', async () => {
-      const scriptLocation = await retrieveTopCallFrameWithoutResuming();
-      assert.deepEqual(scriptLocation, 'with-sourcemap.ll:5');
-    });
+        await step('check that the code has paused on the breakpoint at the correct script location', async () => {
+          const scriptLocation = await retrieveTopCallFrameWithoutResuming();
+          assert.deepEqual(scriptLocation, 'with-sourcemap.ll:5');
+        });
 
-    await step('resume script execution', async () => {
-      await frontend.keyboard.press('F8');
-      await waitFor(TURNED_OFF_PAUSE_BUTTON_SELECTOR);
-    });
+        await step('resume script execution', async () => {
+          await frontend.keyboard.press('F8');
+          await waitFor(TURNED_OFF_PAUSE_BUTTON_SELECTOR);
+        });
 
-    await step('remove the breakpoint from the fifth line', async () => {
-      await removeBreakpointForLine(frontend, '5');
-    });
+        await step('remove the breakpoint from the fifth line', async () => {
+          await removeBreakpointForLine(frontend, '5');
+        });
 
-    await step('reload the page', async () => {
-      await reloadPageAndWaitForSourceFile(frontend, target, fileName);
-    });
+        await step('reload the page', async () => {
+          await reloadPageAndWaitForSourceFile(frontend, target, fileName);
+        });
 
-    await step('open original source file', async () => {
-      await openFileInEditor('with-sourcemap.ll');
-    });
+        await step('open original source file', async () => {
+          await openFileInEditor('with-sourcemap.ll');
+        });
 
-    await waitForFunction(async () => !(await isBreakpointSet(5)));
-    await checkBreakpointDidNotActivate();
+        await waitForFunction(async () => !(await isBreakpointSet(5)));
+        await checkBreakpointDidNotActivate();
 
-    await step('add a breakpoint to line No.6', async () => {
-      await addBreakpointForLine(frontend, 6);
-    });
+        await step('add a breakpoint to line No.6', async () => {
+          await addBreakpointForLine(frontend, 6);
+        });
 
-    await step('reload the page', async () => {
-      await reloadPageAndWaitForSourceFile(frontend, target, fileName);
-    });
+        await step('reload the page', async () => {
+          await reloadPageAndWaitForSourceFile(frontend, target, fileName);
+        });
 
-    await waitForFunction(async () => await isBreakpointSet(6));
+        await waitForFunction(async () => await isBreakpointSet(6));
 
-    await step('check that the code has paused on the breakpoint at the correct script location', async () => {
-      const scriptLocation = await retrieveTopCallFrameWithoutResuming();
-      assert.deepEqual(scriptLocation, 'with-sourcemap.ll:6');
-    });
-  });
+        await step('check that the code has paused on the breakpoint at the correct script location', async () => {
+          const scriptLocation = await retrieveTopCallFrameWithoutResuming();
+          assert.deepEqual(scriptLocation, 'with-sourcemap.ll:6');
+        });
+      });
 });

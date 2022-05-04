4cf9a8553fcbe12da54649fd7394ca95cb342096
Benedikt Meurer <bmeurer@chromium.org>
2022-05-04 08:02:07 +0200

    [e2e] Fix flakiness due to not waiting for Call Stack to update.
    Fixed: 1321707
    Change-Id: Ib56cdbe889d07e148bceacc4a86f8b1abee0273e
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/3627329
    Reviewed-by: Jaroslav Sevcik <jarin@chromium.org>
    Commit-Queue: Benedikt Meurer <bmeurer@chromium.org>
    Commit-Queue: Jaroslav Sevcik <jarin@chromium.org>
    Auto-Submit: Benedikt Meurer <bmeurer@chromium.org>

--- test/e2e/sources/can-break-with-wasm-sourcemaps_test.ts
+++ test/e2e/sources/can-break-with-wasm-sourcemaps_test.ts
@@ -20,74 +20,81 @@ import {
 } from '../helpers/sources-helpers.js';
 
 describe('The Sources Tab', async () => {
+  const fileName = 'with-sourcemap.ll';
+
   it('can add breakpoint for a sourcemapped wasm module', async () => {
     const {target, frontend} = getBrowserAndPages();
 
-    await openSourceCodeEditorForFile('with-sourcemap.ll', 'wasm/wasm-with-sourcemap.html');
+    await openSourceCodeEditorForFile(fileName, 'wasm/wasm-with-sourcemap.html');
     await addBreakpointForLine(frontend, 5);
 
     const scriptLocation = await retrieveTopCallFrameScriptLocation('main();', target);
-    assert.deepEqual(scriptLocation, 'with-sourcemap.ll:5');
+    assert.deepEqual(scriptLocation, `${fileName}:5`);
   });
 
-  // Flaky on mac
-  it.skipOnPlatforms(
-      ['mac'], '[crbug.com/1321707]: hits two breakpoints that are set and activated separately', async function() {
-        const {target, frontend} = getBrowserAndPages();
-        const fileName = 'with-sourcemap.ll';
+  it('hits two breakpoints that are set and activated separately', async () => {
+    const {target, frontend} = getBrowserAndPages();
 
-        await step('navigate to a page and open the Sources tab', async () => {
-          await openSourceCodeEditorForFile(fileName, 'wasm/wasm-with-sourcemap.html');
-        });
+    await step('navigate to a page and open the Sources tab', async () => {
+      await openSourceCodeEditorForFile(fileName, 'wasm/wasm-with-sourcemap.html');
+    });
 
-        await step('add a breakpoint to line No.5', async () => {
-          await addBreakpointForLine(frontend, 5);
-        });
+    await step('add a breakpoint to line No.5', async () => {
+      await addBreakpointForLine(frontend, 5);
+    });
 
-        await step('reload the page', async () => {
-          await reloadPageAndWaitForSourceFile(frontend, target, fileName);
-        });
+    await step('reload the page', async () => {
+      await reloadPageAndWaitForSourceFile(frontend, target, fileName);
+    });
 
-        await waitForFunction(async () => await isBreakpointSet(5));
+    await step('open original source file', async () => {
+      await openFileInEditor(fileName);
+    });
 
-        await step('check that the code has paused on the breakpoint at the correct script location', async () => {
-          const scriptLocation = await retrieveTopCallFrameWithoutResuming();
-          assert.deepEqual(scriptLocation, 'with-sourcemap.ll:5');
-        });
+    await waitForFunction(async () => await isBreakpointSet(5));
 
-        await step('resume script execution', async () => {
-          await frontend.keyboard.press('F8');
-          await waitFor(TURNED_OFF_PAUSE_BUTTON_SELECTOR);
-        });
-
-        await step('remove the breakpoint from the fifth line', async () => {
-          await removeBreakpointForLine(frontend, '5');
-        });
-
-        await step('reload the page', async () => {
-          await reloadPageAndWaitForSourceFile(frontend, target, fileName);
-        });
-
-        await step('open original source file', async () => {
-          await openFileInEditor('with-sourcemap.ll');
-        });
-
-        await waitForFunction(async () => !(await isBreakpointSet(5)));
-        await checkBreakpointDidNotActivate();
-
-        await step('add a breakpoint to line No.6', async () => {
-          await addBreakpointForLine(frontend, 6);
-        });
-
-        await step('reload the page', async () => {
-          await reloadPageAndWaitForSourceFile(frontend, target, fileName);
-        });
-
-        await waitForFunction(async () => await isBreakpointSet(6));
-
-        await step('check that the code has paused on the breakpoint at the correct script location', async () => {
-          const scriptLocation = await retrieveTopCallFrameWithoutResuming();
-          assert.deepEqual(scriptLocation, 'with-sourcemap.ll:6');
-        });
+    await step('check that the code has paused on the breakpoint at the correct script location', async () => {
+      await waitForFunction(async () => {
+        const scriptLocation = await retrieveTopCallFrameWithoutResuming();
+        return scriptLocation === `${fileName}:5`;
       });
+    });
+
+    await step('resume script execution', async () => {
+      await frontend.keyboard.press('F8');
+      await waitFor(TURNED_OFF_PAUSE_BUTTON_SELECTOR);
+    });
+
+    await step('remove the breakpoint from the fifth line', async () => {
+      await removeBreakpointForLine(frontend, '5');
+    });
+
+    await step('reload the page', async () => {
+      await reloadPageAndWaitForSourceFile(frontend, target, fileName);
+    });
+
+    await step('open original source file', async () => {
+      await openFileInEditor(fileName);
+    });
+
+    await waitForFunction(async () => !(await isBreakpointSet(5)));
+    await checkBreakpointDidNotActivate();
+
+    await step('add a breakpoint to line No.6', async () => {
+      await addBreakpointForLine(frontend, 6);
+    });
+
+    await step('reload the page', async () => {
+      await reloadPageAndWaitForSourceFile(frontend, target, fileName);
+    });
+
+    await waitForFunction(async () => await isBreakpointSet(6));
+
+    await step('check that the code has paused on the breakpoint at the correct script location', async () => {
+      await waitForFunction(async () => {
+        const scriptLocation = await retrieveTopCallFrameWithoutResuming();
+        return scriptLocation === `${fileName}:6`;
+      });
+    });
+  });
 });

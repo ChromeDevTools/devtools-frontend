20e371365af4e8a639ab9258cfbfcfe85733af19
Simon Zünd <szuend@chromium.org>
2022-05-12 10:15:09 +0200

    [e2e] For debugging, pause when tabs are ready
    Enabling the pool delayed tab creation into the `beforeEach` block.
    So when we pause in the `beforeAll` block in debug mode, the DevTools
    and target tab are not yet booted up.
    
    This CL moves the pause into the `beforeEach` block but still only
    pause once.
    
    R=pfaffe@chromium.org
    
    Bug: 1297458
    Change-Id: Ic3f1ddc41696d4e11c6797effc20d1101399d163
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/3642145
    Reviewed-by: Philip Pfaffe <pfaffe@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>

--- test/conductor/mocha_hooks.ts
+++ test/conductor/mocha_hooks.ts
@@ -60,6 +60,8 @@ const testSuiteCoverageMap = createCoverageMap();
 const SHOULD_GATHER_COVERAGE_INFORMATION = process.env.COVERAGE === '1' && DERIVED_SERVER_TYPE === 'component-docs';
 const INTERACTIONS_COVERAGE_LOCATION = path.join(process.cwd(), 'interactions-coverage/');
 
+let didPauseAtBeginning = false;
+
 // These are the 'root hook plugins': https://mochajs.org/#root-hook-plugins
 // These open and configure the browser before tests are run.
 export const mochaHooks = {
@@ -72,23 +74,6 @@ export const mochaHooks = {
     // reasonably quickly (2 seconds by default).
     this.timeout(0);
     await preFileSetup(Number(process.env.testServerPort));
-
-    // Pause when running interactively in debug mode. This is mututally
-    // exclusive with parallel mode.
-    if (process.env['DEBUG_TEST']) {
-      console.log('Running in debug mode.');
-      console.log(' - Press enter to run the test suite.');
-      console.log(' - Press ctrl + c to quit.');
-
-      await new Promise<void>(resolve => {
-        const {stdin} = process;
-
-        stdin.on('data', () => {
-          stdin.pause();
-          resolve();
-        });
-      });
-    }
   },
   // In serial mode, run after all tests end, once only.
   // In parallel mode, run after all tests end, for each file.
@@ -128,6 +113,29 @@ export const mochaHooks = {
     // Sets the timeout higher for this hook only.
     this.timeout(10000);
     await resetPagesBetweenTests();
+
+    // Pause when running interactively in debug mode. This is mututally
+    // exclusive with parallel mode.
+    // We need to pause after `resetPagesBetweenTests`, otherwise the DevTools
+    // and target tab are not available to us to set breakpoints in.
+    // We still only want to pause once, so we remember that we did pause.
+    if (process.env['DEBUG_TEST'] && !didPauseAtBeginning) {
+      this.timeout(0);
+      didPauseAtBeginning = true;
+
+      console.log('Running in debug mode.');
+      console.log(' - Press enter to run the test.');
+      console.log(' - Press ctrl + c to quit.');
+
+      await new Promise<void>(resolve => {
+        const {stdin} = process;
+
+        stdin.on('data', () => {
+          stdin.pause();
+          resolve();
+        });
+      });
+    }
   },
   afterEach: async function(this: Mocha.Suite) {
     if (!SHOULD_GATHER_COVERAGE_INFORMATION) {

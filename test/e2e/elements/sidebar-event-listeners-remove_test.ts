b33e361ecfa9469dbbaf091896eecaa7c0956912
Alex Rudenko <alexrudenko@chromium.org>
2022-05-16 13:21:29 +0200

    Disable flaky test
    Bug: 1325790
    Change-Id: I687024d31d3323b0d54c6e9b6c4c4c148d400624
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/3646475
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Auto-Submit: Alex Rudenko <alexrudenko@chromium.org>

--- test/e2e/elements/sidebar-event-listeners-remove_test.ts
+++ test/e2e/elements/sidebar-event-listeners-remove_test.ts
@@ -18,7 +18,8 @@ describe('Removing event listeners in the elements sidebar', async () => {
     await loadEventListenersAndSelectButtonNode();
   });
 
-  it('shows "Remove" by each node for a given event', async () => {
+  // TODO: crbug.com/1325790 flaky test.
+  it.skipOnPlatforms(['mac'], '[crbug.com/1325790] shows "Remove" by each node for a given event', async () => {
     await openEventListenersPaneAndWaitForListeners();
     const {
       firstListenerText,

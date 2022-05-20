c367dcf536a36698d0512cd42e101e91128efb91
Christy Chen <christychen126@gmail.com>
2022-05-20 13:22:48 -0700

    [A11y] Fix DOMBreakpoint sidebar screen reader issue
    Screen Reader is announcing the DomBP content twice and not announcing the checkbox state in DOMbp sidebar. This is because the focusable element is the list element, but the element itself doesnt have an aria-label, so the screen reader will read through the label of the checkbox and the BP description, causing it to announce it twice.
    Now add the aria-label with complete info to the list element so when the bp item get focused, the screen reader would read out the complete info once.
    
    Bug:1327440
    Change-Id: I94fd9d3e0b93377f684fea6b5d384c944e29dc3d
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/3654682
    Commit-Queue: Christy Chen <chrche@microsoft.com>
    Reviewed-by: Mathias Bynens <mathias@chromium.org>
    Reviewed-by: Johan Bay <jobay@chromium.org>

--- front_end/panels/browser_debugger/DOMBreakpointsSidebarPane.ts
+++ front_end/panels/browser_debugger/DOMBreakpointsSidebarPane.ts
@@ -53,6 +53,13 @@ const UIStrings = {
   */
   sS: '{PH1}: {PH2}',
   /**
+  *@description Text with three placeholders separated by a colon and a comma
+  *@example {Node removed} PH1
+  *@example {div#id1} PH2
+  *@example {checked} PH3
+  */
+  sSS: '{PH1}: {PH2}, {PH3}',
+  /**
   *@description Text exposed to screen readers on checked items.
   */
   checked: 'checked',
@@ -196,19 +203,26 @@ export class DOMBreakpointsSidebarPane extends UI.Widget.VBox implements
     description.textContent = breakpointTypeLabel ? breakpointTypeLabel() : null;
     const breakpointTypeText = breakpointTypeLabel ? breakpointTypeLabel() : '';
     UI.ARIAUtils.setAccessibleName(checkboxElement, breakpointTypeText);
+    const checkedStateText = item.enabled ? i18nString(UIStrings.checked) : i18nString(UIStrings.unchecked);
     const linkifiedNode = document.createElement('monospace');
     linkifiedNode.style.display = 'block';
     labelElement.appendChild(linkifiedNode);
     void Common.Linkifier.Linkifier.linkify(item.node, {preventKeyboardFocus: true, tooltip: undefined})
         .then(linkified => {
           linkifiedNode.appendChild(linkified);
+          // Give the checkbox an aria-label as it is required for all form element
           UI.ARIAUtils.setAccessibleName(
               checkboxElement, i18nString(UIStrings.sS, {PH1: breakpointTypeText, PH2: linkified.deepTextContent()}));
+          // The parent list element is the one that actually gets focused.
+          // Assign it an aria-label with complete information for the screen reader to read out properly
+          UI.ARIAUtils.setAccessibleName(
+              element,
+              i18nString(
+                  UIStrings.sSS, {PH1: breakpointTypeText, PH2: linkified.deepTextContent(), PH3: checkedStateText}));
         });
 
     labelElement.appendChild(description);
 
-    const checkedStateText = item.enabled ? i18nString(UIStrings.checked) : i18nString(UIStrings.unchecked);
     if (item === this.#highlightedBreakpoint) {
       element.classList.add('breakpoint-hit');
       UI.ARIAUtils.setDescription(element, i18nString(UIStrings.sBreakpointHit, {PH1: checkedStateText}));

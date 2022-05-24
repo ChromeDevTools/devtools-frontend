4c09b86906e43a22f34ce85f5fb917287cda3662
Jack Franklin <jacktfranklin@chromium.org>
2022-05-24 11:10:55 +0000

    Add `requestById` to NetworkManager
    It feels safer and nicer to look up requests by an ID, not by the URL.
    The dispatcher already had this method; we just need to expose it from
    the NetworkManager, much like the pre-existing `requestForURL`.
    
    Bug: none
    Change-Id: I5fc599bed04d6d79d12e31e0afdb8c324c888788
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/3663321
    Auto-Submit: Jack Franklin <jacktfranklin@chromium.org>
    Commit-Queue: Jack Franklin <jacktfranklin@chromium.org>
    Reviewed-by: Philip Pfaffe <pfaffe@chromium.org>

--- front_end/core/sdk/NetworkManager.ts
+++ front_end/core/sdk/NetworkManager.ts
@@ -255,6 +255,10 @@ export class NetworkManager extends SDKModel<EventTypes> {
     return this.dispatcher.requestForURL(url);
   }
 
+  requestforId(id: string): NetworkRequest|null {
+    return this.dispatcher.requestForId(id);
+  }
+
   private cacheDisabledSettingChanged({data: enabled}: Common.EventTarget.EventTargetEvent<boolean>): void {
     void this.#networkAgent.invoke_setCacheDisabled({cacheDisabled: enabled});
   }
@@ -520,8 +524,8 @@ export class NetworkDispatcher implements ProtocolProxyApi.NetworkDispatcher {
     }
   }
 
-  requestForId(url: string): NetworkRequest|null {
-    return this.#requestsById.get(url) || null;
+  requestForId(id: string): NetworkRequest|null {
+    return this.#requestsById.get(id) || null;
   }
 
   requestForURL(url: Platform.DevToolsPath.UrlString): NetworkRequest|null {

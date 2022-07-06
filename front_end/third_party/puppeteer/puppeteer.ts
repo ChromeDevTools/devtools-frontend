c9a66695fd7326e7a8a23d84ada30da18c65b59f
Randolf J <jrandolf@google.com>
2022-07-06 23:34:02 +0200

    Fix `ElementHandle` extension
    Bug: none
    Change-Id: I5f7d4ec242fd26043084e2f08c905cbdf1d1228c
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/3748106
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>

--- front_end/third_party/puppeteer/puppeteer.ts
+++ front_end/third_party/puppeteer/puppeteer.ts
@@ -6,7 +6,7 @@ import { Browser } from "./package/lib/esm/puppeteer/common/Browser.js";
 import { Connection } from "./package/lib/esm/puppeteer/common/Connection.js";
 import { type ConnectionTransport } from "./package/lib/esm/puppeteer/common/ConnectionTransport.js";
 import { Frame } from "./package/lib/esm/puppeteer/common/FrameManager.js";
-import { ElementHandle } from "./package/lib/esm/puppeteer/common/ElementHandle";
+import { ElementHandle } from "./package/lib/esm/puppeteer/common/ElementHandle.js";
 import { Page } from "./package/lib/esm/puppeteer/common/Page.js";
 
 export { Browser, Connection, ConnectionTransport, ElementHandle, Frame, Page };

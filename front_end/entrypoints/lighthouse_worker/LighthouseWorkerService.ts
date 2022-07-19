43ce2e517fd28249774a5b574deb5424979f2f5e
Adam Raine <asraine@google.com>
2022-07-19 12:30:36 -0700

    Support using legacy flags in Lighthouse FR modes
    Without this change, the following PR would break our flags usage:
    https://github.com/GoogleChrome/lighthouse/pull/14050
    
    Bug: None
    Change-Id: If5d05982fcd6a4fdd379e2de1406b976338cdf56
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/3774533
    Reviewed-by: Paul Irish <paulirish@chromium.org>
    Commit-Queue: Paul Irish <paulirish@chromium.org>

--- front_end/entrypoints/lighthouse_worker/LighthouseWorkerService.ts
+++ front_end/entrypoints/lighthouse_worker/LighthouseWorkerService.ts
@@ -147,19 +147,22 @@ async function invokeLH(action: string, args: any): Promise<unknown> {
     };
 
     if (action === 'snapshot') {
+      // TODO: Remove `configContext` once Lighthouse roll removes it
       // @ts-expect-error https://github.com/GoogleChrome/lighthouse/issues/11628
-      return await self.runLighthouseSnapshot({config, page, configContext});
+      return await self.runLighthouseSnapshot({config, page, configContext, flags});
     }
 
     if (action === 'startTimespan') {
+      // TODO: Remove `configContext` once Lighthouse roll removes it
       // @ts-expect-error https://github.com/GoogleChrome/lighthouse/issues/11628
-      const timespan = await self.startLighthouseTimespan({config, page, configContext});
+      const timespan = await self.startLighthouseTimespan({config, page, configContext, flags});
       endTimespan = timespan.endTimespan;
       return;
     }
 
+    // TODO: Remove `configContext` once Lighthouse roll removes it
     // @ts-expect-error https://github.com/GoogleChrome/lighthouse/issues/11628
-    return await self.runLighthouseNavigation(url, {config, page, configContext});
+    return await self.runLighthouseNavigation(url, {config, page, configContext, flags});
   } catch (err) {
     return ({
       fatal: true,

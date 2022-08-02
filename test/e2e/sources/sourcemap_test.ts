99f16bc3f8f39747ae610b946ca86913429189fd
Jaroslav Sevcik <jarin@chromium.org>
2022-08-02 09:44:47 +0200

    Disable a sources/sourcemap_test.ts e2e test on all platforms
    Disables the test
    "automatically ignore-lists third party code from source maps"
    on all platforms (previously only disabled on Mac) because it fails
    also on Linux bots.
    
    For example, see
    
    https://logs.chromium.org/logs/devtools-frontend/buildbucket/cr-buildbucket/8807302857063168113/+/u/E2E_tests/stdout
    https://logs.chromium.org/logs/devtools-frontend/buildbucket/cr-buildbucket/8807328950339600017/+/u/E2E_tests/stdout
    
    Bug: chromium:1346228
    Change-Id: I816f9c5dbb99dd6ef8ad00589e454aea0e15a804
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/3802972
    Commit-Queue: Jaroslav Sevcik <jarin@chromium.org>
    Auto-Submit: Jaroslav Sevcik <jarin@chromium.org>
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>

--- test/e2e/sources/sourcemap_test.ts
+++ test/e2e/sources/sourcemap_test.ts
@@ -365,9 +365,9 @@ describe('The Sources Tab', async function() {
     });
   });
 
-  // Flaky - timeouts almost every time on mac.
-  it.skipOnPlatforms(
-      ['mac'], '[crbug.com/1346228] automatically ignore-lists third party code from source maps', async function() {
+  // TODO(crbug.com/1346228) Flaky - timeouts.
+  it.skip(
+      '[crbug.com/1346228] automatically ignore-lists third party code from source maps', async function() {
         const {target} = getBrowserAndPages();
         await openSourceCodeEditorForFile('webpack-main.js', 'webpack-index.html');
 

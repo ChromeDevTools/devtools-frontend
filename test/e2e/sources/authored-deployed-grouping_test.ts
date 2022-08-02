bce1b38483d4392e9969c465c025d331690333c3
Jaroslav Sevcik <jarin@chromium.org>
2022-08-02 09:30:54 +0200

    Skip sources/authored-deployed-grouping_test.ts e2e test
    Skips sources/authored-deployed-grouping_test.ts
    "can mix group by authored/deployed and group by folder" test because
    of flaky timeout (see crbug.com/1349268 for details).
    
    Bug: chromium:1349268
    Change-Id: I3001709794a2bd798cd1191e2333ec273ceeb95e
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/3802971
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Auto-Submit: Jaroslav Sevcik <jarin@chromium.org>
    Commit-Queue: Jaroslav Sevcik <jarin@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>

--- test/e2e/sources/authored-deployed-grouping_test.ts
+++ test/e2e/sources/authored-deployed-grouping_test.ts
@@ -268,7 +268,8 @@ describe('Source Panel grouping', async function() {
     assert.deepEqual(await readSourcesTreeView(), groupedExpectedTree);
   });
 
-  it('can mix group by authored/deployed and group by folder', async () => {
+  // TODO(crbug.com/1349268) Currently disabled because it flakily times out.
+  it.skip('[crbug.com/1349268] can mix group by authored/deployed and group by folder', async () => {
     // Have the target load the page.
     await goToResource(targetPage);
     await openSourcesPanel();

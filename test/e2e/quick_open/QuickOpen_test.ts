d9baeb84e5251aeedf6821af1ea18785650941ea
Eric Leese <leese@chromium.org>
2022-08-05 15:22:21 +0200

    Consolidate quick open test and fix flakiness
    Tests now use keyboard instead of mouse to open
    Quick Open menu, which is more reliable on the mac
    bots.
    
    Fixed: 1350083,1337781,1342142
    Change-Id: If4121e5a144e660934d3b8f23c7caa33f5abc324
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/3810340
    Commit-Queue: Eric Leese <leese@chromium.org>
    Reviewed-by: Kim-Anh Tran <kimanh@chromium.org>
    Reviewed-by: Andres Olivares <andoli@chromium.org>

--- test/e2e/quick_open/QuickOpen_test.ts
+++ test/e2e/quick_open/QuickOpen_test.ts
@@ -4,12 +4,12 @@
 
 import {assert} from 'chai';
 
-import {click, getBrowserAndPages, goToResource, step, waitFor} from '../../shared/helper.js';
+import {$$, click, enableExperiment, getBrowserAndPages, goToResource, step, waitFor} from '../../shared/helper.js';
 import {describe, it} from '../../shared/mocha-extensions.js';
 import {navigateToElementsTab} from '../helpers/elements-helpers.js';
 import {getMenuItemAtPosition, getMenuItemTitleAtPosition, openFileQuickOpen} from '../helpers/quick_open-helpers.js';
-import {togglePreferenceInSettingsTab} from '../helpers/settings-helpers.js';
-import {listenForSourceFilesLoaded, waitForSourceLoadedEvent} from '../helpers/sources-helpers.js';
+import {setIgnoreListPattern, togglePreferenceInSettingsTab} from '../helpers/settings-helpers.js';
+import {listenForSourceFilesLoaded, openSourcesPanel, waitForSourceLoadedEvent} from '../helpers/sources-helpers.js';
 
 async function openAFileWithQuickMenu() {
   const {frontend} = getBrowserAndPages();
@@ -29,9 +29,27 @@ async function openAFileWithQuickMenu() {
   });
 }
 
+async function typeIntoQuickOpen(query: string, expectEmptyResults?: boolean) {
+  await openFileQuickOpen();
+  const prompt = await waitFor('[aria-label="Quick open prompt"]');
+  await prompt.type(query);
+  if (expectEmptyResults) {
+    await waitFor('.filtered-list-widget :not(.hidden).not-found-text');
+  } else {
+    // Because each highlighted character is in its own div, we can count the highlighted
+    // characters in one item to see that the list reflects the full query.
+    const highlightSelector = new Array(query.length).fill('.highlight').join(' ~ ');
+    await waitFor('.filtered-list-widget-title ' + highlightSelector);
+  }
+}
+
+async function readQuickOpenResults(): Promise<string[]> {
+  const items = await $$('.filtered-list-widget-title');
+  return await Promise.all(items.map(element => element.evaluate(el => el.textContent as string)));
+}
+
 describe('Quick Open menu', () => {
-  // Flaky on mac.
-  it.skipOnPlatforms(['mac'], '[crbug.com/1342142] lists available files', async () => {
+  it('lists available files', async () => {
     await goToResource('pages/hello-world.html');
     await openFileQuickOpen();
     const firstItemTitle = await getMenuItemTitleAtPosition(0);
@@ -42,5 +60,25 @@ describe('Quick Open menu', () => {
     await openAFileWithQuickMenu();
     await togglePreferenceInSettingsTab('Focus Sources panel when triggering a breakpoint');
     await openAFileWithQuickMenu();
+  });
+
+  it('sorts authored above deployed', async () => {
+    await goToResource('sources/multi-workers-sourcemap.html');
+    await openSourcesPanel();
+
+    await typeIntoQuickOpen('mult');
+    const list = await readQuickOpenResults();
+    assert.deepEqual(list, ['multi-workers.js', 'multi-workers.min.js', 'multi-workers-sourcemap.html']);
+  });
+
+  it('Does not list ignore-listed files', async () => {
+    await enableExperiment('justMyCode');
+    await setIgnoreListPattern('workers.js');
+    await goToResource('sources/multi-workers-sourcemap.html');
+    await openSourcesPanel();
+
+    await typeIntoQuickOpen('mult');
+    const list = await readQuickOpenResults();
+    assert.deepEqual(list, ['multi-workers.min.js', 'multi-workers-sourcemap.html']);
   });
 });

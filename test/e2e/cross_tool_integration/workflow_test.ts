054e912f343b1a8b999f3bfefd528e599a379272
Philip Pfaffe <pfaffe@chromium.org>
2022-09-26 10:52:49 +0000

    [e2e] Disable workflow test on other platforms as well
    Bug: 1322534
    Change-Id: I7d61c05b46fadebc342b4ff922a37c5be78576a4
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/3918150
    Auto-Submit: Philip Pfaffe <pfaffe@chromium.org>
    Reviewed-by: Andres Olivares <andoli@chromium.org>
    Commit-Queue: Andres Olivares <andoli@chromium.org>
    Commit-Queue: Philip Pfaffe <pfaffe@chromium.org>

--- test/e2e/cross_tool_integration/workflow_test.ts
+++ test/e2e/cross_tool_integration/workflow_test.ts
@@ -51,8 +51,8 @@ describe('A user can navigate across', async function() {
     await waitFor('.panel[aria-label="sources"]');
   });
 
-  // Flaky on mac
-  it.skipOnPlatforms(['mac'], '[crbug.com/1322534]: Console -> Issues', async () => {
+  // Flaky test
+  it.skip('[crbug.com/1322534]: Console -> Issues', async () => {
     await navigateToConsoleTab();
     await navigateToIssuesPanelViaInfoBar();
 

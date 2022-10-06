ce8e180bf96b17caabc41779ca31b951945daaf7
Jack Franklin <jacktfranklin@chromium.org>
2022-10-06 10:08:30 +0000

    Replace "long task" link with new web.dev article
    Bug: none
    Change-Id: Ia5fb3b6470b7de608b4b3a3a2690e62fd56ae7f9
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/3937924
    Reviewed-by: Nancy Li <nancyly@chromium.org>
    Commit-Queue: Nancy Li <nancyly@chromium.org>
    Auto-Submit: Jack Franklin <jacktfranklin@chromium.org>

--- front_end/panels/timeline/TimelineUIUtils.ts
+++ front_end/panels/timeline/TimelineUIUtils.ts
@@ -3314,7 +3314,7 @@ export class TimelineUIUtils {
 
       case warnings.LongTask: {
         const longTaskLink =
-            UI.XLink.XLink.create('https://web.dev/rail/#goals-and-guidelines', i18nString(UIStrings.longTask));
+            UI.XLink.XLink.create('https://web.dev/optimize-long-tasks/', i18nString(UIStrings.longTask));
         span.appendChild(i18n.i18n.getFormatLocalizedString(
             str_, UIStrings.sTookS,
             {PH1: longTaskLink, PH2: i18n.TimeUtilities.millisToString((event.duration || 0), true)}));

4478aa88299db527943f499278d0eee59410d455
Simon Zünd <szuend@chromium.org>
2022-10-11 07:56:16 +0000

    Revert "Replace checks for non-null parentTarget with a check for a type == Frame in the NetworkLogView."
    This reverts commit 8be8469f1c49ac2699270555ad76a10ed4164ee4.
    
    Reason for revert: Causes flaky test failures (e.g. https://ci.chromium.org/ui/p/devtools-frontend/builders/ci/Stand-alone%20Mac/8392/overview)
    
    The failures originate from this test due to a cascade of `window.requestAnimationFrame` callbacks that can run while other tests are running. If we fall in-between test execution than the `Settings` instance is missing and we throw, causing unrelated before/after hooks to fail. 
    
    Original change's description:
    > Replace checks for non-null parentTarget with a check for a type == Frame in the NetworkLogView.
    >
    > Bug: 1370050
    > Change-Id: I49da2fad3ea2b6bcaeff72a0d553183904bd92b3
    > Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/3936529
    > Auto-Submit: Danil Somsikov <dsv@chromium.org>
    > Reviewed-by: Wolfgang Beyer <wolfi@chromium.org>
    > Commit-Queue: Wolfgang Beyer <wolfi@chromium.org>
    
    Bug: 1370050
    Change-Id: I5459f40a51d007c8a3857b1eaa953e8baa733782
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/3941776
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>

--- test/unittests/front_end/panels/network/NetworkLogView_test.ts
+++ test/unittests/front_end/panels/network/NetworkLogView_test.ts
@@ -2,17 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import * as Common from '../../../../../front_end/core/common/common.js';
-import * as Network from '../../../../../front_end/panels/network/network.js';
+import * as SDK from '../../../../../front_end/core/sdk/sdk.js';
 import type * as Protocol from '../../../../../front_end/generated/protocol.js';
 import type * as Platform from '../../../../../front_end/core/platform/platform.js';
-import * as SDK from '../../../../../front_end/core/sdk/sdk.js';
-import * as UI from '../../../../../front_end/ui/legacy/legacy.js';
-import {assertNotNullOrUndefined} from '../../../../../front_end/core/platform/platform.js';
-import {createTarget} from '../../helpers/EnvironmentHelpers.js';
-import {describeWithMockConnection} from '../../helpers/MockConnection.js';
+import * as Network from '../../../../../front_end/panels/network/network.js';
 
-describeWithMockConnection('NetworkLogView', () => {
+describe('NetworkLogView', () => {
   it('can create curl command parameters for headers without value', async () => {
     const request = SDK.NetworkRequest.NetworkRequest.create(
         'requestId' as Protocol.Network.RequestId,
@@ -27,51 +22,5 @@ describeWithMockConnection('NetworkLogView', () => {
     const expected =
         'curl \'https://www.example.com/file.html\' \\\n  -H \'header-with-value: some value\' \\\n  -H \'no-value-header;\' \\\n  --compressed';
     assert.strictEqual(actual, expected);
-  });
-
-  function createNetworkLogView(): Network.NetworkLogView.NetworkLogView {
-    return new Network.NetworkLogView.NetworkLogView(
-        {addFilter: () => {}, filterButton: () => ({addEventListener: () => {}})} as unknown as UI.FilterBar.FilterBar,
-        document.createElement('div'), Common.Settings.Settings.instance().createSetting('networkLogLargeRows', false));
-  }
-
-  it('adds dividers on main frame load events', async () => {
-    sinon.stub(Common.Settings.Settings, 'instance').returns({
-      createSetting: () => ({get: () => [], set: () => {}, addChangeListener: () => {}, setDisabled: () => {}}),
-      moduleSetting: () => ({get: () => [], set: () => {}, addChangeListener: () => {}, setDisabled: () => {}}),
-    } as unknown as Common.Settings.Settings);
-    sinon.stub(UI.ShortcutRegistry.ShortcutRegistry, 'instance').returns({
-      shortcutTitleForAction: () => {},
-      shortcutsForAction: () => [],
-    } as unknown as UI.ShortcutRegistry.ShortcutRegistry);
-
-    const networkLogView = createNetworkLogView();
-    const addEventDividers = sinon.spy(networkLogView.columns(), 'addEventDividers');
-    const tabTarget = createTarget({type: SDK.Target.Type.Tab});
-    const mainFrameUnderTabTarget = createTarget({parentTarget: tabTarget});
-    const mainFrameWithoutTabTarget = createTarget();
-    const subframeTarget = createTarget({parentTarget: mainFrameWithoutTabTarget});
-
-    const sendLoadEvents = (target: SDK.Target.Target, loadTime: number, domContentLoadTime: number) => {
-      const resourceTreeModel = target.model(SDK.ResourceTreeModel.ResourceTreeModel);
-      assertNotNullOrUndefined(resourceTreeModel);
-      resourceTreeModel.dispatchEventToListeners(SDK.ResourceTreeModel.Events.Load, {resourceTreeModel, loadTime});
-      resourceTreeModel.dispatchEventToListeners(SDK.ResourceTreeModel.Events.DOMContentLoaded, domContentLoadTime);
-    };
-    networkLogView.setRecording(true);
-
-    sendLoadEvents(subframeTarget, 1, 2);
-    assert.isTrue(addEventDividers.notCalled);
-
-    sendLoadEvents(mainFrameUnderTabTarget, 5, 6);
-    assert.isTrue(addEventDividers.calledTwice);
-    assert.isTrue(addEventDividers.getCall(0).calledWith([5], 'network-load-divider'));
-    assert.isTrue(addEventDividers.getCall(1).calledWith([6], 'network-dcl-divider'));
-
-    addEventDividers.resetHistory();
-    sendLoadEvents(mainFrameWithoutTabTarget, 3, 4);
-    assert.isTrue(addEventDividers.calledTwice);
-    assert.isTrue(addEventDividers.getCall(0).calledWith([3], 'network-load-divider'));
-    assert.isTrue(addEventDividers.getCall(1).calledWith([4], 'network-dcl-divider'));
   });
 });

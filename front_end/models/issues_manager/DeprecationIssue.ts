e07c0afcd66cbdc88535e253c371ee5c454ea6a8
Matt Wolenetz <wolenetz@chromium.org>
2022-10-12 12:40:18 -0700

    Describe the MSE AbortRemove change in DeprecationIssue.ts
    Bug: 1320345
    Change-Id: I3154f20c97f6cc3163b98338fe2b206b4ee01527
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/3946376
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Matthew Wolenetz <wolenetz@chromium.org>

--- front_end/models/issues_manager/DeprecationIssue.ts
+++ front_end/models/issues_manager/DeprecationIssue.ts
@@ -145,7 +145,31 @@ const UIStrings = {
   localCSSFileExtensionRejected:
       'CSS cannot be loaded from `file:` URLs unless they end in a `.css` file extension.',
   /**
-   * @description TODO(crbug.com/1320345): Description needed for translation
+   * @description This is a deprecation warning to developers that occurs when
+   * the script attempts to use the Media Source Extensions API in a way that
+   * is no longer supported by the specification for the API. The usage
+   * that is problematic is when the script calls the `SourceBuffer.abort()`
+   * method at a time when there is still processing happening in response to a
+   * previous `SourceBuffer.remove()` call for the same SourceBuffer object.
+   * More precisely, we show this warning to developers when script calls the
+   * SourceBuffer abort() method while the asynchronous processing of a remove()
+   * call on that SourceBuffer is not yet complete. Early versions of the Media
+   * Source Extensions specification allowed such aborts, but standardization of
+   * the specification resulted in disallowing the aborts. The script should
+   * instead wait for the asynchronous remove() operation to complete, which is
+   * observable by listening for the associated 'updateend' event from the
+   * SourceBuffer. A note is also included in the warning, describing when
+   * abort() is meaningful and allowed by the specification for purposes other
+   * than interrupting a remove() operation's asynchronous steps. Those
+   * supported purposes include using abort() to interrupt processing that may
+   * still be happening in response to a previous appendBuffer() call on that
+   * SourceBuffer, or using abort() to clear the internal of any unprocessed
+   * data remaining from previous appendBuffer() calls.
+   * See https://www.w3.org/TR/media-source-2/#dom-sourcebuffer-abort for the
+   * currently specified behavior, which would throw an exception once the
+   * deprecated removal abort is no longer supported.
+   * See https://github.com/w3c/media-source/issues/19 for the discussion that
+   * led to the specification change.
    */
   mediaSourceAbortRemove:
       'Using `SourceBuffer.abort()` to abort `remove()`\'s asynchronous range removal is deprecated due to specification change. Support will be removed in the future. You should listen to the `updateend` event instead. `abort()` is intended to only abort an asynchronous media append or reset parser state.',

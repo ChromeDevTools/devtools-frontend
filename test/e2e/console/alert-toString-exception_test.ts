447b9467926bb17ab3a49d63cc1d1186b41e8678
Al Muthanna Athamina <almuthanna@chromium.org>
2022-10-24 10:31:23 +0200

    [E2E] Convert alert-toString-exception Layout test to Mocha test
    Bug: chromium:1377770
    Change-Id: I0b4c6003e217906e8ba71ea6e2781a4d55aa1660
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/3966176
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Almothana Athamneh <almuthanna@chromium.org>

---
+++ test/e2e/console/alert-toString-exception_test.ts
@@ -0,0 +1,23 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {assert} from 'chai';
+
+import {
+  getBrowserAndPages,
+} from '../../shared/helper.js';
+import {describe, it} from '../../shared/mocha-extensions.js';
+import {getConsoleMessages, navigateToConsoleTab} from '../helpers/console-helpers.js';
+
+describe('The Console Tab', async () => {
+  it('Does not crash if it fails to convert alert() argument to string', async () => {
+    await navigateToConsoleTab();
+    const {target} = getBrowserAndPages();
+
+    target.reload();
+
+    const result = (await getConsoleMessages('alert-toString-exception'))[0];
+    assert.strictEqual(result, 'Uncaught Exception in toString().');
+  });
+});

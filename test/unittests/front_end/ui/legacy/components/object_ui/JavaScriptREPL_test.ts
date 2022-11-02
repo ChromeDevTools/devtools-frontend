438406dfade89fefe141a9b22c52d25baf119663
Benedikt Meurer <bmeurer@chromium.org>
2022-11-02 18:26:05 +0100

    [unittests] Migrate console-eval-object-literal.js web test.
    Bug: chromium:1246866
    Change-Id: I11d8e9da032b02554801a35cf194a4142158137c
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/3998575
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Auto-Submit: Benedikt Meurer <bmeurer@chromium.org>

--- test/unittests/front_end/ui/legacy/components/object_ui/JavaScriptREPL_test.ts
+++ test/unittests/front_end/ui/legacy/components/object_ui/JavaScriptREPL_test.ts
@@ -7,23 +7,36 @@ import * as ObjectUI from '../../../../../../../front_end/ui/legacy/components/o
 const {assert} = chai;
 
 describe('JavaScriptREPL', () => {
-  describe('preprocessExpression', () => {
-    it('wraps object literals in ()', () => {
-      const expr = '{a : 10}';
-      const processedExpr = ObjectUI.JavaScriptREPL.JavaScriptREPL.preprocessExpression(expr);
-      assert.strictEqual(`(${expr})`, processedExpr);
+  describe('wrapObjectLiteral', () => {
+    const {wrapObjectLiteral} = ObjectUI.JavaScriptREPL.JavaScriptREPL;
+
+    it('wraps simple object literals in parens', () => {
+      assert.strictEqual(wrapObjectLiteral('{a: 10}'), '({a: 10})');
+      assert.strictEqual(wrapObjectLiteral('{a:1, b:2}'), '({a:1, b:2})');
     });
 
-    it('ignores whitespace', () => {
-      const expr = ' \n {  a : 10  } \t ';
-      const processedExpr = ObjectUI.JavaScriptREPL.JavaScriptREPL.preprocessExpression(expr);
-      assert.strictEqual(`(${expr})`, processedExpr);
+    it('wraps nested object literals in parens', () => {
+      assert.strictEqual(wrapObjectLiteral('{x: {y: {z: 1}}}'), '({x: {y: {z: 1}}})');
+    });
+
+    it('leaves object literals with syntax errors untouched', () => {
+      assert.strictEqual(wrapObjectLiteral('{a: {}'), '{a: {}');
+      assert.strictEqual(wrapObjectLiteral('{a: [}]};'), '{a: [}]};');
+      assert.strictEqual(wrapObjectLiteral('{ a: 4 }),({ b: 7 }'), '{ a: 4 }),({ b: 7 }');
     });
 
     it('leaves blocks untouched', () => {
-      const expr = '{ a = 10 }';
-      const processedExpr = ObjectUI.JavaScriptREPL.JavaScriptREPL.preprocessExpression(expr);
-      assert.strictEqual(expr, processedExpr);
+      assert.strictEqual(wrapObjectLiteral('{a = 10}'), '{a = 10}');
+      assert.strictEqual(wrapObjectLiteral('{var a = 1; eval("{ a:1, b:2 }");}'), '{var a = 1; eval("{ a:1, b:2 }");}');
+      assert.strictEqual(wrapObjectLiteral('{ for (var i = 0; i < 5; ++i); }'), '{ for (var i = 0; i < 5; ++i); }');
+      assert.strictEqual(wrapObjectLiteral('{ let a = 4; a; }'), '{ let a = 4; a; }');
+      assert.strictEqual(wrapObjectLiteral('{ let a = 4; }; { let b = 5; };'), '{ let a = 4; }; { let b = 5; };');
+    });
+
+    it('leaves expressions untouched', () => {
+      assert.strictEqual(wrapObjectLiteral('{} + []'), '{} + []');
+      assert.strictEqual(wrapObjectLiteral('{} + 2'), '{} + 2');
+      assert.strictEqual(wrapObjectLiteral('{a: 1} + 2'), '{a: 1} + 2');
     });
   });
 });

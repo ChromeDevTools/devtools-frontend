855d0d2613ed99fdd22db0821c64da3fcbf4ac8d
Simon Zünd <szuend@chromium.org>
2022-11-21 11:15:47 +0100

    [l10n] Turn en-US.json and en-XL.json into build output
    This CL introduces a variant of `collect-strings.js` that can be
    invoked from GN and allows more options to be configured via command
    line arguments. Also the new variant skips writing the *.ctc.json files
    as they are only required for translation but not during runtime.
    
    We can't modify the existing `collect-strings.js` as it's still used
    by PRESUBMIT.py and some other l10n infra scripts.
    
    Note that we don't use the generated files just yet. Replacing the
    in-tree en-US.json/en-XL.json with these generated ones will happen
    later, once we also have implemented minification of the generated
    en-US/en-XL.json.
    
    Verified locally that:
      * The output matches exactly the in-tree version (via sha256sum)
      * The build works in a standalone and Chromium integrated build
    
    R=jacktfranklin@chromium.org, kimanh@chromium.org
    
    Bug: 1185727
    Change-Id: I66c8ca2b7d8f97d554921daa1141ea4b1ce11580
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4040927
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Jack Franklin <jacktfranklin@chromium.org>
    Reviewed-by: Kim-Anh Tran <kimanh@chromium.org>

---
+++ front_end/core/i18n/collect-ui-strings.js
@@ -0,0 +1,65 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+const path = require('path');
+
+const {collectAllStringsInDir, createPsuedoLocaleStrings} = require('../../../third_party/i18n/collect-strings.js');
+const {bakePlaceholders} = require('../../../third_party/i18n/bake-ctc-to-lhl.js');
+const {writeIfChanged} = require('../../../scripts/build/ninja/write-if-changed.js');
+
+/** @typedef {import('../../../third_party/i18n/bake-ctc-to-lhl.js').CtcMessage} CtcMessage */
+
+const yargsObject = require('yargs')
+                        .option('input-directories', {
+                          type: 'array',
+                          demandOption: true,
+                        })
+                        .option('output-directory', {
+                          type: 'string',
+                          demandOption: true,
+                        })
+                        .option('include-en-xl', {
+                          type: 'boolean',
+                        })
+                        .strict()
+                        .argv;
+
+/**
+ * @param {string} outputDirectory
+ * @param {string} locale
+ * @param {Record<string, CtcMessage>} strings
+ */
+function convertCtcToLhLAndSave(outputDirectory, locale, strings) {
+  const outputPath = path.join(outputDirectory, `${locale}.json`);
+
+  /** @type {Record<string, CtcMessage>} */
+  const sortedCtcStrings = {};
+  const sortedEntries = Object.entries(strings).sort(([keyA], [keyB]) => keyA.localeCompare(keyB));
+  for (const [key, defn] of sortedEntries) {
+    sortedCtcStrings[key] = defn;
+  }
+
+  const convertedStrings = bakePlaceholders(sortedCtcStrings);
+  writeIfChanged(outputPath, JSON.stringify(convertedStrings, null, 2) + '\n');
+}
+
+const inputDirectories = yargsObject['input-directories'];
+if (inputDirectories.length === 0) {
+  throw new Error('Provide at least one directory!');
+}
+
+/** @type {Record<string, CtcMessage} */
+let collectedStrings = {};
+for (const directory of inputDirectories) {
+  collectedStrings = {
+    ...collectedStrings,
+    ...collectAllStringsInDir(directory),
+  };
+}
+
+const outputDirectory = yargsObject['output-directory'];
+convertCtcToLhLAndSave(outputDirectory, 'en-US', collectedStrings);
+if (yargsObject['include-en-xl']) {
+  convertCtcToLhLAndSave(outputDirectory, 'en-XL', createPsuedoLocaleStrings(collectedStrings));
+}

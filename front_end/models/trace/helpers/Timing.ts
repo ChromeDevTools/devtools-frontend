1953ca12b372001dd9c48807a58bc73bc6eb3b24
Jack Franklin <jacktfranklin@chromium.org>
2022-11-30 11:42:33 +0000

    Add time formatting helpers to TraceModel
    Bug: 138602
    Change-Id: I7179e20e3de3b75f265555e97dddab20f5f3df23
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4066041
    Auto-Submit: Jack Franklin <jacktfranklin@chromium.org>
    Commit-Queue: Nancy Li <nancyly@chromium.org>
    Reviewed-by: Nancy Li <nancyly@chromium.org>

--- front_end/models/trace/helpers/Timing.ts
+++ front_end/models/trace/helpers/Timing.ts
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import * as Platform from '../../../core/platform/platform.js';
 import * as Types from '../types/types.js';
 
 export const millisecondsToMicroseconds = (value: Types.Timing.MilliSeconds): Types.Timing.MicroSeconds =>
@@ -12,3 +13,100 @@ export const secondsToMilliseconds = (value: Types.Timing.Seconds): Types.Timing
 
 export const secondsToMicroseconds = (value: Types.Timing.Seconds): Types.Timing.MicroSeconds =>
     millisecondsToMicroseconds(secondsToMilliseconds(value));
+
+export function detectBestTimeUnit(timeInMicroseconds: Types.Timing.MicroSeconds): Types.Timing.TimeUnit {
+  if (timeInMicroseconds < 1000) {
+    return Types.Timing.TimeUnit.MICROSECONDS;
+  }
+
+  const timeInMilliseconds = timeInMicroseconds / 1000;
+  if (timeInMilliseconds < 1000) {
+    return Types.Timing.TimeUnit.MILLISECONDS;
+  }
+
+  const timeInSeconds = timeInMilliseconds / 1000;
+  if (timeInSeconds < 60) {
+    return Types.Timing.TimeUnit.SECONDS;
+  }
+
+  return Types.Timing.TimeUnit.MINUTES;
+}
+
+interface FormatOptions extends Intl.NumberFormatOptions {
+  format?: Types.Timing.TimeUnit;
+}
+
+const defaultFormatOptions = {
+  style: 'unit',
+  unit: 'millisecond',
+  unitDisplay: 'narrow',
+};
+
+// Create a bunch of common formatters up front, so that we're not creating
+// them repeatedly during rendering.
+const serialize = (value: {}): string => JSON.stringify(value);
+const formatterFactory = (key: string|undefined): Intl.NumberFormat => {
+  return new Intl.NumberFormat(navigator.language, key ? JSON.parse(key) : {});
+};
+const formatters = new Map<string, Intl.NumberFormat>();
+
+// Microsecond Formatter.
+Platform.MapUtilities.getWithDefault(formatters, serialize({style: 'decimal'}), formatterFactory);
+
+// Millisecond Formatter
+Platform.MapUtilities.getWithDefault(formatters, serialize(defaultFormatOptions), formatterFactory);
+
+// Second Formatter
+Platform.MapUtilities.getWithDefault(
+    formatters, serialize({...defaultFormatOptions, unit: 'second'}), formatterFactory);
+
+// Minute Formatter
+Platform.MapUtilities.getWithDefault(
+    formatters, serialize({...defaultFormatOptions, unit: 'minute'}), formatterFactory);
+
+export function formatMicrosecondsTime(
+    timeInMicroseconds: Types.Timing.MicroSeconds, opts: FormatOptions = {}): string {
+  if (!opts.format) {
+    opts.format = detectBestTimeUnit(timeInMicroseconds);
+  }
+
+  const timeInMilliseconds = timeInMicroseconds / 1000;
+  const timeInSeconds = timeInMilliseconds / 1000;
+  const formatterOpts = {...defaultFormatOptions, ...opts};
+
+  switch (opts.format) {
+    case Types.Timing.TimeUnit.MICROSECONDS: {
+      const formatter =
+          Platform.MapUtilities.getWithDefault(formatters, serialize({style: 'decimal'}), formatterFactory);
+      return `${formatter.format(timeInMicroseconds)}μs`;
+    }
+
+    case Types.Timing.TimeUnit.MILLISECONDS: {
+      const formatter = Platform.MapUtilities.getWithDefault(formatters, serialize(formatterOpts), formatterFactory);
+      return formatter.format(timeInMilliseconds);
+    }
+
+    case Types.Timing.TimeUnit.SECONDS: {
+      const formatter = Platform.MapUtilities.getWithDefault(
+          formatters, serialize({...formatterOpts, unit: 'second'}), formatterFactory);
+      return formatter.format(timeInSeconds);
+    }
+
+    default: {
+      // Switch to mins & seconds.
+      const minuteFormatter = Platform.MapUtilities.getWithDefault(
+          formatters, serialize({...formatterOpts, unit: 'minute'}), formatterFactory);
+      const secondFormatter = Platform.MapUtilities.getWithDefault(
+          formatters, serialize({...formatterOpts, unit: 'second'}), formatterFactory);
+      const timeInMinutes = timeInSeconds / 60;
+      const [mins, divider, fraction] = minuteFormatter.formatToParts(timeInMinutes);
+
+      let seconds = 0;
+      if (divider && fraction) {
+        // Convert the fraction value (a string) to the nearest second.
+        seconds = Math.round(Number(`0.${fraction.value}`) * 60);
+      }
+      return `${minuteFormatter.format(Number(mins.value))} ${secondFormatter.format(seconds)}`;
+    }
+  }
+}
--- test/unittests/front_end/models/trace/helpers/Timing_test.ts
+++ test/unittests/front_end/models/trace/helpers/Timing_test.ts
@@ -5,25 +5,78 @@
 import * as TraceModel from '../../../../../../front_end/models/trace/trace.js';
 
 const {assert} = chai;
+function milliToMicro(value: number) {
+  return TraceModel.Types.Timing.MicroSeconds(value * 1000);
+}
+function secToMicro(value: TraceModel.Types.Timing.Seconds): TraceModel.Types.Timing.MicroSeconds {
+  return milliToMicro(value * 1000);
+}
 
-describe('Timing conversions', () => {
-  it('can convert milliseconds to microseconds', () => {
-    const input = TraceModel.Types.Timing.MilliSeconds(1);
-    const expected = TraceModel.Types.Timing.MicroSeconds(1000);
-    assert.strictEqual(TraceModel.Helpers.Timing.millisecondsToMicroseconds(input), expected);
+describe('Timing helpers', () => {
+  describe('Timing conversions', () => {
+    it('can convert milliseconds to microseconds', () => {
+      const input = TraceModel.Types.Timing.MilliSeconds(1);
+      const expected = TraceModel.Types.Timing.MicroSeconds(1000);
+      assert.strictEqual(TraceModel.Helpers.Timing.millisecondsToMicroseconds(input), expected);
+    });
+
+    it('can convert seconds to milliseconds', () => {
+      const input = TraceModel.Types.Timing.Seconds(1);
+      const expected = TraceModel.Types.Timing.MilliSeconds(1000);
+      assert.strictEqual(TraceModel.Helpers.Timing.secondsToMilliseconds(input), expected);
+    });
+
+    it('can convert seconds to microseconds', () => {
+      const input = TraceModel.Types.Timing.Seconds(1);
+      // 1 Second = 1000 Milliseconds
+      // 1000 Milliseconds = 1,000,000 Microseconds
+      const expected = TraceModel.Types.Timing.MicroSeconds(1_000_000);
+      assert.strictEqual(TraceModel.Helpers.Timing.secondsToMicroseconds(input), expected);
+    });
+  });
+  describe('detectBestTimeUnit', () => {
+    it('detects microseconds', () => {
+      const time = TraceModel.Types.Timing.MicroSeconds(890);
+      assert.strictEqual(
+          TraceModel.Helpers.Timing.detectBestTimeUnit(time), TraceModel.Types.Timing.TimeUnit.MICROSECONDS);
+    });
+
+    it('detects milliseconds', () => {
+      const time = milliToMicro(8.9122);
+      assert.strictEqual(
+          TraceModel.Helpers.Timing.detectBestTimeUnit(time), TraceModel.Types.Timing.TimeUnit.MILLISECONDS);
+    });
+
+    it('detects seconds', () => {
+      const time = secToMicro(TraceModel.Types.Timing.Seconds(8.9134));
+      assert.strictEqual(TraceModel.Helpers.Timing.detectBestTimeUnit(time), TraceModel.Types.Timing.TimeUnit.SECONDS);
+    });
+
+    it('detects minutes', () => {
+      const time = secToMicro(TraceModel.Types.Timing.Seconds(203));  // 3 mins, 23 sec in seconds.
+      assert.strictEqual(TraceModel.Helpers.Timing.detectBestTimeUnit(time), TraceModel.Types.Timing.TimeUnit.MINUTES);
+    });
   });
 
-  it('can convert seconds to milliseconds', () => {
-    const input = TraceModel.Types.Timing.Seconds(1);
-    const expected = TraceModel.Types.Timing.MilliSeconds(1000);
-    assert.strictEqual(TraceModel.Helpers.Timing.secondsToMilliseconds(input), expected);
-  });
+  describe('formatTime', () => {
+    it('formats microseconds', () => {
+      const time = TraceModel.Types.Timing.MicroSeconds(890);
+      assert.strictEqual(TraceModel.Helpers.Timing.formatMicrosecondsTime(time), '890μs');
+    });
 
-  it('can convert seconds to microseconds', () => {
-    const input = TraceModel.Types.Timing.Seconds(1);
-    // 1 Second = 1000 Milliseconds
-    // 1000 Milliseconds = 1,000,000 Microseconds
-    const expected = TraceModel.Types.Timing.MicroSeconds(1_000_000);
-    assert.strictEqual(TraceModel.Helpers.Timing.secondsToMicroseconds(input), expected);
+    it('formats milliseconds', () => {
+      const time = milliToMicro(8.9122);
+      assert.strictEqual(TraceModel.Helpers.Timing.formatMicrosecondsTime(time), '8.912ms');
+    });
+
+    it('formats seconds', () => {
+      const time = secToMicro(TraceModel.Types.Timing.Seconds(8.9134));
+      assert.strictEqual(TraceModel.Helpers.Timing.formatMicrosecondsTime(time), '8.913s');
+    });
+
+    it('formats minutes', () => {
+      const time = secToMicro(TraceModel.Types.Timing.Seconds(203));  // 3 mins, 23 sec in seconds.
+      assert.strictEqual(TraceModel.Helpers.Timing.formatMicrosecondsTime(time), '3m 23s');
+    });
   });
 });

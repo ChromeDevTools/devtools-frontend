6e7543691f60a2556407c151f1cfa0936d41ea7e
Vladimir Levin <vmpstr@chromium.org>
2022-12-05 15:15:23 -0500

    VT: Update page-transition to view-transition in a DCHECK.
    This patch updates the DCHECK to check the correct view-transition
    pseudo prefix.
    
    R=changhaohan@chromium.org
    
    Fixed: 1356613
    Change-Id: I3deb09cfeeff02a34ee997ffe553b0e2b6b749ca
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4080429
    Reviewed-by: Changhao Han <changhaohan@chromium.org>
    Commit-Queue: Changhao Han <changhaohan@chromium.org>
    Commit-Queue: Vladimir Levin <vmpstr@chromium.org>

--- front_end/core/sdk/DOMModel.ts
+++ front_end/core/sdk/DOMModel.ts
@@ -1390,8 +1390,8 @@ export class DOMModel extends SDKModel<EventTypes> {
     const currentPseudoElements = parent.pseudoElements().get(pseudoType);
     if (currentPseudoElements) {
       Platform.DCHECK(
-          () => pseudoType.startsWith('page-transition'),
-          'DOMModel.pseudoElementAdded expects parent to not already have this pseudo type added; only page-transition* pseudo elements can coexist under the same parent.');
+          () => pseudoType.startsWith('view-transition'),
+          'DOMModel.pseudoElementAdded expects parent to not already have this pseudo type added; only view-transition* pseudo elements can coexist under the same parent.');
       currentPseudoElements.push(node);
     } else {
       parent.pseudoElements().set(pseudoType, [node]);

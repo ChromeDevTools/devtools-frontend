e7dd05f62e5dae1d75a23c893c198f8a961dd071
Benedikt Meurer <bmeurer@chromium.org>
2022-12-13 10:22:18 +0100

    [snippets] Enable JavaScript autocompletion.
    Snippets are supposed to be run in the current context, basically like a
    stashed version of JavaScript code that you'd otherwise enter via the
    Console. Therefore it makes sense to provide the same autocompletion
    results for Snippets that we provide for the Console.
    
    Before: https://imgur.com/8dA6NQS.png
    After: https://imgur.com/6OdVm8l.png
    Fixed: chromium:772949
    Change-Id: I5bbcad8c2c723f6350edf38a52ff47d45a25940b
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4100765
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Commit-Queue: Benedikt Meurer <bmeurer@chromium.org>
    Auto-Submit: Benedikt Meurer <bmeurer@chromium.org>
    Commit-Queue: Yang Guo <yangguo@chromium.org>

--- front_end/panels/sources/SnippetsPlugin.ts
+++ front_end/panels/sources/SnippetsPlugin.ts
@@ -5,7 +5,9 @@
 import * as Host from '../../core/host/host.js';
 import * as i18n from '../../core/i18n/i18n.js';
 import type * as Workspace from '../../models/workspace/workspace.js';
+import type * as CodeMirror from '../../third_party/codemirror.next/codemirror.next.js';
 import * as UI from '../../ui/legacy/legacy.js';
+import * as TextEditor from '../../ui/components/text_editor/text_editor.js';
 import * as Snippets from '../snippets/snippets.js';
 
 import {Plugin} from './Plugin.js';
@@ -33,5 +35,9 @@ export class SnippetsPlugin extends Plugin {
     runSnippet.setText(Host.Platform.isMac() ? i18nString(UIStrings.enter) : i18nString(UIStrings.ctrlenter));
 
     return [runSnippet];
+  }
+
+  editorExtension(): CodeMirror.Extension {
+    return TextEditor.JavaScript.completion();
   }
 }

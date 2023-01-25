839b02d036e4bee786fd1f9f043b5fce6fc0da6c
Al Muthanna Athamina <almuthanna@chromium.org>
2023-01-25 17:00:25 +0100

    [E2E] Convert switch-panels-while-editing-as-html Layout test to Mocha test
    Bug: chromium:1377770
    Change-Id: Ic599a4c7df11ac8940121bbe28d95b17342f2633
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4194332
    Commit-Queue: Almothana Athamneh <almuthanna@chromium.org>
    Reviewed-by: Danil Somsikov <dsv@chromium.org>

---
+++ test/e2e/elements/switch-panels-while-editing-as-html_test.ts
@@ -0,0 +1,28 @@
+// Copyright 2021 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {goToResource, waitFor, waitForElementWithTextContent, waitForNone} from '../../shared/helper.js';
+import {describe, it} from '../../shared/mocha-extensions.js';
+import {findSubMenuEntryItem} from '../helpers/context-menu-helpers.js';
+import {
+  expandSelectedNodeRecursively,
+  navigateToElementsTab,
+} from '../helpers/elements-helpers.js';
+import {openSourcesPanel} from '../helpers/sources-helpers.js';
+
+describe('The Elements tab', async function() {
+  it('does not break when switching panels while editing as HTML', async () => {
+    await goToResource('elements/switch-panels-while-editing-as-html.html');
+    await expandSelectedNodeRecursively();
+    const elementsContentPanel = await waitFor('#elements-content');
+    const selectedNode = await waitForElementWithTextContent('Inspected Node', elementsContentPanel);
+    await selectedNode.click({button: 'right'});
+    const editAsHTMLOption = await findSubMenuEntryItem('Edit as HTML');
+    await editAsHTMLOption.click();
+    await waitFor('.elements-disclosure devtools-text-editor');
+    await openSourcesPanel();
+    await navigateToElementsTab();
+    await waitForNone('.elements-disclosure devtools-text-editor');
+  });
+});

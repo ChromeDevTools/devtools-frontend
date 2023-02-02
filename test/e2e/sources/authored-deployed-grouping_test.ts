b98a529c2c9f76857bfde8af43161f0e8f3a74e4
Wolfgang Beyer <wolfi@chromium.org>
2023-02-02 09:32:20 +0000

    [e2e] Skip flaky test on mac
    Bug: 1412336
    Change-Id: Ide6acfba651c288863d1dc3f5c8af0a5f1d4b118
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4217073
    Commit-Queue: Danil Somsikov <dsv@chromium.org>
    Auto-Submit: Wolfgang Beyer <wolfi@chromium.org>
    Reviewed-by: Danil Somsikov <dsv@chromium.org>

--- test/e2e/sources/authored-deployed-grouping_test.ts
+++ test/e2e/sources/authored-deployed-grouping_test.ts
@@ -269,7 +269,8 @@ describe('Source Panel grouping', async function() {
     await validateNavigationTree();
   }
 
-  it('can enable and disable group by authored/deployed', async () => {
+  // Flaky on mac
+  it.skipOnPlatforms(['mac'], '[crbug.com/1412336] can enable and disable group by authored/deployed', async () => {
     // Have the target load the page.
     await goToResource(targetPage);
     await openSourcesPanel();

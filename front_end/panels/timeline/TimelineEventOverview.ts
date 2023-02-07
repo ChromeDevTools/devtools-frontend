8a3f1fbb46e250e86886b503e7fb6058ee98b1c6
Nancy Li <nancyly@chromium.org>
2023-02-07 18:06:10 +0000

    [Perf Panel] Change the CPU track display
    - When draw the CPU usage, make the idle events to be count as |Idle| rather than |other| (|other| is used for system nodes, see |TimelineUIUtils.categories()|)
    
    This CL will affect both web and node. And it is originally for node user, so guarded it with the |timelineDoNotSkipSystemNodesOfCpuProfile| flag.
    
    Bug: 1406240
    Change-Id: I07843b867bb484f1fde6fff3f8ebc544f0977c74
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4224365
    Reviewed-by: Jack Franklin <jacktfranklin@chromium.org>
    Commit-Queue: Nancy Li <nancyly@chromium.org>

--- front_end/panels/timeline/TimelineEventOverview.ts
+++ front_end/panels/timeline/TimelineEventOverview.ts
@@ -35,6 +35,7 @@ import * as TimelineModel from '../../models/timeline_model/timeline_model.js';
 import * as PerfUI from '../../ui/legacy/components/perf_ui/perf_ui.js';
 import * as UI from '../../ui/legacy/legacy.js';
 import * as Coverage from '../coverage/coverage.js';
+import * as Root from '../../core/root/root.js';
 import * as Protocol from '../../generated/protocol.js';
 
 import {type PerformanceModel} from './PerformanceModel.js';
@@ -222,7 +223,7 @@ export class TimelineEventOverviewCPUActivity extends TimelineEventOverview {
     const otherIndex = categoryOrder.indexOf('other');
     const idleIndex = 0;
     console.assert(idleIndex === categoryOrder.indexOf('idle'));
-    for (let i = idleIndex + 1; i < categoryOrder.length; ++i) {
+    for (let i = 0; i < categoryOrder.length; ++i) {
       categoryToIndex.set(categories[categoryOrder[i]], i);
     }
 
@@ -265,7 +266,12 @@ export class TimelineEventOverviewCPUActivity extends TimelineEventOverview {
       function onEventStart(e: SDK.TracingModel.Event): void {
         const index = categoryIndexStack.length ? categoryIndexStack[categoryIndexStack.length - 1] : idleIndex;
         quantizer.appendInterval(e.startTime, (index as number));
-        categoryIndexStack.push(categoryToIndex.get(TimelineUIUtils.eventStyle(e).category) || otherIndex);
+        const categoryIndex = categoryToIndex.get(TimelineUIUtils.eventStyle(e).category);
+        if (Root.Runtime.experiments.isEnabled('timelineDoNotSkipSystemNodesOfCpuProfile')) {
+          categoryIndexStack.push(categoryIndex !== undefined ? categoryIndex : otherIndex);
+        } else {
+          categoryIndexStack.push(categoryIndex || otherIndex);
+        }
       }
 
       function onEventEnd(e: SDK.TracingModel.Event): void {

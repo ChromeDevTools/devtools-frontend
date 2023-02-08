486c4e198af6265c9ee86031f9301fa798475b29
Kim-Anh Tran <kimanh@chromium.org>
2023-02-08 12:37:35 +0100

    [e2e] Disable flaky script-in-multiple-workers_test on all platforms
    Bug: 1368493
    Change-Id: I29d4e45d5ad886e1c85a11fe182b3b891bc4e8c5
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4231974
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Auto-Submit: Kim-Anh Tran <kimanh@chromium.org>
    Reviewed-by: Simon Zünd <szuend@chromium.org>

--- test/e2e/sources/script-in-multiple-workers_test.ts
+++ test/e2e/sources/script-in-multiple-workers_test.ts
@@ -240,8 +240,8 @@ describe('Multi-Workers', async function() {
         await validateSourceTabs();
       });
 
-      // Flaky on mac
-      it.skipOnPlatforms(['mac'], '[crbug.com/1368493] for newly created workers', async () => {
+      // Flaky test.
+      it.skip('[crbug.com/1368493] for newly created workers', async () => {
         const {target} = getBrowserAndPages();
         // Launch new worker to hit breakpoint
         await target.evaluate(`new Worker('${scriptFile}').postMessage({});`);

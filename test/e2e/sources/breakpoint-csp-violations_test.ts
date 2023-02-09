75cc5a570307904a0ebbaafba01a126713ebd8d3
Al Muthanna Athamina <almuthanna@chromium.org>
2023-02-09 14:04:37 +0100

    [e2e] Deflake CSP Violation tests on Mac
    Bug: 1413970
    Cq-Include-Trybots: luci.devtools-frontend.try:devtools_frontend_mac_rel
    Change-Id: I5f1383cd68bc006ef82927afb3beeccde92b1319
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4231015
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Almothana Athamneh <almuthanna@chromium.org>

--- test/e2e/sources/breakpoint-csp-violations_test.ts
+++ test/e2e/sources/breakpoint-csp-violations_test.ts
@@ -4,14 +4,14 @@
 
 import {assert} from 'chai';
 
-import {click, goToResource} from '../../shared/helper.js';
+import {click, goToResource, waitForAria} from '../../shared/helper.js';
 import {describe, it} from '../../shared/mocha-extensions.js';
 import {getPausedMessages, openSourcesPanel, PAUSE_ON_UNCAUGHT_EXCEPTION_SELECTOR} from '../helpers/sources-helpers.js';
 
 describe('Breakpoints on CSP Violation', async () => {
-  // Flaky on mac.
-  it.skipOnPlatforms(['mac'], '[crbug.com/1413970] CSP Violations should come up before break on exceptions', async () => {
+  it('CSP Violations should come up before break on exceptions', async () => {
     await openSourcesPanel();
+    await waitForAria('CSP Violation Breakpoints');
     await click('[aria-label="CSP Violation Breakpoints"]');
     await click('[aria-label="Trusted Type Violations"]');
     await click(PAUSE_ON_UNCAUGHT_EXCEPTION_SELECTOR);
@@ -33,9 +33,9 @@ describe('Breakpoints on CSP Violation', async () => {
     await resource;
   });
 
-  // Flaky on mac
-  it.skipOnPlatforms(['mac'], '[crbug.com/1413970] CSP Violations should show in report-only mode', async () => {
+  it('CSP Violations should show in report-only mode', async () => {
     await openSourcesPanel();
+    await waitForAria('CSP Violation Breakpoints');
     await click('[aria-label="CSP Violation Breakpoints"]');
     await click('[aria-label="Trusted Type Violations"]');
 

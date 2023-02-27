6aaeaf75a63758a777d9db53598d9ba1a227bbc8
Al Muthanna Athamina <almuthanna@chromium.org>
2023-02-27 14:56:16 +0100

    Change bug for animation panel skipped test
    Bug: 1418288
    Change-Id: I1c30a6e7a7ab43c896eadbbe397423ddc3eac5ac
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4279227
    Reviewed-by: Philip Pfaffe <pfaffe@chromium.org>
    Commit-Queue: Philip Pfaffe <pfaffe@chromium.org>
    Auto-Submit: Almothana Athamneh <almuthanna@chromium.org>
    Commit-Queue: Almothana Athamneh <almuthanna@chromium.org>

--- test/e2e/animations/animations_test.ts
+++ test/e2e/animations/animations_test.ts
@@ -10,8 +10,9 @@ import {
 } from '../helpers/animations-helpers.js';
 
 describe('The Animations Panel', async () => {
-  // Inconsistent behavior on Animations panel causes tets to be flaky
-  it.skip('[crbug.com/941646] Listens for animation in webpage', async () => {
+  // test is flaky because the animation preview button appears before it is
+  // fully functional.
+  it.skip('[crbug.com/1419862] Listens for animation in webpage', async () => {
     await waitForAnimationsPanelToLoad();
     await navigateToSiteWithAnimation();
     await waitForAnimationContent();

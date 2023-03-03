c0e707bf563029b8ff25ecc5c7b2c86f0e33933c
Andrés Olivares <andoli@chromium.org>
2023-03-03 07:16:19 +0000

    [RPP] Report interaction tests files to coverage instrumentation
    Adds the examples used in the performance panel interaction test to the preload cache for code coverage.
    
    Bug: none
    Change-Id: I1494844f270fb5b866a366e6206a66f29c059086
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4295977
    Commit-Queue: Andres Olivares <andoli@chromium.org>
    Reviewed-by: Jack Franklin <jacktfranklin@chromium.org>

--- test/interactions/panels/performance/timeline/timeline_test.ts
+++ test/interactions/panels/performance/timeline/timeline_test.ts
@@ -5,13 +5,16 @@
 import {waitFor} from '../../../../shared/helper.js';
 import {describe} from '../../../../shared/mocha-extensions.js';
 import {assertElementScreenshotUnchanged, itScreenshot} from '../../../../shared/screenshots.js';
-import {loadComponentDocExample} from '../../../helpers/shared.js';
+import {loadComponentDocExample, preloadForCodeCoverage} from '../../../helpers/shared.js';
 
 describe('Performance panel', () => {
+  preloadForCodeCoverage('performance_panel/basic.html');
+  preloadForCodeCoverage('performance_panel/liviu.html');
+
   // Disabled until screenshot tests infrastructure is available in DevTools bots.
   itScreenshot.skip('[crbug.com/1407638] renders the timeline correctly', async () => {
     await loadComponentDocExample('performance_panel/basic.html?trace=animation');
-    await waitFor('#timeline-overview-panel');
+    await waitFor('.timeline-flamechart');
     const panel = await waitFor('body');
     await assertElementScreenshotUnchanged(panel, 'performance/timeline.png');
   });

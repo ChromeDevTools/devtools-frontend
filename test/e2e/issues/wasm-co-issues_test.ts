6c2da0e4855b86c5487a037034f1cf1f5c6f5cf5
Randolf <jrandolf@chromium.org>
2023-03-06 14:14:50 +0000

    Fix Issue panel flake.
    This PR updates the WASM issue test:
    
     - Cross-origin sharing of WASM modules is deprecated (https://developer.chrome.com/blog/wasm-module-sharing-restricted-to-same-origin/).
    
    Fixed: 1247980
    Change-Id: I93c998cc6c3e0139e5979406cdad3f549a9a1c04
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4310481
    Commit-Queue: Randolf Jung <jrandolf@chromium.org>
    Reviewed-by: Eric Leese <leese@chromium.org>

--- test/e2e/issues/wasm-co-issues_test.ts
+++ test/e2e/issues/wasm-co-issues_test.ts
@@ -1,49 +1,21 @@
-// Copyright 2021 The Chromium Authors. All rights reserved.
+// Copyright 2023 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {assert} from 'chai';
+// Copyright 2021 The Chromium Authors. All rights reserved.
 
-import {assertNotNullOrUndefined, goToResourceWithCustomHost} from '../../shared/helper.js';
+import {goToResourceWithCustomHost} from '../../shared/helper.js';
 import {describe, it} from '../../shared/mocha-extensions.js';
-import {
-  ensureResourceSectionIsExpanded,
-  expandIssue,
-  getIssueByTitle,
-  getResourcesElement,
-  navigateToIssuesTab,
-  waitForTableFromResourceSectionContents,
-} from '../helpers/issues-helpers.js';
+import {expandIssue, getIssueByTitle, navigateToIssuesTab} from '../helpers/issues-helpers.js';
 
 describe('Wasm cross-origin sharing issue', async () => {
   beforeEach(async () => {
     await goToResourceWithCustomHost('a.devtools.test', 'issues/wasm-co-sharing.html');
   });
 
-  // Disabled until wasm module sharing is reenabled.
-  it.skip('[crbug.com/1247980] should appear when cross-origin sharing a wasm module', async () => {
+  it('should be deprecated when cross-origin sharing a wasm module', async () => {
     await navigateToIssuesTab();
     await expandIssue();
-    const issueElement = await getIssueByTitle('Share WebAssembly modules only between same-origin environments');
-    assertNotNullOrUndefined(issueElement);
-    const section = await getResourcesElement('module', issueElement);
-    const text = await section.label.evaluate(el => el.textContent);
-    assert.strictEqual(text, '1 module');
-    await ensureResourceSectionIsExpanded(section);
-    const expectedTableRows = [
-      [
-        'Wasm Module URL',
-        'Source Origin',
-        'Target Origin',
-        'Status',
-      ],
-      [
-        /.*a.devtools.test.*wasm/,
-        /.*a.devtools.test.*/,
-        /.*b.devtools.test.*/,
-        /warning|blocked/,
-      ],
-    ];
-    await waitForTableFromResourceSectionContents(section.content, expectedTableRows);
+    await getIssueByTitle('Deprecated Feature Used');
   });
 });

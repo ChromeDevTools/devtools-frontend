f3609112e484da12959e5539f8d80c78bf212261
Nancy Li <nancyly@chromium.org>
2023-03-11 00:06:52 +0000

    Revert "Add `run` to mutex."
    This reverts commit 6add8a58f782253c2c280a7d50fd05e0f6dd712a.
    
    Reason for revert: potentially breaks the "DevTools Linux"
    
    Original change's description:
    > Add `run` to mutex.
    >
    > `run` acts like a C++ [`lock_guard`](https://en.cppreference.com/w/cpp/thread/lock_guard) which allows safer usage of mutexes. This CL also prevents release from being called more than once.
    >
    > Bug: none
    > Change-Id: Ice16d3813a3eb42485dd442cbf3c8887e66afaad
    > Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4319349
    > Reviewed-by: Simon Zünd <szuend@chromium.org>
    
    Bug: none
    Change-Id: Icfd5dd7cdd5acb1ff34b14912f1f66cf580d84b5
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4330676
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: Nancy Li <nancyly@chromium.org>

--- front_end/core/common/Mutex.ts
+++ front_end/core/common/Mutex.ts
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-type ReleaseFunction = () => void;
+type ReleaseFn = () => void;
 
 /**
  * Use Mutex class to coordinate local concurrent operations.
@@ -12,45 +12,31 @@ type ReleaseFunction = () => void;
  */
 export class Mutex {
   #locked = false;
-  #acquirers: Array<() => void> = [];
+  #acquiringQueue: Array<(release: ReleaseFn) => void> = [];
 
-  // This is FIFO.
-  acquire(): Promise<ReleaseFunction> {
-    let promise: Promise<void>;
-    if (!this.#locked) {
-      this.#locked = true;
-      promise = Promise.resolve();
-    } else {
-      promise = new Promise<void>(resolve => {
-        this.#acquirers.push(resolve);
-      });
-    }
-    return promise.then(() => this.#release.bind(this, {resolved: false}));
+  acquire(): Promise<ReleaseFn> {
+    let resolver = (_release: ReleaseFn): void => {};
+    const promise = new Promise<ReleaseFn>((resolve): void => {
+      resolver = resolve;
+    });
+    this.#acquiringQueue.push(resolver);
+    this.#processAcquiringQueue();
+    return promise;
   }
 
-  #release(state: {resolved: boolean}): void {
-    if (state.resolved) {
-      throw new Error('Cannot release more than once.');
-    }
-    state.resolved = true;
-
-    const resolve = this.#acquirers.shift();
-    if (!resolve) {
-      this.#locked = false;
+  #processAcquiringQueue(): void {
+    if (this.#locked) {
       return;
     }
-    resolve();
+    const nextAquirePromise = this.#acquiringQueue.shift();
+    if (nextAquirePromise) {
+      this.#locked = true;
+      nextAquirePromise(this.#release.bind(this));
+    }
   }
 
-  async run<T>(action: () => Promise<T>): Promise<T> {
-    const release = await this.acquire();
-    try {
-      // Note we need to await here because we want the await to release AFTER
-      // that await happens. Returning action() will trigger the release
-      // immediately which is counter to what we want.
-      return await action();
-    } finally {
-      release();
-    }
+  #release(): void {
+    this.#locked = false;
+    this.#processAcquiringQueue();
   }
 }

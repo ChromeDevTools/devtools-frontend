503d1d38f5c28050881c44956e133b34118f2d95
Kateryna Prokopenko <kprokopenko@chromium.org>
2023-03-20 12:32:09 +0000

    [DevToolsIcons] Introduce color tokens for icons
    Introduce both icon color palette and application color tokens for icons
    
    Bug: 1210894
    Change-Id: I3bdf48579ffe18c6846c1aa33f27f5fc2bc75cbb
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4311760
    Reviewed-by: Changhao Han <changhaohan@chromium.org>
    Commit-Queue: Kateryna Prokopenko <kprokopenko@chromium.org>

--- front_end/ui/legacy/utils/inject-core-styles.ts
+++ front_end/ui/legacy/utils/inject-core-styles.ts
@@ -2,12 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import applicationColorTokensStyles from '../applicationColorTokens.css.legacy.js';
 import inspectorCommonStyles from '../inspectorCommon.css.legacy.js';
 import textButtonStyles from '../textButton.css.legacy.js';
 import * as ThemeSupport from '../theme_support/theme_support.js';
 import themeColorsStyles from '../themeColors.css.legacy.js';
 
 export function injectCoreStyles(root: Element|ShadowRoot): void {
+  ThemeSupport.ThemeSupport.instance().appendStyle(root, applicationColorTokensStyles);
   ThemeSupport.ThemeSupport.instance().appendStyle(root, inspectorCommonStyles);
   ThemeSupport.ThemeSupport.instance().appendStyle(root, textButtonStyles);
   ThemeSupport.ThemeSupport.instance().appendStyle(root, themeColorsStyles);

ae6db1ec3904c2df974cd7e046ba7187855a0c40
Jack Franklin <jacktfranklin@chromium.org>
2023-03-22 14:49:17 +0000

    Remove dummy screenshot test
    It has served its purpose, but now the infra is in place we do not need
    it.
    
    Bug: None
    Change-Id: I120f714d9f81041003110dccd66005cf68c859b3
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4361239
    Commit-Queue: Jack Franklin <jacktfranklin@chromium.org>
    Reviewed-by: Andres Olivares <andoli@chromium.org>
    Commit-Queue: Andres Olivares <andoli@chromium.org>
    Auto-Submit: Jack Franklin <jacktfranklin@chromium.org>

--- test/interactions/panels/performance/timeline/timeline_test.ts
+++ test/interactions/panels/performance/timeline/timeline_test.ts
@@ -9,18 +9,11 @@ import {loadComponentDocExample, preloadForCodeCoverage} from '../../../helpers/
 
 describe('Performance panel', () => {
   preloadForCodeCoverage('performance_panel/basic.html');
-  preloadForCodeCoverage('performance_panel/liviu.html');
 
   itScreenshot('renders the timeline correctly', async () => {
     await loadComponentDocExample('performance_panel/basic.html?trace=animation');
     await waitFor('.timeline-flamechart');
     const panel = await waitFor('body');
     await assertElementScreenshotUnchanged(panel, 'performance/timeline.png');
-  });
-
-  itScreenshot('screenshot smoke test', async () => {
-    await loadComponentDocExample('performance_panel/liviu.html');
-    const container = await waitFor('#container');
-    await assertElementScreenshotUnchanged(container, 'performance/liviu-test.png', 0);
   });
 });

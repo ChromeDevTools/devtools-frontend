14fa4fb97a210a8796298281dc75e355a127f600
Al Muthanna Athamina <almuthanna@chromium.org>
2023-03-28 12:43:31 +0200

    Migrate the timeline-js-line-level-profile-no-url-end-to-end_test test
    Bug: 1414607
    Change-Id: Id30edce72797b502ba7168bcc0aef5c02bc43637
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4374999
    Commit-Queue: Almothana Athamneh <almuthanna@chromium.org>
    Reviewed-by: Jack Franklin <jacktfranklin@chromium.org>

---
+++ test/e2e/performance/timeline-js-line-level-profile-no-url-end-to-end_test.ts
@@ -0,0 +1,34 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {getBrowserAndPages} from '../../conductor/puppeteer-state.js';
+import {waitForAria, waitForMany} from '../../shared/helper.js';
+import {describe, it} from '../../shared/mocha-extensions.js';
+import {navigateToPerformanceTab, startRecording, stopRecording} from '../helpers/performance-helpers.js';
+import {openSourcesPanel} from '../helpers/sources-helpers.js';
+
+describe('The Performance panel', () => {
+  it('can collect a line-level CPU profile and show it in the text editor', async () => {
+    const {target} = getBrowserAndPages();
+    await navigateToPerformanceTab();
+    await startRecording();
+    await target.evaluate(() => {
+      (function() {
+        const endTime = Date.now() + 100;
+        let s = 0;
+        while (Date.now() < endTime) {
+          s += Math.cos(s);
+        }
+        return s;
+      })();
+    });
+    await stopRecording();
+    await openSourcesPanel();
+    const domain = await waitForAria('__puppeteer_evaluation_script__, domain');
+    domain.click({clickCount: 2});
+    const file = await waitForAria(', file');
+    await file.click({clickCount: 2});
+    await waitForMany('.cm-performanceGutter .cm-gutterElement', 3);
+  });
+});

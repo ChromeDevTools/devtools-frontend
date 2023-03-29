6ca00175fdd7e49a60001b00bba0c5305e55ebdb
Al Muthanna Athamina <almuthanna@chromium.org>
2023-03-29 10:30:17 +0200

    Skip timeline-js-line-level-profile-no-url-end-to-end on mac
    Bug: 1414607
    Change-Id: I6e23500c36518321fce1e80d9f57fb5f84f38ede
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4379738
    Auto-Submit: Almothana Athamneh <almuthanna@chromium.org>
    Commit-Queue: Ergün Erdoğmuş <ergunsh@chromium.org>
    Reviewed-by: Ergün Erdoğmuş <ergunsh@chromium.org>

--- test/e2e/performance/timeline-js-line-level-profile-no-url-end-to-end_test.ts
+++ test/e2e/performance/timeline-js-line-level-profile-no-url-end-to-end_test.ts
@@ -9,26 +9,28 @@ import {navigateToPerformanceTab, startRecording, stopRecording} from '../helper
 import {openSourcesPanel} from '../helpers/sources-helpers.js';
 
 describe('The Performance panel', () => {
-  it('can collect a line-level CPU profile and show it in the text editor', async () => {
-    const {target} = getBrowserAndPages();
-    await navigateToPerformanceTab();
-    await startRecording();
-    await target.evaluate(() => {
-      (function() {
-        const endTime = Date.now() + 100;
-        let s = 0;
-        while (Date.now() < endTime) {
-          s += Math.cos(s);
-        }
-        return s;
-      })();
-    });
-    await stopRecording();
-    await openSourcesPanel();
-    const domain = await waitForAria('__puppeteer_evaluation_script__, domain');
-    domain.click({clickCount: 2});
-    const file = await waitForAria(', file');
-    await file.click({clickCount: 2});
-    await waitForMany('.cm-performanceGutter .cm-gutterElement', 3);
-  });
+  // Flaky on mac.
+  it.skipOnPlatforms(
+      ['mac'], '[crbug.com/1414607] can collect a line-level CPU profile and show it in the text editor', async () => {
+        const {target} = getBrowserAndPages();
+        await navigateToPerformanceTab();
+        await startRecording();
+        await target.evaluate(() => {
+          (function() {
+            const endTime = Date.now() + 100;
+            let s = 0;
+            while (Date.now() < endTime) {
+              s += Math.cos(s);
+            }
+            return s;
+          })();
+        });
+        await stopRecording();
+        await openSourcesPanel();
+        const domain = await waitForAria('__puppeteer_evaluation_script__, domain');
+        domain.click({clickCount: 2});
+        const file = await waitForAria(', file');
+        await file.click({clickCount: 2});
+        await waitForMany('.cm-performanceGutter .cm-gutterElement', 3);
+      });
 });

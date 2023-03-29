7eb4dcf6f816f18d26403804270064aa40c62b0f
Al Muthanna Athamina <almuthanna@chromium.org>
2023-03-29 13:53:15 +0200

    Assign different bug for line-level CPU profile skipped test
    Reduce the number of line-level CPU profile assertion to two
    since on Mac it displays 2 some of the time. Also assign a new bug to
    the skip and use the click helper funtion.
    
    Bug: 1414607
    Change-Id: Ic76e5a3283e54fc536dd4a788f2e08e57922e6eb
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4379739
    Reviewed-by: Philip Pfaffe <pfaffe@chromium.org>
    Commit-Queue: Almothana Athamneh <almuthanna@chromium.org>
    Reviewed-by: Jack Franklin <jacktfranklin@chromium.org>

--- test/e2e/performance/timeline-js-line-level-profile-no-url-end-to-end_test.ts
+++ test/e2e/performance/timeline-js-line-level-profile-no-url-end-to-end_test.ts
@@ -3,15 +3,15 @@
 // found in the LICENSE file.
 
 import {getBrowserAndPages} from '../../conductor/puppeteer-state.js';
-import {waitForAria, waitForMany} from '../../shared/helper.js';
+import {click, waitForMany} from '../../shared/helper.js';
 import {describe, it} from '../../shared/mocha-extensions.js';
 import {navigateToPerformanceTab, startRecording, stopRecording} from '../helpers/performance-helpers.js';
 import {openSourcesPanel} from '../helpers/sources-helpers.js';
 
 describe('The Performance panel', () => {
-  // Flaky on mac.
+  // issue on Mac
   it.skipOnPlatforms(
-      ['mac'], '[crbug.com/1414607] can collect a line-level CPU profile and show it in the text editor', async () => {
+      ['mac'], '[crbug.com/1428866] can collect a line-level CPU profile and show it in the text editor', async () => {
         const {target} = getBrowserAndPages();
         await navigateToPerformanceTab();
         await startRecording();
@@ -27,10 +27,8 @@ describe('The Performance panel', () => {
         });
         await stopRecording();
         await openSourcesPanel();
-        const domain = await waitForAria('__puppeteer_evaluation_script__, domain');
-        domain.click({clickCount: 2});
-        const file = await waitForAria(', file');
-        await file.click({clickCount: 2});
-        await waitForMany('.cm-performanceGutter .cm-gutterElement', 3);
+        await click('[aria-label="__puppeteer_evaluation_script__, domain"]', {clickOptions: {clickCount: 2}});
+        await click('[aria-label=", file"]', {clickOptions: {clickCount: 2}});
+        await waitForMany('.cm-performanceGutter .cm-gutterElement', 2);
       });
 });

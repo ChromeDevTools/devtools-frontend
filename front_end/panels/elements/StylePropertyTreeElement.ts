0f7dbb5d0f6c302aef8a3dd97f6f6013697fab39
Alex Rudenko <alexrudenko@chromium.org>
2023-03-30 10:52:45 +0200

    Reland "Remove the CSSHints experiment flag"
    This is a reland of commit 52adf93b3267f6dc09b9c8b7015d3135905dd48e
    
    Original change's description:
    > Remove the CSSHints experiment flag
    >
    > Bug: 1373012
    > Change-Id: I3b284122eba7dc30954eb95bd3f82688e4287280
    > Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4382816
    > Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    > Reviewed-by: Changhao Han <changhaohan@chromium.org>
    > Commit-Queue: Changhao Han <changhaohan@chromium.org>
    > Auto-Submit: Alex Rudenko <alexrudenko@chromium.org>
    
    Bug: 1373012
    Change-Id: I35e5ec8ef7f4d5b24b25168af3cb3d4bf42d5c90
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4386995
    Commit-Queue: Changhao Han <changhaohan@chromium.org>
    Reviewed-by: Changhao Han <changhaohan@chromium.org>
    Auto-Submit: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>

--- front_end/panels/elements/StylePropertyTreeElement.ts
+++ front_end/panels/elements/StylePropertyTreeElement.ts
@@ -970,10 +970,6 @@ export class StylePropertyTreeElement extends UI.TreeOutline.TreeElement {
 
   updateAuthoringHint(): void {
     this.listItemElement.classList.remove('inactive-property');
-    if (!Root.Runtime.experiments.isEnabled(Root.Runtime.ExperimentName.CSS_AUTHORING_HINTS)) {
-      return;
-    }
-
     const existingElement = this.listItemElement.querySelector('.hint');
     if (existingElement) {
       activeHints.delete(existingElement);

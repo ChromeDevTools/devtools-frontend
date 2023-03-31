4258c29437b228d35689ad1fff55d5fa683287a9
Al Muthanna Athamina <almuthanna@chromium.org>
2023-03-31 11:48:17 +0200

    Unskip css hints test
    The test does not seem to flake anymore. Please find stress runs.
    
    Bug: 1418288
    Fixed: 1377773
    Change-Id: I31cfa9a78ceec261f2bcd938d57b5450912dde00
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4386972
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Auto-Submit: Almothana Athamneh <almuthanna@chromium.org>
    Commit-Queue: Danil Somsikov <dsv@chromium.org>

--- test/e2e/elements/css-hints_test.ts
+++ test/e2e/elements/css-hints_test.ts
@@ -46,8 +46,7 @@ describe('CSS hints in the Styles panel', async () => {
     assert.deepEqual(propertiesWithHints, []);
   });
 
-  // Flaky on windows.
-  it.skipOnPlatforms(['win32'], '[crbug.com/1377773] updates the hint if the styles are edited', async () => {
+  it('updates the hint if the styles are edited', async () => {
     await goToResourceAndWaitForStyleSection('elements/inactive-css-page.html');
     await waitForStyleRule('body');
     await waitForAndClickTreeElementWithPartialText('wrapper');

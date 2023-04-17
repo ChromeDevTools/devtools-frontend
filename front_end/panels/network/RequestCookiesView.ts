85729920a11de8072f1bbebe6b2c595504773c16
Simon Zünd <szuend@chromium.org>
2023-04-17 08:24:24 +0200

    [icon] Update malformed response cookie error icon to GM3
    Screenshots: https://imgur.com/a/a6acbiV
    
    R=kprokopenko@chromium.org
    
    Bug: 1427397
    Change-Id: I61067e59a0a2645c78e99faf4d3ee291492bed62
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4427865
    Reviewed-by: Kateryna Prokopenko <kprokopenko@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>

--- front_end/panels/network/RequestCookiesView.ts
+++ front_end/panels/network/RequestCookiesView.ts
@@ -32,6 +32,7 @@ import * as Common from '../../core/common/common.js';
 import * as i18n from '../../core/i18n/i18n.js';
 import * as SDK from '../../core/sdk/sdk.js';
 import * as Protocol from '../../generated/protocol.js';
+import * as IconButton from '../../ui/components/icon_button/icon_button.js';
 import * as CookieTable from '../../ui/legacy/components/cookie_table/cookie_table.js';
 import * as UI from '../../ui/legacy/legacy.js';
 
@@ -286,7 +287,9 @@ export class RequestCookiesView extends UI.Widget.Widget {
       this.malformedResponseCookiesList.removeChildren();
       for (const malformedCookie of malformedResponseCookies) {
         const listItem = this.malformedResponseCookiesList.createChild('span', 'cookie-line source-code');
-        const icon = UI.Icon.Icon.create('smallicon-error', 'cookie-warning-icon');
+        const icon = new IconButton.Icon.Icon();
+        icon.data = {iconName: 'cross-circle-filled', color: 'var(--icon-error-small)', width: '14px', height: '14px'};
+        icon.classList.add('cookie-warning-icon');
         listItem.appendChild(icon);
         UI.UIUtils.createTextChild(listItem, malformedCookie.cookieLine);
 

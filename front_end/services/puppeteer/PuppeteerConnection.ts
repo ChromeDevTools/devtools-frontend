c9d6acaee582dc22ed7b87eafaf55825f56f8b9a
Alex Rudenko <alexrudenko@chromium.org>
2023-06-22 12:32:25 +0200

    Remove tracking of the message IDs
    PuppeteerConnection already filters out messages from unknown
    sessions so we can rely on Puppeteer to match request/response
    pairs.
    
    Bug: none
    Change-Id: Iff7f011bc595da82f8ad71d6b6f0c3ea697902f9
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4634661
    Reviewed-by: Jaroslav Sevcik <jarin@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>

--- front_end/services/puppeteer/PuppeteerConnection.ts
+++ front_end/services/puppeteer/PuppeteerConnection.ts
@@ -8,15 +8,12 @@ import type * as SDK from '../../core/sdk/sdk.js';
 
 class Transport implements puppeteer.ConnectionTransport {
   #connection: SDK.Connections.ParallelConnectionInterface;
-  #knownIds = new Set<number>();
 
   constructor(connection: SDK.Connections.ParallelConnectionInterface) {
     this.#connection = connection;
   }
 
   send(data: string): void {
-    const message = JSON.parse(data);
-    this.#knownIds.add(message.id);
     this.#connection.sendRawMessage(data);
   }
 
@@ -27,13 +24,11 @@ class Transport implements puppeteer.ConnectionTransport {
   set onmessage(cb: (message: string) => void) {
     this.#connection.setOnMessage((message: Object) => {
       const data = (message) as {id: number, method: string, params: unknown, sessionId?: string};
-      if (data.id && !this.#knownIds.has(data.id)) {
-        return;
-      }
-      this.#knownIds.delete(data.id);
+
       if (!data.sessionId) {
         return;
       }
+
       return cb(JSON.stringify({
         ...data,
         // Puppeteer is expecting to use the default session, but we give it a non-default session in #connection.

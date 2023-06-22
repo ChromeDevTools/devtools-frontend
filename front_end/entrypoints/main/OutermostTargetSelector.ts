b4bd013487b82b57a1b627287e99d2946de2666e
Danil Somsikov <dsv@chromium.org>
2023-06-22 10:34:06 +0200

    Use displayNameForURL in the outermost target selector.
    Screenshot: https://imgur.com/a/xEOoR4R
    
    Bug: 1453802
    Change-Id: Idefe14a016436a0e2f8f003825895912c692c131
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4632541
    Reviewed-by: Yang Guo <yangguo@chromium.org>
    Auto-Submit: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Yang Guo <yangguo@chromium.org>

--- front_end/entrypoints/main/OutermostTargetSelector.ts
+++ front_end/entrypoints/main/OutermostTargetSelector.ts
@@ -6,7 +6,9 @@ import * as Common from '../../core/common/common.js';
 import * as i18n from '../../core/i18n/i18n.js';
 import * as Platform from '../../core/platform/platform.js';
 import * as SDK from '../../core/sdk/sdk.js';
+import * as Bindings from '../../models/bindings/bindings.js';
 import * as UI from '../../ui/legacy/legacy.js';
+
 import type * as Protocol from '../../generated/protocol.js';
 
 import outermostTargetSelectorStyles from './outermostTargetSelector.css.js';
@@ -166,9 +168,9 @@ export class OutermostTargetSelector implements SDK.TargetManager.Observer, UI.S
       return '';
     }
     const components = [];
-    const url = Common.ParsedURL.ParsedURL.fromString(targetInfo.url);
+    const url = Bindings.ResourceUtils.displayNameForURL(targetInfo.url as Platform.DevToolsPath.UrlString);
     if (url) {
-      components.push(url.domain());
+      components.push(url);
     }
     if (targetInfo.subtype) {
       components.push(targetInfo.subtype);

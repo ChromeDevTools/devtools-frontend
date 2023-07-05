ba6b4436affe4d9af21d915ceec243c5978fc20c
Philip Pfaffe <pfaffe@chromium.org>
2023-07-05 12:18:00 +0000

    Add lit-html 1.3 to third_party
    Bug: 1454724
    Change-Id: I540bdadcbdbf1df24c7a81ae78b71c6d549abdb6
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4632726
    Commit-Queue: Philip Pfaffe <pfaffe@chromium.org>
    Reviewed-by: Benedikt Meurer <bmeurer@chromium.org>

---
+++ extensions/cxx_debugging/third_party/lit-html/ts3.4/test/test-utils/shadow-root.d.ts
@@ -0,0 +1,5 @@
+/**
+ * A helper for creating a shadowRoot on an element.
+ */
+export declare const renderShadowRoot: (result: unknown, element: Element) => void;
+//# sourceMappingURL=shadow-root.d.ts.map

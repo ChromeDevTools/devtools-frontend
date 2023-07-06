fafd9942b50a2631706fb24db98b5a2cd381a746
Al Muthanna Athamina <almuthanna@chromium.org>
2023-07-06 15:12:22 +0200

    Deflake command-line-api-getEventListeners test
    Split the test "returns the correct values when using the
    getEventListeners method" test so it would not timeout.
    
    Bug: 1418288
    Change-Id: I4adf6836d6f34e917df80451c0ce0e32b781a800
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4665822
    Auto-Submit: Almothana Athamneh <almuthanna@chromium.org>
    Reviewed-by: Philip Pfaffe <pfaffe@chromium.org>
    Commit-Queue: Almothana Athamneh <almuthanna@chromium.org>

--- test/e2e/console/command-line-api-getEventListeners_test.ts
+++ test/e2e/console/command-line-api-getEventListeners_test.ts
@@ -14,68 +14,78 @@ import {
 const checkCommandResult = checkCommandResultFunction(0);
 
 describe('The Console Tab', async function() {
-  it('returns the correct values when using the getEventListeners method', async () => {
+  beforeEach(async () => {
     await goToResource('../resources/console/command-line-api-getEventListeners.html');
     await navigateToConsoleTab();
+  });
 
+  it('inner listeners are displayed correctly', async () => {
     await checkCommandResult(
         'innerListeners();',
         '{keydown: Array(2), wheel: Array(1)}',
-        'inner listeners are not displayed correctly',
     );
+  });
 
+  it('inner listeners are displayed correctly after removal', async () => {
     await checkCommandResult(
         'removeInnerListeners(); getEventListeners(innerElement());',
         '{keydown: Array(1)}',
-        'inner listeners after removal are not displayed correctly',
     );
+  });
 
+  it('Event listeners are gotten correctly for an element', async () => {
     await checkCommandResult(
         'getEventListeners(document.getElementById("outer"));',
         '{mousemove: Array(1), mousedown: Array(1), keydown: Array(1), keyup: Array(1)}',
-        'outer listeners are not displayed correctly',
     );
+  });
 
+  it('Event listeners are gotten correctly for a button', async () => {
     await checkCommandResult(
         'getEventListeners(document.getElementById("button"));',
         '{click: Array(1), mouseover: Array(1)}',
-        'attribute event listeners are not displayed correctly',
     );
+  });
 
+  it('Event listeners are gotten correctly for a window', async () => {
     await checkCommandResult(
         'getEventListeners(window);',
         '{popstate: Array(1)}',
-        'window listeners are not displayed correctly',
     );
+  });
 
+  it('Event listeners are gotten correctly for an empty element', async () => {
     await checkCommandResult(
         'getEventListeners(document.getElementById("empty"));',
         '{}',
-        'empty listeners are not displayed correctly',
     );
+  });
 
+  it('Event listeners are gotten correctly for an invalid element', async () => {
     await checkCommandResult(
         'getEventListeners(document.getElementById("invalid"));',
         '{}',
-        'invalid listeners are not displayed correctly',
     );
+  });
 
+  it('Event listeners are gotten correctly for an empty map', async () => {
     await checkCommandResult(
         'getEventListeners({});',
         '{}',
-        'object listeners are not displayed correctly',
     );
+  });
 
+  it('Event listeners are gotten correctly for a null value', async () => {
     await checkCommandResult(
         'getEventListeners(null);',
         '{}',
-        'null listeners are not displayed correctly',
     );
+  });
 
+  it('Event listeners are gotten correctly for an undefined value', async () => {
     await checkCommandResult(
         'getEventListeners(undefined);',
         '{}',
-        'undefined listeners are not displayed correctly',
     );
   });
 });

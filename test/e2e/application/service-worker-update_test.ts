96ebeeb161f9e706470599145a2e817f7a070df8
Al Muthanna Athamina <almuthanna@chromium.org>
2023-07-12 15:51:30 +0200

    Unregister service workers before registering a new one
    Bug: 1418288
    Change-Id: Ic249bd15513418af86962a50e4b526070af39ed9
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4679111
    Commit-Queue: Almothana Athamneh <almuthanna@chromium.org>
    Auto-Submit: Almothana Athamneh <almuthanna@chromium.org>
    Reviewed-by: Philip Pfaffe <pfaffe@chromium.org>

--- test/e2e/application/service-worker-update_test.ts
+++ test/e2e/application/service-worker-update_test.ts
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 import {assert} from 'chai';
 
+import {unregisterAllServiceWorkers} from '../../conductor/hooks.js';
 import {getBrowserAndPages, step, waitFor} from '../../shared/helper.js';
 import {beforeEach, describe, it} from '../../shared/mocha-extensions.js';
 import {navigateToApplicationTab, navigateToServiceWorkers} from '../helpers/application-helpers.js';
@@ -15,6 +16,10 @@ describe('The Application Tab', async function() {
     const {target} = getBrowserAndPages();
     await navigateToApplicationTab(target, TEST_HTML_FILE);
     await navigateToServiceWorkers();
+  });
+
+  afterEach(async () => {
+    await unregisterAllServiceWorkers();
   });
 
   it('Navigate to a page with service worker we should find service worker update timeline info', async () => {

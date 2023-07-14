60caeab5398be95e5fa34212a49c3b9cffb59cc1
Jack Franklin <jacktfranklin@google.com>
2023-07-14 10:05:52 +0100

    Add e2e test for Performance Panel reload and record
    Bug: 1464849
    Change-Id: I385aed92c2dbf6bfd7c7e767f5072738df581385
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4684350
    Commit-Queue: Jack Franklin <jacktfranklin@chromium.org>
    Reviewed-by: Andres Olivares <andoli@chromium.org>

--- test/e2e/helpers/performance-helpers.ts
+++ test/e2e/helpers/performance-helpers.ts
@@ -7,6 +7,7 @@ import type * as puppeteer from 'puppeteer-core';
 import {click, goToResource, platform, waitFor, waitForAria} from '../../shared/helper.js';
 
 export const RECORD_BUTTON_SELECTOR = '[aria-label="Record"]';
+export const RELOAD_AND_RECORD_BUTTON_SELECTOR = '[aria-label="Start profiling and reload page"]';
 export const STOP_BUTTON_SELECTOR = '[aria-label="Stop"]';
 export const SUMMARY_TAB_SELECTOR = '[aria-label="Summary"]';
 export const BOTTOM_UP_SELECTOR = '[aria-label="Bottom-Up"]';
@@ -58,6 +59,14 @@ export async function startRecording() {
 
   // Wait for the button to turn to its stop state.
   await waitFor(STOP_BUTTON_SELECTOR);
+}
+
+export async function reloadAndRecord() {
+  await click(RELOAD_AND_RECORD_BUTTON_SELECTOR);
+  // Make sure the timeline details panel appears. It's a sure way to assert
+  // that a recording is actually displayed as some of the other elements in
+  // the timeline remain in the DOM even after the recording has been cleared.
+  await waitFor('.timeline-details-chip-body');
 }
 
 export async function stopRecording() {
--- test/e2e/performance/recording_test.ts
+++ test/e2e/performance/recording_test.ts
@@ -8,18 +8,26 @@ import {describe, it} from '../../shared/mocha-extensions.js';
 import {
   getTotalTimeFromSummary,
   navigateToPerformanceTab,
+  reloadAndRecord,
   startRecording,
   stopRecording,
 } from '../helpers/performance-helpers.js';
 
 describe('The Performance panel', () => {
-  it('can start and stop a new recording', async () => {
+  it('supports the user manually starting and stopping a recording', async () => {
     await navigateToPerformanceTab('empty');
 
     await startRecording();
     await stopRecording();
 
     // Check that the recording shows the pie chart with a non 0 total time.
+    const totalTime = await getTotalTimeFromSummary();
+    assert.isAbove(totalTime, 0, 'The recording was created successfully');
+  });
+
+  it('can reload and record a trace', async () => {
+    await navigateToPerformanceTab('fake-website');
+    await reloadAndRecord();
     const totalTime = await getTotalTimeFromSummary();
     assert.isAbove(totalTime, 0, 'The recording was created successfully');
   });

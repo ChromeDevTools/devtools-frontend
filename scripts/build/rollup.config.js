7d525b4cc3f99a757298cf146ac4364734f9c0dd
Simon Zünd <szuend@chromium.org>
2023-08-09 09:42:46 +0200

    [build] Remove 'minify-html-template-literals' plugin from rollup config
    R=alexrudenko@chromium.org
    
    Bug: 1471374,1213034
    Change-Id: Ia6ee0be69248904129b9e950f1e16f82013ec9b5
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4765264
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>

--- scripts/build/rollup.config.js
+++ scripts/build/rollup.config.js
@@ -4,33 +4,10 @@
 
 // @ts-check
 
-import {defaultStrategy} from 'minify-html-literals/src/strategy';  // eslint-disable-line rulesdir/es_modules_import
-import minifyHTML from 'rollup-plugin-minify-html-template-literals';
 import sourcemaps from 'rollup-plugin-sourcemaps';
 import {terser} from 'rollup-plugin-terser';
 
 const devtools_plugin = require('./devtools_plugin.js');
-
-/**
- * @type {import("minify-html-literals").Strategy<import("html-minifier").Options, unknown>}
- */
-const minifyHTMLStrategy = {
-  getPlaceholder() {
-    return 'TEMPLATE_EXPRESSION';
-  },
-  combineHTMLStrings(parts, placeholder) {
-    return defaultStrategy.combineHTMLStrings(parts, placeholder);
-  },
-  minifyHTML(html, options) {
-    return defaultStrategy.minifyHTML(html, options);
-  },
-  minifyCSS(css, options) {
-    return defaultStrategy.minifyCSS(css, options);
-  },
-  splitHTMLByPlaceholder(html, placeholder) {
-    return defaultStrategy.splitHTMLByPlaceholder(html, placeholder);
-  }
-};
 
 /** @type {function({configDCHECK: boolean, configSourcemaps: boolean}): import("rollup").MergedRollupOptions} */
 // eslint-disable-next-line import/no-default-export
@@ -42,18 +19,6 @@ export default commandLineArgs => ({
     sourcemap: Boolean(commandLineArgs.configSourcemaps),
   }],
   plugins: [
-    minifyHTML({
-      options: {
-        strategy: minifyHTMLStrategy,
-        minifyOptions: {
-          collapseInlineTagWhitespace: false,
-          collapseWhitespace: true,
-          conservativeCollapse: true,
-          minifyCSS: false,
-          removeOptionalTags: true,
-        },
-      },
-    }),
     terser({
       compress: {
         pure_funcs: commandLineArgs.configDCHECK ? ['Platform.DCHECK'] : [],

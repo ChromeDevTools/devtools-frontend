a15de332f8fe0d5c6afd9f69397a559f933e5855
Al Muthanna Athamina <almuthanna@chromium.org>
2023-08-17 14:00:25 +0200

    Skip recorder test
    Skip flaky test until we figure out why some events are not recorded.
    
    Bug: 1473597, 1419802
    Change-Id: I4b53636f0d1dbb1787629017f2f55a47287a497e
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4789981
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Auto-Submit: Almothana Athamneh <almuthanna@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>

--- test/e2e/recorder/recorder_test.ts
+++ test/e2e/recorder/recorder_test.ts
@@ -335,7 +335,9 @@ describe('Recorder', function() {
     assertRecordingMatchesSnapshot(recording);
   });
 
-  it('should capture a change that causes navigation without blur or change', async () => {
+  // skipped until we figure out why the keyup for Enter is not recorded in
+  // 1% of the runs.
+  it.skip('[crbug.com/1473597] should capture a change that causes navigation without blur or change', async () => {
     await startRecording('recorder/programmatic-navigation-on-keydown.html');
 
     const {target} = getBrowserAndPages();

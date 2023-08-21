91deb234b01dc91c112f3ef34c7df55e2e99d92f
Eric Leese <leese@chromium.org>
2023-08-21 11:47:40 +0200

    Disable flaky overrides tests on mac
    Bug: 1474334
    Change-Id: If4c106e9afaaae5048e43c1b082863ade24eb5b5
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4797650
    Auto-Submit: Eric Leese <leese@chromium.org>
    Commit-Queue: Jecelyn Yeen <jec@chromium.org>
    Reviewed-by: Jecelyn Yeen <jec@chromium.org>

--- test/e2e/sources/overrides_test.ts
+++ test/e2e/sources/overrides_test.ts
@@ -339,24 +339,27 @@ describe('Overrides panel', async function() {
 });
 
 describe('Overrides panel', () => {
-  it('appends correct overrides context menu for Sources > Page file', async () => {
-    await goToResource('elements/elements-panel-styles.html');
-    await openNetworkTab();
-    await selectRequestByName('elements-panel-styles.css', {button: 'right'});
-    await click('aria/Open in Sources panel');
+  // Flaky on mac
+  it.skipOnPlatforms(
+      ['mac'], '[crbug.com/1474334]: appends correct overrides context menu for Sources > Page file', async () => {
+        await goToResource('elements/elements-panel-styles.html');
+        await openNetworkTab();
+        await selectRequestByName('elements-panel-styles.css', {button: 'right'});
+        await click('aria/Open in Sources panel');
 
-    // Open the file in the Sources panel
-    const file = await waitFor('[aria-label="elements-panel-styles.css, file"]');
-    await file.click({button: 'right'});
+        // Open the file in the Sources panel
+        const file = await waitFor('[aria-label="elements-panel-styles.css, file"]');
+        await file.click({button: 'right'});
 
-    const assertShowAllElements = await $$('Show all overrides', undefined, 'aria');
-    const assertOverridesContentElements = await $$('Override content', undefined, 'aria');
+        const assertShowAllElements = await $$('Show all overrides', undefined, 'aria');
+        const assertOverridesContentElements = await $$('Override content', undefined, 'aria');
 
-    assert.strictEqual(assertShowAllElements.length, 0);
-    assert.strictEqual(assertOverridesContentElements.length, 1);
-  });
+        assert.strictEqual(assertShowAllElements.length, 0);
+        assert.strictEqual(assertOverridesContentElements.length, 1);
+      });
 
-  it('hide "Override content" for source mapped file', async () => {
+  // Flaky on mac
+  it.skipOnPlatforms(['mac'], '[crbug.com/1474334]: hide "Override content" for source mapped file', async () => {
     await goToResource('sources/sourcemap-origin.html');
     await openNetworkTab();
     await selectRequestByName('sourcemap-origin.min.js', {button: 'right'});

a3ab3487c28c4080d799b8fe8e406b275cb5affd
Hadrien Jaubert <hadrienjaubert@google.com>
2023-08-23 08:39:28 +0000

    [Performance panel] Update Datagrid inside Bottom-up, Call-tree and Event log to Material 3 colors
    Change the colors of the datagrid to alternating white and blue and change hover color
    
    Bug: 1472261
    Change-Id: Ia922ad6c654532349d46ad16d9fdc2ad192ffd37
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4774499
    Auto-Submit: Hadrien Jaubert <hadrienjaubert@google.com>
    Reviewed-by: Nancy Li <nancyly@chromium.org>
    Commit-Queue: Hadrien Jaubert <hadrienjaubert@google.com>
    Reviewed-by: Kateryna Prokopenko <kprokopenko@chromium.org>

--- test/interactions/panels/performance/timeline/timeline_test.ts
+++ test/interactions/panels/performance/timeline/timeline_test.ts
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {waitFor} from '../../../../shared/helper.js';
+import {click, waitFor} from '../../../../shared/helper.js';
 import {describe} from '../../../../shared/mocha-extensions.js';
 import {assertElementScreenshotUnchanged, itScreenshot} from '../../../../shared/screenshots.js';
 import {loadComponentDocExample, preloadForCodeCoverage} from '../../../helpers/shared.js';
@@ -15,6 +15,73 @@ describe('Performance panel', () => {
     await waitFor('.timeline-flamechart');
     const panel = await waitFor('body');
     await assertElementScreenshotUnchanged(panel, 'performance/timeline.png', 3);
+  });
+
+  itScreenshot('renders correctly the Bottom Up datagrid', async () => {
+    await loadComponentDocExample('performance_panel/basic.html?trace=one-second-interaction');
+    await waitFor('.timeline-flamechart');
+    await waitFor('div.tabbed-pane');
+    await click('#tab-BottomUp');
+    const datagrid = await waitFor('.timeline-tree-view');
+    await assertElementScreenshotUnchanged(datagrid, 'performance/bottomUp.png', 3);
+  });
+
+  itScreenshot('renders correctly the Call Tree datagrid', async () => {
+    await loadComponentDocExample('performance_panel/basic.html?trace=one-second-interaction');
+    await waitFor('.timeline-flamechart');
+    await waitFor('div.tabbed-pane');
+    await click('#tab-CallTree');
+    const datagrid = await waitFor('.timeline-tree-view');
+    await assertElementScreenshotUnchanged(datagrid, 'performance/callTree.png', 3);
+  });
+
+  itScreenshot('renders correctly the Event Log datagrid', async () => {
+    await loadComponentDocExample('performance_panel/basic.html?trace=one-second-interaction');
+    await waitFor('.timeline-flamechart');
+    await waitFor('div.tabbed-pane');
+    await click('#tab-EventLog');
+    const datagrid = await waitFor('.timeline-tree-view');
+    await assertElementScreenshotUnchanged(datagrid, 'performance/eventLog.png', 3);
+  });
+
+  itScreenshot('renders correctly the datagrid in the split widget of Bottom Up', async () => {
+    await loadComponentDocExample('performance_panel/basic.html?trace=one-second-interaction');
+    await waitFor('.timeline-flamechart');
+    await waitFor('div.tabbed-pane');
+    await click('#tab-BottomUp');
+    const datagrid = await waitFor('.timeline-tree-view');
+    await click('[aria-label="Show Heaviest stack"]');
+    const rows = await datagrid.$$('.data-grid-data-grid-node');
+
+    // The trace one-second-interaction contains more than 3 rows in the bottom up tree
+    // so it is safe to click the third one
+    if (rows.length >= 3) {
+      await rows[2].click();
+    } else {
+      throw new Error('There are less than three rows with the class \'data-grid-data-grid-node\'');
+    }
+
+    await assertElementScreenshotUnchanged(datagrid, 'performance/splitWidgetBottomUp.png', 3);
+  });
+
+  itScreenshot('renders correctly the datagrid in the split widget of Call Tree', async () => {
+    await loadComponentDocExample('performance_panel/basic.html?trace=one-second-interaction');
+    await waitFor('.timeline-flamechart');
+    await waitFor('div.tabbed-pane');
+    await click('#tab-CallTree');
+    const datagrid = await waitFor('.timeline-tree-view');
+    await click('[aria-label="Show Heaviest stack"]');
+    const rows = await datagrid.$$('.data-grid-data-grid-node');
+
+    // The trace one-second-interaction contains more than 3 rows in the call tree
+    // so it is safe to click the third one
+    if (rows.length >= 3) {
+      await rows[2].click();
+    } else {
+      throw new Error('There are less than three rows with the class \'data-grid-data-grid-node\'');
+    }
+
+    await assertElementScreenshotUnchanged(datagrid, 'performance/splitWidgetCallTree.png', 3);
   });
 
   itScreenshot('renders the timeline correctly when scrolling', async () => {

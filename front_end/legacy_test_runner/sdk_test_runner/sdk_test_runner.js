600e6a8307e3e77da803d66a7d6d6867d4470570
Simon Zünd <szuend@chromium.org>
2023-08-29 08:08:29 +0200

    [legacy] Use import instead of global for ProtocolClient
    R=wolfi@chromium.org
    
    Bug: 1442410
    Change-Id: Iafb8cf32e0be7495899b9cafa168c4b6c2d4525b
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4821526
    Reviewed-by: Wolfgang Beyer <wolfi@chromium.org>
    Auto-Submit: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Wolfgang Beyer <wolfi@chromium.org>

--- front_end/legacy_test_runner/sdk_test_runner/sdk_test_runner.js
+++ front_end/legacy_test_runner/sdk_test_runner/sdk_test_runner.js
@@ -5,6 +5,7 @@
 import '../../core/sdk/sdk-legacy.js';
 
 import * as Platform from '../../core/platform/platform.js';
+import * as ProtocolClient from '../../core/protocol_client/protocol_client.js';
 import {TestRunner} from '../test_runner/test_runner.js';
 
 /**
@@ -52,13 +53,13 @@ SDKTestRunner.PageMock = class {
     this.enabledDomains.clear();
     self.SDK.targetManager.clearAllTargetsForTest();
 
-    const oldFactory = ProtocolClient.Connection.getFactory();
-    ProtocolClient.Connection.setFactory(() => {
+    const oldFactory = ProtocolClient.InspectorBackend.Connection.getFactory();
+    ProtocolClient.InspectorBackend.Connection.setFactory(() => {
       this.connection = new MockPageConnection(this);
       return this.connection;
     });
     const target = self.SDK.targetManager.createTarget(nextId('mock-target-'), targetName, this.type, null);
-    ProtocolClient.Connection.setFactory(oldFactory);
+    ProtocolClient.InspectorBackend.Connection.setFactory(oldFactory);
 
     this.target = target;
     self.SDK.targetManager.setScopeTarget(target);
@@ -237,7 +238,8 @@ SDKTestRunner.PageMock = class {
     }
 
     this.sendResponse(
-        id, undefined, {message: 'Can\'t handle command ' + methodName, code: ProtocolClient.DevToolsStubErrorCode});
+        id, undefined,
+        {message: 'Can\'t handle command ' + methodName, code: ProtocolClient.InspectorBackend.DevToolsStubErrorCode});
   }
 
   sendResponse(id, result, error) {
--- front_end/legacy_test_runner/test_runner/TestRunner.js
+++ front_end/legacy_test_runner/test_runner/TestRunner.js
@@ -6,7 +6,7 @@
 
 import * as Common from '../../core/common/common.js';  // eslint-disable-line no-unused-vars
 import * as Platform from '../../core/platform/platform.js';
-import * as ProtocolClientModule from '../../core/protocol_client/protocol_client.js';
+import * as ProtocolClient from '../../core/protocol_client/protocol_client.js';
 import * as Root from '../../core/root/root.js';
 import * as Bindings from '../../models/bindings/bindings.js';
 import * as Workspace from '../../models/workspace/workspace.js';
@@ -498,7 +498,7 @@ export async function _evaluateInPage(code) {
     code += `//# sourceURL=${sourceURL}`;
   }
   const response = await TestRunner.RuntimeAgent.invoke_evaluate({expression: code, objectGroup: 'console'});
-  const error = response[ProtocolClientModule.InspectorBackend.ProtocolError];
+  const error = response[ProtocolClient.InspectorBackend.ProtocolError];
   if (error) {
     addResult('Error: ' + error);
     completeTest();
@@ -517,7 +517,7 @@ export async function _evaluateInPage(code) {
 export async function evaluateInPageAnonymously(code, userGesture) {
   const response =
       await TestRunner.RuntimeAgent.invoke_evaluate({expression: code, objectGroup: 'console', userGesture});
-  if (!response[ProtocolClientModule.InspectorBackend.ProtocolError]) {
+  if (!response[ProtocolClient.InspectorBackend.ProtocolError]) {
     return response.result.value;
   }
   addResult(
@@ -542,7 +542,7 @@ export async function evaluateInPageAsync(code) {
   const response = await TestRunner.RuntimeAgent.invoke_evaluate(
       {expression: code, objectGroup: 'console', includeCommandLineAPI: false, awaitPromise: true});
 
-  const error = response[ProtocolClientModule.InspectorBackend.ProtocolError];
+  const error = response[ProtocolClient.InspectorBackend.ProtocolError];
   if (!error && !response.exceptionDetails) {
     return response.result.value;
   }
@@ -613,7 +613,7 @@ export function check(passCondition, failureText) {
  * @param {!Function} callback
  */
 export function deprecatedRunAfterPendingDispatches(callback) {
-  ProtocolClient.test.deprecatedRunAfterPendingDispatches(callback);
+  ProtocolClient.InspectorBackend.test.deprecatedRunAfterPendingDispatches(callback);
 }
 
 /**
@@ -727,7 +727,7 @@ export function markStep(title) {
 }
 
 export function startDumpingProtocolMessages() {
-  ProtocolClient.test.dumpProtocol = self.testRunner.logToStderr.bind(self.testRunner);
+  ProtocolClient.InspectorBackend.test.dumpProtocol = self.testRunner.logToStderr.bind(self.testRunner);
 }
 
 /**

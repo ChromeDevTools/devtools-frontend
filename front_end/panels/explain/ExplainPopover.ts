3cea070eb41300602cbaf2a3f02a85f143c68b69
Alex Rudenko <alexrudenko@chromium.org>
2023-08-29 15:57:24 +0200

    explain: add ability to rate
    Bug: b/295119680
    Change-Id: Idbb9aa793f38fb7b08d498e2a6b09ff007234f30
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4822465
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>

--- front_end/panels/explain/ExplainPopover.ts
+++ front_end/panels/explain/ExplainPopover.ts
@@ -3,13 +3,21 @@
 // found in the LICENSE file.
 
 import * as Host from '../../core/host/host.js';
+import type * as Platform from '../../core/platform/platform.js';
 import * as Marked from '../../third_party/marked/marked.js';
+import * as Buttons from '../../ui/components/buttons/buttons.js';
 import * as MarkdownView from '../../ui/components/markdown_view/markdown_view.js';
 import * as UI from '../../ui/legacy/legacy.js';
+
+const surveyLink =
+    'https://docs.google.com/forms/d/e/1FAIpQLSfKm4UX5PvTkM-N6aZ2UcaNyRhoP075Hr-lOmRaJv_4zN4kkQ/viewform';
 
 export class ExplainPopover {
   #source: Source;
   #contentElement = document.createElement('div');
+  #markdownElement: Element;
+  #answer = '';
+
   #detectOutsideInteraction = (event: Event): void => {
     const el = event?.composedPath()[0] as Node;
     if (!el.isDescendant(this.#contentElement) && this.#contentElement !== el) {
@@ -22,15 +30,60 @@ export class ExplainPopover {
     this.#source = source;
     this.#contentElement.style.padding = '16px';
     this.#contentElement.style.maxWidth = '500px';
+    this.#contentElement.innerHTML = `
+      <div class="markdown">
+      </div>
+      <div class="rating">
+        <span>Rate this response:</span>
+      </div>
+    `;
     this.#popover.contentElement.classList.toggle('has-padding', false);
     this.#popover.contentElement.appendChild(this.#contentElement);
+    this.#markdownElement = this.#contentElement.querySelector('.markdown') as HTMLDivElement;
+    const ratingContainer = this.#contentElement.querySelector('.rating') as HTMLDivElement;
+    ratingContainer.style.display = 'none';
+    ratingContainer.style.flexDirection = 'row';
+    ratingContainer.style.alignItems = 'center';
+    const plusButton = new Buttons.Button.Button();
+    plusButton.data = {
+      variant: Buttons.Button.Variant.TOOLBAR,
+      size: Buttons.Button.Size.TINY,
+      iconName: 'thumb-up',
+    };
+    plusButton.classList.add('plus');
+    plusButton.style.marginLeft = '1em';
+    plusButton.style.width = '20px';
+    plusButton.style.height = '20px';
+    const minusButton = new Buttons.Button.Button();
+    minusButton.data = {
+      variant: Buttons.Button.Variant.TOOLBAR,
+      size: Buttons.Button.Size.TINY,
+      iconName: 'thumb-down',
+    };
+    minusButton.classList.add('minus');
+    minusButton.style.width = '20px';
+    minusButton.style.height = '20px';
+    ratingContainer.append(plusButton);
+    ratingContainer.append(minusButton);
+
+    this.#contentElement.addEventListener('click', event => {
+      const isPlus = (event.target as HTMLElement).matches('.plus');
+      const isMinus = (event.target as HTMLElement).matches('.minus');
+      if (isMinus || isPlus) {
+        const rating = isMinus ? 'Bad' : 'Good';
+        Host.InspectorFrontendHost.InspectorFrontendHostInstance.openInNewTab(
+            `${surveyLink}?usp=pp_url&entry.1692896504=${encodeURIComponent(rating)}&entry.1188367343=${
+                encodeURIComponent(this.#source.getPrompt())}&entry.405375461=${encodeURIComponent(this.#answer)}` as
+            Platform.DevToolsPath.UrlString);
+      }
+    });
   }
 
   #renderMarkdown(content: string): void {
     const markdown = new MarkdownView.MarkdownView.MarkdownView();
     markdown.data = {tokens: Marked.Marked.lexer(content)};
-    this.#contentElement.removeChildren();
-    this.#contentElement.append(markdown);
+    this.#markdownElement.removeChildren();
+    this.#markdownElement.append(markdown);
   }
 
   #setupListeners(): void {
@@ -52,9 +105,13 @@ export class ExplainPopover {
     this.#renderMarkdown('loading...');
     this.#popover.setContentAnchorBox(this.#source.getAnchor());
     this.#popover.show(document);
+    this.#popover.positionContent();
     try {
       const result = await runPrompt(this.#source.getPrompt());
       this.#renderMarkdown(result);
+      this.#answer = result;
+      const ratingContainer = this.#contentElement.querySelector('.rating') as HTMLDivElement;
+      ratingContainer.style.display = 'flex';
     } catch (err) {
       this.#renderMarkdown(`loading failed: ${err.message}`);
     }

1a84516a93ac53cdb79dd7b3fd3cf65d736b1bd7
Andrés Olivares <andoli@chromium.org>
2023-09-11 09:35:30 +0000

    Disable flaky layers panel e2e test
    Bug: 1261763
    Change-Id: I2ee2d64cbf3e3b79bfffb48e017ec082ca65ef03
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4853355
    Commit-Queue: Andres Olivares <andoli@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Auto-Submit: Andres Olivares <andoli@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>

--- test/e2e/layers/layers_test.ts
+++ test/e2e/layers/layers_test.ts
@@ -17,7 +17,8 @@ import {getCurrentUrl} from '../helpers/layers-helpers.js';
 import {openPanelViaMoreTools} from '../helpers/settings-helpers.js';
 
 describe('The Layers Panel', async () => {
-  it('should keep the currently inspected url as an attribute', async () => {
+  // See crbug.com/1261763 for details.
+  it.skip('[crbug.com/1261763] should keep the currently inspected url as an attribute', async () => {
     const {target, frontend} = getBrowserAndPages();
 
     const targetUrl = 'layers/default.html';

8f07cd69685fce3d42f2c64cd559133e6cf87f7e
Hadrien Jaubert <hadrienjaubert@google.com>
2023-09-13 08:59:05 +0000

    Diminish opacity of strokestyle color inside canvas grid
    Bug: 1475911
    Change-Id: I9ecce7fd7a43f53590cc141fc41ec725fc7c5ed8
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4847030
    Reviewed-by: Nancy Li <nancyly@chromium.org>
    Commit-Queue: Hadrien Jaubert <hadrienjaubert@google.com>

--- front_end/ui/legacy/components/perf_ui/TimelineGrid.ts
+++ front_end/ui/legacy/components/perf_ui/TimelineGrid.ts
@@ -116,7 +116,7 @@ export class TimelineGrid {
     context.save();
     context.scale(window.devicePixelRatio, window.devicePixelRatio);
     const height = Math.floor(context.canvas.height / window.devicePixelRatio);
-    context.strokeStyle = getComputedStyle(document.body).getPropertyValue('--sys-color-divider');
+    context.strokeStyle = getComputedStyle(document.body).getPropertyValue('--app-color-strokestyle');
     context.lineWidth = 1;
 
     context.translate(0.5, 0.5);

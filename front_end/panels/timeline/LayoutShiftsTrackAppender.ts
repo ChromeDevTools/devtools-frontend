855aa3ca466f3becef70434ed719efda8bb4b22c
Hadrien Jaubert <hadrienjaubert@google.com>
2023-09-15 10:14:09 +0000

    [Performance Panel] Update colors of Layout Shift track to material 3
    Bug: 1483217
    Change-Id: I843aa2f5b3350a2d31d5cd14cb0f8a3a33c2c84d
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4868412
    Reviewed-by: Nancy Li <nancyly@chromium.org>
    Reviewed-by: Kateryna Prokopenko <kprokopenko@chromium.org>
    Commit-Queue: Hadrien Jaubert <hadrienjaubert@google.com>

--- front_end/panels/timeline/LayoutShiftsTrackAppender.ts
+++ front_end/panels/timeline/LayoutShiftsTrackAppender.ts
@@ -1,17 +1,18 @@
 // Copyright 2023 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+import * as i18n from '../../core/i18n/i18n.js';
 import * as TraceEngine from '../../models/trace/trace.js';
 import type * as PerfUI from '../../ui/legacy/components/perf_ui/perf_ui.js';
+import * as ThemeSupport from '../../ui/legacy/theme_support/theme_support.js';
 
+import {buildGroupStyle, buildTrackHeader, getFormattedTime} from './AppenderUtils.js';
 import {
   type CompatibilityTracksAppender,
-  type TrackAppender,
   type HighlightedEntryInfo,
+  type TrackAppender,
   type TrackAppenderName,
 } from './CompatibilityTracksAppender.js';
-import * as i18n from '../../core/i18n/i18n.js';
-import {buildGroupStyle, buildTrackHeader, getFormattedTime} from './AppenderUtils.js';
 
 const UIStrings = {
   /**
@@ -111,7 +112,7 @@ export class LayoutShiftsTrackAppender implements TrackAppender {
    * Gets the color an event added by this appender should be rendered with.
    */
   colorForEvent(_event: TraceEngine.Types.TraceEvents.TraceEventData): string {
-    return 'rgb(155 127 230)';
+    return ThemeSupport.ThemeSupport.instance().getComputedValue('--app-color-rendering');
   }
 
   /**

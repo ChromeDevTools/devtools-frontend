48b1ed3e80679d52f55ee1afae83da9be3ddbecb
Liviu Rau <liviurau@chromium.org>
2023-09-28 14:21:40 +0200

    Avoid taking screenshots on ScreenshotErrors
    This fixes a bug where after a screenshot error the `onError` handler would take a pair of target/frontend screenshots and obscure the original error.
    
    Behaviour before the fix: https://luci-milo.appspot.com/ui/inv/u-liviurau-2023-09-28-11-48-00-7335b87ae21fb818/test-results
    
    Behaviour after the fix: https://luci-milo.appspot.com/ui/inv/u-liviurau-2023-09-28-12-01-00-5cb60fc22ac97b0d/test-results
    
    Bug: none
    Change-Id: I6f86d89f1476adeb3a3ad9513f0d5c472410ea8f
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4900637
    Reviewed-by: Jack Franklin <jacktfranklin@chromium.org>
    Reviewed-by: Eric Leese <leese@chromium.org>
    Commit-Queue: Eric Leese <leese@chromium.org>
    Auto-Submit: Liviu Rau <liviurau@chromium.org>
    Commit-Queue: Jack Franklin <jacktfranklin@chromium.org>

--- test/shared/mocha-extensions.ts
+++ test/shared/mocha-extensions.ts
@@ -159,7 +159,7 @@ async function timeoutHook(this: Mocha.Runnable, done: Mocha.Done|undefined, err
       err.cause = new Error(msg);
     }
   }
-  if (err && !getEnvVar('DEBUG_TEST')) {
+  if (err && !getEnvVar('DEBUG_TEST') && !(err instanceof ScreenshotError)) {
     const {target, frontend} = await takeScreenshots(this.fullTitle());
     err = ScreenshotError.fromBase64Images(err, target, frontend);
   }
@@ -253,12 +253,11 @@ function wrapMochaCall(
 
     if (callback.length === 0) {
       async function onError(this: unknown, err?: unknown) {
-        if (err && !getEnvVar('DEBUG_TEST')) {
+        if (err && !getEnvVar('DEBUG_TEST') && !(err instanceof ScreenshotError)) {
           const {target, frontend} = await takeScreenshots(name);
-          done.call(this, ScreenshotError.fromBase64Images(err, target, frontend));
-        } else {
-          done.call(this, err);
+          err = ScreenshotError.fromBase64Images(err, target, frontend);
         }
+        done.call(this, err);
       }
       (callback as Mocha.AsyncFunc).bind(this)().then(onError, onError);
     } else {

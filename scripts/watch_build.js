19ffe7f0bcab78e50e5658d5645512e90f1818c3
Ergun Erdogmus <ergunsh@chromium.org>
2023-10-12 17:05:41 +0200

    Enable replacing CSS stylesheets on the fly after file change for watch script
    This CL updates `generate_css_files` script and `watch` script to enable us to get CSS file changes instantly applied for non-legacy CSS files.
    
    Drive by:
    * Added an initial build step before starting to watch changes(1)
    * Improved logs to include the time of the change.
    * Updated `watch` listener to only get activated on `change` events.
    
    (1) was something I was always missing starting this script.
    
    You can try the behavior by:
    * Running `npm run watch` and starting the watch mode.
    * Opening the DevTools.
    * Updating a non-legacy CSS file and expect to see the CSS change instantly on the opened DevTools.
    
    Bug: none for now
    Change-Id: I2aa3ed1b36584d1ef011119d5092d12271fb3017
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4922476
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Ergün Erdoğmuş <ergunsh@chromium.org>

--- scripts/watch_build.js
+++ scripts/watch_build.js
@@ -5,28 +5,71 @@
 const chokidar = require('chokidar');
 const path = require('path');
 const childProcess = require('child_process');
+const fs = require('fs');
 const cwd = process.cwd();
-const env = process.env;
 const frontEndDir = path.join(cwd, 'front_end');
 const testsDir = path.join(cwd, 'test');
+const {WebSocketServer} = require('ws');
+const env = process.env;
+
+const extractArgument = argName => {
+  const arg = process.argv.find(value => value.startsWith(`${argName}`));
+  if (!arg) {
+    return;
+  }
+
+  return arg.slice(`${argName}=`.length);
+};
+
+const relativeFileName = absoluteName => {
+  return path.relative(cwd, absoluteName);
+};
+
+const currentTimeString = () => {
+  return (new Date())
+      .toLocaleTimeString('en-US', {hour12: false, hour: '2-digit', minute: '2-digit', second: '2-digit'});
+};
+
+const connections = {};
+let lastConnectionId = 0;
 
 // Extract the target if it's provided.
-let target = 'Default';
-const targetArg = process.argv.find(value => value.startsWith('--target='));
-if (targetArg) {
-  target = targetArg.slice('--target='.length);
-}
+const target = extractArgument('--target') || 'Default';
+const PORT = 8080;
 
 let restartBuild = false;
 let autoninja;
+
 const changedFiles = new Set();
-const onFileChange = (_, fileName) => {
+
+const startWebSocketServerForCssChanges = () => {
+  const wss = new WebSocketServer({port: PORT});
+
+  wss.on('listening', () => {
+    console.log(`Listening connections for CSS changes at ${PORT}\n`);
+  });
+
+  wss.on('connection', ws => {
+    const connection = {
+      id: ++lastConnectionId,
+      ws,
+    };
+
+    connections[connection.id] = connection;
+    ws.on('close', () => {
+      delete connections[connection.id];
+    });
+  });
+};
+
+const onFileChange = fileName => {
   // Some filesystems emit multiple events in quick succession for a
   // single file change. Here we track the changed files, and reset
   // after a short timeout.
-  if (changedFiles.has(fileName)) {
+  if (!fileName || changedFiles.has(fileName)) {
     return;
   }
+
   changedFiles.add(fileName);
   setTimeout(() => {
     changedFiles.delete(fileName);
@@ -44,18 +87,33 @@ const onFileChange = (_, fileName) => {
     return;
   }
 
+  if (fileName.endsWith('.css')) {
+    console.log(`${currentTimeString()} - ${relativeFileName(fileName)} changed, notifying frontend`);
+    const content = fs.readFileSync(fileName, {encoding: 'utf8', flag: 'r'});
+
+    Object.values(connections).forEach(connection => {
+      connection.ws.send(JSON.stringify({file: fileName, content}));
+    });
+    return;
+  }
+
   autoninja = childProcess.spawn('autoninja', ['-C', `out/${target}`], {cwd, env, stdio: 'inherit'});
   autoninja.on('close', () => {
     autoninja = null;
     if (restartBuild) {
       restartBuild = false;
-      console.log(`\n${fileName} changed, restarting ninja\n`);
+      console.log(`${currentTimeString()}  - ${relativeFileName(fileName)} changed, restarting ninja`);
       onFileChange();
     }
   });
 };
 
+// Run build initially before starting to watch for changes.
+console.log('Running initial build before watching changes');
+childProcess.spawnSync('autoninja', ['-C', `out/${target}`], {cwd, env, stdio: 'inherit'});
+
 // Watch the front_end and test folder and build on any change.
-console.log(`Watching for changes in ${frontEndDir} and ${testsDir}; building to out/${target}`);
-chokidar.watch(frontEndDir).on('all', onFileChange);
-chokidar.watch(testsDir).on('all', onFileChange);
+console.log(`Watching for changes in ${frontEndDir} and ${testsDir}`);
+chokidar.watch(frontEndDir).on('change', onFileChange);
+chokidar.watch(testsDir).on('change', onFileChange);
+startWebSocketServerForCssChanges();

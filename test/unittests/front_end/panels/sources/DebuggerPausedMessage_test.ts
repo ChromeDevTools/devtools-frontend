a27c013e8a7084e8809fe6a0dd055ad0ff5b3492
Simon Zünd <szuend@chromium.org>
2023-10-13 10:38:46 +0000

    Add unit tests for 'EventListener' paused messages
    This CL adds unit tests for DebuggerPausedMessage where the paused
    reason is 'EventListener'. We want to move the corresponding l10n bits
    out of sdk/ and having unit tests upfront makes sure we don't break
    any existing behavior.
    
    R=dsv@chromium.org
    
    Bug: 1445175
    Change-Id: I671633f7bc0b369b5f04d70cb2ab645a0385a039
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4935560
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Danil Somsikov <dsv@chromium.org>

---
+++ test/unittests/front_end/panels/sources/DebuggerPausedMessage_test.ts
@@ -0,0 +1,137 @@
+// Copyright 2023 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {assertNotNullOrUndefined} from '../../../../../front_end/core/platform/platform.js';
+import * as SDK from '../../../../../front_end/core/sdk/sdk.js';
+import * as Protocol from '../../../../../front_end/generated/protocol.js';
+import * as Bindings from '../../../../../front_end/models/bindings/bindings.js';
+import * as Breakpoints from '../../../../../front_end/models/breakpoints/breakpoints.js';
+import * as Workspace from '../../../../../front_end/models/workspace/workspace.js';
+import * as Sources from '../../../../../front_end/panels/sources/sources.js';
+import {assertElement} from '../../helpers/DOMHelpers.js';
+import {describeWithEnvironment} from '../../helpers/EnvironmentHelpers.js';
+
+describeWithEnvironment('DebuggerPausedMessage', () => {
+  let debuggerWorkspaceBinding: Bindings.DebuggerWorkspaceBinding.DebuggerWorkspaceBinding;
+  let breakpointManager: Breakpoints.BreakpointManager.BreakpointManager;
+  let pausedMessage: Sources.DebuggerPausedMessage.DebuggerPausedMessage;
+
+  beforeEach(() => {
+    const workspace = Workspace.Workspace.WorkspaceImpl.instance();
+    const targetManager = SDK.TargetManager.TargetManager.instance();
+    const resourceMapping = new Bindings.ResourceMapping.ResourceMapping(targetManager, workspace);
+    debuggerWorkspaceBinding = Bindings.DebuggerWorkspaceBinding.DebuggerWorkspaceBinding.instance({
+      forceNew: true,
+      resourceMapping,
+      targetManager,
+    });
+    breakpointManager = Breakpoints.BreakpointManager.BreakpointManager.instance({
+      forceNew: true,
+      targetManager,
+      workspace,
+      debuggerWorkspaceBinding,
+    });
+    pausedMessage = new Sources.DebuggerPausedMessage.DebuggerPausedMessage();
+  });
+
+  function getPausedMessageFromDOM(): {main: string, sub?: string} {
+    const mainElement = pausedMessage.element().shadowRoot?.querySelector('.status-main') ?? null;
+    assertElement(mainElement, HTMLDivElement);
+    const main = mainElement.textContent;
+    assertNotNullOrUndefined(main);
+    const sub = pausedMessage.element().shadowRoot?.querySelector('.status-sub')?.textContent ?? undefined;
+    return {main, sub};
+  }
+
+  describe('EventDetails pause', () => {
+    const testCases = [
+      {
+        title: 'shows no sub message if aux data is missing',
+        auxData: undefined,
+        expectedSub: undefined,
+      },
+      {
+        title: 'shows no sub message for unknown instrumentation breakpoints',
+        auxData: {
+          eventName: 'instrumentation:somethingrandom123',
+        },
+        expectedSub: undefined,
+      },
+      {
+        title: 'shows the fixed string for untranslated instrumentation breakpoints',
+        auxData: {
+          eventName: 'instrumentation:setTimeout',
+        },
+        expectedSub: 'setTimeout',
+      },
+      {
+        title: 'shows the translated string for translated instrumentation breakpoints',
+        auxData: {
+          eventName: 'instrumentation:requestAnimationFrame',
+        },
+        expectedSub: 'Request Animation Frame',
+      },
+      {
+        title: 'shows no sub message for unknown listener breakpoints',
+        auxData: {
+          eventName: 'listener:somethingrandom123',
+        },
+        expectedSub: undefined,
+      },
+      {
+        title: 'shows the "targetName" as a prefix for listener breakpoints',
+        auxData: {
+          eventName: 'listener:loadstart',
+          targetName: 'xmlhttprequest',
+        },
+        expectedSub: 'xmlhttprequest.loadstart',
+      },
+      {
+        title: 'shows the "targetName" as a prefix for "*" listener breakpoints',
+        auxData: {
+          eventName: 'listener:pointerover',
+          targetName: 'something-random-123',
+        },
+        expectedSub: 'something-random-123.pointerover',
+      },
+      {
+        title: 'extracts the hex code for WebGL errors',
+        auxData: {
+          eventName: 'instrumentation:webglErrorFired',
+          webglErrorName: 'something 0x42 something',
+        },
+        expectedSub: 'WebGL Error Fired (0x42)',
+      },
+      {
+        title: 'shows the WebGL error name',
+        auxData: {
+          eventName: 'instrumentation:webglErrorFired',
+          webglErrorName: 'something went wrong',
+        },
+        expectedSub: 'WebGL Error Fired (something went wrong)',
+      },
+      {
+        title: 'shows the CSP directive text for script blocked errors',
+        auxData: {
+          eventName: 'instrumentation:scriptBlockedByCSP',
+          directiveText: 'script-src "self"',
+        },
+        expectedSub: 'Script blocked due to Content Security Policy directive: script-src "self"',
+      },
+    ];
+
+    for (const {title, auxData, expectedSub} of testCases) {
+      it(title, async () => {
+        const details = new SDK.DebuggerModel.DebuggerPausedDetails(
+            sinon.createStubInstance(SDK.DebuggerModel.DebuggerModel),
+            /* callFrames */[], Protocol.Debugger.PausedEventReason.EventListener, auxData, /* breakpointIds */[]);
+        await pausedMessage.render(details, debuggerWorkspaceBinding, breakpointManager);
+
+        const {main, sub} = getPausedMessageFromDOM();
+        assert.strictEqual(main, 'Paused on event listener');
+        assert.strictEqual(sub, expectedSub);
+      });
+    }
+  });
+});

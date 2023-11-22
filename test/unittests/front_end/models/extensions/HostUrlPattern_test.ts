00f699975e0334e3e2db08bdbaab7d9941ca1c20
Philip Pfaffe <pfaffe@chromium.org>
2023-11-22 09:24:48 +0000

    Unskip an extensions test
    R=szuend@chromium.org
    
    Bug: 1499180
    Change-Id: Ic09b35859d8c23fb9ae4326bf49e9e5af45ead72
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5050207
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Auto-Submit: Philip Pfaffe <pfaffe@chromium.org>

--- test/unittests/front_end/models/extensions/HostUrlPattern_test.ts
+++ test/unittests/front_end/models/extensions/HostUrlPattern_test.ts
@@ -256,8 +256,7 @@ describe('HostUrlPattern', () => {
     assert.isTrue(pattern.matchesUrl('http://www.example.com:8080/foo' as Platform.DevToolsPath.UrlString));
   });
 
-  // TODO(crbug.com/1499180): Enable once karma uses new headless for running unit tests.
-  it.skip('[crbug.com/1499180] Matches chrome-extension://', () => {
+  it('Matches chrome-extension://', () => {
     const pattern = Extensions.HostUrlPattern.HostUrlPattern.parse('chrome-extension://ftw/*');
     Platform.assertNotNullOrUndefined(pattern);
     assert.strictEqual('chrome-extension', pattern.scheme);

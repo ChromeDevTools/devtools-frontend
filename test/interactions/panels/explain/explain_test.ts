39d40374075d83a40bccc1a1d03207a9b2727452
Alex Rudenko <alexrudenko@chromium.org>
2023-11-22 10:20:08 +0100

    Add screenshot tests for console insights
    This might help us catch any basic visual regressions before shipping.
    
    Bug: b/299423255
    Change-Id: Ic5cef2da46176422e0a1a5588b835d3d68d7977b
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5046568
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>

---
+++ test/interactions/panels/explain/explain_test.ts
@@ -0,0 +1,27 @@
+// Copyright 2023 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {click, waitFor} from '../../../shared/helper.js';
+import {describe, itScreenshot} from '../../../shared/mocha-extensions.js';
+import {assertElementScreenshotUnchanged} from '../../../shared/screenshots.js';
+import {loadComponentDocExample, preloadForCodeCoverage} from '../../helpers/shared.js';
+
+describe('ConsoleInsight', function() {
+  preloadForCodeCoverage('console_insight/static.html');
+
+  // eslint-disable-next-line rulesdir/ban_screenshot_test_outside_perf_panel
+  itScreenshot('renders initial state', async () => {
+    await loadComponentDocExample('console_insight/static.html');
+    await waitFor('.refine-button');
+    await assertElementScreenshotUnchanged(await waitFor('devtools-console-insight'), 'explain/console_insight.png', 3);
+  });
+
+  // eslint-disable-next-line rulesdir/ban_screenshot_test_outside_perf_panel
+  itScreenshot('renders refined state', async () => {
+    await loadComponentDocExample('console_insight/static.html');
+    await click('.refine-button');
+    await assertElementScreenshotUnchanged(
+        await waitFor('devtools-console-insight'), 'explain/console_insight_refined.png', 3);
+  });
+});

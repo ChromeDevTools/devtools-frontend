8cae0a843c81591adceccb7344d890d001442093
Andrés Olivares <andoli@chromium.org>
2023-11-24 10:21:42 +0000

    Add a helper to export a report from all perf tests
    The helper is called in a global `after` statement that will be run
    after all perf tests have finished running. Along with it another
    helper is included to gather the benchmarks of all perf tests.
    
    Bug: 1504824
    Change-Id: Iedb0bc25aee63eac8bbbd3b20ef4d7c1972b2e5a
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5055817
    Commit-Queue: Andres Olivares <andoli@chromium.org>
    Reviewed-by: Jack Franklin <jacktfranklin@chromium.org>

---
+++ test/perf/report/report.ts
@@ -0,0 +1,35 @@
+// Copyright 2023 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import * as fs from 'fs';
+import {join} from 'path';  // eslint-disable-line rulesdir/es_modules_import
+
+export interface Benchmark {
+  name: string;
+  values: number[];
+  mean: number;
+  percentile50: number;
+  percentile90: number;
+  percentile99: number;
+}
+
+const results: Benchmark[] = [];
+
+export function addBenchmarkResult(benchmark: Benchmark) {
+  results.push(benchmark);
+}
+
+export function clearResults() {
+  results.length = 0;
+}
+
+export function writeReport() {
+  // This points to perf-data under devtools root directory
+  // devtools-frontend/perf-data.
+  const directory = join(__dirname, '..', '..', '..', '..', '..', '..', 'perf-data');
+  fs.mkdirSync(directory, {recursive: true});
+
+  const filePath = join(directory, 'devtools-perf.json');
+  fs.writeFileSync(filePath, JSON.stringify(results), {encoding: 'utf8'});
+}

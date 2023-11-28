9ba4749006bf57967e446ffa9fbb66dc0ff7ff8d
Alex Rudenko <alexrudenko@chromium.org>
2023-11-28 10:53:05 +0100

    Add console insight info popover test
    Bug: b/299423255
    Change-Id: I9414e6ebcf3525478ca4d9f61e23d3563b1dc9db
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5065950
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>

--- test/interactions/panels/explain/explain_test.ts
+++ test/interactions/panels/explain/explain_test.ts
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {click, waitFor} from '../../../shared/helper.js';
+import {click, hover, waitFor} from '../../../shared/helper.js';
 import {describe, itScreenshot} from '../../../shared/mocha-extensions.js';
 import {assertElementScreenshotUnchanged} from '../../../shared/screenshots.js';
 import {loadComponentDocExample, preloadForCodeCoverage} from '../../helpers/shared.js';
@@ -23,5 +23,13 @@ describe('ConsoleInsight', function() {
     await click('.refine-button');
     await assertElementScreenshotUnchanged(
         await waitFor('devtools-console-insight'), 'explain/console_insight_refined.png', 3);
+  });
+
+  // eslint-disable-next-line rulesdir/ban_screenshot_test_outside_perf_panel
+  itScreenshot('renders tooltip', async () => {
+    await loadComponentDocExample('console_insight/static.html');
+    await hover('.info');
+    await assertElementScreenshotUnchanged(
+        await waitFor('[data-devtools-glass-pane]'), 'explain/console_insight_info.png', 3);
   });
 });

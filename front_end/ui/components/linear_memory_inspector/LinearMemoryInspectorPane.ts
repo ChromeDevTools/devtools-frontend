6533823f35ce4d93601f111cd599a436aceda96d
Benedikt Meurer <bmeurer@chromium.org>
2023-12-04 09:35:44 +0100

    [lmi] Remove unnecessary tab ID mapping.
    The `#tabIdToInspectorView` field is redundant, as the `TabbedPane`
    literally maintains exactly the same mapping.
    
    Bug: chromium:1507747
    Change-Id: I9803b5f9ccad204fa96b75b09257372e28c0f241
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5083617
    Reviewed-by: Simon Zünd <szuend@chromium.org>

--- front_end/ui/components/linear_memory_inspector/LinearMemoryInspectorPane.ts
+++ front_end/ui/components/linear_memory_inspector/LinearMemoryInspectorPane.ts
@@ -33,7 +33,7 @@ let inspectorInstance: LinearMemoryInspectorPane;
 export class LinearMemoryInspectorPane extends Common.ObjectWrapper.eventMixin<EventTypes, typeof UI.Widget.VBox>(
     UI.Widget.VBox) {
   readonly #tabbedPane: UI.TabbedPane.TabbedPane;
-  readonly #tabIdToInspectorView: Map<string, LinearMemoryInspectorView>;
+
   constructor() {
     super(false);
     this.element.setAttribute('jslog', `${VisualLogging.panel().context('linear-memory-inspector')}`);
@@ -46,8 +46,6 @@ export class LinearMemoryInspectorPane extends Common.ObjectWrapper.eventMixin<E
     this.#tabbedPane.setAllowTabReorder(true, true);
     this.#tabbedPane.addEventListener(UI.TabbedPane.Events.TabClosed, this.#tabClosed, this);
     this.#tabbedPane.show(this.contentElement);
-
-    this.#tabIdToInspectorView = new Map();
   }
 
   static instance(): LinearMemoryInspectorPane {
@@ -57,18 +55,16 @@ export class LinearMemoryInspectorPane extends Common.ObjectWrapper.eventMixin<E
     return inspectorInstance;
   }
 
-  // Introduced to access Views for testings.
-  getViewForTabId(tabId: string): LinearMemoryInspectorView {
-    const view = this.#tabIdToInspectorView.get(tabId);
-    if (!view) {
+  #tabView(tabId: string): LinearMemoryInspectorView {
+    const view = this.#tabbedPane.tabView(tabId);
+    if (view === null) {
       throw new Error(`No linear memory inspector view for the given tab id: ${tabId}`);
     }
-    return view;
+    return view as LinearMemoryInspectorView;
   }
 
   create(tabId: string, title: string, arrayWrapper: LazyUint8Array, address?: number): void {
     const inspectorView = new LinearMemoryInspectorView(arrayWrapper, address, tabId);
-    this.#tabIdToInspectorView.set(tabId, inspectorView);
     this.#tabbedPane.appendTab(tabId, title, inspectorView, undefined, false, true);
     this.#tabbedPane.selectTab(tabId);
   }
@@ -78,7 +74,7 @@ export class LinearMemoryInspectorPane extends Common.ObjectWrapper.eventMixin<E
   }
 
   reveal(tabId: string, address?: number): void {
-    const view = this.getViewForTabId(tabId);
+    const view = this.#tabView(tabId);
 
     if (address !== undefined) {
       view.updateAddress(address);
@@ -88,13 +84,12 @@ export class LinearMemoryInspectorPane extends Common.ObjectWrapper.eventMixin<E
   }
 
   refreshView(tabId: string): void {
-    const view = this.getViewForTabId(tabId);
+    const view = this.#tabView(tabId);
     view.refreshData();
   }
 
   #tabClosed(event: Common.EventTarget.EventTargetEvent<UI.TabbedPane.EventData>): void {
     const {tabId} = event.data;
-    this.#tabIdToInspectorView.delete(tabId);
     this.dispatchEventToListeners(Events.ViewClosed, tabId);
   }
 }

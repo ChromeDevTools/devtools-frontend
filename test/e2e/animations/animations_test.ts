70c877689eeb68ccff11e967e909a7ac928e6b99
Ergun Erdogmus <ergunsh@chromium.org>
2023-12-08 10:51:58 +0100

    [Tests] Move animations panel related web tests to E2E tests
    These tests use `waitForAnimationAdded` which is changed a bit and instead of changing that I have decided to move the tests to devtools-frontend repo.
    
    There is another test[1] that uses this but it is a not very useful test for us now; so I'll just remove it.
    
    Here are the moved tests:
    * http/tests/devtools/animation/animation-timeline.js
    * http/tests/devtools/animation/animation-web-anim-negative-start-time.js
    * http/tests/devtools/animation/animation-KeyframeEffect-crash.js
    
    [1]: third_party/blink/web_tests/http/tests/devtools/animation/animation-transition-setTiming-crash.js
    
    Bug: 1506561
    Change-Id: I2c8eff0db9fd8f36d7ea62ea8f916694b0d2a1ae
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5100070
    Commit-Queue: Ergün Erdoğmuş <ergunsh@chromium.org>
    Reviewed-by: Philip Pfaffe <pfaffe@chromium.org>
    Auto-Submit: Ergün Erdoğmuş <ergunsh@chromium.org>

--- test/e2e/animations/animations_test.ts
+++ test/e2e/animations/animations_test.ts
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import {getBrowserAndPages, goToResource} from '../../shared/helper.js';
 import {describe, it} from '../../shared/mocha-extensions.js';
 import {
   navigateToSiteWithAnimation,
@@ -9,10 +10,46 @@ import {
   waitForAnimationsPanelToLoad,
 } from '../helpers/animations-helpers.js';
 
+const runAnimationTest = async (animationFn: string) => {
+  const {target} = getBrowserAndPages();
+  await waitForAnimationsPanelToLoad();
+  await goToResource('animations/animations-shown.html');
+  await target.evaluate(animationFn);
+  await waitForAnimationContent();
+};
+
 describe('The Animations Panel', async () => {
   it('Listens for animation in webpage', async () => {
     await waitForAnimationsPanelToLoad();
     await navigateToSiteWithAnimation();
     await waitForAnimationContent();
+  });
+
+  it('WAAPI animation with delay is displayed on the timeline', async () => {
+    await runAnimationTest('startAnimationWithDelay()');
+  });
+
+  it('WAAPI animation with end delay is displayed on the timeline', async () => {
+    await runAnimationTest('startAnimationWithEndDelay()');
+  });
+
+  it('WAAPI animation with negative start time is dplayed on the timeline', async () => {
+    await runAnimationTest('startAnimationWithNegativeStartTime()');
+  });
+
+  it('WAAPI animation with step timing is displayed on the timeline', async () => {
+    await runAnimationTest('startAnimationWithStepTiming()');
+  });
+
+  it('WAAPI animation with keyframe effect is displayed on the timeline', async () => {
+    await runAnimationTest('startAnimationWithKeyframeEffect()');
+  });
+
+  it('CSS animation is displayed on the timeline', async () => {
+    await runAnimationTest('startCSSAnimation()');
+  });
+
+  it('CSS transition is displayed on the timeline', async () => {
+    await runAnimationTest('startCSSTransition()');
   });
 });

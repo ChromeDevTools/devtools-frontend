eb245ba3881d39fccf59bebc67f329e170a98c95
Benedikt Meurer <bmeurer@chromium.org>
2023-12-11 15:14:29 +0100

    [e2e] Wait for new device to be saved.
    Fixed: chromium:1510402
    Change-Id: I36dea0b5f327c388e3ced875415a493c5db6b933
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5110563
    Auto-Submit: Benedikt Meurer <bmeurer@chromium.org>
    Reviewed-by: Eric Leese <leese@chromium.org>
    Commit-Queue: Benedikt Meurer <bmeurer@chromium.org>
    Commit-Queue: Eric Leese <leese@chromium.org>

--- test/e2e/emulation/custom-devices_test.ts
+++ test/e2e/emulation/custom-devices_test.ts
@@ -3,8 +3,10 @@
 // found in the LICENSE file.
 import {assert} from 'chai';
 import type * as puppeteer from 'puppeteer-core';
+
 import {
   click,
+  clickElement,
   getBrowserAndPages,
   goToResource,
   pressKey,
@@ -12,7 +14,7 @@ import {
   typeText,
   waitFor,
   waitForAria,
-  clickElement,
+  waitForNone,
 } from '../../shared/helper.js';
 import {describe, it} from '../../shared/mocha-extensions.js';
 import {waitForDomNodeToBeVisible} from '../helpers/elements-helpers.js';
@@ -200,6 +202,7 @@ describe('Custom devices', async () => {
     const finishAddText = await elementTextContent(finishAdd);
     assert.strictEqual(finishAddText, 'Add');
     await clickElement(finishAdd);
+    await waitForNone(FOCUSED_DEVICE_NAME_FIELD_SELECTOR);
 
     // Select the device in the menu.
     await selectDevice('Prime numbers');

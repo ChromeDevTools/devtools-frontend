d6acf695122d34bedd740c12e1eeffdef8af9059
Jack Franklin <jacktfranklin@chromium.org>
2023-12-13 14:13:47 +0000

    Improve main activity zoom for CPU Profiles
    By dropping idle events before calculating the window.
    
    Bug: 1464206
    Change-Id: I0ac5011124f3db52d44ae13ebd4d03b071f16cd9
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5116604
    Reviewed-by: Andres Olivares <andoli@chromium.org>
    Auto-Submit: Jack Franklin <jacktfranklin@chromium.org>
    Commit-Queue: Andres Olivares <andoli@chromium.org>

--- front_end/models/trace/extras/MainThreadActivity.ts
+++ front_end/models/trace/extras/MainThreadActivity.ts
@@ -4,12 +4,25 @@
 import * as Helpers from '../helpers/helpers.js';
 import * as Types from '../types/types.js';
 
+const IDLE_FUNCTION_CALL_NAMES = new Set([
+  '(program)',
+  '(idle)',
+  '(root)',
+]);
+
 export function calculateWindow(
     traceBounds: Types.Timing.TraceWindowMicroSeconds,
     mainThreadEntries: readonly Types.TraceEvents.TraceEntry[]): Types.Timing.TraceWindowMicroSeconds {
   if (!mainThreadEntries.length) {
     return traceBounds;
   }
+  const entriesWithIdleRemoved = mainThreadEntries.filter(entry => {
+    if (Types.TraceEvents.isProfileCall(entry) &&
+        (IDLE_FUNCTION_CALL_NAMES.has(entry.callFrame.functionName) || !entry.callFrame.functionName)) {
+      return false;
+    }
+    return true;
+  });
   /**
    * Calculates regions of low utilization and returns the index of the event
    * that is the first event that should be included.
@@ -18,13 +31,13 @@ export function calculateWindow(
     const threshold = 0.1;
 
     let cutIndex = startIndex;
-    const entryAtCut = mainThreadEntries[cutIndex];
+    const entryAtCut = entriesWithIdleRemoved[cutIndex];
     const timings = Helpers.Timing.eventTimingsMicroSeconds(entryAtCut);
     let cutTime = (timings.startTime + timings.endTime) / 2;
     let usedTime = 0;
     const step = Math.sign(stopIndex - startIndex);
     for (let i = startIndex; i !== stopIndex; i += step) {
-      const task = mainThreadEntries[i];
+      const task = entriesWithIdleRemoved[i];
       const taskTimings = Helpers.Timing.eventTimingsMicroSeconds(task);
       const taskTime = (taskTimings.startTime + taskTimings.endTime) / 2;
       const interval = Math.abs(cutTime - taskTime);
@@ -37,10 +50,10 @@ export function calculateWindow(
     }
     return cutIndex;
   }
-  const rightIndex = findLowUtilizationRegion(mainThreadEntries.length - 1, 0);
+  const rightIndex = findLowUtilizationRegion(entriesWithIdleRemoved.length - 1, 0);
   const leftIndex = findLowUtilizationRegion(0, rightIndex);
-  const leftTimings = Helpers.Timing.eventTimingsMicroSeconds(mainThreadEntries[leftIndex]);
-  const rightTimings = Helpers.Timing.eventTimingsMicroSeconds(mainThreadEntries[rightIndex]);
+  const leftTimings = Helpers.Timing.eventTimingsMicroSeconds(entriesWithIdleRemoved[leftIndex]);
+  const rightTimings = Helpers.Timing.eventTimingsMicroSeconds(entriesWithIdleRemoved[rightIndex]);
 
   let leftTime = leftTimings.startTime;
   let rightTime = rightTimings.endTime;

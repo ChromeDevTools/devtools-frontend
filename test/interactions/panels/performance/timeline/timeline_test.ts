ce18aca78b333046cdfaaeb0c58fd1cdcc697a71
Jack Franklin <jacktfranklin@chromium.org>
2023-12-18 12:17:24 +0000

    Attempt to deflake RPP data grid tests
    By making sure they have some height.
    
    I also don't think we need as many screenshots here so I have removed
    some of them. We should work on testing those components in isolation
    and not requiring to render the entire panel
    
    Bug: 1478133
    Change-Id: Ifd9550277a15c4857e6cdb7d937f0aa7b863f80b
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5123455
    Commit-Queue: Andres Olivares <andoli@chromium.org>
    Reviewed-by: Andres Olivares <andoli@chromium.org>
    Commit-Queue: Jack Franklin <jacktfranklin@chromium.org>
    Auto-Submit: Jack Franklin <jacktfranklin@chromium.org>

--- test/interactions/panels/performance/timeline/timeline_test.ts
+++ test/interactions/panels/performance/timeline/timeline_test.ts
@@ -19,87 +19,32 @@ describe('Performance panel', function() {
     await assertElementScreenshotUnchanged(panel, 'performance/timeline.png', 3);
   });
 
-  // Flaky test
-  itScreenshot.skip('[crbug.com/1478133] renders correctly the Bottom Up datagrid', async () => {
+  itScreenshot('renders correctly the Bottom Up datagrid', async () => {
     await loadComponentDocExample('performance_panel/basic.html?trace=one-second-interaction');
     await waitFor('.timeline-flamechart');
     await waitFor('div.tabbed-pane');
     await click('#tab-BottomUp');
     const datagrid = await waitFor('.timeline-tree-view');
+    await waitForFunction(async () => {
+      const datagrid = await waitFor('.timeline-tree-view');
+      const height = await datagrid.evaluate(elem => elem.clientHeight);
+      return height > 150;
+    });
     await assertElementScreenshotUnchanged(datagrid, 'performance/bottomUp.png', 3);
   });
 
-  // Flaky test
-  itScreenshot.skip('[crbug.com/1478133] renders correctly the Call Tree datagrid', async () => {
+  itScreenshot('renders correctly the Call Tree datagrid', async () => {
     await loadComponentDocExample('performance_panel/basic.html?trace=one-second-interaction');
     await waitFor('.timeline-flamechart');
     await waitFor('div.tabbed-pane');
     await click('#tab-CallTree');
     const datagrid = await waitFor('.timeline-tree-view');
-    await assertElementScreenshotUnchanged(datagrid, 'performance/callTree.png', 3);
-  });
-
-  // Flaky test
-  itScreenshot.skip('[crbug.com/1478133] renders correctly the Event Log datagrid', async () => {
-    await loadComponentDocExample('performance_panel/basic.html?trace=one-second-interaction');
-    await waitFor('.timeline-flamechart');
-    await waitFor('div.tabbed-pane');
-    await click('#tab-EventLog');
-    const datagrid = await waitFor('.timeline-tree-view');
-    // This value is obtained by waiting for the scroll of the datagrid to be completed
-    const TOP_OFFSET = 2938;
-    const scrollableDatagrid = await waitFor('.data-container');
-    // Wait for the scroll of the datagrid to be done before taking a screenshot
     await waitForFunction(async () => {
-      const scrollablePosition = await scrollableDatagrid.evaluate(el => {
-        return el.scrollTop;
-      });
-      return scrollablePosition === TOP_OFFSET;
+      const datagrid = await waitFor('.timeline-tree-view');
+      const height = await datagrid.evaluate(elem => elem.clientHeight);
+      return height > 150;
     });
-
-    await assertElementScreenshotUnchanged(datagrid, 'performance/eventLog.png', 4);
-  });
-
-  // Flaky test
-  itScreenshot.skip('[crbug.com/1478133] renders correctly the datagrid in the split widget of Bottom Up', async () => {
-    await loadComponentDocExample('performance_panel/basic.html?trace=one-second-interaction');
-    await waitFor('.timeline-flamechart');
-    await waitFor('div.tabbed-pane');
-    await click('#tab-BottomUp');
-    const datagrid = await waitFor('.timeline-tree-view');
-    await click('[aria-label="Show Heaviest stack"]');
-    const rows = await datagrid.$$('.data-grid-data-grid-node');
-
-    // The trace one-second-interaction contains more than 3 rows in the bottom up tree
-    // so it is safe to click the third one
-    if (rows.length >= 3) {
-      await rows[2].click();
-    } else {
-      throw new Error('There are less than three rows with the class \'data-grid-data-grid-node\'');
-    }
-
-    await assertElementScreenshotUnchanged(datagrid, 'performance/splitWidgetBottomUp.png', 3);
-  });
-
-  // Flaky test
-  itScreenshot.skip('[crbug.com/1478133] renders correctly the datagrid in the split widget of Call Tree', async () => {
-    await loadComponentDocExample('performance_panel/basic.html?trace=one-second-interaction');
-    await waitFor('.timeline-flamechart');
-    await waitFor('div.tabbed-pane');
-    await click('#tab-CallTree');
-    const datagrid = await waitFor('.timeline-tree-view');
-    await click('[aria-label="Show Heaviest stack"]');
-    const rows = await datagrid.$$('.data-grid-data-grid-node');
-
-    // The trace one-second-interaction contains more than 3 rows in the call tree
-    // so it is safe to click the third one
-    if (rows.length >= 3) {
-      await rows[2].click();
-    } else {
-      throw new Error('There are less than three rows with the class \'data-grid-data-grid-node\'');
-    }
-
-    await assertElementScreenshotUnchanged(datagrid, 'performance/splitWidgetCallTree.png', 3);
+    await assertElementScreenshotUnchanged(datagrid, 'performance/callTree.png', 3);
   });
 
   itScreenshot('renders the timeline correctly when scrolling', async () => {

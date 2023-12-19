99425b58a129f221ff3eaa5d2d86487f36cfb584
Kateryna Prokopenko <kprokopenko@chromium.org>
2023-12-19 14:24:23 +0000

    Visual logging for Application > Cookies
    Bug: b/308381403
    Change-Id: Ia680e394dcb5d1e8d49fd1cd70bb22a216d0e1b2
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5134892
    Auto-Submit: Kateryna Prokopenko <kprokopenko@chromium.org>
    Reviewed-by: Benedikt Meurer <bmeurer@chromium.org>
    Commit-Queue: Benedikt Meurer <bmeurer@chromium.org>

--- front_end/panels/application/CookieItemsView.ts
+++ front_end/panels/application/CookieItemsView.ts
@@ -37,6 +37,7 @@ import * as SDK from '../../core/sdk/sdk.js';
 import * as IssuesManager from '../../models/issues_manager/issues_manager.js';
 import * as CookieTable from '../../ui/legacy/components/cookie_table/cookie_table.js';
 import * as UI from '../../ui/legacy/legacy.js';
+import * as VisualLogging from '../../ui/visual_logging/visual_logging.js';
 
 import cookieItemsViewStyles from './cookieItemsView.css.js';
 import {StorageItemsView} from './StorageItemsView.js';
@@ -98,7 +99,8 @@ class CookiePreviewWidget extends UI.Widget.VBox {
     header.appendChild(span);
     this.contentElement.appendChild(header);
 
-    const toggle = UI.UIUtils.CheckboxLabel.create(i18nString(UIStrings.showUrlDecoded), this.showDecodedSetting.get());
+    const toggle = UI.UIUtils.CheckboxLabel.create(
+        i18nString(UIStrings.showUrlDecoded), this.showDecodedSetting.get(), undefined, 'show-url-decoded');
     toggle.classList.add('cookie-preview-widget-toggle');
     toggle.checkboxElement.addEventListener('click', () => this.showDecoded(!this.showDecodedSetting.get()));
     header.appendChild(toggle);
@@ -111,6 +113,7 @@ class CookiePreviewWidget extends UI.Widget.VBox {
     this.value = value;
 
     this.contentElement.classList.add('cookie-preview-widget');
+    this.contentElement.setAttribute('jslog', `${VisualLogging.section().context('cookie-preview')}`);
     this.contentElement.appendChild(value);
   }
 
@@ -172,6 +175,7 @@ export class CookieItemsView extends StorageItemsView {
     super(i18nString(UIStrings.cookies), 'cookiesPanel');
 
     this.element.classList.add('storage-view');
+    this.element.setAttribute('jslog', `${VisualLogging.pane().context('cookies')}`);
 
     this.model = model;
     this.cookieDomain = cookieDomain;
@@ -201,7 +205,7 @@ export class CookieItemsView extends StorageItemsView {
     this.onlyIssuesFilterUI = new UI.Toolbar.ToolbarCheckbox(
         i18nString(UIStrings.onlyShowCookiesWithAnIssue), i18nString(UIStrings.onlyShowCookiesWhichHaveAn), () => {
           this.updateWithCookies(this.allCookies);
-        });
+        }, 'only-show-cookies-with-issues');
     this.appendToolbarItem(this.onlyIssuesFilterUI);
 
     this.refreshThrottler = new Common.Throttler.Throttler(300);

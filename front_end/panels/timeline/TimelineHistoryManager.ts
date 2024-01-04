86e5954812146251c4073ab2740714c11a9419c9
Benedikt Meurer <bmeurer@chromium.org>
2024-01-04 09:47:08 +0100

    [DevToolsIcons] Migrate timeline history button off of legacy `Icon`.
    Bug: chromium:1514825, chromium:1421730
    Change-Id: I88a986123e384d0595a3184b2a3da60ea4496ed8
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5166408
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Benedikt Meurer <bmeurer@chromium.org>

--- front_end/panels/timeline/TimelineHistoryManager.ts
+++ front_end/panels/timeline/TimelineHistoryManager.ts
@@ -540,8 +540,6 @@ export class ToolbarButton extends UI.Toolbar.ToolbarItem {
     element.classList.add('history-dropdown-button');
     super(element);
     this.contentElement = this.element.createChild('span', 'content');
-    const dropdownArrowIcon = UI.Icon.Icon.create('triangle-down');
-    this.element.appendChild(dropdownArrowIcon);
     this.element.addEventListener('click', () => void action.execute(), false);
     this.setEnabled(action.enabled());
     action.addEventListener(UI.ActionRegistration.Events.Enabled, event => this.setEnabled(event.data));

b955372986361ab89ef035bb46c14e7e87f62941
Benedikt Meurer <bmeurer@chromium.org>
2024-01-08 06:56:03 +0100

    [cleanup] Remove ObjectEventListenersSidebarPane.instance().
    Also simplify the class quite a bit and turn the refresh toolbar button
    into a proper Action, which also automatically instruments the button
    for VE logging.
    
    Bug: chromium:1379221, b:308382451
    Change-Id: I990253c98c3141e052eac7a91ccb13075087c70a
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5173891
    Commit-Queue: Benedikt Meurer <bmeurer@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Auto-Submit: Benedikt Meurer <bmeurer@chromium.org>

--- front_end/panels/browser_debugger/browser_debugger-meta.ts
+++ front_end/panels/browser_debugger/browser_debugger-meta.ts
@@ -2,13 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import * as i18n from '../../core/i18n/i18n.js';
 import * as SDK from '../../core/sdk/sdk.js';
 import * as UI from '../../ui/legacy/legacy.js';
 import type * as Sources from '../sources/sources.js';
 
 import type * as BrowserDebugger from './browser_debugger.js';
 
-import * as i18n from '../../core/i18n/i18n.js';
 const UIStrings = {
   /**
    *@description Command for showing the 'Event Listener Breakpoints' tool
@@ -74,6 +74,10 @@ const UIStrings = {
    *@description Command for showing the 'Content scripts' tool in the sources panel
    */
   showContentScripts: 'Show Content scripts',
+  /**
+   *@description Label for a button in the sources panel that refreshes the list of global event listeners.
+   */
+  refreshGlobalListeners: 'Refresh global listeners',
 };
 const str_ = i18n.i18n.registerUIStrings('panels/browser_debugger/browser_debugger-meta.ts', UIStrings);
 const i18nLazyString = i18n.i18n.getLazilyComputedLocalizedString.bind(undefined, str_);
@@ -84,6 +88,14 @@ async function loadBrowserDebuggerModule(): Promise<typeof BrowserDebugger> {
     loadedBrowserDebuggerModule = await import('./browser_debugger.js');
   }
   return loadedBrowserDebuggerModule;
+}
+
+function maybeRetrieveContextTypes<T = unknown>(
+    getClassCallBack: (loadedBrowserDebuggerModule: typeof BrowserDebugger) => T[]): T[] {
+  if (loadedBrowserDebuggerModule === undefined) {
+    return [];
+  }
+  return getClassCallBack(loadedBrowserDebuggerModule);
 }
 
 let loadedSourcesModule: (typeof Sources|undefined);
@@ -156,7 +168,7 @@ UI.ViewManager.registerViewExtension({
 UI.ViewManager.registerViewExtension({
   async loadView() {
     const BrowserDebugger = await loadBrowserDebuggerModule();
-    return BrowserDebugger.ObjectEventListenersSidebarPane.ObjectEventListenersSidebarPane.instance();
+    return new BrowserDebugger.ObjectEventListenersSidebarPane.ObjectEventListenersSidebarPane();
   },
   id: 'sources.globalListeners',
   location: UI.ViewManager.ViewLocationValues.SOURCES_SIDEBAR_BOTTOM,
@@ -216,6 +228,23 @@ UI.ViewManager.registerViewExtension({
   async loadView() {
     const Sources = await loadSourcesModule();
     return new Sources.SourcesNavigator.ContentScriptsNavigatorView();
+  },
+});
+
+UI.ActionRegistration.registerActionExtension({
+  category: UI.ActionRegistration.ActionCategory.DEBUGGER,
+  actionId: 'browser-debugger.refresh-global-event-listeners',
+  async loadActionDelegate() {
+    const BrowserDebugger = await loadBrowserDebuggerModule();
+    return new BrowserDebugger.ObjectEventListenersSidebarPane.ActionDelegate();
+  },
+  title: i18nLazyString(UIStrings.refreshGlobalListeners),
+  iconClass: UI.ActionRegistration.IconClass.REFRESH,
+  contextTypes() {
+    return maybeRetrieveContextTypes(
+        BrowserDebugger =>
+            [BrowserDebugger.ObjectEventListenersSidebarPane.ObjectEventListenersSidebarPane,
+    ]);
   },
 });
 

d66f6544f4f9a9abb0ecb31f476541ba9796bc7f
Alina Varkki <alinavarkki@google.com>
2024-01-09 13:09:50 +0000

    [RPP] Show Breadcrumbs only after one was created
    Bug: 1467739
    Change-Id: Ic3a0b28c9efe1e610e8f2664db9c01f59c75d804
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5180529
    Commit-Queue: Alina Varkki <alinavarkki@chromium.org>
    Reviewed-by: Nancy Li <nancyly@chromium.org>

--- front_end/panels/timeline/components/BreadcrumbsUI.ts
+++ front_end/panels/timeline/components/BreadcrumbsUI.ts
@@ -43,11 +43,13 @@ export class BreadcrumbsUI extends HTMLElement {
     this.dispatchEvent(new BreadcrumbRemovedEvent(breadcrumb));
   }
 
-  #scrollLastCrumbIntoView(): void {
+  #showBreadcrumbsAndScrollLastCrumbIntoView(): void {
     const container = this.#shadow.querySelector<HTMLDivElement>('.breadcrumbs');
     if (!container) {
       return;
     }
+    // Display Breadcrumbs after at least one was created
+    container.style.display = 'flex';
     requestAnimationFrame(() => {
       // If the width of all the elements is greater than the width of the
       // container, we need to scroll the last element into view.
@@ -99,8 +101,8 @@ export class BreadcrumbsUI extends HTMLElement {
     // clang-format on
     render(output, this.#shadow, {host: this});
     if (this.#breadcrumb?.child) {
-      // If we have >1 crumbs, ensure the last one is visible by scrolling the container.
-      this.#scrollLastCrumbIntoView();
+      // If we have >1 crumbs show breadcrumbs and ensure the last one is visible by scrolling the container.
+      this.#showBreadcrumbsAndScrollLastCrumbIntoView();
     }
   }
 }

ed78339df93c95531de9dd50a73204fc2a84aef1
Philip Pfaffe <pfaffe@chromium.org>
2024-01-10 13:43:13 +0000

    [elements] Add an extended API for matchers
    Some matchers will find it useful to reason about matched pieces of their corresponding tree children. For example the color-mix swatch is wired up with the inner color swatches to produce the color-mix icon. To properly render the properties it's also important to understand computed values of any var()s. This CL therefore adds two features:
    - A mechanism to lazily build the computed text from matched chunks
    - A registry for UI controls that renderers use to pass swatches, e.g.,
      to parent renderers.
    
    Bug: chromium:1504820
    Change-Id: I3a3ce2ecdcff91681c553651d52c5dd12b0e2f16
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5099445
    Reviewed-by: Ergün Erdoğmuş <ergunsh@chromium.org>
    Commit-Queue: Philip Pfaffe <pfaffe@chromium.org>

--- test/unittests/front_end/panels/elements/PropertyParser_test.ts
+++ test/unittests/front_end/panels/elements/PropertyParser_test.ts
@@ -65,9 +65,10 @@ describe('PropertyParser', () => {
     const tree = cssParser.parse(rule).topNode;
     const ast = new Elements.PropertyParser.SyntaxTree(property, rule, tree);
     const matchedResult = Elements.PropertyParser.BottomUpTreeMatching.walk(ast, []);
-    const context = {ast, matchedResult};
+    const context = new Elements.PropertyParser.RenderingContext(ast, matchedResult);
     assert.deepStrictEqual(
-        textFragments(Elements.PropertyParser.Renderer.render(tree, context)).join(''), rule, Printer.walk(ast).get());
+        textFragments(Elements.PropertyParser.Renderer.render(tree, context).nodes).join(''), rule,
+        Printer.walk(ast).get());
   });
 
   it('correctly identifies spacing', () => {
@@ -128,9 +129,9 @@ describe('PropertyParser', () => {
     Platform.assertNotNullOrUndefined(tree);
     const ast = new Elements.PropertyParser.SyntaxTree(property, rule, tree);
     const matchedResult = Elements.PropertyParser.BottomUpTreeMatching.walk(ast, []);
-    const context = {ast, matchedResult};
+    const context = new Elements.PropertyParser.RenderingContext(ast, matchedResult);
     assert.deepStrictEqual(
-        textFragments(Elements.PropertyParser.Renderer.render(tree, context)).join(''), `--property: ${property}`,
+        textFragments(Elements.PropertyParser.Renderer.render(tree, context).nodes).join(''), `--property: ${property}`,
         Printer.walk(ast).get());
   });
 
@@ -169,5 +170,133 @@ describe('PropertyParser', () => {
     assert.isNull(Elements.PropertyParser.tokenizePropertyValue(''));
     assert.isNull(Elements.PropertyParser.tokenizePropertyValue('/*'));
     assert.isNull(Elements.PropertyParser.tokenizePropertyValue('}'));
+  });
+
+  class ComputedTextMatch implements Elements.PropertyParser.Match {
+    type: string = 'computed-text-test';
+    constructor(readonly text: string, readonly constructedText: string) {
+    }
+    render(): Node[] {
+      return [];
+    }
+    computedText?(): string {
+      return this.constructedText;
+    }
+  }
+
+  it('computes ComputedText', () => {
+    const originalText = 'abcdefghijklmnopqrstuvwxyz';
+    // computed text:    '        +++--     ------  '
+    // Where + means a replacement, - means a deletion, i.e., computed texts are shorter than the corresponding
+    // original snippet.
+
+    const computedText = new Elements.PropertyParser.ComputedText(originalText);
+
+    // 'abcdefghijklmnopqrstuvwxyz'
+    //    |----|
+    assert.strictEqual(computedText.get(2, 8), 'cdefgh');
+
+    computedText.push(new ComputedTextMatch('ijklm', '012'), originalText.indexOf('i'));
+    computedText.push(new ComputedTextMatch('stuvwx', ''), originalText.indexOf('s'));
+
+    // Range starts in original text before the first chunk, ends in original text before the first chunk
+    // 'abcdefghijklmnopqrstuvwxyz'
+    //    |----|
+    // 'abcdefgh012  nopqr      yz'
+    assert.strictEqual(computedText.get(originalText.indexOf('c'), originalText.indexOf('h')), 'cdefg');
+
+    // Range ends in original text after the first chunk
+    // 'abcdefghijklmnopqrstuvwxyz'
+    //    |-----------|
+    // 'abcdefgh012  nopqr      yz'
+    assert.strictEqual(computedText.get(originalText.indexOf('c'), originalText.indexOf('o')), 'cdefgh012n');
+
+    // Range ends in original text ends on the beginning of a chunk
+    // 'abcdefghijklmnopqrstuvwxyz'
+    //    |-----|
+    // 'abcdefgh012  nopqr      yz'
+    assert.strictEqual(computedText.get(originalText.indexOf('c'), originalText.indexOf('i')), 'cdefgh');
+
+    // Range ends in original text ends on the end of a chunk
+    // 'abcdefghijklmnopqrstuvwxyz'
+    //    |----------|
+    // 'abcdefgh012  nopqr      yz'
+    assert.strictEqual(computedText.get(originalText.indexOf('c'), originalText.indexOf('n')), 'cdefgh012');
+
+    // Range ends in original text after the second chunk
+    // 'abcdefghijklmnopqrstuvwxyz'
+    //    |----------------------|
+    // 'abcdefgh012  nopqr      yz'
+    assert.strictEqual(computedText.get(originalText.indexOf('c'), originalText.indexOf('z')), 'cdefgh012nopqry');
+
+    // Range ends in original text after the second chunk containing no chunk
+    // 'abcdefghijklmnopqrstuvwxyz'
+    //                          ||
+    // 'abcdefgh012  nopqr      yz'
+    assert.strictEqual(computedText.get(originalText.indexOf('y'), originalText.indexOf('z') + 1), 'yz');
+
+    // Range ends in original text on the end of the second chunk
+    // 'abcdefghijklmnopqrstuvwxyz'
+    //    |---------------------|
+    // 'abcdefgh012  nopqr      yz'
+    assert.strictEqual(computedText.get(originalText.indexOf('c'), originalText.indexOf('y')), 'cdefgh012nopqr');
+
+    // range starts in original text after the chunk
+    // 'abcdefghijklmnopqrstuvwxyz'
+    //                |-|
+    // 'abcdefgh012  nopqr      yz'
+    assert.strictEqual(computedText.get(originalText.indexOf('o'), originalText.indexOf('q')), 'op');
+
+    // range starts on the first chunk
+    // 'abcdefghijklmnopqrstuvwxyz'
+    //          |-------|
+    // 'abcdefgh012  nopqr      yz'
+    assert.strictEqual(computedText.get(originalText.indexOf('i'), originalText.indexOf('q')), '012nop');
+
+    // range starts on the second chunk
+    // 'abcdefghijklmnopqrstuvwxyz'
+    //                    |------|
+    // 'abcdefgh012  nopqr      yz'
+    assert.strictEqual(computedText.get(originalText.indexOf('s'), originalText.indexOf('z')), 'y');
+
+    // range starts in the middle of a chunk
+    // 'abcdefghijklmnopqrstuvwxyz'
+    //           |-----|
+    // 'abcdefgh012  nopqr      yz'
+    assert.strictEqual(computedText.get(originalText.indexOf('j'), originalText.indexOf('p')), 'jklmno');
+
+    // range ends in the middle of a chunk
+    // 'abcdefghijklmnopqrstuvwxyz'
+    //       |-----|
+    // 'abcdefgh012  nopqr      yz'
+    assert.strictEqual(computedText.get(originalText.indexOf('f'), originalText.indexOf('l')), 'fgh');
+  });
+
+  it('computes ComputedText with back-to-back chunks', () => {
+    const computedText = new Elements.PropertyParser.ComputedText('abcdefgh');
+    computedText.push(new ComputedTextMatch('abcd', '01234'), 0);
+    computedText.push(new ComputedTextMatch('efgh', '56789'), 4);
+    assert.strictEqual(computedText.get(0, 8), '0123456789');
+  });
+
+  it('correctly substitutes more general matches', () => {
+    const computedText = new Elements.PropertyParser.ComputedText('abcdefgh');
+    computedText.push(new ComputedTextMatch('cd', '01'), 2);
+    computedText.push(new ComputedTextMatch('fg', '56'), 5);
+    computedText.push(new ComputedTextMatch('bcdefgh', 'xx'), 1);
+    assert.strictEqual(computedText.chunkCount, 1);
+    assert.strictEqual(computedText.get(0, 8), 'axx');
+  });
+
+  it('correctly produces the computed text during matching', () => {
+    const ast = tokenizePropertyValue('1px /* red */ solid');
+    const width = ast.tree.parent?.getChild('NumberLiteral');
+    Platform.assertNotNullOrUndefined(width);
+    const style = ast.tree.parent?.getChild('ValueName');
+    Platform.assertNotNullOrUndefined(style);
+    const matching = Elements.PropertyParser.BottomUpTreeMatching.walk(ast, []);
+    assert.strictEqual(matching.computedText.get(0, ast.propertyValue.length), '1px  solid');
+    assert.strictEqual(matching.getComputedText(width), '1px');
+    assert.strictEqual(matching.getComputedText(style), 'solid');
   });
 });

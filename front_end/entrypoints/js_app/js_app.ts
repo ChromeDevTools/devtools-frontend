cf9d2920ef2ba5593942b12c5a69744e184f97ac
Benedikt Meurer <bmeurer@chromium.org>
2024-01-26 14:37:48 +0100

    [js_app] Add navigator tree to the js_app.
    When using chrome://inspect to debug Node instances, Chrome will use the
    js_app entrypoint (unlike when using the dedicated node_app via the Node
    dedicated page), which up until now lacked the tree view in the Sources
    panel navigator sidebar.
    
    This CL hooks up the tree - named 'Scripts', since with the js_app only
    scripts will show up in that tree.
    
    Fixed: chromium:1518364
    Screenshot: https://i.imgur.com/1zbVicE.png
    Change-Id: I0550ebc08657e5a62e239e1d4dac38c6c1ec8db4
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5238549
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Auto-Submit: Benedikt Meurer <bmeurer@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>

--- front_end/entrypoints/js_app/js_app.ts
+++ front_end/entrypoints/js_app/js_app.ts
@@ -1,15 +1,18 @@
 // Copyright 2018 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+
 import '../shell/shell.js';
-import '../../panels/mobile_throttling/mobile_throttling-meta.js';
 import '../../panels/js_profiler/js_profiler-meta.js';
+import '../../panels/mobile_throttling/mobile_throttling-meta.js';
 
 import * as Common from '../../core/common/common.js';
 import * as Host from '../../core/host/host.js';
 import * as i18n from '../../core/i18n/i18n.js';
 import * as SDK from '../../core/sdk/sdk.js';
+import type * as Sources from '../../panels/sources/sources.js';
 import * as Components from '../../ui/legacy/components/utils/utils.js';
+import * as UI from '../../ui/legacy/legacy.js';
 import * as Main from '../main/main.js';
 
 const UIStrings = {
@@ -17,13 +20,29 @@ const UIStrings = {
    *@description Text that refers to the main target.
    */
   main: 'Main',
+  /**
+   *@description Title of the 'Scripts' tool in the Network Navigator View, which is part of the Sources tool
+   */
+  networkTitle: 'Scripts',
+  /**
+   *@description Command for showing the 'Scripts' tool in the Network Navigator View, which is part of the Sources tool
+   */
+  showNode: 'Show Scripts',
 };
 
 const str_ = i18n.i18n.registerUIStrings('entrypoints/js_app/js_app.ts', UIStrings);
 const i18nString = i18n.i18n.getLocalizedString.bind(undefined, str_);
+const i18nLazyString = i18n.i18n.getLazilyComputedLocalizedString.bind(undefined, str_);
 
 let jsMainImplInstance: JsMainImpl;
+let loadedSourcesModule: (typeof Sources|undefined);
 
+async function loadSourcesModule(): Promise<typeof Sources> {
+  if (!loadedSourcesModule) {
+    loadedSourcesModule = await import('../../panels/sources/sources.js');
+  }
+  return loadedSourcesModule;
+}
 export class JsMainImpl implements Common.Runnable.Runnable {
   static instance(opts: {forceNew: boolean|null} = {forceNew: null}): JsMainImpl {
     const {forceNew} = opts;
@@ -43,6 +62,19 @@ export class JsMainImpl implements Common.Runnable.Runnable {
     }, Components.TargetDetachedDialog.TargetDetachedDialog.webSocketConnectionLost);
   }
 }
+
+UI.ViewManager.registerViewExtension({
+  location: UI.ViewManager.ViewLocationValues.NAVIGATOR_VIEW,
+  id: 'navigator-network',
+  title: i18nLazyString(UIStrings.networkTitle),
+  commandPrompt: i18nLazyString(UIStrings.showNode),
+  order: 2,
+  persistence: UI.ViewManager.ViewPersistence.PERMANENT,
+  async loadView() {
+    const Sources = await loadSourcesModule();
+    return Sources.SourcesNavigator.NetworkNavigatorView.instance();
+  },
+});
 
 Common.Runnable.registerEarlyInitializationRunnable(JsMainImpl.instance);
 new Main.MainImpl.MainImpl();

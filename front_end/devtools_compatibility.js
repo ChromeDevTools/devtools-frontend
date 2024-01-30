8c771eb3fd6c783ff68ae5cb836d7b283433d89c
Wolfgang Beyer <wolfi@chromium.org>
2024-01-30 11:31:14 +0000

    [UMA] Add histograms for network panel filters to devtools_compatibility.js
    Bug: chromium:1486431
    Change-Id: Ie9e198e927c48484b27a899211f782be5558ad2c
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5246392
    Reviewed-by: Kateryna Prokopenko <kprokopenko@chromium.org>
    Commit-Queue: Kateryna Prokopenko <kprokopenko@chromium.org>
    Auto-Submit: Wolfgang Beyer <wolfi@chromium.org>

--- front_end/devtools_compatibility.js
+++ front_end/devtools_compatibility.js
@@ -454,6 +454,12 @@ const EnumeratedHistogram = {
   BadgeActivated: 'DevTools.BadgeActivated',
   AnimationPlaybackRateChanged: 'DevTools.AnimationPlaybackRateChanged',
   AnimationPointDragged: 'DevTools.AnimationPointDragged',
+  LegacyResourceTypeFilterNumberOfSelectedChanged: 'DevTools.LegacyResourceTypeFilterNumberOfSelectedChanged',
+  LegacyResourceTypeFilterItemSelected: 'DevTools.LegacyResourceTypeFilterItemSelected',
+  ResourceTypeFilterNumberOfSelectedChanged: 'DevTools.ResourceTypeFilterNumberOfSelectedChanged',
+  ResourceTypeFilterItemSelected: 'DevTools.ResourceTypeFilterItemSelected',
+  NetworkPanelMoreFiltersNumberOfSelectedChanged: 'DevTools.NetworkPanelMoreFiltersNumberOfSelectedChanged',
+  NetworkPanelMoreFiltersItemSelected: 'DevTools.NetworkPanelMoreFiltersItemSelected',
 };
 
 /**

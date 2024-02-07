d9b49b7a7a85b46d55d428613b3a7b88f6e94854
Ergün Erdoğmuş <ergunsh@chromium.org>
2024-02-07 12:32:32 +0000

    Revert "Fix node unfound error when updating protocol resources"
    This reverts commit 3c0899ea74a100f83b566b08c6bbe3914ebc3cff.
    
    Reason for revert: it doesn't fix the problem and causes another one where we try to execute a python script as a node script.
    
    Original change's description:
    > Fix node unfound error when updating protocol resources
    >
    > This error has been consistently reported in https://ci.chromium.org/ui/p/devtools-frontend/builders/ci-hp/Roll%20deps%20and%20chromium%20pin%20into%20devtools-frontend.
    >
    > Bug: None
    > Change-Id: I25131059c3491407a843dd72bf515c7da6c8bca6
    > Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5272591
    > Auto-Submit: Changhao Han <changhaohan@chromium.org>
    > Reviewed-by: Liviu Rau <liviurau@chromium.org>
    > Commit-Queue: Liviu Rau <liviurau@chromium.org>
    
    Bug: None
    Change-Id: I229efdd3bd9f766b2792ca5b906d3ec58e5d8691
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5272389
    Reviewed-by: Liviu Rau <liviurau@chromium.org>
    Commit-Queue: Liviu Rau <liviurau@chromium.org>
    Auto-Submit: Ergün Erdoğmuş <ergunsh@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Liviu Rau <liviurau@google.com>

--- scripts/deps/roll_deps.py
+++ scripts/deps/roll_deps.py
@@ -138,7 +138,6 @@ def generate_dom_pinned_properties(options):
 def generate_protocol_resources(options):
     print('generating protocol resources')
     subprocess.check_call([
-        node_path(options),
         os.path.join(options.devtools_dir, 'scripts', 'deps',
                      'generate_protocol_resources.py')
     ],

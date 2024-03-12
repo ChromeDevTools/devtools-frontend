0c5b271c1b1cd0c14808f200a0890bd7065054cd
Moti Zilberman <motiz88@gmail.com>
2024-03-12 23:02:47 +0000

    Revert "path (#18)"
    This reverts commit 2394a3c1316d138b4c8556b8603bdd66493ededd.

--- scripts/eslint_rules/lib/check_license_header.js
+++ scripts/eslint_rules/lib/check_license_header.js
@@ -70,7 +70,6 @@ const META_CODE_PATHS = [
   'entrypoints/rn_inspector',
   'panels/rn_welcome',
   'core/host/RNPerfMetrics.ts',
-  'global_typings/react_native.d.ts',
 ];
 
 const OTHER_LICENSE_HEADERS = [

6803ae61f284ddff135ecc19926edb8297cf6a58
Simon Zünd <szuend@chromium.org>
2024-03-12 10:00:04 +0100

    [network] Fix HTML preview
    ContentData already takes care of properly encoding data URLs when it's
    needed. The HTML preview view doesn't have to encode it again.
    
    This CL also moves the creation of the HTML preview into a static
    factory method, so callers don't actualy have to worry about this and
    just pass a ContentData.
    
    R=bmeurer@chromium.org
    
    Fixed: 328471611
    Change-Id: I2ea32d85157599ec695147a56f2e3ac59f6191db
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5363614
    Reviewed-by: Benedikt Meurer <bmeurer@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>

---
+++ front_end/panels/network/RequestHTMLView.test.ts
@@ -0,0 +1,28 @@
+// Copyright 2024 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {assertNotNullOrUndefined} from '../../core/platform/platform.js';
+import * as TextUtils from '../../models/text_utils/text_utils.js';
+
+import * as Network from './network.js';
+
+describe('RequestHTMLView', () => {
+  it('renders a HTML document into an iframe', async () => {
+    const content = '<body><p>octothorp: #</p><p>hello world!<p>%3Cp%3EURI%20encoded%20tag!%3C%2Fp%3E</body>';
+    const contentData = new TextUtils.ContentData.ContentData(content, false, 'text/html');
+    const htmlView = Network.RequestHTMLView.RequestHTMLView.create(contentData);
+    assertNotNullOrUndefined(htmlView);
+    htmlView.markAsRoot();
+    const div = document.createElement('div');
+    htmlView.show(div);
+
+    const iframe = htmlView.contentElement.querySelector('iframe');
+    assertNotNullOrUndefined(iframe);
+
+    assert.strictEqual(iframe.src, contentData.asDataUrl());
+    assert.strictEqual(decodeURIComponent(iframe.src), 'data:text/html,' + content);
+
+    htmlView.detach();
+  });
+});

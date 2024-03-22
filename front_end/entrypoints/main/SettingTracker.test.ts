221e787761fb48b7b104bcd3899791749d1e4031
Alex Rudenko <alexrudenko@chromium.org>
2024-03-22 07:46:32 +0100

    Track changes for console insight settings
    Bug: b/299423255
    Change-Id: I294cbc0bc0603c310ba87daf21e243195e7210a4
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5382412
    Reviewed-by: Wolfgang Beyer <wolfi@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>

--- front_end/entrypoints/main/SettingTracker.test.ts
+++ front_end/entrypoints/main/SettingTracker.test.ts
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 import * as Common from '../../core/common/common.js';
+import * as Root from '../../core/root/root.js';
 import {describeWithEnvironment} from '../../testing/EnvironmentHelpers.js';
 
 import * as Main from './main.js';
@@ -20,5 +21,29 @@ describeWithEnvironment('SettingTracker', () => {
     assert.strictEqual(
         Common.Settings.Settings.instance().createLocalSetting('console-insights-onboarding-finished', false).get(),
         false);
+  });
+
+  it('sets console-insights-enabled to false if ci_disabledByDefault is true', async () => {
+    Common.Settings.moduleSetting('console-insights-enabled').set(true);
+    Common.Settings.Settings.instance().createLocalSetting('console-insights-toggled', false).set(false);
+    Root.Runtime.Runtime.setQueryParamForTesting('ci_disabledByDefault', 'true');
+    new Main.SettingTracker.SettingTracker();
+    assert.strictEqual(Common.Settings.moduleSetting('console-insights-enabled').get(), false);
+  });
+
+  it('sets console-insights-enabled to true if ci_disabledByDefault is false', async () => {
+    Common.Settings.moduleSetting('console-insights-enabled').set(false);
+    Common.Settings.Settings.instance().createLocalSetting('console-insights-toggled', false).set(false);
+    Root.Runtime.Runtime.setQueryParamForTesting('ci_disabledByDefault', 'false');
+    new Main.SettingTracker.SettingTracker();
+    assert.strictEqual(Common.Settings.moduleSetting('console-insights-enabled').get(), true);
+  });
+
+  it('does not change console-insights-enabled if console-insights-toggled is true', async () => {
+    Common.Settings.moduleSetting('console-insights-enabled').set(true);
+    Common.Settings.Settings.instance().createLocalSetting('console-insights-toggled', false).set(true);
+    Root.Runtime.Runtime.setQueryParamForTesting('ci_disabledByDefault', 'true');
+    new Main.SettingTracker.SettingTracker();
+    assert.strictEqual(Common.Settings.moduleSetting('console-insights-enabled').get(), true);
   });
 });
--- front_end/entrypoints/main/SettingTracker.ts
+++ front_end/entrypoints/main/SettingTracker.ts
@@ -3,27 +3,48 @@
 // found in the LICENSE file.
 
 import * as Common from '../../core/common/common.js';
+import * as Root from '../../core/root/root.js';
+
+const consoleInsightsToggledSettingName = 'console-insights-toggled';
+// Keep setting names in sync with front_end/panels/explain/*.
+const consoleInsightsEnabledSettingName = 'console-insights-enabled';
 
 export class SettingTracker {
   constructor() {
     this.#trackConsoleInsightSettingChange();
+    this.#syncConsoleInsightSettingsWithQueryParams();
   }
 
   #trackConsoleInsightSettingChange(): void {
-    // Keep setting names in sync with front_end/panels/explain/*.
-    let setting: Common.Settings.Setting<unknown>;
-    try {
-      setting = Common.Settings.moduleSetting('console-insights-enabled');
-    } catch {
-      return;
-    }
-    setting.addChangeListener(() => {
+    const setting = this.#getModuleSetting(consoleInsightsEnabledSettingName);
+    setting?.addChangeListener(() => {
       // If setting was turned on, reset the consent.
       if (setting.get()) {
         Common.Settings.Settings.instance()
             .createLocalSetting('console-insights-onboarding-finished', false)
             .set(false);
       }
+      // If console-insights-enabled was edited by the user, it becomes "sticky",
+      // which means Finch won't change the setting state.
+      Common.Settings.Settings.instance().createLocalSetting(consoleInsightsToggledSettingName, false).set(true);
     });
+  }
+
+  #getModuleSetting(name: string): Common.Settings.Setting<unknown>|undefined {
+    try {
+      return Common.Settings.moduleSetting(name);
+    } catch {
+      return;
+    }
+  }
+
+  #syncConsoleInsightSettingsWithQueryParams(): void {
+    const toggledSetting =
+        Common.Settings.Settings.instance().createLocalSetting(consoleInsightsToggledSettingName, false);
+    const enabledSetting = this.#getModuleSetting(consoleInsightsEnabledSettingName);
+    if (!toggledSetting.get()) {
+      // If the setting was not toggled, update according to ci_disabledByDefault.
+      enabledSetting?.set(Root.Runtime.Runtime.queryParam('ci_disabledByDefault') !== 'true');
+    }
   }
 }

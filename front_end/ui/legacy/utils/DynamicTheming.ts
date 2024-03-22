060461d773e885b4135f5c6802f17b4f2859da58
Benedikt Meurer <bmeurer@chromium.org>
2024-03-22 09:27:23 +0100

    [theme_support] Fix race condition when opening with Cmd+Shift+C.
    https://crrev.com/c/5307616 introduced a very subtle timing issue by
    waiting for the `theme/colors.css` file to load when starting the
    DevTools app. This innocent looking `await` change causes the
    keyboard event handling (which is in a completely different place) to
    be unable to toggle the select tool.
    
    This CL reverts the one line related to the `await` and instead puts
    the call to `applyTheme()` into the `onload` handler, and thereby
    fixes the timing issue.
    
    Fixed: b:330587444
    Bug: b:41495790
    Change-Id: I8e11560ea30dc6d682e9724edb1c30771eb99180
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5382169
    Auto-Submit: Benedikt Meurer <bmeurer@chromium.org>
    Commit-Queue: Benedikt Meurer <bmeurer@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    (cherry picked from commit d59bcb07135d224c6c862b7cabcbd1322c3264ec)
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5382174

--- front_end/ui/legacy/utils/DynamicTheming.ts
+++ front_end/ui/legacy/utils/DynamicTheming.ts
@@ -20,17 +20,19 @@ export class DynamicTheming {
     newColorsCssLink.setAttribute('rel', 'stylesheet');
     newColorsCssLink.setAttribute('type', 'text/css');
     const newColorsLoaded = new Promise<boolean>(resolve => {
-      newColorsCssLink.onload = resolve.bind(this, true);
-      newColorsCssLink.onerror = resolve.bind(this, false);
+      newColorsCssLink.onload = () => {
+        ThemeSupport.ThemeSupport.instance().applyTheme(document);
+        resolve(true);
+      };
+      newColorsCssLink.onerror = () => {
+        resolve(false);
+      };
     });
     const COLORS_CSS_SELECTOR = 'link[href*=\'//theme/colors.css\']';
     const colorCssNode = document.querySelector(COLORS_CSS_SELECTOR);
     document.body.appendChild(newColorsCssLink);
-    if (await newColorsLoaded) {
-      if (colorCssNode) {
-        colorCssNode.remove();
-      }
-      ThemeSupport.ThemeSupport.instance().applyTheme(document);
+    if (colorCssNode && await newColorsLoaded) {
+      colorCssNode.remove();
     }
   }
 }

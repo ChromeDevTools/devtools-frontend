864bc721a3f0f26b94f5730e59739a25e3182ce5
Moti Zilberman <motiz88@gmail.com>
2024-04-10 09:05:18 +0100

    Show alert if loaded in a non-Blink browser (#39)

---
+++ front_end/entrypoints/shell/browser_compatibility_guard.ts
@@ -0,0 +1,3 @@
+if (!navigator.userAgent.includes("Chrome")) {
+  alert("DevTools is only supported in Chrome, Edge and other Blink-based browsers.\n\nOpen this page in a compatible browser to continue.");
+}
--- front_end/entrypoints/shell/shell.ts
+++ front_end/entrypoints/shell/shell.ts
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import './browser_compatibility_guard.js';
 import '../../Images/Images.js';
 import '../../core/root/root-legacy.js';
 import '../../core/dom_extension/dom_extension.js';

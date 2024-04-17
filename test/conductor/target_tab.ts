c45a262697b57f03bf4281b542c98e8baf7ca877
Alex Rudenko <alexrudenko@chromium.org>
2024-04-17 08:24:15 +0200

    Remove cross-fetch
    It is not a direct dependency of devtools-frontend
    and it is not needed.
    
    Bug: none
    Change-Id: I931ad18dea320cad4fcf015a102e61a4879a4989
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5461353
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>

--- test/conductor/target_tab.ts
+++ test/conductor/target_tab.ts
@@ -5,7 +5,6 @@
 // use require here due to
 // https://github.com/evanw/esbuild/issues/587#issuecomment-901397213
 import puppeteer = require('puppeteer-core');
-import fetch from 'cross-fetch';
 
 import {loadEmptyPageAndWaitForContent} from './frontend_tab.js';
 

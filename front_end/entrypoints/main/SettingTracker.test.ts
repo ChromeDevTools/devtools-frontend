860c0e179c19bfabac6b7a33714b7b452323ff3b
Wolfgang Beyer <wolfi@chromium.org>
2024-04-19 10:05:55 +0000

    Fix order for console insights setting tracking
    First sync the setting with the query param, then listen to changes of
    the setting. Otherwise the sync would already trigger the change
    listener and cause the 'console-insights-toggled' setting to be set to
    true.
    
    Bug: chromium:299423255
    Change-Id: I35790d940d1f2db42f65f96e7f85a44b8168abc4
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5466283
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Auto-Submit: Wolfgang Beyer <wolfi@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>

--- front_end/entrypoints/main/SettingTracker.test.ts
+++ front_end/entrypoints/main/SettingTracker.test.ts
@@ -9,12 +9,18 @@ import {describeWithEnvironment} from '../../testing/EnvironmentHelpers.js';
 import * as Main from './main.js';
 
 describeWithEnvironment('SettingTracker', () => {
+  let settingTracker: Main.SettingTracker.SettingTracker|null;
+
+  afterEach(() => {
+    settingTracker?.dispose();
+  });
+
   it('resets console-insights-onboarding-finished if console-insights-enabled becomes true', async () => {
     Common.Settings.moduleSetting('console-insights-enabled').set(true);
     Common.Settings.moduleSetting('console-insights-enabled').set(false);
     Common.Settings.Settings.instance().createLocalSetting('console-insights-onboarding-finished', false).set(true);
     // Force instance that will track the settings.
-    new Main.SettingTracker.SettingTracker();
+    settingTracker = new Main.SettingTracker.SettingTracker();
     Common.Settings.moduleSetting('console-insights-enabled').set(true);
     assert.strictEqual(
         Common.Settings.Settings.instance().createLocalSetting('console-insights-onboarding-finished', false).get(),
@@ -23,25 +29,29 @@ describeWithEnvironment('SettingTracker', () => {
 
   it('sets console-insights-enabled to false if ci_disabledByDefault is true', async () => {
     Common.Settings.moduleSetting('console-insights-enabled').set(true);
-    Common.Settings.Settings.instance().createLocalSetting('console-insights-toggled', false).set(false);
+    const toggledSetting = Common.Settings.Settings.instance().createLocalSetting('console-insights-toggled', false);
+    toggledSetting.set(false);
     Root.Runtime.Runtime.setQueryParamForTesting('ci_disabledByDefault', 'true');
-    new Main.SettingTracker.SettingTracker();
+    settingTracker = new Main.SettingTracker.SettingTracker();
     assert.strictEqual(Common.Settings.moduleSetting('console-insights-enabled').get(), false);
+    assert.strictEqual(toggledSetting.get(), false);
   });
 
   it('sets console-insights-enabled to true if ci_disabledByDefault is false', async () => {
     Common.Settings.moduleSetting('console-insights-enabled').set(false);
-    Common.Settings.Settings.instance().createLocalSetting('console-insights-toggled', false).set(false);
+    const toggledSetting = Common.Settings.Settings.instance().createLocalSetting('console-insights-toggled', false);
+    toggledSetting.set(false);
     Root.Runtime.Runtime.setQueryParamForTesting('ci_disabledByDefault', 'false');
-    new Main.SettingTracker.SettingTracker();
+    settingTracker = new Main.SettingTracker.SettingTracker();
     assert.strictEqual(Common.Settings.moduleSetting('console-insights-enabled').get(), true);
+    assert.strictEqual(toggledSetting.get(), false);
   });
 
   it('does not change console-insights-enabled if console-insights-toggled is true', async () => {
     Common.Settings.moduleSetting('console-insights-enabled').set(true);
     Common.Settings.Settings.instance().createLocalSetting('console-insights-toggled', false).set(true);
     Root.Runtime.Runtime.setQueryParamForTesting('ci_disabledByDefault', 'true');
-    new Main.SettingTracker.SettingTracker();
+    settingTracker = new Main.SettingTracker.SettingTracker();
     assert.strictEqual(Common.Settings.moduleSetting('console-insights-enabled').get(), true);
   });
 });

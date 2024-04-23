002df0c83865ec766c9e4059aaef91bc3e95db4a
Simon Zünd <szuend@chromium.org>
2024-04-23 12:58:00 +0200

    [CodeMirror] Update @leszer-parser/javascript to 1.4.15
    Add support for `new.target`.
    
    R=bmeurer@chromium.org
    
    Bug: None
    Change-Id: Ied2f4a04b78983fc09d001a707cf67a9b79da8f4
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5471669
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Auto-Submit: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Benedikt Meurer <bmeurer@chromium.org>
    Reviewed-by: Benedikt Meurer <bmeurer@chromium.org>

--- front_end/ui/components/code_highlighter/CodeHighlighter.test.ts
+++ front_end/ui/components/code_highlighter/CodeHighlighter.test.ts
@@ -62,6 +62,10 @@ it('can highlight JavaScript with `import { default as name }` syntax', testHigh
 [keyword import] {[keyword default] [keyword as] [definition name]} [keyword from] [string 'module'];
 `, 'text/javascript')),
 
+it('can highlight JavaScript with `new.target` syntax', testHighlight(`
+[keyword new].[property target]
+`, 'text/javascript'));
+
   it('can highlight TypeScript', testHighlight(`
 [keyword type] [type X] = {
   [property x]: [type boolean]

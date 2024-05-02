141c8c936712ace2a7fef80503acec4127c32f2d
Alex Hunt <hello@alexhunt.io>
2024-05-02 15:06:15 +0100

    Switch to baseline-grayscale theme

--- front_end/ui/legacy/theme_support/ThemeSupport.ts
+++ front_end/ui/legacy/theme_support/ThemeSupport.ts
@@ -154,12 +154,8 @@ export class ThemeSupport extends EventTarget {
     }
 
     // Baseline is the name of Chrome's default color theme and there are two of these: default and grayscale.
-    // The collective name for the rest of the color themes is dynamic.
-    // In the baseline themes Chrome uses custom values for surface colors, whereas for dynamic themes these are color-mixed.
-    // To match Chrome we need to know if any of the baseline themes is currently active and assign specific values to surface colors.
-    const selectedTheme = getComputedStyle(document.body).getPropertyValue('--user-color-source');
-    document.documentElement.classList.toggle('baseline-default', selectedTheme === 'baseline-default');
-    document.documentElement.classList.toggle('baseline-grayscale', selectedTheme === 'baseline-grayscale');
+    // [RN] Force 'baseline-grayscale' theme for now.
+    document.documentElement.classList.add('baseline-grayscale');
   }
 
   static async fetchColors(document: Document|undefined): Promise<void> {

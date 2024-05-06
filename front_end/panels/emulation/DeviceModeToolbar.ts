15a5d6eb1b5f8ac02c1124052b2bc3e3543e27d0
Changhao Han <changhaohan@chromium.org>
2024-05-06 11:11:06 +0200

    Add parent for device mode size inputs
    Fixed: b/330713588
    Change-Id: I7f1b736b34d97ba41e9b1fb2e0a495dfc09c53a0
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5513774
    Commit-Queue: Danil Somsikov <dsv@chromium.org>
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Auto-Submit: Changhao Han <changhaohan@chromium.org>

--- front_end/panels/emulation/DeviceModeToolbar.ts
+++ front_end/panels/emulation/DeviceModeToolbar.ts
@@ -8,6 +8,7 @@ import * as i18n from '../../core/i18n/i18n.js';
 import * as Platform from '../../core/platform/platform.js';
 import * as EmulationModel from '../../models/emulation/emulation.js';
 import * as UI from '../../ui/legacy/legacy.js';
+import * as VisualLogging from '../../ui/visual_logging/visual_logging.js';
 import * as MobileThrottling from '../mobile_throttling/mobile_throttling.js';
 
 import * as EmulationComponents from './components/components.js';
@@ -249,6 +250,7 @@ export class DeviceModeToolbar {
 
     this.elementInternal = document.createElement('div');
     this.elementInternal.classList.add('device-mode-toolbar');
+    this.elementInternal.setAttribute('jslog', `${VisualLogging.toolbar('device-mode').track({resize: true})}`);
 
     const leftContainer = this.elementInternal.createChild('div', 'device-mode-toolbar-spacer');
     leftContainer.createChild('div', 'device-mode-toolbar-spacer');

6fceabdde9eacfa16ad0cb90c791a3ee4842fdff
Jack Franklin <jacktfranklin@chromium.org>
2024-05-10 13:39:35 +0100

    Migrate NetworkPanel to the new trace engine
    We are removing the legacy engine, this CL updates the NetworkPanel to
    use the new engine and avoid use of TracingManager (whose only job in
    this instance was to gather events to pass into the engine).
    
    Bug: 40287735
    Change-Id: I5a43822a11ea904a0d376add306206198435f5b8
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5531449
    Commit-Queue: Ergün Erdoğmuş <ergunsh@chromium.org>
    Auto-Submit: Jack Franklin <jacktfranklin@chromium.org>
    Reviewed-by: Ergün Erdoğmuş <ergunsh@chromium.org>
    Commit-Queue: Jack Franklin <jacktfranklin@chromium.org>

--- front_end/panels/network/NetworkPanel.ts
+++ front_end/panels/network/NetworkPanel.ts
@@ -639,7 +639,6 @@ export class NetworkPanel extends UI.Panel.Panel implements
   private onRowSizeChanged(): void {
     this.updateUI();
   }
-
   private onRequestSelected(event: Common.EventTarget.EventTargetEvent<SDK.NetworkRequest.NetworkRequest|null>): void {
     const request = event.data;
     this.currentRequest = request;
@@ -819,10 +818,10 @@ export class FilmStripRecorder implements TraceEngine.TracingManager.TracingMana
   private resourceTreeModel: SDK.ResourceTreeModel.ResourceTreeModel|null;
   private readonly timeCalculator: NetworkTimeCalculator;
   private readonly filmStripView: PerfUI.FilmStripView.FilmStripView;
-  private tracingModel: TraceEngine.Legacy.TracingModel|null;
   private callback: ((filmStrip: TraceEngine.Extras.FilmStrip.Data) => void)|null;
   // Used to fetch screenshots of the page load and show them in the panel.
   #traceEngine: TraceEngine.TraceModel.Model<TraceEngine.Extras.FilmStrip.HandlersWithFilmStrip>;
+  #collectedTraceEvents: TraceEngine.Types.TraceEvents.TraceEventData[] = [];
 
   constructor(timeCalculator: NetworkTimeCalculator, filmStripView: PerfUI.FilmStripView.FilmStripView) {
     this.#traceEngine = new TraceEngine.TraceModel.Model({
@@ -833,27 +832,21 @@ export class FilmStripRecorder implements TraceEngine.TracingManager.TracingMana
     this.resourceTreeModel = null;
     this.timeCalculator = timeCalculator;
     this.filmStripView = filmStripView;
-    this.tracingModel = null;
     this.callback = null;
   }
 
   traceEventsCollected(events: TraceEngine.TracingManager.EventPayload[]): void {
-    if (this.tracingModel) {
-      this.tracingModel.addEvents(events);
-    }
+    // TODO(crbug.com/339804979): once the old trace engine is removed, update
+    // the TS types here to avoid this typecast.
+    this.#collectedTraceEvents.push(...events as unknown as TraceEngine.Types.TraceEvents.TraceEventData[]);
   }
 
   async tracingComplete(): Promise<void> {
-    if (!this.tracingModel || !this.tracingManager) {
+    if (!this.tracingManager) {
       return;
     }
-    this.tracingModel.tracingComplete();
     this.tracingManager = null;
-    await this.#traceEngine.parse(
-        // OPP's data layer uses `EventPayload` as the type to represent raw JSON from the trace.
-        // When we pass this into the new data engine, we need to tell TS to use the new TraceEventData type.
-        this.tracingModel.allRawEvents() as unknown as TraceEngine.Types.TraceEvents.TraceEventData[],
-    );
+    await this.#traceEngine.parse(this.#collectedTraceEvents);
 
     const data = this.#traceEngine.traceParsedData(this.#traceEngine.size() - 1);
     if (!data) {
@@ -885,6 +878,7 @@ export class FilmStripRecorder implements TraceEngine.TracingManager.TracingMana
   }
 
   startRecording(): void {
+    this.#collectedTraceEvents = [];
     this.filmStripView.reset();
     this.filmStripView.setStatusText(i18nString(UIStrings.recordingFrames));
     const tracingManager =
@@ -895,7 +889,6 @@ export class FilmStripRecorder implements TraceEngine.TracingManager.TracingMana
 
     this.tracingManager = tracingManager;
     this.resourceTreeModel = this.tracingManager.target().model(SDK.ResourceTreeModel.ResourceTreeModel);
-    this.tracingModel = new TraceEngine.Legacy.TracingModel();
     void this.tracingManager.start(this, '-*,disabled-by-default-devtools.screenshot', '');
 
     Host.userMetrics.actionTaken(Host.UserMetrics.Action.FilmStripStartedRecording);

0d5f84692822c0c5368b8b138ab20866d9cc484b
Eric Leese <leese@chromium.org>
2024-05-15 12:07:11 +0000

    Only use pause location for breakpoint if it is valid
    Frequently when paused on DOM breakpoints or CSP breakpoints
    the debugger stops in a location where it is not valid to set
    a breakpoint. When selecting "Never Pause Here" at such locations,
    my previous change caused a breakpoint to be set on the following
    line. With this change, the debugger checks for this and falls
    back to setting it at the default location for the line.
    
    Bug: b:40924349
    Change-Id: I8a62a0f5b4ec6573e896a024379253d92d8917e0
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5541794
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Auto-Submit: Eric Leese <leese@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>

--- front_end/panels/sources/DebuggerPlugin.ts
+++ front_end/panels/sources/DebuggerPlugin.ts
@@ -1214,6 +1214,15 @@ export class DebuggerPlugin extends Plugin {
     return this.breakpoints.filter(b => b.position >= line.from && b.position <= line.to).map(b => b.breakpoint);
   }
 
+  private async linePossibleBreakpoints(line: CodeMirror.Line): Promise<Workspace.UISourceCode.UILocation[]> {
+    const start = this.transformer.editorLocationToUILocation(line.number - 1, 0);
+    const end = this.transformer.editorLocationToUILocation(
+        line.number - 1, Math.min(line.length, MAX_POSSIBLE_BREAKPOINT_LINE));
+    const range = new TextUtils.TextRange.TextRange(
+        start.lineNumber, start.columnNumber || 0, end.lineNumber, end.columnNumber || 0);
+    return await this.breakpointManager.possibleBreakpoints(this.uiSourceCode, range);
+  }
+
   // Compute the decorations for existing breakpoints (both on the
   // gutter and inline in the code)
   private async computeBreakpointDecoration(state: CodeMirror.EditorState, breakpoints: BreakpointDescription[]):
@@ -1245,13 +1254,8 @@ export class DebuggerPlugin extends Plugin {
       }
       if (breakpoint.enabled() && forThisLine.every(b => !b.enabled())) {
         // Start a request for possible breakpoint positions on this line
-        const start = this.transformer.editorLocationToUILocation(line.number - 1, 0);
-        const end = this.transformer.editorLocationToUILocation(
-            line.number - 1, Math.min(line.length, MAX_POSSIBLE_BREAKPOINT_LINE));
-        const range = new TextUtils.TextRange.TextRange(
-            start.lineNumber, start.columnNumber || 0, end.lineNumber, end.columnNumber || 0);
-        possibleBreakpointRequests.push(this.breakpointManager.possibleBreakpoints(this.uiSourceCode, range)
-                                            .then(locations => addPossibleBreakpoints(line, locations)));
+        possibleBreakpointRequests.push(
+            this.linePossibleBreakpoints(line).then(locations => addPossibleBreakpoints(line, locations)));
       }
       forThisLine.push(breakpoint);
       if (breakpoint.enabled()) {
@@ -1624,6 +1628,24 @@ export class DebuggerPlugin extends Plugin {
     }
   }
 
+  private async defaultBreakpointLocation(line: CodeMirror.Line): Promise<{lineNumber: number, columnNumber?: number}> {
+    // If the breakpoint is being set at the execution location, use the execution location exactly,
+    // otherwise calculate the location from the line number.
+    if (this.executionLocation) {
+      const editorExecutionLocation = this.transformer.uiLocationToEditorLocation(
+          this.executionLocation.lineNumber, this.executionLocation.columnNumber);
+      if (editorExecutionLocation.lineNumber === line.number - 1) {
+        const possibleBreakpoints = await this.linePossibleBreakpoints(line);
+        for (const location of possibleBreakpoints) {
+          if (location.compareTo(this.executionLocation) === 0) {
+            return this.executionLocation;
+          }
+        }
+      }
+    }
+    return this.transformer.editorLocationToUILocation(line.number - 1);
+  }
+
   private async createNewBreakpoint(
       line: CodeMirror.Line, condition: Breakpoints.BreakpointManager.UserCondition, enabled: boolean,
       isLogpoint: boolean): Promise<void> {
@@ -1632,15 +1654,7 @@ export class DebuggerPlugin extends Plugin {
     }
     Host.userMetrics.actionTaken(Host.UserMetrics.Action.ScriptsBreakpointSet);
     this.#recordSourcesPanelDebuggedMetrics();
-    // If the breakpoint is being set at the execution location, use the execution location exactly,
-    // otherwise calculate the location from the line number.
-    const origin =
-        (this.executionLocation &&
-         this.transformer
-                 .uiLocationToEditorLocation(this.executionLocation.lineNumber, this.executionLocation.columnNumber)
-                 .lineNumber === line.number - 1) ?
-        this.executionLocation :
-        this.transformer.editorLocationToUILocation(line.number - 1);
+    const origin = await this.defaultBreakpointLocation(line);
     await this.setBreakpoint(origin.lineNumber, origin.columnNumber, condition, enabled, isLogpoint);
   }
 

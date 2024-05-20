79ced5df8d7d79f9a7a81dd02a18e85406863e49
Ruslan Lesiutin <rdlesyutin@gmail.com>
2024-05-20 18:11:49 +0100

    bump[third_party/react-devtools]: 5.2.0 (#63)

--- front_end/third_party/react-devtools/package/frontend.d.ts
+++ front_end/third_party/react-devtools/package/frontend.d.ts
@@ -1,5 +1,12 @@
-export type MessagePayload = null | string | number | boolean | {[key: string]: MessagePayload} | MessagePayload[];
-export type Message = {event: string, payload?: MessagePayload};
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+export type MessagePayload = null | string | number | boolean | { [key: string]: MessagePayload } | MessagePayload[];
+export type Message = { event: string, payload?: MessagePayload };
 
 export type WallListener = (message: Message) => void;
 export type Wall = {
@@ -16,10 +23,31 @@ export type BrowserTheme = 'dark' | 'light';
 export function createBridge(wall: Wall): Bridge;
 export function createStore(bridge: Bridge): Store;
 
+export type Source = {
+  sourceURL: string,
+  line: number,
+  column: number,
+};
+export type ViewElementSource = (
+  source: Source,
+  symbolicatedSource: Source | null,
+) => void;
+export type ViewAttributeSource = (
+  id: number,
+  path: Array<string | number>,
+) => void;
+export type CanViewElementSource = (
+  source: Source,
+  symbolicatedSource: Source | null,
+) => boolean;
+
 export type InitializationOptions = {
   bridge: Bridge,
   store: Store,
   theme?: BrowserTheme,
+  viewAttributeSourceFunction?: ViewAttributeSource,
+  viewElementSourceFunction?: ViewElementSource,
+  canViewElementSourceFunction?: CanViewElementSource,
 };
-export function initialize(node: Element | Document, options: InitializationOptions): void;
 
+export function initialize(node: Element | Document, options: InitializationOptions): void;

ecbae84d39c9a39365dfbcb791c6b78d4df6e373
Adam Raine <asraine@chromium.org>
2024-05-20 13:17:45 -0400

    [RPP Observations] Move timeline landing page to separate file
    Bug: 334926021
    Change-Id: Ife589f776681dba9ebe92db0a41927a0b138c0c6
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5549633
    Reviewed-by: Paul Irish <paulirish@chromium.org>
    Commit-Queue: Adam Raine <asraine@chromium.org>

---
+++ front_end/panels/timeline/TimelineLandingPage.ts
@@ -0,0 +1,103 @@
+// Copyright 2024 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import * as i18n from '../../core/i18n/i18n.js';
+import type * as Platform from '../../core/platform/platform.js';
+import * as PanelFeedback from '../../ui/components/panel_feedback/panel_feedback.js';
+import * as UI from '../../ui/legacy/legacy.js';
+
+const UIStrings = {
+  /**
+   * @description Text for an option to learn more about something
+   */
+  learnmore: 'Learn more',
+  /**
+   * @description Text in Timeline Panel of the Performance panel
+   */
+  wasd: 'WASD',
+  /**
+   * @description Text in Timeline Panel of the Performance panel
+   * @example {record} PH1
+   * @example {Ctrl + R} PH2
+   */
+  clickTheRecordButtonSOrHitSTo: 'Click the record button {PH1} or hit {PH2} to start a new recording.',
+  /**
+   * @description Text in Timeline Panel of the Performance panel
+   * @example {reload button} PH1
+   * @example {Ctrl + R} PH2
+   */
+  clickTheReloadButtonSOrHitSTo: 'Click the reload button {PH1} or hit {PH2} to record the page load.',
+  /**
+   * @description Text in Timeline Panel of the Performance panel
+   * @example {Ctrl + U} PH1
+   * @example {Learn more} PH2
+   */
+  afterRecordingSelectAnAreaOf:
+      'After recording, select an area of interest in the overview by dragging. Then, zoom and pan the timeline with the mousewheel or {PH1} keys. {PH2}',
+};
+
+const str_ = i18n.i18n.registerUIStrings('panels/timeline/TimelineLandingPage.ts', UIStrings);
+const i18nString = i18n.i18n.getLocalizedString.bind(undefined, str_);
+
+interface Options {
+  isNode?: boolean;
+}
+
+export class TimelineLandingPage extends UI.Widget.VBox {
+  private readonly toggleRecordAction: UI.ActionRegistration.Action;
+
+  constructor(toggleRecordAction: UI.ActionRegistration.Action, options?: Options) {
+    super();
+
+    this.toggleRecordAction = toggleRecordAction;
+
+    function encloseWithTag(tagName: string, contents: string): HTMLElement {
+      const e = document.createElement(tagName);
+      e.textContent = contents;
+      return e;
+    }
+
+    const learnMoreNode = UI.XLink.XLink.create(
+        'https://developer.chrome.com/docs/devtools/evaluate-performance/', i18nString(UIStrings.learnmore), undefined,
+        undefined, 'learn-more');
+
+    const recordKey = encloseWithTag(
+        'b',
+        UI.ShortcutRegistry.ShortcutRegistry.instance().shortcutsForAction('timeline.toggle-recording')[0].title());
+    const reloadKey = encloseWithTag(
+        'b', UI.ShortcutRegistry.ShortcutRegistry.instance().shortcutsForAction('timeline.record-reload')[0].title());
+    const navigateNode = encloseWithTag('b', i18nString(UIStrings.wasd));
+
+    this.contentElement.classList.add('timeline-landing-page', 'fill');
+    const centered = this.contentElement.createChild('div');
+
+    const recordButton = UI.UIUtils.createInlineButton(UI.Toolbar.Toolbar.createActionButton(this.toggleRecordAction));
+    const reloadButton =
+        UI.UIUtils.createInlineButton(UI.Toolbar.Toolbar.createActionButtonForId('timeline.record-reload'));
+
+    centered.createChild('p').appendChild(i18n.i18n.getFormatLocalizedString(
+        str_, UIStrings.clickTheRecordButtonSOrHitSTo, {PH1: recordButton, PH2: recordKey}));
+
+    centered.createChild('p').appendChild(i18n.i18n.getFormatLocalizedString(
+        str_, UIStrings.clickTheReloadButtonSOrHitSTo, {PH1: reloadButton, PH2: reloadKey}));
+
+    centered.createChild('p').appendChild(i18n.i18n.getFormatLocalizedString(
+        str_, UIStrings.afterRecordingSelectAnAreaOf, {PH1: navigateNode, PH2: learnMoreNode}));
+
+    if (options?.isNode) {
+      const previewSection = new PanelFeedback.PanelFeedback.PanelFeedback();
+      previewSection.data = {
+        feedbackUrl: 'https://crbug.com/1354548' as Platform.DevToolsPath.UrlString,
+        quickStartUrl: 'https://goo.gle/js-profiler-deprecation' as Platform.DevToolsPath.UrlString,
+        quickStartLinkText: i18nString(UIStrings.learnmore),
+      };
+      centered.appendChild(previewSection);
+      const feedbackButton = new PanelFeedback.FeedbackButton.FeedbackButton();
+      feedbackButton.data = {
+        feedbackUrl: 'https://crbug.com/1354548' as Platform.DevToolsPath.UrlString,
+      };
+      centered.appendChild(feedbackButton);
+    }
+  }
+}
--- front_end/panels/timeline/TimelinePanel.ts
+++ front_end/panels/timeline/TimelinePanel.ts
@@ -47,7 +47,6 @@ import * as AnnotationsManager from '../../services/annotations_manager/annotati
 import * as TraceBounds from '../../services/trace_bounds/trace_bounds.js';
 import * as Adorners from '../../ui/components/adorners/adorners.js';
 import type * as Buttons from '../../ui/components/buttons/buttons.js';
-import * as PanelFeedback from '../../ui/components/panel_feedback/panel_feedback.js';
 import * as PerfUI from '../../ui/legacy/components/perf_ui/perf_ui.js';
 import * as UI from '../../ui/legacy/legacy.js';
 import * as VisualLogging from '../../ui/visual_logging/visual_logging.js';
@@ -64,6 +63,7 @@ import {NodeNamesUpdated, SourceMapsResolver} from './SourceMapsResolver.js';
 import {type Client, TimelineController} from './TimelineController.js';
 import {TimelineFlameChartView} from './TimelineFlameChartView.js';
 import {TimelineHistoryManager} from './TimelineHistoryManager.js';
+import {TimelineLandingPage} from './TimelineLandingPage.js';
 import {TimelineLoader} from './TimelineLoader.js';
 import {TimelineMiniMap} from './TimelineMiniMap.js';
 import timelinePanelStyles from './timelinePanel.css.js';
@@ -209,33 +209,6 @@ const UIStrings = {
    *@description Text in Timeline Panel of the Performance panel
    */
   bufferUsage: 'Buffer usage',
-  /**
-   *@description Text for an option to learn more about something
-   */
-  learnmore: 'Learn more',
-  /**
-   *@description Text in Timeline Panel of the Performance panel
-   */
-  wasd: 'WASD',
-  /**
-   *@description Text in Timeline Panel of the Performance panel
-   *@example {record} PH1
-   *@example {Ctrl + R} PH2
-   */
-  clickTheRecordButtonSOrHitSTo: 'Click the record button {PH1} or hit {PH2} to start a new recording.',
-  /**
-   * @description Text in Timeline Panel of the Performance panel
-   * @example {reload button} PH1
-   * @example {Ctrl + R} PH2
-   */
-  clickTheReloadButtonSOrHitSTo: 'Click the reload button {PH1} or hit {PH2} to record the page load.',
-  /**
-   *@description Text in Timeline Panel of the Performance panel
-   *@example {Ctrl + U} PH1
-   *@example {Learn more} PH2
-   */
-  afterRecordingSelectAnAreaOf:
-      'After recording, select an area of interest in the overview by dragging. Then, zoom and pan the timeline with the mousewheel or {PH1} keys. {PH2}',
   /**
    *@description Text in Timeline Panel of the Performance panel
    */
@@ -1354,55 +1327,7 @@ export class TimelinePanel extends UI.Panel.Panel implements Client, TimelineMod
       return;
     }
 
-    function encloseWithTag(tagName: string, contents: string): HTMLElement {
-      const e = document.createElement(tagName);
-      e.textContent = contents;
-      return e;
-    }
-
-    const learnMoreNode = UI.XLink.XLink.create(
-        'https://developer.chrome.com/docs/devtools/evaluate-performance/', i18nString(UIStrings.learnmore), undefined,
-        undefined, 'learn-more');
-
-    const recordKey = encloseWithTag(
-        'b',
-        UI.ShortcutRegistry.ShortcutRegistry.instance().shortcutsForAction('timeline.toggle-recording')[0].title());
-    const reloadKey = encloseWithTag(
-        'b', UI.ShortcutRegistry.ShortcutRegistry.instance().shortcutsForAction('timeline.record-reload')[0].title());
-    const navigateNode = encloseWithTag('b', i18nString(UIStrings.wasd));
-
-    this.landingPage = new UI.Widget.VBox();
-    this.landingPage.contentElement.classList.add('timeline-landing-page', 'fill');
-    const centered = this.landingPage.contentElement.createChild('div');
-
-    const recordButton = UI.UIUtils.createInlineButton(UI.Toolbar.Toolbar.createActionButton(this.toggleRecordAction));
-    const reloadButton =
-        UI.UIUtils.createInlineButton(UI.Toolbar.Toolbar.createActionButtonForId('timeline.record-reload'));
-
-    centered.createChild('p').appendChild(i18n.i18n.getFormatLocalizedString(
-        str_, UIStrings.clickTheRecordButtonSOrHitSTo, {PH1: recordButton, PH2: recordKey}));
-
-    centered.createChild('p').appendChild(i18n.i18n.getFormatLocalizedString(
-        str_, UIStrings.clickTheReloadButtonSOrHitSTo, {PH1: reloadButton, PH2: reloadKey}));
-
-    centered.createChild('p').appendChild(i18n.i18n.getFormatLocalizedString(
-        str_, UIStrings.afterRecordingSelectAnAreaOf, {PH1: navigateNode, PH2: learnMoreNode}));
-
-    if (isNode) {
-      const previewSection = new PanelFeedback.PanelFeedback.PanelFeedback();
-      previewSection.data = {
-        feedbackUrl: 'https://crbug.com/1354548' as Platform.DevToolsPath.UrlString,
-        quickStartUrl: 'https://goo.gle/js-profiler-deprecation' as Platform.DevToolsPath.UrlString,
-        quickStartLinkText: i18nString(UIStrings.learnmore),
-      };
-      centered.appendChild(previewSection);
-      const feedbackButton = new PanelFeedback.FeedbackButton.FeedbackButton();
-      feedbackButton.data = {
-        feedbackUrl: 'https://crbug.com/1354548' as Platform.DevToolsPath.UrlString,
-      };
-      centered.appendChild(feedbackButton);
-    }
-
+    this.landingPage = new TimelineLandingPage(this.toggleRecordAction, {isNode});
     this.landingPage.show(this.statusPaneContainer);
   }
 

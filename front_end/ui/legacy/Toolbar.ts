176147959c8fb52d9919993770409051b2e7cdd1
Benedikt Meurer <bmeurer@chromium.org>
2024-05-21 14:52:38 +0200

    [toolbar] Disable pointer events while a native context menu is open.
    Fixed: 339560549
    Change-Id: I16e9b0b8bba4e1bc305b2758bb9d34edac50ccc5
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5555020
    Commit-Queue: Kateryna Prokopenko <kprokopenko@chromium.org>
    Auto-Submit: Benedikt Meurer <bmeurer@chromium.org>
    Reviewed-by: Kateryna Prokopenko <kprokopenko@chromium.org>
    Commit-Queue: Benedikt Meurer <bmeurer@chromium.org>

--- front_end/ui/legacy/Toolbar.ts
+++ front_end/ui/legacy/Toolbar.ts
@@ -887,8 +887,10 @@ export class ToolbarMenuButton extends ToolbarButton {
       useSoftMenu: this.useSoftMenu,
       x: this.element.getBoundingClientRect().left,
       y: this.element.getBoundingClientRect().top + this.element.offsetHeight,
+      onSoftMenuClosed: () => this.element.removeAttribute('aria-expanded'),
     });
     this.contextMenuHandler(contextMenu);
+    this.element.setAttribute('aria-expanded', 'true');
     void contextMenu.show();
   }
 

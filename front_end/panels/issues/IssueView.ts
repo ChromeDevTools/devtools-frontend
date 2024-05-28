437b27d3926277278af0fdf78ff627af52bc12d7
Kateryna Prokopenko <kprokopenko@chromium.org>
2024-05-28 09:38:22 +0000

    [GM3Restyling] Remove icon in front of Learn more link
    Bug: 325441197
    Change-Id: I574f33fda1d1a01837629c51962da0d78f36de3c
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5574906
    Commit-Queue: Changhao Han <changhaohan@chromium.org>
    Reviewed-by: Changhao Han <changhaohan@chromium.org>
    Auto-Submit: Kateryna Prokopenko <kprokopenko@chromium.org>

--- front_end/panels/issues/IssueView.ts
+++ front_end/panels/issues/IssueView.ts
@@ -450,10 +450,6 @@ export class IssueView extends UI.TreeOutline.TreeElement {
       const link = UI.Fragment.html`<x-link class="link devtools-link" tabindex="0" href=${description.link}>${
                        i18nString(UIStrings.learnMoreS, {PH1: description.linkTitle})}</x-link>` as UI.XLink.XLink;
       link.setAttribute('jslog', `${VisualLogging.link('learn-more').track({click: true})}`);
-      const linkIcon = new IconButton.Icon.Icon();
-      linkIcon.data = {iconName: 'open-externally', color: 'var(--icon-link)', width: '16px', height: '16px'};
-      linkIcon.classList.add('link-icon');
-      link.prepend(linkIcon);
 
       const linkListItem = linkList.createChild('li');
       linkListItem.appendChild(link);

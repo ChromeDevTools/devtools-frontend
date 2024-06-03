f9bd99f384528a0520e20335caa4b970fc5fa41e
Andrés Olivares <andoli@chromium.org>
2024-06-03 13:20:01 +0000

    Disable failing test on mac
    Bug: 344543189
    Change-Id: Iaa62aa0c442d6e720df45a7e41fb7ee82f48504a
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5587880
    Reviewed-by: Alina Varkki <alinavarkki@chromium.org>
    Commit-Queue: Alina Varkki <alinavarkki@chromium.org>
    Commit-Queue: Andres Olivares <andoli@chromium.org>
    Auto-Submit: Andres Olivares <andoli@chromium.org>

--- test/e2e/performance/landing-page_test.ts
+++ test/e2e/performance/landing-page_test.ts
@@ -20,7 +20,8 @@ describe('The Performance panel landing page', () => {
     await enableExperiment('timeline-observations');
   });
 
-  it('displays live metrics', async () => {
+  // Flaky on mac
+  it.skipOnPlatforms(['mac'], '[crbug.com/344543189] displays live metrics', async () => {
     const {target, frontend} = await getBrowserAndPages();
 
     await navigateToPerformanceTab();

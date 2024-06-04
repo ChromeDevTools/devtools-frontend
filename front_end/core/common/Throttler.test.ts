07e8c77f610219873e17c728e3cfe352ea5aef38
Danil Somsikov <dsv@chromium.org>
2024-06-04 11:45:49 +0000

    [test] Strengthen the Throttler tests
    Bug: 343845140
    Change-Id: I8a523f588fdc097b8c250c0904c296cc5d217125
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5596374
    Reviewed-by: Ergün Erdoğmuş <ergunsh@chromium.org>
    Auto-Submit: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Danil Somsikov <dsv@chromium.org>

--- front_end/core/common/Throttler.test.ts
+++ front_end/core/common/Throttler.test.ts
@@ -7,38 +7,73 @@ import * as Common from './common.js';
 const Throttler = Common.Throttler.Throttler;
 
 describe('Throttler class', () => {
-  it('is able to schedule a process as soon as possible', async () => {
-    let result = 'original value';
+  let clock: sinon.SinonFakeTimers;
 
-    async function assignVar1() {
-      result = 'new value';
-    }
+  beforeEach(() => {
+    clock = sinon.useFakeTimers();
+    clock.tick(100);
+  });
+
+  afterEach(() => {
+    clock.restore();
+  });
+
+  it('is able to schedule a process as soon as possible', async () => {
+    const process = sinon.spy();
 
     const throttler = new Throttler(10);
-    const promiseTest = throttler.schedule(assignVar1, true);
+    void throttler.schedule(async () => {});
+    await clock.tickAsync(10);
+    void throttler.schedule(process, true);
 
-    assert.strictEqual(result, 'original value', 'process was not scheduled correctly');
-    await promiseTest;
-    assert.strictEqual(result, 'new value', 'process was not scheduled correctly');
+    assert.isFalse(process.called);
+    await clock.tickAsync(0);
+    assert.isTrue(process.calledOnce);
   });
 
   it('is able to schedule two processes as soon as possible', async () => {
-    let result = 'original value';
-
-    async function assignVar1() {
-      result = 'new value 1';
-    }
-
-    async function assignVar2() {
-      result = 'new value 2';
-    }
+    const process1 = sinon.spy();
+    const process2 = sinon.spy();
 
     const throttler = new Throttler(10);
-    const promiseTest = throttler.schedule(assignVar1, true);
-    void throttler.schedule(assignVar2, true);
+    void throttler.schedule(async () => {});
+    await clock.tickAsync(10);
+    const promiseTest = throttler.schedule(process1, true);
+    void throttler.schedule(process2, true);
 
-    assert.strictEqual(result, 'original value', 'process was not scheduled correctly');
+    assert.isFalse(process1.called);
+    assert.isFalse(process2.called);
+    clock.tickAsync(0);
     await promiseTest;
-    assert.strictEqual(result, 'new value 2', 'process was not scheduled correctly');
+    assert.isFalse(process1.called);
+    assert.isTrue(process2.calledOnce);
+  });
+
+  it('by default schedules a process delayed only if another process ran recently', async () => {
+    const process = sinon.spy();
+
+    const throttler = new Throttler(10);
+    void throttler.schedule(process);
+
+    assert.isFalse(process.called);
+    await clock.tickAsync(0);
+    assert.isTrue(process.calledOnce);
+    process.resetHistory();
+
+    void throttler.schedule(process);
+    await clock.tickAsync(0);
+    assert.isFalse(process.called);
+    await clock.tickAsync(5);
+    assert.isFalse(process.called);
+    void throttler.schedule(process);
+    await clock.tickAsync(5);
+    assert.isTrue(process.calledOnce);
+
+    await clock.tickAsync(11);
+    process.resetHistory();
+    void throttler.schedule(process);
+    assert.isFalse(process.called);
+    await clock.tickAsync(0);
+    assert.isTrue(process.calledOnce);
   });
 });

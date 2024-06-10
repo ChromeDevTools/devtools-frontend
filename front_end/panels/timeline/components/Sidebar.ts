f020e970de48840d1a5c1cc425f03bae813dd5ae
Nancy Li <nancyly@chromium.org>
2024-06-10 12:34:24 +0000

    [RPP] Add tabs in sidebar
    screenshot: https://screenshot.googleplex.com/4iNWEWZ4vPoXJsi
    Bug: 344902973
    Change-Id: I38360c53cc091c83bbdafbf37816cba298953d09
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5604324
    Reviewed-by: Jack Franklin <jacktfranklin@chromium.org>
    Commit-Queue: Nancy Li <nancyly@chromium.org>

--- front_end/panels/timeline/components/Sidebar.ts
+++ front_end/panels/timeline/components/Sidebar.ts
@@ -12,6 +12,11 @@ import sidebarStyles from './sidebar.css.js';
 const COLLAPSED_WIDTH = 40;
 const DEFAULT_EXPANDED_WIDTH = 240;
 
+const enum SidebarTabsName {
+  INSIGHTS = 'Insights',
+  ANNOTATION = 'Annotation',
+}
+
 export class SidebarWidget extends UI.SplitWidget.SplitWidget {
   #sidebarExpanded: boolean = false;
   #sidebarUI = new SidebarUI();
@@ -47,6 +52,7 @@ export class SidebarWidget extends UI.SplitWidget.SplitWidget {
 export class SidebarUI extends HTMLElement {
   static readonly litTagName = LitHtml.literal`devtools-performance-sidebar`;
   readonly #shadow = this.attachShadow({mode: 'open'});
+  #activeTab: SidebarTabsName = SidebarTabsName.INSIGHTS;
 
   connectedCallback(): void {
     this.#shadow.adoptedStyleSheets = [sidebarStyles];
@@ -54,6 +60,81 @@ export class SidebarUI extends HTMLElement {
 
   #toggleButtonClick(): void {
     this.dispatchEvent(new Event('togglebuttonclick'));
+  }
+
+  #onTabHeaderClicked(activeTab: SidebarTabsName): void {
+    this.#activeTab = activeTab;
+    // When the tabs are shown the sidebar is always open.
+    this.render(/* expanded= */ true);
+  }
+
+  #renderHeader(): LitHtml.LitTemplate {
+    // clang-format off
+    return LitHtml.html`
+      <div class="tabs-header">
+        <input
+          type="button"
+          value=${SidebarTabsName.INSIGHTS}
+          ?active=${this.#activeTab === SidebarTabsName.INSIGHTS}
+          @click=${()=>this.#onTabHeaderClicked(SidebarTabsName.INSIGHTS)}>
+        <input
+          type="button"
+          value=${SidebarTabsName.ANNOTATION}
+          ?active=${this.#activeTab === SidebarTabsName.ANNOTATION}
+          @click=${()=>this.#onTabHeaderClicked(SidebarTabsName.ANNOTATION)}>
+      </div>
+    `;
+    // clang-format on
+  }
+
+  #renderInsightsTabContent(): LitHtml.TemplateResult {
+    // clang-format off
+    return LitHtml.html`
+      <h2>Content for Insights Tab</h2>
+      <p>This is the content of the Insights tab.</p>
+    `;
+    // clang-format on
+  }
+
+  #renderAnnotationTabContent(): LitHtml.TemplateResult {
+    // clang-format off
+    return LitHtml.html`
+      <h2>Content for Annotation Tab</h2>
+      <p>This is the content of the Annotation tab.</p>
+    `;
+    // clang-format on
+  }
+
+  #renderContent(): LitHtml.TemplateResult|null {
+    switch (this.#activeTab) {
+      case SidebarTabsName.INSIGHTS:
+        return this.#renderInsightsTabContent();
+      case SidebarTabsName.ANNOTATION:
+        return this.#renderAnnotationTabContent();
+      default:
+        return null;
+    }
+  }
+
+  #updateActiveIndicatorPosition(): void {
+    const insightsTabHeaderElement = this.#shadow.querySelector('.tabs-header input:nth-child(1)');
+    const annotationTabHeaderElement = this.#shadow.querySelector('.tabs-header input:nth-child(2)');
+    const tabSliderElement = this.#shadow.querySelector<HTMLElement>('.tab-slider');
+    if (insightsTabHeaderElement && annotationTabHeaderElement && tabSliderElement) {
+      const insightsTabHeaderWidth = insightsTabHeaderElement.getBoundingClientRect().width;
+      const annotationTabHeaderWidth = annotationTabHeaderElement.getBoundingClientRect().width;
+
+      switch (this.#activeTab) {
+        case SidebarTabsName.INSIGHTS:
+          tabSliderElement.style.left = '0';
+          tabSliderElement.style.width = `${insightsTabHeaderWidth + /* margin-left of tabs-header*/ 4}px`;
+          return;
+        case SidebarTabsName.ANNOTATION:
+          tabSliderElement.style.left = `${insightsTabHeaderWidth + /* margin-left of tabs-header*/ 4}px`;
+          tabSliderElement.style.width = `${annotationTabHeaderWidth}px`;
+          return;
+      }
+    }
   }
 
   render(expanded: boolean): void {
@@ -65,12 +146,17 @@ export class SidebarUI extends HTMLElement {
       'is-closed': !expanded,
     })}>
       <div class="tab-bar">
+        ${expanded? this.#renderHeader() :LitHtml.nothing}
         <${IconButton.Icon.Icon.litTagName} name=${toggleIcon} @click=${this.#toggleButtonClick} class="sidebar-toggle-button">
         </${IconButton.Icon.Icon.litTagName}>
       </div>
+      <div class="tab-slider" ?hidden=${!expanded}></div>
+      <div class="tab-headers-bottom-line" ?hidden=${!expanded}></div>
+      ${expanded? this.#renderContent() :LitHtml.nothing}
     </div>`;
     // clang-format on
     LitHtml.render(output, this.#shadow, {host: this});
+    this.#updateActiveIndicatorPosition();
   }
 }
 

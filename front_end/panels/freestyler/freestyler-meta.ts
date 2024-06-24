370bdbedf538d57eb9ec715d4534d5a773ae835f
Alex Rudenko <alexrudenko@chromium.org>
2024-06-24 11:14:31 +0200

    Remove query flag check
    Bug: none
    Change-Id: I36ce88c910980c9f335f4623b5be01bd3b8e4e7a
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5645876
    Reviewed-by: Wolfgang Beyer <wolfi@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>

--- front_end/panels/freestyler/freestyler-meta.ts
+++ front_end/panels/freestyler/freestyler-meta.ts
@@ -4,7 +4,7 @@
 
 import * as Common from '../../core/common/common.js';
 import * as i18n from '../../core/i18n/i18n.js';
-import * as Root from '../../core/root/root.js';
+import type * as Root from '../../core/root/root.js';
 import * as UI from '../../ui/legacy/legacy.js';
 
 import type * as Freestyler from './freestyler.js';
@@ -52,9 +52,7 @@ async function loadFreestylerModule(): Promise<typeof Freestyler> {
 }
 
 function isFeatureAvailable(config?: Root.Runtime.HostConfig): boolean {
-  // Remove query param check once HostConfig is available.
-  return Root.Runtime.Runtime.queryParam('freestyler_dogfood') === 'true' ||
-      config?.devToolsFreestylerDogfood?.enabled === true;
+  return config?.devToolsFreestylerDogfood?.enabled === true;
 }
 
 UI.ViewManager.registerViewExtension({

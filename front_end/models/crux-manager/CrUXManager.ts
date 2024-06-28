330b828fbccaa47eec2cbc0c92a31cc25e539bc9
Adam Raine <asraine@chromium.org>
2024-06-28 04:54:52 +0000

    [RPP Observations] Configure .gitallowed to ignore CrUX API key
    Bug: 349721878
    Change-Id: I979816de544f7d20e583c61abedc40d2a5844b36
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5664436
    Reviewed-by: Benedikt Meurer <bmeurer@chromium.org>
    Commit-Queue: Adam Raine <asraine@chromium.org>

--- front_end/models/crux-manager/CrUXManager.ts
+++ front_end/models/crux-manager/CrUXManager.ts
@@ -5,6 +5,8 @@
 import * as Common from '../../core/common/common.js';
 import * as SDK from '../../core/sdk/sdk.js';
 
+// This key is expected to be visible in the frontend.
+// b/349721878
 const CRUX_API_KEY = 'AIzaSyCCSOx25vrb5z0tbedCB3_JRzzbVW6Uwgw';
 const DEFAULT_ENDPOINT = `https://chromeuxreport.googleapis.com/v1/records:queryRecord?key=${CRUX_API_KEY}`;
 

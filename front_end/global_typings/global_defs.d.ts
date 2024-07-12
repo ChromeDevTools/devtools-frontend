3af196e4eda6c3f8ecf201a2fba104b20d2a428c
Alex Rudenko <alexrudenko@chromium.org>
2024-07-12 13:40:58 +0200

    Remove global types
    Redundant with the latest TS.
    
    Bug: none
    Change-Id: I1d4260d016f142b1225799f6e601ca870e4fbcc3
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5702296
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Auto-Submit: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Simon Zünd <szuend@chromium.org>

--- front_end/global_typings/global_defs.d.ts
+++ front_end/global_typings/global_defs.d.ts
@@ -2,10 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-interface ImportMeta {
-  url: string;
-}
-
 declare module '*.css.js' {
   const styles: CSSStyleSheet;
   export default styles;
@@ -14,14 +10,4 @@ declare module '*.css.js' {
 declare module '*.css.legacy.js' {
   const styles: {cssContent: string};
   export default styles;
-}
-
-// Remove once https://github.com/microsoft/TypeScript/pull/57748 is available.
-// Also see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Array.
-interface ArrayConstructor {
-  fromAsync<T>(asyncIterableLike: AsyncIterable<T>|Iterable<T|PromiseLike<T>>|
-               ArrayLike<T|PromiseLike<T>>): Promise<T[]>;
-  fromAsync<T, U>(
-      asyncIterableLike: AsyncIterable<T>|Iterable<T>|ArrayLike<T>, mapFn: (value: Awaited<T>) => U,
-      thisArg?: any): Promise<Awaited<U>[]>;
 }

f2d5246099c07964bbbd764547f03ff0fd6d4379
Simon Zünd <szuend@chromium.org>
2024-07-12 10:19:58 +0200

    [sources] Use ScopeChainModel in the ScopesChainSidebarPane
    This CL builds on top of https://crrev.com/c/5700945 and utilizes
    the new ScopeChainModel class in the sidebar pane to get notified
    when the scope chain for the currently selected call frame gets
    updated.
    
    We do this by listening to the CallFrame flavor update and creating
    a new ScopeChainModel for the currently selected call frame.
    
    R=pfaffe@chromium.org
    
    Bug: 40277685
    Change-Id: I84941a8d007257f02f023977fdf7df92269b7978
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5701560
    Reviewed-by: Philip Pfaffe <pfaffe@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>

--- front_end/panels/sources/ScopeChainSidebarPane.ts
+++ front_end/panels/sources/ScopeChainSidebarPane.ts
@@ -80,7 +80,7 @@ export class ScopeChainSidebarPane extends UI.Widget.VBox implements UI.ContextF
   private readonly expandController: ObjectUI.ObjectPropertiesSection.ObjectPropertiesSectionsTreeExpandController;
   private readonly linkifier: Components.Linkifier.Linkifier;
   private infoElement: HTMLDivElement;
-  #scopesScript: SDK.Script.Script|null = null;
+  #scopeChainModel: SourceMapScopes.ScopeChainModel.ScopeChainModel|null = null;
 
   private constructor() {
     super(true);
@@ -96,9 +96,7 @@ export class ScopeChainSidebarPane extends UI.Widget.VBox implements UI.ContextF
     this.infoElement = document.createElement('div');
     this.infoElement.className = 'gray-info-message';
     this.infoElement.tabIndex = -1;
-    SDK.TargetManager.TargetManager.instance().addModelListener(
-        SDK.DebuggerModel.DebuggerModel, SDK.DebuggerModel.Events.DebugInfoAttached, this.debugInfoAttached, this);
-    void this.update();
+    this.flavorChanged(UI.Context.Context.instance().flavor(SDK.DebuggerModel.CallFrame));
   }
 
   static instance(): ScopeChainSidebarPane {
@@ -108,8 +106,17 @@ export class ScopeChainSidebarPane extends UI.Widget.VBox implements UI.ContextF
     return scopeChainSidebarPaneInstance;
   }
 
-  flavorChanged(_object: Object|null): void {
-    void this.update();
+  flavorChanged(callFrame: SDK.DebuggerModel.CallFrame|null): void {
+    this.#scopeChainModel?.dispose();
+    this.#scopeChainModel = null;
+
+    if (callFrame) {
+      this.#scopeChainModel = new SourceMapScopes.ScopeChainModel.ScopeChainModel(callFrame);
+      this.#scopeChainModel.addEventListener(
+          SourceMapScopes.ScopeChainModel.Events.ScopeChainUpdated, event => this.update(event.data), this);
+    } else {
+      void this.update(null);
+    }
   }
 
   override focus(): void {
@@ -122,41 +129,7 @@ export class ScopeChainSidebarPane extends UI.Widget.VBox implements UI.ContextF
     }
   }
 
-  private sourceMapAttached(
-      event: Common.EventTarget.EventTargetEvent<{client: SDK.Script.Script, sourceMap: SDK.SourceMap.SourceMap}>):
-      void {
-    if (event.data.client === this.#scopesScript) {
-      void this.update();
-    }
-  }
-
-  private setScopeSourceMapSubscription(callFrame: SDK.DebuggerModel.CallFrame|null): void {
-    const oldScript = this.#scopesScript;
-    this.#scopesScript = callFrame?.script ?? null;
-
-    // Shortcut for the case when we are listening to the same model.
-    if (oldScript?.debuggerModel === this.#scopesScript?.debuggerModel) {
-      return;
-    }
-
-    if (oldScript) {
-      oldScript.debuggerModel.sourceMapManager().removeEventListener(
-          SDK.SourceMapManager.Events.SourceMapAttached, this.sourceMapAttached, this);
-    }
-
-    if (this.#scopesScript) {
-      this.#scopesScript.debuggerModel.sourceMapManager().addEventListener(
-          SDK.SourceMapManager.Events.SourceMapAttached, this.sourceMapAttached, this);
-    }
-  }
-
-  private debugInfoAttached(event: Common.EventTarget.EventTargetEvent<SDK.Script.Script>): void {
-    if (event.data === this.#scopesScript) {
-      void this.update();
-    }
-  }
-
-  private async update(): Promise<void> {
+  private async update(eventScopeChain: SourceMapScopes.ScopeChainModel.ScopeChain|null): Promise<void> {
     // The `resolveThisObject(callFrame)` and `resolveScopeChain(callFrame)` calls
     // below may take a while to complete, so indicate to the user that something
     // is happening (see https://crbug.com/1162416).
@@ -166,8 +139,7 @@ export class ScopeChainSidebarPane extends UI.Widget.VBox implements UI.ContextF
 
     this.linkifier.reset();
 
-    const callFrame = UI.Context.Context.instance().flavor(SDK.DebuggerModel.CallFrame);
-    this.setScopeSourceMapSubscription(callFrame);
+    const callFrame = eventScopeChain?.callFrame ?? null;
     const [thisObject, scopeChain] = await Promise.all([
       SourceMapScopes.NamesResolver.resolveThisObject(callFrame),
       SourceMapScopes.NamesResolver.resolveScopeChain(callFrame),

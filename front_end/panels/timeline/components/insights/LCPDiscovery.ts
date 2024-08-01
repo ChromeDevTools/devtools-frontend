350d38d66823cc72e32e3672ef737ce6e6695c43
Jack Franklin <jacktfranklin@chromium.org>
2024-08-01 12:37:00 +0100

    RPP: update LCP request discovery details
    1. Update the styling based on the latest designs
    2. Fix the alignment of the icons in the list of issues.
    3. Add the discovery time text (exact text tbc, but this will do for now)
    
    Bug: 351758501
    Change-Id: Ia8f0fcf599b76cd96c5a5ab69f3201ef8f608f82
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5756478
    Auto-Submit: Jack Franklin <jacktfranklin@chromium.org>
    Reviewed-by: Nancy Li <nancyly@chromium.org>
    Commit-Queue: Nancy Li <nancyly@chromium.org>

--- front_end/panels/timeline/components/insights/LCPDiscovery.ts
+++ front_end/panels/timeline/components/insights/LCPDiscovery.ts
@@ -3,24 +3,38 @@
 // found in the LICENSE file.
 
 import * as Common from '../../../../core/common/common.js';
+import * as i18n from '../../../../core/i18n/i18n.js';
 import * as Platform from '../../../../core/platform/platform.js';
 import {type LCPInsightResult} from '../../../../models/trace/insights/types.js';
-import type * as TraceEngine from '../../../../models/trace/trace.js';
+import * as TraceEngine from '../../../../models/trace/trace.js';
 import * as ComponentHelpers from '../../../../ui/components/helpers/helpers.js';
 import * as IconButton from '../../../../ui/components/icon_button/icon_button.js';
 import * as LitHtml from '../../../../ui/lit-html/lit-html.js';
 
+import discoveryStyles from './lcpDiscovery.css.js';
 import sidebarInsightStyles from './sidebarInsight.css.js';
 import * as SidebarInsight from './SidebarInsight.js';
 import {type ActiveInsight, InsightsCategories} from './types.js';
 
 export const InsightName = 'lcp-discovery';
 
-interface ImageData {
+const UIStrings = {
+  /**
+   * @description Text to tell the user how long after the earliest discovery time their LCP element loaded.
+   * @example {401ms} PH1
+   */
+  lcpLoadDelay: 'LCP image loaded {PH1} after earliest start point.',
+};
+
+const str_ = i18n.i18n.registerUIStrings('panels/timeline/components/insights/LCPDiscovery.ts', UIStrings);
+// const i18nString = i18n.i18n.getLocalizedString.bind(undefined, str_);
+
+interface LCPImageDiscoveryData {
   shouldIncreasePriorityHint: boolean;
   shouldPreloadImage: boolean;
   shouldRemoveLazyLoading: boolean;
   resource: TraceEngine.Types.TraceEvents.SyntheticNetworkRequest;
+  discoveryDelay: TraceEngine.Types.Timing.MicroSeconds|null;
 }
 
 export function getLCPInsightData(
@@ -41,8 +55,8 @@ export function getLCPInsightData(
   return lcpInsight;
 }
 
-function getImageData(insights: TraceEngine.Insights.Types.TraceInsightData|null, navigationId: string|null): ImageData|
-    null {
+function getImageData(
+    insights: TraceEngine.Insights.Types.TraceInsightData|null, navigationId: string|null): LCPImageDiscoveryData|null {
   const lcpInsight = getLCPInsightData(insights, navigationId);
   if (!lcpInsight) {
     return null;
@@ -64,12 +78,20 @@ function getImageData(insights: TraceEngine.Insights.Types.TraceInsightData|null
     return null;
   }
 
-  return {
+  const data: LCPImageDiscoveryData = {
     shouldIncreasePriorityHint,
     shouldPreloadImage,
     shouldRemoveLazyLoading,
     resource: lcpInsight.lcpResource,
+    discoveryDelay: null,
   };
+
+  if (lcpInsight.earliestDiscoveryTimeTs && lcpInsight.lcpResource) {
+    const discoveryDelay = lcpInsight.lcpResource.ts - lcpInsight.earliestDiscoveryTimeTs;
+    data.discoveryDelay = TraceEngine.Types.Timing.MicroSeconds(discoveryDelay);
+  }
+
+  return data;
 }
 
 export class LCPDiscovery extends HTMLElement {
@@ -132,7 +154,14 @@ export class LCPDiscovery extends HTMLElement {
     `;
   }
 
-  #renderDiscovery(imageData: ImageData): LitHtml.TemplateResult {
+  #renderDiscoveryDelay(delay: TraceEngine.Types.Timing.MicroSeconds): Element {
+    const timeWrapper = document.createElement('span');
+    timeWrapper.classList.add('discovery-time-ms');
+    timeWrapper.innerText = i18n.TimeUtilities.formatMicroSecondsTime(delay);
+    return i18n.i18n.getFormatLocalizedString(str_, UIStrings.lcpLoadDelay, {PH1: timeWrapper});
+  }
+
+  #renderDiscovery(imageData: LCPImageDiscoveryData): LitHtml.TemplateResult {
     // clang-format off
     return LitHtml.html`
         <div class="insights">
@@ -143,21 +172,21 @@ export class LCPDiscovery extends HTMLElement {
           @insighttoggleclick=${this.#sidebarClicked}
         >
           <div slot="insight-description" class="insight-description">
-            The LCP image should be requested as early as possible.
-            <div class="insight-results">
-              <div class="insight-entry">
+          ${imageData.discoveryDelay ? LitHtml.html`<p class="discovery-delay">${this.#renderDiscoveryDelay(imageData.discoveryDelay)}</p>` : LitHtml.nothing}
+            <ul class="insight-results discovery-icon-results">
+              <li class="insight-entry">
                 ${this.#adviceIcon(imageData.shouldIncreasePriorityHint)}
-                fetchpriority=high applied
-              </div>
-              <div class="insight-entry">
+                <span>fetchpriority=high applied</span>
+              </li>
+              <li class="insight-entry">
                 ${this.#adviceIcon(imageData.shouldPreloadImage)}
-                Request is discoverable in initial document
-              </div>
-              <div class="insight-entry">
+                <span>Request is discoverable in initial document</span>
+              </li>
+              <li class="insight-entry">
                 ${this.#adviceIcon(imageData.shouldRemoveLazyLoading)}
-                lazyload not applied
-              </div>
-            </div>
+                <span>lazyload not applied</span>
+              </li>
+            </ul>
           </div>
           <div slot="insight-content" class="insight-content">
             <img class="element-img" data-src=${imageData.resource.args.data.url} src=${imageData.resource.args.data.url}>
@@ -172,7 +201,7 @@ export class LCPDiscovery extends HTMLElement {
   }
 
   connectedCallback(): void {
-    this.#shadow.adoptedStyleSheets = [sidebarInsightStyles];
+    this.#shadow.adoptedStyleSheets = [sidebarInsightStyles, discoveryStyles];
   }
 
   #shouldRenderForCateogory(): boolean {

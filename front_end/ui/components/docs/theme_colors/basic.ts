1b22dcc0c69183dcb0605c26a49a204143d0c41a
Kim-Anh Tran <kimanh@chromium.org>
2024-08-07 09:50:44 +0200

    Update theme colors documentation
    Screenshot: https://i.imgur.com/b5U194R.png
    Bug: 357513556
    Change-Id: I4333a7cb404ccf5a73f35c5fdfc3dab432056def
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5766217
    Commit-Queue: Kim-Anh Tran <kimanh@chromium.org>
    Reviewed-by: Jack Franklin <jacktfranklin@chromium.org>

--- front_end/ui/components/docs/theme_colors/basic.ts
+++ front_end/ui/components/docs/theme_colors/basic.ts
@@ -8,84 +8,149 @@ import * as ComponentHelpers from '../../helpers/helpers.js';
 await ComponentHelpers.ComponentServerSetup.setup();
 
 const THEME_VARIABLES_NAMES = new Set([
-  '--color-primary-old',
-  '--color-primary-variant',
-  '--color-background',
-  '--color-background-inverted',
-  '--color-background-opacity-50',
-  '--color-background-opacity-80',
-  '--color-background-elevation-0',
-  '--color-background-elevation-1',
-  '--color-background-elevation-2',
-  '--color-background-elevation-dark-only',
-  '--color-background-highlight',
-  '--divider-line',
-  '--color-background-hover-overlay',
-  '--color-selection-highlight',
-  '--color-selection-highlight-border',
-  '--color-match-highlight',
-  '--color-text-primary',
-  '--color-text-secondary',
-  '--color-text-secondary-selected',
-  '--color-text-disabled',
-  '--color-details-hairline',
-  '--color-details-hairline-light',
-  '--color-accent-red',
-  '--color-red',
-  '--color-accent-green',
-  '--color-green',
-  '--color-link',
-  '--color-syntax-1',
-  '--color-syntax-2',
-  '--color-syntax-3',
-  '--color-syntax-4',
-  '--color-syntax-5',
-  '--color-syntax-6',
-  '--color-syntax-7',
-  '--color-syntax-8',
-  '--drop-shadow',
-  '--drop-shadow-depth-1',
-  '--drop-shadow-depth-2',
-  '--drop-shadow-depth-3',
-  '--drop-shadow-depth-4',
-  '--drop-shadow-depth-5',
-  '--box-shadow-outline-color',
-  '--color-scrollbar-mac',
-  '--color-scrollbar-mac-hover',
-  '--color-scrollbar-other',
-  '--color-scrollbar-other-hover',
-  '--lighthouse-red',
-  '--lighthouse-orange',
-  '--lighthouse-green',
-  '--issue-color-red',
-  '--issue-color-yellow',
-  '--issue-color-blue',
-  '--input-outline',
-
+  '--sys-color-base',
+  '--sys-color-base-container',
+  '--sys-color-base-container-elevated',
+  '--sys-color-blue',
+  '--sys-color-blue-bright',
+  '--sys-color-cdt-base',
+  '--sys-color-cdt-base-container',
+  '--sys-color-cyan',
+  '--sys-color-cyan-bright',
+  '--sys-color-divider',
+  '--sys-color-divider-on-tonal-container',
+  '--sys-color-divider-prominent',
+  '--sys-color-error',
+  '--sys-color-error-bright',
+  '--sys-color-error-container',
+  '--sys-color-error-outline',
+  '--sys-color-gradient-primary',
+  '--sys-color-gradient-tertiary',
+  '--sys-color-green',
+  '--sys-color-green-bright',
+  '--sys-color-inverse-on-surface',
+  '--sys-color-inverse-primary',
+  '--sys-color-inverse-surface',
+  '--sys-color-neutral-bright',
+  '--sys-color-neutral-container',
+  '--sys-color-neutral-outline',
+  '--sys-color-omnibox-container',
+  '--sys-color-on-base',
+  '--sys-color-on-base-divider',
+  '--sys-color-on-blue',
+  '--sys-color-on-cyan',
+  '--sys-color-on-error',
+  '--sys-color-on-error-container',
+  '--sys-color-on-green',
+  '--sys-color-on-orange',
+  '--sys-color-on-pink',
+  '--sys-color-on-primary',
+  '--sys-color-on-purple',
+  '--sys-color-on-secondary',
+  '--sys-color-on-surface',
+  '--sys-color-on-surface-error',
+  '--sys-color-on-surface-green',
+  '--sys-color-on-surface-primary',
+  '--sys-color-on-surface-secondary',
+  '--sys-color-on-surface-subtle',
+  '--sys-color-on-surface-yellow',
+  '--sys-color-on-tertiary',
+  '--sys-color-on-tertiary-container',
+  '--sys-color-on-tonal-container',
+  '--sys-color-on-yellow',
+  '--sys-color-on-yellow-container',
+  '--sys-color-orange',
+  '--sys-color-orange-bright',
+  '--sys-color-outline',
+  '--sys-color-pink',
+  '--sys-color-pink-bright',
+  '--sys-color-primary',
+  '--sys-color-primary-bright',
+  '--sys-color-purple',
+  '--sys-color-purple-bright',
+  '--sys-color-secondary',
+  '--sys-color-state-disabled',
+  '--sys-color-state-disabled-container',
+  '--sys-color-state-focus-highlight',
+  '--sys-color-state-focus-ring',
+  '--sys-color-state-focus-select',
+  '--sys-color-state-header-hover',
+  '--sys-color-state-hover-bright-blend-protection',
+  '--sys-color-state-hover-dim-blend-protection',
+  '--sys-color-state-hover-on-prominent',
+  '--sys-color-state-hover-on-subtle',
+  '--sys-color-state-on-header-hover',
+  '--sys-color-state-ripple-neutral-on-prominent',
+  '--sys-color-state-ripple-neutral-on-subtle',
+  '--sys-color-state-ripple-primary',
+  '--sys-color-surface',
+  '--sys-color-surface1',
+  '--sys-color-surface2',
+  '--sys-color-surface3',
+  '--sys-color-surface4',
+  '--sys-color-surface5',
+  '--sys-color-surface-error',
+  '--sys-color-surface-green',
+  '--sys-color-surface-variant',
+  '--sys-color-surface-yellow',
+  '--sys-color-surface-yellow-high',
+  '--sys-color-tertiary',
+  '--sys-color-tertiary-container',
+  '--sys-color-token-atom',
+  '--sys-color-token-attribute',
+  '--sys-color-token-attribute-value',
+  '--sys-color-token-builtin',
+  '--sys-color-token-comment',
+  '--sys-color-token-definition',
+  '--sys-color-token-deleted',
+  '--sys-color-token-inserted',
+  '--sys-color-token-keyword',
+  '--sys-color-token-meta',
+  '--sys-color-token-number',
+  '--sys-color-token-property',
+  '--sys-color-token-property-special',
+  '--sys-color-token-pseudo-element',
+  '--sys-color-token-string',
+  '--sys-color-token-string-special',
+  '--sys-color-token-subtle',
+  '--sys-color-token-tag',
+  '--sys-color-token-type',
+  '--sys-color-token-variable',
+  '--sys-color-token-variable-special',
+  '--sys-color-tonal-container',
+  '--sys-color-tonal-outline',
+  '--sys-color-yellow',
+  '--sys-color-yellow-bright',
+  '--sys-color-yellow-container',
+  '--sys-color-yellow-outline',
+  '--sys-elevation-level1',
+  '--sys-elevation-level2',
+  '--sys-elevation-level3',
+  '--sys-elevation-level4',
+  '--sys-elevation-level5',
 ]);
 
-function appendStyles(mode: 'light'|'dark') {
-  const container = document.querySelector(`.${mode}-mode-container`) as HTMLElement;
-  const listItems = Array.from(THEME_VARIABLES_NAMES).map(varName => {
+function appendStyles() {
+  const container = document.getElementById('container') as HTMLElement;
+  const items = Array.from(THEME_VARIABLES_NAMES).map(varName => {
     const value = getComputedStyle(container).getPropertyValue(varName);
     if (!value) {
       throw new Error(`Could not find value for CSS variable ${varName}.`);
     }
+
     let styles = {};
-    if (varName.includes('--box-shadow')) {
-      styles = {boxShadow: `0 0 0 1px var(${varName})`, borderBottomWidth: 0};
-
-    } else if (varName.includes('--drop-shadow')) {
+    if (varName.includes('--sys-elevation')) {
       styles = {boxShadow: `var(${varName})`, borderBottomWidth: 0};
-
     } else {
       styles = {borderBottomColor: `var(${varName})`};
     }
-    const liStyles = LitHtml.Directives.styleMap(styles);
-    return LitHtml.html`<li style=${liStyles}><code>${varName}: ${value}</code></li>`;
+    const style = LitHtml.Directives.styleMap(styles);
+    return LitHtml.html`
+      <div style=${style}><code>${varName}: ${value}</code></div>
+      <div style=${style} class='theme-with-dark-background'><code>${varName}: ${value}</code></div>
+    `;
   });
-  LitHtml.render(LitHtml.html`${listItems}`, container);
+  LitHtml.render(LitHtml.html`${items}`, container);
 }
 
-appendStyles('light');
-appendStyles('dark');
+appendStyles();

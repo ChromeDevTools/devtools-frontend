90fbb7ea3f9826254d91e4bf451a94e6c7590426
Nikolay Vitkov <nvitkov@chromium.org>
2024-08-13 13:07:58 +0200

    Remove period from strings
    Single sentences should not have a period at the end
    
    Bug: none
    Change-Id: I29ceb2a3d595ca79a514af5bfc8d698b56ce8a0b
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5783082
    Commit-Queue: Nikolay Vitkov <nvitkov@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Wolfgang Beyer <wolfi@chromium.org>

--- front_end/panels/explain/explain-meta.ts
+++ front_end/panels/explain/explain-meta.ts
@@ -30,16 +30,16 @@ const UIStrings = {
    * @description Message shown to the user if the DevTools locale is not
    * supported.
    */
-  wrongLocale: 'To use this feature, update your Language preference in DevTools Settings to English.',
+  wrongLocale: 'To use this feature, update your Language preference in DevTools Settings to English',
   /**
    * @description Message shown to the user if the age check is not successful.
    */
-  ageRestricted: 'This feature is only available to users who are 18 years of age or older.',
+  ageRestricted: 'This feature is only available to users who are 18 years of age or older',
   /**
    * @description Message shown to the user if the user's region is not
    * supported.
    */
-  geoRestricted: 'This feature is unavailable in your region.',
+  geoRestricted: 'This feature is unavailable in your region',
   /**
    * @description Message shown to the user if the enterprise policy does
    * not allow this feature.

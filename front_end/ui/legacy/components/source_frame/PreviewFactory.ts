4485e880685dd3a4dd1edb630375580347f6588a
Simon Zünd <szuend@chromium.org>
2024-08-13 13:36:59 +0200

    [sources] Use ContentData instead of DeferredContent in PreviewFactory
    R=pfaffe@chromium.org
    
    Bug: 41481944
    Change-Id: Ica104cf6a1f5ebbded048e143884c9ba812cdab1
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5783743
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Philip Pfaffe <pfaffe@chromium.org>

--- front_end/ui/legacy/components/source_frame/PreviewFactory.ts
+++ front_end/ui/legacy/components/source_frame/PreviewFactory.ts
@@ -4,7 +4,7 @@
 
 import * as Common from '../../../../core/common/common.js';
 import * as i18n from '../../../../core/i18n/i18n.js';
-import type * as TextUtils from '../../../../models/text_utils/text_utils.js';
+import * as TextUtils from '../../../../models/text_utils/text_utils.js';
 import * as UI from '../../legacy.js';
 
 import {FontView} from './FontView.js';
@@ -36,35 +36,30 @@ export class PreviewFactory {
         return new FontView(mimeType, provider);
     }
 
-    const deferredContent = await provider.requestContent();
-    if (deferredContent.content === null) {
-      return new UI.EmptyWidget.EmptyWidget(deferredContent.error);
+    const contentData = await provider.requestContentData();
+    if (TextUtils.ContentData.ContentData.isError(contentData)) {
+      return new UI.EmptyWidget.EmptyWidget(contentData.error);
     }
-    if (!deferredContent.content) {
+    if (!contentData.isTextContent) {
+      return null;
+    }
+
+    if (!contentData.text) {
       return new UI.EmptyWidget.EmptyWidget(i18nString(UIStrings.nothingToPreview));
     }
 
-    let content: string = deferredContent.content;
-    if (deferredContent.isEncoded) {
-      content = window.atob(content);
-    }
-
-    const parsedXML = XMLView.parseXML(content, mimeType);
+    const parsedXML = XMLView.parseXML(contentData.text, contentData.mimeType);
     if (parsedXML) {
       return XMLView.createSearchableView(parsedXML);
     }
 
-    const jsonView = await JSONView.createView(content);
+    const jsonView = await JSONView.createView(contentData.text);
     if (jsonView) {
       return jsonView;
     }
 
-    if (resourceType.isTextType()) {
-      const highlighterType =
-          mimeType.replace(/;.*/, '') /* remove charset */ || provider.contentType().canonicalMimeType();
-      return ResourceSourceFrame.createSearchableView(provider, highlighterType);
-    }
-
-    return null;
+    const highlighterType =
+        mimeType.replace(/;.*/, '') /* remove charset */ || provider.contentType().canonicalMimeType();
+    return ResourceSourceFrame.createSearchableView(provider, highlighterType);
   }
 }

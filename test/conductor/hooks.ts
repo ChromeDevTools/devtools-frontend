91f5a380f206a7f1e7cdcf9d0d3c6f66e0444132
Alex Rudenko <alexrudenko@chromium.org>
2024-08-22 13:49:56 +0200

    Add '--disable-field-trial-config'
    No-Tree-Checks: True
    Bug: 357020932
    Change-Id: Id3da24ea8ce0a8ff18e75bde2b78df0521557ba2
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5803113
    Auto-Submit: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Philip Pfaffe <pfaffe@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>

--- test/conductor/hooks.ts
+++ test/conductor/hooks.ts
@@ -118,6 +118,7 @@ function launchChrome() {
     '--enable-blink-features=CSSContainerQueries,HighlightInheritance',  // TODO(crbug.com/1218390) Remove globally enabled flags and conditionally enable them
     '--disable-blink-features=WebAssemblyJSPromiseIntegration',  // TODO(crbug.com/325123665) Remove once heap snapshots work again with JSPI
     `--disable-features=${disabledFeatures.join(',')}`,
+    '--disable-field-trial-config',
   ];
   const opts: puppeteer.LaunchOptions&puppeteer.BrowserLaunchArgumentOptions&puppeteer.BrowserConnectOptions = {
     headless,

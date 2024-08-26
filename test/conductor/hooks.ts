46b6d8cd8703c31f4cb8b46acaf19b16b0cadbb3
Alex Rudenko <alexrudenko@chromium.org>
2024-08-26 13:26:33 +0200

    Disables RenderDocument and QueueNavigationsWhileWaitingForCommit
    Experiments that caused flakiness in e2e tests.
    
    Bug: 357020932, 361519377
    Change-Id: Ic5e545c5f3951970b68e8cd9909fd0fcb50cdcec
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5806954
    Reviewed-by: Philip Pfaffe <pfaffe@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>

--- test/conductor/hooks.ts
+++ test/conductor/hooks.ts
@@ -102,8 +102,10 @@ function launchChrome() {
   ];
 
   const disabledFeatures = [
-    'DeferRendererTasksAfterInput',  // crbug.com/361078921
-    'PMProcessPriorityPolicy',       // crbug.com/361252079
+    'DeferRendererTasksAfterInput',           // crbug.com/361078921
+    'PMProcessPriorityPolicy',                // crbug.com/361252079
+    'RenderDocument',                         // crbug.com/361519377
+    'QueueNavigationsWhileWaitingForCommit',  // crbug.com/361519377
   ];
   const launchArgs = [
     '--remote-allow-origins=*',

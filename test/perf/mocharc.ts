7003681549ba35cbe7021543f81c72f4b9497f93
Alex Rudenko <alexrudenko@chromium.org>
2024-08-30 10:01:23 +0200

    Mocha retries only on CI
    As indicated by LUCI_CONTEXT env.
    
    Bug: none
    Change-Id: I3dc1cdf414af00746acef3a4caf84615553a9c0f
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5823926
    Reviewed-by: Philip Pfaffe <pfaffe@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>

--- test/perf/mocharc.ts
+++ test/perf/mocharc.ts
@@ -13,7 +13,8 @@ module.exports = {
   spec,
   require : [path.join(path.dirname(__dirname), 'conductor', 'mocha_hooks.js'), 'source-map-support/register'],
   timeout : TestConfig.debug ? 0 : 10_000,
-  retries : 1,
+  // Retry only on CI.
+  retries : Boolean(process.env.LUCI_CONTEXT) ? 1 : 0,
   reporter : path.join(path.dirname(__dirname), 'shared', 'mocha-resultsdb-reporter'),
   suiteName : 'perf',
   slow : 1000, ...TestConfig.mochaGrep,

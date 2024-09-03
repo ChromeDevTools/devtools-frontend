5935bd1f8f6f2fc0eb93d57e3472b8dcd17eec27
Alex Rudenko <alexrudenko@chromium.org>
2024-09-03 14:49:56 +0200

    Enable field trial testing config
    This configuration looks stable. If there are any issues, feel free to revert.
    
    Bug: 364165032
    Change-Id: I315466254c8a1ff01492abd9e2a38711cd300411
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5811745
    Reviewed-by: Nikolay Vitkov <nvitkov@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Nikolay Vitkov <nvitkov@chromium.org>
    Auto-Submit: Alex Rudenko <alexrudenko@chromium.org>

--- test/conductor/hooks.ts
+++ test/conductor/hooks.ts
@@ -102,9 +102,10 @@ function launchChrome() {
   ];
 
   const disabledFeatures = [
-    'DeferRendererTasksAfterInput',           // crbug.com/361078921
-    'PMProcessPriorityPolicy',                // crbug.com/361252079
-    'RenderDocument',                         // crbug.com/361519377
+    'BrowserThreadPoolAdjustment',   // crbug.com/364165032
+    'DeferRendererTasksAfterInput',  // crbug.com/361078921
+    'PMProcessPriorityPolicy',       // crbug.com/361252079
+    'RenderDocument',                // crbug.com/361519377
   ];
   const launchArgs = [
     '--remote-allow-origins=*',
@@ -119,7 +120,6 @@ function launchChrome() {
     '--enable-blink-features=CSSContainerQueries,HighlightInheritance',  // TODO(crbug.com/1218390) Remove globally enabled flags and conditionally enable them
     '--disable-blink-features=WebAssemblyJSPromiseIntegration',  // TODO(crbug.com/325123665) Remove once heap snapshots work again with JSPI
     `--disable-features=${disabledFeatures.join(',')}`,
-    '--disable-field-trial-config',
   ];
   const opts: puppeteer.LaunchOptions&puppeteer.BrowserLaunchArgumentOptions&puppeteer.BrowserConnectOptions = {
     headless,

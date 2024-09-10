c555931ccd75cfb2b547949a4502c19d73313117
Philip Pfaffe <pfaffe@chromium.org>
2024-09-10 13:01:11 +0000

    Allow empty-string initial values when parsing property values
    Bug: 365578428
    Change-Id: Ic18d24c4cc7bcecff1ec3f20f4e8aea33cd34d73
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5851132
    Auto-Submit: Philip Pfaffe <pfaffe@chromium.org>
    Reviewed-by: Ergün Erdoğmuş <ergunsh@chromium.org>
    Commit-Queue: Philip Pfaffe <pfaffe@chromium.org>
    Commit-Queue: Ergün Erdoğmuş <ergunsh@chromium.org>

--- front_end/core/sdk/CSSMatchedStyles.ts
+++ front_end/core/sdk/CSSMatchedStyles.ts
@@ -1086,7 +1086,8 @@ class DOMInheritanceCascade {
     const accumulatedCSSVariables = new Map<string, CSSVariableValue|null>();
     for (const rule of this.#registeredProperties) {
       const initialValue = rule.initialValue();
-      accumulatedCSSVariables.set(rule.propertyName(), initialValue ? {value: initialValue, declaration: rule} : null);
+      accumulatedCSSVariables.set(
+          rule.propertyName(), initialValue !== null ? {value: initialValue, declaration: rule} : null);
     }
     for (let i = this.#nodeCascades.length - 1; i >= 0; --i) {
       const nodeCascade = this.#nodeCascades[i];

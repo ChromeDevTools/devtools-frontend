45fec2d03e40e5a38d5714acc48fbcb2f70aae2f
Joshua Thomas <masnoble@chromium.org>
2024-09-17 11:02:00 -0400

    Change 'Security' name to 'Securtity & Privacy' if feature is enabled
    Corresponding Chromium CL: https://crrev.com/c/5867918
    
    Flag enabled: https://screenshot.googleplex.com/8M3bCiJgpgATAa3.png
    Flag disabled: https://screenshot.googleplex.com/8T969gqtxXWRN6m.png
    
    Bug: 365737493
    Change-Id: Ia69cdc2623b2ff18c6532d194c8ed4d84567805f
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5867655
    Reviewed-by: Shuran Huang <shuuran@chromium.org>
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Joshua Thomas <masnoble@chromium.org>

--- front_end/panels/security/security-meta.ts
+++ front_end/panels/security/security-meta.ts
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import * as Common from '../../core/common/common.js';
 import * as i18n from '../../core/i18n/i18n.js';
 import * as UI from '../../ui/legacy/legacy.js';
 
@@ -9,14 +10,23 @@ import type * as Security from './security.js';
 
 const UIStrings = {
   /**
-   *@description Title of the security panel
+   *@description Default Title of the security panel
    */
   security: 'Security',
   /**
-   *@description Command to open the security panel
+   *@description Title of security and privacy panel. This is used when the kDevToolsPrivacyUI feature flag is enabled.
+   */
+  securityAndPrivacy: 'Security & Privacy',
+  /**
+   *@description Default command to open the security panel
    */
   showSecurity: 'Show Security',
+  /**
+   *@description Command to open the security and privacy panel. This is used when the kDevToolPrivacyUI feature flag is enabled
+   */
+  showSecurityAndPrivacy: 'Show Security and Privacy',
 };
+
 const str_ = i18n.i18n.registerUIStrings('panels/security/security-meta.ts', UIStrings);
 const i18nLazyString = i18n.i18n.getLazilyComputedLocalizedString.bind(undefined, str_);
 
@@ -32,8 +42,12 @@ async function loadSecurityModule(): Promise<typeof Security> {
 UI.ViewManager.registerViewExtension({
   location: UI.ViewManager.ViewLocationValues.PANEL,
   id: 'security',
-  title: i18nLazyString(UIStrings.security),
-  commandPrompt: i18nLazyString(UIStrings.showSecurity),
+  title: () => Common.Settings.Settings.instance().getHostConfig().devToolsPrivacyUI?.enabled ?
+      i18nLazyString(UIStrings.securityAndPrivacy)() :
+      i18nLazyString(UIStrings.security)(),
+  commandPrompt: () => Common.Settings.Settings.instance().getHostConfig().devToolsPrivacyUI?.enabled ?
+      i18nLazyString(UIStrings.showSecurityAndPrivacy)() :
+      i18nLazyString(UIStrings.showSecurity)(),
   order: 80,
   persistence: UI.ViewManager.ViewPersistence.CLOSEABLE,
   async loadView() {

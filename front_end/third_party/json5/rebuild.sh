12fe3976833f9f2038038f76f38fa77671f1c1bb
Oliver Dunk <oliverdunk@chromium.org>
2024-10-09 22:50:37 +0100

    DevTools: Add json5 dependency to front_end/third_party
    Adds json5 as a dependency to the frontend code. This will be used in
    the Application > Extension Storage implementation to allow for more
    relaxed parsing of values being set by the user.
    
    This CL follows the process in https://chromium.googlesource.com/devtools/devtools-frontend/+/HEAD/docs/dependencies.md#third_party-code-included-in-devtools-bundle. Security and ATL approval has been given for this inclusion.
    
    Bug: 40963428
    Change-Id: I93c6146453454e7051137806948c13a6defcd3c4
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5920841
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Oliver Dunk <oliverdunk@chromium.org>

---
+++ front_end/third_party/json5/rebuild.sh
@@ -0,0 +1,5 @@
+npm install
+./node_modules/.bin/esbuild --bundle package/lib/index.js --format=esm --outdir=lib
+rm -rf node_modules package-lock.json
+# Because there's a bug in clang causing it to reformat import lists even where formatting is disabled, run it right away
+git cl format --js

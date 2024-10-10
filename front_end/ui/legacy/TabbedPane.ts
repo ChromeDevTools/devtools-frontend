ca4ef70fdefbb0bbf0c43f5306b99fae312b7699
Benedikt Meurer <bmeurer@chromium.org>
2024-10-10 12:53:25 +0200

    Make 'x' on tabs react to Enter/Space correctly.
    Fixed: 372411090
    Change-Id: Ibc25ef2126422536cd763af50bce6a313f70d74e
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5920267
    Reviewed-by: Kim-Anh Tran <kimanh@chromium.org>
    Auto-Submit: Benedikt Meurer <bmeurer@chromium.org>
    Commit-Queue: Kim-Anh Tran <kimanh@chromium.org>

--- front_end/ui/legacy/TabbedPane.ts
+++ front_end/ui/legacy/TabbedPane.ts
@@ -1214,6 +1214,7 @@ export class TabbedPaneTab {
       tabElement.classList.add('measuring');
     } else {
       tabElement.addEventListener('click', this.tabClicked.bind(this), false);
+      tabElement.addEventListener('keydown', this.tabKeyDown.bind(this), false);
       tabElement.addEventListener('auxclick', this.tabClicked.bind(this), false);
       tabElement.addEventListener('mousedown', this.tabMouseDown.bind(this), false);
       tabElement.addEventListener('mouseup', this.tabMouseUp.bind(this), false);
@@ -1262,6 +1263,19 @@ export class TabbedPaneTab {
   private isCloseIconClicked(element: HTMLElement): boolean {
     return element?.classList.contains('tabbed-pane-close-button') ||
         element?.parentElement?.classList.contains('tabbed-pane-close-button') || false;
+  }
+
+  private tabKeyDown(ev: Event): void {
+    const event = ev as KeyboardEvent;
+    switch (event.key) {
+      case 'Enter':
+      case ' ':
+        if (this.isCloseIconClicked(event.target as HTMLElement)) {
+          this.closeTabs([this.id]);
+          ev.consume(true);
+          return;
+        }
+    }
   }
 
   private tabClicked(ev: Event): void {

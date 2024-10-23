39cc1b69d722b91298a8002bc1cb071d303c49e4
Kateryna Prokopenko <kprokopenko@chromium.org>
2024-10-23 08:23:15 +0000

    Temporarily still use soft menu in Performance panel till backend lands
    Bug: 368240754
    Change-Id: Ibd7ff4a1620ab2771129c83b672904cf61501063
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5954343
    Reviewed-by: Kim-Anh Tran <kimanh@chromium.org>
    Commit-Queue: Kim-Anh Tran <kimanh@chromium.org>
    Auto-Submit: Kateryna Prokopenko <kprokopenko@chromium.org>

--- front_end/panels/timeline/TimelineFlameChartDataProvider.ts
+++ front_end/panels/timeline/TimelineFlameChartDataProvider.ts
@@ -213,7 +213,8 @@ export class TimelineFlameChartDataProvider extends Common.ObjectWrapper.ObjectW
       return;
     }
 
-    const contextMenu = new UI.ContextMenu.ContextMenu(event);
+    // TODO(crbug.com/368240754): Temporarily use soft menu for the shortcuts to show, till the accelerators backend CLs land.
+    const contextMenu = new UI.ContextMenu.ContextMenu(event, {useSoftMenu: true});
 
     if (UI.ActionRegistry.ActionRegistry.instance().hasAction('drjones.performance-panel-context')) {
       const aiNode = this.getAIEventNodeTreeFromEntryIndex(entryIndex);
@@ -229,6 +230,7 @@ export class TimelineFlameChartDataProvider extends Common.ObjectWrapper.ObjectW
       disabled: !possibleActions?.[PerfUI.FlameChart.FilterAction.MERGE_FUNCTION],
       jslogContext: 'hide-function',
     });
+    hideEntryOption.setShortcut('H');
     hideEntryOption.setAccelerator(UI.KeyboardShortcut.Keys.H, [UI.KeyboardShortcut.Modifiers.None]);
     hideEntryOption.setIsDevToolsPerformanceMenuItem(true);
 
@@ -238,6 +240,7 @@ export class TimelineFlameChartDataProvider extends Common.ObjectWrapper.ObjectW
       disabled: !possibleActions?.[PerfUI.FlameChart.FilterAction.COLLAPSE_FUNCTION],
       jslogContext: 'hide-children',
     });
+    hideChildrenOption.setShortcut('C');
     hideChildrenOption.setAccelerator(UI.KeyboardShortcut.Keys.C, [UI.KeyboardShortcut.Modifiers.None]);
     hideChildrenOption.setIsDevToolsPerformanceMenuItem(true);
 
@@ -248,6 +251,7 @@ export class TimelineFlameChartDataProvider extends Common.ObjectWrapper.ObjectW
           disabled: !possibleActions?.[PerfUI.FlameChart.FilterAction.COLLAPSE_REPEATING_DESCENDANTS],
           jslogContext: 'hide-repeating-children',
         });
+    hideRepeatingChildrenOption.setShortcut('R');
     hideRepeatingChildrenOption.setAccelerator(UI.KeyboardShortcut.Keys.R, [UI.KeyboardShortcut.Modifiers.None]);
     hideRepeatingChildrenOption.setIsDevToolsPerformanceMenuItem(true);
 
@@ -257,6 +261,7 @@ export class TimelineFlameChartDataProvider extends Common.ObjectWrapper.ObjectW
       disabled: !possibleActions?.[PerfUI.FlameChart.FilterAction.RESET_CHILDREN],
       jslogContext: 'reset-children',
     });
+    resetChildrenOption.setShortcut('U');
     resetChildrenOption.setAccelerator(UI.KeyboardShortcut.Keys.U, [UI.KeyboardShortcut.Modifiers.None]);
     resetChildrenOption.setIsDevToolsPerformanceMenuItem(true);
 

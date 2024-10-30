a67e0c903f923e2f98855e73a241c0c5dc523402
Alex Rudenko <alexrudenko@chromium.org>
2024-10-30 09:58:27 +0100

    Enable MacAllowBackgroundingRenderProcesses
    Bug: 357020932
    Change-Id: I7ab0c1128935632cee1bfa021fc3bdf651d1eb71
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5975680
    Reviewed-by: Philip Pfaffe <pfaffe@chromium.org>
    Auto-Submit: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Philip Pfaffe <pfaffe@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>

--- test/conductor/hooks.ts
+++ test/conductor/hooks.ts
@@ -76,13 +76,12 @@ function launchChrome() {
   ];
 
   const disabledFeatures = [
-    'BrowserThreadPoolAdjustment',            // crbug.com/364165032
-    'DeferRendererTasksAfterInput',           // crbug.com/361078921
-    'PMProcessPriorityPolicy',                // crbug.com/361252079
+    'BrowserThreadPoolAdjustment',                 // crbug.com/364165032
+    'DeferRendererTasksAfterInput',                // crbug.com/361078921
+    'PMProcessPriorityPolicy',                     // crbug.com/361252079
+    'MojoChannelAssociatedSendUsesRunOrPostTask',  // crbug.com/376228320
     'BoostRenderProcessForLoading',
-    'MacAllowBackgroundingRenderProcesses',
     'MojoChannelAssociatedCrashesOnSendError',
-    'MojoChannelAssociatedSendUsesRunOrPostTask',
   ];
   const launchArgs = [
     '--remote-allow-origins=*',

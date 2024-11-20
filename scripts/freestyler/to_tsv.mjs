bf9bcbf328882dcda5b84990956ffc93b9c52eae
Alex Rudenko <alexrudenko@chromium.org>
2024-11-20 14:55:12 +0100

    A helper script to convert to tsv
    Bug: 379059135
    Change-Id: I555f763d2123f1cb1b39fd3ae9468bb0f69613f1
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6038154
    Reviewed-by: Ergün Erdoğmuş <ergunsh@chromium.org>
    Commit-Queue: Ergün Erdoğmuş <ergunsh@chromium.org>
    Auto-Submit: Alex Rudenko <alexrudenko@chromium.org>

---
+++ scripts/freestyler/to_tsv.mjs
@@ -0,0 +1,40 @@
+// Copyright 2024 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+
+// Converts a JSON results file into two TSV files: queries and responses.
+// Usage: node to_tsv.mjs path/to/file.json
+
+import fs from 'fs/promises';
+import assert from 'assert';
+
+const file = process.argv[2];
+
+assert(file.endsWith('.json'));
+
+const data = JSON.parse(await fs.readFile(file, 'utf-8'));
+
+const responses = [];
+const queries = [];
+
+function formatRequest(request) {
+  const lines = [
+    ...(request.chat_history || []).map(item => item.text),
+    request.input,
+  ]
+  return lines.join('\n\n===\n\n');
+}
+
+const escapeCell = val => `"${(String(val)).replaceAll('"', '""')}"`;
+
+for (const example of data.examples) {
+  responses.push(escapeCell(example.response));
+  queries.push(escapeCell(formatRequest(example.request)));
+}
+
+assert(responses.length === queries.length);
+
+await fs.writeFile(file.replace('.json', '-queries.tsv'), queries.join('\n'));
+await fs.writeFile(file.replace('.json', '-responses.tsv'), responses.join('\n'));
+

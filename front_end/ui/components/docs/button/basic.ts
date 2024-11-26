c146b79184a316ea938d889ab71035c6bca63cb6
Kim-Anh Tran <kimanh@chromium.org>
2024-11-26 09:02:18 +0100

    [doc] Update component doc button documentation
    This CL simplifies the documentation for buttons to concentrate
    on the most important variants.
    
    Screenshot: https://i.imgur.com/64NmrHW.png
    Bug: 357513556
    Change-Id: I54a9b2120985a8ccdddf2d3f943db65fdef493ab
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6012181
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Auto-Submit: Kim-Anh Tran <kimanh@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>

--- front_end/ui/components/docs/button/basic.ts
+++ front_end/ui/components/docs/button/basic.ts
@@ -9,396 +9,160 @@ import * as ComponentHelpers from '../../helpers/helpers.js';
 await ComponentHelpers.ComponentServerSetup.setup();
 await FrontendHelpers.initializeGlobalVars();
 
-const testIcon = 'file-image';
-const toggledIconName = 'file-document';
-const plusIcon = 'plus';
-const minusIcon = 'minus';
+const BUTTONS_SECTION = '#buttons';
+const PRIMARY_SECTION = '#primary-buttons';
+const TONAL_SECTION = '#tonal-buttons';
+const OUTLINED_SECTION = '#outlined-buttons';
+const TEXT_SECTION = '#text-buttons';
+const ICON_SECTION = '#icon-buttons';
 
-function appendButton(button: Buttons.Button.Button): void {
-  document.querySelector('#container')?.appendChild(button);
+const DEFAULT_TEXT = 'Default';
+const WITH_ICON_TEXT = 'With icon';
+const MICRO_TEXT = 'Micro';
+
+function append(section: string, element: HTMLElement): void {
+  document.querySelector(section)?.appendChild(element);
 }
 
-function appendToToolbar(element: HTMLElement): void {
-  document.querySelector('#toolbar')?.appendChild(element);
-}
+// Buttons
+{
+  const primaryButton = new Buttons.Button.Button();
+  primaryButton.data = {
+    variant: Buttons.Button.Variant.PRIMARY,
+  };
+  primaryButton.innerText = 'Primary button';
+  append(BUTTONS_SECTION, primaryButton);
 
-function appendToSmallToolbar(element: HTMLElement): void {
-  document.querySelector('#small-toolbar')?.appendChild(element);
-}
+  const tonalButton = new Buttons.Button.Button();
+  tonalButton.data = {
+    variant: Buttons.Button.Variant.TONAL,
+  };
+  tonalButton.innerText = 'Tonal button';
+  append(BUTTONS_SECTION, tonalButton);
 
-// Primary
-const primaryButton = new Buttons.Button.Button();
-primaryButton.data = {
-  variant: Buttons.Button.Variant.PRIMARY,
-};
-primaryButton.innerText = 'Click me';
-primaryButton.title = 'Custom title';
-primaryButton.onclick = () => alert('clicked');
-appendButton(primaryButton);
+  const outlinedButton = new Buttons.Button.Button();
+  outlinedButton.data = {
+    variant: Buttons.Button.Variant.OUTLINED,
+  };
+  outlinedButton.innerText = 'Outlined button';
+  append(BUTTONS_SECTION, outlinedButton);
 
-// Primary (forced active)
-const forcedActive = new Buttons.Button.Button();
-forcedActive.data = {
-  variant: Buttons.Button.Variant.PRIMARY,
-  active: true,
-};
-forcedActive.innerText = 'Forced active';
-forcedActive.onclick = () => alert('clicked');
-appendButton(forcedActive);
+  const textButton = new Buttons.Button.Button();
+  textButton.data = {
+    variant: Buttons.Button.Variant.TEXT,
+  };
+  textButton.innerText = 'Text button';
+  append(BUTTONS_SECTION, textButton);
 
-// Primary (forced spinner)
-const forcedSpinner = new Buttons.Button.Button();
-forcedSpinner.data = {
-  variant: Buttons.Button.Variant.PRIMARY,
-  spinner: true,
-};
-forcedSpinner.innerText = 'Forced spinner';
-forcedSpinner.onclick = () => alert('clicked');
-appendButton(forcedSpinner);
-
-// Secondary
-const secondarymicroButton = new Buttons.Button.Button();
-secondarymicroButton.innerText = 'Click me';
-secondarymicroButton.onclick = () => alert('clicked');
-secondarymicroButton.data = {
-  variant: Buttons.Button.Variant.OUTLINED,
-  size: Buttons.Button.Size.MICRO,
-};
-appendButton(secondarymicroButton);
-
-// Secondary
-const secondaryButton = new Buttons.Button.Button();
-secondaryButton.innerText = 'Click me';
-secondaryButton.onclick = () => alert('clicked');
-secondaryButton.data = {
-  variant: Buttons.Button.Variant.OUTLINED,
-};
-appendButton(secondaryButton);
-
-// Secondary spinner
-const secondarySpinnerButton = new Buttons.Button.Button();
-secondarySpinnerButton.innerText = 'Click me';
-secondarySpinnerButton.onclick = () => alert('clicked');
-secondarySpinnerButton.data = {
-  variant: Buttons.Button.Variant.OUTLINED,
-  spinner: true,
-};
-appendButton(secondarySpinnerButton);
-
-// Secondary spinner
-const textButton = new Buttons.Button.Button();
-textButton.innerText = 'Click me';
-textButton.onclick = () => alert('clicked');
-textButton.data = {
-  variant: Buttons.Button.Variant.TEXT,
-};
-appendButton(textButton);
-
-// Primary
-const disabledPrimaryButtons = new Buttons.Button.Button();
-disabledPrimaryButtons.data = {
-  variant: Buttons.Button.Variant.PRIMARY,
-  disabled: true,
-};
-disabledPrimaryButtons.innerText = 'Cannot click me';
-disabledPrimaryButtons.onclick = () => alert('clicked');
-appendButton(disabledPrimaryButtons);
-
-// Primary spinner
-const disabledSpinnerPrimaryButtons = new Buttons.Button.Button();
-disabledSpinnerPrimaryButtons.data = {
-  variant: Buttons.Button.Variant.PRIMARY,
-  disabled: true,
-  spinner: true,
-};
-disabledSpinnerPrimaryButtons.innerText = 'Cannot click me';
-disabledSpinnerPrimaryButtons.onclick = () => alert('clicked');
-appendButton(disabledSpinnerPrimaryButtons);
-
-// Secondary
-const disabledSecondaryButton = new Buttons.Button.Button();
-disabledSecondaryButton.innerText = 'Cannot click me';
-disabledSecondaryButton.onclick = () => alert('clicked');
-disabledSecondaryButton.data = {
-  variant: Buttons.Button.Variant.OUTLINED,
-  disabled: true,
-};
-appendButton(disabledSecondaryButton);
-
-// Secondary spinner
-const disabledSpinnerSecondaryButton = new Buttons.Button.Button();
-disabledSpinnerSecondaryButton.innerText = 'Cannot click me';
-disabledSpinnerSecondaryButton.onclick = () => alert('clicked');
-disabledSpinnerSecondaryButton.data = {
-  variant: Buttons.Button.Variant.OUTLINED,
-  disabled: true,
-  spinner: true,
-};
-appendButton(disabledSpinnerSecondaryButton);
-
-// Primary Icon
-const primaryIconButton = new Buttons.Button.Button();
-primaryIconButton.innerText = 'Click me';
-primaryIconButton.data = {
-  variant: Buttons.Button.Variant.PRIMARY,
-  iconName: testIcon,
-};
-primaryIconButton.onclick = () => alert('clicked');
-appendButton(primaryIconButton);
-
-// Secondary Icon
-const secondaryIconButton = new Buttons.Button.Button();
-secondaryIconButton.innerText = 'Focus the first button';
-secondaryIconButton.onclick = () => {
-  primaryButton.focus();
-};
-secondaryIconButton.data = {
-  variant: Buttons.Button.Variant.OUTLINED,
-  iconName: testIcon,
-};
-appendButton(secondaryIconButton);
-
-// Text Icon
-const textIconButton = new Buttons.Button.Button();
-textIconButton.innerText = 'Click me';
-textIconButton.onclick = () => alert('clicked');
-textIconButton.data = {
-  variant: Buttons.Button.Variant.TEXT,
-  iconName: testIcon,
-};
-appendButton(textIconButton);
-
-// Secondary Icon Micro
-const secondaryMicroIconButton = new Buttons.Button.Button();
-secondaryMicroIconButton.innerText = 'Click me';
-secondaryMicroIconButton.onclick = () => {
-  primaryButton.focus();
-};
-secondaryMicroIconButton.data = {
-  variant: Buttons.Button.Variant.OUTLINED,
-  iconName: testIcon,
-  size: Buttons.Button.Size.MICRO,
-};
-appendButton(secondaryMicroIconButton);
-
-// Primary Icon Only
-const primaryIconOnlyButton = new Buttons.Button.Button();
-primaryIconOnlyButton.data = {
-  variant: Buttons.Button.Variant.PRIMARY,
-  iconName: testIcon,
-};
-primaryIconOnlyButton.onclick = () => alert('clicked');
-primaryIconOnlyButton.style.width = '24px';
-appendButton(primaryIconOnlyButton);
-
-// Primary Icon with a name
-const primaryIconByNameButton = new Buttons.Button.Button();
-primaryIconByNameButton.data = {
-  variant: Buttons.Button.Variant.PRIMARY,
-  iconName: 'file-image',
-};
-primaryIconByNameButton.onclick = () => alert('clicked');
-primaryIconByNameButton.innerHTML = 'Icon by name';
-appendButton(primaryIconByNameButton);
-
-// Secondary Icon Only
-const secondaryIconOnlyButton = new Buttons.Button.Button();
-secondaryIconOnlyButton.onclick = () => alert('clicked');
-secondaryIconOnlyButton.style.width = '24px';
-secondaryIconOnlyButton.data = {
-  variant: Buttons.Button.Variant.OUTLINED,
-  iconName: testIcon,
-};
-appendButton(secondaryIconOnlyButton);
-
-// Primary Toggle
-const primaryToggleIconButton = new Buttons.Button.Button();
-primaryToggleIconButton.style.width = '24px';
-primaryToggleIconButton.data = {
-  variant: Buttons.Button.Variant.ICON_TOGGLE,
-  toggleType: Buttons.Button.ToggleType.PRIMARY,
-  iconName: testIcon,
-  toggledIconName,
-  toggled: false,
-};
-appendButton(primaryToggleIconButton);
-
-// Red Toggle
-const redToggleIconButton = new Buttons.Button.Button();
-redToggleIconButton.style.width = '24px';
-redToggleIconButton.data = {
-  variant: Buttons.Button.Variant.ICON_TOGGLE,
-  toggleType: Buttons.Button.ToggleType.RED,
-  iconName: testIcon,
-  toggledIconName,
-  toggled: true,
-};
-appendButton(redToggleIconButton);
-
-// Small Primary Icon
-const smallPrimaryIconButton = new Buttons.Button.Button();
-smallPrimaryIconButton.innerText = 'Click me';
-smallPrimaryIconButton.data = {
-  variant: Buttons.Button.Variant.PRIMARY,
-  iconName: testIcon,
-  size: Buttons.Button.Size.SMALL,
-};
-smallPrimaryIconButton.onclick = () => alert('clicked');
-appendButton(smallPrimaryIconButton);
-
-// Small Secondary Icon Only
-const smallSecondaryIconOnlyButton = new Buttons.Button.Button();
-smallSecondaryIconOnlyButton.onclick = () => alert('clicked');
-smallSecondaryIconOnlyButton.style.width = '18px';
-smallSecondaryIconOnlyButton.data = {
-  variant: Buttons.Button.Variant.OUTLINED,
-  iconName: testIcon,
-  size: Buttons.Button.Size.SMALL,
-};
-appendButton(smallSecondaryIconOnlyButton);
-
-// Disabled Primary Icon
-const disabledPrimaryIconButton = new Buttons.Button.Button();
-disabledPrimaryIconButton.innerText = 'Cannot click me';
-disabledPrimaryIconButton.data = {
-  variant: Buttons.Button.Variant.PRIMARY,
-  iconName: testIcon,
-  size: Buttons.Button.Size.SMALL,
-  disabled: true,
-};
-disabledPrimaryIconButton.onclick = () => alert('clicked');
-appendButton(disabledPrimaryIconButton);
-
-// Small Disabled Secondary Icon Only
-const disabledSecondaryIconOnlyButton = new Buttons.Button.Button();
-disabledSecondaryIconOnlyButton.onclick = () => alert('clicked');
-disabledSecondaryIconOnlyButton.style.width = '18px';
-disabledSecondaryIconOnlyButton.data = {
-  variant: Buttons.Button.Variant.OUTLINED,
-  iconName: testIcon,
-  size: Buttons.Button.Size.SMALL,
-  disabled: true,
-};
-appendButton(disabledSecondaryIconOnlyButton);
-
-// Round Button
-const roundButton = new Buttons.Button.Button();
-roundButton.data = {
-  variant: Buttons.Button.Variant.ICON,
-  iconName: testIcon,
-};
-roundButton.title = 'Round Button';
-roundButton.onclick = () => alert('clicked');
-appendButton(roundButton);
-
-// Disabled Round Button
-const roundButtonDisabled = new Buttons.Button.Button();
-roundButtonDisabled.data = {
-  variant: Buttons.Button.Variant.ICON,
-  iconName: testIcon,
-  disabled: true,
-};
-roundButtonDisabled.title = 'Disabled Round Button';
-roundButtonDisabled.onclick = () => alert('clicked');
-appendButton(roundButtonDisabled);
-
-// Small Round Buttons
-const roundIcons = [
-  {iconName: testIcon},
-  {iconName: plusIcon},
-  {iconName: minusIcon},
-];
-for (const roundIcon of roundIcons) {
-  const smallRoundButton = new Buttons.Button.Button();
-  smallRoundButton.data = {
+  const iconButton = new Buttons.Button.Button();
+  iconButton.data = {
     variant: Buttons.Button.Variant.ICON,
-    size: Buttons.Button.Size.SMALL,
-    ...roundIcon,
+    iconName: 'gear',
   };
-  smallRoundButton.title = 'Small Round Button';
-  smallRoundButton.onclick = () => alert('clicked');
-  appendButton(smallRoundButton);
+  append(BUTTONS_SECTION, iconButton);
 }
 
-// Small Disabled Round Button
-const smallRoundButtonDisabled = new Buttons.Button.Button();
-smallRoundButtonDisabled.data = {
-  variant: Buttons.Button.Variant.ICON,
-  iconName: testIcon,
-  disabled: true,
-  size: Buttons.Button.Size.SMALL,
-};
-smallRoundButtonDisabled.title = 'Small Disabled Round Button';
-smallRoundButtonDisabled.onclick = () => alert('clicked');
-appendButton(smallRoundButtonDisabled);
-
-// Tonal
-const tonalButton = new Buttons.Button.Button();
-tonalButton.data = {
-  variant: Buttons.Button.Variant.TONAL,
-};
-tonalButton.innerText = 'Click me';
-tonalButton.title = 'Custom title';
-tonalButton.onclick = () => alert('clicked');
-appendButton(tonalButton);
-
-for (let i = 0; i < 6; i++) {
-  // Regular Toolbar Button
-  const toolbarButton = new Buttons.Button.Button();
-  toolbarButton.onclick = () => alert('clicked');
-  toolbarButton.data = {
-    variant: i % 2 === 1 ? Buttons.Button.Variant.TOOLBAR : Buttons.Button.Variant.PRIMARY_TOOLBAR,
-    iconName: testIcon,
+// Primary buttons
+{
+  const primaryDefault = new Buttons.Button.Button();
+  primaryDefault.data = {
+    variant: Buttons.Button.Variant.PRIMARY,
   };
-  appendToToolbar(toolbarButton);
-  if (i % 3 === 1) {
-    const sep = document.createElement('div');
-    sep.classList.add('separator');
-    appendToToolbar(sep);
-  }
+  primaryDefault.innerText = DEFAULT_TEXT;
+  append(PRIMARY_SECTION, primaryDefault);
+
+  const primaryWithIcon = new Buttons.Button.Button();
+  primaryWithIcon.innerText = WITH_ICON_TEXT;
+  primaryWithIcon.data = {
+    variant: Buttons.Button.Variant.PRIMARY,
+    iconName: 'plus',
+  };
+  append(PRIMARY_SECTION, primaryWithIcon);
 }
 
-// Disabled Toolbar Button
-const toolbarButton = new Buttons.Button.Button();
-toolbarButton.onclick = () => alert('clicked');
-toolbarButton.data = {
-  variant: Buttons.Button.Variant.TOOLBAR,
-  iconName: testIcon,
-  disabled: true,
-};
-appendToToolbar(toolbarButton);
-
-for (let i = 0; i < 6; i++) {
-  // Small Toolbar Button
-  const smallToolbarButton = new Buttons.Button.Button();
-  smallToolbarButton.onclick = () => alert('clicked');
-  smallToolbarButton.data = {
-    variant: Buttons.Button.Variant.TOOLBAR,
-    size: Buttons.Button.Size.SMALL,
-    iconName: testIcon,
+// Tonal buttons
+{
+  const tonalDefault = new Buttons.Button.Button();
+  tonalDefault.data = {
+    variant: Buttons.Button.Variant.TONAL,
   };
-  appendToSmallToolbar(smallToolbarButton);
-  if (i % 3 === 1) {
-    const sep = document.createElement('div');
-    sep.classList.add('separator');
-    appendToSmallToolbar(sep);
-  }
+  tonalDefault.innerText = DEFAULT_TEXT;
+  append(TONAL_SECTION, tonalDefault);
+
+  const tonalWithIcon = new Buttons.Button.Button();
+  tonalWithIcon.innerText = WITH_ICON_TEXT;
+
+  tonalWithIcon.data = {
+    variant: Buttons.Button.Variant.PRIMARY,
+    iconName: 'plus',
+  };
+  append(TONAL_SECTION, tonalWithIcon);
 }
 
-// Submit Button
-const submitButton = new Buttons.Button.Button();
-submitButton.data = {
-  variant: Buttons.Button.Variant.PRIMARY,
-  type: 'submit',
-};
-submitButton.innerText = 'Submit';
-document.querySelector('#form')?.append(submitButton);
+// Outlined buttons
+{
+  const outlinedDefault = new Buttons.Button.Button();
+  outlinedDefault.data = {
+    variant: Buttons.Button.Variant.OUTLINED,
+  };
+  outlinedDefault.innerText = DEFAULT_TEXT;
+  append(OUTLINED_SECTION, outlinedDefault);
 
-// Reset Button
-const resetButton = new Buttons.Button.Button();
-resetButton.data = {
-  variant: Buttons.Button.Variant.OUTLINED,
-  type: 'reset',
-};
-resetButton.innerText = 'Reset';
-document.querySelector('#form')?.append(resetButton);
+  const outlinedWithIcon = new Buttons.Button.Button();
+  outlinedWithIcon.innerText = WITH_ICON_TEXT;
+  outlinedWithIcon.data = {
+    variant: Buttons.Button.Variant.OUTLINED,
+    iconName: 'plus',
+  };
+  append(OUTLINED_SECTION, outlinedWithIcon);
+
+  const outlinedMicro = new Buttons.Button.Button();
+  outlinedMicro.innerText = MICRO_TEXT;
+  outlinedMicro.data = {
+    variant: Buttons.Button.Variant.OUTLINED,
+    size: Buttons.Button.Size.MICRO,
+  };
+  append(OUTLINED_SECTION, outlinedMicro);
+}
+
+// Text buttons
+{
+  const textDefault = new Buttons.Button.Button();
+  textDefault.innerText = DEFAULT_TEXT;
+  textDefault.data = {
+    variant: Buttons.Button.Variant.TEXT,
+  };
+  append(TEXT_SECTION, textDefault);
+
+  const textWithIcon = new Buttons.Button.Button();
+  textWithIcon.innerText = WITH_ICON_TEXT;
+
+  textWithIcon.data = {
+    variant: Buttons.Button.Variant.TEXT,
+    iconName: 'plus',
+  };
+  append(TEXT_SECTION, textWithIcon);
+}
+
+// Icon buttons
+{
+  const iconDefault = new Buttons.Button.Button();
+  iconDefault.data = {
+    variant: Buttons.Button.Variant.ICON,
+    iconName: 'gear',
+  };
+  append(ICON_SECTION, iconDefault);
+
+  const iconToggle = new Buttons.Button.Button();
+  iconToggle.data = {
+    variant: Buttons.Button.Variant.ICON_TOGGLE,
+    iconName: 'gear',
+    toggledIconName: 'gear',
+    toggled: true,
+    toggleType: Buttons.Button.ToggleType.PRIMARY,
+  };
+  const toggledWithLabel = document.createElement('span');
+  toggledWithLabel.textContent = '(Toggle)';
+  append(ICON_SECTION, iconToggle);
+  append(ICON_SECTION, toggledWithLabel);
+}

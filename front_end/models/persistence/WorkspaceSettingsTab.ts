6e47d4260c49f07f25e236d485d0266cedaa09c9
Yang Guo <yangguo@chromium.org>
2024-11-27 09:32:34 +0100

    Correct decode file name in Workspace settings
    Fixed: 381043531
    Change-Id: I09a6a9535bfba6b156fc894dedbaa3502b097df2
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6049266
    Commit-Queue: Yang Guo <yangguo@chromium.org>
    Reviewed-by: Kim-Anh Tran <kimanh@chromium.org>
    Commit-Queue: Kim-Anh Tran <kimanh@chromium.org>
    Auto-Submit: Yang Guo <yangguo@chromium.org>

--- front_end/models/persistence/WorkspaceSettingsTab.ts
+++ front_end/models/persistence/WorkspaceSettingsTab.ts
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import * as Common from '../../core/common/common.js';
+import * as Host from '../../core/host/host.js';
 import * as i18n from '../../core/i18n/i18n.js';
 import type * as Platform from '../../core/platform/platform.js';
 import * as Cards from '../../ui/components/cards/cards.js';
@@ -157,7 +159,7 @@ export class WorkspaceSettingsTab extends UI.Widget.VBox {
   }
 
   private getFilename(fileSystem: PlatformFileSystem): string {
-    const fileSystemPath = fileSystem.path();
+    const fileSystemPath = Common.ParsedURL.ParsedURL.urlToRawPathString(fileSystem.path(), Host.Platform.isWin());
     const lastIndexOfSlash = fileSystemPath.lastIndexOf('/');
     return fileSystemPath.substr(lastIndexOfSlash + 1);
   }

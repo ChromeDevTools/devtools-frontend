e9e049ed91bfae3d5b894784e41e64e37410a5ce
Kim-Anh Tran <kimanh@chromium.org>
2024-11-28 06:27:54 +0000

    Revert "Correct decode file name in Workspace settings"
    This reverts commit 6e47d4260c49f07f25e236d485d0266cedaa09c9.
    
    Reason for revert: Blocking the roll (CL needs to be adapted)
    
    Original change's description:
    > Correct decode file name in Workspace settings
    >
    > Fixed: 381043531
    > Change-Id: I09a6a9535bfba6b156fc894dedbaa3502b097df2
    > Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6049266
    > Commit-Queue: Yang Guo <yangguo@chromium.org>
    > Reviewed-by: Kim-Anh Tran <kimanh@chromium.org>
    > Commit-Queue: Kim-Anh Tran <kimanh@chromium.org>
    > Auto-Submit: Yang Guo <yangguo@chromium.org>
    
    Change-Id: Iee5af0a3bec281c08d14a13ae0091636e894d82f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6055420
    Commit-Queue: Kim-Anh Tran <kimanh@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>

--- front_end/models/persistence/WorkspaceSettingsTab.ts
+++ front_end/models/persistence/WorkspaceSettingsTab.ts
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import * as Common from '../../core/common/common.js';
-import * as Host from '../../core/host/host.js';
 import * as i18n from '../../core/i18n/i18n.js';
 import type * as Platform from '../../core/platform/platform.js';
 import * as Cards from '../../ui/components/cards/cards.js';
@@ -159,7 +157,7 @@ export class WorkspaceSettingsTab extends UI.Widget.VBox {
   }
 
   private getFilename(fileSystem: PlatformFileSystem): string {
-    const fileSystemPath = Common.ParsedURL.ParsedURL.urlToRawPathString(fileSystem.path(), Host.Platform.isWin());
+    const fileSystemPath = fileSystem.path();
     const lastIndexOfSlash = fileSystemPath.lastIndexOf('/');
     return fileSystemPath.substr(lastIndexOfSlash + 1);
   }

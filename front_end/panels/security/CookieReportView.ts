e4b72f61f7e6290c0f8d3b00839dc32e5542aac6
Shuran Huang <shuuran@chromium.org>
2025-01-07 13:28:12 +0000

    Handle Grace period exceptions that are applied at the 1p level
    Bug: b/382264360
    Change-Id: Ie61254dba7feb6a08ffefcf063e99766d58f5ddc
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6148734
    Auto-Submit: Shuran Huang <shuuran@chromium.org>
    Reviewed-by: Wolfgang Beyer <wolfi@chromium.org>
    Commit-Queue: Shuran Huang <shuuran@chromium.org>

--- front_end/panels/security/CookieReportView.ts
+++ front_end/panels/security/CookieReportView.ts
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import type * as Common from '../../core/common/common.js';
+import * as Common from '../../core/common/common.js';
 import * as i18n from '../../core/i18n/i18n.js';
 import * as SDK from '../../core/sdk/sdk.js';
 import * as Protocol from '../../generated/protocol.js';
@@ -451,8 +451,11 @@ export class CookieReportView extends UI.Widget.VBox {
         })}`;
       }
       case Protocol.Audits.InsightType.GracePeriod: {
+        const url = SDK.TargetManager.TargetManager.instance().primaryPageTarget()?.inspectedURL();
         const gracePeriodLink = UI.XLink.XLink.create(
             'https://developers.google.com/privacy-sandbox/cookies/dashboard?url=' +
+                // The order of the URLs matters - needs to be 1P + 3P.
+                (url ? Common.ParsedURL.ParsedURL.fromString(url)?.host + '+' : '') +
                 (domain.charAt(0) === '.' ? domain.substring(1) : domain),
             i18nString(UIStrings.reportedIssues), undefined, undefined, 'compatibility-lookup-link');
 

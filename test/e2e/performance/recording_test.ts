8b387fbc861c84ecc5af9b2e2938784516672ee5
Jack Franklin <jacktfranklin@chromium.org>
2025-01-14 11:51:39 +0000

    e2e: add test that imports a stored trace file
    We have e2e tests for live recordings, but I wanted to add one for a
    saved file too.
    
    Bug: none
    Change-Id: Ie951551299aa780daaf8939d6d55a8bd9594862d
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6168858
    Auto-Submit: Jack Franklin <jacktfranklin@chromium.org>
    Reviewed-by: Andres Olivares <andoli@chromium.org>
    Commit-Queue: Andres Olivares <andoli@chromium.org>

--- test/e2e/performance/recording_test.ts
+++ test/e2e/performance/recording_test.ts
@@ -2,8 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {waitForFunction} from '../../shared/helper.js';
+import {assert} from 'chai';
+import * as path from 'path';
 
+import {GEN_DIR} from '../../conductor/paths.js';
+import {waitFor, waitForFunction} from '../../shared/helper.js';
 import {
   getTotalTimeFromSummary,
   navigateToPerformanceTab,
@@ -31,6 +34,20 @@ describe('The Performance panel', () => {
     await waitForFunction(async () => {
       const totalTime = await getTotalTimeFromSummary();
       return totalTime > 0;
+    });
+  });
+
+  it('can import a stored trace file', async () => {
+    await navigateToPerformanceTab('empty');
+    const uploadProfileHandle = await waitFor<HTMLInputElement>('input[type=file]');
+    assert.isNotNull(uploadProfileHandle, 'unable to upload the performance profile');
+    const testTrace = path.join(GEN_DIR, 'test/e2e/resources/performance/timeline/web.dev-trace.json.gz');
+    await uploadProfileHandle.uploadFile(testTrace);
+    const canvas = await waitFor('canvas.flame-chart-canvas');
+    // Check that we have rendered the timeline canvas.
+    await waitForFunction(async () => {
+      const height = await canvas.evaluate(elem => elem.clientHeight);
+      return height > 200;
     });
   });
 });

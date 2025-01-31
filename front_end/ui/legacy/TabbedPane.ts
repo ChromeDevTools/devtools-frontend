dd16bac88313ff7437828b47e467a8b721f85d88
Paul Irish <paulirish@chromium.org>
2025-01-31 09:52:00 -0800

    style upgrades

--- front_end/ui/legacy/TabbedPane.ts
+++ front_end/ui/legacy/TabbedPane.ts
@@ -446,10 +446,10 @@ export class TabbedPane extends Common.ObjectWrapper.eventMixin<EventTypes, type
     this.updateTabElements();
   }
 
-  setBadge(id: string, content: string|null): void {
+  setBadge(id: string, content: string|null, className?: string): void {
     const badge = document.createElement('span');
     badge.textContent = content;
-    badge.classList.add('badge');
+    badge.classList.add('badge', className ?? '');
     this.setSuffixElement(id, content ? badge : null);
   }
 

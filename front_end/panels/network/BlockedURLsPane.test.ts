64703819c5a7e3f9a43a3f7a526195ad52a1121f
Kim-Anh Tran <kimanh@chromium.org>
2025-02-18 09:22:49 +0100

    [GM3Restyling] Update network blocking request empty states
    Before: https://i.imgur.com/2Me0vzH.png
    After: https://i.imgur.com/i3OuKr4.png
    Bug: 325443331
    Change-Id: I473e959a63b16412e672b53a8ba76417f1488389
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6275932
    Commit-Queue: Kim-Anh Tran <kimanh@chromium.org>
    Reviewed-by: Kateryna Prokopenko <kprokopenko@chromium.org>

--- front_end/panels/network/BlockedURLsPane.test.ts
+++ front_end/panels/network/BlockedURLsPane.test.ts
@@ -5,6 +5,7 @@
 import * as Platform from '../../core/platform/platform.js';
 import * as SDK from '../../core/sdk/sdk.js';
 import * as Logs from '../../models/logs/logs.js';
+import {dispatchClickEvent} from '../../testing/DOMHelpers.js';
 import {createTarget, registerNoopActions} from '../../testing/EnvironmentHelpers.js';
 import {describeWithMockConnection} from '../../testing/MockConnection.js';
 
@@ -18,6 +19,31 @@ describeWithMockConnection('BlockedURLsPane', () => {
       'network.add-network-request-blocking-pattern',
       'network.remove-all-network-request-blocking-patterns',
     ]);
+  });
+
+  it('shows a placeholder', () => {
+    const blockedURLsPane = new Network.BlockedURLsPane.BlockedURLsPane();
+    const blockedElement = blockedURLsPane.contentElement.querySelector('.blocked-urls');
+    const placeholder = blockedElement?.shadowRoot?.querySelector('.empty-state');
+    assert.exists(placeholder);
+    assert.deepEqual(placeholder.querySelector('.empty-state-header')?.textContent, 'No blocked network requests');
+    assert.deepEqual(
+        placeholder.querySelector('.empty-state-description > span')?.textContent,
+        'Add a pattern to block network requests by clicking on the \"Add pattern\" button.');
+  });
+
+  it('Add pattern button triggers showing the editor view', () => {
+    const blockedURLsPane = new Network.BlockedURLsPane.BlockedURLsPane();
+    const blockedElement = blockedURLsPane.contentElement.querySelector('.blocked-urls');
+    const list = blockedElement?.shadowRoot?.querySelector('.list');
+    const placeholder = list?.querySelector('.empty-state');
+
+    const button = placeholder?.querySelector('devtools-button');
+    assert.exists(button);
+
+    assert.isNull(list?.querySelector('.editor-content'));
+    dispatchClickEvent(button);
+    assert.exists(list?.querySelector('.editor-content'));
   });
 
   describe('update', () => {

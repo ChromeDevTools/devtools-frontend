12187c44f6e6012c9da60847d691eec8a9fb2b64
Nikolay Vitkov <nvitkov@chromium.org>
2025-03-13 13:38:01 +0100

    [cleanup] Types around ViewFunctionHelper
    Bug: none
    Change-Id: I55cd01185cd811cf57b14da941a7d8a3f5546b87
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6351856
    Commit-Queue: Nikolay Vitkov <nvitkov@chromium.org>
    Auto-Submit: Nikolay Vitkov <nvitkov@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>

--- front_end/testing/ViewFunctionHelpers.ts
+++ front_end/testing/ViewFunctionHelpers.ts
@@ -2,11 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import type * as Platform from '../core/platform/platform.js';
 import type * as UI from '../ui/legacy/legacy.js';
 
-type WidgetConstructor = abstract new (...args: any[]) => UI.Widget.Widget|HTMLElement;
+type WidgetConstructor = Platform.Constructor.AbstractConstructor<UI.Widget.Widget|HTMLElement>;
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-type ViewFunctionLike = ((input: any, output: any, target: HTMLElement) => void);
+type ViewFunctionLike = (input: any, output: any, target: HTMLElement) => void;
 
 type FindViewFunction<ParametersT extends readonly unknown[]> = ParametersT extends [infer Head, ...infer Tail] ?
     Head extends ViewFunctionLike ? Head : FindViewFunction<Tail>:
@@ -18,13 +19,9 @@ type ViewFunction<WidgetConstructorT extends WidgetConstructor> =
 type ViewFunctionParameters<WidgetConstructorT extends WidgetConstructor> =
     Parameters<ViewFunction<WidgetConstructorT>>;
 
-export type ViewInput<WidgetConstructorT extends WidgetConstructor> =
-    ViewFunctionParameters<WidgetConstructorT>[0];
+type ViewInput<WidgetConstructorT extends WidgetConstructor> = ViewFunctionParameters<WidgetConstructorT>[0];
 
-export type ViewOutput<WidgetConstructorT extends WidgetConstructor> =
-    // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-    {} extends ViewFunctionParameters<WidgetConstructorT>[1] ? never :
-    ViewFunctionParameters<WidgetConstructorT>[1];
+type ViewOutput<WidgetConstructorT extends WidgetConstructor> = ViewFunctionParameters<WidgetConstructorT>[1];
 
 interface ViewStubExtensions<WidgetConstructorT extends WidgetConstructor> {
   input: ViewInput<WidgetConstructorT>;
@@ -41,8 +38,9 @@ export type ViewFunctionStub<WidgetConstructorT extends WidgetConstructor> =
     ViewFunction<WidgetConstructorT>&ViewStubExtensions<WidgetConstructorT>;
 
 export function createViewFunctionStub<WidgetConstructorT extends WidgetConstructor>(
-    constructor: WidgetConstructorT,
-    outputValues?: ViewOutput<WidgetConstructorT>): ViewFunctionStub<WidgetConstructorT> {
+    _constructor: WidgetConstructorT,
+    outputValues?: ViewOutput<WidgetConstructorT>,
+    ): ViewFunctionStub<WidgetConstructorT> {
   const result: InternalViewStubExtensions<WidgetConstructorT> =
       ((input: ViewInput<WidgetConstructorT>, output: ViewOutput<WidgetConstructorT>, _target: HTMLElement) => {
         ++result.callCount;

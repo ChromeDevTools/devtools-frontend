db52fce5045b0eb01a409ff6551a786b2f70f277
Jack Franklin <jacktfranklin@chromium.org>
2025-03-25 18:07:03 +0000

    Define window.scheduler as non-optional
    We used the wicg-task-scheduling types for `window.scheduler` but it
    types it as optional - this is true for the general case but because we
    are in Chrome we know it exists.
    
    Unfortunately you cannot override the member from optional or not on
    `Window`, so this CL removes the use of the
    `@types/wicg-task-scheduling` package and reuses the code inline with
    the extension to the `Window` interface making it always present.
    
    Bug: none
    Change-Id: I8399d3477f89f88e758ed31399c6256fcbd31de9
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6393155
    Auto-Submit: Jack Franklin <jacktfranklin@chromium.org>
    Reviewed-by: Connor Clark <cjamcl@chromium.org>
    Commit-Queue: Connor Clark <cjamcl@chromium.org>

--- front_end/global_typings/global_defs.d.ts
+++ front_end/global_typings/global_defs.d.ts
@@ -6,3 +6,53 @@ declare module '*.css.js' {
   const styles: {cssText: string};
   export default styles;
 }
+
+// Types for the Scheduler API.
+// These are taken from
+// https://github.com/DefinitelyTyped/DefinitelyTyped/tree/master/types/wicg-task-scheduling
+// but modified because within Chrome we can use the API without worrying that
+// it is undefined.
+// This code is licensed under the MIT license
+/**
+ * MIT License
+ * Copyright (c) Microsoft Corporation.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE
+ */
+type PostTaskPriority = 'user-blocking'|'user-visible'|'background';
+
+interface PostTaskOptions {
+  priority?: PostTaskPriority;
+  signal?: AbortSignal;
+  delay?: number;
+}
+
+interface Scheduler {
+  yield(): Promise<void>;
+  postTask<T>(callback: () => T): Promise<T>;
+  postTask<T>(callback: () => T, options: PostTaskOptions): Promise<T>;
+}
+
+interface Window {
+  readonly scheduler: Scheduler;
+}
+
+interface WorkerGlobalScope {
+  readonly scheduler?: Scheduler;
+}

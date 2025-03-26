72cff9ffa56e10b836a39e3847ef0b26b1ed5e94
Adam Raine <asraine@chromium.org>
2025-03-26 14:46:17 -0700

    [Lighthouse] Roll Lighthouse 12.5.1
    Bug: 40543651
    Change-Id: Id022662b3b9587789e24721e6797de0731e8da63
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6398222
    Commit-Queue: Connor Clark <cjamcl@chromium.org>
    Auto-Submit: Adam Raine <asraine@chromium.org>
    Reviewed-by: Connor Clark <cjamcl@chromium.org>
    Commit-Queue: Adam Raine <asraine@chromium.org>

--- test/e2e/lighthouse/navigation_test.ts
+++ test/e2e/lighthouse/navigation_test.ts
@@ -94,7 +94,7 @@ describe('Navigation', function() {
     // 1 refresh after auditing to reset state
     assert.strictEqual(numNavigations, 5);
 
-    assert.strictEqual(lhr.lighthouseVersion, '12.5.0');
+    assert.strictEqual(lhr.lighthouseVersion, '12.5.1');
     assert.match(lhr.finalUrl, /^https:\/\/localhost:[0-9]+\/test\/e2e\/resources\/lighthouse\/hello.html/);
 
     assert.strictEqual(lhr.configSettings.throttlingMethod, 'simulate');

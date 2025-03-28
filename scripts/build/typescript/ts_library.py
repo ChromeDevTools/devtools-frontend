3dffede8b4a9cb16525e7e702df8f84f54567750
Eric Leese <leese@chromium.org>
2025-03-28 03:45:28 -0700

    Change error path rewriting to work in full chromium checkout
    Instead of calculating the path relative to the root of the
    DevTools source tree, we now just remove "../../" to get the
    relative path. This makes terminal links in vscode work in
    full Chromium checkouts as well as devtools only checkouts.
    
    Bug: None
    Change-Id: I7bf410c353c5929bc9a0ef064449fa88f3bc1264
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6405079
    Reviewed-by: Benedikt Meurer <bmeurer@chromium.org>
    Commit-Queue: Nikolay Vitkov <nvitkov@chromium.org>
    Reviewed-by: Nikolay Vitkov <nvitkov@chromium.org>

--- scripts/build/typescript/ts_library.py
+++ scripts/build/typescript/ts_library.py
@@ -170,18 +170,14 @@ def runEsbuild(opts, tsconfig_output_location, tsconfig_output_directory):
 
 
 def rewriteTypeScriptErrorPaths(stderr: str):
-    root_path = Path(ROOT_DIRECTORY_OF_REPOSITORY).resolve()
-
     def rewriteLine(match: re.Match[str]) -> str:
-        absolute_path = Path(match[1]).resolve()
-        relative_path = absolute_path.relative_to(root_path)
-        return f"{relative_path}({match[2]}): error {match[3]}: {match[4]}"
+        return f"{match[1]}({match[2]}): error {match[3]}: {match[4]}"
 
     lines = stderr.splitlines()
     for i, line in enumerate(lines):
         lines[i] = re.sub(
             # We use similar pattern in ".vscode/devtools-workspace-tasks.json"
-            r'^([^\s].*)\((\d+,\d+)\): error (TS\d+):\s*(.*)$',
+            r'^\.\./\.\./([^\s].*)\((\d+,\d+)\): error (TS\d+):\s*(.*)$',
             rewriteLine,
             line)
 

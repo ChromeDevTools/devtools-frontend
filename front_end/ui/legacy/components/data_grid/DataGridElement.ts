eed8b20236b80c08c54fc2246d0235565e24053d
Danil Somsikov <dsv@chromium.org>
2025-04-07 11:49:11 -0700

    [M136] Emit create event when user clicks outside of the editor
    Bug: 408244902
    Fixed: 409200069
    Change-Id: I796e53b62a01219c84742373bf6e00c027f32560
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6436657
    Reviewed-by: Philip Pfaffe <pfaffe@chromium.org>
    Auto-Submit: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Danil Somsikov <dsv@chromium.org>
    (cherry picked from commit e8b639c4812cbc6276f37fb393da8537422844f8)
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6456264

--- front_end/ui/legacy/components/data_grid/DataGridElement.ts
+++ front_end/ui/legacy/components/data_grid/DataGridElement.ts
@@ -341,12 +341,15 @@ class DataGridElement extends HTMLElement {
       moveDirection?: string): void {
     if (node.isCreationNode) {
       this.#usedCreationNode = node;
-      if (moveDirection === 'forward') {
-        const hasNextEditableColumn =
-            this.#columns.slice(this.#columns.findIndex(({id}) => id === columnId) + 1).some(({editable}) => editable);
-        if (!hasNextEditableColumn) {
-          node.deselect();
-        }
+      let hasNextEditableColumn = false;
+      if (moveDirection) {
+        const index = this.#columns.findIndex(({id}) => id === columnId);
+        const nextColumns =
+            moveDirection === 'forward' ? this.#columns.slice(index + 1) : this.#columns.slice(0, index);
+        hasNextEditableColumn = nextColumns.some(({editable}) => editable);
+      }
+      if (!hasNextEditableColumn) {
+        node.deselect();
       }
       return;
     }

17962fd43fc84e400d32621223b1930dfa07b038
Mathias Bynens <mathias@chromium.org>
2025-04-08 10:48:23 +0200

    Update set-data-type-reference extension
    Bug: chromium:397586315
    Change-Id: I8b754aa7ce08e4bbdd124fc882eb32fbbaf4f133
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6438915
    Commit-Queue: Mathias Bynens <mathias@chromium.org>
    Auto-Submit: Mathias Bynens <mathias@chromium.org>
    Reviewed-by: Nikolay Vitkov <nvitkov@chromium.org>
    Commit-Queue: Nikolay Vitkov <nvitkov@chromium.org>

--- scripts/eslint_rules/tests/set-data-type-reference.test.ts
+++ scripts/eslint_rules/tests/set-data-type-reference.test.ts
@@ -1,7 +1,7 @@
 // Copyright 2020 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-import rule from '../lib/set-data-type-reference.js';
+import rule from '../lib/set-data-type-reference.ts';
 
 import {RuleTester} from './utils/tsUtils.ts';
 

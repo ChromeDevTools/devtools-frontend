60b8f1e18b34ca09bebdab02f99d7a67a2194884
Alex Rudenko <alexrudenko@chromium.org>
2025-04-22 14:30:38 +0200

    Skip a flaky unit test
    Bug: 412603772
    Change-Id: I452ce24169f81986c9f143c75ab0b5827b40bc28
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6478176
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Wolfgang Beyer <wolfi@chromium.org>
    Auto-Submit: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Wolfgang Beyer <wolfi@chromium.org>

--- front_end/models/trace/insights/LegacyJavaScript.test.ts
+++ front_end/models/trace/insights/LegacyJavaScript.test.ts
@@ -22,7 +22,8 @@ describeWithEnvironment('LegacyJavaScript', function() {
     assert.deepEqual(insight.metricSavings, {FCP: 0, LCP: 0} as Trace.Insights.Types.MetricSavings);
   });
 
-  it('has results when savings are big enough', async function() {
+  // crbug.com/412603772 flaky.
+  it.skip('[crbug.com/412603772]: has results when savings are big enough', async function() {
     if (this.timeout() > 0) {
       this.timeout(20000);
     }

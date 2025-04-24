d9b5e7295b0ca5110b279c85fa49507b8d0631a1
Benedikt Meurer <bmeurer@chromium.org>
2025-04-24 12:41:23 +0200

    [sources] Sort automatic file systems before regular file systems.
    Screenshot: http://screen/6a7GPGzpK2UgoSq.png
    Fixed: 413227120
    Change-Id: Ibd14b3e5e5c324ce375334043c28bfde691b770d
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6486629
    Commit-Queue: Benedikt Meurer <bmeurer@chromium.org>
    Auto-Submit: Benedikt Meurer <bmeurer@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>

--- front_end/panels/sources/NavigatorView.ts
+++ front_end/panels/sources/NavigatorView.ts
@@ -190,7 +190,7 @@ const TYPE_ORDERS = new Map([
   [Types.File, 10],
   [Types.Frame, 70],
   [Types.Worker, 90],
-  [Types.AutomaticFileSystem, 100],
+  [Types.AutomaticFileSystem, 99],
   [Types.FileSystem, 100],
 ]);
 

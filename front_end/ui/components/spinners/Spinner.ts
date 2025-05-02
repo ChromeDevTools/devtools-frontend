287aee78ca203d31d22b8f62c97688db3eecc1d5
Benedikt Meurer <bmeurer@chromium.org>
2025-05-02 13:24:07 +0200

    [unified-css] Migrate the `Spinner` component.
    Also simplify the DOM / CSS a bit in general.
    
    Fixed: 404268176
    Change-Id: Ia2cd9b0d2c96e895ad3c93f29fba65b99ac3208f
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6509106
    Commit-Queue: Benedikt Meurer <bmeurer@chromium.org>
    Reviewed-by: Samiya Caur <samiyac@chromium.org>
    Auto-Submit: Benedikt Meurer <bmeurer@chromium.org>
    Commit-Queue: Samiya Caur <samiyac@chromium.org>

--- front_end/ui/components/spinners/Spinner.ts
+++ front_end/ui/components/spinners/Spinner.ts
@@ -5,46 +5,34 @@
 
 import {html, render} from '../../lit/lit.js';
 
-import spinnerStylesRaw from './spinner.css.js';
-
-/* eslint-disable rulesdir/no-adopted-style-sheets --
- * TODO(crbug.com/391381439): Fully migrate off of Constructable Stylesheets.
- **/
-const spinnerStyles = new CSSStyleSheet();
-spinnerStyles.replaceSync(spinnerStylesRaw.cssText);
+import spinnerStyles from './spinner.css.js';
 
 export class Spinner extends HTMLElement {
   readonly #shadow = this.attachShadow({mode: 'open'});
 
   connectedCallback(): void {
-    this.#shadow.adoptedStyleSheets = [spinnerStyles];
-    this.#render();
-  }
-
-  #render(): void {
     // The radius of the circles are set to 2.75rem as per implementation
     // of indeterminate progress indicator in
     // https://github.com/material-components/material-components-web/tree/master/packages/mdc-circular-progress.
     // Changing the value of the radius will cause errors in animation.
     // clang-format off
     render(html`
+      <style>${spinnerStyles.cssText}</style>
       <div class="indeterminate-spinner">
-        <div class="left-circle-graphic-container">
-          <svg class="left-circle-graphic" viewBox="0 0 100 100">
+        <div class="left-circle">
+          <svg viewBox="0 0 100 100">
             <circle cx="50%" cy="50%" r="2.75rem"></circle></svg>
         </div>
-        <div class="center-circle-graphic-container">
-          <svg class="center-circle-graphic" viewBox="0 0 100 100">
+        <div class="center-circle">
+          <svg viewBox="0 0 100 100">
             <circle cx="50%" cy="50%" r="2.75rem"></circle></svg>
         </div>
-        <div class="right-circle-graphic-container">
-          <svg class="right-circle-graphic" viewBox="0 0 100 100">
+        <div class="right-circle">
+          <svg viewBox="0 0 100 100">
             <circle cx="50%" cy="50%" r="2.75rem"></circle></svg>
         </div>
       </div>
-    `, this.#shadow, {
-      host: this,
-    });
+    `, this.#shadow, {host: this});
     // clang-format on
   }
 }

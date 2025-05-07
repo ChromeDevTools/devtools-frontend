0baa9d17065fb42f851d1a39204171adf45d4b8c
Connor Clark <cjamcl@chromium.org>
2025-05-07 13:17:24 -0700

    Don't render insight component if the model error'd
    Fixed: 416303415
    Change-Id: I98664ff39f9c64620840189e269c941e73921695
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6518812
    Auto-Submit: Connor Clark <cjamcl@chromium.org>
    Commit-Queue: Paul Irish <paulirish@chromium.org>
    Reviewed-by: Paul Irish <paulirish@chromium.org>

--- front_end/panels/timeline/components/SidebarSingleInsightSet.ts
+++ front_end/panels/timeline/components/SidebarSingleInsightSet.ts
@@ -400,6 +400,10 @@ export class SidebarSingleInsightSet extends HTMLElement {
         continue;
       }
 
+      if (model instanceof Error) {
+        continue;
+      }
+
       const fieldMetrics = this.#getFieldMetrics(insightSetKey);
 
       // clang-format off

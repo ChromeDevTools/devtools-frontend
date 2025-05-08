088802e3f5c696db19e03bcbbe413ac0f3aed375
Kateryna Prokopenko <kprokopenko@chromium.org>
2025-05-08 11:07:23 +0000

    [DevToolsUIKit] Polish Icon documentation
    Screenshot: https://imgur.com/a/thhbp9F
    
    Bug: 414330824
    Change-Id: I8303e0f64d217109e9973635a10973d6331a46ff
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6513565
    Reviewed-by: Kim-Anh Tran <kimanh@chromium.org>
    Auto-Submit: Kateryna Prokopenko <kprokopenko@chromium.org>
    Commit-Queue: Kateryna Prokopenko <kprokopenko@chromium.org>

--- front_end/ui/components/docs/icon_component/basic.ts
+++ front_end/ui/components/docs/icon_component/basic.ts
@@ -2,17 +2,20 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import * as Lit from '../../../lit/lit.js';
 import * as ComponentHelpers from '../../helpers/helpers.js';
 import * as IconButton from '../../icon_button/icon_button.js';
+
+const {html} = Lit;
 
 await ComponentHelpers.ComponentServerSetup.setup();
 
 const iconTable = document.getElementById('icon-overview');
 
 const row1 = document.createElement('tr');
-const iconName1 = document.createElement('td');
-iconName1.textContent = 'select-element';
-row1.appendChild(iconName1);
+const iconDescription1 = document.createElement('td');
+iconDescription1.textContent = 'Programmatically created with default size and color';
+row1.appendChild(iconDescription1);
 
 const icon = IconButton.Icon.create('select-element');
 const icon1 = document.createElement('td');
@@ -21,18 +24,13 @@ row1.appendChild(icon1);
 
 iconTable?.appendChild(row1);
 
-icon.onclick = () => {
-  // Change of colour through a data-setter, which rerenders the component. Getting the data first in order not to specify the data fields all over again
-  icon.data = {...icon.data, color: 'blue'};
-};
-
 const row2 = document.createElement('tr');
-const iconName2 = document.createElement('td');
-iconName2.textContent = 'issue-exclamation-filled';
-row2.appendChild(iconName2);
+const iconDescription2 = document.createElement('td');
+iconDescription2.textContent = 'Programmatically created with custom size and color';
+row2.appendChild(iconDescription2);
 
 const otherIcon = IconButton.Icon.create('issue-exclamation-filled');
-otherIcon.style.color = 'var(--icon-link)';
+otherIcon.classList.toggle('custom-size-and-color');
 const icon2 = document.createElement('td');
 icon2.appendChild(otherIcon);
 row2.appendChild(icon2);
@@ -40,50 +38,31 @@ row2.appendChild(icon2);
 iconTable?.appendChild(row2);
 
 const row3 = document.createElement('tr');
-const iconName3 = document.createElement('td');
-iconName3.textContent = 'select-element';
-row3.appendChild(iconName3);
+const iconDescription3 = document.createElement('td');
+iconDescription3.textContent = 'Created through html template with default size and color';
+row3.appendChild(iconDescription3);
 
-const otherIcon2 = IconButton.Icon.create('select-element', 'custom-color');
 const icon3 = document.createElement('td');
-icon3.appendChild(otherIcon2);
+Lit.render(
+    html`
+        <devtools-icon name="select-element"></devtools-icon>
+      `,
+    icon3);
 row3.appendChild(icon3);
 
 iconTable?.appendChild(row3);
 
-(() => {
-  const div = document.createElement('div');
-  const span1 = document.createElement('span');
-  span1.textContent = 'Some text';
-  div.appendChild(span1);
-  const otherIcon3 = IconButton.Icon.create('select-element', 'custom-color');
-  div.appendChild(otherIcon3);
-  const span2 = document.createElement('span');
-  span2.textContent = 'with a large icon';
-  div.appendChild(span2);
-  document.body.append(div);
-})();
+const row4 = document.createElement('tr');
+const iconDescription4 = document.createElement('td');
+iconDescription4.textContent = 'Created through html template with custom size and color';
+row4.appendChild(iconDescription4);
 
-(() => {
-  const div = document.createElement('div');
-  const span1 = document.createElement('span');
-  span1.textContent = 'Some text';
-  div.appendChild(span1);
-  const otherIcon3 = IconButton.Icon.create('select-element', 'custom-color');
-  otherIcon3.style.width = '14px';
-  otherIcon3.style.height = '14px';
-  div.appendChild(otherIcon3);
-  const span2 = document.createElement('span');
-  span2.textContent = 'with a small icon';
-  div.appendChild(span2);
+const icon4 = document.createElement('td');
+Lit.render(
+    html`
+        <devtools-icon name="select-element" class="custom-size-and-color"></devtools-icon>
+      `,
+    icon4);
+row4.appendChild(icon4);
 
-  document.body.append(div);
-})();
-
-const iconInFlex = document.getElementById('icon-in-flex') as IconButton.Icon.Icon;
-iconInFlex.data = {
-  iconName: 'cross-circle',
-  width: '20px',
-  height: '20px',
-  color: 'var(--icon-error)',
-};
+iconTable?.appendChild(row4);

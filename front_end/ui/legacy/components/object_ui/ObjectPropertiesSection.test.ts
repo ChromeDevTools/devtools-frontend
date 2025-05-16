f14eb12f8b2482c558985f96e469b0eaeb2e9e10
Philip Pfaffe <pfaffe@chromium.org>
2025-05-16 11:47:53 +0000

    Remove ObjectPropertiesSection test
    An inspector test was added in crrev.com/c/6555882
    
    Bug: 357805727
    Change-Id: I5b60111d133d0fcddc5a02538a8db457618c863b
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6555868
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>

--- front_end/ui/legacy/components/object_ui/ObjectPropertiesSection.test.ts
+++ front_end/ui/legacy/components/object_ui/ObjectPropertiesSection.test.ts
@@ -6,72 +6,10 @@ import * as Common from '../../../../core/common/common.js';
 import * as SDK from '../../../../core/sdk/sdk.js';
 import {dispatchClickEvent} from '../../../../testing/DOMHelpers.js';
 import {describeWithEnvironment} from '../../../../testing/EnvironmentHelpers.js';
-import {someMutations} from '../../../../testing/MutationHelpers.js';
-import {
-  describeWithRealConnection,
-  getExecutionContext,
-} from '../../../../testing/RealConnection.js';
-import * as UI from '../../legacy.js';
 
 import * as ObjectUI from './object_ui.js';
 
 describe('ObjectPropertiesSection', () => {
-  describeWithRealConnection('ObjectPropertiesSection', () => {
-    async function evaluateAndGetProperties(code: string, accessorPropertiesOnly = false, generatePreview = false):
-        Promise<{object: SDK.RemoteObject.RemoteObject, properties: SDK.RemoteObject.RemoteObjectProperty[]}> {
-      const targetManager = SDK.TargetManager.TargetManager.instance();
-      const target = targetManager.rootTarget();
-      assert.exists(target);
-      const runtimeModel = target.model(SDK.RuntimeModel.RuntimeModel);
-      assert.exists(runtimeModel);
-      const executionContext = await getExecutionContext(runtimeModel);
-      UI.Context.Context.instance().setFlavor(SDK.RuntimeModel.ExecutionContext, executionContext);
-      const {result} = await ObjectUI.JavaScriptREPL.JavaScriptREPL.evaluateAndBuildPreview(
-          code, false /* throwOnSideEffect */, true /* replMode */, 500 /* timeout */);
-      if (!(result && 'object' in result && result.object)) {
-        throw new Error('Cannot evaluate test object');
-      }
-      const {object} = result;
-      const {properties} = await object.getAllProperties(accessorPropertiesOnly, generatePreview);
-      assert.exists(properties);
-      return {object, properties};
-    }
-
-    async function setupTreeOutline(code: string, accessorPropertiesOnly: boolean, generatePreview: boolean) {
-      const {object, properties} = await evaluateAndGetProperties(code, accessorPropertiesOnly, generatePreview);
-      const treeOutline = new ObjectUI.ObjectPropertiesSection.ObjectPropertiesSectionsTreeOutline();
-      ObjectUI.ObjectPropertiesSection.ObjectPropertyTreeElement.populateWithProperties(
-          treeOutline.rootElement(), properties, null, true /* skipProto */, false /* skipGettersAndSetters */, object);
-      return treeOutline;
-    }
-
-    it('can reveal private accessor values', async () => {
-      const VALUE = '42';
-      const treeOutline = await setupTreeOutline(
-          `(() => {
-           class A {
-             get #bar() { return ${VALUE}; }
-           };
-           return new A();
-         })()`,
-          true, false);
-
-      const propertiesSection =
-          treeOutline.rootElement().firstChild() as ObjectUI.ObjectPropertiesSection.ObjectPropertyTreeElement;
-
-      propertiesSection.update();
-
-      const calculateValueButton =
-          propertiesSection.valueElement.querySelector('.object-value-calculate-value-button') as HTMLElement;
-      assert.exists(calculateValueButton);
-      const mutations = someMutations(propertiesSection.listItemElement);
-      calculateValueButton.click();
-      await mutations;
-
-      assert.strictEqual(VALUE, propertiesSection.valueElement.innerHTML);
-    });
-  });
-
   describeWithEnvironment('ObjectPropertiesSection', () => {
     describe('appendMemoryIcon', () => {
       it('appends a memory icon for inspectable object types', () => {

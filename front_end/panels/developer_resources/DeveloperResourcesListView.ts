40779fc5b82be87a1ab1e9357f4d5144086a62b7
Danil Somsikov <dsv@chromium.org>
2025-05-21 09:09:41 -0700

    Address remaining UI eng vision violations in the DeveloperResourcesListView
    Bug: 407751014
    Change-Id: If37af9ad065953d4f4f96ef7e0b5510ee85eb3cf
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6574478
    Reviewed-by: Benedikt Meurer <bmeurer@chromium.org>
    Commit-Queue: Danil Somsikov <dsv@chromium.org>

--- front_end/panels/developer_resources/DeveloperResourcesListView.ts
+++ front_end/panels/developer_resources/DeveloperResourcesListView.ts
@@ -1,8 +1,6 @@
 // Copyright (c) 2020 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-/* eslint-disable rulesdir/no-imperative-dom-api */
-/* eslint-disable rulesdir/no-lit-render-outside-of-view */
 
 import '../../ui/legacy/components/data_grid/data_grid.js';
 
@@ -13,9 +11,11 @@ import * as SDK from '../../core/sdk/sdk.js';
 import type * as Protocol from '../../generated/protocol.js';
 import * as TextUtils from '../../models/text_utils/text_utils.js';
 import * as UI from '../../ui/legacy/legacy.js';
-import {html, nothing, render} from '../../ui/lit/lit.js';
+import {Directives, html, nothing, render} from '../../ui/lit/lit.js';
 
 import developerResourcesListViewStyles from './developerResourcesListView.css.js';
+
+const {ref} = Directives;
 
 const UIStrings = {
   /**
@@ -86,7 +86,8 @@ const {withThousandsSeparator} = Platform.NumberUtilities;
 
 export interface ViewInput {
   items: SDK.PageResourceLoader.PageResource[];
-  highlight: (element: Element, textContent: string, columnId: string) => void;
+  selectedItem: SDK.PageResourceLoader.PageResource|null;
+  highlight: (element: Element|undefined, textContent: string|undefined, columnId: string) => void;
   filters: TextUtils.TextUtils.ParsedFilter[];
   onContextMenu: (e: CustomEvent<{menu: UI.ContextMenu.ContextMenu, element: HTMLElement}>) => void;
   onSelect: (e: CustomEvent<HTMLElement>) => void;
@@ -96,98 +97,84 @@ export interface ViewInput {
 
 export type View = (input: ViewInput, output: object, target: HTMLElement) => void;
 
+const DEFAULT_VIEW: View = (input, _output, target) => {
+  // clang-format off
+  render(html`
+      <style>${developerResourcesListViewStyles}</style>
+      <devtools-data-grid name=${i18nString(UIStrings.developerResources)} striped class="flex-auto"
+         .filters=${input.filters} @contextmenu=${input.onContextMenu} @selected=${input.onSelect}>
+        <table>
+          <tr>
+            <th id="status" sortable fixed width="60px">
+              ${i18nString(UIStrings.status)}
+            </th>
+            <th id="url" sortable width="250px">
+              ${i18nString(UIStrings.url)}
+            </th>
+            <th id="initiator" sortable width="80px">
+              ${i18nString(UIStrings.initiator)}
+            </th>
+            <th id="size" sortable fixed width="80px" align="right">
+              ${i18nString(UIStrings.totalBytes)}
+            </th>
+            <th id="duration" sortable fixed width="80px" align="right">
+              ${i18nString(UIStrings.duration)}
+            </th>
+            <th id="error-message" sortable width="200px">
+              ${i18nString(UIStrings.error)}
+            </th>
+          </tr>
+          ${input.items.map((item, index) => {
+            const splitURL = /^(.*)(\/[^/]*)$/.exec(item.url);
+            return html`
+            <tr selected=${(item === input.selectedItem) || nothing}
+                data-url=${item.url ?? nothing}
+                data-initiator-url=${item.initiator.initiatorUrl ?? nothing}
+                data-index=${index}>
+              <td>${item.success === true  ? i18nString(UIStrings.success) :
+                    item.success === false ? i18nString(UIStrings.failure) :
+                                             i18nString(UIStrings.pending)}</td>
+              <td title=${item.url} aria-label=${item.url}>
+                <div aria-hidden="true" part="url-outer"
+                     ${ref(e => input.highlight(e, item.url, 'url'))}>
+                  <div part="url-prefix">${splitURL ? splitURL[1] : item.url}</div>
+                  <div part="url-suffix">${splitURL ? splitURL[2] : ''}</div>
+                </div>
+              </td>
+              <td title=${item.initiator.initiatorUrl || ''}
+                  aria-label=${item.initiator.initiatorUrl || ''}
+                  @mouseenter=${() => input.onInitiatorMouseEnter(item.initiator.frameId)}
+                  @mouseleave=${input.onInitiatorMouseLeave}
+              >${item.initiator.initiatorUrl || ''}</td>
+              <td aria-label=${item.size !== null ? i18nString(UIStrings.sBytes, {n: item.size}) : nothing}
+                  data-value=${item.size ?? nothing}>${
+                item.size !== null ?  html`<span>${withThousandsSeparator(item.size)}</span>` : ''}</td>
+              <td aria-label=${item.duration !== null ? i18n.TimeUtilities.millisToString(item.duration) : nothing}
+                  data-value=${item.duration ?? nothing}>${
+                  item.duration !== null ? html`<span>${i18n.TimeUtilities.millisToString(item.duration)}</span>` : ''}</td>
+              <td class="error-message">
+                ${item.errorMessage ? html`
+                <span ${ref(e => input.highlight(e, item.errorMessage, 'error-message'))}>
+                  ${item.errorMessage}
+                </span>` : nothing}
+              </td>
+            </tr>`;
+          })}
+          </table>
+        </devtools-data-grid>`,
+        target, {host: input});
+  // clang-format on
+};
+
 export class DeveloperResourcesListView extends UI.Widget.VBox {
   #items: SDK.PageResourceLoader.PageResource[] = [];
   #selectedItem: SDK.PageResourceLoader.PageResource|null = null;
   #onSelect: ((item: SDK.PageResourceLoader.PageResource|null) => void)|null = null;
   readonly #view: View;
   #filters: TextUtils.TextUtils.ParsedFilter[] = [];
-  constructor(element: HTMLElement, view: View = (input, _, target) => {
-    // clang-format off
-        render(html`
-            <devtools-data-grid
-              name=${i18nString(UIStrings.developerResources)}
-              striped
-              .filters=${input.filters}
-               @contextmenu=${input.onContextMenu}
-               @selected=${input.onSelect}
-              class="flex-auto"
-            >
-              <table>
-                <tr>
-                  <th id="status" sortable fixed width="60px">
-                    ${i18nString(UIStrings.status)}
-                  </th>
-                  <th id="url" sortable width="250px">
-                    ${i18nString(UIStrings.url)}
-                  </th>
-                  <th id="initiator" sortable width="80px">
-                    ${i18nString(UIStrings.initiator)}
-                  </th>
-                  <th id="size" sortable fixed width="80px" align="right">
-                    ${i18nString(UIStrings.totalBytes)}
-                  </th>
-                  <th id="duration" sortable fixed width="80px" align="right">
-                    ${i18nString(UIStrings.duration)}
-                  </th>
-                  <th id="error-message" sortable width="200px">
-                    ${i18nString(UIStrings.error)}
-                  </th>
-                </tr>
-                ${input.items.map((item, index) => html`
-                  <tr selected=${(item === this.#selectedItem) || nothing}
-                      data-url=${item.url ?? nothing}
-                      data-initiator-url=${item.initiator.initiatorUrl ?? nothing}
-                      data-index=${index}>
-                    <td>${item.success === true  ? i18nString(UIStrings.success) :
-                          item.success === false ? i18nString(UIStrings.failure) :
-                                                   i18nString(UIStrings.pending)}</td>
-                    <td title=${item.url} aria-label=${item.url}>${(() => {
-                        const url = renderUrl(item.url);
-                        input.highlight(url, item.url, 'url');
-                        return url;
-                      })()}</td>
-                    <td title=${item.initiator.initiatorUrl || ''}
-                        aria-label=${item.initiator.initiatorUrl || ''}
-                        @mouseenter=${() => input.onInitiatorMouseEnter(item.initiator.frameId)}
-                        @mouseleave=${input.onInitiatorMouseLeave}
-                    >${item.initiator.initiatorUrl || ''}</td>
-                    <td aria-label=${item.size !== null ? i18nString(UIStrings.sBytes, {n: item.size}) : nothing}
-                        data-value=${item.size ?? nothing}>${
-                      item.size !== null ?  html`<span>${withThousandsSeparator(item.size)}</span>` : ''}</td>
-                    <td aria-label=${item.duration !== null ? i18n.TimeUtilities.millisToString(item.duration) : nothing}
-                        data-value=${item.duration ?? nothing}>${
-                        item.duration !== null ? html`<span>${i18n.TimeUtilities.millisToString(item.duration)}</span>` : ''}</td>
-                    <td class="error-message">${(() => {
-                        const cell = document.createElement('span');
-                        if (item.errorMessage) {
-                          cell.textContent = item.errorMessage;
-                          input.highlight(cell, item.errorMessage, 'error-message');
-                        }
-                        return cell;
-                      })()}</td>
-                  </tr>`)}
-              </table>
-            </devtools-data-grid>`,
-            target, {host: input});
-    // clang-format on
-    function renderUrl(url: string): HTMLElement {
-      const outer = document.createElement('div');
-      UI.ARIAUtils.setHidden(outer, true);
-      outer.setAttribute('part', 'url-outer');
-      const domain = outer.createChild('div');
-      domain.setAttribute('part', 'url-prefix');
-      const path = outer.createChild('div');
-      path.setAttribute('part', 'url-suffix');
-      const splitURL = /^(.*)(\/[^/]*)$/.exec(url);
-      domain.textContent = splitURL ? splitURL[1] : url;
-      path.textContent = splitURL ? splitURL[2] : '';
-      return outer;
-    }
-  }) {
+  constructor(element: HTMLElement, view = DEFAULT_VIEW) {
     super(true, undefined, element);
     this.#view = view;
-    this.registerRequiredCSS(developerResourcesListViewStyles);
   }
 
   set selectedItem(item: SDK.PageResourceLoader.PageResource|null) {
@@ -244,6 +231,7 @@ export class DeveloperResourcesListView extends UI.Widget.VBox {
   override performUpdate(): void {
     const input = {
       items: this.#items,
+      selectedItem: this.#selectedItem,
       filters: this.#filters,
       highlight: this.#highlight.bind(this),
       onContextMenu: (e: CustomEvent<{menu: UI.ContextMenu.ContextMenu, element: HTMLElement}>) => {
@@ -269,12 +257,20 @@ export class DeveloperResourcesListView extends UI.Widget.VBox {
     this.#view(input, output, this.contentElement);
   }
 
-  #highlight(element: Element, textContent: string, columnId: string): void {
-    const filter = this.#filters.find(filter => filter.key?.split(',')?.includes(columnId));
-    if (!filter?.regex || element.querySelector('.filter-highlight')) {
+  #highlight(element: Element|undefined, textContent: string|undefined, columnId: string): void {
+    if (!element || !textContent) {
       return;
     }
-    const matches = filter.regex.exec(textContent);
+    const highlightContainers =
+        new Set<Element>([...element.querySelectorAll('.filter-highlight')].map(e => e.parentElement as Element));
+    for (const container of highlightContainers) {
+      container.textContent = container.textContent;
+    }
+    const filter = this.#filters.find(filter => filter.key?.split(',')?.includes(columnId));
+    if (!filter?.regex) {
+      return;
+    }
+    const matches = filter.regex.exec(element.textContent ?? '');
     if (!matches?.length) {
       return;
     }

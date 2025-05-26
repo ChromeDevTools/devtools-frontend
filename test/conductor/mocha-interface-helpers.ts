9a055caec3d9c6b138c8d20285af95e23482fce6
Alex Rudenko <alexrudenko@chromium.org>
2025-05-26 13:16:18 +0200

    [e2e] fix timeouts for retries
    crrev.com/c/6081455 attempted to fix the issue with
    mocha not setting timeouts on retries. Unfortunately,
    the solution has a side-effect resulting in multiple done mocha
    error for the last test. This CL works around the issue
    by caching the actual timeout per test function.
    
    Bug: none
    Change-Id: I629ff0f71b8eb66ee876ba0f8cd69ce4ced49525
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6586927
    Auto-Submit: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>

--- test/conductor/mocha-interface-helpers.ts
+++ test/conductor/mocha-interface-helpers.ts
@@ -88,6 +88,12 @@ async function createScreenshotError(error: Error): Promise<Error> {
   return error;
 }
 
+/**
+ * We track the initial timeouts for each functions because mocha
+ * does not reset test timeout for retries.
+ */
+const timeoutByTestFunction = new WeakMap<Mocha.AsyncFunc, number>();
+
 export function makeInstrumentedTestFunction(fn: Mocha.AsyncFunc, label: string) {
   return async function testFunction(this: Mocha.Context) {
     const abortController = new AbortController();
@@ -96,8 +102,9 @@ export function makeInstrumentedTestFunction(fn: Mocha.AsyncFunc, label: string)
     AsyncScope.abortSignal = abortController.signal;
     // Promisify the function in case it is sync.
     const promise = (async () => await fn.call(this))();
-    const actualTimeout = this.timeout();
-    // Disable test timeout.
+    const actualTimeout = timeoutByTestFunction.get(fn) ?? this.timeout();
+    timeoutByTestFunction.set(fn, actualTimeout);
+    // Disable mocha test timeout.
     this.timeout(0);
     const t = actualTimeout !== 0 ? setTimeout(async () => {
       abortController.abort();
@@ -133,7 +140,6 @@ export function makeInstrumentedTestFunction(fn: Mocha.AsyncFunc, label: string)
             })
         .finally(() => {
           clearTimeout(t);
-          this.timeout(actualTimeout);
         });
     return await testPromise;
   };

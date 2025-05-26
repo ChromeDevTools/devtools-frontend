2577032f6e5a0e348c23c6ecdabb54e0d233041b
Yang Guo <yangguo@chromium.org>
2025-05-26 15:16:42 +0200

    Migrate layers-test to e2e_non_hosted
    Also unskip the two tests now that they are more robust
    
    Fixed: 416404922, 327062511, 40799085
    Change-Id: I597f7badf49b2f0d62fbe7c460ccf4a10e9e5db0
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6586793
    Commit-Queue: Yang Guo <yangguo@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Auto-Submit: Yang Guo <yangguo@chromium.org>

--- test/e2e/helpers/layers-helpers.ts
+++ test/e2e/helpers/layers-helpers.ts
@@ -2,17 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {waitFor} from '../../shared/helper.js';
-
 import {veImpression} from './visual-logging-helpers.js';
 
 export const LAYERS_TAB_SELECTOR = '#tab-layers';
-
-export async function getCurrentUrl() {
-  await waitFor('[aria-label="layers"]');
-  const element = await waitFor('[aria-label="layers"]');
-  return await element.evaluate(e => e.getAttribute('test-current-url'));
-}
 
 export function veImpressionForLayersPanel() {
   return veImpression('Panel', 'layers', [
---
+++ test/e2e_non_hosted/layers/layers_test.ts
@@ -0,0 +1,66 @@
+// Copyright 2020 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {assert} from 'chai';
+
+import {openPanelViaMoreTools} from '../../e2e/helpers/settings-helpers.js';
+import type {DevToolsPage} from '../../e2e_non_hosted/shared/frontend-helper.js';
+
+describe('The Layers Panel', function() {
+  async function getCurrentUrl(devToolsPage: DevToolsPage) {
+    await devToolsPage.waitFor('[aria-label="layers"]');
+    const element = await devToolsPage.waitFor('[aria-label="layers"]');
+    return await element.evaluate(e => e.getAttribute('test-current-url'));
+  }
+
+  it('should keep the currently inspected url as an attribute', async ({devToolsPage, inspectedPage}) => {
+    const targetUrl = 'layers/default.html';
+    await inspectedPage.bringToFront();
+    await inspectedPage.goToResource(targetUrl);
+
+    await devToolsPage.bringToFront();
+    await openPanelViaMoreTools('Layers', devToolsPage);
+    await inspectedPage.bringToFront();
+    await inspectedPage.raf();
+    await devToolsPage.bringToFront();
+
+    await devToolsPage.waitFor('[aria-label="layers"]:not([test-current-url=""])');
+
+    await devToolsPage.waitForFunction(async () => {
+      return await getCurrentUrl(devToolsPage) === `${inspectedPage.getResourcesPath()}/${targetUrl}`;
+    });
+  });
+
+  it('should update the layers view when going offline', async ({devToolsPage, inspectedPage}) => {
+    await openPanelViaMoreTools('Layers', devToolsPage);
+
+    const targetUrl = 'layers/default.html';
+    await inspectedPage.goToResource(targetUrl, {waitUntil: 'networkidle0'});
+    await devToolsPage.bringToFront();
+    await devToolsPage.raf();
+    await inspectedPage.bringToFront();
+    await devToolsPage.waitFor('[aria-label="layers"]:not([test-current-url=""])');
+    assert.strictEqual(await getCurrentUrl(devToolsPage), `${inspectedPage.getResourcesPath()}/${targetUrl}`);
+
+    const session = await inspectedPage.page.createCDPSession();
+    try {
+      await session.send('Network.emulateNetworkConditions', {
+        offline: true,
+        latency: 0,
+        downloadThroughput: 0,
+        uploadThroughput: 0,
+      });
+      await inspectedPage.page.reload({waitUntil: 'networkidle0'});
+      await devToolsPage.bringToFront();
+      await devToolsPage.raf();
+      await inspectedPage.bringToFront();
+      await devToolsPage.waitFor(`[aria-label="layers"]:not([test-current-url="${targetUrl}"])`);
+      await devToolsPage.waitForFunction(async () => {
+        return (await getCurrentUrl(devToolsPage)) === 'chrome-error://chromewebdata/';
+      });
+    } finally {
+      await session.detach();
+    }
+  });
+});

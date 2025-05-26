c87c0e12291acda960528fd0f9c09b34ac4645d2
Samiya Caur <samiyac@chromium.org>
2025-05-26 19:05:41 +0000

    Migrate timeline-js-line-level-profile-no-url-end-to-end_test to e2e_non_hosted
    Fixed: 415306436, 416404714
    Change-Id: Idf698323633514740d4a7046b7924a31123a90f9
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6587809
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Samiya Caur <samiyac@chromium.org>

---
+++ test/e2e_non_hosted/performance/timeline-js-line-level-profile-no-url-end-to-end_test.ts
@@ -0,0 +1,36 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {navigateToPerformanceTab, startRecording, stopRecording} from '../../e2e/helpers/performance-helpers.js';
+import {openSourcesPanel} from '../../e2e/helpers/sources-helpers.js';
+
+describe('The Performance panel', () => {
+  setup({dockingMode: 'undocked'});
+
+  it('can collect a line-level CPU profile and show it in the text editor', async ({devToolsPage, inspectedPage}) => {
+    await navigateToPerformanceTab(undefined, devToolsPage, inspectedPage);
+    await inspectedPage.goToResource('../resources/ai_assistance/index.html');
+    await startRecording(devToolsPage);
+    await inspectedPage.evaluate(() => {
+      (function() {
+        const endTime = Date.now() + 100;
+        let s = 0;
+        while (Date.now() < endTime) {
+          s += Math.cos(s);
+        }
+        return s;
+      })();
+    });
+    await stopRecording(devToolsPage);
+    await openSourcesPanel(devToolsPage);
+
+    const elements = await devToolsPage.waitForMany('.navigator-file-tree-item', 2);
+    // The first one will be `data:text/html,<!DOCTYPE html>`
+    // And the second file is the code we evaluated before (Line 16), and it's the one we want to open.
+    const button = await elements[1].$('.tree-element-title');
+    // Add some offsets so we won't click on the edge of the element
+    await button?.click({offset: {x: 3, y: 3}});
+    await devToolsPage.waitForMany('.cm-performanceGutter .cm-gutterElement', 3);
+  });
+});

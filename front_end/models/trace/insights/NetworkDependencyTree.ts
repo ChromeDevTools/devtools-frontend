3d8f4ca2564df69de8ca5aaf9bf8188145c5351a
Connor Clark <cjamcl@chromium.org>
2025-05-29 17:16:16 -0700

    Clarify wording of preconnect advice estSavingTableDescription
    We only show a warning after 5 or more preconnects. This string was off
    by one.
    
    Bug: 417192818
    Change-Id: I502d4b45af2021b31adcd5ee03577e4e797c0f2e
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6604382
    Commit-Queue: Paul Irish <paulirish@chromium.org>
    Commit-Queue: Connor Clark <cjamcl@chromium.org>
    Auto-Submit: Connor Clark <cjamcl@chromium.org>
    Reviewed-by: Paul Irish <paulirish@chromium.org>

--- front_end/models/trace/insights/NetworkDependencyTree.ts
+++ front_end/models/trace/insights/NetworkDependencyTree.ts
@@ -91,7 +91,7 @@ export const UIStrings = {
    * @description Description of the table that recommends preconnecting to the origins to save time.
    */
   estSavingTableDescription:
-      'Add [preconnect](https://developer.chrome.com/docs/lighthouse/performance/uses-rel-preconnect/) hints to your most important origins, but try to use fewer than 4.',
+      'Add [preconnect](https://developer.chrome.com/docs/lighthouse/performance/uses-rel-preconnect/) hints to your most important origins, but try to use no more than 4.',
   /**
    * @description Label for a column in a data table; entries will be the origin of a web resource
    */

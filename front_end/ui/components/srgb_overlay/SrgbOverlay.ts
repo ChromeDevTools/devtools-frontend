7e2ebc1a802d5e1c2e03df3a27dc9614335b5c54
Ergun Erdogmus <ergunsh@chromium.org>
2025-06-17 14:54:20 +0200

    [SrgbOverlay] Remove `TODO` regarding using a `Color` object
    See crbug.com/40254113#comment3
    
    Bug: 1409892
    Change-Id: I23b5a4156e2a95561783435561ede3b29df1178e
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6651473
    Reviewed-by: Philip Pfaffe <pfaffe@chromium.org>
    Commit-Queue: Philip Pfaffe <pfaffe@chromium.org>
    Auto-Submit: Ergün Erdoğmuş <ergunsh@chromium.org>
    Commit-Queue: Ergün Erdoğmuş <ergunsh@chromium.org>

--- front_end/ui/components/srgb_overlay/SrgbOverlay.ts
+++ front_end/ui/components/srgb_overlay/SrgbOverlay.ts
@@ -21,7 +21,6 @@ const SRGB_LABEL_BOTTOM = 3;
 const SRGB_TEXT_UPPER_POINT_FROM_BOTTOM = SRGB_LABEL_HEIGHT + SRGB_LABEL_BOTTOM;
 
 const EPSILON = 0.001;
-// TODO(crbug.com/1409892): Use `Color` class here for a better code (and not duplicate isInGamut logic here)
 function isColorInSrgbGamut(hsv: Common.ColorUtils.Color3D): boolean {
   const rgba = Common.Color.hsva2rgba([...hsv, 1]);
   const xyzd50 = Common.ColorConverter.ColorConverter.displayP3ToXyzd50(rgba[0], rgba[1], rgba[2]);

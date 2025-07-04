d27e5781116e3cf0a1cae0c15197d89b56e40ff1
Alex Rudenko <alexrudenko@chromium.org>
2025-07-04 09:20:41 +0200

    Migrate test/e2e/console/console-truncate-long-messages_test.ts
    R=alinavarkki@chromium.org
    
    Fixed: 416405436
    Change-Id: Ie1f46f7856f3cdb7fc2a84ace83b8a37f3d10ec1
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6702305
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Alina Varkki <alinavarkki@chromium.org>
    Auto-Submit: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Alina Varkki <alinavarkki@chromium.org>

--- test/e2e_non_hosted/console/console-truncate-long-messages_test.ts
+++ test/e2e_non_hosted/console/console-truncate-long-messages_test.ts
@@ -3,12 +3,9 @@
 // found in the LICENSE file.
 
 import {
-  goToResource,
-} from '../../shared/helper.js';
-import {
   checkCommandResultFunction,
   navigateToConsoleTab,
-} from '../helpers/console-helpers.js';
+} from '../../e2e/helpers/console-helpers.js';
 
 // offset is 1 because Console.log returns undefined after logging, so we want
 // to check the output before the last one.
@@ -18,81 +15,93 @@ describe('The Console Tab', function() {
   // This test takes longer than usual because each command is typed and
   // checked individually.
   this.timeout(20000);
-  it('Truncates large messages', async () => {
-    await goToResource('../resources/console/command-line-api-getEventListeners.html');
-    await navigateToConsoleTab();
+  it('Truncates large messages', async ({devToolsPage, inspectedPage}) => {
+    await inspectedPage.goToResource('../resources/console/command-line-api-getEventListeners.html');
+    await navigateToConsoleTab(devToolsPage);
     const overMaxLength = 10001;
 
     await checkCommandResult(
         `console.log("a".repeat(${overMaxLength}))`,
         `${'a'.repeat(5000)}`,
         'Console unable to truncate long strings',
+        devToolsPage,
     );
 
     await checkCommandResult(
         `console.log("%s", "a".repeat(${overMaxLength}))`,
         `${'a'.repeat(5000)}`,
         'Console unable to truncate long formatted strings',
+        devToolsPage,
     );
 
     await checkCommandResult(
         `console.log("a".repeat(${overMaxLength}), "b".repeat(${overMaxLength}))`,
         `${'a'.repeat(5000)} ${'b'.repeat(5000)}`,
         'Console unable to truncate multiple long strings',
+        devToolsPage,
     );
 
     await checkCommandResult(
         `console.log("%o", "a".repeat(${overMaxLength}))`,
-        `\'${'a'.repeat(4999)}`,
+        `'${'a'.repeat(4999)}`,
         'Console unable to truncate DOM element',
+        devToolsPage,
     );
 
     await checkCommandResult(
         `console.log("%c" + "a".repeat(${overMaxLength}), "color: green")`,
         `${'a'.repeat(5000)}`,
         'Console unable to truncate formatted string with CSS rules',
+        devToolsPage,
     );
 
     await checkCommandResult(
         `console.log("foo %s %o bar", "a".repeat(${overMaxLength}), {a: 1})`,
         `foo ${'a'.repeat(4996)}{a: 1} bar`,
         'Console unable to truncate formatted string with DOM element',
+        devToolsPage,
     );
 
     await checkCommandResult(
         `console.log({a: 1}, "a".repeat(${overMaxLength}), {b: 1})`,
-        `{a: 1} \'${'a'.repeat(4999)} {b: 1}`,
+        `{a: 1} '${'a'.repeat(4999)} {b: 1}`,
         'Console unable to truncate string containing objects',
+        devToolsPage,
     );
 
     await checkCommandResult(
         `console.log("a".repeat(${overMaxLength}), "https://chromium.org")`,
         `${'a'.repeat(5000)} https://chromium.org`,
         'Console unable to truncate string ending with a link',
+        devToolsPage,
     );
 
     await checkCommandResult(
         `console.log("https://chromium.org", "a".repeat(${overMaxLength}))`,
         `https://chromium.org ${'a'.repeat(5000)}`,
         'Console unable to truncate string beginning with a link',
+        devToolsPage,
     );
 
     await checkCommandResult(
         `console.log(RegExp("a".repeat(${overMaxLength})))`,
         `\/${'a'.repeat(4999)}`,
         'Console unable to truncate a regular expression',
+        devToolsPage,
     );
 
     await checkCommandResult(
         `console.log(Symbol("a".repeat(${overMaxLength})))`,
         `Symbol(${'a'.repeat(4993)}`,
         'Console unable to truncate a symbol',
+        devToolsPage,
     );
 
     await checkCommandResult(
         `console.log(["a".repeat(${overMaxLength})])`,
-        `[\'${'a'.repeat(50)}…${'a'.repeat(49)}\']`,
+        `['${'a'.repeat(50)}…${'a'.repeat(49)}']`,
         'Console unable to truncate an array',
+        devToolsPage,
     );
   });
 });

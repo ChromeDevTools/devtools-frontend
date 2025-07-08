f620f328eb6338530f8467b68b9fdd8513cd12e4
Simon Zünd <szuend@chromium.org>
2025-07-08 10:59:09 +0200

    Migrate test/e2e/console/alert-toString-exception_test.ts
    Fixed: 416404943
    Change-Id: I1ebf1afdf6d9ec8f4a6385e199a58c64b67fdbe6
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6713278
    Auto-Submit: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Wolfgang Beyer <wolfi@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>

---
+++ test/e2e_non_hosted/console/alert-toString-exception_test.ts
@@ -0,0 +1,22 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {assert} from 'chai';
+
+import {
+  getConsoleMessages,
+  navigateToConsoleTab,
+} from '../../e2e/helpers/console-helpers.js';
+
+describe('The Console Tab', () => {
+  it('Does not crash if it fails to convert alert() argument to string', async ({devToolsPage, inspectedPage}) => {
+    await navigateToConsoleTab(devToolsPage);
+
+    await inspectedPage.reload();
+
+    const result =
+        (await getConsoleMessages('alert-toString-exception', undefined, undefined, devToolsPage, inspectedPage))[0];
+    assert.strictEqual(result, 'Uncaught Exception in toString().');
+  });
+});

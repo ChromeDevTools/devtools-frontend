740367c213fde4d221eff0d8fd62bb79677b4747
Simon Zünd <szuend@chromium.org>
2025-07-08 07:14:23 +0200

    Migrate inline-variable_test.ts to e2e_non_hosted
    Fixed: 416404755
    Change-Id: Id330128f9881305d93575e2500db790fe9da744e
    Bug: 416404755
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6712995
    Auto-Submit: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>

---
+++ test/e2e_non_hosted/sources/inline-variable_test.ts
@@ -0,0 +1,36 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {assert} from 'chai';
+
+import {
+  addBreakpointForLine,
+  openSourceCodeEditorForFile,
+  RESUME_BUTTON,
+} from '../../e2e/helpers/sources-helpers.js';
+import type {DevToolsPage} from '../../e2e_non_hosted/shared/frontend-helper.js';
+
+async function retrieveCodeMirrorEditorContent(devToolsPage: DevToolsPage): Promise<string[]> {
+  const editor = await devToolsPage.waitFor('[aria-label="Code editor"]');
+  return await editor.evaluate(
+      (node: Element) => [...node.querySelectorAll('.cm-line')].map(node => node.textContent || '') || []);
+}
+
+describe('Sources Tab', function() {
+  it('shows correct inline variable at definition', async ({devToolsPage, inspectedPage}) => {
+    await openSourceCodeEditorForFile('inline-variable.js', 'inline-variable.html', devToolsPage, inspectedPage);
+    await addBreakpointForLine(3, devToolsPage);
+
+    const scriptEvaluation = inspectedPage.evaluate('simple(41);');
+
+    await devToolsPage.waitFor('.cm-line > .cm-variableValues');
+
+    const contents = await retrieveCodeMirrorEditorContent(devToolsPage);
+    assert.strictEqual(contents[0], 'function simple(a) {a = 41');
+    assert.strictEqual(contents[1], '  let x = a + 1;x = 42');
+
+    await devToolsPage.click(RESUME_BUTTON);
+    await scriptEvaluation;
+  });
+});

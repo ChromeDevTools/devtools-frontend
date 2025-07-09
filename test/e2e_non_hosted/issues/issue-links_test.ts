be4328944d2335681e35bf6b085e47e865bb8d17
Simon Zünd <szuend@chromium.org>
2025-07-09 07:44:30 +0200

    Migrate test/e2e/issues/issue-links_test.ts
    Fixed: 416405408
    Change-Id: Icffc27fa2adc5488253c9091458cbea3a3ba01dd
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6714528
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Auto-Submit: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>

--- test/e2e_non_hosted/issues/issue-links_test.ts
+++ test/e2e_non_hosted/issues/issue-links_test.ts
@@ -4,17 +4,18 @@
 
 import {assert} from 'chai';
 
-import {click, goToResource, waitFor, waitForFunction} from '../../shared/helper.js';
-import {navigateToConsoleTab} from '../helpers/console-helpers.js';
+import {
+  navigateToConsoleTab,
+} from '../../e2e/helpers/console-helpers.js';
 
 describe('Issue links in the console tab', () => {
-  it('should reveal the right issue', async () => {
-    await navigateToConsoleTab();
-    await goToResource('issues/cors-issue-2.html');
-    const issueLinkIcon = await waitFor('devtools-issue-link-icon');
-    const button = await waitFor('button', issueLinkIcon);
+  it('should reveal the right issue', async ({devToolsPage, inspectedPage}) => {
+    await navigateToConsoleTab(devToolsPage);
+    await inspectedPage.goToResource('issues/cors-issue-2.html');
+    const issueLinkIcon = await devToolsPage.waitFor('devtools-issue-link-icon');
+    const button = await devToolsPage.waitFor('button', issueLinkIcon);
     // There are several TypeErrors in the console, we don't care which one we get.
-    const issueTitleFromLink = await waitForFunction(async () => {
+    const issueTitleFromLink = await devToolsPage.waitForFunction(async () => {
       const title = await button.evaluate(el => (el as HTMLElement).title);
       const titleStart = 'Click to open the issue tab and show issue: ';
       if (title.startsWith(titleStart)) {
@@ -22,8 +23,8 @@ describe('Issue links in the console tab', () => {
       }
       return undefined;
     });
-    await click('button', {root: issueLinkIcon});
-    const selectedIssueTitleElement = await waitFor('li.issue.expanded.selected');
+    await devToolsPage.click('button', {root: issueLinkIcon});
+    const selectedIssueTitleElement = await devToolsPage.waitFor('li.issue.expanded.selected');
     const selectedIssueTitle = await selectedIssueTitleElement.evaluate(el => el.textContent);
     // The '1' is the number of issues aggregated.
     assert.strictEqual(selectedIssueTitle, `1${issueTitleFromLink}`);

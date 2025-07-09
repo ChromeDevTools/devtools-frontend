fe611ea808fe80544f6796b8faa3a44401f220db
Paul Irish <paulirish@chromium.org>
2025-07-09 11:03:07 -0700

    RPP: Fix invalid read of persistedGroupConfig
    Bug: 430508616
    Change-Id: Ia71a9d7ac38850e1cd1ac9ce7004076aa3416bf0
    Fixes: 430508616
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6720251
    Reviewed-by: Jack Franklin <jacktfranklin@chromium.org>
    Commit-Queue: Jack Franklin <jacktfranklin@chromium.org>
    Auto-Submit: Paul Irish <paulirish@chromium.org>

--- front_end/ui/legacy/components/perf_ui/FlameChart.ts
+++ front_end/ui/legacy/components/perf_ui/FlameChart.ts
@@ -3419,8 +3419,8 @@ export class FlameChart extends Common.ObjectWrapper.eventMixin<EventTypes, type
     this.timelineLevels = levelIndexes;
     const groups = this.rawTimelineData.groups || [];
     for (let i = 0; i < groups.length; ++i) {
-      const expanded = groups[i].expanded ?? this.#persistedGroupConfig?.[i].expanded ?? false;
-      const hidden = groups[i].hidden ?? this.#persistedGroupConfig?.[i].hidden ?? false;
+      const expanded = groups[i].expanded ?? this.#persistedGroupConfig?.[i]?.expanded ?? false;
+      const hidden = groups[i].hidden ?? this.#persistedGroupConfig?.[i]?.hidden ?? false;
       groups[i].expanded = expanded;
       groups[i].hidden = hidden;
     }

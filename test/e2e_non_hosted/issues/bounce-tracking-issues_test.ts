e5f92b73085f6ad79a8b3e52a37e363d20f1f390
Liviu Rau <liviurau@chromium.org>
2025-07-10 12:09:28 +0200

    Migrate test/e2e/issues/bounce-tracking-issues_test.ts
    Fixed: 416405391
    Change-Id: Ia83a40699c1bf0bf60a0ff982d61a2411b487c4d
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6722411
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Auto-Submit: Liviu Rau <liviurau@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>

--- test/e2e_non_hosted/issues/bounce-tracking-issues_test.ts
+++ test/e2e_non_hosted/issues/bounce-tracking-issues_test.ts
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {assertNotNullOrUndefined, getBrowserAndPages, goToResource} from '../../shared/helper.js';
 import {
   ensureResourceSectionIsExpanded,
   expandIssue,
@@ -10,17 +9,14 @@ import {
   getResourcesElement,
   navigateToIssuesTab,
   waitForTableFromResourceSectionContents,
-} from '../helpers/issues-helpers.js';
+} from '../../e2e/helpers/issues-helpers.js';
+import {assertNotNullOrUndefined} from '../../shared/helper.js';
 
 describe('Bounce Tracking issue', () => {
-  beforeEach(async () => {
-    await goToResource('empty.html');
-  });
-
-  it('should display correct information', async () => {
-    await navigateToIssuesTab();
-    const {frontend} = getBrowserAndPages();
-    await frontend.evaluate(() => {
+  it('should display correct information', async ({devToolsPage, inspectedPage}) => {
+    await inspectedPage.goToResource('empty.html');
+    await navigateToIssuesTab(devToolsPage);
+    await devToolsPage.evaluate(() => {
       const issue = {
         code: 'BounceTrackingIssue',
         details: {
@@ -42,16 +38,16 @@ describe('Bounce Tracking issue', () => {
       // @ts-expect-error
       window.addIssueForTest(issue2);
     });
-    await expandIssue();
-    const issueElement =
-        await getIssueByTitle('Chrome may soon delete state for intermediate websites in a recent navigation chain');
+    await expandIssue(devToolsPage);
+    const issueElement = await getIssueByTitle(
+        'Chrome may soon delete state for intermediate websites in a recent navigation chain', devToolsPage);
     assertNotNullOrUndefined(issueElement);
-    const section = await getResourcesElement('2 potentially tracking websites', issueElement);
-    await ensureResourceSectionIsExpanded(section);
+    const section = await getResourcesElement('2 potentially tracking websites', issueElement, undefined, devToolsPage);
+    await ensureResourceSectionIsExpanded(section, devToolsPage);
     const expectedTableRows = [
       ['example_1.test'],
       ['example_2.test'],
     ];
-    await waitForTableFromResourceSectionContents(section.content, expectedTableRows);
+    await waitForTableFromResourceSectionContents(section.content, expectedTableRows, devToolsPage);
   });
 });

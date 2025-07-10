24cfc3beb800a981ecdc9d3d41b64b03e1835129
Simon Zünd <szuend@chromium.org>
2025-07-10 09:53:55 +0200

    Migrate test/e2e/issues/expect-ct-issues_test.ts
    Fixed: 416405061
    Change-Id: I832b96383cfda870d4f63f1fc3a75e5947016c55
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6718449
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Ergün Erdoğmuş <ergunsh@chromium.org>
    Auto-Submit: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Ergün Erdoğmuş <ergunsh@chromium.org>

--- test/e2e_non_hosted/issues/expect-ct-issues_test.ts
+++ test/e2e_non_hosted/issues/expect-ct-issues_test.ts
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {assertNotNullOrUndefined, getBrowserAndPages} from '../../shared/helper.js';
 import {
   ensureResourceSectionIsExpanded,
   expandIssue,
@@ -10,13 +9,13 @@ import {
   getResourcesElement,
   navigateToIssuesTab,
   waitForTableFromResourceSectionContents,
-} from '../helpers/issues-helpers.js';
+} from '../../e2e/helpers/issues-helpers.js';
+import {assertNotNullOrUndefined} from '../../shared/helper.js';
 
 describe('Expect-CT Issue', () => {
-  it('should display deprecation issue for Expect-CT header', async () => {
-    await navigateToIssuesTab();
-    const {frontend} = getBrowserAndPages();
-    await frontend.evaluate(() => {
+  it('should display deprecation issue for Expect-CT header', async ({devToolsPage}) => {
+    await navigateToIssuesTab(devToolsPage);
+    await devToolsPage.evaluate(() => {
       const issue = {
         code: 'DeprecationIssue',
         details: {
@@ -34,14 +33,14 @@ describe('Expect-CT Issue', () => {
       window.addIssueForTest(issue);
     });
 
-    await expandIssue();
-    const issueElement = await getIssueByTitle('Deprecated feature used');
+    await expandIssue(devToolsPage);
+    const issueElement = await getIssueByTitle('Deprecated feature used', devToolsPage);
     assertNotNullOrUndefined(issueElement);
-    const section = await getResourcesElement('1 source', issueElement, '.affected-resource-label');
-    await ensureResourceSectionIsExpanded(section);
+    const section = await getResourcesElement('1 source', issueElement, '.affected-resource-label', devToolsPage);
+    await ensureResourceSectionIsExpanded(section, devToolsPage);
     const expectedTableRows = [
       ['empty.html:2'],
     ];
-    await waitForTableFromResourceSectionContents(section.content, expectedTableRows);
+    await waitForTableFromResourceSectionContents(section.content, expectedTableRows, devToolsPage);
   });
 });

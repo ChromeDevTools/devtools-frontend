d27408ce7ab761da7171e927662d5c6ce7a2ea5b
Simon Zünd <szuend@chromium.org>
2025-07-10 09:40:04 +0200

    Migrate test/e2e/issues/cookie-domain-attribute-non-ascii_test.ts
    Fixed: 416405351
    Change-Id: I2a47a2474dc6bbb696c5c54567fa655309b0140f
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6718448
    Reviewed-by: Philip Pfaffe <pfaffe@chromium.org>
    Auto-Submit: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Philip Pfaffe <pfaffe@chromium.org>

---
+++ test/e2e_non_hosted/issues/cookie-domain-attribute-non-ascii_test.ts
@@ -0,0 +1,49 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {
+  ensureResourceSectionIsExpanded,
+  expandIssue,
+  getIssueByTitle,
+  getResourcesElement,
+  navigateToIssuesTab,
+  waitForTableFromResourceSectionContents,
+} from '../../e2e/helpers/issues-helpers.js';
+import {assertNotNullOrUndefined} from '../../shared/helper.js';
+
+describe('Cookie domain attribute should not contain non-ASCII characters issue', () => {
+  it('should display an issue when a cookie has a domain attribute with non-ASCII characters',
+     async ({devToolsPage, inspectedPage}) => {
+       await navigateToIssuesTab(devToolsPage);
+       await inspectedPage.goToResource('empty.html');
+
+       await inspectedPage.evaluate(async () => {
+         const img = document.createElement('img');
+         const done = new Promise(c => {
+           img.onerror = c;
+         });
+         img.src = './issues/cookie-domain-non-ascii.rawresponse';
+         // This rule is only relevant for unit tests.
+         // eslint-disable-next-line rulesdir/no-document-body-mutation
+         document.body.appendChild(img);
+         await done;
+       });
+
+       await navigateToIssuesTab(devToolsPage);
+       await expandIssue(devToolsPage);
+       const issueElement =
+           await getIssueByTitle('Ensure cookie `Domain` attribute values only contain ASCII characters', devToolsPage);
+       assertNotNullOrUndefined(issueElement);
+       const section =
+           await getResourcesElement('1 Raw Set-Cookie header', issueElement, '.affected-resource-label', devToolsPage);
+       await ensureResourceSectionIsExpanded(section, devToolsPage);
+       const expectedTableRows = [
+         ['tasty_cookie=yum; Domain=öxample.com'],
+       ];
+       await waitForTableFromResourceSectionContents(section.content, expectedTableRows, devToolsPage);
+
+       const cookies = await inspectedPage.page.cookies();
+       await inspectedPage.page.deleteCookie(...cookies);
+     });
+});

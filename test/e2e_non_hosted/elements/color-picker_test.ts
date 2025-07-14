796a9beaf51c7b21485e5c8de2d2b496cf1c31a8
Liviu Rau <liviurau@chromium.org>
2025-07-14 10:44:57 +0200

    Migrate test/e2e/elements/color-picker_test.ts
    Fixed: 416405428
    Change-Id: I13385695ff971fd55d16df64b0403a6ddf6e4548
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6725901
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Liviu Rau <liviurau@chromium.org>
    Auto-Submit: Liviu Rau <liviurau@chromium.org>

---
+++ test/e2e_non_hosted/elements/color-picker_test.ts
@@ -0,0 +1,34 @@
+// Copyright 2022 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import {getColorSwatch, goToResourceAndWaitForStyleSection} from '../../e2e/helpers/elements-helpers.js';
+import {assertNotNullOrUndefined} from '../../shared/helper.js';
+
+describe('ColorPicker', () => {
+  it('scrolls to the bottom when previewing palettes', async ({devToolsPage, inspectedPage}) => {
+    await goToResourceAndWaitForStyleSection('elements/css-variables-many.html', devToolsPage, inspectedPage);
+
+    const swatch = await devToolsPage.waitForFunction(() => getColorSwatch(/* parent*/ undefined, 0, devToolsPage));
+    await devToolsPage.clickElement(swatch);
+
+    const panel = await devToolsPage.waitFor('.palette-panel');
+    await devToolsPage.click('.spectrum-palette-switcher');
+    await devToolsPage.waitForFunction(() => panel.isIntersectingViewport({threshold: 1}));
+
+    const palette =
+        await devToolsPage.waitForFunction(async () => await devToolsPage.$textContent('CSS Variables') ?? undefined);
+
+    // Need to wait for the spectrum overlay to disappear (i.e., finish its transition) for it to not eat our next click
+    const overlay = await devToolsPage.$('.spectrum-overlay');
+    assertNotNullOrUndefined(overlay);
+
+    await devToolsPage.clickElement(palette);
+    await devToolsPage.waitForFunction(
+        async () => await (await devToolsPage.waitFor('.spectrum-overlay'))
+                        .evaluate(e => e.computedStyleMap().get('visibility')?.toString() === 'hidden'));
+
+    await devToolsPage.click('.spectrum-palette-switcher');
+    await devToolsPage.waitForFunction(() => panel.isIntersectingViewport({threshold: 1}));
+  });
+});

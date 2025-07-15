80ec403c77de782395727e0ae9b53053808939f7
Ergun Erdogmus <ergunsh@chromium.org>
2025-07-15 12:59:22 +0300

    [UIEngineering] Add `widgetScoped` helper to scope styles to a widget's view
    Bug: 431945418
    Change-Id: I45b91e90c66e7364d19e2ad15625d17a4c8aa9e1
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6733265
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Ergün Erdoğmuş <ergunsh@chromium.org>

--- front_end/panels/elements/LayoutPane.ts
+++ front_end/panels/elements/LayoutPane.ts
@@ -283,8 +283,8 @@ const DEFAULT_VIEW: View = (input, output, target) => {
   // clang-format off
   render(html`
       <div style="min-width: min-content;" jslog=${VisualLogging.pane('layout').track({resize: true})}>
-        <style>@scope to (devtools-widget) { ${layoutPaneStyles} }</style>
-        <style>@scope to (devtools-widget) { ${UI.inspectorCommonStyles} }</style>
+        <style>${UI.Widget.widgetScoped(layoutPaneStyles)}</style>
+        <style>${UI.Widget.widgetScoped(UI.inspectorCommonStyles)}</style>
         <details open>
           <summary class="header"
             @keydown=${input.onSummaryKeyDown}

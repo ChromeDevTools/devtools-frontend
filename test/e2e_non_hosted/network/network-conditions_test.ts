854f43005b1d92f9220d8d8e2bb5251c12b40691
Liviu Rau <liviurau@google.com>
2025-07-25 10:44:56 +0200

    Migrate test/e2e/network/network-conditions_test.ts
    Fixed: 416404740
    Change-Id: I0a4c5efae6097a7d4390a24b134acbbe0da6124d
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6787530
    Auto-Submit: Liviu Rau <liviurau@chromium.org>
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Liviu Rau <liviurau@chromium.org>

--- test/e2e_non_hosted/network/network-conditions_test.ts
+++ test/e2e_non_hosted/network/network-conditions_test.ts
@@ -3,17 +3,11 @@
 // found in the LICENSE file.
 
 import {assert} from 'chai';
-import type {ElementHandle, Page} from 'puppeteer-core';
+import type {ElementHandle} from 'puppeteer-core';
 
-import {
-  getBrowserAndPages,
-  pressKey,
-  tabForward,
-  typeText,
-  waitFor,
-  waitForAria,
-} from '../../shared/helper.js';
-import {navigateToNetworkTab} from '../helpers/network-helpers.js';
+import {navigateToNetworkTab} from '../../e2e/helpers/network-helpers.js';
+import type {DevToolsPage} from '../../e2e_non_hosted/shared/frontend-helper.js';
+import type {InspectedPage} from '../../e2e_non_hosted/shared/target-helper.js';
 
 interface Navigator {
   userAgentData?: {
@@ -30,15 +24,11 @@ interface Navigator {
   };
 }
 
-describe('The Network Tab', function() {
-  beforeEach(async () => {
-    await navigateToNetworkTab('empty.html');
-  });
-
-  async function openNetworkConditions(sectionClassName: string) {
-    const networkConditionsButton = await waitForAria('More network conditions…');
+describe('The Network Tab', () => {
+  async function openNetworkConditions(devToolsPage: DevToolsPage, sectionClassName: string) {
+    const networkConditionsButton = await devToolsPage.waitForAria('More network conditions…');
     await networkConditionsButton.click();
-    return await waitFor(sectionClassName);
+    return await devToolsPage.waitFor(sectionClassName);
   }
 
   async function assertDisabled(checkbox: ElementHandle<HTMLInputElement>, expected: boolean) {
@@ -51,9 +41,9 @@ describe('The Network Tab', function() {
     assert.strictEqual(checked, expected);
   }
 
-  async function getUserAgentMetadataFromTarget(target: Page) {
+  async function getUserAgentMetadataFromTarget(target: InspectedPage) {
     const getUserAgentMetaData = async () => {
-      const nav = <Navigator>navigator;
+      const nav = navigator as Navigator;
       return {
         brands: nav.userAgentData?.brands,
         fullVersionList: nav.userAgentData?.fullVersionList,
@@ -72,12 +62,13 @@ describe('The Network Tab', function() {
     return await target.evaluate(getUserAgentMetaDataStr);
   }
 
-  it('can change accepted content encodings', async () => {
-    const section = await openNetworkConditions('.network-config-accepted-encoding');
-    const autoCheckbox = await (await waitForAria('Use browser default', section)).toElement('input');
-    const deflateCheckbox = await (await waitForAria('deflate', section)).toElement('input');
-    const gzipCheckbox = await (await waitForAria('gzip', section)).toElement('input');
-    const brotliCheckbox = await (await waitForAria('br', section)).toElement('input');
+  it('can change accepted content encodings', async ({devToolsPage, inspectedPage}) => {
+    await navigateToNetworkTab('empty.html', devToolsPage, inspectedPage);
+    const section = await openNetworkConditions(devToolsPage, '.network-config-accepted-encoding');
+    const autoCheckbox = await (await devToolsPage.waitForAria('Use browser default', section)).toElement('input');
+    const deflateCheckbox = await (await devToolsPage.waitForAria('deflate', section)).toElement('input');
+    const gzipCheckbox = await (await devToolsPage.waitForAria('gzip', section)).toElement('input');
+    const brotliCheckbox = await (await devToolsPage.waitForAria('br', section)).toElement('input');
     await brotliCheckbox.evaluate(el => el.scrollIntoView(true));
     await assertChecked(autoCheckbox, true);
     await assertChecked(deflateCheckbox, true);
@@ -116,9 +107,9 @@ describe('The Network Tab', function() {
     await assertDisabled(brotliCheckbox, true);
   });
 
-  it('can override userAgentMetadata', async () => {
-    const {target, browser} = getBrowserAndPages();
-    const fullVersion = (await browser.version()).split('/')[1];
+  it('can override userAgentMetadata', async ({browser, devToolsPage, inspectedPage}) => {
+    await navigateToNetworkTab('empty.html', devToolsPage, inspectedPage);
+    const fullVersion = (await browser.browser.version()).split('/')[1];
     const majorVersion = fullVersion.split('.', 1)[0];
     const fixedVersionUAValue =
         'Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30';
@@ -165,9 +156,9 @@ describe('The Network Tab', function() {
       architecture: '',
       model: '',
     };
-    const section = await openNetworkConditions('.network-config-ua');
-    const autoCheckbox = await (await waitForAria('Use browser default', section)).toElement('input');
-    const uaDropdown = await waitForAria('User agent', section);
+    const section = await openNetworkConditions(devToolsPage, '.network-config-ua');
+    const autoCheckbox = await (await devToolsPage.waitForAria('Use browser default', section)).toElement('input');
+    const uaDropdown = await devToolsPage.waitForAria('User agent', section);
     await assertChecked(autoCheckbox, true);
     await autoCheckbox.click();
     await assertChecked(autoCheckbox, false);
@@ -176,57 +167,57 @@ describe('The Network Tab', function() {
     await uaDropdown.select(fixedVersionUAValue);
     await uaDropdown.click();
 
-    const fixedVersionUserAgentMetadata = await getUserAgentMetadataFromTarget(target);
+    const fixedVersionUserAgentMetadata = await getUserAgentMetadataFromTarget(inspectedPage);
     assert.deepEqual(fixedVersionUserAgentMetadata, fixedVersionUserAgentMetadataExpected);
 
     await uaDropdown.click();
     await uaDropdown.select(dynamicVersionUAValue);
     await uaDropdown.click();
 
-    const dynamicVersionUserAgentMetadata = await getUserAgentMetadataFromTarget(target);
+    const dynamicVersionUserAgentMetadata = await getUserAgentMetadataFromTarget(inspectedPage);
     assert.deepEqual(dynamicVersionUserAgentMetadata, dynamicVersionUserAgentMetadataExpected);
 
     await uaDropdown.click();
     await uaDropdown.select(noMetadataVersionUAValue);
     await uaDropdown.click();
 
-    const noMetadataVersionUserAgentMetadata = await getUserAgentMetadataFromTarget(target);
+    const noMetadataVersionUserAgentMetadata = await getUserAgentMetadataFromTarget(inspectedPage);
     assert.deepEqual(noMetadataVersionUserAgentMetadata, noMetadataVersionUserAgentMetadataExpected);
   });
 
-  it('restores default userAgentMetadata', async () => {
-    const {target, browser} = getBrowserAndPages();
-    const fullVersion = (await browser.version()).split('/')[1];
+  it('restores default userAgentMetadata', async ({browser, devToolsPage, inspectedPage}) => {
+    await navigateToNetworkTab('empty.html', devToolsPage, inspectedPage);
+    const fullVersion = (await browser.browser.version()).split('/')[1];
     const majorVersion = fullVersion.split('.', 1)[0];
     const customUAValue =
         `Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/${
             majorVersion}.0.0.0 Mobile Safari/537.36`;
-    const section = await openNetworkConditions('.network-config-ua');
-    const autoCheckbox = await (await waitForAria('Use browser default', section)).toElement('input');
-    const uaDropdown = await waitForAria('User agent', section);
+    const section = await openNetworkConditions(devToolsPage, '.network-config-ua');
+    const autoCheckbox = await (await devToolsPage.waitForAria('Use browser default', section)).toElement('input');
+    const uaDropdown = await devToolsPage.waitForAria('User agent', section);
     await assertChecked(autoCheckbox, true);
 
-    const defaultUserAgentMetadata = await getUserAgentMetadataFromTarget(target);
+    const defaultUserAgentMetadata = await getUserAgentMetadataFromTarget(inspectedPage);
     await autoCheckbox.click();
     await assertChecked(autoCheckbox, false);
 
     await uaDropdown.click();
     await uaDropdown.select(customUAValue);
     await uaDropdown.click();
-    const customUserAgentMetadata = await getUserAgentMetadataFromTarget(target);
+    const customUserAgentMetadata = await getUserAgentMetadataFromTarget(inspectedPage);
     assert.notDeepEqual(defaultUserAgentMetadata, customUserAgentMetadata);
 
     await autoCheckbox.click();
     await assertChecked(autoCheckbox, true);
-    const restoredUserAgentMetadata = await getUserAgentMetadataFromTarget(target);
+    const restoredUserAgentMetadata = await getUserAgentMetadataFromTarget(inspectedPage);
     assert.deepEqual(defaultUserAgentMetadata, restoredUserAgentMetadata);
   });
 
-  it('can apply customized userAgentMetadata', async () => {
-    const {target} = getBrowserAndPages();
-    const section = await openNetworkConditions('.network-config-ua');
-    const autoCheckbox = await (await waitForAria('Use browser default', section)).toElement('input');
-    const uaDropdown = await waitForAria('User agent', section);
+  it('can apply customized userAgentMetadata', async ({devToolsPage, inspectedPage}) => {
+    await navigateToNetworkTab('empty.html', devToolsPage, inspectedPage);
+    const section = await openNetworkConditions(devToolsPage, '.network-config-ua');
+    const autoCheckbox = await (await devToolsPage.waitForAria('Use browser default', section)).toElement('input');
+    const uaDropdown = await devToolsPage.waitForAria('User agent', section);
     await assertChecked(autoCheckbox, true);
 
     await autoCheckbox.click();
@@ -234,66 +225,66 @@ describe('The Network Tab', function() {
 
     // Choose "Custom..." UA, Move focus to UA string and enter test value
     await uaDropdown.select('custom');
-    const userAgent = await waitForAria('Enter a custom user agent');
+    const userAgent = await devToolsPage.waitForAria('Enter a custom user agent');
     await userAgent.click();
-    await userAgent.type('Test User Agent String');
-    await tabForward();       // focus help button
-    await pressKey('Space');  // open client hints section
-    await tabForward();       // focus help link
+    await devToolsPage.typeText('Test User Agent String');
+    await devToolsPage.tabForward();       // focus help button
+    await devToolsPage.pressKey('Space');  // open client hints section
+    await devToolsPage.tabForward();       // focus help link
 
     // UA brands
-    await tabForward();  // focus brand name
-    await typeText('Test Brand 1');
-    await tabForward();  // focus brand version
-    await typeText('99');
-    await tabForward();       // focus delete brand button
-    await tabForward();       // focus add brand button
-    await pressKey('Enter');  // add a second brand
-    await typeText('Test Brand 2');
-    await tabForward();  // focus brand version
-    await typeText('100');
-    await tabForward();       // focus delete brand button
-    await tabForward();       // focus add brand button
-    await pressKey('Enter');  // add a third brand
-    await typeText('Test Brand 3');
-    await tabForward();  // focus brand version
-    await typeText('101');
-    await tabForward();  // focus delete brand button
-    await tabForward();  // focus add brand button
+    await devToolsPage.tabForward();  // focus brand name
+    await devToolsPage.typeText('Test Brand 1');
+    await devToolsPage.tabForward();  // focus brand version
+    await devToolsPage.typeText('99');
+    await devToolsPage.tabForward();       // focus delete brand button
+    await devToolsPage.tabForward();       // focus add brand button
+    await devToolsPage.pressKey('Enter');  // add a second brand
+    await devToolsPage.typeText('Test Brand 2');
+    await devToolsPage.tabForward();  // focus brand version
+    await devToolsPage.typeText('100');
+    await devToolsPage.tabForward();       // focus delete brand button
+    await devToolsPage.tabForward();       // focus add brand button
+    await devToolsPage.pressKey('Enter');  // add a third brand
+    await devToolsPage.typeText('Test Brand 3');
+    await devToolsPage.tabForward();  // focus brand version
+    await devToolsPage.typeText('101');
+    await devToolsPage.tabForward();  // focus delete brand button
+    await devToolsPage.tabForward();  // focus add brand button
 
     // full-version brands
-    await tabForward();  // focus brand
-    await typeText('FV Brand 1');
-    await tabForward();  // focus brand version
-    await typeText('9.8.7');
-    await tabForward();  // focus delete brand button
-    await tabForward();  // focus add brand button
+    await devToolsPage.tabForward();  // focus brand
+    await devToolsPage.typeText('FV Brand 1');
+    await devToolsPage.tabForward();  // focus brand version
+    await devToolsPage.typeText('9.8.7');
+    await devToolsPage.tabForward();  // focus delete brand button
+    await devToolsPage.tabForward();  // focus add brand button
 
-    await tabForward();  // focus browser full version
-    await typeText('99.99');
+    await devToolsPage.tabForward();  // focus browser full version
+    await devToolsPage.typeText('99.99');
 
     // Focus on form factors checkbox
     for (let i = 0; i < 7; ++i) {
-      await tabForward();
+      await devToolsPage.tabForward();
       // Enable form factors Desktop and XR
       if (i === 0 || i === 4) {
-        await pressKey('Space');
+        await devToolsPage.pressKey('Space');
       }
     }
 
-    await tabForward();  // focus platform name
-    await typeText('Test Platform');
-    await tabForward();  // focus platform version
-    await typeText('10');
-    await tabForward();  // focus architecture
-    await typeText('Test Architecture');
-    await tabForward();  // focus device model
-    await typeText('Test Model');
-    await tabForward();  // focus mobile checkbox
-    await pressKey('Space');
-    await tabForward();  // focus update button
-    await pressKey('Enter');
-    const userAgentMetadata = await getUserAgentMetadataFromTarget(target);
+    await devToolsPage.tabForward();  // focus platform name
+    await devToolsPage.typeText('Test Platform');
+    await devToolsPage.tabForward();  // focus platform version
+    await devToolsPage.typeText('10');
+    await devToolsPage.tabForward();  // focus architecture
+    await devToolsPage.typeText('Test Architecture');
+    await devToolsPage.tabForward();  // focus device model
+    await devToolsPage.typeText('Test Model');
+    await devToolsPage.tabForward();  // focus mobile checkbox
+    await devToolsPage.pressKey('Space');
+    await devToolsPage.tabForward();  // focus update button
+    await devToolsPage.pressKey('Enter');
+    const userAgentMetadata = await getUserAgentMetadataFromTarget(inspectedPage);
     assert.deepEqual(userAgentMetadata, {
       brands: [
         {brand: 'Test Brand 1', version: '99'},
@@ -310,24 +301,24 @@ describe('The Network Tab', function() {
     });
 
     // Delete a brand
-    const brand = await waitForAria('Brand 1', section);  // move focus back to first brand
+    const brand = await devToolsPage.waitForAria('Brand 1', section);  // move focus back to first brand
     await brand.click();
-    await tabForward();  // focus brand version
-    await tabForward();  // focus delete brand button
-    await pressKey('Enter');
+    await devToolsPage.tabForward();  // focus brand version
+    await devToolsPage.tabForward();  // focus delete brand button
+    await devToolsPage.pressKey('Enter');
 
     // Edit a value
-    const platformVersion = await waitForAria('Platform version', section);
+    const platformVersion = await devToolsPage.waitForAria('Platform version', section);
     await platformVersion.click();
-    await typeText('11');
+    await devToolsPage.typeText('11');
 
     // Update
-    await tabForward();  // focus architecture
-    await tabForward();  // focus device model
-    await tabForward();  // focus mobile checkbox
-    await tabForward();  // focus update button
-    await pressKey('Enter');
-    const updatedUserAgentMetadata = await getUserAgentMetadataFromTarget(target);
+    await devToolsPage.tabForward();  // focus architecture
+    await devToolsPage.tabForward();  // focus device model
+    await devToolsPage.tabForward();  // focus mobile checkbox
+    await devToolsPage.tabForward();  // focus update button
+    await devToolsPage.pressKey('Enter');
+    const updatedUserAgentMetadata = await getUserAgentMetadataFromTarget(inspectedPage);
     assert.deepEqual(updatedUserAgentMetadata, {
       brands: [
         {brand: 'Test Brand 2', version: '100'},

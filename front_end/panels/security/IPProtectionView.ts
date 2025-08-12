ac1d63eeeacb619e97cedd11093b41c30375c243
clarej <clarej@google.com>
2025-08-12 19:22:21 +0000

    Reland "[IPP Devtools] P&S view for when not in incognito"
    This is a reland of commit 2b4395c2802dd9f15e0754cad7422142360a5ba9
    
    Original change's description:
    > [IPP Devtools] P&S view for when not in incognito
    >
    > Create a view that will appear when the IPP panel in the Privacy and
    > Security section is navigated to and the window is not incognito.
    >
    > Bug: 429151632
    > Change-Id: I61b5a8264ccf5aaaa2ef79b7a6f275d48b224f96
    > Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6731770
    > Reviewed-by: Philip Pfaffe <pfaffe@chromium.org>
    > Reviewed-by: Danil Somsikov <dsv@chromium.org>
    > Commit-Queue: Clare Jin <clarej@google.com>
    
    Bug: 429151632
    Change-Id: Ib2a968e2c1e158a0c32d6685cd86e7082a2f7e29
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6841345
    Commit-Queue: Clare Jin <clarej@google.com>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Philip Pfaffe <pfaffe@chromium.org>

--- front_end/panels/security/IPProtectionView.ts
+++ front_end/panels/security/IPProtectionView.ts
@@ -7,12 +7,15 @@ import '../../ui/components/switch/switch.js';
 import '../../ui/components/cards/cards.js';
 
 import * as i18n from '../../core/i18n/i18n.js';
+import type * as Platform from '../../core/platform/platform.js';
+import * as Root from '../../core/root/root.js';
 import * as UI from '../../ui/legacy/legacy.js';
 import * as Lit from '../../ui/lit/lit.js';
 
 import ipProtectionViewStyles from './ipProtectionView.css.js';
 
 const {render, html} = Lit;
+const {widgetConfig} = UI.Widget;
 
 const UIStrings = {
   /**
@@ -31,11 +34,22 @@ const UIStrings = {
    *@description Description in the card within the IP Protection tool
    */
   cardDescription: 'Only when DevTools is open',
+  /**
+   * @description Text informing the user that IP Proxy is not available
+   */
+  notInIncognito: 'IP proxy unavailable',
+  /**
+   * @description Description in the widget instructing users to open site in incognito
+   */
+  openIncognito: 'IP proxy is only available within incognito mode. Open site in incognito.',
 } as const;
 
 const str_ = i18n.i18n.registerUIStrings('panels/security/IPProtectionView.ts', UIStrings);
 export const i18nString = i18n.i18n.getLocalizedString.bind(undefined, str_);
 export const i18nFormatString = i18n.i18n.getFormatLocalizedString.bind(undefined, str_);
+
+const INCOGNITO_EXPLANATION_URL = 'https://support.google.com/chrome/answer/95464?hl=en&co=GENIE.Platform%3DDesktop' as
+    Platform.DevToolsPath.UrlString;
 
 export type View = (input: object, output: object, target: HTMLElement) => void;
 
@@ -45,31 +59,44 @@ export const DEFAULT_VIEW: View = (input, _, target) => {
     <style>
       ${ipProtectionViewStyles}
     </style>
-    <div class="overflow-auto">
-      <div class="ip-protection">
-        <div class="header">
-          <h1>${i18nString(UIStrings.viewTitle)}</h1>
-          <div class="body">${i18nString(UIStrings.viewExplanation)}</div>
-        </div>
-        <devtools-card class="card-container">
-          <div class="card">
-            <div class="card-header">
-              <div class="lhs">
-                <div class="text">
-                  <h2 class="main-text">${i18nString(UIStrings.cardTitle)}</h2>
-                  <div class="body-subtext">
-                    ${i18nString(UIStrings.cardDescription)}
+    ${Root.Runtime.hostConfig.isOffTheRecord ? html`
+      <div class="overflow-auto">
+        <div class="ip-protection">
+          <div class="header">
+            <h1>${i18nString(UIStrings.viewTitle)}</h1>
+            <div class="body">${i18nString(UIStrings.viewExplanation)}</div>
+          </div>
+          <devtools-card class="card-container">
+            <div class="card">
+              <div class="card-header">
+                <div class="lhs">
+                  <div class="text">
+                    <h2 class="main-text">${i18nString(UIStrings.cardTitle)}</h2>
+                    <div class="body-subtext">
+                      ${i18nString(UIStrings.cardDescription)}
+                    </div>
                   </div>
-                </div>
-                <div>
-                  <devtools-switch></devtools-switch>
+                  <div>
+                    <devtools-switch></devtools-switch>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </devtools-card>
+          </devtools-card>
+        </div>
       </div>
-    </div>
+    ` : html`
+      <div class="empty-report">
+        <devtools-widget
+          class="learn-more"
+          .widgetConfig=${widgetConfig(UI.EmptyWidget.EmptyWidget, {
+            header: i18nString(UIStrings.notInIncognito),
+            text: i18nString(UIStrings.openIncognito),
+            link: INCOGNITO_EXPLANATION_URL,
+          })}>
+        </devtools-widget>
+      </div>
+  `}
   `, target);
   // clang-format on
 };

6513937e5bb08d1f1288b23b02bc1404731fda1f
Liviu Rau <liviurau@google.com>
2025-08-25 17:48:38 +0200

    Migrate test/e2e/issues/user-reidentification-issues_test.ts
    Bug: 416394514
    Change-Id: Ib4e4d154dfd91c0a645dd82eab110608e0eb09ad
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6879708
    Commit-Queue: Liviu Rau <liviurau@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>

--- test/e2e_non_hosted/issues/user-reidentification-issues_test.ts
+++ test/e2e_non_hosted/issues/user-reidentification-issues_test.ts
@@ -1,10 +1,9 @@
-// Copyright 2025 The Chromium Authors. All rights reserved.
+// Copyright 2025 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 import {assert} from 'chai';
 
-import {getBrowserAndPages, goToResource} from '../../shared/helper.js';
 import {
   ensureResourceSectionIsExpanded,
   expandIssue,
@@ -12,17 +11,13 @@ import {
   getResourcesElement,
   navigateToIssuesTab,
   waitForTableFromResourceSectionContents,
-} from '../helpers/issues-helpers.js';
+} from '../../e2e/helpers/issues-helpers.js';
 
 describe('User Reidentification issue', () => {
-  beforeEach(async () => {
-    await goToResource('empty.html');
-  });
-
-  it('should display correct information', async () => {
-    await navigateToIssuesTab();
-    const {frontend} = getBrowserAndPages();
-    await frontend.evaluate(() => {
+  it('should display correct information', async ({devToolsPage, inspectedPage}) => {
+    await inspectedPage.goToResource('empty.html');
+    await navigateToIssuesTab(devToolsPage);
+    await devToolsPage.evaluate(() => {
       const issue = {
         code: 'UserReidentificationIssue',
         details: {
@@ -46,16 +41,16 @@ describe('User Reidentification issue', () => {
       // @ts-expect-error
       window.addIssueForTest(issue2);
     });
-    await expandIssue();
-    const issueElement = await getIssueByTitle('Resources suspected of tracking users are blocked');
+    await expandIssue(devToolsPage);
+    const issueElement = await getIssueByTitle('Resources suspected of tracking users are blocked', devToolsPage);
     assert.isOk(issueElement);
-    const section = await getResourcesElement('2 requests', issueElement);
-    await ensureResourceSectionIsExpanded(section);
+    const section = await getResourcesElement('2 requests', issueElement, undefined, devToolsPage);
+    await ensureResourceSectionIsExpanded(section, devToolsPage);
     assert.isOk(section);
     const expectedTableRows = [
       ['script.js'],
       ['script2.js'],
     ];
-    await waitForTableFromResourceSectionContents(section.content, expectedTableRows);
+    await waitForTableFromResourceSectionContents(section.content, expectedTableRows, devToolsPage);
   });
 });

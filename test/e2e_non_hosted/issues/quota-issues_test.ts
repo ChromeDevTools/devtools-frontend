8cff4f0e7287a7f4acdba42567592baba6c5fc30
Liviu Rau <liviurau@google.com>
2025-08-26 15:43:06 +0200

    Migrate test/e2e/issues/quota-issues_test.ts
    Bug: 416405293
    Change-Id: Ib717c0b8a3a2d1ce772882f8e345bc7866e7a42b
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6884668
    Commit-Queue: Liviu Rau <liviurau@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>

--- test/e2e_non_hosted/issues/quota-issues_test.ts
+++ test/e2e_non_hosted/issues/quota-issues_test.ts
@@ -4,7 +4,6 @@
 
 import {assert} from 'chai';
 
-import {getBrowserAndPages, goToResource} from '../../shared/helper.js';
 import {
   ensureResourceSectionIsExpanded,
   expandIssue,
@@ -12,17 +11,13 @@ import {
   getResourcesElement,
   navigateToIssuesTab,
   waitForTableFromResourceSectionContents,
-} from '../helpers/issues-helpers.js';
+} from '../../e2e/helpers/issues-helpers.js';
 
 describe('Quota Issues', () => {
-  beforeEach(async () => {
-    await goToResource('empty.html');
-  });
-
-  it('should display correct information', async () => {
-    await navigateToIssuesTab();
-    const {frontend} = getBrowserAndPages();
-    await frontend.evaluate(() => {
+  it('should display correct information', async ({devToolsPage, inspectedPage}) => {
+    await inspectedPage.goToResource('empty.html');
+    await navigateToIssuesTab(devToolsPage);
+    await devToolsPage.evaluate(() => {
       const issue = {
         code: 'DeprecationIssue',
         details: {
@@ -40,14 +35,14 @@ describe('Quota Issues', () => {
       window.addIssueForTest(issue);
     });
 
-    await expandIssue();
-    const issueElement = await getIssueByTitle('Deprecated feature used');
+    await expandIssue(devToolsPage);
+    const issueElement = await getIssueByTitle('Deprecated feature used', devToolsPage);
     assert.isOk(issueElement);
-    const section = await getResourcesElement('1 source', issueElement, '.affected-resource-label');
-    await ensureResourceSectionIsExpanded(section);
+    const section = await getResourcesElement('1 source', issueElement, '.affected-resource-label', devToolsPage);
+    await ensureResourceSectionIsExpanded(section, devToolsPage);
     const expectedTableRows = [
       ['empty.html:2'],
     ];
-    await waitForTableFromResourceSectionContents(section.content, expectedTableRows);
+    await waitForTableFromResourceSectionContents(section.content, expectedTableRows, devToolsPage);
   });
 });

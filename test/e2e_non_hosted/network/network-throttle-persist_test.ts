e436f15559103f9fa0ddf523c0b90429b5325c72
Liviu Rau <liviurau@google.com>
2025-08-27 08:42:52 +0200

    Migrate test/e2e/network/network-throttle-persist_test.ts
    Bug: 416405218
    Change-Id: I43d9e9e45c5a9cc890621a3730eb78a56b450b28
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6885141
    Commit-Queue: Liviu Rau <liviurau@chromium.org>
    Auto-Submit: Liviu Rau <liviurau@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>

--- test/e2e_non_hosted/network/network-throttle-persist_test.ts
+++ test/e2e_non_hosted/network/network-throttle-persist_test.ts
@@ -5,51 +5,46 @@
 import {assert} from 'chai';
 import type {ElementHandle} from 'puppeteer-core';
 
-import {waitForAria} from '../../shared/helper.js';
-import {reloadDevTools} from '../helpers/cross-tool-helper.js';
-import {navigateToNetworkTab} from '../helpers/network-helpers.js';
+import {navigateToNetworkTab} from '../../e2e/helpers/network-helpers.js';
 
 describe('The Network Tab', function() {
   // These tests reload panels repeatedly, which can take a longer time.
   this.timeout(20_000);
-
-  beforeEach(async () => {
-    await navigateToNetworkTab('empty.html');
-  });
 
   async function assertOption(select: ElementHandle<HTMLSelectElement>, expected: string) {
     assert.strictEqual(await select.evaluate(el => el.selectedOptions.length), 1);
     assert.strictEqual(await select.evaluate(el => el.selectedOptions[0].getAttribute('aria-label')), expected);
   }
 
-  it('can persist throttling conditions', async () => {
+  it('can persist throttling conditions', async ({devToolsPage, inspectedPage}) => {
+    await navigateToNetworkTab('empty.html', devToolsPage, inspectedPage);
     // Start with no throttling, select an option "A".
     {
-      const select = await waitForAria<HTMLSelectElement>('Throttling');
+      const select = await devToolsPage.waitForAria<HTMLSelectElement>('Throttling');
       await assertOption(select, 'Disabled: No throttling');
       await select.select('3G');
       await assertOption(select, 'Presets: 3G');
     }
     // Verify persistence for "A", select another option "B".
-    await reloadDevTools({queryParams: {panel: 'network'}});
+    await devToolsPage.reloadWithParams({panel: 'network'});
     {
-      const select = await waitForAria<HTMLSelectElement>('Throttling');
+      const select = await devToolsPage.waitForAria<HTMLSelectElement>('Throttling');
       await assertOption(select, 'Presets: 3G');
       await select.select('Slow 4G');
       await assertOption(select, 'Presets: Slow 4G');
     }
     // Verify persistence for "B", disable throttling.
-    await reloadDevTools({queryParams: {panel: 'network'}});
+    await devToolsPage.reloadWithParams({panel: 'network'});
     {
-      const select = await waitForAria<HTMLSelectElement>('Throttling');
+      const select = await devToolsPage.waitForAria<HTMLSelectElement>('Throttling');
       await assertOption(select, 'Presets: Slow 4G');
       await select.select('No throttling');
       await assertOption(select, 'Disabled: No throttling');
     }
     // Verify persistence of disabled throttling.
-    await reloadDevTools({queryParams: {panel: 'network'}});
+    await devToolsPage.reloadWithParams({panel: 'network'});
     {
-      const select = await waitForAria<HTMLSelectElement>('Throttling');
+      const select = await devToolsPage.waitForAria<HTMLSelectElement>('Throttling');
       await assertOption(select, 'Disabled: No throttling');
     }
   });

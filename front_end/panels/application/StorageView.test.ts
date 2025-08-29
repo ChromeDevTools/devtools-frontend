94a3c6c5f86e63562144068e3785ba072b4e4b07
Nikolay Vitkov <nvitkov@chromium.org>
2025-08-29 14:58:32 +0200

    Remove some type casts
    I forgot to address the nit for https://crrev.com/c/6897849
    So this resolves the comment
    
    Bug: none
    Change-Id: I83a34ad481a40f6dc8fced7d614a7bdd1570ee07
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6897733
    Commit-Queue: Ergün Erdoğmuş <ergunsh@chromium.org>
    Reviewed-by: Ergün Erdoğmuş <ergunsh@chromium.org>
    Auto-Submit: Nikolay Vitkov <nvitkov@chromium.org>

--- front_end/panels/application/StorageView.test.ts
+++ front_end/panels/application/StorageView.test.ts
@@ -25,9 +25,10 @@ describeWithMockConnection('StorageView', () => {
     domStorageModel = target.model(Resources.DOMStorageModel.DOMStorageModel);
     domStorageModel?.enable();
     storageKeyManager = target.model(SDK.StorageKeyManager.StorageKeyManager);
-    sinon.stub(target.networkAgent(), 'invoke_getCookies').returns(Promise.resolve({
-      cookies: [] as Protocol.Network.Cookie[],
-    } as Protocol.Network.GetCookiesResponse));
+    sinon.stub(target.networkAgent(), 'invoke_getCookies').resolves({
+      cookies: [],
+      getError: () => undefined,
+    });
   });
 
   expectConsoleLogs({

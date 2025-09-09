151c2235c740a456822abc80e374e91ca2f3b7ba
Ergun Erdogmus <ergunsh@chromium.org>
2025-09-09 18:32:20 +0200

    [GdpIntegration] Use `profile/u/me` instead of `profile/u/` only
    Bug: none
    Change-Id: I4ba64b1817e6086c2510c2be18f28eecbbe1985a
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6929597
    Commit-Queue: Nikolay Vitkov <nvitkov@chromium.org>
    Reviewed-by: Nikolay Vitkov <nvitkov@chromium.org>
    Auto-Submit: Ergün Erdoğmuş <ergunsh@chromium.org>
    Commit-Queue: Ergün Erdoğmuş <ergunsh@chromium.org>

--- front_end/core/host/GdpClient.ts
+++ front_end/core/host/GdpClient.ts
@@ -69,7 +69,7 @@ export interface Profile {
 }
 
 // TODO(crbug.com/441679275): Update once the API is enabled for prod.
-export const GOOGLE_DEVELOPER_PROGRAM_PROFILE_LINK = 'https://developers.devsite.corp.google.com/profile/u/';
+export const GOOGLE_DEVELOPER_PROGRAM_PROFILE_LINK = 'https://developers.devsite.corp.google.com/profile/u/me';
 
 async function makeHttpRequest<R extends object>(request: DispatchHttpRequestRequest): Promise<R|null> {
   if (!Root.Runtime.hostConfig.devToolsGdpProfiles?.enabled) {

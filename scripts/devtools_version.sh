737e9d902d5807ba090a3508d79465dc8ac18d10
Alex Rudenko <alexrudenko@chromium.org>
2025-09-25 14:10:47 +0200

    Fix the version calculation script
    Bug: none
    Change-Id: Ia3c24c2a1968ba85547e705e5bc24b5c11cc42f8
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/6983149
    Auto-Submit: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Jack Franklin <jacktfranklin@chromium.org>

--- scripts/devtools_version.sh
+++ scripts/devtools_version.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Copyright 2025 The Chromium Authors
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
 # This script outputs the current DevTools version (e.g., `v1.0.1509326`) for
 # GitHub Actions to apply as a git tag (see tag.yml).
 
@@ -12,7 +16,7 @@ chrome_version="$(cat DEPS | grep "'chrome'" | head -n1 | sed "s/[^0-9.]//g")"
 
 # Find most recent roll of chromium INTO devtools-frontend standalone.
 #   NOTE: this isn't exactly the same as when frontend was rolled into chromium. But.. it shouldn't make a huge difference for these purposes.. :)
-chromium_commit_position=$(curl "https://chromiumdash.appspot.com/fetch_releases?platform=Mac" --silent | jq --arg chrome_version "$chrome_version" '.[] | select(.version == $chrome_version).chromium_main_branch_position')
+chromium_commit_position=$(curl "https://chromiumdash.appspot.com/fetch_releases?platform=Linux,Mac" --silent | jq --arg chrome_version "$chrome_version" '.[] | select(.version == $chrome_version).chromium_main_branch_position')
 
 # verify we have a real number
 re='^[0-9]+$'

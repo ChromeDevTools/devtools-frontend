97166fbad3b0a140e9baa921d14beb328a8f8a76
Liviu Rau <liviurau@chromium.org>
2025-10-07 05:57:58 -0700

    Revert "Reland "Port test/perf to non-hosted""
    This reverts commit 34bd889a5f8c4cd4f33ec273ff040d72f0000e92.
    
    Reason for revert: the runner is timing out https://ci.chromium.org/ui/p/devtools-frontend/builders/ci/Stand-alone%20Linux/22381/overview
    
    Original change's description:
    > Reland "Port test/perf to non-hosted"
    >
    > This is a reland of commit fc6a60468a893699134928cbf631c6f258f497fd
    >
    > Renamed mocha_hooks.ts back to test_setup.ts. That way the contents of the file will be reset and it can be renamed later without the risk of mocha picking up old unsupported hooks.
    >
    > Original change's description:
    > > Port test/perf to non-hosted
    > >
    > > Bug: 448015235
    > > Change-Id: Ibbfe022bd6e708f6d45b4062120d1e5f30e3f410
    > > Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/7002908
    > > Reviewed-by: Paul Irish <paulirish@chromium.org>
    > > Commit-Queue: Liviu Rau <liviurau@chromium.org>
    >
    > Bug: 448015235
    > Change-Id: I10c3130444ca82c98b281e8b5ac11be56644af9f
    > Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/7016407
    > Reviewed-by: Jack Franklin <jacktfranklin@chromium.org>
    > Commit-Queue: Liviu Rau <liviurau@chromium.org>
    
    Bug: 448015235
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: Ifa1ad5c3d20cdfb3986b8f61fa97bc9afaf0d135
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/7016232
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Nikolay Vitkov <nvitkov@chromium.org>
    Commit-Queue: Liviu Rau <liviurau@chromium.org>

--- test/perf/setup/test_setup.ts
+++ test/perf/setup/test_setup.ts
@@ -1,14 +1,9 @@
-// Copyright 2025 The Chromium Authors
+// Copyright 2023 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import {clearResults, collectMeasurements, writeReport} from '../report/report.js';
-
-export async function mochaGlobalTeardown(this: Mocha.Suite) {
-  collectMeasurements();
+import {clearResults, writeReport} from '../report/report.js';
+after(() => {
   writeReport();
-}
-
-export async function mochaGlobalSetup(this: Mocha.Suite) {
   clearResults();
-}
+});

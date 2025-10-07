6e40b28d5a33bd0a597bfa2ba0d5388ccac58680
Piotr Paulski <piotrpaulski@chromium.org>
2025-10-07 16:56:49 +0000

    Improve console-context-selector_test by merging two helpers into one
    Modify `clickMenuAndWaitForItemWithText` to be able to search for
    multiple menu items, thus removing the need for separate helper
    that only looks for menu items within open menu.
    `waitForMenuItemWithText` is inferior and caused flakiness in the past
    due to its inability to react to menu being closed due to background
    processing in the panel.
    
    Bug: 430774159
    Change-Id: I12de704170d916f4ef094882de1a638e0c792fc1
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/7013470
    Reviewed-by: Nikolay Vitkov <nvitkov@chromium.org>
    Commit-Queue: Piotr Paulski <piotrpaulski@chromium.org>

--- test/e2e_non_hosted/console/console-context-selector_test.ts
+++ test/e2e_non_hosted/console/console-context-selector_test.ts
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 import {assert} from 'chai';
+import type * as puppeteer from 'puppeteer-core';
 
 import {navigateToConsoleTab} from '../../e2e/helpers/console-helpers.js';
 import {openSourcesPanel, RESUME_BUTTON} from '../../e2e/helpers/sources-helpers.js';
@@ -15,30 +16,33 @@ describe('The Console Tab', () => {
     await inspectedPage.evaluate('setup()');
     await devToolsPage.waitFor('[aria-label="JavaScript context: top"]');
 
-    async function waitForMenuItemWithText(text: string, devToolsPage: DevToolsPage) {
-      const result = await devToolsPage.waitForFunction(async () => {
-        for (const menuItem of await devToolsPage.waitForMany('[role=menuitem]', 1)) {
-          if (await devToolsPage.$textContent(text, menuItem)) {
-            return menuItem;
-          }
+    async function findMenuItemWithText(text: string, menuItems: Array<puppeteer.ElementHandle<Element>>) {
+      for (const menuItem of menuItems) {
+        if (await devToolsPage.$textContent(text, menuItem)) {
+          return menuItem;
         }
-        return null;
-      });
-      assert.exists(result);
-      return result;
+      }
+      return null;
     }
 
-    async function clickMenuAndWaitForItemWithText(menuSelector: string, itemText: string, devToolsPage: DevToolsPage) {
+    async function clickMenuAndWaitForItemsWithText(
+        menuSelector: string, itemLabels: string[], devToolsPage: DevToolsPage) {
       const result = await devToolsPage.waitForFunction(async () => {
         await devToolsPage.click(menuSelector);
-        for (const menuItem of await devToolsPage.waitForManyWithTries(
-                 '[role=menuitem]', 1, 3, undefined, undefined, undefined) ??
-             []) {
-          if (await devToolsPage.$textContent(itemText, menuItem)) {
-            return menuItem;
-          }
+        const menuItems =
+            await devToolsPage.waitForManyWithTries('[role=menuitem]', 1, 3, undefined, undefined, undefined);
+        if (!menuItems) {
+          return null;
         }
-        return null;
+        const selectedItems: Array<puppeteer.ElementHandle<Element>> = [];
+        for (const itemText of itemLabels) {
+          const itemForLabel = await findMenuItemWithText(itemText, menuItems);
+          if (!itemForLabel) {
+            return null;
+          }
+          selectedItems.push(itemForLabel);
+        }
+        return selectedItems;
       });
       assert.exists(result);
       return result;
@@ -47,8 +51,8 @@ describe('The Console Tab', () => {
     const FRAME_CONTEXT_LABEL = 'myframe (context-selector-inner.html)';
 
     // switch to the iframe context
-    const frameMenuItem = await clickMenuAndWaitForItemWithText(
-        '[aria-label="JavaScript context: top"]', FRAME_CONTEXT_LABEL, devToolsPage);
+    const [frameMenuItem] = await clickMenuAndWaitForItemsWithText(
+        '[aria-label="JavaScript context: top"]', [FRAME_CONTEXT_LABEL], devToolsPage);
     await frameMenuItem.click();
     await devToolsPage.pressKey('Enter');
     await devToolsPage.waitFor(`[aria-label="JavaScript context: ${FRAME_CONTEXT_LABEL}"]`);
@@ -57,8 +61,8 @@ describe('The Console Tab', () => {
     void inspectedPage.evaluate('pauseInMain()');
     await devToolsPage.waitFor('#tab-sources[aria-selected="true"]');
     await navigateToConsoleTab(devToolsPage);
-    const disabledFrameMenuItem = await clickMenuAndWaitForItemWithText(
-        '[aria-label="JavaScript context: top"]', FRAME_CONTEXT_LABEL, devToolsPage);
+    const [disabledFrameMenuItem] = await clickMenuAndWaitForItemsWithText(
+        '[aria-label="JavaScript context: top"]', [FRAME_CONTEXT_LABEL], devToolsPage);
     assert.isTrue(await disabledFrameMenuItem.evaluate(node => node.classList.contains('disabled')));
     await devToolsPage.pressKey('Escape');
 
@@ -67,8 +71,8 @@ describe('The Console Tab', () => {
     await devToolsPage.click(RESUME_BUTTON);
     await navigateToConsoleTab(devToolsPage);
 
-    const enabledFrameMenuItem = await clickMenuAndWaitForItemWithText(
-        '[aria-label="JavaScript context: top"]', FRAME_CONTEXT_LABEL, devToolsPage);
+    const [enabledFrameMenuItem] = await clickMenuAndWaitForItemsWithText(
+        '[aria-label="JavaScript context: top"]', [FRAME_CONTEXT_LABEL], devToolsPage);
     assert.isFalse(await enabledFrameMenuItem.evaluate(node => node.classList.contains('disabled')));
     await devToolsPage.pressKey('Escape');
 
@@ -78,10 +82,9 @@ describe('The Console Tab', () => {
     await devToolsPage.waitFor('#tab-sources[aria-selected="true"]');
     await navigateToConsoleTab(devToolsPage);
 
-    const topMenuItem = await clickMenuAndWaitForItemWithText(
-        `[aria-label="JavaScript context: ${WORKER_CONTEXT_LABEL}"]`, 'top', devToolsPage);
+    const [topMenuItem, workerFrameMenuItem] = await clickMenuAndWaitForItemsWithText(
+        `[aria-label="JavaScript context: ${WORKER_CONTEXT_LABEL}"]`, ['top', FRAME_CONTEXT_LABEL], devToolsPage);
     assert.isFalse(await topMenuItem.evaluate(node => node.classList.contains('disabled')));
-    const workerFrameMenuItem = await waitForMenuItemWithText(FRAME_CONTEXT_LABEL, devToolsPage);
     assert.isFalse(await workerFrameMenuItem.evaluate(node => node.classList.contains('disabled')));
     await devToolsPage.pressKey('Escape');
 
@@ -97,8 +100,8 @@ describe('The Console Tab', () => {
     await devToolsPage.waitFor('#tab-sources[aria-selected="true"]');
     await navigateToConsoleTab(devToolsPage);
 
-    const disabledTopMenuItem = await clickMenuAndWaitForItemWithText(
-        `[aria-label="JavaScript context: ${FRAME_CONTEXT_LABEL}"]`, 'top', devToolsPage);
+    const [disabledTopMenuItem] = await clickMenuAndWaitForItemsWithText(
+        `[aria-label="JavaScript context: ${FRAME_CONTEXT_LABEL}"]`, ['top'], devToolsPage);
     assert.isTrue(await disabledTopMenuItem.evaluate(node => node.classList.contains('disabled')));
     await devToolsPage.pressKey('Escape');
 
@@ -107,8 +110,8 @@ describe('The Console Tab', () => {
     await devToolsPage.click(RESUME_BUTTON);
     await navigateToConsoleTab(devToolsPage);
 
-    const enabledTopMenuItem = await clickMenuAndWaitForItemWithText(
-        `[aria-label="JavaScript context: ${FRAME_CONTEXT_LABEL}"]`, 'top', devToolsPage);
+    const [enabledTopMenuItem] = await clickMenuAndWaitForItemsWithText(
+        `[aria-label="JavaScript context: ${FRAME_CONTEXT_LABEL}"]`, ['top'], devToolsPage);
     assert.isFalse(await enabledTopMenuItem.evaluate(node => node.classList.contains('disabled')));
     await devToolsPage.pressKey('Escape');
   });

7aa11eabfafbf18d8b41e7211a061f0293768c5f
Alex Rudenko <alexrudenko@chromium.org>
2025-10-28 11:36:19 +0100

    Expose i18n to MCP
    We would need to instantiate a DevTools locale to use
    classes with i18n.
    
    Bug: 451502260
    Change-Id: Ib42f49e5ff76c6921618583e97da1c644fa09872
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/7090576
    Reviewed-by: Natallia Harshunova <nharshunova@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>

--- mcp/mcp.ts
+++ mcp/mcp.ts
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+export * as I18n from '../front_end/core/i18n/i18n.js';
 export {ConnectionTransport} from '../front_end/core/protocol_client/ConnectionTransport.js';
 export {
   PerformanceInsightFormatter

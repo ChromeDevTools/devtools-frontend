d3cb82c65b5ab8ff99a85362c7e3f0362ddae08b
Kim-Anh Tran <kimanh@chromium.org>
2025-10-30 08:56:56 +0100

    [docs] Move Spinner docs to static documentation
    This moves the spinner doc to be hosted on
    https://chromedevtools.github.io/devtools-frontend/
    
    Screenshot: https://b.corp.google.com/issues/414332459#comment10
    Bug: 414332459
    Change-Id: I9c704c3f1aef12d41568193c66e8be168517954b
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/7100558
    Auto-Submit: Kim-Anh Tran <kimanh@chromium.org>
    Reviewed-by: Samiya Caur <samiyac@chromium.org>
    Commit-Queue: Kim-Anh Tran <kimanh@chromium.org>

---
+++ front_end/ui/components/spinners/Spinners.docs.ts
@@ -0,0 +1,13 @@
+// Copyright 2025 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import './spinners.js';
+
+import * as Lit from '../../lit/lit.js';
+
+const {html} = Lit;
+
+export async function render(container: HTMLElement) {
+  Lit.render(html`<devtools-spinner></devtools-spinner>`, container);
+}

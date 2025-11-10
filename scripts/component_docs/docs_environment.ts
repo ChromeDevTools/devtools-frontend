dab5aa5bc0c1060dec2b539e2f40f0942e921c27
Kim-Anh Tran <kimanh@chromium.org>
2025-11-10 10:37:00 +0100

    [docs] Migrate Markdown* docs to static documentation
    The image/link markdown docs aren't needed, as they are already
    included in the main MarkdownView docs.
    
    Also include Images and ThemeSupport dependency.
    
    Screenshot: crbug.com/414332459#comment51
    Bug: 414332459
    Change-Id: I045d69d0152292dabc2cfa4a6ab45a978dd955fe
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/7124604
    Commit-Queue: Kim-Anh Tran <kimanh@chromium.org>
    Reviewed-by: Simon Zünd <szuend@chromium.org>

--- scripts/component_docs/docs_environment.ts
+++ scripts/component_docs/docs_environment.ts
@@ -2,10 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import * as Common from '../../front_end/core/common/common.js';
 import * as FrontendHelpers from '../../front_end/testing/EnvironmentHelpers.js';
+import * as ThemeSupport from '../../front_end/ui/legacy/theme_support/theme_support.js';
 
-export function setup(): Promise<void> {
-  return FrontendHelpers.initializeGlobalVars();
+export async function setup(): Promise<void> {
+  await FrontendHelpers.initializeGlobalVars();
+  const setting = Common.Settings.Settings.instance().moduleSetting('ui-theme');
+  ThemeSupport.ThemeSupport.instance({forceNew: true, setting});
 }
 
 export function cleanup(): Promise<void> {

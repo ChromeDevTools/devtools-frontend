0117805c961fd556515972b79a7042408d6fd036
Philip Pfaffe <pfaffe@chromium.org>
2025-11-10 23:42:28 -0800

    Revert "[cleanup] Move rebuild for test script parts"
    This reverts commit 7787f61f6b00e8704677f5a69bee6115f81068c8.
    
    Reason for revert: This broke running tests in a chromium tree.
    
    Original change's description:
    > [cleanup] Move rebuild for test script parts
    >
    > Remove rebuilding logic from generated file and move it up to the `run_on_target.mjs`, this ensures that we get the correct generated file we want to run via the command.
    >
    > Bug: none
    > Change-Id: I7295ebaa5e53b80011387c4717e2eca6d10f6d8c
    > Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/7107198
    > Commit-Queue: Nikolay Vitkov <nvitkov@chromium.org>
    > Reviewed-by: Philip Pfaffe <pfaffe@chromium.org>
    > Reviewed-by: Benedikt Meurer <bmeurer@chromium.org>
    
    Bug: none
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I2b5d894029d3926b40863b6f63ebf5f4c2931019
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/7137842
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Auto-Submit: Philip Pfaffe <pfaffe@chromium.org>

--- scripts/devtools_build.mjs
+++ scripts/devtools_build.mjs
@@ -415,27 +415,23 @@ async function computeBuildTargetsForFiles(target, filenames) {
 
 /**
  * @param {string} target
- * @param {{ filenames?: string[], signal?: AbortSignal, extraBuildTargets?: string[]}} options
+ * @param {{ filenames?: string[], signal?: AbortSignal}} options
  * @returns a `BuildResult` with statistics for the build.
  */
 export async function build(target, options = {}) {
   const startTime = performance.now();
   const outDir = path.join(rootPath(), 'out', target);
-  const extraBuildTargets = options.extraBuildTargets ?? [];
 
   // Build just the devtools-frontend resources in |outDir|. This is important
   // since we might be running in a full Chromium checkout and certainly don't
   // want to build all of Chromium first.
-  const buildTargets = await computeBuildTargetsForFiles(target, options.filenames);
+  const buildTargets = await computeBuildTargetsForFiles(
+      target,
+      options.filenames,
+  );
   try {
     const autoninjaExe = vpython3ExecutablePath();
-    const autoninjaArgs = [
-      autoninjaPyPath(),
-      '-C',
-      outDir,
-      ...buildTargets,
-      ...extraBuildTargets,
-    ];
+    const autoninjaArgs = [autoninjaPyPath(), '-C', outDir, ...buildTargets];
     await spawn(autoninjaExe, autoninjaArgs, {
       shell: true,
       signal: options.signal,
--- scripts/run_on_target.mjs
+++ scripts/run_on_target.mjs
@@ -4,43 +4,33 @@
 
 import * as childProcess from 'child_process';
 import * as fs from 'fs';
-import ora from 'ora';
 import * as path from 'path';
 import yargs from 'yargs';
-import { hideBin } from 'yargs/helpers';
+import unparse from 'yargs-unparser';
 
-import { build, prepareBuild } from './devtools_build.mjs';
-import { isInChromiumDirectory } from './devtools_paths.js';
 import { ENV, getEnvString } from './env-utils.mjs';
 
-const options = yargs(hideBin(process.argv))
+const argv = yargs(process.argv.slice(2))
   .parserConfiguration({
-    // Populate the ['_'] with unknown
-    'unknown-options-as-args': true,
+    'strip-aliased': true,
   })
-  .command(
-    '$0 [script]',
-    'Run any script that is generated inside out/<target>/gen',
-  )
-  .options('skip-ninja', {
-    type: 'boolean',
-    default: false,
-    desc: 'Skip rebuilding',
-  })
+  .command('$0 [script]')
   .option('target', {
     alias: 't',
     type: 'string',
-    default: getEnvString(ENV.TARGET, 'Default'),
+    default: getEnvString(ENV.TARGET, 'Default')
   })
   .help(false)
   .version(false)
   .parseSync();
 
-const target = options.target;
-/** @type {string} */
-let script = options.script;
+const target = argv.target;
+let script = argv.script;
 
-let sourceRoot = path.dirname(path.dirname(path.resolve(options['$0'])));
+delete argv.target;
+delete argv.script;
+
+let sourceRoot = path.dirname(path.dirname(path.resolve(argv['$0'])));
 
 // Ensure that we can find the node_modules folder even if the out folder is
 // not a sibling of the node_modules folder.
@@ -80,33 +70,25 @@ if (
   process.exit(1);
 }
 const scriptPath = path.resolve(cwd, script);
-
-if (!options['skip-ninja']) {
-  if (!fs.existsSync(scriptPath)) {
-    console.error(`Script path ${scriptPath} does not exist, trying ninja...`);
-    // Prepare the build target if not initialized.
-    prepareBuild(target);
+if (!fs.existsSync(scriptPath)) {
+  console.error(`Script path ${scriptPath} does not exist, trying ninja...`);
+  const { error, status } = childProcess.spawnSync(
+    'autoninja',
+    ['-C', cwd, script],
+    { stdio: 'inherit', cwd: sourceRoot },
+  );
+  if (error) {
+    throw error;
   }
-
-  const spinner = ora('Rebuilding...').start();
-  const extraBuildTargets = isInChromiumDirectory().isInChromium
-    ? ['chrome']
-    : [];
-
-  await build(target, {
-    extraBuildTargets,
-  });
-  spinner.clear();
+  if (status) {
+    process.exit(status);
+  }
 }
 
 const { argv0 } = process;
-
 const { status } = childProcess.spawnSync(
   argv0,
-  [scriptPath, ...options['_'].map(String)],
-  {
-    stdio: 'inherit',
-    env,
-  },
+  [scriptPath, ...unparse(argv)],
+  { stdio: 'inherit', env },
 );
 process.exit(status);

fb221c202f42beebd5910367432b35edf40263dd
Kim-Anh Tran <kimanh@chromium.org>
2025-11-11 09:45:43 +0100

    [docs] Migrate ExpandableList to static documentation
    Bypass-Check-License: Moving docs
    Screenshot: crbug.com/414332459#comment54
    Bug: 414332459
    Change-Id: Id7158b6b3b1c0d2b0b234eef5bd006ae45ec234c
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/7137681
    Commit-Queue: Kim-Anh Tran <kimanh@chromium.org>
    Auto-Submit: Kim-Anh Tran <kimanh@chromium.org>
    Reviewed-by: Wolfgang Beyer <wolfi@chromium.org>

---
+++ front_end/ui/components/expandable_list/ExpandableList.docs.ts
@@ -0,0 +1,30 @@
+// Copyright 2021 The Chromium Authors
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import * as Lit from '../../lit/lit.js';
+
+import {ExpandableList} from './expandable_list.js';
+
+const {html} = Lit;
+
+export function render(container: HTMLElement): void {
+  const component = new ExpandableList.ExpandableList();
+
+  const rows = [];
+  rows.push(html`
+    <div>This is row 1. Click on the triangle icon to expand.</div>
+  `);
+
+  for (let rowNumber = 2; rowNumber < 11; rowNumber++) {
+    rows.push(html`
+      <div>This is row ${rowNumber}.</div>
+    `);
+  }
+
+  component.data = {
+    rows,
+  };
+
+  container.appendChild(component);
+}

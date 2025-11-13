48b90d7ba6ef61719ec9e7685d6bd9662318872d
Simon Zünd <szuend@chromium.org>
2025-11-13 14:55:48 +0100

    Create *WorkspaceBinding instances in Universe ctor instead of MainImpl
    R=nvitkov@chromium.org
    
    Bug: 458180550
    Change-Id: Ia5db349e465158661bc2a25c08e200df0e79b304
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/7150846
    Reviewed-by: Nikolay Vitkov <nvitkov@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Nikolay Vitkov <nvitkov@chromium.org>
    Auto-Submit: Simon Zünd <szuend@chromium.org>

--- front_end/entrypoints/main/MainImpl.ts
+++ front_end/entrypoints/main/MainImpl.ts
@@ -448,23 +448,7 @@ export class MainImpl {
     Workspace.FileManager.FileManager.instance({forceNew: true});
 
     Bindings.NetworkProject.NetworkProjectManager.instance();
-    const resourceMapping = new Bindings.ResourceMapping.ResourceMapping(
-        targetManager,
-        Workspace.Workspace.WorkspaceImpl.instance(),
-    );
     new Bindings.PresentationConsoleMessageHelper.PresentationConsoleMessageManager();
-    Bindings.CSSWorkspaceBinding.CSSWorkspaceBinding.instance({
-      forceNew: true,
-      resourceMapping,
-      targetManager,
-    });
-    Bindings.DebuggerWorkspaceBinding.DebuggerWorkspaceBinding.instance({
-      forceNew: true,
-      resourceMapping,
-      targetManager,
-      ignoreListManager: Workspace.IgnoreListManager.IgnoreListManager.instance(),
-      workspace: Workspace.Workspace.WorkspaceImpl.instance(),
-    });
     targetManager.setScopeTarget(targetManager.primaryPageTarget());
     UI.Context.Context.instance().addFlavorChangeListener(SDK.Target.Target, ({data}) => {
       const outermostTarget = data?.outermostTarget();

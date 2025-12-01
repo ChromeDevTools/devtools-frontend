e9b10acac3d066e580b66b6442000d43387692f0
Simon Zünd <szuend@chromium.org>
2025-12-01 10:37:13 +0100

    [object_ui] Split 'evaluateAndBuildPreview' in JavaScriptREPL
    This CL splits the 'evaluate' part from the 'evaluateAndBuildPreview'.
    We want to disentangle business logic from UI logic in
    ConsolePinPane.ts and having a method that does both eval plus
    presentation makes this harder.
    
    Note that we pass the `ExecutionContext` to `evaluate` on purpose:
    This way, the function has no dependency on UI whatsoever and we could
    potentially move this code lower in our layering (e.g. to 'models/').
    
    R=pfaffe@chromium.org
    
    Bug: 457388389,407750444
    Change-Id: I85ac703679a4df9590fc81430b1de0e4f1a811a2
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/7208995
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Philip Pfaffe <pfaffe@chromium.org>

--- front_end/ui/legacy/components/object_ui/JavaScriptREPL.ts
+++ front_end/ui/legacy/components/object_ui/JavaScriptREPL.ts
@@ -2,12 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import * as Platform from '../../../../core/platform/platform.js';
 import * as SDK from '../../../../core/sdk/sdk.js';
 import * as Formatter from '../../../../models/formatter/formatter.js';
 import * as SourceMapScopes from '../../../../models/source_map_scopes/source_map_scopes.js';
 import * as Acorn from '../../../../third_party/acorn/acorn.js';
-import {render} from '../../../lit/lit.js';
 import * as UI from '../../legacy.js';
 
 import {RemoteObjectPreviewFormatter} from './RemoteObjectPreviewFormatter.js';
@@ -46,16 +44,13 @@ export class JavaScriptREPL {
     }
   }
 
-  static async evaluateAndBuildPreview(
-      text: string, throwOnSideEffect: boolean, replMode: boolean, timeout?: number, allowErrors?: boolean,
-      objectGroup?: string, awaitPromise = false, silent = false): Promise<{
-    preview: DocumentFragment,
-    result: SDK.RuntimeModel.EvaluationResult|null,
-  }> {
-    const executionContext = UI.Context.Context.instance().flavor(SDK.RuntimeModel.ExecutionContext);
+  static async evaluate(
+      text: string, executionContext: SDK.RuntimeModel.ExecutionContext, throwOnSideEffect: boolean, replMode: boolean,
+      timeout?: number, objectGroup?: string, awaitPromise = false,
+      silent = false): Promise<SDK.RuntimeModel.EvaluationResult|null> {
     const isTextLong = text.length > maxLengthForEvaluation;
-    if (!text || !executionContext || (throwOnSideEffect && isTextLong)) {
-      return {preview: document.createDocumentFragment(), result: null};
+    if (!text || (throwOnSideEffect && isTextLong)) {
+      return null;
     }
 
     let expression = text;
@@ -81,39 +76,29 @@ export class JavaScriptREPL {
       replMode,
       silent,
     };
-    const result = await executionContext.evaluate(options, false /* userGesture */, awaitPromise);
-    const preview = JavaScriptREPL.buildEvaluationPreview(result, allowErrors);
-    return {preview, result};
+    return await executionContext.evaluate(options, false /* userGesture */, awaitPromise);
   }
 
-  private static buildEvaluationPreview(result: SDK.RuntimeModel.EvaluationResult, allowErrors?: boolean):
-      DocumentFragment {
-    const fragment = document.createDocumentFragment();
-    if ('error' in result) {
-      return fragment;
+  static async evaluateAndBuildPreview(
+      text: string, throwOnSideEffect: boolean, replMode: boolean, timeout?: number, allowErrors?: boolean,
+      objectGroup?: string, awaitPromise = false, silent = false): Promise<{
+    preview: DocumentFragment,
+    result: SDK.RuntimeModel.EvaluationResult|null,
+  }> {
+    const executionContext = UI.Context.Context.instance().flavor(SDK.RuntimeModel.ExecutionContext);
+    if (!executionContext) {
+      return {preview: document.createDocumentFragment(), result: null};
     }
 
-    if (result.exceptionDetails?.exception?.description) {
-      const exception = result.exceptionDetails.exception.description;
-      if (exception.startsWith('TypeError: ') || allowErrors) {
-        fragment.createChild('span').textContent = result.exceptionDetails.text + ' ' + exception;
-      }
-      return fragment;
+    const result = await JavaScriptREPL.evaluate(
+        text, executionContext, throwOnSideEffect, replMode, timeout, objectGroup, awaitPromise, silent);
+    if (!result) {
+      return {preview: document.createDocumentFragment(), result: null};
     }
 
     const formatter = new RemoteObjectPreviewFormatter();
-    const {preview, type, subtype, className, description} = result.object;
-    if (preview && type === 'object' && subtype !== 'node' && subtype !== 'trustedtype') {
-      /* eslint-disable-next-line  @devtools/no-lit-render-outside-of-view */
-      render(formatter.renderObjectPreview(preview), fragment);
-    } else {
-      /* eslint-disable-next-line  @devtools/no-lit-render-outside-of-view */
-      render(
-          formatter.renderPropertyPreview(
-              type, subtype, className, Platform.StringUtilities.trimEndWithMaxLength(description || '', 400)),
-          fragment);
-    }
-    return fragment;
+    const preview = formatter.renderEvaluationResultPreview(result, allowErrors);
+    return {preview, result};
   }
 }
 

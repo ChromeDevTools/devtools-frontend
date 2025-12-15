fe5b903dc1c132c9f521f0410417240b9a373bb1
copybara-service <copybara-service@users.noreply.github.com>
2025-12-15 07:15:51 +0000

    deploy: 85a1104be9df22fec101665625ee502c9fdf26ee

--- front_end/panels/issues/AffectedResourcesView.js
+++ front_end/panels/issues/AffectedResourcesView.js
@@ -198,7 +198,6 @@ export class AffectedResourcesView extends UI.TreeOutline.TreeElement {
         sourceCodeLocation.classList.add('affected-source-location');
         if (sourceLocation) {
             const maxLengthForDisplayedURLs = 40; // Same as console messages.
-            // TODO(crbug.com/1108503): Add some mechanism to be able to add telemetry to this element.
             const linkifier = new Components.Linkifier.Linkifier(maxLengthForDisplayedURLs);
             const sourceAnchor = linkifier.linkifyScriptLocation(target || null, sourceLocation.scriptId || null, sourceLocation.url, sourceLocation.lineNumber, { columnNumber: sourceLocation.columnNumber, inlineFrameIndex: 0 });
             sourceAnchor.setAttribute('jslog', `${VisualLogging.link('source-location').track({ click: true })}`);

2225cf6ce3af9486189c03ff05952434054c7ade
Danil Somsikov <dsv@chromium.org>
2025-12-16 08:02:54 -0800

    Fix the render blocking request insight
    Bug: 407941514
    Change-Id: I1308d9b66fc36a478fb5dd0f2520d90f2b692280
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/7265292
    Auto-Submit: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Danil Somsikov <dsv@chromium.org>
    Reviewed-by: Connor Clark <cjamcl@chromium.org>

--- front_end/panels/timeline/components/insights/RenderBlocking.ts
+++ front_end/panels/timeline/components/insights/RenderBlocking.ts
@@ -5,15 +5,17 @@
 import * as i18n from '../../../../core/i18n/i18n.js';
 import type {RenderBlockingInsightModel} from '../../../../models/trace/insights/RenderBlocking.js';
 import * as Trace from '../../../../models/trace/trace.js';
+import * as UI from '../../../../ui/legacy/legacy.js';
 import * as Lit from '../../../../ui/lit/lit.js';
 
 import {BaseInsightComponent} from './BaseInsightComponent.js';
 import {eventRef} from './EventRef.js';
-import {createLimitedRows, renderOthersLabel, type TableDataRow} from './Table.js';
+import {createLimitedRows, renderOthersLabel, Table, type TableDataRow} from './Table.js';
 
 const {UIStrings, i18nString, createOverlayForRequest} = Trace.Insights.Models.RenderBlocking;
 
 const {html} = Lit;
+const {widgetConfig} = UI.Widget;
 
 export class RenderBlocking extends BaseInsightComponent<RenderBlockingInsightModel> {
   override internalName = 'render-blocking-requests';
@@ -58,12 +60,12 @@ export class RenderBlocking extends BaseInsightComponent<RenderBlockingInsightMo
     // clang-format off
     return html`
       <div class="insight-section">
-        <devtools-widget
-          .data=${{
+        <devtools-widget .widgetConfig=${widgetConfig(Table, {
+           data: {
             insight: this,
             headers: [i18nString(UIStrings.renderBlockingRequest), i18nString(UIStrings.duration)],
             rows,
-          }}>
+          }})}>
         </devtools-widget>
       </div>
     `;

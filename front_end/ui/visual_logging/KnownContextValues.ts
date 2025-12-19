751b2a8dc75c8f2f90efb6fa5bf63ad4e663f821
“jalonthomas” <jalonthomas@google.com>
2025-12-19 11:56:46 -0500

    Add deprecation message for 'Privacy' section of the 'Privacy & Security' panel
    The first step toward deprecating the 'Privacy' section to align with
    Chrome's decision to maintain its current approach to user choice for
    third-party cookies.
    
    Mocks can be found under 'User Notification' in this deprecation plan
    doc:
    http://docs/document/d/1-NuVEYMME-Xla0BY4BgbCRHPnqko_Ecf8O--zwXi6ug?resourcekey=0-y4svRJeCuNQcrfxw0OkmdA&tab=t.0#heading=h.ke47sslzyi8w
    
    Bug:462017663
    Change-Id: I952d18417f6982f4306a467a4575d47300172f7f
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/7275937
    Reviewed-by: Paul Irish <paulirish@chromium.org>
    Reviewed-by: Peter Müller <petermueller@chromium.org>
    Commit-Queue: Jalon Thomas <jalonthomas@google.com>

--- front_end/ui/visual_logging/KnownContextValues.ts
+++ front_end/ui/visual_logging/KnownContextValues.ts
@@ -2969,6 +2969,7 @@ export const knownContextValues = new Set([
   'privacy-notice',
   'privacy-policy',
   'privacy-policy.console-insights',
+  'privacy-sandbox-update',
   'private-state-tokens',
   'production-origin',
   'profile-loading-failed',

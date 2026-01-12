eaf53930b880b6aacf2e317ae716cf5851b4d1af
Philip Pfaffe <pfaffe@chromium.org>
2026-01-12 16:01:37 +0000

    Add an e2e test for global network conditions
    Bug: 464987273
    Change-Id: I8d83046bf66e1142694ddf006f20ee03f583d411
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/7451019
    Reviewed-by: Eric Leese <leese@chromium.org>
    Commit-Queue: Philip Pfaffe <pfaffe@chromium.org>
    Commit-Queue: Eric Leese <leese@chromium.org>
    Auto-Submit: Philip Pfaffe <pfaffe@chromium.org>

--- test/e2e/network/network-throttle-persist.test.ts
+++ test/e2e/network/network-throttle-persist.test.ts
@@ -5,18 +5,39 @@
 import {assert} from 'chai';
 import type {ElementHandle} from 'puppeteer-core';
 
-import {navigateToNetworkTab} from '../helpers/network-helpers.js';
+import {navigateToNetworkTab, waitForSomeRequestsToAppear} from '../helpers/network-helpers.js';
 
 describe('The Network Tab', function() {
-  // These tests reload panels repeatedly, which can take a longer time.
-  this.timeout(20_000);
-
   async function assertOption(select: ElementHandle<HTMLSelectElement>, expected: string) {
     assert.strictEqual(await select.evaluate(el => el.selectedOptions.length), 1);
     assert.strictEqual(await select.evaluate(el => el.selectedOptions[0].getAttribute('aria-label')), expected);
   }
 
+  it('can throttle requests', async ({devToolsPage, inspectedPage}) => {
+    await navigateToNetworkTab('hello.html', devToolsPage, inspectedPage);
+    await waitForSomeRequestsToAppear(1, devToolsPage);
+
+    const select = await devToolsPage.waitForAria<HTMLSelectElement>('Throttling');
+    await assertOption(select, 'Disabled: No throttling');
+    await select.select('Offline');
+    await assertOption(select, 'Presets: Offline');
+
+    await inspectedPage.reload().catch(() => {});
+
+    await inspectedPage.waitForElementWithTextContent('No internet');
+
+    await waitForSomeRequestsToAppear(1, devToolsPage);
+
+    const name = await devToolsPage.waitFor<HTMLElement>('.network-log-grid .network-error-row .name-column');
+    const status = await devToolsPage.waitFor<HTMLElement>('.network-log-grid .network-error-row .status-column');
+    assert.strictEqual('hello.html', await name.evaluate(e => e.innerText));
+    assert.strictEqual('(failed)net::ERR_INTERNET_DISCONNECTED', await status.evaluate(e => e.innerText));
+  });
+
   it('can persist throttling conditions', async ({devToolsPage, inspectedPage}) => {
+    // This test reload panels repeatedly, which can take a longer time.
+    this.timeout(20_000);
+
     await navigateToNetworkTab('empty.html', devToolsPage, inspectedPage);
     // Start with no throttling, select an option "A".
     {

b0bbc8c82066f881d17cb94cbb06fd72d70551ff
Jack Franklin <jacktfranklin@chromium.org>
2026-01-22 12:41:03 +0000

    Add JSDocs to ExpectStubCall methods
    It took me a minute to look through the code and understand the
    variations and when each particular method is useful. These comments
    should help future people :)
    
    R=pfaffe@chromium.org
    
    Bug: none
    Change-Id: I429c6e620b0107a37ab1e14de5b94ff0f17e94f9
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/7511004
    Commit-Queue: Jack Franklin <jacktfranklin@chromium.org>
    Reviewed-by: Philip Pfaffe <pfaffe@chromium.org>
    Auto-Submit: Jack Franklin <jacktfranklin@chromium.org>
    Commit-Queue: Philip Pfaffe <pfaffe@chromium.org>

--- front_end/testing/ExpectStubCall.ts
+++ front_end/testing/ExpectStubCall.ts
@@ -2,6 +2,17 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+/**
+ * Returns a Promise that resolves with the arguments of the `stub`'s call once
+ * it has been called `callCount` times.
+ * If `fakeFn` is provided, it will be used as the fake implementation for the stub.
+ *
+ * @param stub The Sinon stub to observe.
+ * @param options An object that can contain:
+ *   - `fakeFn`: An optional function to use as the fake implementation of the stub.
+ *   - `callCount`: The number of times the stub should be called before the Promise resolves. Defaults to 1.
+ * @returns A Promise that resolves with the arguments of the `stub`'s last call.
+ */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function expectCall<TArgs extends any[] = any[], TReturnValue = any>(
     stub: sinon.SinonStub<TArgs, TReturnValue>,
@@ -17,6 +28,20 @@ export function expectCall<TArgs extends any[] = any[], TReturnValue = any>(
   });
 }
 
+/**
+ * Returns a Promise that resolves with the arguments of the `stub`'s call once
+ * it has been called `callCount` times.
+ * If the `stub` has already been called `callCount` times or more, the Promise
+ * resolves immediately. This means you don't have to call this before the stub
+ * might be called.
+ * If `fakeFn` is provided, it will be used as the fake implementation for the stub.
+ *
+ * @param stub The Sinon stub to observe.
+ * @param options An object that can contain:
+ *   - `fakeFn`: An optional function to use as the fake implementation of the stub.
+ *   - `callCount`: The number of times the stub should be called before the Promise resolves. Defaults to 1.
+ * @returns A Promise that resolves with the arguments passed to the `stub` on its last call.
+ */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function expectCalled<TArgs extends any[] = any[], TReturnValue = any>(
     stub: sinon.SinonStub<TArgs, TReturnValue>,
@@ -31,6 +56,15 @@ export function expectCalled<TArgs extends any[] = any[], TReturnValue = any>(
 type Args<T> = T extends(...args: infer TArgs) => unknown ? TArgs : never;
 type Ret<T> = T extends(...args: any[]) => infer TRet ? TRet : never;
 
+/**
+ * Spies on a method of an object and returns a Promise that resolves with the
+ * arguments and result of the method's call.
+ * The original method is restored after the first call.
+ *
+ * @param obj The object to spy on.
+ * @param method The name of the method to spy on.
+ * @returns A Promise that resolves with an object containing the `args` and `result` of the method call.
+ */
 export function spyCall<T, Fn extends keyof T>(obj: T, method: Fn): Promise<{args: Args<T[Fn]>, result: Ret<T[Fn]>}> {
   const {promise, resolve} = Promise.withResolvers<{args: Args<T[Fn]>, result: Ret<T[Fn]>}>();
 

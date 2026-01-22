6e1ebd618212edde10b13db3c4195ee68d11eb9a
Alex Rudenko <alexrudenko@chromium.org>
2026-01-22 15:10:54 +0100

    Remove obsolete eslint disable stmt
    Bug: none
    Change-Id: I4000c634ec2d7c4e012966a2f7fe26fac3994ed2
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/7511405
    Reviewed-by: Nikolay Vitkov <nvitkov@chromium.org>
    Commit-Queue: Nikolay Vitkov <nvitkov@chromium.org>
    Auto-Submit: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>

--- front_end/panels/recorder/components/ReplaySection.ts
+++ front_end/panels/recorder/components/ReplaySection.ts
@@ -1,7 +1,6 @@
 // Copyright 2023 The Chromium Authors
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-/* eslint-disable @devtools/no-lit-render-outside-of-view */
 
 import * as Host from '../../../core/host/host.js';
 import * as i18n from '../../../core/i18n/i18n.js';
@@ -185,7 +184,6 @@ export const DEFAULT_VIEW = (input: ViewInput, _output: ViewOutput, target: HTML
         </devtools-button>
       </div>`,
     target,
-    { host: target },
   );
   // clang-format on
 };

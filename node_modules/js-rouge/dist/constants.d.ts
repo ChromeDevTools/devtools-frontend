869857a23a78d624adba9f8d1904f52e4c10ea48
Kim-Anh Tran <kimanh@chromium.org>
2026-01-26 10:24:28 +0100

    [node_modules] Add rouge package for ROUGE ratings in DevTools evals
    This adds a helper package for computing ROUGE scores for evaluating
    AI Assistance model outputs.
    
    Bug: 475195894
    Change-Id: I33381670c5fc03ef4d09cb043e0bbb4658aea273
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/7510758
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Kim-Anh Tran <kimanh@chromium.org>

---
+++ node_modules/js-rouge/dist/constants.d.ts
@@ -0,0 +1,10 @@
+export declare const TREEBANK_CONTRACTIONS: RegExp[];
+export declare const HONORIFICS: string[];
+export declare const ABBR_COMMON: string[];
+export declare const ABBR_ORGANIZATIONS: string[];
+export declare const ABBR_PLACES: string[];
+export declare const ABBR_TIME: string[];
+export declare const ABBR_DATES: string[];
+export declare const GATE_EXCEPTIONS: string[];
+export declare const GATE_SUBSTITUTIONS: string[];
+//# sourceMappingURL=constants.d.ts.map

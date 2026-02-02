e1ae94a873d98de0ad8ce596b5d62ccbc5f5f574
Nikolay Vitkov <nvitkov@chromium.org>
2026-02-02 18:21:09 +0100

    Lint all files all the time
    We should lint all files in the repo to ensure that we don't get
    sideeffect errors coming from EsLint TypeScript or our rules that
    user Type information.
    
    We will pay a small price locally of around ~10 sec, compare to
    the previous approach of running it only for specific files.
    
    Bug: 480874485
    Change-Id: I87b6760975516aea257572a0d3d25ac7d154c4ba
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/7531350
    Commit-Queue: Nikolay Vitkov <nvitkov@chromium.org>
    Reviewed-by: Liviu Rau <liviurau@chromium.org>
    Reviewed-by: Simon Zünd <szuend@chromium.org>

--- scripts/test/run_lint_check.mjs
+++ scripts/test/run_lint_check.mjs
@@ -352,7 +352,9 @@ async function run() {
     }
   }
 
-  const frontEndFiles = scripts.filter(script => script.includes('front_end'));
+  const frontEndFiles = scripts.filter(
+    script => script.includes('front_end') && !script.endsWith('.test.ts'),
+  );
   const esLintRules = scripts.filter(script =>
     script.includes('scripts/eslint_rules')
   );

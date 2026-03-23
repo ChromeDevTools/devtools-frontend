756fa3310a89c31c9664c6949aa9a05af968d2db
Elorm Coch <elormcoch@microsoft.com>
2026-03-23 08:58:55 -0700

    Improve garbage collection UI feedback by adding snackbar notification on completion
    Bug: 493708151
    Change-Id: I0bdb1256323b6f04625a190b3839b4337d8c44c5
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/7683481
    Reviewed-by: Kim-Anh Tran <kimanh@chromium.org>
    Commit-Queue: Sylvester Elorm Coch <elormcoch@microsoft.com>

--- front_end/ui/legacy/components/perf_ui/GCActionDelegate.ts
+++ front_end/ui/legacy/components/perf_ui/GCActionDelegate.ts
@@ -2,15 +2,31 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import * as i18n from '../../../../core/i18n/i18n.js';
 import * as SDK from '../../../../core/sdk/sdk.js';
+import * as Snackbars from '../../../components/snackbars/snackbars.js';
 import type * as UI from '../../legacy.js';
+
+const UIStrings = {
+  /**
+   * @description Notification message shown to the user when garbage collection has completed.
+   */
+  garbageCollectionCompleted: 'Garbage collection completed',
+} as const;
+
+const str_ = i18n.i18n.registerUIStrings('ui/legacy/components/perf_ui/GCActionDelegate.ts', UIStrings);
+const i18nString = i18n.i18n.getLocalizedString.bind(undefined, str_);
 
 export class GCActionDelegate implements UI.ActionRegistration.ActionDelegate {
   handleAction(_context: UI.Context.Context, _actionId: string): boolean {
-    for (const heapProfilerModel of SDK.TargetManager.TargetManager.instance().models(
-             SDK.HeapProfilerModel.HeapProfilerModel)) {
-      void heapProfilerModel.collectGarbage();
-    }
+    const promises = SDK.TargetManager.TargetManager.instance()
+                         .models(SDK.HeapProfilerModel.HeapProfilerModel)
+                         .map(heapProfilerModel => heapProfilerModel.collectGarbage());
+
+    void Promise.all(promises).then(() => {
+      Snackbars.Snackbar.Snackbar.show({message: i18nString(UIStrings.garbageCollectionCompleted)});
+    });
+
     return true;
   }
 }

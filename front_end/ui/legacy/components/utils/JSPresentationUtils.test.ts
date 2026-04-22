dbf870d7d479d8a23d4d7847726c5954c1e4482a
Simon Zünd <szuend@chromium.org>
2026-04-22 06:10:33 +0000

    Add ignore-listing test to JSPresentationUtils.test.ts
    This is currently tested only in ConsoleViewMessage.test.ts but the
    "Show more" / "Show less" buttons should really be tested here.
    
    Drive-by: Migrate to TestUniverse for more isolation.
    
    R=alexrudenko@chromium.org
    
    Bug: 485140575
    Change-Id: I7e82b0732d023b2167633ab40ac5174f6ff70304
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/7785350
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Eric Leese <leese@chromium.org>

--- front_end/ui/legacy/components/utils/JSPresentationUtils.test.ts
+++ front_end/ui/legacy/components/utils/JSPresentationUtils.test.ts
@@ -2,32 +2,35 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import * as Platform from '../../../../core/platform/platform.js';
 import type * as SDK from '../../../../core/sdk/sdk.js';
 import type * as Protocol from '../../../../generated/protocol.js';
-import * as Bindings from '../../../../models/bindings/bindings.js';
+import type * as Bindings from '../../../../models/bindings/bindings.js';
 import * as StackTrace from '../../../../models/stack_trace/stack_trace.js';
-import * as Workspace from '../../../../models/workspace/workspace.js';
 import {renderElementIntoDOM} from '../../../../testing/DOMHelpers.js';
-import {createTarget} from '../../../../testing/EnvironmentHelpers.js';
-import {describeWithMockConnection} from '../../../../testing/MockConnection.js';
+import {setupLocaleHooks} from '../../../../testing/LocaleHelpers.js';
+import {setupRuntimeHooks} from '../../../../testing/RuntimeHelpers.js';
+import {setupSettingsHooks} from '../../../../testing/SettingsHelpers.js';
+import {TestUniverse} from '../../../../testing/TestUniverse.js';
 
 import * as Components from './utils.js';
 
-describeWithMockConnection('JSPresentationUtils', () => {
+const {urlString} = Platform.DevToolsPath;
+
+describe('JSPresentationUtils', () => {
+  setupRuntimeHooks();
+  setupSettingsHooks();
+  setupLocaleHooks();
+
+  let universe: TestUniverse;
+
+  beforeEach(() => {
+    universe = new TestUniverse();
+  });
+
   function setUpEnvironment() {
-    const target = createTarget();
-    const workspace = Workspace.Workspace.WorkspaceImpl.instance();
-    const targetManager = target.targetManager();
-    const resourceMapping = new Bindings.ResourceMapping.ResourceMapping(targetManager, workspace);
-    const ignoreListManager = Workspace.IgnoreListManager.IgnoreListManager.instance({forceNew: true});
-    const debuggerWorkspaceBinding = Bindings.DebuggerWorkspaceBinding.DebuggerWorkspaceBinding.instance({
-      forceNew: true,
-      resourceMapping,
-      targetManager,
-      ignoreListManager,
-      workspace,
-    });
-    return {target, debuggerWorkspaceBinding};
+    const target = universe.createTarget({});
+    return {target, debuggerWorkspaceBinding: universe.debuggerWorkspaceBinding};
   }
 
   async function createStackTrace(
@@ -49,7 +52,7 @@ describeWithMockConnection('JSPresentationUtils', () => {
     const {target, debuggerWorkspaceBinding} = setUpEnvironment();
     const stackTrace = await createStackTrace(target, debuggerWorkspaceBinding);
     const component = new Components.JSPresentationUtils.StackTracePreviewContent();
-    component.options = {tabStops: false};
+    component.options = {tabStops: false, ignoreListManager: universe.ignoreListManager};
     component.stackTrace = stackTrace;
     await component.updateComplete;
 
@@ -71,7 +74,7 @@ describeWithMockConnection('JSPresentationUtils', () => {
     const stackTrace = await createStackTrace(target, debuggerWorkspaceBinding);
 
     const component = new Components.JSPresentationUtils.StackTracePreviewContent();
-    component.options = {expandable: true};
+    component.options = {expandable: true, ignoreListManager: universe.ignoreListManager};
     renderElementIntoDOM(component);
     assert.isFalse(component.hasContent());
     component.stackTrace = stackTrace;
@@ -87,5 +90,47 @@ describeWithMockConnection('JSPresentationUtils', () => {
       '\tbar\t@\tbar.js:2',
       '\tbaz\t@\tbaz.js:3',
     ]);
+  });
+
+  it('toggles ignore-listed frames when clicking Show more/less', async () => {
+    const {target, debuggerWorkspaceBinding} = setUpEnvironment();
+    const stackTrace = await createStackTrace(target, debuggerWorkspaceBinding);
+
+    const ignoreListManager = universe.ignoreListManager;
+    ignoreListManager.ignoreListURL(urlString`bar.js`);
+
+    const component = new Components.JSPresentationUtils.StackTracePreviewContent();
+    component.options = {expandable: true, ignoreListManager};
+    renderElementIntoDOM(component);
+
+    component.stackTrace = stackTrace;
+    await component.updateComplete;
+
+    // Expand to see frames
+    const expandButton = component.contentElement.querySelector('button');
+    assert.exists(expandButton);
+    expandButton.click();
+    await component.updateComplete;
+
+    // Initially show-hidden-rows should be false
+    assert.isFalse(component.element.classList.contains('show-hidden-rows'));
+
+    // Find and click "Show more"
+    const showMoreLink = component.contentElement.querySelector('.show-all-link .link') as HTMLElement;
+    assert.exists(showMoreLink);
+    showMoreLink.click();
+    await component.updateComplete;
+
+    // Now show-hidden-rows should be true
+    assert.isTrue(component.element.classList.contains('show-hidden-rows'));
+
+    // Find and click "Show less"
+    const showLessLink = component.contentElement.querySelector('.show-less-link .link') as HTMLElement;
+    assert.exists(showLessLink);
+    showLessLink.click();
+    await component.updateComplete;
+
+    // Now show-hidden-rows should be false again
+    assert.isFalse(component.element.classList.contains('show-hidden-rows'));
   });
 });

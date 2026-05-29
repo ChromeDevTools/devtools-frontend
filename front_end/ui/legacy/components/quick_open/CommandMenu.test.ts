adb4f611491e3bd68ab8b7184a36a20ba5e39185
Nikolay Vitkov <nvitkov@chromium.org>
2026-05-29 13:31:28 +0200

    Migrate command-menu legacy test to unit test
    Chromium CL to remove test - https://crrev.com/c/7885338
    
    Bug: none
    Change-Id: I9f7884196094466c2c44c507b08cdc96d405e20f
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/7882785
    Reviewed-by: Nicholas Roscino <nroscino@chromium.org>
    Commit-Queue: Nikolay Vitkov <nvitkov@chromium.org>

--- front_end/ui/legacy/components/quick_open/CommandMenu.test.ts
+++ front_end/ui/legacy/components/quick_open/CommandMenu.test.ts
@@ -12,6 +12,7 @@ import {
 } from '../../../../testing/EnvironmentHelpers.js';
 import {setupLocaleHooks} from '../../../../testing/LocaleHelpers.js';
 import {render} from '../../../../ui/lit/lit.js';
+import * as UI from '../../legacy.js';
 
 import * as QuickOpen from './quick_open.js';
 
@@ -145,5 +146,65 @@ describe('CommandMenu', () => {
             command.category === Common.Settings.getLocalizedSettingsCategory(settingCategory));
     assert.lengthOf(disableSettingCommands, 1, 'Commands for changing a setting\'s value were not added correctly');
     assert.lengthOf(enableSettingCommands, 1, 'Commands for changing a setting\'s value were not added correctly');
+  });
+});
+
+describe('CommandMenu Views', () => {
+  const viewId = 'mock-view';
+  const viewTitle = 'Mock view';
+  const commandPrompt = 'Show mock view';
+  const category = UI.ViewManager.ViewLocationCategory.PANEL;
+
+  before(async () => {
+    UI.ViewManager.registerLocationResolver({
+      name: UI.ViewManager.ViewLocationValues.PANEL,
+      category,
+      async loadResolver() {
+        return {
+          resolveLocation() {
+            return null;
+          }
+        };
+      }
+    });
+
+    UI.ViewManager.registerViewExtension({
+      location: UI.ViewManager.ViewLocationValues.PANEL,
+      id: viewId,
+      title: () => viewTitle as Platform.UIString.LocalizedString,
+      commandPrompt: () => commandPrompt as Platform.UIString.LocalizedString,
+      async loadView() {
+        return new UI.Widget.Widget();
+      },
+    });
+
+    await initializeGlobalVars({reset: false});
+  });
+
+  after(async () => {
+    await deinitializeGlobalVars();
+  });
+
+  it('adds commands for showing a view', async () => {
+    const allCommands = QuickOpen.CommandMenu.CommandMenu.instance({forceNew: true}).commands();
+    const viewCommands = allCommands.filter(
+        command => command.title === commandPrompt &&
+            command.category === UI.ViewManager.getLocalizedViewLocationCategory(category));
+    assert.lengthOf(viewCommands, 1, 'Commands for showing a view were not added correctly');
+  });
+
+  it('executes a command for showing a view', async () => {
+    const allCommands = QuickOpen.CommandMenu.CommandMenu.instance({forceNew: true}).commands();
+    const viewCommand = allCommands.find(
+        command => command.title === commandPrompt &&
+            command.category === UI.ViewManager.getLocalizedViewLocationCategory(category));
+    assert.exists(viewCommand);
+
+    const showViewStub = sinon.stub(UI.ViewManager.ViewManager.instance(), 'showView').resolves();
+
+    viewCommand.execute();
+
+    sinon.assert.calledOnceWithExactly(showViewStub, viewId, true);
+    showViewStub.restore();
   });
 });

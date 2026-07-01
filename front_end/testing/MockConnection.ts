a0a9fcc255b245f33135673b6154bdabdee46905
Simon Zünd <szuend@chromium.org>
2026-07-01 04:39:54 +0000

    Deprecate 'describeWithMockConnection' via JSDoc comment
    R=kimanh@chromium.org
    
    No-Try: true
    Bug: 493763857
    Change-Id: Icf3f2c02769c82837efa9b1003966863f0c7ef8b
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/8025658
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Kim-Anh Tran <kimanh@chromium.org>
    Auto-Submit: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Kim-Anh Tran <kimanh@chromium.org>

--- front_end/testing/MockConnection.ts
+++ front_end/testing/MockConnection.ts
@@ -43,6 +43,10 @@ async function disable() {
   await deinitializeGlobalVars();
 }
 
+/**
+ * @deprecated use `describeWithEnvironment` instead. They are near equivalent.
+ * `describeWithMockConnection` cleans up DOM and waits one more animation frame
+ */
 export function describeWithMockConnection(title: string, fn: (this: Mocha.Suite) => void, opts: {reset: boolean} = {
   reset: true,
 }) {

1af8f75c9aa806eab89fa7b421c75d24aa260c27
Philip Pfaffe <pfaffe@chromium.org>
2026-08-05 11:45:52 +0000

    [extensions] Check embedder name for scripts with sourceURL comment
    Embedder name is currently the only source of the "original" url for a
    script with sourceURL. We'll look into adding a more "official" field in
    a followup.
    
    Fixed: 533047468
    Change-Id: I4b58772a315fdf4b419a5203d570f10d8fa43bf4
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/8200685
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Philip Pfaffe <pfaffe@chromium.org>

--- front_end/panels/common/ExtensionServer.test.ts
+++ front_end/panels/common/ExtensionServer.test.ts
@@ -1009,6 +1009,58 @@ describe('Runtime hosts policy', () => {
     assert.deepEqual(resources.map(r => r.url), [blockedUrl, allowedUrl]);
   });
 
+  it('blocks scripts with sourceURL comments if the embedderName is a blocked URL', async () => {
+    const target = getBackend(context).createTarget({id: 'target' as Protocol.Target.TargetID});
+    target.setInspectedURL(allowedUrl);
+
+    const script = sinon.createStubInstance(SDK.Script.Script, {target, contentURL: allowedUrl});
+    script.hasSourceURL = true;
+    script.embedderName.returns(blockedUrl);
+    const workspaceBinding = sinon.createStubInstance(Bindings.DebuggerWorkspaceBinding.DebuggerWorkspaceBinding);
+    workspaceBinding.scriptsForUISourceCode.callsFake(uiSourceCode => {
+      if (uiSourceCode.contentURL() === allowedUrl) {
+        return [script];
+      }
+      return [];
+    });
+    sinon.stub(Bindings.DebuggerWorkspaceBinding.DebuggerWorkspaceBinding, 'instance').returns(workspaceBinding);
+    const project = new Bindings.ContentProviderBasedProject.ContentProviderBasedProject(
+        Workspace.Workspace.WorkspaceImpl.instance(), target.id(), Workspace.Workspace.projectTypes.Network, '',
+        false /* isServiceProject */);
+    await createUISourceCode(project, allowedUrl, Common.ResourceType.resourceTypes.Script);
+
+    assert.exists(context.chrome.devtools);
+    const resources =
+        await new Promise<Chrome.DevTools.Resource[]>(r => context.chrome.devtools?.inspectedWindow.getResources(r));
+    assert.deepEqual(resources.map(r => r.url), []);
+  });
+
+  it('allows scripts with sourceURL comments if the embedderName is not a URL', async () => {
+    const target = getBackend(context).createTarget({id: 'target' as Protocol.Target.TargetID});
+    target.setInspectedURL(allowedUrl);
+
+    const script = sinon.createStubInstance(SDK.Script.Script, {target, contentURL: allowedUrl});
+    script.hasSourceURL = true;
+    script.embedderName.returns(urlString`eval`);
+    const workspaceBinding = sinon.createStubInstance(Bindings.DebuggerWorkspaceBinding.DebuggerWorkspaceBinding);
+    workspaceBinding.scriptsForUISourceCode.callsFake(uiSourceCode => {
+      if (uiSourceCode.contentURL() === allowedUrl) {
+        return [script];
+      }
+      return [];
+    });
+    sinon.stub(Bindings.DebuggerWorkspaceBinding.DebuggerWorkspaceBinding, 'instance').returns(workspaceBinding);
+    const project = new Bindings.ContentProviderBasedProject.ContentProviderBasedProject(
+        Workspace.Workspace.WorkspaceImpl.instance(), target.id(), Workspace.Workspace.projectTypes.Network, '',
+        false /* isServiceProject */);
+    await createUISourceCode(project, allowedUrl, Common.ResourceType.resourceTypes.Script);
+
+    assert.exists(context.chrome.devtools);
+    const resources =
+        await new Promise<Chrome.DevTools.Resource[]>(r => context.chrome.devtools?.inspectedWindow.getResources(r));
+    assert.deepEqual(resources.map(r => r.url), [allowedUrl]);
+  });
+
   const requestToManager = new Map<SDK.NetworkRequest.NetworkRequest, SDK.NetworkManager.NetworkManager>();
 
   function createRequest(

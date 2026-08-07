e3b1a44c39d1d56bf2b41643bd65d04be6a40217
Natallia Harshunova <nharshunova@chromium.org>
2026-08-07 09:21:52 +0000

    Add HostConfigDevToolsComments
    Fixed: 542930179
    Change-Id: I8027120fb49ab01d045057a5cd94a2eb978c913e
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/8222847
    Auto-Submit: Natallia Harshunova <nharshunova@chromium.org>
    Commit-Queue: Wolfgang Beyer <wolfi@chromium.org>
    Reviewed-by: Wolfgang Beyer <wolfi@chromium.org>
    Commit-Queue: Natallia Harshunova <nharshunova@chromium.org>

--- front_end/core/root/Runtime.ts
+++ front_end/core/root/Runtime.ts
@@ -540,6 +540,9 @@ interface DevToolsPlusButton {
 interface DevToolsInstrumentationBreakpoints {
   enabled: boolean;
 }
+interface HostConfigDevToolsComments {
+  enabled: boolean;
+}
 
 /**
  * The host configuration that we expect from the DevTools back-end.
@@ -599,6 +602,7 @@ export type HostConfig = Platform.TypeScriptUtilities.RecursivePartial<{
   devToolsPlusButton: DevToolsPlusButton,
   devToolsInstrumentationBreakpoints: DevToolsInstrumentationBreakpoints,
   extensionsOnChromeUrls: ExtensionsOnChromeUrls,
+  devToolsComments: HostConfigDevToolsComments,
 }>;
 
 /**

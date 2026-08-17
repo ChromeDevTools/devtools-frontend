06033544ac5564d3347ad06e2c53d456f14e04fd
Yang Guo <yangguo@chromium.org>
2026-08-17 06:06:58 +0000

    Ensure consistent UI Strings in front_end/ui/legacy/components/settings_ui
    Summary of changes:
    - Improve @description comments to be specific, informative, and
      non-redundant.
    - Ensure all @description comments end with a terminating period.
    
    Fixed: 547585161
    Change-Id: Idcd4c4de89670086a2bfa7ee8996b8055591b39e
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/8256774
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Yang Guo <yangguo@chromium.org>
    Auto-Submit: Yang Guo <yangguo@chromium.org>
    Commit-Queue: Simon Zünd <szuend@chromium.org>

--- front_end/ui/legacy/components/settings_ui/SettingsUI.ts
+++ front_end/ui/legacy/components/settings_ui/SettingsUI.ts
@@ -19,11 +19,11 @@ const {createRef, ref} = Directives;
 
 const UIStrings = {
   /**
-   * @description Note when a setting change will require the user to reload DevTools
+   * @description Warning note displayed below a setting dropdown when changing the setting requires reloading DevTools.
    */
   srequiresReload: '*Requires reload',
   /**
-   * @description Message to display if a setting change requires a reload of DevTools
+   * @description Message displayed in a warning bar when a setting change requires reloading DevTools.
    */
   settingsChangedReloadDevTools: 'Settings changed. To apply, reload DevTools.',
 } as const;

9cbbf24d9c48f92697e8d76b556217cd194440ae
Simon Zünd <szuend@chromium.org>
2026-09-03 09:54:13 +0000

    [ts_library] Migrate json5 to split compilation
    Migrate front_end/third_party/json5 to TypeScript split compilation
    (--isolatedDeclarations).
    
    Bug: 547522497
    Change-Id: I49274bbb5445d6f630762d2211535195486df0aa
    TAG=agy
    CONV=7d2a6a7a-0568-46ec-a637-63b21f1a8544
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/8353043
    Commit-Queue: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Mathias Bynens <mathias@chromium.org>
    Auto-Submit: Simon Zünd <szuend@chromium.org>
    Reviewed-by: Mathias Bynens <mathias@chromium.org>

--- front_end/third_party/json5/json5.ts
+++ front_end/third_party/json5/json5.ts
@@ -1,4 +1,4 @@
 import JSON5 from './lib/index.js';
 
-export const parse = JSON5.parse;
-export const stringify = JSON5.stringify;
+export const parse: typeof JSON5.parse = JSON5.parse;
+export const stringify: typeof JSON5.stringify = JSON5.stringify;

96a9ecb4a562e24b703641dc439512d4b681e3af
Alex Rudenko <alexrudenko@chromium.org>
2026-09-07 16:00:22 +0200

    Remove duplicate diff patch types
    They break chrome-devtools-mcp build.
    
    Change-Id: Id14cd8fdcf0a8336539a60baeebd18f6c8b11f1f
    Bug: none
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/8367285
    Reviewed-by: Natallia Harshunova <nharshunova@chromium.org>
    Auto-Submit: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Natallia Harshunova <nharshunova@chromium.org>

--- front_end/third_party/diff/DiffWrapper.ts
+++ front_end/third_party/diff/DiffWrapper.ts
@@ -4,14 +4,7 @@
 
 import * as Common from '../../core/common/common.js';
 
-declare global {
-  /* eslint-disable @typescript-eslint/naming-convention */
-  class diff_match_patch {
-    diff_main(text1: string, text2: string): Array<{0: number, 1: string}>;
-    diff_cleanupSemantic(diff: Array<{0: number, 1: string}>): void;
-  }
-  /* eslint-enable @typescript-eslint/naming-convention */
-}
+import './diff_match_patch.js';
 
 export const DiffWrapper = {
   charDiff: function(text1: string, text2: string, cleanup?: boolean): {0: number, 1: string}[] {

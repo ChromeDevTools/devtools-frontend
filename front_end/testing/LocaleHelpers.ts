dec4fd9e7515ca2a81164378a9a07a318f7697a1
Simon Zünd <szuend@chromium.org>
2026-09-14 08:05:48 +0000

    [testing] Fix EnUsLocaleData compilation issue
    Generating the en-US.json was only a data_deps so the file might have
    been truncated and empty by the time //front_end/testing is
    type-checked.
    
    By promoting it to a `deps` entry, we make sure the full en-US.json is
    written and tsc can find the JSON file to infer the type from it.
    
    R=alexrudenko@chromium.org
    No-Tree-Checks: true
    Bug: None
    Change-Id: Ic48644e14547bf0bc48d85db43bd51389747c754
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/8399191
    Auto-Submit: Simon Zünd <szuend@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>

--- front_end/testing/LocaleHelpers.ts
+++ front_end/testing/LocaleHelpers.ts
@@ -3,9 +3,7 @@
 // found in the LICENSE file.
 
 import * as i18n from '../core/i18n/i18n.js';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore TSC is not able to find type definitions but this error does not always happen
-// eslint-disable-next-line  @devtools/es-modules-import
+// eslint-disable-next-line @devtools/es-modules-import
 import EnUsLocaleData from '../core/i18n/locales/en-US.json' with {type : 'json'};
 
 export async function initializeGlobalLocaleVars(): Promise<void> {

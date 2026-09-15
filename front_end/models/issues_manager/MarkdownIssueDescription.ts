e1578a6c62c0195ff9a791ad295739c638ab3f92
Alex Rudenko <alexrudenko@chromium.org>
2026-09-15 09:25:26 +0000

    Migrate models/issues_manager to devtools_foundation_module
    Bug: 505974404
    Change-Id: If65fd5de889efe1d1fd8e1286a1531508b150c3f
    TAG=agy
    CONV=42028151-9082-411d-88bf-cd020d0ee3a6
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/8397694
    Reviewed-by: Simon Zünd <szuend@chromium.org>
    Auto-Submit: Alex Rudenko <alexrudenko@chromium.org>
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>

--- front_end/models/issues_manager/MarkdownIssueDescription.ts
+++ front_end/models/issues_manager/MarkdownIssueDescription.ts
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import * as Platform from '../../core/platform/platform.js';
 import * as Marked from '../../third_party/marked/marked.js';
 
 /**
@@ -57,8 +58,7 @@ export interface IssueDescription {
 
 export async function getFileContent(url: URL): Promise<string> {
   try {
-    const response = await fetch(url.toString());
-    return await response.text();
+    return await Platform.HostRuntime.HOST_RUNTIME.loadTextFile(url);
   } catch {
     throw new Error(
         `Markdown file ${url.toString()} not found. Make sure it is correctly listed in the relevant BUILD.gn files.`);
